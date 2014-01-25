opendaylight/controller

Extract dom-serializer-api|impl from binding-broker-impl
Change-Id: I1b2844bfac5062fd15c1f011c3104b3dddb33b51
Signed-off-by: Tony Tkacik <ttkacik@cisco.com>
Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/sal/dom/broker/osgi/SchemaServiceActivator.java
@@ -1,0 +1,30 @@
+package org.opendaylight.controller.sal.dom.broker.osgi;
+
+import java.util.Hashtable;
+
+import org.opendaylight.controller.sal.core.api.model.SchemaService;
+import org.opendaylight.controller.sal.dom.broker.SchemaServiceImpl;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class SchemaServiceActivator implements BundleActivator {
+
+    
+    private ServiceRegistration<SchemaService> schemaServiceReg;
+    private SchemaServiceImpl schemaService;
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        schemaService = new SchemaServiceImpl();
+        schemaService.setContext(context);
+        schemaService.start();
+        schemaServiceReg = context.registerService(SchemaService.class, schemaService, new Hashtable<String,String>());
+    }
+    
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        schemaServiceReg.unregister();
+        schemaService.close();
+    }
+}

opendaylight/mdsal

opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/sal/dom/broker/osgi/SchemaServiceActivator.java
@@ -1,0 +1,30 @@
+package org.opendaylight.controller.sal.dom.broker.osgi;
+
+import java.util.Hashtable;
+
+import org.opendaylight.controller.sal.core.api.model.SchemaService;
+import org.opendaylight.controller.sal.dom.broker.GlobalBundleScanningSchemaServiceImpl;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class SchemaServiceActivator implements BundleActivator {
+
+    
+    private ServiceRegistration<SchemaService> schemaServiceReg;
+    private GlobalBundleScanningSchemaServiceImpl schemaService;
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        schemaService = new GlobalBundleScanningSchemaServiceImpl();
+        schemaService.setContext(context);
+        schemaService.start();
+        schemaServiceReg = context.registerService(SchemaService.class, schemaService, new Hashtable<String,String>());
+    }
+    
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        schemaServiceReg.unregister();
+        schemaService.close();
+    }
+}