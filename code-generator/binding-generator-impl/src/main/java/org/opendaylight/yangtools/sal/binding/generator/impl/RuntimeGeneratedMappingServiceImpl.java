opendaylight/yangtools

BUG-981: remove deprecated elements
This patch removes deprecated constructor and setters, such that the
RuntimeGeneratedMappingServiceImpl can be instantiated in one go.

Change-Id: I7941479291210be9b1ee37919feae055c63c228c
Signed-off-by: Robert Varga <rovarga@cisco.com>

code-generator/binding-generator-impl/src/main/java/org/opendaylight/yangtools/sal/binding/generator/impl/RuntimeGeneratedMappingServiceImpl.java
@@ -81,22 +81,11 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
     private final HashMultimap<Type, SettableFuture<Type>> promisedTypes = HashMultimap.create();
     private final ClassLoadingStrategy classLoadingStrategy;
 
-    // FIXME: will become final
-    private ClassPool pool;
-    private TransformerGenerator binding;
-    private LazyGeneratedCodecRegistry registry;
+    private final LazyGeneratedCodecRegistry registry;
+    private final TransformerGenerator binding;
+    private final ClassPool pool;
 
     private SchemaContext schemaContext;
-
-    @Deprecated
-    public RuntimeGeneratedMappingServiceImpl() {
-        this(GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy());
-    }
-
-    @Deprecated
-    public RuntimeGeneratedMappingServiceImpl(final ClassLoadingStrategy strat) {
-        classLoadingStrategy = strat;
-    }
 
     public RuntimeGeneratedMappingServiceImpl(final ClassPool pool) {
         this(pool, GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy());
@@ -106,11 +95,6 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
         this.pool = Preconditions.checkNotNull(pool);
         this.classLoadingStrategy = Preconditions.checkNotNull(strat);
 
-        // FIXME: merge into constructor once legacy init() is removed
-        doInit();
-    }
-
-    private void doInit() {
         binding = new TransformerGenerator(pool);
         registry = new LazyGeneratedCodecRegistry(this, classLoadingStrategy);
 
@@ -125,11 +109,6 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
         // listenerRegistration = ctx.registerService(SchemaServiceListener,
         // this, new Hashtable<String, String>());
         // }
-    }
-
-    @Deprecated
-    public void setPool(final ClassPool pool) {
-        this.pool = pool;
     }
 
     @Override
@@ -329,10 +308,6 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
         }
     }
 
-    @Deprecated
-    public void init() {
-        doInit();
-    }
 
     @Override
     public Set<QName> getRpcQNamesFor(final Class<? extends RpcService> service) {

opendaylight/mdsal

code-generator/binding-generator-impl/src/main/java/org/opendaylight/yangtools/sal/binding/generator/impl/RuntimeGeneratedMappingServiceImpl.java
@@ -89,10 +89,9 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
 
     private final ClassLoadingStrategy classLoadingStrategy;
 
-    // FIXME: will become final
-    private ClassPool pool;
-    private AbstractTransformerGenerator binding;
-    private LazyGeneratedCodecRegistry registry;
+    private final AbstractTransformerGenerator binding;
+    private final LazyGeneratedCodecRegistry registry;
+    private final ClassPool pool;
 
     /*
      * FIXME: updated here, access from AbstractTransformer
@@ -106,16 +105,6 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
     private final ConcurrentMap<Type, Type> typeDefinitions = new ConcurrentHashMap<>();
     private SchemaContext schemaContext;
 
-    @Deprecated
-    public RuntimeGeneratedMappingServiceImpl() {
-        this(GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy());
-    }
-
-    @Deprecated
-    public RuntimeGeneratedMappingServiceImpl(final ClassLoadingStrategy strat) {
-        classLoadingStrategy = strat;
-    }
-
     public RuntimeGeneratedMappingServiceImpl(final ClassPool pool) {
         this(pool, GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy());
     }
@@ -124,11 +113,6 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
         this.pool = Preconditions.checkNotNull(pool);
         this.classLoadingStrategy = Preconditions.checkNotNull(strat);
 
-        // FIXME: merge into constructor once legacy init() is removed
-        doInit();
-    }
-
-    private void doInit() {
         binding = new TransformerGenerator(this, pool);
         registry = new LazyGeneratedCodecRegistry(this, binding, classLoadingStrategy);
         binding.setListener(registry);
@@ -137,11 +121,6 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
         // listenerRegistration = ctx.registerService(SchemaServiceListener,
         // this, new Hashtable<String, String>());
         // }
-    }
-
-    @Deprecated
-    public void setPool(final ClassPool pool) {
-        this.pool = pool;
     }
 
     @Override
@@ -352,10 +331,6 @@ public class RuntimeGeneratedMappingServiceImpl implements BindingIndependentMap
         }
     }
 
-    @Deprecated
-    public void init() {
-        doInit();
-    }
 
     @Override
     public Set<QName> getRpcQNamesFor(final Class<? extends RpcService> service) {