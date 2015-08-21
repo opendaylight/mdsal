d57802123bf4d01f8c7e5bf09ffc48f71a793034
Tony Tkacik <ttkacik@cisco.com>
2015-08-21 12:45:19 +0200

    Cleaned up sal-dom-* packages and removed legacy interfaces
     - Removed deprecated interfaces from mdsal-common-api,
       some of this interfaces will be present in controller
       for backwards compatibility
    
     - Changed groupId to org.opendaylight.mdsal
     - Changed artifactId from sal-core to msdal-dom
    
    Change-Id: I222bee07e5fd13fe10d19f01ba4f9bdf102bf3e5
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>
    
    DOM Cleanup
    
    Change-Id: I175e854817dd9c847e0badca25ee86cee59f8f23
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ dom/mdsal-dom-inmemory-datastore/src/test/java/org/opendaylight/controller/md/sal/dom/store/impl/SchemaUpdateForTransactionTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.store.impl;
+
+import static org.junit.Assert.assertNotNull;
+
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.concurrent.ExecutionException;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.opendaylight.controller.sal.core.spi.data.DOMStoreReadWriteTransaction;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class SchemaUpdateForTransactionTest {
+
+    private static final YangInstanceIdentifier TOP_PATH = YangInstanceIdentifier.of(TestModel.TEST_QNAME);
+    private SchemaContext schemaContext;
+    private InMemoryDOMDataStore domStore;
+
+    @Before
+    public void setupStore() {
+        domStore = new InMemoryDOMDataStore("TEST", MoreExecutors.newDirectExecutorService());
+        // loadSchemas(RockTheHouseInput.class);
+    }
+
+    public void loadSchemas(final Class<?>... classes) {
+        // YangModuleInfo moduleInfo;
+        // try {
+        // ModuleInfoBackedContext context = ModuleInfoBackedContext.create();
+        // for (Class<?> clz : classes) {
+        // moduleInfo = BindingReflections.getModuleInfo(clz);
+        //
+        // context.registerModuleInfo(moduleInfo);
+        // }
+        // schemaContext = context.tryToCreateSchemaContext().get();
+        // domStore.onGlobalContextUpdated(schemaContext);
+        // } catch (Exception e) {
+        // Throwables.propagateIfPossible(e);
+        // }
+    }
+
+    /**
+     * Test suite tests allocating transaction when schema context
+     * does not contain module necessary for client write,
+     * then triggering update of global schema context
+     * and then performing write (according to new module).
+     *
+     * If transaction between allocation and schema context was
+     * unmodified, it is safe to change its schema context
+     * to new one (e.g. it will be same as if allocated after
+     * schema context update.)
+     *
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    @Ignore
+    @Test
+    public void testTransactionSchemaUpdate() throws InterruptedException, ExecutionException {
+        // FIXME: Rewrite this test to be pure DOM only.
+        assertNotNull(domStore);
+
+        // We allocate transaction, initial schema context does not
+        // contain Lists model
+        final DOMStoreReadWriteTransaction writeTx = domStore.newReadWriteTransaction();
+        assertNotNull(writeTx);
+
+        // we trigger schema context update to contain Lists model
+        // loadSchemas(RockTheHouseInput.class, Top.class);
+
+        /**
+         *
+         * Writes /test in writeTx, this write should not fail
+         * with IllegalArgumentException since /test is in
+         * schema context.
+         *
+         */
+        // writeTx.write(TOP_PATH, ImmutableNodes.containerNode(Top.QNAME));
+
+    }
+
+}
