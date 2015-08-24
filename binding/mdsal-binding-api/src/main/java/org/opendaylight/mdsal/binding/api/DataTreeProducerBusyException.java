ff89a6bd712f633a1cfc3938afcb3c53ccd0de68
Tony Tkacik <ttkacik@cisco.com>
2015-08-24 16:53:16 +0200

    Bug 3867: Introduced Binding APIs for Data Tree Producer.
    Change-Id: I234581af7704446b0a3af64723528647b58505f0
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/DataTreeProducerBusyException.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.api;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Exception indicating that the {@link DataTreeProducer} has an open user transaction and cannot be
+ * closed.
+ */
+public class DataTreeProducerBusyException extends DataTreeProducerException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+
+    public DataTreeProducerBusyException(@Nonnull final String message, @Nonnull final Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataTreeProducerBusyException(@Nonnull final String message) {
+        super(message);
+    }
+
+}
