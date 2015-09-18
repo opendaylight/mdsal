8705dddccc2ea9cf61310cf99e611d329538ea0c
Michal Polkorab <michal.polkorab@pantheon.sk>
2015-09-18 16:10:59 +0200

    Bug 4327 - Fixed DataTreeChangeListener registration in PingPongDataBroker
     - delegate broker was incorrectly queried for DOMDataTreeChangeService
     - it must ask for supported extensions instead of instanceof
    
    Change-Id: I0310926893fc517bb1e1dca48d4966d6ffc18c74
    Signed-off-by: Michal Polkorab <michal.polkorab@pantheon.sk>

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/PingPongDataBroker.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/PingPongDataBroker.java
@@ -55,9 +55,11 @@ public final class PingPongDataBroker extends ForwardingDOMDataBroker implements
 
     @Override
     public <L extends DOMDataTreeChangeListener> ListenerRegistration<L> registerDataTreeChangeListener(final DOMDataTreeIdentifier treeId, final L listener) {
-        if (delegate instanceof DOMDataTreeChangeService) {
-            return ((DOMDataTreeChangeService)delegate).registerDataTreeChangeListener(treeId, listener);
-        }
+        final DOMDataTreeChangeService treeService =
+                (DOMDataTreeChangeService) delegate.getSupportedExtensions().get(DOMDataTreeChangeService.class);
+        if(treeService != null) {
+            return treeService.registerDataTreeChangeListener(treeId, listener);
+        } 
 
         throw new UnsupportedOperationException("Delegate " + delegate + " does not support required functionality");
     }
