ecebf61bc027c4d6c4bf9581bc4a574bb5d3a8b7
Tom Pantelis <tpanteli@brocade.com>
2015-09-28 10:34:16 -0400

    Add DOM entity ownership interfaces
    Change-Id: I7416d28efdfd10cad6468087f96765baa412ca13
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

---
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/clustering/DOMEntityOwnershipChange.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.api.clustering;
+
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.common.api.clustering.GenericEntityOwnershipChange;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+/**
+ * DOM version of {@link GenericEntityOwnershipChange}.
+ *
+ * @author Thomas Pantelis
+ */
+public class DOMEntityOwnershipChange extends GenericEntityOwnershipChange<YangInstanceIdentifier, DOMEntity> {
+
+    /**
+     * {@inheritDoc}
+     */
+    public DOMEntityOwnershipChange(@Nonnull DOMEntity entity, boolean wasOwner, boolean isOwner,
+            boolean hasOwner) {
+        super(entity, wasOwner, isOwner, hasOwner);
+    }
+}
