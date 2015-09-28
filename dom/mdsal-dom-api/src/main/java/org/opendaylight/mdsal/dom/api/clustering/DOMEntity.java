abd9cfd0f0e4aa46c4c8fe71630596e4d9676e9f
Tom Pantelis <tpanteli@brocade.com>
2015-09-28 07:29:51 -0400

    Add general-entity yang to common
    Change-Id: Iebf021d92a475174ceadb76895b5553b37df06ed
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/clustering/DOMEntity.java
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/clustering/DOMEntity.java
@@ -7,8 +7,11 @@
  */
 package org.opendaylight.mdsal.dom.api.clustering;
 
+import com.google.common.base.Preconditions;
 import javax.annotation.Nonnull;
 import org.opendaylight.mdsal.common.api.clustering.GenericEntity;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsal.core.general.entity.rev150820.Entity;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
 /**
@@ -19,9 +22,7 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 public class DOMEntity extends GenericEntity<YangInstanceIdentifier> {
     private static final long serialVersionUID = 1L;
 
-//    private static final QName ENTITY_QNAME =
-//            org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.core.general.entity.rev150820.Entity.QNAME;
-//    private static final QName ENTITY_NAME = QName.create(ENTITY_QNAME, "name");
+    private static final QName ENTITY_NAME = QName.create(Entity.QNAME, "name");
 
 
     /** Constructs an instance.
@@ -40,8 +41,8 @@ public class DOMEntity extends GenericEntity<YangInstanceIdentifier> {
      * @param type the type of the entity
      * @param entityName the name of the entity used to construct a general-entity YangInstanceIdentifier
      */
-//    public DOMEntity(@Nonnull String type, @Nonnull String entityName) {
-//        super(type, YangInstanceIdentifier.builder().node(ENTITY_QNAME).nodeWithKey(ENTITY_QNAME, ENTITY_NAME,
-//                Preconditions.checkNotNull(entityName, "entityName should not be null")).build());
-//    }
+    public DOMEntity(@Nonnull String type, @Nonnull String entityName) {
+        super(type, YangInstanceIdentifier.builder().node(Entity.QNAME).nodeWithKey(Entity.QNAME, ENTITY_NAME,
+                Preconditions.checkNotNull(entityName, "entityName should not be null")).build());
+    }
 }
