9b0d4e50e10f3ca347b3ceb4213fd3c8e96b7f01
Tom Pantelis <tpanteli@brocade.com>
2015-09-28 07:27:41 -0400

    Add binding entity ownership interfaces
    Change-Id: Ie79ae7a8a7df24b9f8869720dbe7ae6ae7555876
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

---
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/clustering/Entity.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.api.clustering;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.common.api.clustering.GenericEntity;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Binding version of {@link GenericEntity}.
+ *
+ * @author Thomas Pantelis
+ */
+@Beta
+public class Entity extends GenericEntity<InstanceIdentifier<?>> {
+    private static final long serialVersionUID = 1L;
+
+    /** Constructs an instance.
+     *
+     * @param type the entity type
+     * @param id the entity id.
+     */
+    public Entity(@Nonnull final String type, @Nonnull final InstanceIdentifier<?> id) {
+        super(type, id);
+    }
+
+    /**
+     * Construct an Entity with an with a name. The general-entity schema is used to construct the
+     * InstanceIdentifier.
+     *
+     * @param type the type of the entity
+     * @param entityName the name of the entity used to construct a general-entity InstanceIdentifier
+     */
+    // FIXME: needs to be enabled in a follow-up
+//    public Entity(@Nonnull String type, @Nonnull String entityName) {
+//        super(type, InstanceIdentifier.builder(org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.
+//                ns.yang.controller.md.sal.core.general.entity.rev150820.Entity.class,
+//                    new EntityKey(Preconditions.checkNotNull(entityName, "entityName should not be null"))).build());
+//    }
+}
