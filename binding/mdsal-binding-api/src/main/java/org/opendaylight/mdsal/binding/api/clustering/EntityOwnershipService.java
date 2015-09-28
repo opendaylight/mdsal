5a9fcd2f91d2b1522acdfd045db93e032774225e
Tom Pantelis <tpanteli@brocade.com>
2015-09-28 07:27:41 -0400

    Add binding entity ownership interfaces
    Change-Id: Ie79ae7a8a7df24b9f8869720dbe7ae6ae7555876
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

---
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/clustering/EntityOwnershipService.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.api.clustering;
+
+import com.google.common.base.Optional;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.common.api.clustering.CandidateAlreadyRegisteredException;
+import org.opendaylight.mdsal.common.api.clustering.EntityOwnershipState;
+import org.opendaylight.mdsal.common.api.clustering.GenericEntityOwnershipService;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Binding version of {@link GenericEntityOwnershipService}.
+ *
+ * @author Thomas Pantelis
+ */
+public interface EntityOwnershipService extends
+        GenericEntityOwnershipService<InstanceIdentifier<?>, Entity, EntityOwnershipListener> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    EntityOwnershipCandidateRegistration registerCandidate(@Nonnull Entity entity)
+            throws CandidateAlreadyRegisteredException;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    EntityOwnershipListenerRegistration registerListener(@Nonnull String entityType,
+            @Nonnull EntityOwnershipListener listener);
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    Optional<EntityOwnershipState> getOwnershipState(@Nonnull Entity forEntity);
+}
