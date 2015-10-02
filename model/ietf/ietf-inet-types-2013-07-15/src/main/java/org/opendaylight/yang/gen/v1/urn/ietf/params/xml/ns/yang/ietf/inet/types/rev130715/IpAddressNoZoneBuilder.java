665c9af32e6a29e4b0877c976a7bacb62b64bbe7
Wojciech Dec <wdec@cisco.com>
2015-10-02 11:55:11 +0200

    Bug890 - Adding ietf-inet-types 2013-07-15 yang model
    Change-Id: I195646ee95dd8b3e6b4a39b773e5eb6c462fe0ca
    Signed-off-by: Wojciech Dec <wdec@cisco.com>

---
+++ model/ietf/ietf-inet-types-2013-07-15/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/inet/types/rev130715/IpAddressNoZoneBuilder.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ */
+public class IpAddressNoZoneBuilder {
+
+    private static final Pattern IPV4_NO_ZONE_PATTERN =
+        Pattern.compile("[0-9\\.]*");
+    private static final Pattern IPV6_NO_ZONE_PATTERN1 =
+        Pattern.compile("[0-9a-fA-F:\\.]*");
+
+
+    public static IpAddressNoZone getDefaultInstance(final String defaultValue) {
+        final Matcher ipv4NoZoneMatcher = IPV4_NO_ZONE_PATTERN.matcher(defaultValue);
+
+        if (ipv4NoZoneMatcher.matches()) {
+            if (IPV6_NO_ZONE_PATTERN1.matcher(defaultValue).matches()) {
+                throw new IllegalArgumentException(
+                    String.format("Cannot create IpNoZoneAddress from \"%s\", matches both %s and %s",
+                        defaultValue, Ipv4AddressNoZone.class.getSimpleName(), Ipv6AddressNoZone.class.getSimpleName()));
+
+            }
+            return new IpAddressNoZone((new Ipv4AddressNoZone(defaultValue)));
+        } else if (IPV6_NO_ZONE_PATTERN1.matcher(defaultValue).matches()) {
+            return new IpAddressNoZone((new Ipv6AddressNoZone(defaultValue)));
+        } else {
+            throw new IllegalArgumentException("Cannot create IpAddress from " + defaultValue);
+        }
+    }
+
+}
