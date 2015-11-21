f0deb1ea87a192ccb19cf9e8b24f0c967870264a
Robert Varga <rovarga@cisco.com>
2015-11-21 01:36:52 +0100

    WIP: BUG-4638: disable check
    Change-Id: Iffd590668262f7e233d93302403d179e532fe4a0
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/yang/types/stmt/parser/retest/TypeProviderImplTest.java
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/yang/types/stmt/parser/retest/TypeProviderImplTest.java
@@ -11,23 +11,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
-import java.io.FileNotFoundException;
-import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
-import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
-
-import org.opendaylight.yangtools.sal.binding.yang.types.TypeProviderImpl;
-import com.google.common.base.Optional;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.opendaylight.yangtools.binding.generator.util.generated.type.builder.GeneratedTypeBuilderImpl;
 import org.opendaylight.yangtools.sal.binding.model.api.Type;
+import org.opendaylight.yangtools.sal.binding.yang.types.TypeProviderImpl;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.IdentitySchemaNode;
@@ -37,18 +31,20 @@ import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.BinaryTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.BooleanTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.DecimalTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.EmptyTypeDefinition;
 import org.opendaylight.yangtools.yang.model.api.type.EnumTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.EnumTypeDefinition.EnumPair;
-import org.opendaylight.yangtools.yang.model.util.BinaryType;
-import org.opendaylight.yangtools.yang.model.util.BooleanType;
-import org.opendaylight.yangtools.yang.model.util.Decimal64;
-import org.opendaylight.yangtools.yang.model.util.EmptyType;
-import org.opendaylight.yangtools.yang.model.util.EnumerationType;
-import org.opendaylight.yangtools.yang.model.util.IdentityrefType;
-import org.opendaylight.yangtools.yang.model.util.StringType;
+import org.opendaylight.yangtools.yang.model.api.type.IdentityrefTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.StringTypeDefinition;
+import org.opendaylight.yangtools.yang.model.util.type.BaseTypes;
+import org.opendaylight.yangtools.yang.model.util.type.IdentityrefTypeBuilder;
 import org.opendaylight.yangtools.yang.parser.builder.impl.IdentitySchemaNodeBuilder;
 import org.opendaylight.yangtools.yang.parser.builder.impl.LeafSchemaNodeBuilder;
 import org.opendaylight.yangtools.yang.parser.builder.impl.ModuleBuilder;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 import org.opendaylight.yangtools.yang.parser.util.YangValidationException;
 
 public class TypeProviderImplTest {
@@ -119,7 +115,7 @@ public class TypeProviderImplTest {
         final SchemaPath refTypePath = SchemaPath.create(true, QName.create("cont1"), QName.create("list1"));
         final GeneratedTypeBuilderImpl refType = new GeneratedTypeBuilderImpl("org.opendaylight.yangtools.test", "TestType");
         typeProvider.putReferencedType(refTypePath, refType);
-        final StringType stringType = StringType.getInstance();
+        final StringTypeDefinition stringType = BaseTypes.stringType();
         LeafSchemaNodeBuilder leafSchemaNodeBuilder = new LeafSchemaNodeBuilder("test-module", 111, QName.create("Cont1"), SchemaPath.ROOT);
         leafSchemaNodeBuilder.setType(stringType);
         LeafSchemaNode leafSchemaNode = leafSchemaNodeBuilder.build();
@@ -141,28 +137,28 @@ public class TypeProviderImplTest {
         assertEquals("\"default value\"", typeProvider.getTypeDefaultConstruction(leafSchemaNode, "default value"));
 
         // binary type
-        final BinaryType binaryType = BinaryType.getInstance();
+        final BinaryTypeDefinition binaryType = BaseTypes.binaryType();
         leafSchemaNodeBuilder = new LeafSchemaNodeBuilder("test-module", 111, QName.create("Cont1"), SchemaPath.ROOT);
         leafSchemaNodeBuilder.setType(binaryType);
         leafSchemaNode = leafSchemaNodeBuilder.build();
         assertEquals("new byte[] {-45}", typeProvider.getTypeDefaultConstruction(leafSchemaNode, "01"));
 
         // boolean type
-        final BooleanType booleanType = BooleanType.getInstance();
+        final BooleanTypeDefinition booleanType = BaseTypes.booleanType();
         leafSchemaNodeBuilder = new LeafSchemaNodeBuilder("test-module", 111, QName.create("Cont1"), SchemaPath.ROOT);
         leafSchemaNodeBuilder.setType(booleanType);
         leafSchemaNode = leafSchemaNodeBuilder.build();
         assertEquals("new java.lang.Boolean(\"false\")", typeProvider.getTypeDefaultConstruction(leafSchemaNode, "false"));
 
         // decimal type
-        final Decimal64 decimalType = Decimal64.create(refTypePath, 4);
+        final DecimalTypeDefinition decimalType = BaseTypes.decimalTypeBuilder(refTypePath).setFractionDigits(4).build();
         leafSchemaNodeBuilder = new LeafSchemaNodeBuilder("test-module", 111, QName.create("Cont1"), SchemaPath.ROOT);
         leafSchemaNodeBuilder.setType(decimalType);
         leafSchemaNode = leafSchemaNodeBuilder.build();
         assertEquals("new java.math.BigDecimal(\"111\")", typeProvider.getTypeDefaultConstruction(leafSchemaNode, "111"));
 
         // empty type
-        final EmptyType emptyType = EmptyType.getInstance();
+        final EmptyTypeDefinition emptyType = BaseTypes.emptyType();
         leafSchemaNodeBuilder = new LeafSchemaNodeBuilder("test-module", 111, QName.create("Cont1"), SchemaPath.ROOT);
         leafSchemaNodeBuilder.setType(emptyType);
         leafSchemaNode = leafSchemaNodeBuilder.build();
@@ -170,7 +166,7 @@ public class TypeProviderImplTest {
 
         // enum type
         expException.expect(NoSuchElementException.class);
-        final EnumerationType enumType = EnumerationType.create(refTypePath, new ArrayList<EnumTypeDefinition.EnumPair>(), Optional.<EnumPair> absent());
+        final EnumTypeDefinition enumType =  BaseTypes.enumerationTypeBuilder(refTypePath).build();
         leafSchemaNodeBuilder = new LeafSchemaNodeBuilder("test-module", 111, QName.create("Cont1"), SchemaPath.ROOT);
         leafSchemaNodeBuilder.setType(enumType);
         leafSchemaNode = leafSchemaNodeBuilder.build();
@@ -183,7 +179,10 @@ public class TypeProviderImplTest {
         final ModuleBuilder testModBuilder = new ModuleBuilder("test-module", "/test");
         final IdentitySchemaNodeBuilder identityNodeBuilder = testModBuilder.addIdentity(QName.create("IdentityRefTest"), 111, SchemaPath.ROOT);
         final IdentitySchemaNode identitySchemaNode = identityNodeBuilder.build();
-        final IdentityrefType identityRef = IdentityrefType.create(refTypePath, identitySchemaNode);
+
+        final IdentityrefTypeBuilder identityRefBuilder = BaseTypes.identityrefTypeBuilder(refTypePath);
+        identityRefBuilder.setIdentity(identitySchemaNode);
+        final IdentityrefTypeDefinition identityRef =  identityRefBuilder.build();
         leafSchemaNodeBuilder = new LeafSchemaNodeBuilder("test-module", 111, QName.create("Cont1"), SchemaPath.ROOT);
         leafSchemaNodeBuilder.setType(identityRef);
 
