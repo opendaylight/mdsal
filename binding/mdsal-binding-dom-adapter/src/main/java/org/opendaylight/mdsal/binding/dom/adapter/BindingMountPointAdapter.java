d62ada99bcba14f73c60244a6e068942c6abbd80
Robert Varga <rovarga@cisco.com>
2016-02-05 12:48:07 +0100

    WIP: use ClassValue
    Change-Id: Ide86a1d7fc5773f9a1386263bc942177c600beca
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingMountPointAdapter.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingMountPointAdapter.java
@@ -7,30 +7,51 @@
  */
 package org.opendaylight.mdsal.binding.dom.adapter;
 
-import org.opendaylight.mdsal.dom.api.DOMMountPoint;
-import org.opendaylight.mdsal.dom.api.DOMService;
-
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Verify;
 import org.opendaylight.mdsal.binding.api.BindingService;
 import org.opendaylight.mdsal.binding.api.MountPoint;
-import com.google.common.base.Optional;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.LoadingCache;
+import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.mdsal.dom.api.DOMService;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public class BindingMountPointAdapter implements MountPoint {
+    private static final class MountPointAdapterLoader extends BindingDOMAdapterLoader {
+        private final DOMMountPoint domMountPoint;
+
+        MountPointAdapterLoader(final BindingToNormalizedNodeCodec codec, final DOMMountPoint domMountPoint) {
+            super(codec);
+            this.domMountPoint = Preconditions.checkNotNull(domMountPoint);
+        }
+
+        @Override
+        protected DOMService getDelegate(final Class<? extends DOMService> reqDeleg) {
+            return domMountPoint.getService(reqDeleg).orNull();
+        }
+    }
+
+    private static final class Services extends ClassValue<Optional<BindingService>> {
+        private final BindingDOMAdapterLoader loader;
+
+        Services(final BindingToNormalizedNodeCodec codec, final DOMMountPoint domMountPoint) {
+            this.loader = new MountPointAdapterLoader(codec, domMountPoint);
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        protected Optional<BindingService> computeValue(final Class<?> type) {
+            Verify.verify(BindingService.class.isAssignableFrom(type));
+            return loader.load((Class<? extends BindingService>) type);
+        }
+    }
 
     private final InstanceIdentifier<?> identifier;
-    private LoadingCache<Class<? extends BindingService>, Optional<BindingService>> services;
+    private final Services services;
 
     public BindingMountPointAdapter(final BindingToNormalizedNodeCodec codec, final DOMMountPoint domMountPoint) {
         identifier = codec.getCodecRegistry().fromYangInstanceIdentifier(domMountPoint.getIdentifier());
-        services = CacheBuilder.newBuilder().build(new BindingDOMAdapterLoader(codec) {
-
-            @Override
-            protected DOMService getDelegate(Class<? extends DOMService> reqDeleg) {
-                return domMountPoint.getService(reqDeleg).orNull();
-            }
-        });
+        services = new Services(codec, domMountPoint);
     }
 
     @Override
@@ -39,12 +60,11 @@ public class BindingMountPointAdapter implements MountPoint {
     }
 
     @Override
-    public <T extends BindingService> Optional<T> getService(Class<T> service) {
-        Optional<BindingService> potential = services.getUnchecked(service);
+    public <T extends BindingService> Optional<T> getService(final Class<T> service) {
+        Optional<BindingService> potential = services.get(service);
         if(potential.isPresent()) {
             return Optional.of(service.cast(potential.get()));
         }
         return Optional.absent();
     }
-
 }
