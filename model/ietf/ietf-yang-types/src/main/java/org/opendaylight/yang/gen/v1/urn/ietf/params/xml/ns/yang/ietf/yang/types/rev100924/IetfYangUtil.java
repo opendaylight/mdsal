3bdaaf5b22b48c3909b6ac0a96b30306c3a57dd3
Robert Varga <robert.varga@pantheon.sk>
2016-02-18 01:49:05 +0100

    BUG-2825: add utility methods for instantiating DTOs
    Change-Id: I9a98c55b850bf13695c8f581f8acee81705d570b
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>
    (cherry picked from commit 4ba08793b4fe9fbc958270de742241e5f093d1b4)

---
+++ model/ietf/ietf-yang-types/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/yang/types/rev100924/IetfYangUtil.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.mdsal.model.ietf.util.AbstractIetfYangUtil;
+
+/**
+ * Utility methods for working with types defined in ietf-yang-types.
+ */
+@Beta
+public final class IetfYangUtil extends AbstractIetfYangUtil<MacAddress> {
+    public static final IetfYangUtil INSTANCE = new IetfYangUtil();
+
+    private IetfYangUtil() {
+        super(MacAddress.class);
+    }
+
+    @Override
+    protected String getValue(final MacAddress macAddress) {
+        return macAddress.getValue();
+    }
+}
