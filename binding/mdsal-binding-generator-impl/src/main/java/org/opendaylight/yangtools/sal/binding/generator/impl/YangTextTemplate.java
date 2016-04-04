abfc6234a6a971c39613ecc5d1d15ddba897de3c
Robert Varga <rovarga@cisco.com>
2016-04-04 18:11:10 +0200

    Minimize xtend code in mdsal-binding-generator-impl
    This introduces YangTextTemplate as a Java class and moves the trivial
    bits of java code from xtend to Java.
    
    As a side-effect it also fixes space trimming code, which has never worked
    due to xtend bug, which lead to characters being compared via Char.equals(String).
    
    Change-Id: Ia86db1136fca056cf5c0e21b55e1cc904870664a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/yangtools/sal/binding/generator/impl/YangTextTemplate.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.sal.binding.generator.impl;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Strings;
+import com.google.common.collect.Iterables;
+import java.util.StringTokenizer;
+import org.opendaylight.yangtools.yang.common.QName;
+
+final class YangTextTemplate {
+    private static final CharMatcher NEWLINE_OR_TAB = CharMatcher.anyOf("\n\t");
+
+    private YangTextTemplate() {
+        throw new UnsupportedOperationException();
+    }
+
+    static String formatSchemaPath(final String moduleName, final Iterable<QName> schemaPath) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(moduleName);
+
+        QName currentElement = Iterables.getFirst(schemaPath, null);
+        for (QName pathElement : schemaPath) {
+            sb.append('/');
+            if (!currentElement.getNamespace().equals(pathElement.getNamespace())) {
+                currentElement = pathElement;
+                sb.append(pathElement);
+            } else {
+                sb.append(pathElement.getLocalName());
+            }
+        }
+        return sb.toString();
+    }
+
+    static String formatToAugmentPath(final Iterable<QName> schemaPath) {
+        final StringBuilder sb = new StringBuilder();
+        for (QName pathElement : schemaPath) {
+            sb.append("\\(").append(pathElement.getNamespace()).append(')').append(pathElement.getLocalName());
+        }
+        return sb.toString();
+    }
+
+    static String formatToParagraph(final String text, final int nextLineIndent) {
+        if (Strings.isNullOrEmpty(text)) {
+            return "";
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        final StringBuilder lineBuilder = new StringBuilder();
+        boolean isFirstElementOnNewLineEmptyChar = false;
+        final String lineIndent = Strings.repeat(" ", nextLineIndent);
+
+        String formattedText = NEWLINE_OR_TAB.removeFrom(text);
+        formattedText = formattedText.replaceAll(" +", " ");
+
+        final StringTokenizer tokenizer = new StringTokenizer(formattedText, " ", true);
+
+        while (tokenizer.hasMoreElements()) {
+            final String nextElement = tokenizer.nextElement().toString();
+
+            if (lineBuilder.length() + nextElement.length() > 80) {
+                // Trim trailing whitespace
+                for (int i = lineBuilder.length() - 1; i >= 0 && lineBuilder.charAt(i) != ' '; --i) {
+                    lineBuilder.setLength(i);
+                }
+
+                // Trim leading whitespace
+                while (lineBuilder.charAt(0) == ' ') {
+                    lineBuilder.deleteCharAt(0);
+                }
+
+                sb.append(lineBuilder).append('\n');
+                lineBuilder.setLength(0);
+
+                if (nextLineIndent > 0) {
+                    sb.append(lineIndent);
+                }
+
+                if (" ".equals(nextElement.toString())) {
+                    isFirstElementOnNewLineEmptyChar = !isFirstElementOnNewLineEmptyChar;
+                }
+            }
+            if (isFirstElementOnNewLineEmptyChar) {
+                isFirstElementOnNewLineEmptyChar = !isFirstElementOnNewLineEmptyChar;
+            } else {
+                lineBuilder.append(nextElement);
+            }
+        }
+
+        return sb.append(lineBuilder).append('\n').toString();
+    }
+}
