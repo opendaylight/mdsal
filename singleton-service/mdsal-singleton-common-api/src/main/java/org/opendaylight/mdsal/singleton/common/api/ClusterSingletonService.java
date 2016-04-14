7e31bf4986748da0c4d43869215a36c5d4f81dc1
Vaclav Demcak <vdemcak@cisco.com>
2016-04-14 11:32:29 +0200

    Bug 5421 - Single cluster-wide service API
    ClusterSingletonServices provides a functionality which alowe to have only
    one fully instantiated service instance in a cluster at one time.
    
    * Cluster Singleton API contract definitions
    * adoc for ClusterSingletonService implementation
    
    Change-Id: I89180c0a6b6427d6cb1087457aaf2978b8544dda
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ singleton-service/mdsal-singleton-common-api/src/main/java/org/opendaylight/mdsal/singleton/common/api/ClusterSingletonService.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.singleton.common.api;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * {@link ClusterSingletonService} interface represents a single cluster service instance. It has to implement
+ * every service (RPCs or Applications) which would like to be instantiated on same Cluster Node. Grouping is
+ * realized by ServiceGroupIdentifier. Servicies with same ServiceGroupIdentifier have to run on same Cluster
+ * Node. ServiceGroupIdentifier must not change during whole {@link ClusterSingletonService} lifecycle.
+ */
+public interface ClusterSingletonService {
+
+    /**
+     * This method is invoked to instantiate an underlying service instance when
+     * ownership has been granted for the service entity.
+     */
+    void instantiateServiceInstance();
+
+    /**
+     * This method is invoked to close the underlying service instance when ownership has been lost
+     * for the service entity. If the act of closing the instance may perform blocking operations or
+     * take some time, it should be done asynchronously to avoid blocking the current thread.
+     *
+     * @return a ListenableFuture that is completed when the underlying instance close operation is complete.
+     */
+    ListenableFuture<Void> closeServiceInstance();
+
+    /**
+     * Method returns identifier for {@link ClusterSingletonService} Group.
+     *
+     * @return defined Service Group Identifier
+     */
+    String getServiceGroupIdentifier();
+
+}
---
+++ singleton-service/mdsal-singleton-common-api/src/main/java/org/opendaylight/mdsal/singleton/common/api/ClusterSingletonServiceRegistrationDelegator.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.singleton.common.api;
+
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * Package protected help class represent a Delegator for {@link ClusterSingletonService}
+ * instance and {@link ClusterSingletonServiceRegistration} implementation.
+ * Close registration means remove {@link ClusterSingletonService} instance from internal
+ * ClusterSingletonServiceGroup list reference.
+ *
+ * Close {@link ClusterSingletonServiceRegistration} is prepared for a possible restart
+ * service or application in osgi container. Any another servicies from group can not be
+ * stoped.
+ */
+class ClusterSingletonServiceRegistrationDelegator
+        implements ClusterSingletonServiceRegistration, ClusterSingletonService {
+
+    private final ClusterSingletonService service;
+    private final ClusterSingletonServiceGroup<?, ?, ?> group;
+
+    ClusterSingletonServiceRegistrationDelegator(final ClusterSingletonService service,
+            final ClusterSingletonServiceGroup<?, ?, ?> group) {
+        this.service = Preconditions.checkNotNull(service);
+        this.group = Preconditions.checkNotNull(group);
+    }
+
+    @Override
+    public void close() throws Exception {
+        group.unregisterService(this);
+    }
+
+    @Override
+    public void instantiateServiceInstance() {
+        service.instantiateServiceInstance();
+    }
+
+    @Override
+    public ListenableFuture<Void> closeServiceInstance() {
+        return service.closeServiceInstance();
+    }
+
+    @Override
+    public String getServiceGroupIdentifier() {
+        return service.getServiceGroupIdentifier();
+    }
+}
