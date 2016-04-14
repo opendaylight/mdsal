3cc5b499ff34e221b247409530321ed02ebca03f
Vaclav Demcak <vdemcak@cisco.com>
2016-04-14 11:32:29 +0200

    Bug 5421 - Single cluster-wide service instances grouping
    ClusterSingletonService grouping means to run all registred service to same
    Group on same ClusterNode. So we could say, service goup will always run on
    same ClusterNode.
    
     * implement grouping functionality for ClusterSingletonService instancies
     * implement double candidate funct. for handling ClusterSingletonService
       service group instances
     * add junit test suite + import Mockito
    
    Change-Id: If91a6f1bf786c42c32201899bc0bb826729f0585
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ singleton-service/mdsal-singleton-common-spi/src/test/java/org/opendaylight/mdsal/singleton/common/spi/util/TestEntity.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.singleton.common.spi.util;
+
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.eos.common.api.GenericEntity;
+import org.opendaylight.yangtools.yang.common.QName;
+
+/**
+ * Test util class.
+ */
+public class TestEntity extends GenericEntity<TestInstanceIdentifier> {
+    private static final long serialVersionUID = 1L;
+
+    static final QName ENTITY = QName
+            .create("urn:opendaylight:params:xml:ns:yang:mdsal:core:test-entity", "2016-06-06", "entity").intern();
+    static final QName ENTITY_NAME = QName.create(ENTITY, "name").intern();
+
+    /** Constructs an instance.
+     *
+     * @param type the entity type
+     * @param id the entity id.
+     */
+    public TestEntity(@Nonnull final String type, @Nonnull final TestInstanceIdentifier id) {
+        super(type, id);
+    }
+
+    /**
+     * Construct an Entity with an with a name. The general-entity schema is used to construct the
+     * {@link TestInstanceIdentifier}.
+     *
+     * @param type the type of the entity
+     * @param entityName the name of the entity used to construct a general-entity YangInstanceIdentifier
+     */
+    public TestEntity(@Nonnull final String type, @Nonnull final String entityName) {
+        super(type, new TestInstanceIdentifier(entityName));
+    }
+}
