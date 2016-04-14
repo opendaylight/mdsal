b4975c597606bd4c183b45a7b9ebcc256274c5e6
Vaclav Demcak <vdemcak@cisco.com>
2016-04-14 11:32:29 +0200

    Bug 5421 - Single cluster-wide project instances
     *add Abstract class for every clustered project provider
     *implement double candidate functionality for handling
       singleton project instances
     *add junit test suite + import Mockito
     *add adoc documentation
    
    Change-Id: If5bac3f61cc3164da1e09c2caf6713c1e3da658a
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/clustering/DOMAbstractProjectProvider.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.dom.api.clustering;
+
+import org.opendaylight.mdsal.common.api.clustering.AbstractGenericProjectProvider;
+import org.opendaylight.mdsal.common.api.clustering.GenericEntityOwnershipService;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+/**
+ * Binding version of {@link AbstractGenericProjectProvider}
+ */
+public abstract class DOMAbstractProjectProvider
+        extends
+        AbstractGenericProjectProvider<YangInstanceIdentifier, DOMEntity, DOMEntityOwnershipChange, DOMEntityOwnershipListener, DOMEntityOwnershipService, DOMEntityOwnershipListenerRegistration>
+        implements DOMEntityOwnershipListener {
+
+    /**
+     * Initialization all needed class internal property for {@link DOMAbstractProjectProvider}
+     *
+     * @param entityOwnershipService - we need only {@link GenericEntityOwnershipService}
+     */
+    public DOMAbstractProjectProvider(final DOMEntityOwnershipService entityOwnershipService) {
+        super(entityOwnershipService);
+    }
+
+    @Override
+    protected final DOMEntity createEntity(final String type, final String ident) {
+        return new DOMEntity(type, ident);
+    }
+
+    @Override
+    protected final DOMEntityOwnershipListenerRegistration registerListener(final String type,
+            final DOMEntityOwnershipService eos) {
+        return eos.registerListener(type, this);
+    }
+
+}
