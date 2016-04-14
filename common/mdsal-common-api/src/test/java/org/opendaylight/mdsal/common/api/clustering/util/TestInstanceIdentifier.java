ba7b31f44f14f6e8a928d2fe3084a778d411cd58
Vaclav Demcak <vdemcak@cisco.com>
2016-04-14 11:32:29 +0200

    Bug 5421 - Single cluster-wide service instances grouping
     * add API for ClusterSingletonServiceProvider & ClusterSingletonService
     * implement grouping functionality for ClusterSingletonService instancies
     * implement double candidate funct. for handling ClusterSingletonService
       service group instances
     * add Abstract ClusterSingletonServiceProvider implementation
     * add DOM & biniding ClusterSingletonServiceProvider
     * add junit test suite + import Mockito
     * adoc Documentation
    
    Change-Id: If5bac3f61cc3164da1e09c2caf6713c1e3da658a
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ common/mdsal-common-api/src/test/java/org/opendaylight/mdsal/common/api/clustering/util/TestInstanceIdentifier.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.common.api.clustering.util;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import java.util.LinkedList;
+import java.util.List;
+import org.opendaylight.yangtools.concepts.Path;
+
+/**
+ * Test helper class. {@link Path} for testing only
+ */
+public class TestInstanceIdentifier implements Path<TestInstanceIdentifier> {
+
+    static final TestInstanceIdentifier EMPTY_INSTANCE = new TestInstanceIdentifier(ImmutableList.of());
+
+    private final ImmutableList<String> path;
+
+    /**
+     * {@link TestInstanceIdentifier} constructor
+     *
+     * @param path
+     */
+    public TestInstanceIdentifier(final Iterable<? extends TestInstanceIdentifier> path) {
+        Preconditions.checkArgument(path != null);
+        final List<String> tiis = new LinkedList<>();
+        for (final TestInstanceIdentifier t : path) {
+            tiis.add(t.toString());
+        }
+        this.path = ImmutableList.copyOf(tiis);
+    }
+
+    TestInstanceIdentifier(final String path) {
+        this.path = ImmutableList.of(path);
+    }
+
+    @Override
+    public boolean contains(final TestInstanceIdentifier other) {
+        return path.contains(other);
+    }
+
+}
