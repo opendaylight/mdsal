578778ef693d73f186457a53f3ff645c97748d31
Vaclav Demcak <vdemcak@cisco.com>
2016-04-14 11:32:29 +0200

    Bug 5421 - Single cluster-wide service API
    ClusterSingletonServices provides a functionality which alowe to have only
    one fully instantiated service instance in a cluster at one time.
    
    * Cluster Singleton API contract definitions
    * adoc for ClusterSingletonService implementation
    
    Change-Id: I89180c0a6b6427d6cb1087457aaf2978b8544dda
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ common/mdsal-common-api/src/main/java/org/opendaylight/mdsal/common/api/clustering/ClusterSingletonServiceRegistration.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.common.api.clustering;
+
+/**
+ * {@link ClusterSingletonServiceRegistration} interface contains a marker for
+ * holding ClusterSingletonService registration and posibility to close it
+ * via {@link AutoCloseable} interface.
+ */
+public interface ClusterSingletonServiceRegistration extends AutoCloseable {
+
+    /**
+     * Empty body for mark a ClusterSingletonService Registration instance
+     */
+}
