44381c7e9b3812682ed70961a51633d9b3be8bb7
Vaclav Demcak <vdemcak@cisco.com>
2016-04-14 11:32:29 +0200

    Bug 5421 - Single cluster-wide service instances implementation
     * add Abstract ClusterSingletonServiceProvider implementation
     * add DOM & biniding ClusterSingletonServiceProvider impl.
     * add junit test suite + import Mockito
    
    Change-Id: If5bac3f61cc3164da1e09c2caf6713c1e3da658a
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/clustering/DOMClusterSingletonServiceProviderImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.dom.api.clustering;
+
+import org.opendaylight.mdsal.common.api.clustering.AbstractClusterSingletonServiceProviderImpl;
+import org.opendaylight.mdsal.common.api.clustering.GenericEntityOwnershipService;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+
+/**
+ * Binding version of {@link AbstractClusterSingletonServiceProviderImpl}
+ */
+public final class DOMClusterSingletonServiceProviderImpl extends
+        AbstractClusterSingletonServiceProviderImpl<YangInstanceIdentifier, DOMEntity,
+                                                    DOMEntityOwnershipChange,
+                                                    DOMEntityOwnershipListener,
+                                                    DOMEntityOwnershipService,
+                                                    DOMEntityOwnershipListenerRegistration>
+        implements DOMClusterSingletonServiceProvider {
+
+    /**
+     * Initialization all needed class internal property for {@link DOMClusterSingletonServiceProviderImpl}
+     *
+     * @param entityOwnershipService - we need only {@link GenericEntityOwnershipService}
+     */
+    public DOMClusterSingletonServiceProviderImpl(final DOMEntityOwnershipService entityOwnershipService) {
+        super(entityOwnershipService);
+    }
+
+    @Override
+    protected final DOMEntity createEntity(final String type, final String ident) {
+        return new DOMEntity(type, ident);
+    }
+
+    @Override
+    protected final DOMEntityOwnershipListenerRegistration registerListener(final String type,
+            final DOMEntityOwnershipService eos) {
+        return eos.registerListener(type, this);
+    }
+
+    @Override
+    protected String getServiceIdentifierFromEntity(final DOMEntity entity) {
+        final YangInstanceIdentifier yii = entity.getIdentifier();
+        final NodeIdentifierWithPredicates niiwp = (NodeIdentifierWithPredicates) yii.getLastPathArgument();
+        return niiwp.getKeyValues().values().iterator().next().toString();
+    }
+}
