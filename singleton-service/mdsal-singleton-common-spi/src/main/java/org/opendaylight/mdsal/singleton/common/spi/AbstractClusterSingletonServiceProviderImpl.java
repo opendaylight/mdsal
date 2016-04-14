6223b5a2374e0d71f9c59fe84953487c4e55b09b
Vaclav Demcak <vdemcak@cisco.com>
2016-04-14 11:32:29 +0200

    Bug 5421 - Single cluster-wide service instances grouping
    ClusterSingletonService grouping means to run all registred service to same
    Group on same ClusterNode. So we could say, service goup will always run on
    same ClusterNode.
    
     * implement grouping functionality for ClusterSingletonService instancies
     * implement double candidate funct. for handling ClusterSingletonService
       service group instances
     * add junit test suite + import Mockito
    
    Change-Id: If91a6f1bf786c42c32201899bc0bb826729f0585
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ singleton-service/mdsal-singleton-common-spi/src/main/java/org/opendaylight/mdsal/singleton/common/spi/AbstractClusterSingletonServiceProviderImpl.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.singleton.common.spi;
+
+/**
+ * Abstract class {@link AbstractClusterSingletonServiceProviderImpl} represents implementations of
+ * {@link org.opendaylight.mdsal.singleton.common.api.CommonClusterSingletonServiceProvider}.
+ */
+public abstract class AbstractClusterSingletonServiceProviderImpl {
+
+    // TODO: add implementation
+}
