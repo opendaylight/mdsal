fe0c79b4ad709b4404ebe943a0d4ffb54007a560
Martin Ciglan <mciglan@cisco.com>
2016-04-18 16:53:33 +0200

    Bug 1411: Design Binding specification v2
    Change-Id: I1ae6051867f793e0502b72e3d030122001d6a158
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/prototype/src/test/java/org/opendaylight/yang/gen/v1/urn/test/mainyangtest/rev160101/$YangModelBindingProvider.java
@@ -0,0 +1,8 @@
+package org.opendaylight.yang.gen.v1.urn.test.mainyangtest.rev160101;
+
+public final class $YangModelBindingProvider implements org.opendaylight.yangtools.yang.binding.YangModelBindingProvider {
+
+    public org.opendaylight.yangtools.yang.binding.YangModuleInfo getModuleInfo() {
+        return $YangModuleInfoImpl.getInstance();
+    }
+}
---
+++ binding2/prototype/src/test/java/org/opendaylight/yang/gen/v1/urn/test/mainyangtestimporting/rev160101/MainGroupCont1.java
@@ -0,0 +1,19 @@
+package org.opendaylight.yang.gen.v1.urn.test.mainyangtestimporting.rev160101;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+import org.opendaylight.yang.gen.v1.urn.test.mainyangtest.rev160101.main.group.MainGroupCont;
+import org.opendaylight.yangtools.yang.binding.TreeNode;
+
+
+public interface MainGroupCont1
+    extends
+        TreeNode,
+    Augmentation<MainGroupCont>
+{
+
+
+
+
+    java.lang.String getImportedAugmentedLeaf3();
+
+}
+
