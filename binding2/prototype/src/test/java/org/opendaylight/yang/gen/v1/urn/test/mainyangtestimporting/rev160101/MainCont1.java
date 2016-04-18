cb28a6c0e10e176a7ec13ad045e75161d0a1c3bb
Martin Ciglan <mciglan@cisco.com>
2016-04-18 16:53:33 +0200

    Bug 1411: Design Binding specification v2
    Change-Id: I1ae6051867f793e0502b72e3d030122001d6a158
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/prototype/src/test/java/org/opendaylight/yang/gen/v1/urn/test/mainyangtestimporting/rev160101/MainCont1.java
@@ -0,0 +1,19 @@
+package org.opendaylight.yang.gen.v1.urn.test.mainyangtestimporting.rev160101;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+import org.opendaylight.yangtools.yang.binding.TreeNode;
+import org.opendaylight.yang.gen.v1.urn.test.mainyangtest.rev160101.MainCont;
+
+
+public interface MainCont1
+    extends
+        TreeNode,
+    Augmentation<MainCont>
+{
+
+
+
+
+    java.lang.String getImportedAugmentedLeaf1();
+
+}
+
