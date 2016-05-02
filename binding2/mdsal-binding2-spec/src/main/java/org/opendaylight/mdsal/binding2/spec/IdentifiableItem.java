e5a967bbf73fae8f7ce221b1bf9a8fdfe64e4466
Martin Ciglan <mciglan@cisco.com>
2016-05-02 13:07:54 +0200

    Binding2: Added concepts.  - minor fixes
    Change-Id: Ic098b74d1ad53956528dccd4e5144c172d48fc22
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/mdsal-binding2-spec/src/main/java/org/opendaylight/mdsal/binding2/spec/IdentifiableItem.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.spec;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+
+/**
+ * An IdentifiableItem represents a object that is usually present in a collection and can be
+ * identified uniquely by a key. In YANG terms this would probably represent an item in a list.
+ *
+ * @param <I> An object that is identifiable by an identifier
+ * @param <T> The identifier of the object
+ */
+@Beta
+public final class IdentifiableItem<I extends TreeNode, T> extends TreeArgument<I> {
+    private final Class<I> type;
+    private final T key;
+
+    public IdentifiableItem(final Class<I> type, final T key) {
+        this.type = Preconditions.checkNotNull(type);
+        this.key = Preconditions.checkNotNull(key, "Key may not be null.");
+    }
+
+    @Override
+    public Class<I> getType() {
+        return type;
+    }
+
+    public T getKey() {
+        return this.key;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return super.equals(obj) && key.equals(((IdentifiableItem<?, ?>) obj).getKey());
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode() * 31 + key.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return type.getName() + "[key=" + key + "]";
+    }
+}
