f95383fc647fabf2ecce75263f92889ec779294a
Isaku Yamahata <isaku.yamahata@intel.com>
2016-05-10 12:55:54 -0700

    Bug 5883 - no constructor for indentityref in union
    The generated java code for union with identityref doens't include a
    constructor for identityref.
    
    module union-with-identityref {
        yang-version 1;
        namespace "urn:opendaylight:yang:union:test";
        prefix "uniontest";
    
        description "test union with identityref";
    
        revision "2016-05-09";
    
        identity ident-base;
        identity ident-one {
            base ident-base;
        }
        identity ident-two {
            base ident-base;
        }
    
        typedef union-type {
            type union {
                type uint8;
                type identityref {
                    base ident-base;
                }
            }
        }
    }
    
    generated java code:
    public class UnionType
     implements Serializable {
        private static final long serialVersionUID = 4724108168179933454L;
        private final java.lang.Short _uint8;
        private final char[] _value;
    
        public UnionType(java.lang.Short _uint8) {
            super();
            this._uint8 = _uint8;
            this._value = _uint8.toString().toCharArray();
        }
    
    Change-Id: Idf8f7df54afd0934e47b19c131ea41d046a7b5df
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/yangtools/sal/binding/yang/types/TypeProviderImpl.java
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/yangtools/sal/binding/yang/types/TypeProviderImpl.java
@@ -777,8 +777,7 @@ public final class TypeProviderImpl implements TypeProvider {
                     makeSerializable((GeneratedTOBuilderImpl) genTOBuilder);
                     returnType = genTOBuilder.toInstance();
                 } else {
-                    final Type javaType = BaseYangTypes.BASE_YANG_TYPES_PROVIDER.javaTypeForSchemaDefinitionType(
-                            innerTypeDefinition, typedef);
+                    final Type javaType = javaTypeForSchemaDefinitionType(innerTypeDefinition, typedef);
                     returnType = wrapJavaTypeIntoTO(basePackageName, typedef, javaType, module.getName());
                 }
                 if (returnType != null) {
@@ -924,8 +923,7 @@ public final class TypeProviderImpl implements TypeProvider {
                         unionTypeName, unionGenTOBuilder);
                 updateUnionTypeAsProperty(unionGenTOBuilder, enumeration, unionTypeName);
             } else {
-                final Type javaType = BaseYangTypes.BASE_YANG_TYPES_PROVIDER.javaTypeForSchemaDefinitionType(unionType,
-                        parentNode);
+                final Type javaType = javaTypeForSchemaDefinitionType(unionType, parentNode);
                 updateUnionTypeAsProperty(unionGenTOBuilder, javaType, unionTypeName);
             }
         }
