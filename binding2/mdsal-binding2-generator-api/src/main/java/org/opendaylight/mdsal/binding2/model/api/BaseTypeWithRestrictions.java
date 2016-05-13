53f2d1eefe212ccbce77f5b1ecd35aad66533ef6
Martin Ciglan <mciglan@cisco.com>
2016-05-13 10:21:19 +0200

    IN PROGRESS Bug 1411-2: MDSAL Binding2 Generator API
    - generator API
    - model API
    
    Change-Id: I38da58355bb76637c9e7e3abb8e70b405535af25
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/mdsal-binding2-generator-api/src/main/java/org/opendaylight/mdsal/binding2/model/api/BaseTypeWithRestrictions.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.model.api;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * The BaseTypeWithRestrictions is marker interface that encapsulates definition of java
+ * concrete type only with changed restriction values.
+ */
+@Beta
+public interface BaseTypeWithRestrictions extends ConcreteType {
+}
