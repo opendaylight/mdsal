7e2f444ccc62888c1e0352e36984b52302e52ee8
Martin Ciglan <mciglan@cisco.com>
2016-05-13 10:21:19 +0200

    Bug 1411-2: MDSAL Binding2 Generator API
    - generator API
    - model API
    - review comments implemented/typos fixed
    
    Change-Id: I38da58355bb76637c9e7e3abb8e70b405535af25
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/mdsal-binding2-generator-api/src/main/java/org/opendaylight/mdsal/binding2/model/api/AccessModifier.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.model.api;
+
+import com.google.common.annotations.Beta;
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
+
+/**
+ *
+ * Enum definition which provides access modifiers that are described
+ * in Java programming language
+ *
+ */
+@Beta
+public enum AccessModifier {
+    DEFAULT {
+        @Override
+        boolean matchesMember(Member member) {
+            return member.getModifiers() == 0; //no modifiers, package-private in fact
+        }
+    },
+
+    PRIVATE {
+        @Override
+        boolean matchesMember(Member member) {
+            return Modifier.isPrivate(member.getModifiers());
+        }
+    },
+
+    PUBLIC {
+        @Override
+        boolean matchesMember(Member member) {
+            return Modifier.isPublic(member.getModifiers());
+        }
+    },
+
+    PROTECTED {
+        @Override
+        boolean matchesMember(Member member) {
+            return Modifier.isProtected(member.getModifiers());
+        }
+    };
+
+    /**
+     *
+     * @param member reflects identifying information about a single member (a field or a method) or a
+     *               constructor.
+     * @return Return {@code true} if the integer argument includes the
+     *         {@code public} modifier, {@code false} otherwise.
+     */
+    abstract boolean matchesMember(Member member);
+
+}
