2547830b13e2dc04a5b92f09898c64ef1d4afe8c
Peter Nosal <peter.nosal@pantheon.tech>
2016-05-30 13:05:44 +0200

    Bug 5947: Increasing code coverage for mdsal project - increasing coverage for module mdsal-dom-broker
    Change-Id: I83683f9a7ac835d118e7e4a3e8446f5f593221e4
    Signed-off-by: Peter Nosal <peter.nosal@pantheon.tech>

--- dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/GlobalDOMRpcRoutingTableEntryTest.java
+++ dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/GlobalDOMRpcRoutingTableEntryTest.java
@@ -10,44 +10,22 @@ package org.opendaylight.mdsal.dom.broker;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
-import com.google.common.util.concurrent.CheckedFuture;
-import com.google.common.util.concurrent.Futures;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import org.junit.Test;
-import org.opendaylight.mdsal.dom.api.DOMRpcException;
-import org.opendaylight.mdsal.dom.api.DOMRpcIdentifier;
 import org.opendaylight.mdsal.dom.api.DOMRpcImplementation;
 import org.opendaylight.mdsal.dom.api.DOMRpcImplementationNotAvailableException;
-import org.opendaylight.mdsal.dom.api.DOMRpcResult;
-import org.opendaylight.mdsal.dom.broker.test.util.TestModel;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
-import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
-import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 
-
-public class GlobalDOMRpcRoutingTableEntryTest {
-    private static final DataContainerChild<?, ?> OUTER_LIST = ImmutableNodes.mapNodeBuilder(TestModel.OUTER_LIST_QNAME)
-            .withChild(ImmutableNodes.mapEntry(TestModel.OUTER_LIST_QNAME, TestModel.ID_QNAME, 1))
-            .build();
-
-    private static final NormalizedNode<?, ?> TEST_CONTAINER = Builders.containerBuilder()
-            .withNodeIdentifier(new YangInstanceIdentifier.NodeIdentifier(TestModel.TEST_QNAME))
-            .withChild(OUTER_LIST)
-            .build();
-
-    private static final String EXCEPTION_TEXT = "testException";
+public class GlobalDOMRpcRoutingTableEntryTest extends TestUtils{
 
     @Test
     public void basicTest() throws Exception {
@@ -58,7 +36,7 @@ public class GlobalDOMRpcRoutingTableEntryTest {
 
         doReturn(SchemaPath.ROOT).when(rpcDefinition).getPath();
 
-        rpcImplementation.add(new TestRpcImplementation());
+        rpcImplementation.add(getTestRpcImplementation());
         rpcImplementations.put(yangInstanceIdentifier, rpcImplementation);
 
         final GlobalDOMRpcRoutingTableEntry globalDOMRpcRoutingTableEntry =
@@ -74,23 +52,9 @@ public class GlobalDOMRpcRoutingTableEntryTest {
         try{
             globalDOMRpcRoutingTableEntry.newInstance(rpcImplementations)
                     .invokeRpc(TEST_CONTAINER).checkedGet();
+            fail("Expected DOMRpcImplementationNotAvailableException");
         }catch(DOMRpcImplementationNotAvailableException e){
             assertTrue(e.getMessage().contains(EXCEPTION_TEXT));
-        }
-    }
-
-    private static final class TestRpcImplementation implements DOMRpcImplementation {
-        private final CheckedFuture<DOMRpcResult, DOMRpcException> unknownRpc;
-
-        private TestRpcImplementation() {
-            unknownRpc = Futures.immediateFailedCheckedFuture(
-                    new DOMRpcImplementationNotAvailableException(EXCEPTION_TEXT));
-        }
-
-        @Nonnull
-        public CheckedFuture<DOMRpcResult, DOMRpcException> invokeRpc(
-                @Nonnull DOMRpcIdentifier rpc, @Nullable NormalizedNode<?, ?> input) {
-            return unknownRpc;
         }
     }
 }
