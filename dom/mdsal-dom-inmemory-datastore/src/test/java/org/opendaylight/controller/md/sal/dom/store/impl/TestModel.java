dab43e531d174bf060088bba83bf78c2d4227140
Martin Ciglan <mciglan@cisco.com>
2016-05-31 09:32:57 +0200

    1. Make sure there is no YangParserImpl occurence in MD-SAL 2. Bit of clean-up
    Change-Id: Id86e2017d28693887faa90b62b183c9407a2c05c
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

--- dom/mdsal-dom-inmemory-datastore/src/test/java/org/opendaylight/controller/md/sal/dom/store/impl/TestModel.java
+++ dom/mdsal-dom-inmemory-datastore/src/test/java/org/opendaylight/controller/md/sal/dom/store/impl/TestModel.java
@@ -7,8 +7,6 @@
  */
 package org.opendaylight.controller.md.sal.dom.store.impl;
 
-import com.google.common.io.ByteSource;
-import com.google.common.io.Resources;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
@@ -17,7 +15,6 @@ import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
-import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
 import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
@@ -25,33 +22,27 @@ import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline
 
 public class TestModel {
 
-    public static final QName TEST_QNAME = QName.create("urn:opendaylight:params:xml:ns:yang:controller:md:sal:dom:store:test", "2014-03-13",
-        "test");
+    public static final QName TEST_QNAME =
+            QName.create("urn:opendaylight:params:xml:ns:yang:controller:md:sal:dom:store:test", "2014-03-13", "test");
     public static final QName OUTER_LIST_QNAME = QName.create(TEST_QNAME, "outer-list");
     public static final QName INNER_LIST_QNAME = QName.create(TEST_QNAME, "inner-list");
     public static final QName OUTER_CHOICE_QNAME = QName.create(TEST_QNAME, "outer-choice");
     public static final QName ID_QNAME = QName.create(TEST_QNAME, "id");
     public static final QName NAME_QNAME = QName.create(TEST_QNAME, "name");
     public static final QName VALUE_QNAME = QName.create(TEST_QNAME, "value");
-    private static final String DATASTORE_TEST_YANG = "/odl-datastore-test.yang";
-
-    public static final YangInstanceIdentifier TEST_PATH = YangInstanceIdentifier.of(TEST_QNAME);
-    public static final YangInstanceIdentifier OUTER_LIST_PATH = YangInstanceIdentifier.builder(TEST_PATH).node(OUTER_LIST_QNAME).build();
     public static final QName TWO_QNAME = QName.create(TEST_QNAME, "two");
     public static final QName THREE_QNAME = QName.create(TEST_QNAME, "three");
+    public static final YangInstanceIdentifier TEST_PATH = YangInstanceIdentifier.of(TEST_QNAME);
+    public static final YangInstanceIdentifier OUTER_LIST_PATH =
+            YangInstanceIdentifier.builder(TEST_PATH).node(OUTER_LIST_QNAME).build();
+    private static final String DATASTORE_TEST_YANG = "/odl-datastore-test.yang";
 
-  //  private static ByteSource getInputStream() {
-  //      return Resources.asByteSource(TestModel.class.getResource(DATASTORE_TEST_YANG));
-   // }
+    public static SchemaContext createTestContext() throws IOException, YangSyntaxErrorException, ReactorException {
+        return parseYangStreams(Collections.singletonList(getInputStream()));
+    }
 
     private static InputStream getInputStream() {
         return TestModel.class.getResourceAsStream(DATASTORE_TEST_YANG);
-    }
-
-    public static SchemaContext createTestContext() throws IOException, YangSyntaxErrorException, ReactorException {
-        //YangParserImpl parser = new YangParserImpl();
-        //return parser.parseSources(Collections.singletonList(getInputStream()));
-        return parseYangStreams(Collections.singletonList(getInputStream()));
     }
 
     private static SchemaContext parseYangStreams(List<InputStream> streams)
