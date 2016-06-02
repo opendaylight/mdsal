f4855763f474b2106a64467ee07d7d25691d05a0
Tomas Cere <tcere@cisco.com>
2016-06-02 17:06:54 +0200

    Move DOMDataTreeShardWriteTransaction to dom-api
    Change-Id: I759f66abd6a14b8c876a34e22c7c8e91ebaf9858
    Signed-off-by: Tomas Cere <tcere@cisco.com>

--- dom/mdsal-dom-inmemory-datastore/src/main/java/org/opendaylight/mdsal/dom/store/inmemory/DOMDataTreeShardProducer.java
+++ dom/mdsal-dom-inmemory-datastore/src/main/java/org/opendaylight/mdsal/dom/store/inmemory/DOMDataTreeShardProducer.java
@@ -12,6 +12,7 @@ import com.google.common.annotations.Beta;
 import java.util.Collection;
 import javax.annotation.Nonnull;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeShardWriteTransaction;
 
 // FIXME: this should probably be moved to a different package
 @Beta
