e57625fdcdb764b7f502270f28783386c3971cf9
Tomas Cere <tcere@cisco.com>
2016-06-02 17:06:54 +0200

    Move DOMDataTreeShardWriteTransaction to dom-api
    Change-Id: I759f66abd6a14b8c876a34e22c7c8e91ebaf9858
    Signed-off-by: Tomas Cere <tcere@cisco.com>

--- dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeShardWriteTransaction.java
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeShardWriteTransaction.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.mdsal.dom.store.inmemory;
+package org.opendaylight.mdsal.dom.api;
 
 import com.google.common.annotations.Beta;
 import com.google.common.util.concurrent.ListenableFuture;
