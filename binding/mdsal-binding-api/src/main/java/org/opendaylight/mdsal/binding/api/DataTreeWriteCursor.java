aafde8511bf3c518ac363ce30d01d711cfabdd06
Tomas Cere <tcere@cisco.com>
2016-06-02 17:19:49 +0200

    BUG 6057: Rewrite ShardedDOMProducer to use new cursor api
    Change ShardedDOMProducer, ShardedDOMDataTreeWriteTransaction to use
    cursor api's.
    Make the user facing cursor implementation enforce claimed subtrees.
    Add binding adapters for the DOM cursor api's.
    
    Change-Id: I21fa6533b8ce7da0e5cc864569f6f143a273ae86
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/DataTreeWriteCursor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding.api;
+
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.BackendFailedException;
+
+/**
+ * {@inheritDoc}
+ *
+ * In addition this cursor also provides write operations(delete, merge, write).
+ */
+public interface DataTreeWriteCursor extends DataTreeCursor {
+
+    /**
+     * Delete the specified child.
+     *
+     * @param child Child identifier
+     * @throws BackendFailedException when implementation-specific errors occurs while servicing the
+     *         request.
+     */
+    void delete(PathArgument child);
+
+    /**
+     * Merge the specified data with the currently-present data at specified path.
+     *
+     * @param child Child identifier
+     * @param data Data to be merged
+     * @throws BackendFailedException when implementation-specific errors occurs while servicing the
+     *         request.
+     */
+    <T extends DataObject> void merge(PathArgument child, T data);
+
+    /**
+     * Replace the data at specified path with supplied data.
+     *
+     * @param child Child identifier
+     * @param data New node data
+     * @throws BackendFailedException when implementation-specific errors occurs while servicing the
+     *         request.
+     */
+    <T extends DataObject> void write(PathArgument child, T data);
+}
