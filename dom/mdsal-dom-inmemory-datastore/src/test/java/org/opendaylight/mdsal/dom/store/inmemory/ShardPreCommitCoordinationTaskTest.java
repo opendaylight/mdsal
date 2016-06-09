510edc555674062d9e52bc3a1343682f835cc0cb
Peter Nosal <peter.nosal@pantheon.tech>
2016-06-09 15:45:33 +0200

    Bug 5947: Increasing code coverage - mdsal-dom-inmemory-datastore
    Change-Id: I7e764751b95d79fd538ea421f5ba24c7155ee312
    Signed-off-by: Peter Nosal <peter.nosal@pantheon.tech>

---
+++ dom/mdsal-dom-inmemory-datastore/src/test/java/org/opendaylight/mdsal/dom/store/inmemory/ShardPreCommitCoordinationTaskTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.store.inmemory;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.opendaylight.mdsal.dom.store.inmemory.TestUtils.COHORTS;
+import static org.opendaylight.mdsal.dom.store.inmemory.TestUtils.DOM_DATA_TREE_IDENTIFIER;
+import static org.opendaylight.mdsal.dom.store.inmemory.TestUtils.DOM_STORE_THREE_PHASE_COMMIT_COHORT;
+import static org.opendaylight.mdsal.dom.store.inmemory.TestUtils.LISTENABLE_FUTURE;
+import static org.opendaylight.mdsal.dom.store.inmemory.TestUtils.resetMocks;
+
+import org.junit.After;
+import org.junit.Test;
+import org.opendaylight.mdsal.common.api.TransactionCommitFailedException;
+
+public class ShardPreCommitCoordinationTaskTest {
+
+    @Test
+    public void basicTest() throws Exception {
+        doReturn(Void.TYPE).when(LISTENABLE_FUTURE).get();
+        doReturn(LISTENABLE_FUTURE).when(DOM_STORE_THREE_PHASE_COMMIT_COHORT).preCommit();
+
+        COHORTS.add(DOM_STORE_THREE_PHASE_COMMIT_COHORT);
+
+        ShardPreCommitCoordinationTask shardPreCommitCoordinationTask =
+                new ShardPreCommitCoordinationTask(DOM_DATA_TREE_IDENTIFIER, COHORTS);
+
+        shardPreCommitCoordinationTask.call();
+        verify(DOM_STORE_THREE_PHASE_COMMIT_COHORT).preCommit();
+    }
+
+    @Test(expected = TransactionCommitFailedException.class)
+    public void exceptionCallTest() throws Exception {
+        doThrow(new InterruptedException()).when(LISTENABLE_FUTURE).get();
+        doReturn(LISTENABLE_FUTURE).when(DOM_STORE_THREE_PHASE_COMMIT_COHORT).preCommit();
+
+        COHORTS.add(DOM_STORE_THREE_PHASE_COMMIT_COHORT);
+        ShardPreCommitCoordinationTask shardPreCommitCoordinationTask =
+                new ShardPreCommitCoordinationTask(DOM_DATA_TREE_IDENTIFIER, COHORTS);
+        shardPreCommitCoordinationTask.call();
+    }
+
+    @After
+    public void reset() {
+        resetMocks();
+    }
+}
