6e46aa52bf7b472a9d09f287b203223eb5912c10
Martin Ciglan <mciglan@cisco.com>
2016-06-10 09:39:15 +0200

    IN PROGRESS Bug 1411-3: MDSAL Binding2 Generator Util
    - general util classes
    - generated type & builder classes
    - Types & BindingTypes
    - Deviation issue fixed
    
    Change-Id: I9f602a893f6cbd2e9e30c6ca6dc3810e0c678d89
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/mdsal-binding2-generator-util/src/main/java/org/opendaylight/mdsal/binding2/generator/util/Binding2Mapping.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.generator.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableSet;
+import java.util.Set;
+
+/**
+ * Standard Util class that provides generated Java related functionality
+ */
+@Beta
+public final class Binding2Mapping {
+
+    private Binding2Mapping() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static final Set<String> JAVA_RESERVED_WORDS = ImmutableSet.of("abstract", "assert", "boolean", "break",
+            "byte", "case", "catch", "char", "class", "const", "continue", "default", "double", "do", "else", "enum",
+            "extends", "false", "final", "finally", "float", "for", "goto", "if", "implements", "import", "instanceof",
+            "int", "interface", "long", "native", "new", "null", "package", "private", "protected", "public", "return",
+            "short", "static", "strictfp", "super", "switch", "synchronized", "this", "throw", "throws", "transient",
+            "true", "try", "void", "volatile", "while");
+
+    //TODO: implement methods
+
+}
