b76fe81cc1706fd60ca42ceb896ddca65da6ee43
Martin Ciglan <mciglan@cisco.com>
2016-06-10 09:39:15 +0200

    Bug 1411-3: MDSAL Binding2 Generator Util
    - general util classes & methods
    - generated type & builder classes
    - Types & BindingTypes
    - Annotations, Enumeration
    - Method signature support
    - Deviation/AnnotationType issue fixed
    - Objects.hash() used in hashCode() methods
    - review comments fixed
    
    FIXME/TODO:
    Binding2GeneratorUtil - implement rest of static methods, colon-dash issue
    Binding2Mapping - implement rest of static methods, underscore issue
    
    Change-Id: I9f602a893f6cbd2e9e30c6ca6dc3810e0c678d89
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/mdsal-binding2-generator-util/src/main/java/org/opendaylight/mdsal/binding2/generator/util/TypeConstants.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.generator.util;
+
+import com.google.common.annotations.Beta;
+
+/**
+ *
+ * Contains constants used in relations with <code>Type</code>.
+ */
+@Beta
+public final class TypeConstants {
+
+    /**
+     * Name of the class constant which hold list of the regular expression
+     * strings.
+     */
+    public static final String PATTERN_CONSTANT_NAME = "PATTERN_CONSTANTS";
+
+    /**
+     * Creation of new instance is prohibited.
+     */
+    private TypeConstants() {
+    }
+}
