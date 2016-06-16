8bad6bbbb543ad63cd3a88850865a4afdef12b9f
Martin Ciglan <mciglan@cisco.com>
2016-06-16 14:27:11 +0200

    Bug 1411-4: MDSAL Binding2 Generator Impl
    - BindingGeneratorImpl decomposition proposal
    - introduction for Twirl templates for Yang Snippets generation
    - rather more templates, less logic, more content
    - JUnit test - to be finished
    - added several twirl templates for YANG
    - review comments fixed
    - apply https://git.opendaylight.org/gerrit/#/c/44939/ for spec v2
    
    Change-Id: I79cb7e9c0d41c23408949dd5d4d40d572cc86652
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>
    Signed-off-by: Filip Gregor <fgregor@cisco.com>
    (cherry picked from commit a5e2366bfdf89f9b859b0a7ecfe4bdf4b4cc423e)

---
+++ binding2/mdsal-binding2-generator-impl/src/main/java/org/opendaylight/mdsal/binding2/generator/impl/ModuleToGenType.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.generator.impl;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.annotations.Beta;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.opendaylight.mdsal.binding2.generator.spi.TypeProvider;
+import org.opendaylight.mdsal.binding2.generator.util.Binding2Mapping;
+import org.opendaylight.mdsal.binding2.generator.yang.types.TypeProviderImpl;
+import org.opendaylight.mdsal.binding2.model.api.Type;
+import org.opendaylight.mdsal.binding2.model.api.type.builder.GeneratedTypeBuilder;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.util.DataNodeIterator;
+
+@Beta
+final class ModuleToGenType {
+
+    private ModuleToGenType() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    static Map<Module, ModuleContext> generate(final Module module, final SchemaContext schemaContext,
+                                               TypeProvider typeProvider, final boolean verboseClassComments) {
+        Map<Module, ModuleContext> genCtx = new HashMap<>();
+
+        genCtx.put(module, new ModuleContext());
+        genCtx = allTypeDefinitionsToGenTypes(module, genCtx, typeProvider);
+
+        //TODO: call generate for other entities (groupings, rpcs, identities, notifications)
+
+        if (!module.getChildNodes().isEmpty()) {
+            final GeneratedTypeBuilder moduleType = GenHelperUtil.moduleToDataType(module, genCtx, verboseClassComments);
+            genCtx.get(module).addModuleNode(moduleType);
+            final String basePackageName = Binding2Mapping.getRootPackageName(module);
+            GenHelperUtil.resolveDataSchemaNodes(module, basePackageName, moduleType, moduleType, module
+                    .getChildNodes());
+        }
+
+        return genCtx;
+    }
+
+    /**
+     * Converts all extended type definitions of module to the list of
+     * <code>Type</code> objects.
+     *
+     * @param module
+     *            module from which is obtained set of type definitions
+     * @throws IllegalArgumentException
+     *             <ul>
+     *             <li>if module is null</li>
+     *             <li>if name of module is null</li>
+     *             </ul>
+     * @throws IllegalStateException
+     *             if set of type definitions from module is null
+     */
+    private static Map<Module, ModuleContext> allTypeDefinitionsToGenTypes(final Module module, Map<Module, ModuleContext> genCtx,
+                                                     TypeProvider typeProvider) {
+        checkArgument(module != null, "Module reference cannot be NULL.");
+        checkArgument(module.getName() != null, "Module name cannot be NULL.");
+        final DataNodeIterator it = new DataNodeIterator(module);
+        final List<TypeDefinition<?>> typeDefinitions = it.allTypedefs();
+        checkState(typeDefinitions != null, "Type Definitions for module «module.name» cannot be NULL.");
+
+        typeDefinitions.stream().filter(typedef -> typedef != null).forEach(typedef -> {
+            final Type type = ((TypeProviderImpl) typeProvider).generatedTypeForExtendedDefinitionType(typedef,
+                    typedef);
+            if (type != null) {
+                final ModuleContext ctx = genCtx.get(module);
+                ctx.addTypedefType(typedef.getPath(), type);
+                ctx.addTypeToSchema(type, typedef);
+            }
+        });
+        return genCtx;
+    }
+}
