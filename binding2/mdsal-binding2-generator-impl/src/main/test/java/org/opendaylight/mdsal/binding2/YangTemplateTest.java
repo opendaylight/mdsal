7b77477c76de1face9b2b23a07ca9e73dc0b30f5
Martin Ciglan <mciglan@cisco.com>
2016-06-16 14:27:11 +0200

    IN PROGRESS Bug 1411-4: MDSAL Binding2 Generator Impl
    - introduction for Twirl template for Yang Snippets generation
    - JUnit test
    
    Change-Id: I79cb7e9c0d41c23408949dd5d4d40d572cc86652
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/mdsal-binding2-generator-impl/src/main/test/java/org/opendaylight/mdsal/binding2/YangTemplateTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2;
+
+import com.google.common.annotations.Beta;
+import java.net.URISyntaxException;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding2.txt.yangtemplate;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+
+@Beta
+public class YangTemplateTest {
+
+    private Set<Module> modules;
+
+    @Before
+    public void setup() throws URISyntaxException, ReactorException {
+        modules = TestUtils.loadModules(getClass().getResource("/yang-template").toURI());
+    }
+
+    @Test
+    public void printYangSnippetForModule() {
+        for (Module module : modules) {
+            /**
+             * We should be able to call Scala render method from Binding Generator implementation
+             * for 3 different inputs:
+             * 1. single SchemaNode
+             * 2. set of SchemaNode
+             * 3. whole Module
+             */
+            final String body = yangtemplate.render(null, null, module).body();
+            //FIXME: don't do it this way, only for very first attempt to show results
+            System.out.println("module ".concat(module.getName()).concat(":").concat(body));
+        }
+
+    }
+}
