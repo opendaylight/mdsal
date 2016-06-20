18bb5c7fb6a06761829bcb0dc050eee77bb126ab
Vaclav Demcak <vdemcak@cisco.com>
2016-06-20 15:09:18 +0200

    Bug 5421 - Add new Eos instancies classes
    - implementation missing components for start MD-SAL DOMEos
      OSGi services in controller
    
    Change-Id: I90823003be5556bab6acd326ee40004cff30e1fe
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ common/mdsal-common-api/src/main/java/org/opendaylight/mdsal/common/api/clustering/AbstractGenericEntityOwnershipCandidateRegistration.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.common.api.clustering;
+
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.concepts.AbstractObjectRegistration;
+import org.opendaylight.yangtools.concepts.Path;
+
+/**
+ * Abstract base class for an EntityOwnershipCandidateRegistration.
+ *
+ * @param <P> the instance identifier path type
+ * @param <E> the GenericEntity type
+ */
+public abstract class AbstractGenericEntityOwnershipCandidateRegistration<P extends Path<P>, E extends GenericEntity<P>>
+        extends AbstractObjectRegistration<E>
+        implements GenericEntityOwnershipCandidateRegistration<P, E> {
+
+    protected AbstractGenericEntityOwnershipCandidateRegistration(@Nonnull final E entity) {
+        super(Preconditions.checkNotNull(entity, "entity cannot be null"));
+    }
+}
+
