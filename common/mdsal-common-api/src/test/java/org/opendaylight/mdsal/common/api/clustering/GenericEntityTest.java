3846fb6fb1b298e1483617c81786335f55bbb548
Peter Nosal <peter.nosal@pantheon.tech>
2016-06-25 22:14:05 +0200

    Bug 5947: Increasing code coverage - mdsal-common-api
    Change-Id: I3dd94a2d2ff0e28daff7a6efc7d01074d26939e5
    Signed-off-by: Peter Nosal <peter.nosal@pantheon.tech>

---
+++ common/mdsal-common-api/src/test/java/org/opendaylight/mdsal/common/api/clustering/GenericEntityTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.common.api.clustering;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.annotation.Nonnull;
+import org.junit.Test;
+import org.opendaylight.yangtools.concepts.Path;
+
+public class GenericEntityTest {
+
+    @Test
+    public void basicTest() throws Exception {
+        final TestClass testClass = new TestClass();
+        final GenericEntity genericEntity = new GenericEntity<>("testType", testClass);
+        final GenericEntity genericEntityDiff = new GenericEntity<>("differentTestType", new TestClassDiff());
+
+        assertEquals(TestClass.class, genericEntity.getIdentifier().getClass());
+        assertEquals("testType", genericEntity.getType());
+        assertTrue(genericEntity.toString().contains("testType"));
+        assertNotEquals(genericEntity.hashCode(), genericEntityDiff.hashCode());
+        assertTrue(genericEntity.equals(genericEntity));
+        assertTrue(genericEntity.equals(new GenericEntity<>("testType", testClass)));
+        assertFalse(genericEntity.equals(genericEntityDiff));
+        assertFalse(genericEntity.equals(new String()));
+        assertFalse(genericEntity.equals(new GenericEntity<>("differentTestType", testClass)));
+    }
+
+    private final class TestClass implements Path {
+        @Override
+        public boolean contains(@Nonnull Path other) {
+            return false;
+        }
+    }
+
+    private final class TestClassDiff implements Path {
+        @Override
+        public boolean contains(@Nonnull Path other) {
+            return false;
+        }
+    }
+}
