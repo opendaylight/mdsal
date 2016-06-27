fe7810bd2bb0d251b399a4f8b04e897bbcae1a69
Peter Nosal <peter.nosal@pantheon.tech>
2016-06-27 10:20:50 +0200

    Bug 5947: Increasing code coverage - ietf-type-util
    Change-Id: I37f82d3a2dd0bde934ad49a1f13c526828f103cb
    Signed-off-by: Peter Nosal <peter.nosal@pantheon.tech>

--- model/ietf/ietf-type-util/src/test/java/org/opendaylight/mdsal/model/ietf/util/IpUtil.java
+++ model/ietf/ietf-type-util/src/test/java/org/opendaylight/mdsal/model/ietf/util/IpUtil.java
@@ -5,10 +5,17 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.mdsal.model.ietf.util;
 
+import java.util.regex.Pattern;
+import javax.annotation.RegEx;
+
 final class IpUtil extends AbstractIetfInetUtil<IpClass, IpClass, IpClass, IpClass, IpClass, IpClass> {
+
+    @RegEx
+    private static final String IP_V4_REGEX = "^\\d+\\.\\d+\\.\\d+\\.\\d+$";
+    private static final Pattern IP_V4_PATTERN = Pattern.compile(IP_V4_REGEX);
+
     IpUtil() {
         super(IpClass.class, IpClass.class, IpClass.class, IpClass.class);
     }
@@ -55,11 +62,11 @@ final class IpUtil extends AbstractIetfInetUtil<IpClass, IpClass, IpClass, IpCla
 
     @Override
     protected IpClass maybeIpv4Address(IpClass addr) {
-        return addr;
+        return IP_V4_PATTERN.matcher(addr._value).matches() ? addr : null;
     }
 
     @Override
     protected IpClass maybeIpv6Address(IpClass addr) {
-        return addr;
+        return addr._value.indexOf(':') != -1 ? addr : null;
     }
 }
