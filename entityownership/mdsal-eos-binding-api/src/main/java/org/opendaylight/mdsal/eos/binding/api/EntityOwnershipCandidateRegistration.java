e9a5fa453137f362ea9580b91548b3452f5aaae7
Vaclav Demcak <vdemcak@cisco.com>
2016-07-07 13:38:26 +0200

    Move Binding EntityOwnershipService
    Binding EntityOwnershipService API is moved to spearate module.
    
    * split mdsal-binding-api for MD-SAL Binding core API and EOS
    * add new artifacts definitions
    
    Change-Id: I30eb448932630bdc28f9230eb05b1a3aa7642c8d
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ entityownership/mdsal-eos-binding-api/src/main/java/org/opendaylight/mdsal/eos/binding/api/EntityOwnershipCandidateRegistration.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.eos.binding.api;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.mdsal.eos.common.api.GenericEntityOwnershipCandidateRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Binding version of {@link GenericEntityOwnershipCandidateRegistration}.
+ *
+ * @author Thomas Pantelis
+ */
+@Beta
+public interface EntityOwnershipCandidateRegistration extends
+        GenericEntityOwnershipCandidateRegistration<InstanceIdentifier<?>, Entity> {
+}
---
+++ entityownership/mdsal-eos-binding-api/src/main/java/org/opendaylight/mdsal/eos/binding/api/EntityOwnershipListener.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.eos.binding.api;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.mdsal.eos.common.api.GenericEntityOwnershipListener;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Binding interface for GenericEntityOwnershipListener.
+ *
+ * @author Thomas Pantelis
+ */
+@Beta
+public interface EntityOwnershipListener extends
+        GenericEntityOwnershipListener<InstanceIdentifier<?>, EntityOwnershipChange> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void ownershipChanged(EntityOwnershipChange ownershipChange);
+}
