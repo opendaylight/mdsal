b36795bfe099cc41647f18eff6016f726deb0649
Vaclav Demcak <vdemcak@cisco.com>
2016-07-08 14:30:38 +0200

    Rename EosCommonSpi package name
    * rename package from org.opendaylight.mdsal.common.api.clustering
      to org.opendaylight.mdsal.eos.common.spi
    
    Change-Id: Ic5c67f9575265a56a7a04e6787ca0ba33aa37ba3
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- entityownership/mdsal-eos-common-spi/src/test/java/org/opendaylight/mdsal/eos/common/spi/AbstractGenericEntityOwnershipCandidateRegistrationTest.java
+++ entityownership/mdsal-eos-common-spi/src/test/java/org/opendaylight/mdsal/eos/common/spi/AbstractGenericEntityOwnershipCandidateRegistrationTest.java
@@ -5,13 +5,14 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.mdsal.common.api.clustering;
+package org.opendaylight.mdsal.eos.common.spi;
 
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
 import org.opendaylight.mdsal.eos.common.api.GenericEntity;
+import org.opendaylight.mdsal.eos.common.spi.AbstractGenericEntityOwnershipCandidateRegistration;
 
 public class AbstractGenericEntityOwnershipCandidateRegistrationTest
         extends AbstractGenericEntityOwnershipCandidateRegistration {
