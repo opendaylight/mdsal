1d3ea7081284dd3e0eb25ef5082b8afd9c221798
Vaclav Demcak <vdemcak@cisco.com>
2016-07-21 13:49:47 +0200

    Remove mdsal-singleton-dom-api
    We are using only DOM implementation so extra DOM API is not needed.
    
    * delete mdsal-singleton-dom-api module
    * clean artifacts/pom.xml
    * clean features/pom.xml
    * clean features.xml
    * clean singleton-service/pom.xml
    * clean mdsal-singleton-dom-impl/pom.xml
    
    inpact for controller patch:
    https://git.opendaylight.org/gerrit/#/c/42093/2
    fix is here:
    depends on - https://git.opendaylight.org/gerrit/#/c/42311/
    blocks - https://git.opendaylight.org/gerrit/#/c/42317/
    
    Change-Id: I8ece69ec65c3ed574160ef8a1c8cd27476f8d1f2
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- singleton-service/mdsal-singleton-dom-impl/src/main/java/org/opendaylight/mdsal/singleton/dom/impl/DOMClusterSingletonServiceProviderImpl.java
+++ singleton-service/mdsal-singleton-dom-impl/src/main/java/org/opendaylight/mdsal/singleton/dom/impl/DOMClusterSingletonServiceProviderImpl.java
@@ -14,7 +14,6 @@ import org.opendaylight.mdsal.eos.dom.api.DOMEntityOwnershipChange;
 import org.opendaylight.mdsal.eos.dom.api.DOMEntityOwnershipListener;
 import org.opendaylight.mdsal.eos.dom.api.DOMEntityOwnershipListenerRegistration;
 import org.opendaylight.mdsal.eos.dom.api.DOMEntityOwnershipService;
-import org.opendaylight.mdsal.singleton.dom.api.DOMClusterSingletonServiceProvider;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 
@@ -27,7 +26,7 @@ public final class DOMClusterSingletonServiceProviderImpl extends
                                                     DOMEntityOwnershipListener,
                                                     DOMEntityOwnershipService,
                                                     DOMEntityOwnershipListenerRegistration>
-        implements DOMClusterSingletonServiceProvider {
+        implements DOMEntityOwnershipListener {
 
     /**
      * Initialization all needed class internal property for {@link DOMClusterSingletonServiceProviderImpl}
