28da9390c5cea9a731776b470b6c467a023d3935
Michael Vorburger <vorburger@redhat.com>
2016-07-21 03:39:34 +0200

    Checkstyle clean-up src/test/java
    Required to be able to run (enforce) CS on src/test as well. 
    
    Change-Id: Ife78f9baf1fcb462d15ca59675e4116903048c6b
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/ShardedDOMDataTreeTest.java
+++ dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/ShardedDOMDataTreeTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.mdsal.dom.broker;
 
 import static org.junit.Assert.assertEquals;
@@ -98,8 +105,8 @@ public class ShardedDOMDataTreeTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testProducerPathContention() throws Exception {
-        final DOMDataTreeProducer p1 = dataTreeService.createProducer(Collections.singletonList(ROOT_ID));
-        final DOMDataTreeProducer p2 = dataTreeService.createProducer(Collections.singletonList(TEST_ID));
+        dataTreeService.createProducer(Collections.singletonList(ROOT_ID));
+        dataTreeService.createProducer(Collections.singletonList(TEST_ID));
     }
 
     @Test
