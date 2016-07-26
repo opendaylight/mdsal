6e72ff9f7afad96fa7e62e02341634ed95ef06ee
Peter Nosal <peter.nosal@pantheon.tech>
2016-07-26 13:32:04 +0200

    Bug 5947: added some tests for osgi in dom-broker
    Change-Id: I3d404e70bd34daa47a86f072420c5d06c7674425
    Signed-off-by: Peter Nosal <peter.nosal@pantheon.tech>

---
+++ dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/osgi/SchemaServiceActivatorTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.broker.osgi;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.lang.reflect.Field;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Filter;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+public class SchemaServiceActivatorTest {
+
+    @Test
+    public void basicTest() throws Exception {
+        final BundleContext bundleContext = mock(BundleContext.class, CALLS_REAL_METHODS);
+        doReturn(mock(Filter.class)).when(bundleContext).createFilter(any());
+        doNothing().when(bundleContext).addBundleListener(any());
+        doReturn(new Bundle[] {}).when(bundleContext).getBundles();
+        doNothing().when(bundleContext).addServiceListener(any(), any());
+        doReturn(new ServiceReference<?>[] {}).when(bundleContext).getServiceReferences(anyString(), any());
+        doReturn(mock(ServiceRegistration.class)).when(bundleContext).registerService(any(Class.class), any(), any());
+        SchemaServiceActivator schemaServiceActivator = new SchemaServiceActivator();
+        schemaServiceActivator.start(bundleContext);
+
+        final ServiceRegistration registration = mock(ServiceRegistration.class);
+        final OsgiBundleScanningSchemaService osgiBundle = mock(OsgiBundleScanningSchemaService.class, CALLS_REAL_METHODS);
+
+        final Field schemaServiceRegField = SchemaServiceActivator.class.getDeclaredField("schemaServiceReg");
+        schemaServiceRegField.setAccessible(true);
+        schemaServiceRegField.set(schemaServiceActivator, registration);
+
+        final Field schemaServiceField = SchemaServiceActivator.class.getDeclaredField("schemaService");
+        schemaServiceField.setAccessible(true);
+        schemaServiceField.set(schemaServiceActivator, osgiBundle);
+
+        doNothing().when(registration).unregister();
+        doNothing().when(osgiBundle).close();
+
+        schemaServiceActivator.stop(bundleContext);
+
+        verify(registration).unregister();
+        verify(osgiBundle).close();
+    }
+}
