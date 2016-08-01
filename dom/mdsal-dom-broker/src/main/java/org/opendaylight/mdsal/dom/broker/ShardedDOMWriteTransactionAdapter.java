513a714310d323f8e0857b952debf163c70cee26
Jakub Morvay <jmorvay@cisco.com>
2016-08-01 09:36:39 +0200

    Bug 5699 - Migrate existing code to use new sharding apis
    This adds adapters that translates DOMDataBroker API calls to cursor aware
    API calls
    
    Change-Id: Iab0a94af598f355768f6ef48ac8cb358960418b1
    Signed-off-by: Jakub Morvay <jmorvay@cisco.com>

---
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/ShardedDOMWriteTransactionAdapter.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.dom.broker;
+
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.common.api.TransactionCommitFailedException;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeCursorAwareTransaction;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeProducer;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeProducerException;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeService;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteCursor;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShardedDOMWriteTransactionAdapter implements DOMDataTreeWriteTransaction {
+    private static final Logger LOG = LoggerFactory.getLogger(ShardedDOMWriteTransactionAdapter.class);
+
+    private final Map<LogicalDatastoreType, DOMDataTreeProducer> producerMap;
+    private final Map<LogicalDatastoreType, DOMDataTreeCursorAwareTransaction> transactionMap;
+    private final Map<LogicalDatastoreType, DOMDataTreeWriteCursor> cursorMap;
+
+    private final DOMDataTreeService treeService;
+    private final Object txIdentifier;
+    private boolean finished = false;
+    private boolean initialized = false;
+
+    ShardedDOMWriteTransactionAdapter(final Object identifier, final DOMDataTreeService transactionDelegator) {
+        this.treeService = Preconditions.checkNotNull(transactionDelegator);
+        this.txIdentifier = Preconditions.checkNotNull(identifier);
+        this.producerMap = new EnumMap<>(LogicalDatastoreType.class);
+        this.transactionMap = new EnumMap<>(LogicalDatastoreType.class);
+        this.cursorMap = new EnumMap<>(LogicalDatastoreType.class);
+    }
+
+    @Override
+    public boolean cancel() {
+        LOG.debug("{}: Cancelling transaction");
+        if (finished == true) {
+            return false;
+        }
+
+        // We close cursor, cancel transactions and close producers and
+        // mark transaction as finished
+        cursorMap.values().forEach(DOMDataTreeWriteCursor::close);
+        transactionMap.values().forEach(domDataTreeCursorAwareTransaction ->
+                Preconditions.checkState(domDataTreeCursorAwareTransaction.cancel()));
+        closeProducers();
+        finished = true;
+        return true;
+    }
+
+    @Override
+    public CheckedFuture<Void, TransactionCommitFailedException> submit() {
+        checkRunning();
+        LOG.debug("{}: Submitting transaction", txIdentifier);
+        if (initialized == false) {
+            // If underlying producers, transactions and cursors are
+            // not even initialized just seal this transaction and
+            // return immediate future
+            finished = true;
+            return Futures.immediateCheckedFuture(null);
+        }
+        // First we need to close cursors
+        cursorMap.values().forEach(DOMDataTreeWriteCursor::close);
+        final ListenableFuture<List<Void>> aggregatedSubmit = Futures.allAsList(
+                transactionMap.get(LogicalDatastoreType.CONFIGURATION).submit(),
+                transactionMap.get(LogicalDatastoreType.OPERATIONAL).submit());
+
+        // Now we can close producers and mark transaction as finished
+        closeProducers();
+        finished = true;
+
+        return Futures.makeChecked(
+                Futures.transform(aggregatedSubmit, (List<Void> input) -> input.get(0)),
+                TransactionCommitFailedExceptionMapper.COMMIT_ERROR_MAPPER);
+    }
+
+    @Override
+    public Object getIdentifier() {
+        return txIdentifier;
+    }
+
+    @Override
+    public void put(final LogicalDatastoreType store, final YangInstanceIdentifier path,
+                    final NormalizedNode<?, ?> data) {
+        checkRunning();
+        LOG.debug("{}: Invoking put operation at {}:{} with payload {}", txIdentifier, store, path);
+        if (initialized == false) {
+            initializeDataTreeProducerLayer(path.getParent());
+        }
+
+        final DOMDataTreeWriteCursor cursor = cursorMap.get(store);
+        cursor.write(path.getLastPathArgument(), data);
+    }
+
+    @Override
+    public void merge(final LogicalDatastoreType store, final YangInstanceIdentifier path,
+                      final NormalizedNode<?, ?> data) {
+        checkRunning();
+        LOG.debug("{}: Invoking merge operation at {}:{} with payload {}", txIdentifier, store, path);
+        if (initialized == false) {
+            initializeDataTreeProducerLayer(path.getParent());
+        }
+
+        final DOMDataTreeWriteCursor cursor = cursorMap.get(store);
+        cursor.merge(path.getLastPathArgument(), data);
+    }
+
+    @Override
+    public void delete(final LogicalDatastoreType store, final YangInstanceIdentifier path) {
+        checkRunning();
+        LOG.debug("{}: Invoking delete operation at {}:{}", txIdentifier, store, path);
+        if (initialized == false) {
+            initializeDataTreeProducerLayer(path.getParent());
+        }
+
+        final DOMDataTreeWriteCursor cursor = cursorMap.get(store);
+        cursor.delete(path.getLastPathArgument());
+    }
+
+    // TODO initialize producer, transaction and cursor for only
+    // for necessary data store at one time
+    private void initializeDataTreeProducerLayer(final YangInstanceIdentifier path) {
+        Preconditions.checkState(producerMap.isEmpty(), "Producers already initialized");
+        Preconditions.checkState(transactionMap.isEmpty(), "Transactions already initialized");
+        Preconditions.checkState(cursorMap.isEmpty(), "Cursors already initialized");
+
+        LOG.debug("{}: Creating data tree producers on path {}", txIdentifier, path);
+        producerMap.put(LogicalDatastoreType.CONFIGURATION,
+                treeService.createProducer(
+                        Collections.singleton(new DOMDataTreeIdentifier(LogicalDatastoreType.CONFIGURATION, path))));
+        producerMap.put(LogicalDatastoreType.OPERATIONAL,
+                treeService.createProducer(
+                        Collections.singleton(new DOMDataTreeIdentifier(LogicalDatastoreType.OPERATIONAL, path))));
+
+        LOG.debug("{}: Creating DOMDataTreeCursorAwareTransactions delegates", txIdentifier, path);
+        transactionMap.put(LogicalDatastoreType.CONFIGURATION,
+                producerMap.get(LogicalDatastoreType.CONFIGURATION).createTransaction(true));
+        transactionMap.put(LogicalDatastoreType.OPERATIONAL,
+                producerMap.get(LogicalDatastoreType.OPERATIONAL).createTransaction(true));
+
+        LOG.debug("{}: Creating DOMDataTreeWriteCursors delegates");
+        cursorMap.put(LogicalDatastoreType.CONFIGURATION,
+                transactionMap.get(LogicalDatastoreType.CONFIGURATION)
+                        .createCursor(new DOMDataTreeIdentifier(LogicalDatastoreType.CONFIGURATION, path)));
+        cursorMap.put(LogicalDatastoreType.OPERATIONAL,
+                transactionMap.get(LogicalDatastoreType.OPERATIONAL)
+                        .createCursor(new DOMDataTreeIdentifier(LogicalDatastoreType.OPERATIONAL, path)));
+
+        initialized = true;
+    }
+
+    private void checkRunning() {
+        Preconditions.checkState(finished == false, "{}: Transaction already finished");
+    }
+
+    private void closeProducers() {
+        producerMap.values().forEach(domDataTreeProducer ->
+        {
+            try {
+                domDataTreeProducer.close();
+            } catch (final DOMDataTreeProducerException e) {
+                throw new IllegalStateException("Trying to close DOMDataTreeProducer with open transaction", e);
+            }
+        });
+    }
+}
---
+++ dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/ShardedDOMReadTransactionAdapterTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.dom.broker;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.CheckedFuture;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.common.api.ReadFailedException;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeListener;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeLoopException;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeProducer;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeService;
+import org.opendaylight.mdsal.dom.broker.util.TestModel;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidates;
+
+public class ShardedDOMReadTransactionAdapterTest {
+
+    private ShardedDOMReadTransactionAdapter readTx;
+
+    @Before
+    public void setUp() {
+        readTx = new ShardedDOMReadTransactionAdapter("TEST-TX", new TestTreeService());
+    }
+
+    @Test
+    public void testGetIdentifier() {
+        assertEquals("TEST-TX", readTx.getIdentifier());
+    }
+
+    @Test
+    public void testRead() throws Exception {
+        final CheckedFuture<Optional<NormalizedNode<?, ?>>, ReadFailedException> readResult =
+                readTx.read(LogicalDatastoreType.CONFIGURATION, TestModel.TEST_PATH);
+        assertEquals(readResult.checkedGet().get(), TestUtils.TEST_CONTAINER);
+    }
+
+
+    private static class TestTreeService implements DOMDataTreeService {
+
+        @Nonnull
+        @Override
+        public <T extends DOMDataTreeListener> ListenerRegistration<T>
+        registerListener(@Nonnull final T listener, @Nonnull final Collection<DOMDataTreeIdentifier> subtrees,
+                         final boolean allowRxMerges,
+                         @Nonnull final Collection<DOMDataTreeProducer> producers) throws DOMDataTreeLoopException {
+            final Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtree = Maps.newHashMap();
+            subtree.put(new DOMDataTreeIdentifier(LogicalDatastoreType.CONFIGURATION, TestModel.TEST_PATH),
+                    TestUtils.TEST_CONTAINER);
+
+            listener.onDataTreeChanged(Collections.singleton(
+                    DataTreeCandidates.fromNormalizedNode(TestModel.TEST_PATH, TestUtils.TEST_CONTAINER)), subtree);
+
+            return new ListenerRegistration<T>() {
+                @Override
+                public void close() {
+                    // NOOP
+                }
+
+                @Override
+                public T getInstance() {
+                    return listener;
+                }
+            };
+        }
+
+        @Nonnull
+        @Override
+        public DOMDataTreeProducer createProducer(@Nonnull final Collection<DOMDataTreeIdentifier> subtrees) {
+            return null;
+        }
+    }
+}
