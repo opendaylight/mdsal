8d9dbf6377f91f8e49e67a0fa86f8be1d2b79865
Vaclav Demcak <vdemcak@cisco.com>
2016-08-25 16:20:48 +0200

    Bug 6540 - HotFix for IsolatedLeader message handler
    Change-Id: Icc495dbbbc5583ed68f162ace0e7f2ab12c498c3
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- entityownership/mdsal-eos-common-api/src/main/java/org/opendaylight/mdsal/eos/common/api/EntityOwnershipChangeState.java
+++ entityownership/mdsal-eos-common-api/src/main/java/org/opendaylight/mdsal/eos/common/api/EntityOwnershipChangeState.java
@@ -35,6 +35,12 @@ public enum EntityOwnershipChangeState {
     LOCAL_OWNERSHIP_LOST_NO_OWNER(true, false, false),
 
     /**
+     * The local process instance has no change but we need to process an IsolatedLeader change to actual
+     * leader instance.
+     */
+    LOCAL_OWNERSHIP_GRANTED_NOT_CHANGE(true, true, true),
+
+    /**
      * Entity ownership has transitioned to another process instance and this instance was not the previous owner.
      */
     REMOTE_OWNERSHIP_CHANGED(false, false, true),
@@ -47,8 +53,8 @@ public enum EntityOwnershipChangeState {
 
     private static final Map<Key, EntityOwnershipChangeState> BY_KEY;
     static {
-        Builder<Key, EntityOwnershipChangeState> builder = ImmutableMap.<Key, EntityOwnershipChangeState>builder();
-        for(EntityOwnershipChangeState e: values()) {
+        final Builder<Key, EntityOwnershipChangeState> builder = ImmutableMap.<Key, EntityOwnershipChangeState>builder();
+        for(final EntityOwnershipChangeState e: values()) {
             builder.put(new Key(e.wasOwner, e.isOwner, e.hasOwner), e);
         }
 
@@ -59,7 +65,7 @@ public enum EntityOwnershipChangeState {
     private final boolean isOwner;
     private final boolean hasOwner;
 
-    private EntityOwnershipChangeState(boolean wasOwner, boolean isOwner, boolean hasOwner) {
+    private EntityOwnershipChangeState(final boolean wasOwner, final boolean isOwner, final boolean hasOwner) {
         this.wasOwner = wasOwner;
         this.isOwner = isOwner;
         this.hasOwner = hasOwner;
@@ -95,8 +101,8 @@ public enum EntityOwnershipChangeState {
         return name() + " [wasOwner=" + wasOwner + ", isOwner=" + isOwner + ", hasOwner=" + hasOwner + "]";
     }
 
-    public static EntityOwnershipChangeState from(boolean wasOwner, boolean isOwner, boolean hasOwner) {
-        EntityOwnershipChangeState state = BY_KEY.get(new Key(wasOwner, isOwner, hasOwner));
+    public static EntityOwnershipChangeState from(final boolean wasOwner, final boolean isOwner, final boolean hasOwner) {
+        final EntityOwnershipChangeState state = BY_KEY.get(new Key(wasOwner, isOwner, hasOwner));
         Preconditions.checkArgument(state != null, "Invalid combination of wasOwner: %s, isOwner: %s, hasOwner: %s",
                 wasOwner, isOwner, hasOwner);
         return state;
@@ -107,7 +113,7 @@ public enum EntityOwnershipChangeState {
         private final boolean isOwner;
         private final boolean hasOwner;
 
-        Key(boolean wasOwner, boolean isOwner, boolean hasOwner) {
+        Key(final boolean wasOwner, final boolean isOwner, final boolean hasOwner) {
             this.wasOwner = wasOwner;
             this.isOwner = isOwner;
             this.hasOwner = hasOwner;
@@ -124,8 +130,8 @@ public enum EntityOwnershipChangeState {
         }
 
         @Override
-        public boolean equals(Object obj) {
-            Key other = (Key) obj;
+        public boolean equals(final Object obj) {
+            final Key other = (Key) obj;
             return hasOwner == other.hasOwner && isOwner == other.isOwner && wasOwner == other.wasOwner;
         }
     }
