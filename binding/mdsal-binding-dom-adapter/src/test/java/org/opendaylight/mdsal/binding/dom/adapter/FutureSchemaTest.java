a48bc4ea5ea6f7a85db27f392ce090028e7fad3a
Peter Nosal <peter.nosal@pantheon.tech>
2016-08-30 14:51:47 +0200

    Bug 5947: additional tests for binding-dom-adapter
    - FutureSchema
    - BindingDOMTransactionChainAdapter
    - BindingDOMRpcImplementationAdapter
    - BindingDOMRpcAdapterRegistration
    
    Change-Id: I511d39671d6b32ae49a2c8dc58da1427e96c3207
    Signed-off-by: Peter Nosal <peter.nosal@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/FutureSchemaTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import com.google.common.collect.ImmutableSet;
+import java.net.URI;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+public class FutureSchemaTest {
+
+    @Test
+    public void basicTest() throws Exception {
+        final FutureSchema futureSchema = new FutureSchema(0, TimeUnit.MICROSECONDS);
+        assertNotNull(futureSchema);
+        assertFalse(futureSchema.waitForSchema(new URI("test"), new Date(0)));
+        assertFalse(futureSchema.waitForSchema(ImmutableSet.of()));
+        assertEquals(0, futureSchema.getDuration());
+        assertEquals(TimeUnit.MICROSECONDS, futureSchema.getUnit());
+    }
+}
