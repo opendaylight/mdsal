8ea54d03143e65a13414b95a8b7bf7e6e90a3184
Peter Nosal <peter.nosal@pantheon.tech>
2016-09-02 13:51:42 +0200

    Bug 5947: yet another tests for binding-dom-adapter
    Change-Id: I57abe83c9fc49beee81764977b3ae3528ee2dccc
    Signed-off-by: Peter Nosal <peter.nosal@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMDataTreeCommitCohortAdapterTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.api.DataTreeCommitCohort;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTree;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTreeNode;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeCandidate;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.yangtools.binding.data.codec.impl.BindingNormalizedNodeCodecRegistry;
+import org.opendaylight.yangtools.sal.binding.generator.impl.GeneratedClassLoadingStrategy;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+
+public class BindingDOMDataTreeCommitCohortAdapterTest {
+
+    @Test
+    public void canCommitTest() throws Exception {
+        final DataTreeCommitCohort<?> cohort = mock(DataTreeCommitCohort.class);
+        final BindingNormalizedNodeCodecRegistry registry = mock(BindingNormalizedNodeCodecRegistry.class);
+        final BindingToNormalizedNodeCodec codec =
+                new BindingToNormalizedNodeCodec(GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy(), registry);
+
+        final BindingDOMDataTreeCommitCohortAdapter adapter =
+                new BindingDOMDataTreeCommitCohortAdapter<>(codec, cohort);
+        assertNotNull(adapter);
+
+        final DOMDataTreeCandidate domDataTreeCandidate = mock(DOMDataTreeCandidate.class);
+        final DOMDataTreeIdentifier domDataTreeIdentifier =
+                new DOMDataTreeIdentifier(LogicalDatastoreType.OPERATIONAL, YangInstanceIdentifier.EMPTY);
+        doReturn(InstanceIdentifier.create(DataObject.class)).when(registry).fromYangInstanceIdentifier(any());
+        final BindingCodecTree bindingCodecTree = mock(BindingCodecTree.class);
+        final BindingCodecTreeNode bindingCodecTreeNode = mock(BindingCodecTreeNode.class);
+        doReturn(bindingCodecTreeNode).when(bindingCodecTree).getSubtreeCodec(any(InstanceIdentifier.class));
+        doReturn(bindingCodecTree).when(registry).getCodecContext();
+        doReturn(domDataTreeIdentifier).when(domDataTreeCandidate).getRootPath();
+        doReturn(mock(DataTreeCandidateNode.class)).when(domDataTreeCandidate).getRootNode();
+        assertNotNull(LazyDataTreeModification.create(codec, domDataTreeCandidate));
+
+        doReturn(null).when(cohort).canCommit(any(), any());
+        adapter.canCommit(new Object(), domDataTreeCandidate, null);
+        verify(cohort).canCommit(any(), any());
+    }
+}
