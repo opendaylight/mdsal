a0bc681bd2b93030c0a22af4e6e361622fbf8490
Peter Nosal <peter.nosal@pantheon.tech>
2016-09-19 09:47:14 +0200

    Bug 5947: additional tests for dom-broker and inmemory-datastore #4
    Change-Id: I43af7e4dae33e8d7a5fd9c8c4c58afe747f3f7c3
    Signed-off-by: Peter Nosal <peter.nosal@pantheon.tech>

---
+++ dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/AbstractDOMForwardedCompositeTransactionTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.broker;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import org.junit.Test;
+import org.opendaylight.mdsal.dom.spi.store.DOMStoreTransaction;
+
+public class AbstractDOMForwardedCompositeTransactionTest{
+
+    private static final DOMStoreTransaction FAIL_TX1 = mock(DOMStoreTransaction.class);
+    private static final DOMStoreTransaction FAIL_TX2 = mock(DOMStoreTransaction.class);
+
+    @Test(expected = IllegalStateException.class)
+    public void closeSubtransactionsTest() throws Exception {
+        doThrow(UnsupportedOperationException.class).when(FAIL_TX1).close();
+        doThrow(UnsupportedOperationException.class).when(FAIL_TX2).close();
+
+        final AbstractDOMForwardedCompositeTransaction domForwardedCompositeTransaction =
+                new DOMForwardedCompositeTransactionTestImpl("testIdent",
+                        ImmutableMap.of("testKey1", FAIL_TX1, "testKey2", FAIL_TX2));
+
+        domForwardedCompositeTransaction.closeSubtransactions();
+    }
+
+    private class DOMForwardedCompositeTransactionTestImpl
+            extends AbstractDOMForwardedCompositeTransaction<String, DOMStoreTransaction> {
+
+        private DOMForwardedCompositeTransactionTestImpl(Object identifier, 
+                                                         Map<String, DOMStoreTransaction> backingTxs) {
+            super(identifier, backingTxs);
+        }
+    }
+}
