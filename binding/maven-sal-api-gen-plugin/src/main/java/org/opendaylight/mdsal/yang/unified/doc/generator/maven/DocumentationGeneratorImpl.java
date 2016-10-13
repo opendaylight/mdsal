66470c23b3ee70af2208cf1a19ee261619cfdf9b
Rashmi Pujar <rpujar@inocybe.com>
2016-10-13 11:35:06 -0400

    Bug 6859: Cleanup package names for maven-sal-api-gen-plugin module
    The APIs are copied to new package.
    Added deprecated annotation to the old api.
    
    Change-Id: Ie90e74e380546483d99a9aa86295045866704f31
    Signed-off-by: Rashmi Pujar <rpujar@inocybe.com>

--- binding/maven-sal-api-gen-plugin/src/main/java/org/opendaylight/mdsal/yang/unified/doc/generator/maven/DocumentationGeneratorImpl.java
+++ binding/maven-sal-api-gen-plugin/src/main/java/org/opendaylight/mdsal/yang/unified/doc/generator/maven/DocumentationGeneratorImpl.java
@@ -5,28 +5,21 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.unified.doc.generator.maven;
+package org.opendaylight.mdsal.yang.unified.doc.generator.maven;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
-
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.unified.doc.generator.GeneratorImpl;
 import org.opendaylight.yangtools.yang2sources.spi.BasicCodeGenerator;
 
-/**
- * @deprecated Use {@link org.opendaylight.yangtools.yang.unified.doc.generator.maven.DocumentationGeneratorImpl} instead
- */
-@Deprecated
 public class DocumentationGeneratorImpl extends GeneratorImpl implements BasicCodeGenerator {
 
     @Override
     public Collection<File> generateSources(SchemaContext arg0, File arg1, Set<Module> arg2) throws IOException {
-        // TODO Auto-generated method stub
          return generate(arg0, arg1, arg2);
     }
 
