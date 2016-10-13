e792c7e77a06f061b610eebfe8c4ccc652bf7107
Filip Gregor <fgregor@cisco.com>
2016-10-13 16:54:36 +0200

    IN PROGRESS Bug 1411-5 #11: tests
    merge after all pathes
    
    Change-Id: I0eadd7c7ac27af7e66306e8a5768b850947474b4
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

---
+++ binding2/mdsal-binding2-java-api-generator/src/test/java/org/opendaylight/mdsal/binding2/java/api/generator/renderers/UnionBuilderRendererTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.java.api.generator.renderers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding2.generator.util.generated.type.builder.GeneratedTOBuilderImpl;
+import org.opendaylight.mdsal.binding2.model.api.GeneratedTransferObject;
+
+public class UnionBuilderRendererTest {
+    private GeneratedTransferObject genType;
+
+    @Before
+    public void setup() {
+        final GeneratedTOBuilderImpl generatedTOBuilderImpl = new GeneratedTOBuilderImpl("test.package.name", "testName");
+        generatedTOBuilderImpl.setDescription("test description");
+        genType = generatedTOBuilderImpl.toInstance();
+    }
+
+    @Test
+    public void unionBuilderRendererTest() {
+        //        TO DO implement tests
+        String unionBuilderRenderer = new UnionBuilderRenderer(genType).generateTemplate();
+        System.out.println(unionBuilderRenderer);
+    }
+}
