c718b0c6c12f274095fba9ab2496b34bd69c8914
Josh <jhershbe@redhat.com>
2016-10-13 12:06:26 +0200

    WIP mdsal-trace
    Change-Id: I715b35d9d7b8234326c2be9556295e6c7e3c4b3d
    Signed-off-by: Josh <jhershbe@redhat.com>

---
+++ trace/dom-impl/src/main/java/org/opendaylight/mdsal/trace/dom/impl/TracingDOMDataBroker.java
@@ -0,0 +1,10 @@
+package org.opendaylight.mdsal.trace.dom.impl;
+
+/**
+ * Created by jhershbe on 10/6/16.
+ */
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+
+public interface TracingDOMDataBroker extends DOMDataBroker {
+
+}
