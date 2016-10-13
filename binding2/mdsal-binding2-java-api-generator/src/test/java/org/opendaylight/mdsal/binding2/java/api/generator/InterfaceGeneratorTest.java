5337f05cc8445e7c04eff93a6979a907b4a690f5
Filip Gregor <fgregor@cisco.com>
2016-10-13 16:54:36 +0200

    IN PROGRESS Bug 1411-5 #11: tests
    merge after all pathes
    
    Change-Id: I0eadd7c7ac27af7e66306e8a5768b850947474b4
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

---
+++ binding2/mdsal-binding2-java-api-generator/src/test/java/org/opendaylight/mdsal/binding2/java/api/generator/InterfaceGeneratorTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.java.api.generator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding2.generator.util.generated.type.builder.GeneratedTypeBuilderImpl;
+import org.opendaylight.mdsal.binding2.model.api.Type;
+
+public class InterfaceGeneratorTest {
+    private Type genType;
+
+    @Before
+    public void setup() {
+        final GeneratedTypeBuilderImpl generatedTypeBuilder = new GeneratedTypeBuilderImpl("test.package.name", "testName");
+        generatedTypeBuilder.setDescription("test description");
+        genType = generatedTypeBuilder.toInstance();
+    }
+
+    @Test
+    public void interfaceGeneratorTest() {
+//        TO DO implement tests
+        String interfaceGenerator = new InterfaceGenerator().generate(genType);
+        System.out.println(interfaceGenerator);
+    }
+}
