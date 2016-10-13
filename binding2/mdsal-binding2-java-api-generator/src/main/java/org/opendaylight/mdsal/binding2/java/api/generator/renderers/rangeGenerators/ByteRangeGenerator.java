19800c8f789f538603b4e00f775dcf90712dd781
Filip Gregor <fgregor@cisco.com>
2016-10-13 15:47:02 +0200

    IN PROGRESS Bug 1411-5 #3: MDSAL Binding2 Java API Generator
    older range generators
    
    Change-Id: I53d448049bdd0a0fcdc7e113b7c209699321fab9
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

---
+++ binding2/mdsal-binding2-java-api-generator/src/main/java/org/opendaylight/mdsal/binding2/java/api/generator/renderers/rangeGenerators/ByteRangeGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding2.java.api.generator.renderers.rangeGenerators;
+
+final class ByteRangeGenerator extends AbstractSubIntegerRangeGenerator<Byte> {
+    ByteRangeGenerator() {
+        super(Byte.class, byte.class.getName(), Byte.MIN_VALUE, Byte.MAX_VALUE);
+    }
+
+    @Override
+    protected Byte convert(final Number value) {
+        return value.byteValue();
+    }
+}
