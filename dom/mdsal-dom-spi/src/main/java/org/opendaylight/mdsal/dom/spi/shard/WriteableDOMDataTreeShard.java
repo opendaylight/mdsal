8e1785bc9a4c410bc3d40ec8780021ccd5792f34
Tomas Cere <tcere@cisco.com>
2016-12-12 13:11:46 +0100

    Add shard DTO classes to spi
    These are reused in cds shard so amek them reusable.
    
    Change-Id: I4885fd862291642c702a1bf561a920bf8459016c
    Signed-off-by: Tomas Cere <tcere@cisco.com>

--- dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/shard/WriteableDOMDataTreeShard.java
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/shard/WriteableDOMDataTreeShard.java
@@ -6,14 +6,23 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.mdsal.dom.store.inmemory;
+package org.opendaylight.mdsal.dom.spi.shard;
 
+import com.google.common.annotations.Beta;
 import java.util.Collection;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeShard;
-import org.opendaylight.mdsal.dom.spi.shard.DOMDataTreeShardProducer;
 
+/**
+ * Indicates that a shard is writable via the provided {@link #createProducer(Collection) createProducer} method.
+ */
+@Beta
 public interface WriteableDOMDataTreeShard extends DOMDataTreeShard {
 
+    /**
+     * Create a producer that has the ability to write into the provided subtrees.
+     * @param paths Subtrees that the caller wants to write into.
+     * @return Producer.
+     */
     DOMDataTreeShardProducer createProducer(Collection<DOMDataTreeIdentifier> paths);
 }
