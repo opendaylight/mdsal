fddf0176c34d99c04b56ac6dfcd1b6685d390300
Martin Ciglan <mciglan@cisco.com>
2017-01-17 13:11:00 +0100

    Bug 1459-1 - Re-organize mdsal-binding2-spec
    - new artifact mdsal-binding2-util (starts with Binding2Mapping)
    - new 'runtime' package for run-time support: serializer, etc..
    - various replacements done based on previous changes
    
    Change-Id: I4525d2c3f706ce1e77051711c57a3ca55ecc8350
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ binding2/mdsal-binding2-spec/src/main/java/org/opendaylight/mdsal/binding2/spec/runtime/Binding2Serializer.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding2.spec.runtime;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * The concept of a serializer in Binding part, which produces an object from some input.
+ *
+ * @param <P> Product type
+ * @param <I> Input type
+ */
+@Beta
+public interface Binding2Serializer<P, I> {
+
+    /**
+     * Produces an object based on input.
+     *
+     * @param input Input object
+     * @return Product derived from input
+     */
+    P serialize(I input);
+}
