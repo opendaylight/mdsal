767a42e6d5bc9aba919c7c925b16d9fc3b1f842d
Jakub Toth <jatoth@cisco.com>
2017-01-18 01:27:23 +0100

    Bug 7566 - 6 - Map model module for binding2
        *create ietf submodules in model-binding2 with binding2 parent
    
    Change-Id: Ia28191c7047ad014989f8f32573b3acf4e58e05f
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ model-binding2/ietf/ietf-type-util/src/test/java/org/opendaylight/mdsal/model/binding/javav2/ietf/util/MacUtil.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.model.binding.javav2.ietf.util;
+
+import org.opendaylight.mdsal.model.binding.javav2.ietf.util.AbstractIetfYangUtil;
+
+final class MacUtil extends AbstractIetfYangUtil<MacClass> {
+    MacUtil() {
+        super(MacClass.class);
+    }
+
+    @Override
+    protected String getValue(final MacClass macAddress) {
+        return macAddress.getValue();
+    }
+}
