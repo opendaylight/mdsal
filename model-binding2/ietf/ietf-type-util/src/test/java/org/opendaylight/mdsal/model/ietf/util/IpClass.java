2de215d7b417eb1944ab3090de440650c298c509
Jakub Toth <jatoth@cisco.com>
2017-01-18 01:27:23 +0100

    Bug 7566 - 6 - Map model module for binding2
        *create ietf submodules in model-binding2 with binding2 parent
    
    Change-Id: Ia28191c7047ad014989f8f32573b3acf4e58e05f
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ model-binding2/ietf/ietf-type-util/src/test/java/org/opendaylight/mdsal/model/ietf/util/IpClass.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.model.ietf.util;
+
+import com.google.common.base.Preconditions;
+
+public class IpClass {
+    private final String _value;
+
+    public IpClass(final String value) {
+        this._value = Preconditions.checkNotNull(value);
+    }
+
+    public IpClass(final IpClass template) {
+        this._value = template._value;
+    }
+
+    String getValue() {
+        return this._value;
+    }
+}
