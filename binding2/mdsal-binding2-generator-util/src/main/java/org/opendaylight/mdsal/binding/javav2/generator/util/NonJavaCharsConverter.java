d35fe62c7215a841a23ee1298197e893321b5c49
Jakub Toth <jatoth@cisco.com>
2017-02-17 16:21:34 +0100

    Normalizing package names according to https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html
    Added parsing of special case with dash in identifier with tests
    and documetationf for.
    
    Added parsing of reserved keywords for identifiers with tests and
    documentation for.
    
    Change-Id: I7d499b3e96270a8e06e720ef7b63e08fd0362094
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- binding2/mdsal-binding2-generator-util/src/main/java/org/opendaylight/mdsal/binding/javav2/generator/util/NonJavaCharsConverter.java
+++ binding2/mdsal-binding2-generator-util/src/main/java/org/opendaylight/mdsal/binding/javav2/generator/util/NonJavaCharsConverter.java
@@ -11,6 +11,7 @@ import com.google.common.annotations.Beta;
 import java.util.List;
 import org.opendaylight.mdsal.binding.javav2.model.api.Enumeration;
 import org.opendaylight.mdsal.binding.javav2.model.api.Enumeration.Pair;
+import org.opendaylight.mdsal.binding.javav2.util.BindingMapping;
 
 /**
  * This util class converts every non-java char in identifier to java char by its unicode name
@@ -33,6 +34,8 @@ import org.opendaylight.mdsal.binding.javav2.model.api.Enumeration.Pair;
  * <li>\example - ReverseSolidusExample</li>
  * <li>1example - DigitOneExample</li>
  * <li>example1 - Example1</li>
+ * <li>int - IntReservedKeyword</li>
+ * <li>con - ConReservedKeyword</li>
  * </ul>
  * </li>
  * </ul>
@@ -50,18 +53,20 @@ import org.opendaylight.mdsal.binding.javav2.model.api.Enumeration.Pair;
  * <li>\example - REVERSE_SOLIDUS_EXAMPLE</li>
  * <li>1example - DIGIT_ONE_EXAMPLE</li>
  * <li>example1 - EXAMPLE1</li>
+ * <li>int - INT_RESERVED_KEYWORD</li>
+ * <li>con - CON_RESERVED_KEYWORD</li>
  * </ul>
  * </li>
  * </ul>
  * </li>
- * <li>method, variable
+ * <li>method, variable</li>
  * <li>
  * <li>
  * <ul>
  * <li>without special separator</li>
  * <li>the first character of identifier is converting to lower case</li>
  * <li>any other first character of identifier part mapped by non-Java char name from unicode and
- * char in identifier behind non-java char name are converting to upper case
+ * char in identifier behind non-java char name are converting to upper case</li>
  * <li>examples:</li>
  * <li>
  * <ul>
@@ -70,18 +75,86 @@ import org.opendaylight.mdsal.binding.javav2.model.api.Enumeration.Pair;
  * <li>\example - reverseSolidusExample</li>
  * <li>1example - digitOneExample</li>
  * <li>example1 - example1</li>
+ * <li>int - intReservedKeyword</li>
+ * <li>con - conReservedKeyword</li>
+ * </ul>
+ * </li>
+ * </ul>
+ * </li>
+ * <li>package - part of package name
+ * (<a href="https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html"> Naming a
+ * package</a>)</li>
+ * <li>
+ * <li>
+ * <ul>
+ * <li>without special separator for part of package name</li>
+ * <li>identifier is converting to lower case</li>
+ * <li>if identifier contains a reserved Java or Windows keyword, such as 'int' the suggested
+ * convention is to add an underscore to identifier</li>
+ * <li>dash is parsed as underscore according to
+ * <a href="https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html"> Naming a
+ * package</a></li>
+ * <li>examples:</li>
+ * <li>
+ * <ul>
+ * <li>example* - exampleasterisk</li>
+ * <li>example*example - exampleaserisksexample</li>
+ * <li>\example - reversesolidusexample</li>
+ * <li>1example - digitoneexample</li>
+ * <li>example1 - example1</li>
+ * <li>int - int_</li>
+ * <li>con - con_</li>
+ * <li>foo-cont - foo_cont</li>
  * </ul>
  * </li>
  * </ul>
  * </li>
  * </ul>
  *
+ * There is special case in CLASS, INTERFACE, ENUM, ENUM VALUE, CONSTANT, METHOD and VARIABLE if
+ * identifier contains single dash - then the converter ignores the single dash in the way of the
+ * non-java chars. In other way, if dash is the first or the last char in the identifier or there is
+ * more dashes in a row in the identifier, then these dashes are converted as non-java chars.
+ * Example:
+ * <ul>
+ * <li>class, enum, interface</li>
+ * <li>
+ * <ul>
+ * <li>foo-cont - FooCont</li>
+ * <li>foo--cont - FooHyphenMinusHyphenMinusCont</li>
+ * <li>-foo - HyphenMinusFoo</li>
+ * <li>foo- - FooHyphenMinus</li>
+ * </ul>
+ * </li>
+ * <li>enum value, constant
+ * <li>
+ * <ul>
+ * <li>foo-cont - FOO_CONT</li>
+ * <li>foo--cont - FOO_HYPHEN_MINUS_HYPHEN_MINUS_CONT</li>
+ * <li>-foo - HYPHEN_MINUS_FOO</li>
+ * <li>foo- - FOO_HYPHEN_MINUS</li>
+ * </ul>
+ * </li>
+ * <li>method, variable</li>
+ * <li>
+ * <ul>
+ * <li>foo-cont - fooCont</li>
+ * <li>foo--cont - fooHyphenMinusHyphenMinusCont</li>
+ * <li>-foo - hyphenMinusFoo</li>
+ * <li>foo- - fooHyphenMinus</li>
+ * </ul>
+ * </li>
+ * </ul>
  */
 @Beta
 public final class NonJavaCharsConverter {
 
-    private final static int FIRST_CHAR = 0;
-    private final static int FIRST_INDEX = 1;
+    private static final int FIRST_CHAR = 0;
+    private static final int FIRST_INDEX = 1;
+    private static final char UNDERSCORE = '_';
+    private static final char DASH = '-';
+    private static final String EMPTY_STRING = "";
+    private static final String RESERVED_KEYWORD = "reserved_keyword";
 
     private NonJavaCharsConverter() {
         throw new UnsupportedOperationException("Util class");
@@ -117,19 +190,40 @@ public final class NonJavaCharsConverter {
         return convertIdentifierEnumValue(name, name, values, FIRST_INDEX);
     }
 
-    private static String convertIdentifierEnumValue(final String name, final String origName, final List<Pair> values,
-            final int rank) {
-        String newName = name;
-        for (final Pair pair : values) {
-            if (pair.getName().toLowerCase().equals(name.toLowerCase())
-                    || pair.getMappedName().toLowerCase().equals(name.toLowerCase())) {
-                int actualRank = rank;
-                final StringBuilder actualNameBuilder = new StringBuilder(origName).append('_').append(actualRank);
-                newName = convertIdentifierEnumValue(actualNameBuilder.toString(), origName, values,
-                        ++actualRank);
+    /**
+     * Normalizing part of package name by non java chars.
+     *
+     * @param packageNamePart
+     *            - part of package name
+     * @return normalized name
+     */
+    public static String normalizePackageName(final String packageNamePart) {
+        StringBuilder normalizedNameSb = new StringBuilder(packageNamePart);
+        if (BindingMapping.JAVA_RESERVED_WORDS.contains(packageNamePart)
+                || BindingMapping.WINDOWS_RESERVED_WORDS.contains(packageNamePart.toUpperCase())) {
+            return normalizedNameSb.append(UNDERSCORE).toString();
+        }
+        String normalizedPackageNamePart = packageNamePart;
+        if (packageNamePart.contains(String.valueOf(DASH))) {
+            normalizedPackageNamePart = packageNamePart.replaceAll(String.valueOf(DASH), String.valueOf(UNDERSCORE));
+        }
+        normalizedNameSb = new StringBuilder();
+        StringBuilder innserSb = new StringBuilder();
+        for (int i = 0; i < normalizedPackageNamePart.length(); i++) {
+            if (normalizedPackageNamePart.charAt(i) == UNDERSCORE) {
+                if (!innserSb.toString().isEmpty()) {
+                    normalizedNameSb.append(convertIdentifier(innserSb.toString(), JavaIdentifier.PACKAGE));
+                    innserSb = new StringBuilder();
+                }
+                normalizedNameSb.append(UNDERSCORE);
+            } else {
+                innserSb.append(normalizedPackageNamePart.charAt(i));
             }
         }
-        return convertIdentifier(newName, JavaIdentifier.ENUM_VALUE);
+        if (!innserSb.toString().isEmpty()) {
+            normalizedNameSb.append(convertIdentifier(innserSb.toString(), JavaIdentifier.PACKAGE));
+        }
+        return normalizedNameSb.toString();
     }
 
     /**
@@ -147,6 +241,15 @@ public final class NonJavaCharsConverter {
     public static String convertIdentifier(final String identifier, final JavaIdentifier javaIdentifier) {
         final StringBuilder sb = new StringBuilder();
 
+        // if identifier isn't PACKAGE type then check it by reserved keywords
+        if(javaIdentifier != JavaIdentifier.PACKAGE) {
+            if (BindingMapping.JAVA_RESERVED_WORDS.contains(identifier)
+                    || BindingMapping.WINDOWS_RESERVED_WORDS.contains(identifier.toUpperCase())) {
+                return fixCasesByJavaType(sb.append(identifier).append(UNDERSCORE).append(RESERVED_KEYWORD).toString(),
+                        javaIdentifier);
+            }
+        }
+
         // check and convert first char in identifier if there is non-java char
         final char firstChar = identifier.charAt(FIRST_CHAR);
         if (!Character.isJavaIdentifierStart(firstChar)) {
@@ -158,6 +261,14 @@ public final class NonJavaCharsConverter {
         // check and convert other chars in identifier, if there is non-java char
         for (int i = 1; i < identifier.length(); i++) {
             final char actualChar = identifier.charAt(i);
+            // ignore single dash as non java char - if there is more dashes in a row or dash is as
+            // the last char in identifier then parse these dashes as non java chars
+            if ((actualChar == '-') && existNext(identifier, i)) {
+                if ((identifier.charAt(i - 1) != DASH) && (identifier.charAt(i + 1) != DASH)) {
+                    sb.append(UNDERSCORE);
+                    continue;
+                }
+            }
             if (!Character.isJavaIdentifierPart(actualChar)) {
                 // prepare actual string of sb for checking if underscore exist on position of the
                 // last char
@@ -193,6 +304,8 @@ public final class NonJavaCharsConverter {
             case METHOD:
             case VARIABLE:
                 return fixCases(convertedIdentifier);
+            case PACKAGE:
+                return convertedIdentifier.replaceAll(String.valueOf(UNDERSCORE), EMPTY_STRING);
             default:
                 throw new IllegalArgumentException("Unknown java type of identifier : " + javaIdentifier.toString());
         }
@@ -207,9 +320,9 @@ public final class NonJavaCharsConverter {
      */
     private static String fixCases(final String convertedIdentifier) {
         final StringBuilder sb = new StringBuilder();
-        if (convertedIdentifier.contains("_")) {
+        if (convertedIdentifier.contains(String.valueOf(UNDERSCORE))) {
             boolean isFirst = true;
-            for (final String part : convertedIdentifier.split("_")) {
+            for (final String part : convertedIdentifier.split(String.valueOf(UNDERSCORE))) {
                 if (isFirst) {
                     isFirst = false;
                     sb.append(part);
@@ -248,6 +361,9 @@ public final class NonJavaCharsConverter {
      */
     private static String convertFirst(final char c, final boolean existNext) {
         String name = Character.getName(c);
+        if (name.contains(String.valueOf(DASH))) {
+            name = name.replaceAll(String.valueOf(DASH), String.valueOf(UNDERSCORE));
+        }
         name = existNext ? (name + "_") : name;
         return name.contains(" ") ? name.replaceAll(" ", "_") : name;
     }
@@ -276,5 +392,20 @@ public final class NonJavaCharsConverter {
      */
     private static String capitalize(final String identifier) {
         return identifier.substring(FIRST_CHAR, FIRST_CHAR + 1).toUpperCase() + identifier.substring(1);
+    }
+
+    private static String convertIdentifierEnumValue(final String name, final String origName, final List<Pair> values,
+            final int rank) {
+        String newName = name;
+        for (final Pair pair : values) {
+            if (pair.getName().toLowerCase().equals(name.toLowerCase())
+                    || pair.getMappedName().toLowerCase().equals(name.toLowerCase())) {
+                int actualRank = rank;
+                final StringBuilder actualNameBuilder =
+                        new StringBuilder(origName).append(UNDERSCORE).append(actualRank);
+                newName = convertIdentifierEnumValue(actualNameBuilder.toString(), origName, values, ++actualRank);
+            }
+        }
+        return convertIdentifier(newName, JavaIdentifier.ENUM_VALUE);
     }
 }
