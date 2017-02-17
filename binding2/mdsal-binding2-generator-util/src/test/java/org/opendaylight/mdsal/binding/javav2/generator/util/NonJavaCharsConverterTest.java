8c5bcec16617c9790f21596f0f554c4abdda117d
Jakub Toth <jatoth@cisco.com>
2017-02-17 16:21:34 +0100

    Normalizing package names according to https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html
    Added parsing of special case with dash in identifier with tests
    and documetationf for.
    
    Added parsing of reserved keywords for identifiers with tests and
    documentation for.
    
    Change-Id: I7d499b3e96270a8e06e720ef7b63e08fd0362094
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- binding2/mdsal-binding2-generator-util/src/test/java/org/opendaylight/mdsal/binding/javav2/generator/util/NonJavaCharsConverterTest.java
+++ binding2/mdsal-binding2-generator-util/src/test/java/org/opendaylight/mdsal/binding/javav2/generator/util/NonJavaCharsConverterTest.java
@@ -12,8 +12,85 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.opendaylight.mdsal.binding.javav2.util.BindingMapping;
 
 public class NonJavaCharsConverterTest {
+
+    /**
+     * Is the same test for enum and interface identifiers
+     */
+    @Test
+    public void sameClassNamesTest() {
+        final String normalizeIdentifier1 = NonJavaCharsConverter.normalizeClassIdentifier("org.example", "Foo");
+        final String normalizeIdentifier2 = NonJavaCharsConverter.normalizeClassIdentifier("org.example", "fOo");
+        final String normalizeIdentifier3 = NonJavaCharsConverter.normalizeClassIdentifier("org.example", "foo");
+        assertEquals(normalizeIdentifier1, "Foo");
+        assertEquals(normalizeIdentifier2, "Foo1");
+        assertEquals(normalizeIdentifier3, "Foo2");
+    }
+
+    /**
+     * Is the same test for class and interface identifiers
+     */
+    @Test
+    public void identifierResrvedKeyWordsEnum() throws Exception {
+        for (final String reservedKeyword : BindingMapping.JAVA_RESERVED_WORDS) {
+            testIdentifierReservedKeywordEnum(reservedKeyword);
+        }
+        for (final String reservedKeyword : BindingMapping.WINDOWS_RESERVED_WORDS) {
+            testIdentifierReservedKeywordEnum(reservedKeyword);
+        }
+        testIdentifierReservedKeywordEnum("iNt");
+        testIdentifierReservedKeywordEnum("cOn");
+    }
+
+    private void testIdentifierReservedKeywordEnum(final String reservedKeyword) {
+        final StringBuilder expected =
+                new StringBuilder(reservedKeyword.substring(0, 1).toUpperCase())
+                        .append(reservedKeyword.substring(1).toLowerCase())
+                        .append("ReservedKeyword");
+        assertTest(reservedKeyword, expected.toString(), JavaIdentifier.ENUM);
+    }
+
+    /**
+     * Is the same test for constants
+     */
+    @Test
+    public void identifierResrvedKeyWordsEnumValue() throws Exception {
+        for (final String reservedKeyword : BindingMapping.JAVA_RESERVED_WORDS) {
+            testIdentifierReservedKeywordEnumValue(reservedKeyword);
+        }
+        for (final String reservedKeyword : BindingMapping.WINDOWS_RESERVED_WORDS) {
+            testIdentifierReservedKeywordEnumValue(reservedKeyword);
+        }
+        testIdentifierReservedKeywordEnumValue("iNt");
+        testIdentifierReservedKeywordEnumValue("cOn");
+    }
+
+    private void testIdentifierReservedKeywordEnumValue(final String reservedKeyword) {
+        final StringBuilder expected = new StringBuilder(reservedKeyword.toUpperCase()).append("_RESERVED_KEYWORD");
+        assertTest(reservedKeyword, expected.toString(), JavaIdentifier.ENUM_VALUE);
+    }
+
+    /**
+     * Is the same test for variables
+     */
+    @Test
+    public void identifierResrvedKeyWordsMethod() throws Exception {
+        for (final String reservedKeyword : BindingMapping.JAVA_RESERVED_WORDS) {
+            testIdentifierReservedKeywordMethod(reservedKeyword);
+        }
+        for (final String reservedKeyword : BindingMapping.WINDOWS_RESERVED_WORDS) {
+            testIdentifierReservedKeywordMethod(reservedKeyword);
+        }
+        testIdentifierReservedKeywordMethod("iNt");
+        testIdentifierReservedKeywordMethod("cOn");
+    }
+
+    private void testIdentifierReservedKeywordMethod(final String reservedKeyword) {
+        final StringBuilder expected = new StringBuilder(reservedKeyword.toLowerCase()).append("ReservedKeyword");
+        assertTest(reservedKeyword, expected.toString(), JavaIdentifier.METHOD);
+    }
 
     @Test
     public void acceptableCharsEnumTest() throws Exception {
@@ -44,6 +121,18 @@ public class NonJavaCharsConverterTest {
         assertTest("Acc1", "Acc1", JavaIdentifier.ENUM);
         assertTest("1", "DigitOne", JavaIdentifier.ENUM);
         assertTest("%", "PercentSign", JavaIdentifier.ENUM);
+        assertTest("foo-bar", "FooBar", JavaIdentifier.ENUM);
+        assertTest("foo--bar", "FooHyphenMinusHyphenMinusBar", JavaIdentifier.ENUM);
+        assertTest("-foo", "HyphenMinusFoo", JavaIdentifier.ENUM);
+        assertTest("--foo", "HyphenMinusHyphenMinusFoo", JavaIdentifier.ENUM);
+        assertTest("foo-", "FooHyphenMinus", JavaIdentifier.ENUM);
+        assertTest("foo--", "FooHyphenMinusHyphenMinus", JavaIdentifier.ENUM);
+        assertTest("-foo-", "HyphenMinusFooHyphenMinus", JavaIdentifier.ENUM);
+        assertTest("-foo-bar-", "HyphenMinusFooBarHyphenMinus", JavaIdentifier.ENUM);
+        assertTest("-foo--bar-", "HyphenMinusFooHyphenMinusHyphenMinusBarHyphenMinus", JavaIdentifier.ENUM);
+        assertTest("foo.", "FooFullStop", JavaIdentifier.ENUM);
+        assertTest(".foo", "FullStopFoo", JavaIdentifier.ENUM);
+        assertTest("bar.foo", "BarFullStopFoo", JavaIdentifier.ENUM);
     }
 
     @Test
@@ -51,6 +140,9 @@ public class NonJavaCharsConverterTest {
         assertTest("ACCEPTABLE", "ACCEPTABLE", JavaIdentifier.ENUM_VALUE);
     }
 
+    /**
+     * Is the same test for constants
+     */
     @Test
     public void nonAcceptableCharsEnumValueTest() throws Exception {
         assertTest("acceptable", "ACCEPTABLE", JavaIdentifier.ENUM_VALUE);
@@ -73,6 +165,19 @@ public class NonJavaCharsConverterTest {
         assertTest("Acc1", "ACC1", JavaIdentifier.ENUM_VALUE);
         assertTest("1", "DIGIT_ONE", JavaIdentifier.ENUM_VALUE);
         assertTest("%", "PERCENT_SIGN", JavaIdentifier.ENUM_VALUE);
+        assertTest("foo-bar", "FOO_BAR", JavaIdentifier.ENUM_VALUE);
+        assertTest("foo--bar", "FOO_HYPHEN_MINUS_HYPHEN_MINUS_BAR", JavaIdentifier.ENUM_VALUE);
+        assertTest("-foo", "HYPHEN_MINUS_FOO", JavaIdentifier.ENUM_VALUE);
+        assertTest("--foo", "HYPHEN_MINUS_HYPHEN_MINUS_FOO", JavaIdentifier.ENUM_VALUE);
+        assertTest("foo-", "FOO_HYPHEN_MINUS", JavaIdentifier.ENUM_VALUE);
+        assertTest("foo--", "FOO_HYPHEN_MINUS_HYPHEN_MINUS", JavaIdentifier.ENUM_VALUE);
+        assertTest("-foo-", "HYPHEN_MINUS_FOO_HYPHEN_MINUS", JavaIdentifier.ENUM_VALUE);
+        assertTest("-foo-bar-", "HYPHEN_MINUS_FOO_BAR_HYPHEN_MINUS", JavaIdentifier.ENUM_VALUE);
+        assertTest("-foo--bar-", "HYPHEN_MINUS_FOO_HYPHEN_MINUS_HYPHEN_MINUS_BAR_HYPHEN_MINUS",
+                JavaIdentifier.ENUM_VALUE);
+        assertTest("foo.", "FOO_FULL_STOP", JavaIdentifier.ENUM_VALUE);
+        assertTest(".foo", "FULL_STOP_FOO", JavaIdentifier.ENUM_VALUE);
+        assertTest("bar.foo", "BAR_FULL_STOP_FOO", JavaIdentifier.ENUM_VALUE);
     }
 
     @Test
@@ -102,6 +207,90 @@ public class NonJavaCharsConverterTest {
         assertTest("Acc1", "acc1", JavaIdentifier.METHOD);
         assertTest("1", "digitOne", JavaIdentifier.METHOD);
         assertTest("%", "percentSign", JavaIdentifier.METHOD);
+        assertTest("foo-bar", "fooBar", JavaIdentifier.METHOD);
+        assertTest("foo--bar", "fooHyphenMinusHyphenMinusBar", JavaIdentifier.METHOD);
+        assertTest("-foo", "hyphenMinusFoo", JavaIdentifier.METHOD);
+        assertTest("--foo", "hyphenMinusHyphenMinusFoo", JavaIdentifier.METHOD);
+        assertTest("foo-", "fooHyphenMinus", JavaIdentifier.METHOD);
+        assertTest("foo--", "fooHyphenMinusHyphenMinus", JavaIdentifier.METHOD);
+        assertTest("-foo-", "hyphenMinusFooHyphenMinus", JavaIdentifier.METHOD);
+        assertTest("-foo-bar-", "hyphenMinusFooBarHyphenMinus", JavaIdentifier.METHOD);
+        assertTest("-foo--bar-", "hyphenMinusFooHyphenMinusHyphenMinusBarHyphenMinus", JavaIdentifier.METHOD);
+        assertTest("foo.", "fooFullStop", JavaIdentifier.METHOD);
+        assertTest(".foo", "fullStopFoo", JavaIdentifier.METHOD);
+        assertTest("foo.bar", "fooFullStopBar", JavaIdentifier.METHOD);
+    }
+
+
+    @Test
+    public void packageResrvedKeyWordsTest() {
+        for (final String reservedKeyword : BindingMapping.JAVA_RESERVED_WORDS) {
+            testPackageReservedKeyword(reservedKeyword);
+        }
+        for (final String reservedKeyword : BindingMapping.WINDOWS_RESERVED_WORDS) {
+            testPackageReservedKeyword(reservedKeyword);
+        }
+    }
+
+    private void testPackageReservedKeyword(final String reservedKeyword) {
+        final String packageNameNormalizer = NonJavaCharsConverter.normalizePackageNamePart(reservedKeyword);
+        final StringBuilder expected = new StringBuilder(reservedKeyword).append('_');
+        assertEquals(expected.toString().toLowerCase(), packageNameNormalizer);
+    }
+
+    @Test
+    public void digitAtStartTest() {
+        for (int i = 0; i < 10; i++) {
+            final String str_i = String.valueOf(i);
+            final String packageNameNormalizer = NonJavaCharsConverter.normalizePackageNamePart(str_i);
+            final String expected = Character.getName(str_i.charAt(0)).replaceAll(" ", "").toLowerCase();
+            assertEquals(expected.toString(), packageNameNormalizer);
+        }
+    }
+
+    @Test
+    public void normalizePackageNameDashTest() {
+        dashTest("foo-bar", "foo_bar");
+        dashTest("foo--bar", "foo__bar");
+        dashTest("-foo", "_foo");
+        dashTest("--foo", "__foo");
+        dashTest("foo-", "foo_");
+        dashTest("foo--", "foo__");
+        dashTest("-foo-bar", "_foo_bar");
+        dashTest("foo-bar-", "foo_bar_");
+        dashTest("-foo-bar-", "_foo_bar_");
+        dashTest("-foo--bar-", "_foo__bar_");
+    }
+
+    private void dashTest(final String tested, final String expected) {
+        final String actual = NonJavaCharsConverter.normalizePackageNamePart(tested);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void normalizePackageNameTest() {
+        normalizePackageNameTest("*foo", "asteriskfoo");
+        normalizePackageNameTest("/foo", "solidusfoo");
+        normalizePackageNameTest("\\foo", "reversesolidusfoo");
+        normalizePackageNameTest(":foo", "colonfoo");
+
+        normalizePackageNameTest("f*oo", "fasteriskoo");
+        normalizePackageNameTest("f/oo", "fsolidusoo");
+        normalizePackageNameTest("f\\oo", "freversesolidusoo");
+        normalizePackageNameTest("f:oo", "fcolonoo");
+
+        normalizePackageNameTest("foo*", "fooasterisk");
+        normalizePackageNameTest("foo/", "foosolidus");
+        normalizePackageNameTest("foo\\", "fooreversesolidus");
+        normalizePackageNameTest("foo:", "foocolon");
+
+        normalizePackageNameTest("_foo_", "_foo_");
+        normalizePackageNameTest("f_oo", "f_oo");
+    }
+
+    private void normalizePackageNameTest(final String tested, final Object expected) {
+        final String packageNameNormalizer = NonJavaCharsConverter.normalizePackageNamePart(tested);
+        assertEquals(expected, packageNameNormalizer);
     }
 
     private void assertTest(final String testedIdentifier, final String acceptable,
@@ -111,5 +300,61 @@ public class NonJavaCharsConverterTest {
         assertNotNull(convertedIdentifier);
         assertTrue(!convertedIdentifier.isEmpty());
         assertEquals(acceptable, convertedIdentifier);
+    }
+
+    @Test
+    public void realPackageNameExampleTest() {
+        String tested = "org.opendaylight.example.test.rev000000.data.foo";
+        String expected = "org.opendaylight.example.test.rev000000.data.foo";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.foo*";
+        expected = "org.opendaylight.example.test.rev000000.data.fooasterisk";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.foo*bar";
+        expected = "org.opendaylight.example.test.rev000000.data.fooasteriskbar";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.foo.bar";
+        expected = "org.opendaylight.example.test.rev000000.data.foo.bar";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.foo-bar";
+        expected = "org.opendaylight.example.test.rev000000.data.foo_bar";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.foo_";
+        expected = "org.opendaylight.example.test.rev000000.data.foo_";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data._foo";
+        expected = "org.opendaylight.example.test.rev000000.data._foo";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.foo*.*";
+        expected = "org.opendaylight.example.test.rev000000.data.fooasterisk.asterisk";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.int";
+        expected = "org.opendaylight.example.test.rev000000.data.int_";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.iNt";
+        expected = "org.opendaylight.example.test.rev000000.data.int_";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.con";
+        expected = "org.opendaylight.example.test.rev000000.data.con_";
+        testRealPackageNameExample(tested, expected);
+
+        tested = "org.opendaylight.example.test.rev000000.data.CON";
+        expected = "org.opendaylight.example.test.rev000000.data.con_";
+        testRealPackageNameExample(tested, expected);
+    }
+
+    private void testRealPackageNameExample(final String tested, final String expected) {
+        final String actual = NonJavaCharsConverter.normalizeFullPackageName(tested);
+        assertEquals(expected, actual);
     }
 }
