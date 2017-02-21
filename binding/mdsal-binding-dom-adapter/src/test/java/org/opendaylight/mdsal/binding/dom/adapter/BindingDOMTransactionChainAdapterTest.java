15e5757677345f22f37a479214a35fd57bcbbc48
Jakub Toth <jatoth@cisco.com>
2017-02-21 12:43:02 +0100

    Bug 6859 #3 Binding generator v1 refactoring
    Based on transfer of Binding generator v1 from
    Yangtools project to MDSAL in past, we need to finalize
    this process by refactoring package naming:
    
    org.opendaylight.yangtools       -> org.mdsal.binding
    org.opendaylight.yangtools.sal -> org.mdsal.binding
    
    This refactoring is done in subsequent steps,
    covering all necessary modules. Based on experience,
    this should also help users to address possible Binding generator v1
    issues in MDSAL project, not in Yangtools.
    
    - mdsal-binding-generator-impl module refactoring
    
    Change-Id: If3a7615d3c2870d02eae8767ca44bcb0981aaa99
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMTransactionChainAdapterTest.java
+++ binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMTransactionChainAdapterTest.java
@@ -20,13 +20,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.generator.impl.GeneratedClassLoadingStrategy;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeReadTransaction;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
 import org.opendaylight.mdsal.dom.api.DOMService;
 import org.opendaylight.mdsal.dom.api.DOMTransactionChain;
 import org.opendaylight.yangtools.binding.data.codec.impl.BindingNormalizedNodeCodecRegistry;
-import org.opendaylight.yangtools.sal.binding.generator.impl.GeneratedClassLoadingStrategy;
 
 public class BindingDOMTransactionChainAdapterTest {
 
