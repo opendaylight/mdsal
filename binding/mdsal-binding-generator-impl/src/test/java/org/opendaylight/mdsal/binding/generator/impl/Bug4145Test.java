c8256038c26f94128a259a795b26e7ff46bebb53
Jakub Toth <jatoth@cisco.com>
2017-02-21 12:43:02 +0100

    Bug 6859 #3 Binding generator v1 refactoring
    Based on transfer of Binding generator v1 from
    Yangtools project to MDSAL in past, we need to finalize
    this process by refactoring package naming:
    
    org.opendaylight.yangtools       -> org.mdsal.binding
    org.opendaylight.yangtools.sal -> org.mdsal.binding
    
    This refactoring is done in subsequent steps,
    covering all necessary modules. Based on experience,
    this should also help users to address possible Binding generator v1
    issues in MDSAL project, not in Yangtools.
    
    - mdsal-binding-generator-impl module refactoring
    
    Change-Id: If3a7615d3c2870d02eae8767ca44bcb0981aaa99
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Bug4145Test.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.generator.impl.BindingGeneratorImpl;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Bug4145Test {
+    @Test
+    public void bug4145Test() throws URISyntaxException, IOException, YangSyntaxErrorException, ReactorException {
+        File resourceFile = new File(getClass().getResource(
+                "/bug-4145/foo.yang").toURI());
+
+        SchemaContext context = YangParserTestUtils.parseYangSources(resourceFile);
+
+        List<Type> generateTypes = new BindingGeneratorImpl(false)
+                .generateTypes(context);
+        assertNotNull(generateTypes);
+        assertTrue(generateTypes.size() > 0);
+    }
+}
