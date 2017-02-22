368e765ed96047d26db5edeb3d4fa350e9df8476
Jakub Toth <jatoth@cisco.com>
2017-02-22 10:57:28 +0100

    Bug 6859 - PART 5 - Cleanup package names to remove 'yangtools'
      * refactoring maven-sal-api-gen-plugin
    
    Change-Id: I17bd25507894b8b5a42579ee317f5d5fd81f3f20
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- binding/maven-sal-api-gen-plugin/src/test/java/org/opendaylight/mdsal/binding/yang/unified/doc/generator/maven/DocGenTest.java
+++ binding/maven-sal-api-gen-plugin/src/test/java/org/opendaylight/mdsal/binding/yang/unified/doc/generator/maven/DocGenTest.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.unified.doc.generator.maven;
+package org.opendaylight.mdsal.binding.yang.unified.doc.generator.maven;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -21,6 +21,7 @@ import java.util.Set;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.opendaylight.mdsal.binding.yang.unified.doc.generator.maven.DocumentationGeneratorImpl;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
