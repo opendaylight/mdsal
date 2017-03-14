eeb938b6e0dfd54925757ebf4b1a445fd1a3c0c7
Jakub Toth <jatoth@cisco.com>
2017-03-14 11:10:29 +0100

    Bug 7759 - TEST - Getter of BA object fails to construct class instance
     * negative test - if value of data is other like type of data define by yang
    
    Change-Id: Ia6a9abd43e78384f84c867cf6a755cd07d6503c6
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/BindingToNormalizedNodeCodecTest.java
+++ binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/BindingToNormalizedNodeCodecTest.java
@@ -7,12 +7,16 @@
  */
 package org.opendaylight.mdsal.binding.dom.adapter;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 import javassist.ClassPool;
-import org.junit.Assert;
 import org.junit.Test;
 import org.opendaylight.mdsal.binding.generator.impl.GeneratedClassLoadingStrategy;
 import org.opendaylight.mdsal.binding.generator.util.BindingRuntimeContext;
@@ -26,8 +30,8 @@ import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
-import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
@@ -37,38 +41,25 @@ import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 public class BindingToNormalizedNodeCodecTest {
 
+    /**
+     * Positive test.
+     *
+     * <p>
+     * Test for yang with leaf of type int in container where data are created
+     * with int value (acceptable data).
+     *
+     * @throws Exception
+     *             - throw exception
+     */
     @Test
     public void fromNormalizedNodeTest() throws Exception {
-        final DataObjectSerializerGenerator serializerGenerator =
-                StreamWriterGenerator.create(JavassistUtils.forClassPool(ClassPool.getDefault()));
-        final BindingNormalizedNodeCodecRegistry codecRegistry =
-                new BindingNormalizedNodeCodecRegistry(serializerGenerator);
-        final GeneratedClassLoadingStrategy classLoadingStrategy =
-                GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy();
         final SchemaContext schemaCtx = YangParserTestUtils.parseYangSource("/test.yang");
-        final BindingRuntimeContext ctx = BindingRuntimeContext.create(classLoadingStrategy, schemaCtx);
-        codecRegistry.onBindingRuntimeContextUpdated(ctx);
-        final BindingToNormalizedNodeCodec codec =
-                new BindingToNormalizedNodeCodec(classLoadingStrategy, codecRegistry);
-
-        final DataSchemaNode dataChildByName =
-                schemaCtx.getDataChildByName(QName.create("urn:test", "2017-01-01", "cont"));
-        final DataSchemaNode leaf = ((ContainerSchemaNode) dataChildByName)
-                .getDataChildByName(QName.create("urn:test", "2017-01-01", "vlan-id"));
-
-        final DataContainerChild<?, ?> child = Builders.leafBuilder((LeafSchemaNode) leaf).withValue(2420).build();
-        final ContainerNode data =
-                Builders.containerBuilder((ContainerSchemaNode) dataChildByName).withChild(child).build();
-
-        final List<PathArgument> pathArgs = new ArrayList<>();
-        pathArgs.add(NodeIdentifier.create(QName.create("urn:test", "2017-01-01", "cont")));
-
-        final YangInstanceIdentifier path = YangInstanceIdentifier.create(pathArgs);
-        final Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode = codec.fromNormalizedNode(path, data);
+        final NormalizedNode<?, ?> data = prepareData(schemaCtx, 42);
+        final Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode = fromNormalizedNode(data, schemaCtx);
 
         final DataObject value = fromNormalizedNode.getValue();
-        Assert.assertNotNull(value);
-        Assert.assertEquals("Cont", value.getImplementedInterface().getSimpleName());
+        assertNotNull(value);
+        assertEquals("Cont", value.getImplementedInterface().getSimpleName());
         final Object[] objs = {};
         final Object invoked = value.getImplementedInterface().getDeclaredMethods()[0].invoke(value, objs);
         final Field declaredField = invoked.getClass().getDeclaredField("_id");
@@ -76,6 +67,70 @@ public class BindingToNormalizedNodeCodecTest {
         final Object id = declaredField.get(invoked);
         final Field val = id.getClass().getDeclaredField("_value");
         val.setAccessible(true);
-        Assert.assertEquals(2420, val.get(id));
+        assertEquals(42, val.get(id));
+    }
+
+    /**
+     * Negative test.
+     *
+     * <p>
+     * Test for yang with leaf of type int in container where data are created
+     * with String value (non acceptable data - should failed with
+     * {@link IllegalArgumentException})
+     *
+     * @throws Exception
+     *             - throw exception
+     */
+    @Test
+    public void fromNormalizedNodeWithAnotherInputDataTest() throws Exception {
+        final SchemaContext schemaCtx = YangParserTestUtils.parseYangSource("/test.yang");
+        final NormalizedNode<?, ?> data = prepareData(schemaCtx, "42");
+
+        final Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode = fromNormalizedNode(data, schemaCtx);
+        final DataObject value = fromNormalizedNode.getValue();
+        assertNotNull(value);
+        assertEquals("Cont", value.getImplementedInterface().getSimpleName());
+        final Object[] objs = {};
+        try {
+            value.getImplementedInterface().getDeclaredMethods()[0].invoke(value, objs);
+            fail();
+        } catch (final InvocationTargetException e) {
+            final Throwable cause = e.getCause();
+            assertNotNull(cause);
+            assertEquals(cause.getClass(), IllegalArgumentException.class);
+        }
+    }
+
+    private NormalizedNode<?, ?> prepareData(final SchemaContext schemaCtx, final Object value) {
+        final DataSchemaNode dataChildByName =
+                schemaCtx.getDataChildByName(QName.create("urn:test", "2017-01-01", "cont"));
+        final DataSchemaNode leaf = ((ContainerSchemaNode) dataChildByName)
+                .getDataChildByName(QName.create("urn:test", "2017-01-01", "vlan-id"));
+
+        final DataContainerChild<?, ?> child = Builders.leafBuilder((LeafSchemaNode) leaf).withValue(value).build();
+        final NormalizedNode<?, ?> data =
+                Builders.containerBuilder((ContainerSchemaNode) dataChildByName).withChild(child).build();
+        return data;
+    }
+
+    private Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode(final NormalizedNode<?, ?> data,
+            final SchemaContext schemaCtx) throws Exception {
+        final DataObjectSerializerGenerator serializerGenerator =
+                StreamWriterGenerator.create(JavassistUtils.forClassPool(ClassPool.getDefault()));
+        final BindingNormalizedNodeCodecRegistry codecRegistry =
+                new BindingNormalizedNodeCodecRegistry(serializerGenerator);
+        final GeneratedClassLoadingStrategy classLoadingStrategy =
+                GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy();
+        final BindingRuntimeContext ctx = BindingRuntimeContext.create(classLoadingStrategy, schemaCtx);
+        codecRegistry.onBindingRuntimeContextUpdated(ctx);
+        final BindingToNormalizedNodeCodec codec =
+                new BindingToNormalizedNodeCodec(classLoadingStrategy, codecRegistry);
+        final List<PathArgument> pathArgs = new ArrayList<>();
+        pathArgs.add(NodeIdentifier.create(QName.create("urn:test", "2017-01-01", "cont")));
+
+        final YangInstanceIdentifier path = YangInstanceIdentifier.create(pathArgs);
+        final Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode = codec.fromNormalizedNode(path, data);
+        codec.close();
+        return fromNormalizedNode;
     }
 }
