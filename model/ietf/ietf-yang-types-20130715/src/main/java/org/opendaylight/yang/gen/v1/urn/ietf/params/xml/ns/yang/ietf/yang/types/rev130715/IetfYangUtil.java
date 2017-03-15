71acd2bef425d211630c15eff74cce05deacf591
Michael Vorburger <vorburger@redhat.com>
2017-03-15 17:46:02 +0100

    IetfYangUtil's Optional<Uuid> newUuidIfValidPattern(String possibleUuid)
    see also Bug 7992 and Bug 7991 for possible future enhancements;
    
    originally motivated out of code in
    https://git.opendaylight.org/gerrit/#/c/53118/ for Bug 7913.
    
    Change-Id: I3b60e8c448e6befecc57e25462f8ade378ce48e7
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- model/ietf/ietf-yang-types-20130715/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/yang/types/rev130715/IetfYangUtil.java
+++ model/ietf/ietf-yang-types-20130715/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/yang/types/rev130715/IetfYangUtil.java
@@ -8,6 +8,9 @@
 package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715;
 
 import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import java.util.Optional;
+import java.util.regex.Pattern;
 import org.opendaylight.mdsal.model.ietf.util.AbstractIetfYangUtil;
 
 /**
@@ -15,7 +18,10 @@ import org.opendaylight.mdsal.model.ietf.util.AbstractIetfYangUtil;
  */
 @Beta
 public final class IetfYangUtil extends AbstractIetfYangUtil<MacAddress> {
+
     public static final IetfYangUtil INSTANCE = new IetfYangUtil();
+
+    private Pattern uuidPattern;
 
     private IetfYangUtil() {
         super(MacAddress.class);
@@ -25,4 +31,25 @@ public final class IetfYangUtil extends AbstractIetfYangUtil<MacAddress> {
     protected String getValue(final MacAddress macAddress) {
         return macAddress.getValue();
     }
+
+    // https://bugs.opendaylight.org/show_bug.cgi?id=7992 proposes to auto-gen. this
+    public Optional<Uuid> newUuidIfValidPattern(String possibleUuid) {
+        Preconditions.checkNotNull(possibleUuid, "possibleUuid == null");
+
+        // https://bugs.opendaylight.org/show_bug.cgi?id=7991 proposes how this could be avoided
+        if (uuidPattern == null) {
+            // Thread safe because it really doesn't matter even if we were to do this initialization more than once
+            if (Uuid.PATTERN_CONSTANTS.size() != 1) {
+                throw new IllegalStateException("Uuid.PATTERN_CONSTANTS.size() != 1");
+            }
+            uuidPattern = Pattern.compile(Uuid.PATTERN_CONSTANTS.get(0));
+        }
+
+        if (uuidPattern.matcher(possibleUuid).matches()) {
+            return Optional.of(new Uuid(possibleUuid));
+        } else {
+            return Optional.empty();
+        }
+    }
+
 }
