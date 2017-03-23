c996818416042fefe24425e76860edd15a128404
Martin Ciglan <mciglan@cisco.com>
2017-03-23 11:47:47 +0100

    Bug 1411 #5 BindingGeneratorImpl decomposition - Notifications
    - resolve YANG notifications from schema context
    - further util methods added
    - further decomposition in order to
    decrease initial BindingGeneratorImpl complexity
    
    TODO: jatoth JUnit tests, models
    
    Change-Id: Ie9e16d0ee23a7e1c1f2aab7871967261a013bb6f
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

--- binding2/mdsal-binding2-generator-impl/src/main/java/org/opendaylight/mdsal/binding/javav2/generator/impl/ModuleToGenType.java
+++ binding2/mdsal-binding2-generator-impl/src/main/java/org/opendaylight/mdsal/binding/javav2/generator/impl/ModuleToGenType.java
@@ -8,16 +8,28 @@
 
 package org.opendaylight.mdsal.binding.javav2.generator.impl;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.opendaylight.mdsal.binding.javav2.generator.impl.AuxiliaryGenUtils.annotateDeprecatedIfNecessary;
+import static org.opendaylight.mdsal.binding.javav2.generator.impl.AuxiliaryGenUtils.createDescription;
+import static org.opendaylight.mdsal.binding.javav2.generator.impl.GenHelperUtil.moduleTypeBuilder;
+import static org.opendaylight.mdsal.binding.javav2.generator.util.BindingGeneratorUtil.encodeAngleBrackets;
+import static org.opendaylight.mdsal.binding.javav2.generator.util.BindingTypes.NOTIFICATION;
+import static org.opendaylight.mdsal.binding.javav2.generator.util.BindingTypes.NOTIFICATION_LISTENER;
+
 import com.google.common.annotations.Beta;
 import com.google.common.base.Preconditions;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.opendaylight.mdsal.binding.javav2.generator.spi.TypeProvider;
+import org.opendaylight.mdsal.binding.javav2.generator.util.Types;
 import org.opendaylight.mdsal.binding.javav2.generator.yang.types.TypeProviderImpl;
+import org.opendaylight.mdsal.binding.javav2.model.api.AccessModifier;
 import org.opendaylight.mdsal.binding.javav2.model.api.Type;
 import org.opendaylight.mdsal.binding.javav2.model.api.type.builder.GeneratedTypeBuilder;
 import org.opendaylight.mdsal.binding.javav2.util.BindingMapping;
 import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 import org.opendaylight.yangtools.yang.model.util.DataNodeIterator;
@@ -35,8 +47,9 @@ final class ModuleToGenType {
 
         genCtx.put(module, new ModuleContext());
         genCtx = allTypeDefinitionsToGenTypes(module, genCtx, typeProvider);
+        genCtx = notificationsToGenType(module, genCtx, schemaContext, genTypeBuilders, verboseClassComments, typeProvider);
 
-        //TODO: call generate for other entities (groupings, rpcs, identities, notifications)
+        //TODO: call generate for other entities (groupings, rpcs, actions, identities)
 
         if (!module.getChildNodes().isEmpty()) {
             final GeneratedTypeBuilder moduleType = GenHelperUtil.moduleToDataType(module, genCtx, verboseClassComments);
@@ -64,7 +77,7 @@ final class ModuleToGenType {
      *             if set of type definitions from module is null
      */
     private static Map<Module, ModuleContext> allTypeDefinitionsToGenTypes(final Module module, Map<Module, ModuleContext> genCtx,
-                                                     TypeProvider typeProvider) {
+            TypeProvider typeProvider) {
         Preconditions.checkArgument(module != null, "Module reference cannot be NULL.");
         Preconditions.checkArgument(module.getName() != null, "Module name cannot be NULL.");
         final DataNodeIterator it = new DataNodeIterator(module);
@@ -80,6 +93,65 @@ final class ModuleToGenType {
                 ctx.addTypeToSchema(type, typedef);
             }
         });
+        return genCtx;
+    }
+
+    /**
+     * Converts all <b>notifications</b> of the module to the list of
+     * <code>Type</code> objects. In addition are to this list added containers
+     * and lists which are part of this notification.
+     *
+     * @param module
+     *            module from which is obtained set of all notification objects
+     *            to iterate over them
+     * @throws IllegalArgumentException
+     *             <ul>
+     *             <li>if the module equals null</li>
+     *             <li>if the name of module equals null</li>
+     *             </ul>
+     * @throws IllegalStateException
+     *             if set of notifications from module is null
+     */
+    private static Map<Module, ModuleContext> notificationsToGenType(final Module module, Map<Module, ModuleContext> genCtx,
+            final SchemaContext schemaContext, Map<String, Map<String, GeneratedTypeBuilder>> genTypeBuilders,
+            final boolean verboseClassComments, TypeProvider typeProvider) {
+        checkArgument(module != null, "Module reference cannot be NULL.");
+        checkArgument(module.getName() != null, "Module name cannot be NULL.");
+        final Set<NotificationDefinition> notifications = module.getNotifications();
+        if (notifications.isEmpty()) {
+            return genCtx;
+        }
+
+        final GeneratedTypeBuilder listenerInterface = moduleTypeBuilder(module, "Listener", verboseClassComments);
+        listenerInterface.addImplementsType(NOTIFICATION_LISTENER);
+        final String basePackageName = BindingMapping.getRootPackageName(module);
+
+        for (final NotificationDefinition notification : notifications) {
+            if (notification != null) {
+                GenHelperUtil.processUsesAugments(schemaContext, notification, module, genCtx, genTypeBuilders,
+                        verboseClassComments, typeProvider);
+
+                final GeneratedTypeBuilder notificationInterface = GenHelperUtil.addDefaultInterfaceDefinition
+                        (basePackageName, notification, null, module, genCtx, schemaContext,
+                                verboseClassComments, genTypeBuilders);
+                annotateDeprecatedIfNecessary(notification.getStatus(), notificationInterface);
+                notificationInterface.addImplementsType(NOTIFICATION);
+                genCtx.get(module).addChildNodeType(notification, notificationInterface);
+
+                // Notification object
+                GenHelperUtil.resolveDataSchemaNodes(module, basePackageName, notificationInterface,
+                        notificationInterface, notification.getChildNodes(), genCtx, schemaContext,
+                        verboseClassComments, genTypeBuilders, typeProvider);
+
+                listenerInterface.addMethod("on" + notificationInterface.getName())
+                        .setAccessModifier(AccessModifier.PUBLIC).addParameter(notificationInterface, "notification")
+                        .setComment(encodeAngleBrackets(notification.getDescription())).setReturnType(Types.VOID);
+            }
+        }
+        listenerInterface.setDescription(createDescription(notifications, module.getName(), verboseClassComments));
+
+        genCtx.get(module).addTopLevelNodeType(listenerInterface);
+
         return genCtx;
     }
 }
