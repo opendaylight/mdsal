e720f0e5ad2babd097be88c3c6001cebf2b15083
Robert Varga <nite@hq.sk>
2017-03-27 18:21:50 +0200

    BUG-8004: handle implicit RPC input
    RPC input is always defined implicitly, which is something we cannot
    deal with in Binding Specification compatibly, as original generators
    did not emit classes for such methods and implicitly map them to Void.
    
    In order to deal with these, we need to recognize when an RPC input
    is not declared and re-wire it to a null codec. This loses information,
    but it is the best we can do (simply because there is no binding
    representation).
    
    Change-Id: I4edbcd0cc886396bdba79f4c0ccfd91ba3d6b2c7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/SchemaRootCodecContext.java
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/SchemaRootCodecContext.java
@@ -33,6 +33,8 @@ import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
 import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementSource;
 import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
 import org.opendaylight.yangtools.yang.model.util.SchemaNodeUtils;
 
@@ -44,7 +46,6 @@ final class SchemaRootCodecContext<D extends DataObject> extends DataContainerCo
                 public DataContainerCodecContext<?,?> load(final Class<?> key) {
                     return createDataTreeChildContext(key);
                 }
-
             });
 
     private final LoadingCache<Class<?>, ContainerNodeCodecContext<?>> rpcDataByClass = CacheBuilder.newBuilder().build(
@@ -74,19 +75,26 @@ final class SchemaRootCodecContext<D extends DataObject> extends DataContainerCo
                         @SuppressWarnings("rawtypes")
                         final Class childCls = factory().getRuntimeContext().getClassForSchema(childSchema);
                         return streamChild(childCls);
-                    } else {
-                        throw new UnsupportedOperationException("Unsupported child type " + childSchema.getClass());
                     }
+
+                    throw new UnsupportedOperationException("Unsupported child type " + childSchema.getClass());
                 }
             });
 
-    private final LoadingCache<SchemaPath, ContainerNodeCodecContext<?>> rpcDataByPath = CacheBuilder.newBuilder().build(
-            new CacheLoader<SchemaPath, ContainerNodeCodecContext<?>>() {
+    private final LoadingCache<SchemaPath, AbstractContainerNodeCodecContext<?>> rpcDataByPath = CacheBuilder
+            .newBuilder().build(new CacheLoader<SchemaPath, AbstractContainerNodeCodecContext<?>>() {
 
                 @SuppressWarnings({ "rawtypes", "unchecked" })
                 @Override
-                public ContainerNodeCodecContext load(final SchemaPath key) {
+                public AbstractContainerNodeCodecContext load(final SchemaPath key) {
                     final ContainerSchemaNode schema = SchemaContextUtil.getRpcDataSchema(getSchema(), key);
+                    if (schema instanceof EffectiveStatement &&
+                            ((EffectiveStatement) schema).getDeclared().getStatementSource() != StatementSource.DECLARATION) {
+                        // This is an implicitly-defined input or output statement. We do not have a corresponding
+                        // data representation, so we hard-wire it to null.
+                        return UnmappedRpcContainerNodeCodecContext.getInstance();
+                    }
+
                     final Class cls = factory().getRuntimeContext().getClassForSchema(schema);
                     return getRpc(cls);
                 }
@@ -165,7 +173,7 @@ final class SchemaRootCodecContext<D extends DataObject> extends DataContainerCo
         return getOrRethrow(notificationsByPath, notification);
     }
 
-    ContainerNodeCodecContext<?> getRpc(final SchemaPath notification) {
+    AbstractContainerNodeCodecContext<?> getRpc(final SchemaPath notification) {
         return getOrRethrow(rpcDataByPath, notification);
     }
 
@@ -183,7 +191,6 @@ final class SchemaRootCodecContext<D extends DataObject> extends DataContainerCo
         for (final RpcDefinition potential : getSchema().getOperations()) {
             final QName potentialQName = potential.getQName();
             /*
-             *
              * Check if rpc and class represents data from same module and then
              * checks if rpc local name produces same class name as class name
              * appended with Input/Output based on QName associated with bidning
@@ -242,7 +249,7 @@ final class SchemaRootCodecContext<D extends DataObject> extends DataContainerCo
             return cache.getUnchecked(key);
         } catch (final UncheckedExecutionException e) {
             final Throwable cause = e.getCause();
-            if(cause != null) {
+            if (cause != null) {
                 Throwables.propagateIfPossible(cause);
             }
             throw e;
