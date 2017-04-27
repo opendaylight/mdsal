7ee235e06f9ce63320676ff10651003e18162f35
Robert Varga <robert.varga@pantheon.tech>
2017-04-27 18:20:53 +0200

    Speed up OsgiBundleScanningSchemaService close
    On close, the OsgiBundleScanningSchemaService closes the
    BundleTracker which untracks all the bundles and notifies the listener
    of removed bundles. This results in a call to tryToUpdateSchemaContext
    which causes the remaining yang files to be re-parsed to build a new
    SchemaContext. To prevent this extra processing on shutdown, I added
    a "stopping" flag to elide tryToUpdateSchemaContext the same we do
    with the "starting" flag.
    
    Change-Id: I9f7c05277df9bf1ffaec1c699453020312aab203
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit b0a11d8bb72e2b74f64ca1dbd6d981cf3a70c8ea)

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/osgi/OsgiBundleScanningSchemaService.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/osgi/OsgiBundleScanningSchemaService.java
@@ -49,6 +49,7 @@ public class OsgiBundleScanningSchemaService implements SchemaContextProvider, D
     private ServiceTracker<SchemaContextListener, SchemaContextListener> listenerTracker;
     private BundleTracker<Iterable<Registration>> bundleTracker;
     private boolean starting = true;
+    private volatile boolean stopping;
     private static OsgiBundleScanningSchemaService instance;
 
     private OsgiBundleScanningSchemaService(final BundleContext context) {
@@ -124,6 +125,7 @@ public class OsgiBundleScanningSchemaService implements SchemaContextProvider, D
 
     @Override
     public void close() {
+        stopping = true;
         if (bundleTracker != null) {
             bundleTracker.close();
         }
@@ -233,7 +235,7 @@ public class OsgiBundleScanningSchemaService implements SchemaContextProvider, D
     }
 
     public synchronized void tryToUpdateSchemaContext() {
-        if (starting) {
+        if (starting || stopping) {
             return;
         }
         final Optional<SchemaContext> schema = contextResolver.getSchemaContext();
