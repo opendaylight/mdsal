71a4cdf5db28a3a1c4e9bac9c3fd41ec69d5d05a
Jakub Toth <jakub.toth@pantheon.tech>
2017-05-19 18:00:50 +0200

    Bug 8449 - BindingToNormalizedNodeCodec fails to deserialize union of leafrefs
    Fix problem of leafref in typedef called from union
     *generated part
       *generating of new property of GTO for leaf's union type of typedef
         according to return type of referenced leaf via leafref from typedef
     *codec part
       *getting codec of leaf type according to new generator part of leafref
        in typedef
     *tests
    
    Change-Id: I156c3efe1c614c8e6ed2be086edd08b08ace1e76
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding/mdsal-binding-dom-codec/src/test/java/org/opendaylight/yangtools/binding/data/codec/test/LeafrefSerializeDeserializeTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.data.codec.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Map.Entry;
+import javassist.ClassPool;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.bug8449.rev180405.Cont;
+import org.opendaylight.yang.gen.v1.bug8449.rev180405.Cont.Ref;
+import org.opendaylight.yang.gen.v1.bug8449.rev180405.ContBuilder;
+import org.opendaylight.yangtools.binding.data.codec.gen.impl.StreamWriterGenerator;
+import org.opendaylight.yangtools.binding.data.codec.impl.BindingNormalizedNodeCodecRegistry;
+import org.opendaylight.yangtools.sal.binding.generator.util.JavassistUtils;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+public class LeafrefSerializeDeserializeTest extends AbstractBindingRuntimeTest {
+
+    private BindingNormalizedNodeCodecRegistry registry;
+
+    @Override
+    @Before
+    public void setup() {
+        super.setup();
+        final JavassistUtils utils = JavassistUtils.forClassPool(ClassPool.getDefault());
+        this.registry = new BindingNormalizedNodeCodecRegistry(StreamWriterGenerator.create(utils));
+        this.registry.onBindingRuntimeContextUpdated(getRuntimeContext());
+    }
+
+    @Test
+    public void listReferenceTest() {
+        final YangInstanceIdentifier contYII = YangInstanceIdentifier.builder().node(Cont.QNAME).build();
+        final InstanceIdentifier<?> fromYangInstanceIdentifier =
+                this.registry.fromYangInstanceIdentifier(contYII);
+        assertNotNull(fromYangInstanceIdentifier);
+
+        final InstanceIdentifier<Cont> BA_II_CONT = InstanceIdentifier.builder(Cont.class).build();
+        final Ref refVal = new Ref("myvalue");
+        final Cont data = new ContBuilder().setRef(refVal).build();
+        final Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> normalizedNode =
+                this.registry.toNormalizedNode(BA_II_CONT, data);
+        assertNotNull(normalizedNode);
+
+        final Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode =
+                this.registry.fromNormalizedNode(contYII, normalizedNode.getValue());
+        assertNotNull(fromNormalizedNode);
+        final Cont value = (Cont) fromNormalizedNode.getValue();
+        assertEquals(refVal, value.getRef());
+    }
+}
