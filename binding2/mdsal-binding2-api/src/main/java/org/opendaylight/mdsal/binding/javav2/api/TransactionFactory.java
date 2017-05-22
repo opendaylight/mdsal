bcdce61083f7ef5c836a23a417955ee988864bff
Martin Ciglan <martin.ciglan@pantheon.tech>
2017-05-22 14:24:28 +0200

    Binding2 runtime - API
    - DataBroker & relatives
    
    TODO: add Javadocs, test coverage
    
    Change-Id: I2d032ec3a0a565941266610194fb8d176c5392ff
    Signed-off-by: Martin Ciglan <martin.ciglan@pantheon.tech>

---
+++ binding2/mdsal-binding2-api/src/main/java/org/opendaylight/mdsal/binding/javav2/api/TransactionFactory.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding.javav2.api;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.mdsal.binding.javav2.spec.base.InstanceIdentifier;
+import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
+import org.opendaylight.mdsal.common.api.AsyncDataTransactionFactory;
+
+@Beta
+public interface TransactionFactory extends AsyncDataTransactionFactory<InstanceIdentifier<?>, TreeNode> {
+
+    @Override
+    ReadTransaction newReadOnlyTransaction();
+
+    @Override
+    WriteTransaction newWriteOnlyTransaction();
+}
