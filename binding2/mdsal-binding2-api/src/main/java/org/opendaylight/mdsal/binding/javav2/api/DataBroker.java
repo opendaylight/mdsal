2cfeb45bca729abfa28658a4cf23a1713849741e
Martin Ciglan <martin.ciglan@pantheon.tech>
2017-05-22 16:33:18 +0200

    Binding2 runtime - API #3
    - DataTreeProducer, DataTreeService & its relatives
    - Javadocs provided
    - test coverage for classes
    
    Change-Id: I75e2b4666db95153d97a24be38a9e2e78c37237f
    Signed-off-by: Martin Ciglan <martin.ciglan@pantheon.tech>

---
+++ binding2/mdsal-binding2-api/src/main/java/org/opendaylight/mdsal/binding/javav2/api/DataBroker.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding.javav2.api;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.mdsal.binding.javav2.spec.base.InstanceIdentifier;
+import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
+import org.opendaylight.mdsal.common.api.AsyncDataBroker;
+import org.opendaylight.mdsal.common.api.TransactionChainFactory;
+import org.opendaylight.mdsal.common.api.TransactionChainListener;
+
+/**
+ * Provides access to a conceptual data tree store and also provides the ability to
+ * subscribe for changes to data under a given branch of the tree.
+ *
+ * <p>
+ * For more information on usage, please see the documentation in {@link AsyncDataBroker}.
+ *
+ * @see AsyncDataBroker
+ * @see TransactionChainFactory
+ */
+@Beta
+public interface DataBroker extends AsyncDataBroker<InstanceIdentifier<?>, TreeNode>, BindingService,
+        TransactionFactory, DataTreeService, TransactionChainFactory<InstanceIdentifier<?>, TreeNode> {
+
+    @Override
+    ReadTransaction newReadOnlyTransaction();
+
+    @Override
+    WriteTransaction newWriteOnlyTransaction();
+
+    @Override
+    BindingTransactionChain createTransactionChain(TransactionChainListener listener);
+}
