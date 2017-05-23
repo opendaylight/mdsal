eea5bedd1b4258292e20a0199e3ebfbf1ff87b12
Martin Ciglan <martin.ciglan@pantheon.tech>
2017-05-23 10:11:25 +0200

    Binding2 runtime - API #4
    - MountPointService & its relatives
    
    TODO: add Javadocs, test coverage
    
    Change-Id: I15c55a34b0b4bb4df9aefd92749743c4a0cc2c3e
    Signed-off-by: Martin Ciglan <martin.ciglan@pantheon.tech>

---
+++ binding2/mdsal-binding2-api/src/main/java/org/opendaylight/mdsal/binding/javav2/api/MountPoint.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding.javav2.api;
+
+import com.google.common.annotations.Beta;
+import java.util.Optional;
+import org.opendaylight.mdsal.binding.javav2.spec.base.InstanceIdentifier;
+import org.opendaylight.yangtools.concepts.Identifiable;
+
+/**
+ * A Node can be behind a mount point. In this case, the URI has to be in format
+ * <identifier/>/yang-ext:mount/<identifier/>. The first <identifier/> is the path to
+ * a mount point and the second <identifier/> is the path to a node behind the mount point.
+ * A URI can end in a mount point itself by using <identifier/>/yang-ext:mount.
+ */
+@Beta
+public interface MountPoint extends Identifiable<InstanceIdentifier<?>> {
+
+    <T extends BindingService> Optional<T> getService(Class<T> service);
+}
