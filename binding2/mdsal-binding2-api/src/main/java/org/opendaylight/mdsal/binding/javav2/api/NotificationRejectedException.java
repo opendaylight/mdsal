c494fd2190765d0a78a57bbe3fac8073f962c443
Martin Ciglan <martin.ciglan@pantheon.tech>
2017-05-24 15:44:29 +0200

    Binding2 runtime API - #8
    - NotificationPublishService & its relatives
    
    TODO: Add Javadocs, test coverage
    
    Change-Id: I888f72924d8fe38e76f0990fec34b040824439c6
    Signed-off-by: Martin Ciglan <martin.ciglan@pantheon.tech>

---
+++ binding2/mdsal-binding2-api/src/main/java/org/opendaylight/mdsal/binding/javav2/api/NotificationRejectedException.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding.javav2.api;
+
+import com.google.common.annotations.Beta;
+
+@Beta
+public class NotificationRejectedException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public NotificationRejectedException(final String message) {
+        super(message);
+    }
+
+    public NotificationRejectedException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
