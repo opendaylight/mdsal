ce2c4e6647632538f9116487b39902cdc1ddf4f3
Jakub Toth <jakub.toth@pantheon.tech>
2017-05-31 11:12:10 +0200

    Binding v2 DOM Codec - codecs API
      * normalized node codec
      * tree codec
      * tree node codec
    
    Change-Id: Id58cf84d337ce310afb889c54f913fdbb59d79b0
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/api/codecs/BindingTreeCodec.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.codec.api.codecs;
+
+import javax.annotation.Nullable;
+import org.opendaylight.mdsal.binding.javav2.spec.base.InstanceIdentifier;
+import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+
+/**
+ * Navigable tree representing hierarchy of Binding to Normalized Node codecs.
+ *
+ * This navigable tree is associated to concrete set of YANG models, represented
+ * by SchemaContext and provides access to subtree specific serialization
+ * context.
+ */
+public interface BindingTreeCodec {
+
+    /**
+     * Get specific subtree serialization context by Binding path.
+     *
+     * @param path
+     *            - {@link InstanceIdentifier} path
+     * @return subtree codec
+     */
+    @Nullable
+    <T extends TreeNode> BindingTreeNodeCodec<T> getSubtreeCodec(InstanceIdentifier<T> path);
+
+    /**
+     * Get specific subtree serialization context by DOM path.
+     *
+     * @param path
+     *            - {@link YangInstanceIdentifier} path
+     * @return subtree codec
+     */
+    @Nullable
+    BindingTreeNodeCodec<?> getSubtreeCodec(YangInstanceIdentifier path);
+
+    /**
+     * Get specific subtree serialization context by {@link SchemaPath} path.
+     *
+     * @param path
+     *            - {@link SchemaPath} path
+     * @return specific subtree codec
+     */
+    @Nullable
+    BindingTreeNodeCodec<?> getSubtreeCodec(SchemaPath path);
+
+}
