49a25343bdecb832353204667416c11eaf87c5e7
MerlinChan <chen.mingling@zte.com.cn>
2017-06-05 14:56:06 +0800

    Bug 8531:Implementation for offerNotification method with timeout in DOMNotificationRouter.
    Change-Id: I086fe37873dcaa31c4d3ff10aabfc035cdfd42f6
    Signed-off-by: MerlinChan <chen.mingling@zte.com.cn>

--- dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/DOMNotificationRouterTest.java
+++ dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/DOMNotificationRouterTest.java
@@ -7,19 +7,26 @@
  */
 package org.opendaylight.mdsal.dom.broker;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 import com.google.common.collect.Multimap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 import org.junit.Test;
 import org.opendaylight.mdsal.dom.api.DOMNotification;
 import org.opendaylight.mdsal.dom.api.DOMNotificationListener;
+import org.opendaylight.mdsal.dom.api.DOMNotificationPublishService;
 import org.opendaylight.mdsal.dom.spi.DOMNotificationSubscriptionListener;
 import org.opendaylight.yangtools.util.ListenerRegistry;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
@@ -37,14 +44,15 @@ public class DOMNotificationRouterTest extends TestUtils {
     public void complexTest() throws Exception {
         final DOMNotificationSubscriptionListener domNotificationSubscriptionListener =
                 mock(DOMNotificationSubscriptionListener.class);
-        final DOMNotificationListener domNotificationListener = new TestListener();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final TestListener testListener = new TestListener(latch);
         final DOMNotificationRouter domNotificationRouter = DOMNotificationRouter.create(1);
 
         Multimap<SchemaPath, ?> listeners = domNotificationRouter.listeners();
 
         assertTrue(listeners.isEmpty());
-        assertNotNull(domNotificationRouter.registerNotificationListener(domNotificationListener, SchemaPath.ROOT));
-        assertNotNull(domNotificationRouter.registerNotificationListener(domNotificationListener, SchemaPath.SAME));
+        assertNotNull(domNotificationRouter.registerNotificationListener(testListener, SchemaPath.ROOT));
+        assertNotNull(domNotificationRouter.registerNotificationListener(testListener, SchemaPath.SAME));
 
         listeners = domNotificationRouter.listeners();
 
@@ -63,16 +71,16 @@ public class DOMNotificationRouterTest extends TestUtils {
         doReturn("test").when(domNotification).toString();
         doReturn(SchemaPath.ROOT).when(domNotification).getType();
         doReturn(TEST_CHILD).when(domNotification).getBody();
-
-        assertNotNull(domNotificationRouter.offerNotification(domNotification));
-
         try {
-            assertNotNull(domNotificationRouter.offerNotification(domNotification, 1, TimeUnit.SECONDS));
-            assertNotNull(domNotificationRouter.offerNotification(domNotification, 1, TimeUnit.SECONDS));
+            assertNotSame(DOMNotificationPublishService.REJECTED,
+                    domNotificationRouter.offerNotification(domNotification, 5, TimeUnit.SECONDS));
         } catch (Exception e) {
-            assertTrue(e instanceof UnsupportedOperationException);
+            assertTrue(e instanceof InterruptedException);
         }
-
+        latch.await();
+        assertTrue(testListener.getReceivedNotifications().size() == 1);
+        assertEquals(domNotification, testListener.getReceivedNotifications().get(0));
+        assertNotNull(domNotificationRouter.offerNotification(domNotification));
         assertNotNull(domNotificationRouter.putNotification(domNotification));
     }
 
@@ -98,8 +106,22 @@ public class DOMNotificationRouterTest extends TestUtils {
         assertTrue(executor.isShutdown());
     }
 
-    private class TestListener implements DOMNotificationListener {
+    private static class TestListener implements DOMNotificationListener {
+        private final CountDownLatch latch;
+        private final List<DOMNotification>  receivedNotifications = new ArrayList<>();
+
+        TestListener(final CountDownLatch latch) {
+            this.latch = latch;
+        }
+
         @Override
-        public void onNotification(@Nonnull final DOMNotification notification) {}
+        public void onNotification(@Nonnull final DOMNotification notification) {
+            receivedNotifications.add(notification);
+            latch.countDown();
+        }
+
+        public List<DOMNotification> getReceivedNotifications() {
+            return receivedNotifications;
+        }
     }
 }
