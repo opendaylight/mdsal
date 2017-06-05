c7a90c1f0f8fe69343ac11c7aebca4dab3c9e908
Martin Ciglan <martin.ciglan@pantheon.tech>
2017-06-05 16:27:47 +0200

    Binding2 runtime - Codecs impl #2
    - NodeCodecContext & relatives
    
    TODO: test coverage
    
    Change-Id: I024618f2b80207e47eec85819390868ee34b6407
    Signed-off-by: Martin Ciglan <martin.ciglan@pantheon.tech>

---
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/context/base/MissingSchemaForClassException.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding.javav2.dom.codec.impl.context.base;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.impl.MissingSchemaException;
+import org.opendaylight.mdsal.binding.javav2.runtime.context.BindingRuntimeContext;
+import org.opendaylight.mdsal.binding.javav2.spec.structural.Augmentation;
+
+/**
+ *
+ * Thrown when Java Binding class was used in data for which codec does not
+ * have schema.
+ *
+ * By serialization / deserialization of this exception {@link #getBindingClass()}
+ * will return null.
+ */
+@Beta
+public class MissingSchemaForClassException extends MissingSchemaException {
+
+    private static final long serialVersionUID = 1L;
+
+    private final transient Class<?> bindingClass;
+
+    private MissingSchemaForClassException(final Class<?> clz) {
+        super(String.format("Schema is not available for %s", clz));
+        this.bindingClass = Preconditions.checkNotNull(clz);
+    }
+
+    static MissingSchemaForClassException forClass(final Class<?> clz) {
+        return new MissingSchemaForClassException(clz);
+    }
+
+    public Class<?> getBindingClass() {
+        return bindingClass;
+    }
+
+    public static void check(final BindingRuntimeContext runtimeContext, final Class<?> bindingClass) {
+        final Object schema;
+        if (Augmentation.class.isAssignableFrom(bindingClass)) {
+            schema = runtimeContext.getAugmentationDefinition(bindingClass);
+        } else {
+            schema = runtimeContext.getSchemaDefinition(bindingClass);
+        }
+        if (schema == null) {
+            throw MissingSchemaForClassException.forClass(bindingClass);
+        }
+    }
+}
