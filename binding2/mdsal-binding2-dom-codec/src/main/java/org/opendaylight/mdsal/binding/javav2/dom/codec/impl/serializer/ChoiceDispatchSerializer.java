d19047e5f19313d389df628cddfec6c4e0d8c185
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-06 15:18:42 +0200

    Binding v2 DOM Codec - choice serializer implementation
    Change-Id: I2147b0ef3e254d552134ccf371a25fcdb18fe402
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/serializer/ChoiceDispatchSerializer.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.codec.impl.serializer;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import java.io.IOException;
+import org.opendaylight.mdsal.binding.javav2.spec.base.Instantiable;
+import org.opendaylight.mdsal.binding.javav2.spec.base.Item;
+import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
+import org.opendaylight.mdsal.binding.javav2.spec.runtime.BindingStreamEventWriter;
+import org.opendaylight.mdsal.binding.javav2.spec.runtime.TreeNodeSerializer;
+import org.opendaylight.mdsal.binding.javav2.spec.runtime.TreeNodeSerializerImplementation;
+import org.opendaylight.mdsal.binding.javav2.spec.runtime.TreeNodeSerializerRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Beta
+public class ChoiceDispatchSerializer implements TreeNodeSerializerImplementation {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ChoiceDispatchSerializer.class);
+
+    @SuppressWarnings("rawtypes")
+    private final Class choiceClass;
+
+    @SuppressWarnings("rawtypes")
+    private ChoiceDispatchSerializer(final Class choiceClass) {
+        this.choiceClass = Preconditions.checkNotNull(choiceClass);
+    }
+
+    public static final ChoiceDispatchSerializer from(final Class<? extends Instantiable<?>> choiceClass) {
+        return new ChoiceDispatchSerializer(choiceClass);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void serialize(final TreeNodeSerializerRegistry reg, final TreeNode obj,
+            final BindingStreamEventWriter stream) throws IOException {
+        @SuppressWarnings("rawtypes")
+        final Class cazeClass = obj.getClass();
+        stream.startChoiceNode(new Item<>(choiceClass), BindingStreamEventWriter.UNKNOWN_SIZE);
+        final TreeNodeSerializer caseSerializer = reg.getSerializer(cazeClass);
+        if (caseSerializer != null) {
+            caseSerializer.serialize(obj, stream);
+        } else {
+            LOG.warn("No serializer for case {} is available in registry {}", cazeClass, reg);
+        }
+        stream.endNode();
+    }
+}
