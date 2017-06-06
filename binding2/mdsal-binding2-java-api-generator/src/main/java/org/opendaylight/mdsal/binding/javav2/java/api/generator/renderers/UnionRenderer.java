5f8b268dd07c7226b60455ebfd06620cd21ab710
Jie Han <han.jie@zte.com.cn>
2017-06-06 20:05:33 +0800

    Binding generator v2 - Unions generate method 'getValue()'
    Change-Id: I8cfa676111952b169d6d6f31ce902adcce5bf45b
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

--- binding2/mdsal-binding2-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/javav2/java/api/generator/renderers/UnionRenderer.java
+++ binding2/mdsal-binding2-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/javav2/java/api/generator/renderers/UnionRenderer.java
@@ -10,6 +10,7 @@ package org.opendaylight.mdsal.binding.javav2.java.api.generator.renderers;
 
 import static org.opendaylight.mdsal.binding.javav2.generator.util.Types.BOOLEAN;
 
+import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
 import java.beans.ConstructorProperties;
@@ -23,6 +24,7 @@ import org.opendaylight.mdsal.binding.javav2.model.api.Enumeration;
 import org.opendaylight.mdsal.binding.javav2.model.api.GeneratedProperty;
 import org.opendaylight.mdsal.binding.javav2.model.api.GeneratedTransferObject;
 import org.opendaylight.mdsal.binding.javav2.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition;
 
 public class UnionRenderer extends ClassRenderer {
     public UnionRenderer(final GeneratedTransferObject type) {
@@ -90,7 +92,86 @@ public class UnionRenderer extends ClassRenderer {
                 .append(name)
                 .append(".clone()");
         }
-        sb.append(';');
+        sb.append(";\n");
+        return sb.toString();
+    }
+
+    private String generateCharArrayFieldForTypedef(final String fieldName, GeneratedTransferObject typedefType,
+                                                    final boolean isSuperType) {
+        Preconditions.checkState(typedefType.isTypedef(),"Not a typedef type!");
+
+        final StringBuilder sb = new StringBuilder();
+        final List<GeneratedProperty> retTypeCastProperties = typedefType.getProperties();
+
+        if (retTypeCastProperties != null &&
+                !retTypeCastProperties.isEmpty() && retTypeCastProperties.size() == 1 &&
+                retTypeCastProperties.get(0).getName().equals("value")) {
+
+            final StringBuilder sb1 = new StringBuilder(fieldName);
+            sb1.append(".")
+                    .append(TextTemplateUtil.getterMethodName(retTypeCastProperties.get(0)))
+                    .append("()");
+
+            sb.append(generateCharArrayField(sb1.toString(), retTypeCastProperties.get(0)));
+            // generated bits typedef
+        } else if (retTypeCastProperties != null && !retTypeCastProperties.isEmpty() &&
+                typedefType.getBaseType() instanceof BitsTypeDefinition) {
+            sb.append("java.util.Arrays.toString(")
+                    .append(fieldName)
+                    .append(".getValue()).toCharArray();");
+
+            //generated typedef typedef
+        } else if ((retTypeCastProperties == null || retTypeCastProperties.isEmpty())) {
+            Preconditions.checkState(typedefType.getSuperType() != null );
+
+            sb.append(generateCharArrayFieldForTypedef(fieldName,
+                    (GeneratedTransferObject) typedefType.getSuperType(), true));
+        }
+
+        return sb.toString();
+    }
+
+    private String generateCharArrayField(final String fieldName, final GeneratedProperty generatedProperty) {
+        final StringBuilder sb = new StringBuilder();
+        final Type propertyReturnType = generatedProperty.getReturnType();
+
+        // generated type String
+        if ("java.lang.String".equals(propertyReturnType.getFullyQualifiedName())) {
+            sb.append(fieldName).append(".toCharArray();");
+            // generated type InstanceIdentifier
+        } else if ("org.opendaylight.mdsal.binding2.spec.base.InstanceIdentifier".equals(propertyReturnType
+                .getFullyQualifiedName())) {
+                    sb.append(fieldName)
+                    .append(".toString().toCharArray();");
+            //generated type binary, boolean, empty
+        } else if (BOOLEAN.equals(propertyReturnType)) {
+            sb.append(fieldName).append(".toString().toCharArray();");
+            //generated type byte[]
+        } else if ("byte[]".equals(propertyReturnType.getName())) {
+            sb.append("BaseEncoding.base64().encode(").append(fieldName)
+                    .append(").toCharArray();");
+            //generated type int*, uint, decimal64 or enumeration*
+        } else if (propertyReturnType.getFullyQualifiedName().startsWith("java.lang") ||
+                propertyReturnType instanceof Enumeration ||
+                propertyReturnType.getFullyQualifiedName().startsWith("java.math")) {
+            sb.append(fieldName).append(".toString().toCharArray();");
+
+        } else if (propertyReturnType instanceof GeneratedTransferObject) {
+            final GeneratedTransferObject propRetTypeCast = (GeneratedTransferObject) propertyReturnType;
+
+            // generated union type
+            if (propRetTypeCast.isUnionType()) {
+                sb.append(fieldName).append(".getValue();");
+
+                // generated  type
+            } else if (propRetTypeCast.isTypedef()) {
+                sb.append(generateCharArrayFieldForTypedef(fieldName, propRetTypeCast, false));
+            }
+        } else {
+            sb.append(fieldName)
+                    .append(".getValue().toString().toCharArray();");
+        }
+
         return sb.toString();
     }
 
@@ -114,61 +195,19 @@ public class UnionRenderer extends ClassRenderer {
                 .append(TextTemplateUtil.fieldName(property))
                 .append(" != null) {")
                 .append(TextTemplateUtil.fieldName(field))
-                .append(" = ");
-
-            // generated type String
-            if ("java.lang.String".equals(propertyReturnType.getFullyQualifiedName())) {
-                sb.append(TextTemplateUtil.fieldName(property)).append(".toCharArray();");
-            // generated type InstanceIdentifier
-            } else if ("org.opendaylight.mdsal.binding2.spec.base.InstanceIdentifier".equals(propertyReturnType
-                    .getFullyQualifiedName())) {
-                sb.append(TextTemplateUtil.fieldName(field))
-                    .append(" = ")
-                    .append(TextTemplateUtil.fieldName(property))
-                    .append(".toString().toCharArray();");
-            //generated type binary
-            } else if ("byte[]".equals(propertyReturnType.getName())) {
-                sb.append("new ")
-                    .append(importedName(String.class))
-                    .append('(')
-                    .append(TextTemplateUtil.fieldName(property))
-                    .append(").toCharArray();");
-            //generated type int*, uint, decimal64 or enumeration*
-            } else if (propertyReturnType.getFullyQualifiedName().startsWith("java.lang") ||
-                    propertyReturnType instanceof Enumeration ||
-                    propertyReturnType.getFullyQualifiedName().startsWith("java.math")) {
-                sb.append(TextTemplateUtil.fieldName(property)).append(".toString().toCharArray();");
-
-            } else if (propertyReturnType instanceof GeneratedTransferObject) {
-                final GeneratedTransferObject propRetTypeCast = (GeneratedTransferObject) propertyReturnType;
-                final List<GeneratedProperty> retTypeCastProperties = propRetTypeCast.getProperties();
-
-                // generated union type
-                if (propRetTypeCast.isUnionType()) {
-                    sb.append(TextTemplateUtil.fieldName(property)).append(".getValue();");
-
-                // generated boolean typedef
-                } else if (propRetTypeCast.isTypedef() && retTypeCastProperties != null &&
-                        !retTypeCastProperties.isEmpty() && retTypeCastProperties.size() == 1 &&
-                        retTypeCastProperties.get(0).getName().equals("value") &&
-                        BOOLEAN.equals(retTypeCastProperties.get(0).getReturnType())) {
-                    sb.append(TextTemplateUtil.fieldName(property)).append(".isValue().toString().toCharArray();");
-
-                //generated byte[] typedef
-                } else if (propRetTypeCast.isTypedef() && retTypeCastProperties != null &&
-                        !retTypeCastProperties.isEmpty() && retTypeCastProperties.size() == 1 &&
-                        retTypeCastProperties.get(0).getName().equals("value") &&
-                        "byte[]".equals(retTypeCastProperties.get(0).getReturnType().getName())) {
-                    sb.append("BaseEncoding.base64().encode(").append(TextTemplateUtil.fieldName(property))
-                        .append(".getValue()).toCharArray();");
-                }
-            } else {
-                sb.append(TextTemplateUtil.fieldName(property))
-                        .append(".getValue().toString().toCharArray();");
-            }
-            sb.append("}");
+                .append(" = ")
+                .append(generateCharArrayField(TextTemplateUtil.fieldName(property), property))
+                .append("}\n");
             strings.add(sb);
         }
         return String.join(" else ", strings);
+    }
+
+    @Override
+    protected String generateInnerClassBody(GeneratedTransferObject innerClass) {
+        final UnionRenderer unionRenderer = new UnionRenderer((GeneratedTransferObject) innerClass);
+        final String body = unionRenderer.generateAsInnerClass();
+        this.putAllToImportMap(unionRenderer.getImportMap());
+        return body;
     }
 }
