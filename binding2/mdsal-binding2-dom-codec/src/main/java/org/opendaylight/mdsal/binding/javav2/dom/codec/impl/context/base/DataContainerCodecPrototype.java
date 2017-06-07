2e77492bd1791dcecf346fce0207187e24149a83
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-07 11:18:30 +0200

    Binding2 runtime - Codecs impl - context
      * derived contexts
      * fixed based contexts
    
    Change-Id: I2900f4356a7750553180a043d450b3f279c99d63
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

--- binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/context/base/DataContainerCodecPrototype.java
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/context/base/DataContainerCodecPrototype.java
@@ -29,7 +29,7 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 @Beta
-final class DataContainerCodecPrototype<T> implements NodeContextSupplier {
+public final class DataContainerCodecPrototype<T> implements NodeContextSupplier {
 
     private final T schema;
     private final QNameModule namespace;
