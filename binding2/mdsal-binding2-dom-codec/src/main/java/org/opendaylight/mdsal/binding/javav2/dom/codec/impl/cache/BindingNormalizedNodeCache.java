b1967827000248f427f230657e766ffee1e419e2
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-07 13:29:57 +0200

    Binding2 runtime - Codecs impl - cache
      * caching data part
    
    Change-Id: If0b595c4c50dd3a0741629432623210c31bce381
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/cache/BindingNormalizedNodeCache.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.codec.impl.cache;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.impl.context.base.DataContainerCodecContext;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.impl.serializer.CachingNormalizedNodeSerializer;
+import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+/**
+ * Cached NormalizedNode representation of TreeNode.
+ */
+@Beta
+public class BindingNormalizedNodeCache extends CacheLoader<TreeNode, NormalizedNode<?, ?>> {
+
+    @SuppressWarnings("rawtypes")
+    final DataContainerCodecContext subtreeRoot;
+    final AbstractBindingNormalizedNodeCacheHolder cacheHolder;
+    private final LoadingCache<TreeNode, NormalizedNode<?, ?>> cache =
+            CacheBuilder.newBuilder().weakValues().build(this);
+
+    public BindingNormalizedNodeCache(@Nonnull final AbstractBindingNormalizedNodeCacheHolder cacheHolder,
+            @Nonnull final DataContainerCodecContext<?, ?> subtreeRoot) {
+        this.cacheHolder = Preconditions.checkNotNull(cacheHolder, "cacheHolder");
+        this.subtreeRoot = Preconditions.checkNotNull(subtreeRoot, "subtreeRoot");
+    }
+
+    @Override
+    public NormalizedNode<?, ?> load(final TreeNode key) throws Exception {
+        return CachingNormalizedNodeSerializer.serializeUsingStreamWriter(cacheHolder, subtreeRoot, key);
+    }
+
+    /**
+     * Returns cached NormalizedNode representation of TreeNode.
+     *
+     * If representation is not cached, serializes TreeNode and updates cache
+     * with representation.
+     *
+     * @param obj
+     *            - binding object to be deserialized
+     * @return NormalizedNode representation of binding object
+     */
+    public NormalizedNode<?, ?> get(final TreeNode obj) {
+        return cache.getUnchecked(obj);
+    }
+}
---
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/codecs/NonCachingCodec.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.codec.impl.codecs;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.api.codecs.BindingNormalizedNodeCachingCodec;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.api.codecs.BindingNormalizedNodeCodec;
+import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+/**
+ * Non caching codec.
+ *
+ * @param <D>
+ *            - type of tree node
+ */
+@Beta
+public class NonCachingCodec<D extends TreeNode> implements BindingNormalizedNodeCachingCodec<D> {
+
+    private final BindingNormalizedNodeCodec<D> delegate;
+
+    public NonCachingCodec(final BindingNormalizedNodeCodec<D> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Nonnull
+    @Override
+    public D deserialize(@Nonnull final NormalizedNode<?, ?> data) {
+        return delegate.deserialize(data);
+    }
+
+    @Nonnull
+    @Override
+    public NormalizedNode<?, ?> serialize(@Nonnull final D data) {
+        return delegate.serialize(data);
+    }
+
+    @Override
+    public void close() {
+    }
+
+}
