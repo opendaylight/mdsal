85a0d1d408aa4badda8a358e5fae6dbc2d86ce2c
Jie Han <han.jie@zte.com.cn>
2017-06-08 20:31:18 +0800

    Binding generator v2 - Unions fix
    - multi-level innerclass support
    - fix package name of innerclass/multi-innerclass
    Change-Id: Ie1bd0df3afc1d600d94ff73b0c97aa349c242c78
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

--- binding2/mdsal-binding2-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/javav2/java/api/generator/renderers/ClassRenderer.java
+++ binding2/mdsal-binding2-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/javav2/java/api/generator/renderers/ClassRenderer.java
@@ -126,6 +126,10 @@ public class ClassRenderer extends BaseRenderer {
         return generateBody(false);
     }
 
+    protected String generateInnerClassBody(GeneratedTransferObject innerClass) {
+        return new ClassRenderer(innerClass).generateAsInnerClass();
+    }
+
     protected String generateBody(final boolean isInnerClass) {
         importedNames.put("type", importedName(getType()));
         importedNames.put("arrays", importedName(Arrays.class));
@@ -153,7 +157,7 @@ public class ClassRenderer extends BaseRenderer {
         if (!enclosedGeneratedTypes.isEmpty()) {
             for (GeneratedType innerClass : enclosedGeneratedTypes) {
                 if (innerClass instanceof GeneratedTransferObject) {
-                    classTemplateBuilder.add(new ClassRenderer((GeneratedTransferObject) innerClass).generateAsInnerClass());
+                    classTemplateBuilder.add(generateInnerClassBody((GeneratedTransferObject)innerClass));
                 }
             }
         }
