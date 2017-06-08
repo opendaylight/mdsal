3da7525a0c08431182c392e9343cf667aa3fd9b1
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-08 12:15:35 +0200

    Binding2 runtime - Codecs impl - codecs
      * remove codec.*.codecs packages
    
    Change-Id: I17897e62d5343ee059489a999d68c6bab0fe9c2c
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

--- binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/api/factory/BindingTreeCodecFactory.java
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/api/factory/BindingTreeCodecFactory.java
@@ -8,7 +8,7 @@
 package org.opendaylight.mdsal.binding.javav2.dom.codec.api.factory;
 
 import com.google.common.annotations.Beta;
-import org.opendaylight.mdsal.binding.javav2.dom.codec.api.codecs.BindingTreeCodec;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.api.BindingTreeCodec;
 import org.opendaylight.mdsal.binding.javav2.runtime.context.BindingRuntimeContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
