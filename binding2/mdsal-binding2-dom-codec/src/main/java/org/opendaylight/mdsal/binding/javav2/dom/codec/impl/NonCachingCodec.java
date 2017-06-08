1e4ef37d992c12f73fc32ed6feb057a332aec192
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-08 12:15:35 +0200

    Binding2 runtime - Codecs impl - codecs
      * remove codec.*.codecs packages
    
    Change-Id: I17897e62d5343ee059489a999d68c6bab0fe9c2c
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

--- binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/NonCachingCodec.java
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/NonCachingCodec.java
@@ -5,12 +5,12 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.mdsal.binding.javav2.dom.codec.impl.codecs;
+package org.opendaylight.mdsal.binding.javav2.dom.codec.impl;
 
 import com.google.common.annotations.Beta;
 import javax.annotation.Nonnull;
-import org.opendaylight.mdsal.binding.javav2.dom.codec.api.codecs.BindingNormalizedNodeCachingCodec;
-import org.opendaylight.mdsal.binding.javav2.dom.codec.api.codecs.BindingNormalizedNodeCodec;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.api.BindingNormalizedNodeCachingCodec;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.api.BindingNormalizedNodeCodec;
 import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
