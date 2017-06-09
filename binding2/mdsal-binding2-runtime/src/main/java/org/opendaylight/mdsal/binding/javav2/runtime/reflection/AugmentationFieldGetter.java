4ecf20777add8dc0a52f1daeba41c2152633f0de
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-09 15:04:12 +0200

    Binding v2 runtime - refactor - part2
      * various runtime classes lost in generator or even spec v2 packages
        are grouped in new runtime v2 module and used in runtime v2
      * move AugmentationFieldGetter and BindingReflections
      * move tests
    
    Change-Id: I61459f6a392890e2a2c038ab25e4592c2b135e05
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>
    (cherry picked from commit 0836a9a58b22244e880d68cb701346ac6ae03175)

--- binding2/mdsal-binding2-runtime/src/main/java/org/opendaylight/mdsal/binding/javav2/runtime/reflection/AugmentationFieldGetter.java
+++ binding2/mdsal-binding2-runtime/src/main/java/org/opendaylight/mdsal/binding/javav2/runtime/reflection/AugmentationFieldGetter.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.mdsal.binding.javav2.spec.util;
+package org.opendaylight.mdsal.binding.javav2.runtime.reflection;
 
 import com.google.common.annotations.Beta;
 import com.google.common.base.Preconditions;
