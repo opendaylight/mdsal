918a0567e557596f67165884b4a92af24b731a24
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-09 14:41:16 +0200

    Binding v2 runtime - refactor - part1
      * various runtime classes lost in generator or even spec v2 packages
        are grouped in new runtime v2 module and used in runtime v2
      * move BindingRuntimeContext and BindingSchemaContextUtils
    
    Change-Id: Iad8316c29a424b3dae881849d0c59c261d608629
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>
    (cherry picked from commit aef5c64ffa47d32d9914a0a90e3db755b7347061)

--- binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/api/factory/BindingTreeCodecFactory.java
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/api/factory/BindingTreeCodecFactory.java
@@ -9,7 +9,7 @@ package org.opendaylight.mdsal.binding.javav2.dom.codec.api.factory;
 
 import com.google.common.annotations.Beta;
 import org.opendaylight.mdsal.binding.javav2.dom.codec.api.codecs.BindingTreeCodec;
-import org.opendaylight.mdsal.binding.javav2.generator.impl.util.BindingRuntimeContext;
+import org.opendaylight.mdsal.binding.javav2.runtime.context.BindingRuntimeContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 @Beta
--- binding2/mdsal-binding2-runtime/src/main/java/org/opendaylight/mdsal/binding/javav2/runtime/context/util/BindingSchemaContextUtils.java
+++ binding2/mdsal-binding2-runtime/src/main/java/org/opendaylight/mdsal/binding/javav2/runtime/context/util/BindingSchemaContextUtils.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.mdsal.binding.javav2.generator.impl.util;
+package org.opendaylight.mdsal.binding.javav2.runtime.context.util;
 
 import com.google.common.annotations.Beta;
 import com.google.common.base.Optional;
