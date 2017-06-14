1968d644ac746a1bb1f3eb97880416b0e1fd3515
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-14 18:14:28 +0200

    Binding v2 runtime - adapters - registration
    Change-Id: Ibb2dfbfa869852fda66972395d6a2a54633e7909
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>
    (cherry picked from commit 54f5afb6605d01e6d68dc5d96548ee35fe91de74)

---
+++ binding2/mdsal-binding2-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/adapter/registration/BindingDataTreeListenerRegistration.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.adapter.registration;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import org.opendaylight.mdsal.binding.javav2.api.DataTreeListener;
+import org.opendaylight.yangtools.concepts.AbstractListenerRegistration;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+
+/**
+ * Registration of data tree listener.
+ *
+ * @param <L>
+ *            - {@link DataTreeListener} type
+ */
+@Beta
+public class BindingDataTreeListenerRegistration<L extends DataTreeListener>
+        extends AbstractListenerRegistration<L> {
+
+    private final ListenerRegistration<?> domReg;
+
+    public BindingDataTreeListenerRegistration(final L listener, final ListenerRegistration<?> domReg) {
+        super(listener);
+        this.domReg = Preconditions.checkNotNull(domReg);
+    }
+
+    @Override
+    protected void removeRegistration() {
+        domReg.close();
+    }
+}
