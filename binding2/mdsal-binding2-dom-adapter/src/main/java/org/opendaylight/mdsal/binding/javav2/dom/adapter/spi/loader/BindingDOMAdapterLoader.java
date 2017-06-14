c4408b7a6ae14e6d2b0e3a65497700e51d2f578f
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-14 14:44:16 +0200

    Binding v2 runtime - adapters - spi
    Change-Id: I824c5c29fd71ad288df0b3ef9fbeedbc760b98dc
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding2/mdsal-binding2-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/adapter/spi/loader/BindingDOMAdapterLoader.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.adapter.spi.loader;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.binding.javav2.api.BindingService;
+import org.opendaylight.mdsal.binding.javav2.dom.adapter.spi.builder.AdapterBuilder;
+import org.opendaylight.mdsal.binding.javav2.dom.adapter.spi.builder.BindingDOMAdapterBuilder;
+import org.opendaylight.mdsal.binding.javav2.dom.adapter.spi.builder.BindingDOMAdapterBuilder.Factory;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.impl.BindingToNormalizedNodeCodec;
+import org.opendaylight.mdsal.dom.api.DOMService;
+
+/**
+ * Loader for factory of services.
+ */
+@Beta
+public abstract class BindingDOMAdapterLoader extends AdapterLoader<BindingService, DOMService> {
+
+    // TODO add all factory of services
+    @SuppressWarnings("checkstyle:GenericWhitespace")
+    private static final Map<Class<?>, BindingDOMAdapterBuilder.Factory<?>> FACTORIES =
+            ImmutableMap.<Class<?>, BindingDOMAdapterBuilder.Factory<?>> builder().build();
+
+    private final BindingToNormalizedNodeCodec codec;
+
+    public BindingDOMAdapterLoader(final BindingToNormalizedNodeCodec codec) {
+        this.codec = codec;
+    }
+
+    @Nonnull
+    @Override
+    protected final AdapterBuilder<? extends BindingService, DOMService>
+            createBuilder(final Class<? extends BindingService> key) {
+        final Factory<?> factory = FACTORIES.get(key);
+        Preconditions.checkArgument(factory != null, "Unsupported service type %s", key);
+        final BindingDOMAdapterBuilder<?> builder = factory.newBuilder();
+        builder.setCodec(codec);
+        return builder;
+    }
+}
