761f5d286661b9f3d75d57011f1b949ee1c4558d
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-15 15:02:40 +0200

    Binding v2 runtime - adapters - impl - mount point
    Change-Id: Ie714a6d22f5fcf033b8ba157bd3fae9772846807
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding2/mdsal-binding2-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/adapter/impl/mount_point/BindingDOMMountPointServiceAdapter.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.adapter.impl.mount_point;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Optional;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.opendaylight.mdsal.binding.javav2.api.MountPoint;
+import org.opendaylight.mdsal.binding.javav2.api.MountPointListener;
+import org.opendaylight.mdsal.binding.javav2.api.MountPointService;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.impl.BindingToNormalizedNodeCodec;
+import org.opendaylight.mdsal.binding.javav2.spec.base.InstanceIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMMountPoint;
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Mount point service adapter.
+ */
+@Beta
+public class BindingDOMMountPointServiceAdapter implements MountPointService {
+
+    public static final Logger LOG = LoggerFactory.getLogger(BindingDOMMountPointServiceAdapter.class);
+
+    private final BindingToNormalizedNodeCodec codec;
+    private final DOMMountPointService mountService;
+    private final LoadingCache<DOMMountPoint, BindingMountPointAdapter> bindingMountpoints =
+            CacheBuilder.newBuilder().weakKeys().build(new CacheLoader<DOMMountPoint, BindingMountPointAdapter>() {
+
+                @Override
+                public BindingMountPointAdapter load(final DOMMountPoint key) throws Exception {
+                    return new BindingMountPointAdapter(codec, key);
+                }
+            });
+
+    public BindingDOMMountPointServiceAdapter(final DOMMountPointService mountService,
+            final BindingToNormalizedNodeCodec codec) {
+        this.codec = codec;
+        this.mountService = mountService;
+    }
+
+    @Override
+    public Optional<MountPoint> getMountPoint(final InstanceIdentifier<?> mountPoint) {
+
+        final YangInstanceIdentifier domPath = codec.toYangInstanceIdentifierBlocking(mountPoint);
+        final Optional<DOMMountPoint> domMount = mountService.getMountPoint(domPath);
+        if (domMount.isPresent()) {
+            return Optional.fromNullable(bindingMountpoints.getUnchecked(domMount.get()));
+        }
+        return Optional.absent();
+    }
+
+    @Override
+    public <T extends MountPointListener> ListenerRegistration<T> registerListener(final InstanceIdentifier<?> path,
+            final T listener) {
+        return new BindingDOMMountPointListenerAdapter<>(listener, codec, mountService);
+    }
+
+}
