edfe805b0113c80de0086a68879d75119887825a
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-19 15:38:57 +0200

    Binding v2 runtime - adapters - impl - operations invoker
    Change-Id: I7bf7144bf35882694c7223d6b3e5e2196757bd12
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding2/mdsal-binding2-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/adapter/impl/operation/invoker/AbstractMappedOperationInvoker.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.adapter.impl.operation.invoker;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.Future;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.opendaylight.mdsal.binding.javav2.runtime.reflection.BindingReflections;
+import org.opendaylight.mdsal.binding.javav2.spec.base.Instantiable;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Beta
+abstract class AbstractMappedOperationInvoker<T> extends OperationServiceInvoker {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractMappedOperationInvoker.class);
+    private final Map<T, OperationMethodInvoker> map;
+
+    protected AbstractMappedOperationInvoker(final Map<T, Method> map) {
+        final Builder<T, OperationMethodInvoker> b = ImmutableMap.builder();
+
+        for (final Entry<T, Method> e : map.entrySet()) {
+            if (BindingReflections.isRpcOrActionMethod(e.getValue())) {
+                b.put(e.getKey(), OperationMethodInvoker.from(e.getValue()));
+            } else {
+                LOG.debug("Method {} is not an operation method, ignoring it", e.getValue());
+            }
+        }
+
+        this.map = b.build();
+    }
+
+    protected abstract T qnameToKey(QName qname);
+
+    @Override
+    public final Future<RpcResult<?>> invoke(@Nonnull final Object impl, @Nonnull final QName operationName,
+            @Nullable final Instantiable<?> input) {
+
+        Preconditions.checkNotNull(impl, "Implementation must be supplied");
+
+        final OperationMethodInvoker invoker = map.get(qnameToKey(operationName));
+        Preconditions.checkArgument(invoker != null, "Supplied operation is not valid for implementation %s", impl);
+        return invoker.invokeOn(impl, input);
+    }
+}
---
+++ binding2/mdsal-binding2-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/adapter/impl/operation/invoker/OperationServiceInvoker.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.adapter.impl.operation.invoker;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.concurrent.Future;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.opendaylight.mdsal.binding.javav2.spec.base.Instantiable;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Provides single method invocation of operations on supplied instance.
+ *
+ * <p>
+ * Operation Service invoker provides common invocation interface for any subtype of operation. via
+ * {@link #invoke(Object, QName, Instantiable)} method.
+ */
+@Beta
+public abstract class OperationServiceInvoker {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OperationServiceInvoker.class);
+
+    /**
+     * Creates OperationServiceInvoker for specified operation type.
+     *
+     * @param type
+     *            operation interface, which was generated from model.
+     * @return Cached instance of {@link OperationServiceInvoker} for supplied operation type.
+     *
+     */
+    public static OperationServiceInvoker from(final Class<?> type) {
+        return ClassBasedOperationServiceInvoker.instanceFor(type);
+    }
+
+    /**
+     * Creates an OperationServiceInvoker for specified QName-&lt;Method mapping.
+     *
+     * @param qnameToMethod
+     *            translation mapping, must not be null nor empty.
+     * @return An {@link OperationMethodInvoker} instance.
+     */
+    public static OperationServiceInvoker from(final Map<QName, Method> qnameToMethod) {
+        Preconditions.checkArgument(!qnameToMethod.isEmpty());
+        QNameModule module = null;
+
+        for (final QName qname : qnameToMethod.keySet()) {
+            if (module != null) {
+                if (!module.equals(qname.getModule())) {
+                    LOG.debug("QNames from different modules {} and {}, falling back to QName map", module,
+                            qname.getModule());
+                    return QNameOperationServiceInvoker.instanceFor(qnameToMethod);
+                }
+            } else {
+                module = qname.getModule();
+            }
+        }
+
+        // All module are equal, which means we can use localName only
+        return LocalNameOperationServiceInvoker.instanceFor(module, qnameToMethod);
+    }
+
+    /**
+     * Invokes supplied operation on provided implementation of Operation Service.
+     *
+     * @param impl
+     *            Implementation on which operation should be invoked.
+     * @param operationName
+     *            Name of operation to be invoked.
+     * @param input
+     *            Input data for operation.
+     * @return Future which will complete once operation processing is finished.
+     */
+    public abstract Future<RpcResult<?>> invoke(@Nonnull Object impl, @Nonnull QName operationName,
+            @Nullable Instantiable<?> input);
+}
