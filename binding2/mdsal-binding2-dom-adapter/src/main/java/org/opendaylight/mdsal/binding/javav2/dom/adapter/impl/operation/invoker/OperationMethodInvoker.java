e9e7f06fe16481955d2c19c9acf83d486fe48a5e
Jakub Toth <jakub.toth@pantheon.tech>
2017-06-19 15:38:57 +0200

    Binding v2 runtime - adapters - impl - operations invoker
      * added marker for operations
    
    Change-Id: I7bf7144bf35882694c7223d6b3e5e2196757bd12
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding2/mdsal-binding2-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/adapter/impl/operation/invoker/OperationMethodInvoker.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.adapter.impl.operation.invoker;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Optional;
+import com.google.common.base.Throwables;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.reflect.Method;
+import java.util.concurrent.Future;
+import org.opendaylight.mdsal.binding.javav2.runtime.reflection.BindingReflections;
+import org.opendaylight.mdsal.binding.javav2.spec.base.Instantiable;
+import org.opendaylight.mdsal.binding.javav2.spec.base.Operation;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+@Beta
+abstract class OperationMethodInvoker {
+
+    private static final Lookup LOOKUP = MethodHandles.publicLookup();
+
+    protected abstract <T extends Operation> Future<RpcResult<?>> invokeOn(T impl, Instantiable<?> input);
+
+    protected static OperationMethodInvoker from(final Method method) {
+        final Optional<Class<? extends Instantiable<?>>> input = BindingReflections.resolveOperationInputClass(method);
+        try {
+            final MethodHandle methodHandle = LOOKUP.unreflect(method);
+            if (input.isPresent()) {
+                return new OperationMethodInvokerWithInput(methodHandle);
+            }
+            return new OperationMethodInvokerWithoutInput(methodHandle);
+        } catch (final IllegalAccessException e) {
+            throw new IllegalStateException("Lookup on public method failed.", e);
+        }
+    }
+
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    protected Future<RpcResult<?>> invoking(final MethodHandle handle, final Object... args) {
+        try {
+            return (Future<RpcResult<?>>) handle.invokeExact(args);
+        } catch (final Throwable e) {
+            throw Throwables.propagate(e);
+        }
+    }
+}
