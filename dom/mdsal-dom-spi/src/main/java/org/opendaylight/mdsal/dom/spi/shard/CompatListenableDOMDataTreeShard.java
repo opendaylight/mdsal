82533935e4c21c1ad9bddc88402b999c10f47055
Robert Varga <robert.varga@pantheon.tech>
2017-07-21 11:45:24 +0200

    BUG-8733: Add ListenableDOMDataTreeShard
    Implementation reliance on DOMDataTreeChangePublisher is a mistake
    coming from similarities between interfaces. DOMDataTreeShard interfaces
    should work with DOMDataTreeListener instances, not DOMDataTreeChangeListener.
    
    This patch introduces ListenableDOMDataTreeShard, which exposes a proper
    SPI-level method for registering DOMDataTreeListeners.
    
    It also adds AbstractStateAggregator, which can be used to efficiently
    aggregate multiple listeners into a single upcall, without the synchronization
    overhead of ShardedDOMDataTreeListenerContext.
    
    This class is then used to build DOMDataTreeChangeListenerAggregator for
    bridging the old approach with ListenableDOMDataTreeShard via a utility
    proxy, CompatListenableDOMDataTreeShard.
    
    We also introduce DOMDataTreeListenerAggregator, which performs aggregation
    of multiple DOMDataTreeListeners and is useful for ListenableDOMDataTreeShard
    implementations where requested subtrees live in multiple child shards.
    
    Change-Id: I979610e032605ade6d68196d51ae62778311f8c6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/shard/CompatListenableDOMDataTreeShard.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.ro. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.spi.shard;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ForwardingObject;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeChangeListener;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeListener;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeShard;
+import org.opendaylight.mdsal.dom.spi.store.DOMStoreTreeChangePublisher;
+import org.opendaylight.yangtools.concepts.AbstractListenerRegistration;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Compatibility layer between {@link DOMStoreTreeChangePublisher} and {@link ListenableDOMDataTreeShard}. Required
+ * for migration purposes.
+ *
+ * @author Robert Varga
+ *
+ * @deprecated This class is scheduled for removal when we remove compatibility with dom.spi.store APIs.
+ */
+@Deprecated
+public final class CompatListenableDOMDataTreeShard extends ForwardingObject implements ListenableDOMDataTreeShard {
+    private static final Logger LOG = LoggerFactory.getLogger(CompatListenableDOMDataTreeShard.class);
+
+    private final DOMDataTreeShard delegate;
+
+    private CompatListenableDOMDataTreeShard(final DOMDataTreeShard delegate) {
+        Preconditions.checkArgument(delegate instanceof DOMStoreTreeChangePublisher);
+        this.delegate = Preconditions.checkNotNull(delegate);
+    }
+
+    public static ListenableDOMDataTreeShard createIfNeeded(final DOMDataTreeShard delegate) {
+        if (delegate instanceof ListenableDOMDataTreeShard) {
+            return (ListenableDOMDataTreeShard) delegate;
+        }
+
+        LOG.debug("Using compatibility adaptor for {}", delegate);
+        return new CompatListenableDOMDataTreeShard(delegate);
+    }
+
+    @Override
+    protected DOMDataTreeShard delegate() {
+        return delegate;
+    }
+
+    @Override
+    public void onChildAttached(final DOMDataTreeIdentifier prefix, final DOMDataTreeShard child) {
+        delegate.onChildAttached(prefix, child);
+    }
+
+    @Override
+    public void onChildDetached(final DOMDataTreeIdentifier prefix, final DOMDataTreeShard child) {
+        delegate.onChildDetached(prefix, child);
+    }
+
+    private <L extends DOMDataTreeChangeListener> ListenerRegistration<L> registerTreeChangeListener(
+            @Nonnull final YangInstanceIdentifier treeId, @Nonnull final L listener) {
+        return ((DOMStoreTreeChangePublisher) delegate).registerTreeChangeListener(treeId, listener);
+    }
+
+    @Override
+    public <L extends DOMDataTreeListener> ListenerRegistration<L> registerListener(final L listener,
+            final Collection<DOMDataTreeIdentifier> subtrees, final boolean allowRxMerges) {
+        if (subtrees.size() == 1) {
+            final DOMDataTreeIdentifier treeId = Iterables.getOnlyElement(subtrees);
+
+            final ListenerRegistration<?> reg = registerTreeChangeListener(treeId.getRootIdentifier(),
+                changes -> {
+                    final Optional<NormalizedNode<?, ?>> last = Iterables.getLast(changes).getRootNode().getDataAfter();
+                    if (last.isPresent()) {
+                        listener.onDataTreeChanged(changes, ImmutableMap.of(treeId, last.get()));
+                    } else {
+                        listener.onDataTreeChanged(changes, ImmutableMap.of());
+                    }
+                });
+            return new AbstractListenerRegistration<L>(listener) {
+                @Override
+                protected void removeRegistration() {
+                    reg.close();
+                }
+            };
+        }
+
+        final int size = subtrees.size();
+        final Collection<ListenerRegistration<?>> regs = new ArrayList<>(size);
+        final DOMDataTreeChangeListenerAggregator aggregator = new DOMDataTreeChangeListenerAggregator(size,
+            allowRxMerges);
+        for (DOMDataTreeIdentifier treeId : subtrees) {
+            regs.add(registerTreeChangeListener(treeId.getRootIdentifier(), aggregator.createListener(treeId)));
+        }
+
+        return aggregator.start(listener, regs);
+    }
+}
---
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/shard/DOMDataTreeListenerAggregator.java
@@ -0,0 +1,293 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.ro. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.spi.shard;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Stopwatch;
+import com.google.common.base.Verify;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
+import javax.annotation.concurrent.GuardedBy;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeListener;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeListeningException;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeShard;
+import org.opendaylight.yangtools.concepts.AbstractListenerRegistration;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Aggregator which combines multiple disjunct {@link DOMDataTreeListener} and forwards their changes to a central
+ * listener.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class DOMDataTreeListenerAggregator
+        extends AbstractStateAggregator<DOMDataTreeListenerAggregator.State> {
+
+    abstract static class State extends AbstractStateAggregator.State {
+
+    }
+
+    private static final class Aggregated extends State {
+        final Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees;
+        final Collection<DOMDataTreeListeningException> failures;
+        final Collection<DataTreeCandidate> changes;
+
+        Aggregated(final Collection<DataTreeCandidate> changes,
+            final Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees,
+            final Collection<DOMDataTreeListeningException> failures) {
+            this.changes = Preconditions.checkNotNull(changes);
+            this.subtrees = Preconditions.checkNotNull(subtrees);
+            this.failures = Preconditions.checkNotNull(failures);
+        }
+    }
+
+    private static final class Changes extends State {
+        final Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees;
+        final Collection<DataTreeCandidate> changes;
+
+        Changes(final Collection<DataTreeCandidate> changes,
+            final Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees) {
+            this.changes = Preconditions.checkNotNull(changes);
+            this.subtrees = Preconditions.checkNotNull(subtrees);
+        }
+    }
+
+    private static final class Failure extends State {
+        final Collection<DOMDataTreeListeningException> causes;
+
+        Failure(final Collection<DOMDataTreeListeningException> causes) {
+            this.causes = Preconditions.checkNotNull(causes);
+        }
+    }
+
+    private static final class StateBuilder extends AbstractStateAggregator.StateBuilder<State> {
+        @GuardedBy("this")
+        private final Collection<DOMDataTreeListeningException> causes = new ArrayList<>(0);
+        @GuardedBy("this")
+        private final Collection<DataTreeCandidate> changes = new ArrayList<>();
+        @GuardedBy("this")
+        private Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees = ImmutableMap.of();
+
+        @Override
+        protected void append(final State state) {
+            if (state instanceof Changes) {
+                final Changes changes = (Changes) state;
+                this.changes.addAll(changes.changes);
+                subtrees = ImmutableMap.copyOf(changes.subtrees);
+            } else if (state instanceof Failure) {
+                causes.addAll(((Failure) state).causes);
+            } else {
+                throw new IllegalStateException("Unexpected state " + state);
+            }
+        }
+
+        @Override
+        protected synchronized void appendInitial(final State state) {
+            // TODO: we could index and compress state changes here
+            if (state instanceof Changes) {
+                final Changes changes = (Changes) state;
+                this.changes.addAll(changes.changes);
+                subtrees = ImmutableMap.copyOf(changes.subtrees);
+            } else if (state instanceof Failure) {
+                causes.addAll(((Failure) state).causes);
+            } else {
+                throw new IllegalStateException("Unexpected state " + state);
+            }
+        }
+
+        @Override
+        public synchronized Aggregated build() {
+            final Aggregated ret = new Aggregated(ImmutableList.copyOf(changes), subtrees,
+                ImmutableList.copyOf(causes));
+            changes.clear();
+            causes.clear();
+            return ret;
+        }
+    }
+
+    private static final class Operational extends AbstractStateAggregator.Operational<State> {
+        private final DOMDataTreeListener listener;
+        private boolean failed;
+
+        Operational(final Collection<AbstractStateAggregator.StateBuilder<State>> builders,
+                final DOMDataTreeListener listener) {
+            super(builders);
+            this.listener = Preconditions.checkNotNull(listener);
+        }
+
+        @Override
+        protected void notifyListener(final Iterator<State> iterator) {
+            if (failed) {
+                iterator.forEachRemaining(state -> LOG.debug("Listener {} failed, ignoring state {}", state));
+                return;
+            }
+
+            final Stopwatch clock = Stopwatch.createStarted();
+            final List<DataTreeCandidate> changes = new ArrayList<>();
+            final List<DOMDataTreeListeningException> failures = new ArrayList<>(0);
+            final Builder<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees = ImmutableMap.builder();
+            while (iterator.hasNext()) {
+                collectState(iterator.next(), changes, subtrees, failures);
+            }
+
+            if (!changes.isEmpty()) {
+                // Note: it is okay to leak changes, we must never leak mutable subtrees.
+                callListener(listener, changes, subtrees.build());
+            }
+            if (!failures.isEmpty()) {
+                failed = true;
+                listener.onDataTreeFailed(failures);
+            }
+
+            LOG.trace("Listener {} notification completed in {}", listener, clock);
+        }
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(DOMDataTreeListenerAggregator.class);
+
+    // Because a component listener may report a failure before we finish registering all listeners, we need a way
+    // to trigger a failure report from the thread *not* performing the registration.
+    private static final Executor FAILURE_NOTIFIER;
+
+    static {
+        final ThreadFactoryBuilder tfb = new ThreadFactoryBuilder().setDaemon(true)
+                .setNameFormat(DOMDataTreeListenerAggregator.class.getSimpleName() + "-failure-%s");
+        FAILURE_NOTIFIER = Executors.newSingleThreadExecutor(tfb.build());
+    }
+
+    private final boolean allowRxMerges;
+
+    private DOMDataTreeListenerAggregator(final int sizeHint, final boolean allowRxMerges) {
+        super(sizeHint);
+        this.allowRxMerges = allowRxMerges;
+    }
+
+    public static <L extends DOMDataTreeListener, T> ListenerRegistration<L> aggregateIfNeeded(final L listener,
+            final Map<T, Collection<DOMDataTreeIdentifier>> subtrees, final boolean allowRxMerges,
+            final Function<T, DOMDataTreeShard> keyToShard) {
+        if (subtrees.size() == 1) {
+            final Entry<T, Collection<DOMDataTreeIdentifier>> entry = subtrees.entrySet().iterator()
+                    .next();
+            return CompatListenableDOMDataTreeShard.createIfNeeded(keyToShard.apply(entry.getKey()))
+                    .registerListener(listener, entry.getValue(), allowRxMerges);
+        }
+
+        // Alright, this the real deal, we have to aggregate.
+        final int size = subtrees.size();
+        final DOMDataTreeListenerAggregator aggregator = new DOMDataTreeListenerAggregator(size, allowRxMerges);
+        final Collection<ListenerRegistration<DOMDataTreeListener>> regs = new ArrayList<>(size);
+        for (Entry<T, Collection<DOMDataTreeIdentifier>> entry : subtrees.entrySet()) {
+            regs.add(CompatListenableDOMDataTreeShard.createIfNeeded(keyToShard.apply(entry.getKey()))
+                .registerListener(aggregator.createListener(), entry.getValue(), allowRxMerges));
+        }
+
+        return aggregator.start(listener, regs);
+    }
+
+    private DOMDataTreeListener createListener() {
+        // TODO: do not ignore allowRxMerges, but rather create a dedicated subclass or something
+        final StateBuilder builder = new StateBuilder();
+        addBuilder(builder);
+
+        return new DOMDataTreeListener() {
+            @Override
+            public void onDataTreeFailed(final Collection<DOMDataTreeListeningException> causes) {
+                receiveState(builder, new Failure(causes));
+            }
+
+            @Override
+            public void onDataTreeChanged(final Collection<DataTreeCandidate> changes,
+                    final Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees) {
+                receiveState(builder, new Changes(changes, subtrees));
+            }
+        };
+    }
+
+    private <L extends DOMDataTreeListener> ListenerRegistration<L> start(final L listener,
+            final Collection<ListenerRegistration<DOMDataTreeListener>> regs) {
+
+        final Started<State> result = start(builders -> start(listener, regs, builders));
+        if (result instanceof Failed) {
+            return new AbstractListenerRegistration<L>(listener) {
+                @Override
+                protected void removeRegistration() {
+                    // Listeners have already been closed, this is a no-op
+                }
+            };
+        }
+
+        return new AbstractListenerRegistration<L>(listener) {
+            @Override
+            protected void removeRegistration() {
+                regs.forEach(ListenerRegistration::close);
+            }
+        };
+    }
+
+    static Started<State> start(final DOMDataTreeListener listener,
+            final Collection<ListenerRegistration<DOMDataTreeListener>> regs,
+            final Collection<AbstractStateAggregator.StateBuilder<State>> builders) {
+
+        final List<DataTreeCandidate> changes = new ArrayList<>();
+        final List<DOMDataTreeListeningException> failures = new ArrayList<>(0);
+        final Builder<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees = ImmutableMap.builder();
+        for (AbstractStateAggregator.StateBuilder<State> builder : builders) {
+            collectState(builder.build(), changes, subtrees, failures);
+        }
+
+        if (!failures.isEmpty()) {
+            regs.forEach(ListenerRegistration::close);
+            FAILURE_NOTIFIER.execute(() -> listener.onDataTreeFailed(failures));
+            return new Failed<>(builders);
+        }
+        if (!changes.isEmpty()) {
+            callListener(listener, changes, subtrees.build());
+        }
+
+        return new Operational(builders, listener);
+    }
+
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    static void callListener(final DOMDataTreeListener listener, final Collection<DataTreeCandidate> changes,
+            final Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees) {
+        try {
+            listener.onDataTreeChanged(changes, subtrees);
+        } catch (Exception e) {
+            LOG.error("Listener {} failed to process initial changes", listener, e);
+        }
+    }
+
+    static void collectState(final State state, final Collection<DataTreeCandidate> changes,
+            final Builder<DOMDataTreeIdentifier, NormalizedNode<?, ?>> subtrees,
+            final Collection<DOMDataTreeListeningException> failures) {
+        Verify.verify(state instanceof Aggregated, "Unexpected state %s", state);
+        final Aggregated aggregated = (Aggregated) state;
+
+        subtrees.putAll(aggregated.subtrees);
+        changes.addAll(aggregated.changes);
+        failures.addAll(aggregated.failures);
+    }
+}
