45231f996154f3fc8bfee5394f01820144aa086c
Jakub Toth <jakub.toth@pantheon.tech>
2017-07-21 11:58:08 +0200

    Bug 8449 - BindingToNormalizedNodeCodec fails to deserialize union of leafrefs
    Fix problem of leafref in typedef called from union
     *generated part
       *generating of new property of GTO for leaf's union type of typedef
         according to return type of referenced leaf via leafref from typedef
     *codec part
       *getting codec of leaf type according to new generator part of leafref
        in typedef
     *tests
    
    Change-Id: Ibffe4e51ef66f1911c32c71d4f08bbdbdd40e234
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

--- binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/UnionTypeDefTest.java
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/UnionTypeDefTest.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.mdsal.binding.generator.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
@@ -14,8 +15,10 @@ import java.io.File;
 import java.util.List;
 import org.junit.Test;
 import org.opendaylight.mdsal.binding.generator.api.BindingGenerator;
-import org.opendaylight.mdsal.binding.generator.impl.BindingGeneratorImpl;
+import org.opendaylight.mdsal.binding.model.api.GeneratedProperty;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
 import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.mdsal.binding.model.util.Types;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
@@ -23,8 +26,8 @@ public class UnionTypeDefTest {
 
     @Test
     public void unionTypeResolvingTest() throws Exception {
-        File abstractTopology = new File(getClass().getResource("/union-test-models/abstract-topology.yang").toURI());
-        File ietfInetTypes = new File(getClass().getResource("/ietf/ietf-inet-types.yang").toURI());
+        final File abstractTopology = new File(getClass().getResource("/union-test-models/abstract-topology.yang").toURI());
+        final File ietfInetTypes = new File(getClass().getResource("/ietf/ietf-inet-types.yang").toURI());
         final SchemaContext context = YangParserTestUtils.parseYangSources(abstractTopology, ietfInetTypes);
 
         assertNotNull("context is null", context);
@@ -35,5 +38,31 @@ public class UnionTypeDefTest {
         assertFalse("genTypes is empty", genTypes.isEmpty());
 
         // TODO: implement test
+    }
+
+    @Test
+    public void unionTypedefLeafrefTest() throws Exception {
+        final File yang = new File(getClass().getResource("/leafref_typedef_union/bug8449.yang").toURI());
+        final SchemaContext schemaContext = YangParserTestUtils.parseYangSources(yang);
+        assertNotNull(schemaContext);
+        final List<Type> generateTypes = new BindingGeneratorImpl(false).generateTypes(schemaContext);
+        assertNotNull(generateTypes);
+        for (final Type type : generateTypes) {
+            if (type.getName().equals("Cont")) {
+                final GeneratedType gt = ((GeneratedType) type);
+                assertNotNull(gt);
+                final GeneratedType refType = gt.getEnclosedTypes().iterator().next();
+                for (final GeneratedProperty generatedProperty : refType.getProperties()) {
+                    switch (generatedProperty.getName()) {
+                        case "stringRefValue":
+                            assertEquals(Types.STRING, generatedProperty.getReturnType());
+                            break;
+                        case "value":
+                            assertEquals(Types.CHAR_ARRAY, generatedProperty.getReturnType());
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
