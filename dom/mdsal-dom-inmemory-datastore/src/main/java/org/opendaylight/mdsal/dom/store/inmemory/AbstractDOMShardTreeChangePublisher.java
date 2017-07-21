20d2832ce98c3452b1666cefc8710b66039f404e
Robert Varga <robert.varga@pantheon.tech>
2017-07-21 12:11:22 +0200

    BUG-8733: use DataTreeCandidateNodes.empty()
    Empty node has been moved to yang-data-api, use it from there.
    
    Change-Id: I69477c88d11a644095d2a10932a1c930da073687
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-inmemory-datastore/src/main/java/org/opendaylight/mdsal/dom/store/inmemory/AbstractDOMShardTreeChangePublisher.java
+++ dom/mdsal-dom-inmemory-datastore/src/main/java/org/opendaylight/mdsal/dom/store/inmemory/AbstractDOMShardTreeChangePublisher.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeChangeListener;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
 import org.opendaylight.mdsal.dom.spi.AbstractDOMDataTreeChangeListenerRegistration;
@@ -37,10 +36,10 @@ import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTree;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNodes;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidates;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataValidationFailedException;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.NormalizedNodeBuilder;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableMapEntryNodeBuilder;
@@ -124,14 +123,14 @@ abstract class AbstractDOMShardTreeChangePublisher extends AbstractDOMStoreTreeC
             // empty normalized node, root is always present
             if (((DataContainerNode<?>) data).getValue().isEmpty()) {
                 initialCandidate = DataTreeCandidates.newDataTreeCandidate(listenerPath,
-                        new EmptyDataTreeCandidateNode(data.getIdentifier()));
+                    DataTreeCandidateNodes.empty(data.getIdentifier()));
             } else {
                 initialCandidate = DataTreeCandidates.fromNormalizedNode(listenerPath,
-                        translateRootShardIdentifierToListenerPath(listenerPath, preExistingData.get()));
+                    translateRootShardIdentifierToListenerPath(listenerPath, preExistingData.get()));
             }
         } else {
             initialCandidate = DataTreeCandidates.newDataTreeCandidate(listenerPath,
-                    new EmptyDataTreeCandidateNode(listenerPath.getLastPathArgument()));
+                DataTreeCandidateNodes.empty(listenerPath.getLastPathArgument()));
         }
 
         listener.onDataTreeChanged(Collections.singleton(initialCandidate));
@@ -265,56 +264,10 @@ abstract class AbstractDOMShardTreeChangePublisher extends AbstractDOMStoreTreeC
             }
 
             if (modifiedChild == null) {
-                modifiedChild = new EmptyDataTreeCandidateNode(listenerPath.getLastPathArgument());
+                modifiedChild = DataTreeCandidateNodes.empty(listenerPath.getLastPathArgument());
             }
 
             return DataTreeCandidates.newDataTreeCandidate(listenerPath, modifiedChild);
         }
     }
-
-    private static final class EmptyDataTreeCandidateNode implements DataTreeCandidateNode {
-
-        private final PathArgument identifier;
-
-        EmptyDataTreeCandidateNode(final PathArgument identifier) {
-            this.identifier = Preconditions.checkNotNull(identifier, "Identifier should not be null");
-        }
-
-        @Nonnull
-        @Override
-        public PathArgument getIdentifier() {
-            return identifier;
-        }
-
-        @Nonnull
-        @Override
-        public Collection<DataTreeCandidateNode> getChildNodes() {
-            return Collections.emptySet();
-        }
-
-        @Nullable
-        @Override
-        public DataTreeCandidateNode getModifiedChild(final PathArgument identifier) {
-            return null;
-        }
-
-        @Nonnull
-        @Override
-        public ModificationType getModificationType() {
-            return ModificationType.UNMODIFIED;
-        }
-
-        @Nonnull
-        @Override
-        public Optional<NormalizedNode<?, ?>> getDataAfter() {
-            return Optional.absent();
-        }
-
-        @Nonnull
-        @Override
-        public Optional<NormalizedNode<?, ?>> getDataBefore() {
-            return Optional.absent();
-        }
-    }
-
 }
