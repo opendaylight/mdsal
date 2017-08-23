9a249982a1e290dd2f1400c58b4899381e850224
Jie Han <han.jie@zte.com.cn>
2017-08-23 17:19:50 +0800

    Binding codec v2 - fix augmentation #3
    - Since multiple augments with same target were grouped as like
    just one augment, all child nodes of them were resolved as methods
    to the generated type, so in runtime, it should also find all these
    child nodes to add to the new EffectiveAugmentSchema object as
    real childs.
    Change-Id: I433930be45007e545b2e2fb074af32e118aa7cd4
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

--- binding2/mdsal-binding2-generator-api/src/main/java/org/opendaylight/mdsal/binding/javav2/generator/context/ModuleContext.java
+++ binding2/mdsal-binding2-generator-api/src/main/java/org/opendaylight/mdsal/binding/javav2/generator/context/ModuleContext.java
@@ -52,7 +52,7 @@ public final class ModuleContext {
     private final Map<QName,GeneratedTOBuilder> identities = new HashMap<>();
     private final Set<GeneratedTypeBuilder> topLevelNodes = new HashSet<>();
     private final List<GeneratedTypeBuilder> augmentations = new ArrayList<>();
-    private final BiMap<Type,AugmentationSchema> typeToAugmentation = HashBiMap.create();
+    private final Map<Type,List<AugmentationSchema>> typeToAugmentations = new HashMap<>();
     private final BiMap<SchemaPath,Type> targetToAugmentation = HashBiMap.create();
     private final Map<Type,Object> typeToSchema = new HashMap<>();
     private final Multimap<Type, Type> choiceToCases = HashMultimap.create();
@@ -169,8 +169,8 @@ public final class ModuleContext {
         return Collections.unmodifiableList(this.augmentations);
     }
 
-    public BiMap<Type, AugmentationSchema> getTypeToAugmentation() {
-        return Maps.unmodifiableBiMap(this.typeToAugmentation);
+    public Map<Type, List<AugmentationSchema>> getTypeToAugmentations() {
+        return Collections.unmodifiableMap(this.typeToAugmentations);
     }
 
     public BiMap<SchemaPath, Type> getTargetToAugmentation() {
@@ -178,8 +178,11 @@ public final class ModuleContext {
     }
 
     public void addTypeToAugmentation(final GeneratedTypeBuilder builder, final AugmentationSchema schema) {
-        this.typeToAugmentation.put(builder, schema);
         this.typeToSchema.put(builder, schema);
+    }
+
+    public void addTypeToAugmentations(final GeneratedTypeBuilder builder, final List<AugmentationSchema> schemaList) {
+        this.typeToAugmentations.put(builder, schemaList);
     }
 
     public void addTargetToAugmentation(final GeneratedTypeBuilder builder, final SchemaPath augmentTarget) {
