5db0cb95684819e66c7baa791c4e9c9e51c02ce6
Jakub Toth <jakub.toth@pantheon.tech>
2017-08-24 12:46:16 +0200

    Bug 8326 - Split out OsgiBundleScanningSchemaService
     * move osgi part of mdsal-dom broker to new module
       mdsal-dom-schema-service-osgi (with unit tests)
     * add support of injecting ScanningSchemaService (DOMSchemaService)
       via ServiceLoader + unit tests
    
    Change-Id: I2eb2e4f96f50456f84982e5de6d439b0306bcc0f
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/schema/ScanningSchemaServiceProviderTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.broker.schema;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ServiceLoader;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class ScanningSchemaServiceProviderTest {
+
+    private static ArrayList<URL> yangs;
+
+    @BeforeClass
+    public static void setup() {
+        yangs = new ArrayList<>();
+        yangs.add(ScanningSchemaServiceProvider.class.getResource("/odl-datastore-test.yang"));
+    }
+
+    @Test
+    public void initJarScanningSchemaServiceTest() throws Exception {
+        final ScanningSchemaServiceProvider service = new ScanningSchemaServiceProvider();
+        service.registerAvalaibleYangs(yangs);
+        final SchemaContext globalContext = service.getGlobalContext();
+        assertNotNull(globalContext);
+        service.close();
+    }
+
+    @Test
+    public void injectingAndLoadingServiceTest() {
+        final ServiceLoader<ScanningSchemaServiceProvider> service =
+                ServiceLoader.load(ScanningSchemaServiceProvider.class);
+        final Iterator<ScanningSchemaServiceProvider> iterator = service.iterator();
+        final ScanningSchemaServiceProvider schemaService = iterator.next();
+        assertNotNull(schemaService);
+
+        schemaService.registerAvalaibleYangs(yangs);
+
+        assertNotNull(schemaService.getGlobalContext());
+    }
+}
