6eb9fce739df8e7c298ea6b6f12218a25aa34ab2
Jie Han <han.jie@zte.com.cn>
2017-08-29 10:10:31 +0800

    Binding codec v2 - fix getter name of boolean/empty and its type defination
    - Move the fix of
      Bug 8903 - binding-data-codec does not work with boolean type using typedef
      https://git.opendaylight.org/gerrit/62389
      to binding2 codec.
    Change-Id: I14820662036aa3536ebfd32ea5d6af2c92d0f995
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

---
+++ binding2/mdsal-binding2-dom-codec/src/test/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/TypedefTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.javav2.dom.codec.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map.Entry;
+import javassist.ClassPool;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.javav2.dom.codec.generator.impl.StreamWriterGenerator;
+import org.opendaylight.mdsal.binding.javav2.runtime.javassist.JavassistUtils;
+import org.opendaylight.mdsal.binding.javav2.spec.base.InstanceIdentifier;
+import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
+import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.data.DefaultPolicy;
+import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.dto.DefaultPolicyBuilder;
+import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.type.PolicyLoggingFlag;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+
+public class TypedefTest extends AbstractBindingRuntimeTest {
+
+    private static final InstanceIdentifier<DefaultPolicy> BA_DEFAULT_POLICY =
+            InstanceIdentifier.builder(DefaultPolicy.class).build();
+    private BindingNormalizedNodeCodecRegistry registry;
+
+    @Override
+    public void setup() {
+        super.setup();
+        final JavassistUtils utils = JavassistUtils.forClassPool(ClassPool.getDefault());
+        registry = new BindingNormalizedNodeCodecRegistry(StreamWriterGenerator.create(utils));
+        registry.onBindingRuntimeContextUpdated(getRuntimeContext());
+    }
+
+    @Test
+    public void testTypedef() {
+        DefaultPolicy binding = new DefaultPolicyBuilder()
+                .setAction(true)
+                .setAction2(new PolicyLoggingFlag(false))
+                .setAction3(true)
+                .build();
+        final Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> dom =
+                registry.toNormalizedNode(BA_DEFAULT_POLICY, binding);
+        final Entry<InstanceIdentifier<?>, TreeNode> readed =
+                registry.fromNormalizedNode(dom.getKey(),dom.getValue());
+
+        assertEquals(binding,readed.getValue());
+
+    }
+}
