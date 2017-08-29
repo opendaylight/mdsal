5c793c9e44b066b0dcaced954d1baab7a6b0ab93
Jie Han <han.jie@zte.com.cn>
2017-08-29 18:55:15 +0800

    Binding v2 - Map 'type empty' to yangtools.yang.common.Empty
    - make generator map Empty to empty leaf and use prefix 'get'
      for getter methos.
    - fix codec for type Empty and its derived type correspondingly.
    
    Change-Id: Icbc5c729ce4f035323fffc775f4943bc7ce50989
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

--- binding2/mdsal-binding2-dom-codec/src/test/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/TypedefTest.java
+++ binding2/mdsal-binding2-dom-codec/src/test/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/TypedefTest.java
@@ -8,6 +8,7 @@
 package org.opendaylight.mdsal.binding.javav2.dom.codec.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes.leafNode;
 
 import java.util.Map.Entry;
 import javassist.ClassPool;
@@ -17,16 +18,32 @@ import org.opendaylight.mdsal.binding.javav2.runtime.javassist.JavassistUtils;
 import org.opendaylight.mdsal.binding.javav2.spec.base.InstanceIdentifier;
 import org.opendaylight.mdsal.binding.javav2.spec.base.TreeNode;
 import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.data.DefaultPolicy;
+import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.data.TestCont;
 import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.dto.DefaultPolicyBuilder;
+import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.dto.TestContBuilder;
 import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.type.PolicyLoggingFlag;
+import org.opendaylight.mdsal.gen.javav2.urn.test.simple.test.typedef.rev170829.type.TypedefEmpty;
+import org.opendaylight.yangtools.yang.common.Empty;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
 
 
 public class TypedefTest extends AbstractBindingRuntimeTest {
 
     private static final InstanceIdentifier<DefaultPolicy> BA_DEFAULT_POLICY =
             InstanceIdentifier.builder(DefaultPolicy.class).build();
+    private static final InstanceIdentifier<TestCont> BA_TEST_CONT =
+            InstanceIdentifier.builder(TestCont.class).build();
+
+    private static final QName EMPTY_LEAF = QName.create(TestCont.QNAME, "empty-leaf");
+    private static final QName EMPTY_LEAF2 = QName.create(TestCont.QNAME, "empty-leaf2");
+    private static final QName EMPTY_LEAF3 = QName.create(TestCont.QNAME, "empty-leaf3");
+    private static final YangInstanceIdentifier BI_TESTCONT_PATH = YangInstanceIdentifier.of(TestCont.QNAME);
+
     private BindingNormalizedNodeCodecRegistry registry;
 
     @Override
@@ -51,5 +68,38 @@ public class TypedefTest extends AbstractBindingRuntimeTest {
 
         assertEquals(binding,readed.getValue());
 
+    }
+
+    private static TestCont getTypedefEmptyTypeBindingData() {
+        return new TestContBuilder()
+            .setEmptyLeaf(Empty.getInstance())
+            .setEmptyLeaf2(TypedefEmpty.getDefaultInstance())
+            .setEmptyLeaf3(Empty.getInstance())
+            .build();
+    }
+
+    private static ContainerNode getTypedefEmptyTypeNormalizedData() {
+        return ImmutableContainerNodeBuilder.create()
+            .withNodeIdentifier(new NodeIdentifier(TestCont.QNAME))
+            .withChild(leafNode(EMPTY_LEAF, Empty.getInstance()))
+            .withChild(leafNode(EMPTY_LEAF2, Empty.getInstance()))
+            .withChild(leafNode(EMPTY_LEAF3, Empty.getInstance()))
+            .build();
+    }
+
+    @Test
+    public void testTypedefEmptyTypeToNormalizedData() {
+        final Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> dom =
+            registry.toNormalizedNode(BA_TEST_CONT, getTypedefEmptyTypeBindingData());
+
+        assertEquals(getTypedefEmptyTypeNormalizedData(), dom.getValue());
+    }
+
+    @Test
+    public void testTypedefEmptyTypeFromNormalizedData() {
+        final Entry<InstanceIdentifier<?>, TreeNode> readed =
+            registry.fromNormalizedNode(BI_TESTCONT_PATH, getTypedefEmptyTypeNormalizedData());
+
+        assertEquals(getTypedefEmptyTypeBindingData(), readed.getValue());
     }
 }
