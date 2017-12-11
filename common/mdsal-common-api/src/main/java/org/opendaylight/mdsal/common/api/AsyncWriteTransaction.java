62c9f82d3b57edf8f6b9dd7ac10d74273dd81870
Michael Vorburger <vorburger@redhat.com>
2017-12-11 23:17:16 +0100

    AsyncWriteTransaction submit() with @CheckReturnValue
    this seems like a good idea because we would subsequently like to reduce
    the internal logging from a WARN for TransactionCommitFailedException to
    a DEBUG.
    
    see CONTROLLER-1802 and NETVIRT-916 for the full background story to
    where this comes from.
    
    Change-Id: I2de692dfc4d1b2de7d10a56e4341c153214da62d
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- common/mdsal-common-api/src/main/java/org/opendaylight/mdsal/common/api/AsyncWriteTransaction.java
+++ common/mdsal-common-api/src/main/java/org/opendaylight/mdsal/common/api/AsyncWriteTransaction.java
@@ -468,6 +468,7 @@ public interface AsyncWriteTransaction<P extends Path<P>, D> extends AsyncTransa
      * @deprecated Use {@link #commit()} instead.
      */
     @Deprecated
+    @CheckReturnValue
     CheckedFuture<Void, TransactionCommitFailedException> submit();
 
     /**
