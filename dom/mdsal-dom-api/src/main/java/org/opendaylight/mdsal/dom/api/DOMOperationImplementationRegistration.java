39a01583ee3413948ebacc1d980aa0f05c7612c4
Jie Han <han.jie@zte.com.cn>
2018-01-09 10:57:57 +0800

    DOM API - add DOMOperationProviderService
    - This interface would replace DOMRpcProviderService later.
    Change-Id: I44eae7fc90399acb537fa6874c704279f104cfbf
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

--- dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMOperationImplementationRegistration.java
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMOperationImplementationRegistration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -10,13 +10,13 @@ package org.opendaylight.mdsal.dom.api;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
 
 /**
- * A registration of a {@link DOMRpcImplementation}. Used to track and revoke a registration
- * with a {@link DOMRpcProviderService}.
+ * A registration of a {@link DOMOperationImplementation}. Used to track and revoke a registration
+ * with a {@link DOMOperationProviderService}.
  *
  * @param <T> RPC implementation type
  */
-@Deprecated
-public interface DOMRpcImplementationRegistration<T extends DOMRpcImplementation> extends ObjectRegistration<T> {
+public interface DOMOperationImplementationRegistration<T extends DOMOperationImplementation>
+        extends ObjectRegistration<T> {
     @Override
     void close();
 }
