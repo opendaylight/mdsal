b90ef7fcd80e8dc3b45ec062a384ec44c5a06460
Michael Vorburger <vorburger@redhat.com>
2018-01-18 03:15:23 +0100

    DataBrokerTestModule getBindingToNormalizedNodeCodec() & Co.
    need these exposed for a new standalone component style test  I'm on
    
    JIRA: MDSAL-556
    Change-Id: I5efd3a6e507d46d035067a561aaca7c2f80f51e6
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 8a6e5f0946f5ed60f7554c51e7621e2ac3e41edd)

--- binding/mdsal-binding-test-utils/src/main/java/org/opendaylight/mdsal/binding/testutils/DataBrokerTestModule.java
+++ binding/mdsal-binding-test-utils/src/main/java/org/opendaylight/mdsal/binding/testutils/DataBrokerTestModule.java
@@ -7,11 +7,20 @@
  */
 package org.opendaylight.mdsal.binding.testutils;
 
+import com.google.common.annotations.Beta;
 import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.dom.adapter.CurrentAdapterSerializer;
 import org.opendaylight.mdsal.binding.dom.adapter.test.AbstractConcurrentDataBrokerTest;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.mdsal.dom.broker.DOMNotificationRouter;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextProvider;
 
+@Beta
 public class DataBrokerTestModule {
     private final boolean useMTDataTreeChangeListenerExecutor;
+
+    private AbstractConcurrentDataBrokerTest dataBrokerTest;
 
     public DataBrokerTestModule(final boolean useMTDataTreeChangeListenerExecutor) {
         this.useMTDataTreeChangeListenerExecutor = useMTDataTreeChangeListenerExecutor;
@@ -30,12 +39,31 @@ public class DataBrokerTestModule {
             // into this DataBrokerTestModule, and make AbstractDataBrokerTest
             // use it, instead of the way around it currently is (the opposite);
             // this is just for historical reasons... and works for now.
-            AbstractConcurrentDataBrokerTest dataBrokerTest =
-                    new AbstractConcurrentDataBrokerTest(useMTDataTreeChangeListenerExecutor) { };
+            dataBrokerTest = new AbstractConcurrentDataBrokerTest(useMTDataTreeChangeListenerExecutor) { };
             dataBrokerTest.setup();
             return dataBrokerTest.getDataBroker();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public DOMDataBroker getDOMDataBroker() {
+        return dataBrokerTest.getDomBroker();
+    }
+
+    public CurrentAdapterSerializer getBindingToNormalizedNodeCodec() {
+        return dataBrokerTest.getDataBrokerTestCustomizer().getAdapterContext().currentSerializer();
+    }
+
+    public DOMNotificationRouter getDOMNotificationRouter() {
+        return dataBrokerTest.getDataBrokerTestCustomizer().getDomNotificationRouter();
+    }
+
+    public DOMSchemaService getSchemaService() {
+        return dataBrokerTest.getDataBrokerTestCustomizer().getSchemaService();
+    }
+
+    public SchemaContextProvider getSchemaContextProvider() {
+        return dataBrokerTest.getDataBrokerTestCustomizer().getSchemaService()::getGlobalContext;
     }
 }
