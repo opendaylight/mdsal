0887cb9aa8dfdd1c2a91b2085876f388181d816b
Robert Varga <robert.varga@pantheon.tech>
2018-01-26 13:24:54 +0100

    MDSAL-302: make sure uses+augment works in RPCs
    Add missing callouts to generate uses augments for RPC input/output
    nodes. This complements processing for data nodes and notifications.
    
    Change-Id: I4f0d9fddce1e019c57b9a45874a45b22ffe6a15f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal302Test.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Mdsal302Test {
+
+    @Test
+    public void bug4145Test() {
+        SchemaContext context = YangParserTestUtils.parseYangResource("/mdsal-302/mdsal-302.yang");
+
+        List<Type> generateTypes = new BindingGeneratorImpl(false).generateTypes(context);
+        assertNotNull(generateTypes);
+        assertEquals(11, generateTypes.size());
+    }
+}
