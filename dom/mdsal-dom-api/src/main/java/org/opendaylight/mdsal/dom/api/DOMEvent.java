fdd4466fe4f88b7daa7c55c9bd0dc9c57038e8a6
Robert Varga <robert.varga@pantheon.tech>
2018-01-30 16:58:54 +0100

    MDSAL-206: improve DOMEvent instant resolution
    Rather than using a Date, use an java.time.Instant, which is more
    appropriate.
    
    Change-Id: I45513f12f83065d9fc7016e357c53332d0070d6e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMEvent.java
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMEvent.java
@@ -5,20 +5,18 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.mdsal.dom.api;
 
-import java.util.Date;
+import java.time.Instant;
 
 /**
  * Generic event interface.
  */
 public interface DOMEvent {
-
     /**
      * Get the time of the event occurrence.
      *
      * @return the event time
      */
-    Date getEventTime();
+    Instant getEventInstant();
 }
