cbfca9a10dc18d5f0f3679d2fb057d00aa3b5a18
Robert Varga <robert.varga@pantheon.tech>
2018-02-28 23:51:02 +0100

    Export declared regexes to generated code
    This patch exports declared pattern regular expressions to binding
    generators, thus allowing them to report violations using the declared
    string rather than the mangled Pattern string we use to implement
    enforcement.
    
    As it turns out there is place for improvement with generated code
    size, as decoding pattern negation requires a bit of repetition
    as well as needing to capture additional strings. Since we can take
    advantage of common code library, do not encapsulate single expressions
    in an array.
    
    This patch battles those ill effects by exposing CodeHelpers class
    from yang-binding, which hosts common code blocks for reuse by
    generated code. This means classes generated by this and newer codegen
    plugins require a matching or newer version of yang-binding at runtime.
    
    JIRA: MDSAL-313
    Change-Id: I8abb33660c5182703f2db5faf0eee7e08ce15f06
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator-util/src/main/java/org/opendaylight/mdsal/binding/model/util/TypeConstants.java
+++ binding/mdsal-binding-generator-util/src/main/java/org/opendaylight/mdsal/binding/model/util/TypeConstants.java
@@ -8,14 +8,13 @@
 package org.opendaylight.mdsal.binding.model.util;
 
 /**
- *
  * Contains constants used in relations with <code>Type</code>.
  */
 public final class TypeConstants {
 
     /**
-     * Name of the class constant which hold list of the regular expression
-     * strings.
+     * Name of the class constant which holds the map of regular expressions that need to be enforced on the string
+     * value. The map is keyed by Pattern-compatible string and values are XSD-compatible strings.
      */
     public static final String PATTERN_CONSTANT_NAME = "PATTERN_CONSTANTS";
 
