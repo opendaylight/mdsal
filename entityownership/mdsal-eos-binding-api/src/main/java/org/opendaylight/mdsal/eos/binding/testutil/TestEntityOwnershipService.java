3c57ec3ef8215200408584553f998c5c701be3ef
Faseela K <faseela.k@ericsson.com>
2018-03-01 23:21:21 +0530

    TestEntityOwnershipService for component tests
    Change-Id: I73a5ff1c3e2707feccf025f5b6555646b0faa63a
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ entityownership/mdsal-eos-binding-api/src/main/java/org/opendaylight/mdsal/eos/binding/testutil/TestEntityOwnershipService.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.eos.binding.testutil;
+
+import com.google.common.base.Optional;
+
+import org.mockito.Mockito;
+import org.opendaylight.mdsal.eos.binding.api.Entity;
+import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipCandidateRegistration;
+import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipListener;
+import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipListenerRegistration;
+import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipService;
+import org.opendaylight.mdsal.eos.common.api.EntityOwnershipState;
+
+/**
+ * Fake EntityOwnershipService suitable for non-clustered component tests.
+ *
+ */
+public abstract class TestEntityOwnershipService implements EntityOwnershipService {
+
+    private static final EntityOwnershipState STATE = EntityOwnershipState.from(true, true);
+
+    public static EntityOwnershipService newInstance() {
+        return Mockito.mock(TestEntityOwnershipService.class);
+    }
+
+    @Override
+    public EntityOwnershipCandidateRegistration registerCandidate(Entity entity) {
+        return Mockito.mock(EntityOwnershipCandidateRegistration.class);
+    }
+
+    @Override
+    public EntityOwnershipListenerRegistration registerListener(String entityType, EntityOwnershipListener listener) {
+        return Mockito.mock(EntityOwnershipListenerRegistration.class);
+    }
+
+    @Override
+    public Optional<EntityOwnershipState> getOwnershipState(Entity forEntity) {
+        return Optional.of(STATE);
+    }
+
+    @Override
+    public boolean isCandidateRegistered(Entity entity) {
+        return true;
+    }
+}
