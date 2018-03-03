636ac0055012e2ee436d5526a95640c6df35e46a
Robert Varga <robert.varga@pantheon.tech>
2018-03-03 17:36:07 +0100

    Refactor GeneratedTOBuilderImpl
    This builder is used at both runtime and codegen-time and it captures
    information irrelevant to runtime, leading to bloat.
    
    Refactor this class into three classes:
    - AbstractGeneratedTypeBuilder for common information
    - CodegenGeneratedTOBuilder common + codegen information
    - RuntimeGeneratedTOBuilder throwing UnsupportedOperationException
      for codegen information
    
    JIRA: MDSAL-315
    Change-Id: Ie1d3d075590334cb5b8ce41c059ac2e10cf02d56
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-util/src/main/java/org/opendaylight/mdsal/binding/model/util/generated/type/builder/CodegenGeneratedTOBuilder.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.model.util.generated.type.builder;
+
+import org.opendaylight.mdsal.binding.model.api.GeneratedProperty;
+import org.opendaylight.mdsal.binding.model.api.GeneratedTransferObject;
+import org.opendaylight.mdsal.binding.model.api.Restrictions;
+import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedPropertyBuilder;
+import org.opendaylight.yangtools.yang.common.QName;
+
+public final class CodegenGeneratedTOBuilder extends AbstractGeneratedTOBuilder {
+
+    private Restrictions restrictions;
+    private GeneratedPropertyBuilder SUID;
+    private String reference;
+    private String description;
+    private String moduleName;
+    private Iterable<QName> schemaPath;
+
+    public CodegenGeneratedTOBuilder(final String packageName, final String name) {
+        super(packageName, name);
+    }
+
+    @Override
+    public void setRestrictions(final Restrictions restrictions) {
+        this.restrictions = restrictions;
+    }
+
+    @Override
+    public void setSUID(final GeneratedPropertyBuilder suid) {
+        this.SUID = suid;
+    }
+
+    @Override
+    public GeneratedTransferObject toInstance() {
+        return new GTO(this);
+    }
+
+    @Override
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    @Override
+    public void setModuleName(final String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    @Override
+    public void setSchemaPath(final Iterable<QName> schemaPath) {
+        this.schemaPath = schemaPath;
+    }
+
+    @Override
+    public void setReference(final String reference) {
+        this.reference = reference;
+    }
+
+    private static final class GTO extends AbstractGeneratedTransferObject {
+        private final Restrictions restrictions;
+        private final GeneratedProperty SUID;
+        private final String reference;
+        private final String description;
+        private final String moduleName;
+        private final Iterable<QName> schemaPath;
+
+        public GTO(final CodegenGeneratedTOBuilder builder) {
+            super(builder);
+            this.restrictions = builder.restrictions;
+            this.reference = builder.reference;
+            this.description = builder.description;
+            this.moduleName = builder.moduleName;
+            this.schemaPath = builder.schemaPath;
+
+            if (builder.SUID == null) {
+                this.SUID = null;
+            } else {
+                this.SUID = builder.SUID.toInstance(GTO.this);
+            }
+        }
+
+        @Override
+        public Restrictions getRestrictions() {
+            return this.restrictions;
+        }
+
+        @Override
+        public GeneratedProperty getSUID() {
+            return this.SUID;
+        }
+
+        @Override
+        public String getDescription() {
+            return this.description;
+        }
+
+        @Override
+        public String getReference() {
+            return this.reference;
+        }
+
+        @Override
+        public Iterable<QName> getSchemaPath() {
+            return this.schemaPath;
+        }
+
+        @Override
+        public String getModuleName() {
+            return this.moduleName;
+        }
+    }
+}
