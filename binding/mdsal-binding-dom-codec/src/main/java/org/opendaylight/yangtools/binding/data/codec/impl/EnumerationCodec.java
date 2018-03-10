4ee95c01e4368508699c18cc02e282c2ea7f844c
Robert Varga <robert.varga@pantheon.tech>
2018-03-10 09:47:52 +0100

    Add Enumeration yang-binding interface
    All generated enumerations expose two methods. Capture and document
    them in an interface and adjust the template accordingly. Also adds
    a forName() static factory method, to keep symmetry with forValue().
    
    EnumerationCodec is updated to take advantage of the new interface
    and to validate schema/binding assigned name consistency.
    
    JIRA: MDSAL-317
    Change-Id: Iebddded0a4e1ec3534e87a878a8f571cd45ebbae
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 9f510860d74c56632e56073c9e4fc9848bd3aee9)

--- binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/EnumerationCodec.java
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/EnumerationCodec.java
@@ -8,20 +8,23 @@
 package org.opendaylight.yangtools.binding.data.codec.impl;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableBiMap.Builder;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 import org.opendaylight.yangtools.binding.data.codec.impl.ValueTypeCodec.SchemaUnawareCodec;
-import org.opendaylight.yangtools.yang.binding.BindingMapping;
+import org.opendaylight.yangtools.yang.binding.Enumeration;
 import org.opendaylight.yangtools.yang.model.api.type.EnumTypeDefinition;
 import org.opendaylight.yangtools.yang.model.api.type.EnumTypeDefinition.EnumPair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class EnumerationCodec extends ReflectionBasedCodec implements SchemaUnawareCodec {
+    private static final Logger LOG = LoggerFactory.getLogger(EnumerationCodec.class);
+
     private final ImmutableBiMap<String, Enum<?>> yangValueToBinding;
 
     EnumerationCodec(final Class<? extends Enum<?>> enumeration, final ImmutableBiMap<String, Enum<?>> schema) {
@@ -34,18 +37,30 @@ final class EnumerationCodec extends ReflectionBasedCodec implements SchemaUnawa
         @SuppressWarnings({ "rawtypes", "unchecked" })
         final Class<? extends Enum<?>> enumType = (Class) returnType;
         return () -> {
-            final BiMap<String, String> identifierToYang = BindingMapping.mapEnumAssignedNames(
-                enumSchema.getValues().stream().map(EnumPair::getName).collect(Collectors.toList())).inverse();
-
             final Builder<String, Enum<?>> builder = ImmutableBiMap.builder();
             for (Enum<?> enumValue : enumType.getEnumConstants()) {
-                final String yangName = identifierToYang.get(enumValue.name());
-                checkState(yangName != null, "Failed to find enumeration constant %s in mapping %s", enumValue,
-                        identifierToYang);
-                builder.put(yangName, enumValue);
+                checkArgument(enumValue instanceof Enumeration,
+                    "Enumeration constant %s is not implementing Enumeration", enumValue);
+                builder.put(((Enumeration) enumValue).getName(), enumValue);
+            }
+            final ImmutableBiMap<String, Enum<?>> mapping = builder.build();
+
+            // Check if mapping is a bijection
+            final Set<String> assignedNames =  enumSchema.getValues().stream().map(EnumPair::getName)
+                    .collect(Collectors.toSet());
+            for (String name : assignedNames) {
+                if (!mapping.containsKey(name)) {
+                    LOG.warn("Enumeration {} does not contain assigned name '{}' from {}", enumType, name, enumSchema);
+                }
+            }
+            for (String name : mapping.keySet()) {
+                if (!assignedNames.contains(name)) {
+                    LOG.warn("Enumeration {} contains assigned name '{}' not covered by {}", enumType, name,
+                        enumSchema);
+                }
             }
 
-            return new EnumerationCodec(enumType, builder.build());
+            return new EnumerationCodec(enumType, mapping);
         };
     }
 
