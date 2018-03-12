858fd741083c29a3adcb6f162f3f2eb1ec169aa6
Robert Varga <robert.varga@pantheon.tech>
2018-03-12 11:53:51 +0100

    Use QName.withModule()
    There are multiple places where we are creating a well-known QName
    with a new namespace. Use QName.withModule() so we do not end up
    checking localName over and over.
    
    Change-Id: I932bf3762bfb6ae201e6b309f8a7d95887b99b00
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/util/BindingReflections.java
+++ binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/util/BindingReflections.java
@@ -48,6 +48,7 @@ import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -516,10 +517,10 @@ public final class BindingReflections {
             } else if (isRpcType(key)) {
                 final String className = key.getSimpleName();
                 if (className.endsWith(BindingMapping.RPC_OUTPUT_SUFFIX)) {
-                    return QName.create(module, "output").intern();
-                } else {
-                    return QName.create(module, "input").intern();
+                    return YangConstants.operationOutputQName(module.getModule()).intern();
                 }
+
+                return YangConstants.operationInputQName(module.getModule()).intern();
             }
 
             /*
@@ -527,7 +528,6 @@ public final class BindingReflections {
              */
             return module;
         }
-
     }
 
     /**
