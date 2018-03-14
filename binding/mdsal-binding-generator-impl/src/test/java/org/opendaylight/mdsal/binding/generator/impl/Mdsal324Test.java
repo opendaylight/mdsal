ae479b08e008168d73cbb6949b705d7dd3bc5227
Robert Varga <robert.varga@pantheon.tech>
2018-03-14 11:29:08 +0100

    Change nested type enumeration tracking
    Checking for numeral suffix encroaches on model-defined names,
    for example in 'leaf foo1', we end up assuming the number is coming
    from our code, leading to weird renaming effects.
    
    Isolate our counter with '$', which cannot come from the user, hence
    nesting of 'leaf foo1' becomes 'Foo1$1' instead of 'Foo2'.
    
    JIRA: MDSAL-324
    Change-Id: Ica309c6c3da012abdf1d5bb9237544f555b7b1cd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal324Test.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Mdsal324Test {
+
+    @Test
+    public void mdsal324Test() {
+        final SchemaContext context = YangParserTestUtils.parseYangResource("/mdsal324.yang");
+        final List<Type> generateTypes = new BindingGeneratorImpl().generateTypes(context);
+        assertNotNull(generateTypes);
+        assertEquals(4, generateTypes.size());
+    }
+}
