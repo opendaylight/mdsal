bb5485dbed372356dfacb07bc4d7c40b9cc5c4ce
Robert Varga <robert.varga@pantheon.tech>
2018-03-21 03:57:21 +0100

    WIP: Generate code for action statements
    RFC7950 defines actions, which are similar to RPCs in that they
    can be invoked. This patch teaches binding codegen to generate
    the interfaces for them.
    
    This patch still needs some work to emit reasonable code and
    also to define ActionService binding interface.
    
    JIRA: MDSAL-300
    Change-Id: I8c2f9566e25948c8604d76ce52d86304b0f8be03
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal300Test.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Mdsal300Test {
+
+    @Test
+    public void testActionsAndAugmentsTest() {
+        SchemaContext context = YangParserTestUtils.parseYangResourceDirectory("/mdsal-300");
+
+        List<Type> generateTypes = new BindingGeneratorImpl().generateTypes(context);
+        assertNotNull(generateTypes);
+        assertEquals(6, generateTypes.size());
+    }
+}
