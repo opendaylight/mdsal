6aa576b547ce659dfa7bc61bc7a3f58cee0e2b4e
Jie Han <han.jie@zte.com.cn>
2018-03-26 17:15:51 +0800

    Binding specification should generate code without revision captured in package name
    - Support semantic version, if SemVer is present,
      then remove revYYMMDD from package name and append
      major version to package name, e.g.:
      semantic version is '5.4.6', then it would be mapped to
      "semver5".
    
    Change-Id: I5c0b689af2da00475fb9e19aac60cb5476bf6ae1
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

---
+++ common/mdsal-binding-common/src/test/java/org/opendaylight/mdsal/binding/common/BindingMappingBaseTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018 ZTE inc, and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.common;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.URI;
+import java.util.Optional;
+import org.junit.Test;
+import org.opendaylight.yangtools.concepts.SemVer;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.Revision;
+
+public class BindingMappingBaseTest {
+
+    @Test
+    public void basicTest() {
+        assertTrue(BindingMappingBase.getRawRootPackageName(QNameModule.create(URI.create("test:URI"),
+            Revision.of("2017-10-26")), Optional.of(SemVer.valueOf("0.0.1")), "gen.prefix")
+            .equals("gen.prefix.test.URI.semver0"));
+        assertTrue(BindingMappingBase.getRawRootPackageName(QNameModule.create(URI.create("test:URI"),
+            Revision.of("2017-10-26")), Optional.empty(), "gen.prefix")
+            .equals("gen.prefix.test.URI.rev171026"));
+    }
+
+}
