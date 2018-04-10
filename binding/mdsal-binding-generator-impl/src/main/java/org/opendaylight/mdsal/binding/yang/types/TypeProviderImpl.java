1b4812628fa0389e7f4f4b62a97c5806e3534a12
Robert Varga <robert.varga@pantheon.tech>
2018-04-10 19:14:19 +0200

    Fix derived types missing pattern restrictions
    We have missed a place where constants to support restrictions need
    to be generated, which meant that Ipv4AddressNoZone and similar
    constructs were not doing correct enforcement.
    
    JIRA: MDSAL-335
    Change-Id: Id659362038e001207eab9ca5f1618c376aa0ea5a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit a2713a39a1b1aac6a2337a09559f930240523311)

--- binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/yang/types/TypeProviderImpl.java
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/yang/types/TypeProviderImpl.java
@@ -1285,6 +1285,8 @@ public final class TypeProviderImpl implements TypeProvider {
         genTOBuilder.setTypedef(true);
         final Restrictions r = BindingGeneratorUtil.getRestrictions(typedef);
         genTOBuilder.setRestrictions(r);
+        addStringRegExAsConstant(genTOBuilder, resolveRegExpressionsFromTypedef(typedef));
+
         if (typedef.getStatus() == Status.DEPRECATED) {
             genTOBuilder.addAnnotation("", "Deprecated");
         }
