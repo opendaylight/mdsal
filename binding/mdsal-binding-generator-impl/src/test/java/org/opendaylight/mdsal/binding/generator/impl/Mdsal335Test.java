f58468e8990cffe33959b65a6014b63e183adb24
Robert Varga <robert.varga@pantheon.tech>
2018-04-10 19:14:19 +0200

    Fix derived types missing pattern restrictions
    We have missed a place where constants to support restrictions need
    to be generated, which meant that Ipv4AddressNoZone and similar
    constructs were not doing correct enforcement.
    
    JIRA: MDSAL-335
    Change-Id: Id659362038e001207eab9ca5f1618c376aa0ea5a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit a2713a39a1b1aac6a2337a09559f930240523311)

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal335Test.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Mdsal335Test {
+
+    @Test
+    public void mdsal335Test() throws FileNotFoundException, ReactorException, URISyntaxException {
+        final SchemaContext context = YangParserTestUtils.parseYangSource("/mdsal335.yang");
+
+        final List<Type> generateTypes = new BindingGeneratorImpl(false).generateTypes(context);
+        assertNotNull(generateTypes);
+        assertEquals(2, generateTypes.size());
+
+        final Type nozoneType = generateTypes.stream().filter(type -> type.getFullyQualifiedName()
+            .equals("org.opendaylight.yang.gen.v1.mdsal335.norev.Ipv4AddressNoZone")).findFirst().get();
+
+        assertTrue(nozoneType instanceof GeneratedType);
+        final GeneratedType gen = (GeneratedType) nozoneType;
+        assertEquals(1, gen.getConstantDefinitions().size());
+    }
+}
