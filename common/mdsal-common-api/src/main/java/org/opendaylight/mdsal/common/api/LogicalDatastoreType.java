8448b95a6eebf87d7b82a9b9bf05dca320700e17
Robert Varga <robert.varga@pantheon.tech>
2018-04-19 22:37:01 +0200

    Document LogicalDatastoreType
    This adds some sorely-needed javadocs.
    
    Change-Id: Ief1797b7f63c6b85e53ee0e485453b0dbb07b95c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/mdsal-common-api/src/main/java/org/opendaylight/mdsal/common/api/LogicalDatastoreType.java
+++ common/mdsal-common-api/src/main/java/org/opendaylight/mdsal/common/api/LogicalDatastoreType.java
@@ -7,23 +7,41 @@
  */
 package org.opendaylight.mdsal.common.api;
 
+/**
+ * The concept of a logical data store, similar to RFC8342.
+ */
+// FIXME: 3.0.0: turn this into an interface so it can be externally-defined?
+// FIXME: 3.0.0: note that mount points can have different types and policies, which can potentially be mapped
 public enum LogicalDatastoreType {
-
     /**
-     * Logical atastore representing operational state of the system
-     * and it's components
-     * This datastore is used to describe operational state of
-     * the system and it's operation related data.
+     * Logical datastore representing operational state of the system and it's components. This datastore is used
+     * to describe operational state of the system and it's operation related data.
      *
+     * <p>
+     * It is defined to:
+     * <ul>
+     *   <li>contain both {@code config=true} and {@code config=false} nodes</li>
+     *   <li>be replicated across all nodes by default, individual shards may have different strategies, which need to
+     *       be documented
+     *   </li>
+     * </ul>
      */
     OPERATIONAL,
     /**
-     * Logical Datastore representing configuration state of the system
-     * and it's components.
-     * This datastore is used to describe intended state of
-     * the system and intended operation mode.
+     * Logical Datastore representing configuration state of the system and it's components. This datastore is used
+     * to describe intended state of the system and intended operation mode.
      *
+     * <p>
+     * It is defined to:
+     * <ul>
+     *   <li>contain only {@code config=true} nodes</li>
+     *   <li>be replicated across all nodes by default, individual shards may have different strategies, which need to
+     *       be documented
+     *   </li>
+     *   <li>be persisted on all nodes by default, individual shards may have different strategies, which need to
+     *       be documented
+     *   </li>
+     * </ul>
      */
     CONFIGURATION
-
 }
