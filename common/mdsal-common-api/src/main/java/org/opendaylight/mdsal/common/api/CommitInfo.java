d74efc9a282c694a988bafe5f1305bf8494444a1
Robert Varga <robert.varga@pantheon.tech>
2018-04-23 01:29:58 +0200

    Add CommitInfo and return it from transactions
    This patch introduces AsyncWriteTransaction.commit(), which
    returns a FluentFuture instead of a CheckedFuture.
    
    Since it may be useful for implementations to communicate some
    post-commit state, the returned future is specified to return
    a non-null CommitInfo. Utility empty CommitInfo implementation
    is provided.
    
    JIRA: MDSAL-337
    Change-Id: I1c4bd007781515643e602a180d162e35e92116a1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/mdsal-common-api/src/main/java/org/opendaylight/mdsal/common/api/CommitInfo.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.common.api;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.FluentFuture;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Base interface for tagging information about a successful commit. This can include various ways of identifying
+ * the resulting changeset, timing information or any other piece of data about the commit itself the implementation
+ * deems interesting to the client.
+ *
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+public interface CommitInfo {
+    /**
+     * Return an empty {@link CommitInfo}.
+     *
+     * @return An empty {@link CommitInfo} instance.
+     */
+    static CommitInfo empty() {
+        return EmptyCommitInfo.INSTANCE;
+    }
+
+    /**
+     * Return an immediately-completed empty {@link CommitInfo} future.
+     *
+     * @return An empty {@link CommitInfo} instance enclosed in a completed future.
+     */
+    static FluentFuture<CommitInfo> emptyFluentFuture() {
+        return EmptyCommitInfo.FLUENT_INSTANCE;
+    }
+}
