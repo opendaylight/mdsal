59972d23cd2711821b64bb3f1f816550e14da85f
Robert Varga <robert.varga@pantheon.tech>
2018-05-28 17:37:21 +0200

    WIP: Allow DataObjectModification to address grouping nodes
    This adds the API and implementation required to address child
    nodes introduced in cases by the use of a grouping.
    
    Change-Id: I70871dcaa3441fbef81dfb15bfd965d133aa0894
    JIRA: MDSAL-342
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/DataObjectModification.java
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/DataObjectModification.java
@@ -13,42 +13,34 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.opendaylight.yangtools.yang.binding.Augmentation;
 import org.opendaylight.yangtools.yang.binding.ChildOf;
+import org.opendaylight.yangtools.yang.binding.ChoiceIn;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.Identifiable;
 import org.opendaylight.yangtools.yang.binding.Identifier;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.PathArgument;
 
 /**
- * Modified Data Object.
+ * Modified Data Object. Represents modification of Data Object
  *
- *<p>
- * Represents modification of Data Object
- *
- *<p>
  * @param <T> Type of modified object
- *
  */
 public interface DataObjectModification<T extends DataObject> extends
         org.opendaylight.yangtools.concepts.Identifiable<PathArgument> {
 
     /**
-     * Represents type of modification which has occured.
-     *
+     * Represents type of modification which has occurred.
      */
     enum ModificationType {
         /**
          * Child node (direct or indirect) was modified.
-         *
          */
         SUBTREE_MODIFIED,
         /**
          * Node was explicitly created / overwritten.
-         *
          */
         WRITE,
         /**
          * Node was deleted.
-         *
          */
         DELETE
     }
@@ -105,10 +97,39 @@ public interface DataObjectModification<T extends DataObject> extends
             @Nonnull Class<C> childType);
 
     /**
+     * Returns child list item modification if {@code child} was modified by this modification. This method should be
+     * used if the child is defined in a grouping brought into a case inside this object.
+     *
+     * @param caseType Case type class
+     * @param childType Type of list item - must be list item with key
+     * @return Modification of {@code child} if {@code child} was modified, null otherwise.
+     * @throws IllegalArgumentException If supplied {@code childType} class is not valid child according
+     *         to generated model.
+     */
+    <H extends ChoiceIn<? super T> & DataObject, C extends ChildOf<? super H>> Collection<DataObjectModification<C>>
+            getModifiedChildren(@Nonnull Class<H> caseType, @Nonnull Class<C> childType);
+
+    /**
+     * Returns container child modification if {@code child} was modified by this modification. This method should be
+     * used if the child is defined in a grouping brought into a case inside this object.
+     *
+     * <p>
+     * For accessing all modified list items consider iterating over {@link #getModifiedChildren()}.
+     *
+     * @param caseType Case type class
+     * @param child Type of child - must be only container
+     * @return Modification of {@code child} if {@code child} was modified, null otherwise.
+     * @throws IllegalArgumentException If supplied {@code child} class is not valid child according
+     *         to generated model.
+     */
+    @Nullable <H extends ChoiceIn<? super T> & DataObject, C extends ChildOf<? super H>> DataObjectModification<C>
+            getModifiedChildContainer(@Nonnull Class<H> caseType, @Nonnull Class<C> child);
+
+    /**
      * Returns container child modification if {@code child} was modified by this
      * modification.
      *
-     *<p>
+     * <p>
      * For accessing all modified list items consider iterating over {@link #getModifiedChildren()}.
      *
      * @param child Type of child - must be only container
@@ -120,10 +141,9 @@ public interface DataObjectModification<T extends DataObject> extends
             @Nonnull Class<C> child);
 
     /**
-     * Returns augmentation child modification if {@code augmentation} was modified by this
-     * modification.
+     * Returns augmentation child modification if {@code augmentation} was modified by this modification.
      *
-     *<p>
+     * <p>
      * For accessing all modified list items consider iterating over {@link #getModifiedChildren()}.
      *
      * @param augmentation Type of augmentation - must be only container
@@ -134,6 +154,17 @@ public interface DataObjectModification<T extends DataObject> extends
     @Nullable <C extends Augmentation<T> & DataObject> DataObjectModification<C> getModifiedAugmentation(
             @Nonnull Class<C> augmentation);
 
+    /**
+     * Returns child list item modification if {@code child} was modified by this modification.
+     *
+     * @param listItem Type of list item - must be list item with key
+     * @param listKey List item key
+     * @return Modification of {@code child} if {@code child} was modified, null otherwise.
+     * @throws IllegalArgumentException If supplied {@code listItem} class is not valid child according
+     *         to generated model.
+     */
+    <N extends Identifiable<K> & ChildOf<? super T>, K extends Identifier<N>> DataObjectModification<N>
+            getModifiedChildListItem(@Nonnull Class<N> listItem, @Nonnull  K listKey);
 
     /**
      * Returns child list item modification if {@code child} was modified by this modification.
@@ -144,19 +175,18 @@ public interface DataObjectModification<T extends DataObject> extends
      * @throws IllegalArgumentException If supplied {@code listItem} class is not valid child according
      *         to generated model.
      */
-    <C extends Identifiable<K> & ChildOf<? super T>, K extends Identifier<C>> DataObjectModification<C>
-            getModifiedChildListItem(@Nonnull Class<C> listItem,@Nonnull  K listKey);
+    <H extends ChoiceIn<? super T> & DataObject, C extends Identifiable<K> & ChildOf<? super H>,
+            K extends Identifier<C>> DataObjectModification<C> getModifiedChildListItem(@Nonnull Class<H> caseType,
+                    @Nonnull Class<C> listItem, @Nonnull  K listKey);
 
     /**
-     * Returns a child modification if a node identified by {@code childArgument} was modified by
-     * this modification.
+     * Returns a child modification if a node identified by {@code childArgument} was modified by this modification.
      *
      * @param childArgument Path Argument of child node
      * @return Modification of child identified by {@code childArgument} if {@code childArgument}
      *         was modified, null otherwise.
      * @throws IllegalArgumentException If supplied path argument is not valid child according to
      *         generated model.
-     *
      */
     @Nullable DataObjectModification<? extends DataObject> getModifiedChild(PathArgument childArgument);
 
