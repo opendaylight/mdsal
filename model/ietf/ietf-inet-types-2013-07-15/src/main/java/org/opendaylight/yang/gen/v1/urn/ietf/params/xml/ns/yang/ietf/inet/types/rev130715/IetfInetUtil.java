bcc68be25606970babc47ea46dab672762e485da
Robert Varga <robert.varga@pantheon.tech>
2018-06-07 13:50:35 +0200

    Allow ipv4-address-no-zone to be parsed
    Add methods which allow no-zone addresses to be parsed equally
    efficiently as ip addresses.
    
    JIRA: BGPCEP-790
    Change-Id: Ic119430e3398777addeec2b41ca8e83eaead363b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/ietf/ietf-inet-types-2013-07-15/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/inet/types/rev130715/IetfInetUtil.java
+++ model/ietf/ietf-inet-types-2013-07-15/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/inet/types/rev130715/IetfInetUtil.java
@@ -8,18 +8,19 @@
 package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715;
 
 import com.google.common.annotations.Beta;
-
 import org.opendaylight.mdsal.model.ietf.util.AbstractIetfInetUtil;
 
 /**
  * A set of utility methods to efficiently instantiate various ietf-inet-types DTOs.
  */
 @Beta
-public final class IetfInetUtil extends AbstractIetfInetUtil<Ipv4Address, Ipv4Prefix, Ipv6Address, Ipv6Prefix, IpAddress, IpPrefix> {
+public final class IetfInetUtil extends AbstractIetfInetUtil<Ipv4Address, Ipv4AddressNoZone, Ipv4Prefix, Ipv6Address,
+        Ipv6AddressNoZone, Ipv6Prefix, IpAddress, IpAddressNoZone, IpPrefix> {
     public static final IetfInetUtil INSTANCE = new IetfInetUtil();
 
     private IetfInetUtil() {
-        super(Ipv4Address.class, Ipv4Prefix.class, Ipv6Address.class, Ipv6Prefix.class);
+        super(Ipv4Address.class, Ipv4AddressNoZone.class, Ipv4Prefix.class, Ipv6Address.class, Ipv6AddressNoZone.class,
+            Ipv6Prefix.class);
     }
 
     @Override
@@ -28,17 +29,27 @@ public final class IetfInetUtil extends AbstractIetfInetUtil<Ipv4Address, Ipv4Pr
     }
 
     @Override
+    protected IpAddressNoZone ipv4AddressNoZone(final Ipv4AddressNoZone addr) {
+        return new IpAddressNoZone(addr);
+    }
+
+    @Override
+    protected IpAddressNoZone ipv6AddressNoZone(final Ipv6AddressNoZone addr) {
+        return new IpAddressNoZone(addr);
+    }
+
+    @Override
     protected IpAddress ipv6Address(final Ipv6Address addr) {
         return new IpAddress(addr);
     }
 
     @Override
-    protected IpPrefix ipv4Prefix(Ipv4Prefix addr) {
+    protected IpPrefix ipv4Prefix(final Ipv4Prefix addr) {
         return new IpPrefix(addr);
     }
 
     @Override
-    protected IpPrefix ipv6Prefix(Ipv6Prefix addr) {
+    protected IpPrefix ipv6Prefix(final Ipv6Prefix addr) {
         return new IpPrefix(addr);
     }
 
