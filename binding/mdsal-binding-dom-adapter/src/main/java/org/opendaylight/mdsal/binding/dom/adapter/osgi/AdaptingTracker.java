09f2c128cdb2111125cda304539ff153a9e1f4e5
Tom Pantelis <tompantelis@gmail.com>
2018-07-02 18:28:39 -0400

    Add DOM and binding PingPongDataBroker
    Copied from the controller.
    
    Change-Id: I34d22ce2ee1af43338451aa45a62cee43bffe688
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/osgi/AdaptingTracker.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/osgi/AdaptingTracker.java
@@ -10,9 +10,13 @@ package org.opendaylight.mdsal.binding.dom.adapter.osgi;
 import static java.util.Objects.requireNonNull;
 
 import java.util.function.Function;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.mdsal.binding.api.BindingService;
 import org.opendaylight.mdsal.dom.api.DOMService;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.Filter;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.ServiceTracker;
@@ -30,14 +34,30 @@ final class AdaptingTracker<D extends DOMService, B extends BindingService>
         extends ServiceTracker<D, ServiceRegistration<B>> {
     private static final Logger LOG = LoggerFactory.getLogger(AdaptingTracker.class);
 
+    static final String DEFAULT_SERVICE_TYPE = "default";
+
+    static final String SERVICE_TYPE_PROP = "type";
+
     private final Function<D, B> bindingFactory;
     private final Class<B> bindingClass;
+    private final String serviceType;
 
     AdaptingTracker(final BundleContext ctx, final Class<D> domClass, final Class<B> bindingClass,
-        final Function<D, B> bindingFactory) {
-        super(ctx, domClass, null);
+        final Function<D, B> bindingFactory, @Nullable String serviceType) {
+        super(ctx, createOSGiFilter(ctx, domClass, serviceType), null);
         this.bindingClass = requireNonNull(bindingClass);
         this.bindingFactory = requireNonNull(bindingFactory);
+        this.serviceType = serviceType;
+    }
+
+    private static Filter createOSGiFilter(final BundleContext ctx, final Class<?> clazz, final String serviceType) {
+        try {
+            return serviceType != null ? ctx.createFilter(String.format("(&(%s=%s)(%s=%s))",
+                        Constants.OBJECTCLASS, clazz.getName(), SERVICE_TYPE_PROP, serviceType))
+                    : ctx.createFilter(String.format("%s=%s)", Constants.OBJECTCLASS, clazz.getName()));
+        } catch (InvalidSyntaxException e) {
+            throw new IllegalArgumentException("Error creating OSGi service filter", e);
+        }
     }
 
     @Override
@@ -59,7 +79,7 @@ final class AdaptingTracker<D extends DOMService, B extends BindingService>
             return null;
         }
         final B binding = bindingFactory.apply(dom);
-        final Dict props = Dict.fromReference(reference);
+        final Dict props = Dict.from(serviceType);
         final ServiceRegistration<B> reg = context.registerService(bindingClass, binding, props);
         LOG.debug("Registered {} adapter {} of {} with {} as {}", bindingClass.getName(), binding, dom, props, reg);
         return reg;
@@ -67,11 +87,6 @@ final class AdaptingTracker<D extends DOMService, B extends BindingService>
 
     @Override
     public void modifiedService(final ServiceReference<D> reference, final ServiceRegistration<B> service) {
-        if (service != null && reference != null) {
-            final Dict newProps = Dict.fromReference(reference);
-            LOG.debug("Updating service {} with properties {}", service, newProps);
-            service.setProperties(newProps);
-        }
     }
 
     @Override
---
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/ForwardingDOMDataReadWriteTransaction.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.spi;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ForwardingObject;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.FluentFuture;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.common.api.ReadFailedException;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeReadWriteTransaction;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+/**
+ * Utility {@link DOMDataTreeReadWriteTransaction} implementation which forwards all interface
+ * method invocation to a delegate instance.
+ */
+public abstract class ForwardingDOMDataReadWriteTransaction extends ForwardingObject
+        implements DOMDataTreeReadWriteTransaction {
+    @Override
+    @Nonnull
+    protected abstract DOMDataTreeReadWriteTransaction delegate();
+
+    @Override
+    public CheckedFuture<Optional<NormalizedNode<?, ?>>, ReadFailedException> read(final LogicalDatastoreType store,
+            final YangInstanceIdentifier path) {
+        return delegate().read(store, path);
+    }
+
+    @Override
+    public CheckedFuture<Boolean, ReadFailedException> exists(final LogicalDatastoreType store,
+            final YangInstanceIdentifier path) {
+        return delegate().exists(store, path);
+    }
+
+    @Override
+    public Object getIdentifier() {
+        return delegate().getIdentifier();
+    }
+
+    @Override
+    public void put(final LogicalDatastoreType store, final YangInstanceIdentifier path,
+            final NormalizedNode<?, ?> data) {
+        delegate().put(store, path, data);
+    }
+
+    @Override
+    public void merge(final LogicalDatastoreType store, final YangInstanceIdentifier path,
+            final NormalizedNode<?, ?> data) {
+        delegate().merge(store, path, data);
+    }
+
+    @Override
+    public boolean cancel() {
+        return delegate().cancel();
+    }
+
+    @Override
+    public void delete(final LogicalDatastoreType store, final YangInstanceIdentifier path) {
+        delegate().delete(store, path);
+    }
+
+    @Override
+    public FluentFuture<? extends CommitInfo> commit() {
+        return delegate().commit();
+    }
+}
