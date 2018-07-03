3e1c87b476be881a54d44a63509bda87e9af6df3
Tom Pantelis <tompantelis@gmail.com>
2018-07-03 11:48:13 -0400

    Add binding ClusteredDataTreeChangeListener
    Change-Id: I2866ad037acbfed6fd315124ff3c29b4cd12583c
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/ForwardingDOMDataReadWriteTransaction.java
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/ForwardingDOMDataReadWriteTransaction.java
@@ -20,7 +20,7 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
 /**
- * Utility {@link DOMDataReadWriteTransaction} implementation which forwards all interface
+ * Utility {@link DOMDataTreeReadWriteTransaction} implementation which forwards all interface
  * method invocation to a delegate instance.
  */
 public abstract class ForwardingDOMDataReadWriteTransaction extends ForwardingObject
