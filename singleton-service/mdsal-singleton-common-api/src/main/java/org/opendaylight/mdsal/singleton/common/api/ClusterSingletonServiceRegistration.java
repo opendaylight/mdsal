aaca2f7ef307e79f1edbbab39b7548dff8e92036
Robert Varga <robert.varga@pantheon.tech>
2018-07-04 16:48:50 +0200

    Fix ClusterSingletonServiceRegistration
    This interface is intended to be a Registration, not an AutoCloseable,
    and so is its only implementation.
    
    Switch from AutoCloseable to ObjectRegistration, so users can take
    advantage of the simplified API.
    
    Change-Id: I47be557dce38f23e92d9744f019e327fa03c9321
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- singleton-service/mdsal-singleton-common-api/src/main/java/org/opendaylight/mdsal/singleton/common/api/ClusterSingletonServiceRegistration.java
+++ singleton-service/mdsal-singleton-common-api/src/main/java/org/opendaylight/mdsal/singleton/common/api/ClusterSingletonServiceRegistration.java
@@ -5,17 +5,13 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.mdsal.singleton.common.api;
 
-/**
- * {@link ClusterSingletonServiceRegistration} interface contains a marker for
- * holding ClusterSingletonService registration and posibility to close it
- * via {@link AutoCloseable} interface.
- */
-public interface ClusterSingletonServiceRegistration extends AutoCloseable {
+import org.opendaylight.yangtools.concepts.ObjectRegistration;
 
-    /**
-     * Empty body for mark a ClusterSingletonService Registration instance
-     */
+/**
+ * An {@link ObjectRegistration} of a {@link ClusterSingletonService} instance.
+ */
+public interface ClusterSingletonServiceRegistration extends ObjectRegistration<ClusterSingletonService> {
+
 }
