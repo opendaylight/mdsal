ca4392f2a3cef865ed059696a53afe41c42caab4
Robert Varga <robert.varga@pantheon.tech>
2018-07-12 23:48:55 +0200

    Teach DynamicBindingAdapter about properties
    We needs to exert some control over how a we re-export services,
    like ignoring re-exports and overriding properties.
    
    Define the API contract in ServiceProperties and implement the two
    features mentioned.
    
    Change-Id: I37b4ab53517842cd21bf090ea83c8d11a527d49e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/osgi/ServiceProperties.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter.osgi;
+
+/**
+ * Properties recognized and manipulated by {@link DynamicBindingAdapter}. All properties starting with
+ * {@code odl.mdsal.binding.adapter.} are stripped from the re-exported service.
+ *
+ * @author Robert Varga
+ */
+public final class ServiceProperties {
+    static final String PREFIX = "odl.mdsal.binding.adapter.";
+
+    /**
+     * Instruction to ignore the service. When a service with this property is found, {@link DynamicBindingAdapter}
+     * will completely ignore it.
+     */
+    public static final String IGNORE_PROP = "odl.mdsal.binding.adapter.ignore";
+
+    /**
+     * Prefix for properties which should be replaced. For any property with a name which starts with this prefix,
+     * {@link DynamicBindingAdapter} will strip this prefix and advertise the resulting property in the re-exported
+     * service.
+     */
+    public static final String OVERRIDE_PREFIX = "odl.mdsal.binding.adapter.override.";
+}
