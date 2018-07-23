93846b98b4fcee47038a280d380a4e00ecf6f59d
Robert Varga <robert.varga@pantheon.tech>
2018-07-23 12:43:42 +0200

    Move RpcServiceInvoker to mdsal-dom-adapter
    These classes should not be in yang-binding, as their concern lies
    in runtime API realization. Move them to mdsal-dom-adapter.
    
    Change-Id: I5640c2f169b6569dcdbc306580768751791fd415
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/invoke/RpcMethodInvokerWithInput.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/invoke/RpcMethodInvokerWithInput.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.binding.util;
+package org.opendaylight.mdsal.binding.dom.adapter.invoke;
 
 import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.ListenableFuture;
