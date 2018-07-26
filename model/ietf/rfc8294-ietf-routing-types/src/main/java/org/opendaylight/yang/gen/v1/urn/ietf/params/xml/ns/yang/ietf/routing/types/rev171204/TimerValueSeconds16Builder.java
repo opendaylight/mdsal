d8c06d68b48805290446d75c016085eb88b387ee
Robert Varga <robert.varga@pantheon.tech>
2018-07-26 22:16:48 +0200

    Add RFC8294 models
    This adds ietf-routing-types and iana-routing-types.
    
    Change-Id: I5983462c00215c1cfaa1be7a6253b545cf9d5e8d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/ietf/rfc8294-ietf-routing-types/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/routing/types/rev171204/TimerValueSeconds16Builder.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.routing.types.rev171204;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Verify;
+import com.google.common.collect.Maps;
+import java.util.Arrays;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.routing.types.rev171204.TimerValueSeconds16.Enumeration;
+
+/**
+  * Builder for {@link TimerValueSeconds16} instances.
+  *
+  * @author Robert Varga
+  */
+@Beta
+@NonNullByDefault
+public final class TimerValueSeconds16Builder {
+     private static final Map<Enumeration, TimerValueSeconds16> ENUMERATED = Arrays.stream(Enumeration.values())
+             .collect(Maps.toImmutableEnumMap(Verify::verifyNotNull, TimerValueSeconds16::new));
+
+     private TimerValueSeconds16Builder() {
+
+     }
+
+     public static TimerValueSeconds16 getDefaultInstance(final String defaultValue) {
+         return Enumeration.forName(defaultValue).map(ENUMERATED::get)
+                 .orElse(new TimerValueSeconds16(Integer.valueOf(defaultValue)));
+     }
+
+     public static TimerValueSeconds16 forEnumeration(final Enumeration enumeration) {
+         return verifyNotNull(ENUMERATED.get(requireNonNull(enumeration)));
+     }
+}
---
+++ model/ietf/rfc8294-ietf-routing-types/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/routing/types/rev171204/TimerValueSeconds32Builder.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.routing.types.rev171204;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Verify;
+import com.google.common.collect.Maps;
+import java.util.Arrays;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.routing.types.rev171204.TimerValueSeconds32.Enumeration;
+
+/**
+ * Builder for {@link TimerValueSeconds32} instances.
+ *
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+public final class TimerValueSeconds32Builder {
+    private static final Map<Enumeration, TimerValueSeconds32> ENUMERATED = Arrays.stream(Enumeration.values())
+            .collect(Maps.toImmutableEnumMap(Verify::verifyNotNull, TimerValueSeconds32::new));
+
+    private TimerValueSeconds32Builder() {
+
+    }
+
+    public static TimerValueSeconds32 getDefaultInstance(final String defaultValue) {
+        return Enumeration.forName(defaultValue).map(ENUMERATED::get)
+                .orElse(new TimerValueSeconds32(Long.valueOf(defaultValue)));
+    }
+
+    public static TimerValueSeconds32 forEnumeration(final Enumeration enumeration) {
+        return verifyNotNull(ENUMERATED.get(requireNonNull(enumeration)));
+    }
+}
