a4150d21a0c40757d25a4dad02e7746bb1716d10
Robert Varga <robert.varga@pantheon.tech>
2018-07-27 21:32:04 +0200

    Rework inlined union generation
    Unions internal to a leaf union end up being incompletely generated,
    as they lack stringValue(), hashCode(), equals() and do not correctly
    bind to its enclosing builder -- leading to a generated code not being
    compilable.
    
    There are multiple issues here, all of which are addressed in this patch:
    - hashCode/equals properties are created in the wrong place
    - property return type is set to the builder, not its product
    - union builder type is not set as a union
    - builders for nested types are not correctly emitted
    - Class/InterfaceTemplate use different logic to emit the inner classes
    
    JIRA: MDSAL-320
    Change-Id: I626fb4ac42ae6528bc98b809bc33756e8daa08b9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal320Test.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.Iterables;
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.GeneratedProperty;
+import org.opendaylight.mdsal.binding.model.api.GeneratedTransferObject;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.MethodSignature;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Mdsal320Test {
+
+    @Test
+    public void mdsal320Test() {
+        final SchemaContext context = YangParserTestUtils.parseYangResource("/mdsal320.yang");
+
+        final List<Type> generateTypes = new BindingGeneratorImpl().generateTypes(context);
+        assertNotNull(generateTypes);
+        assertEquals(4, generateTypes.size());
+
+        final Type fooType = generateTypes.stream().filter(type -> type.getFullyQualifiedName()
+            .equals("org.opendaylight.yang.gen.v1.urn.odl.yt320.norev.Foo")).findFirst().get();
+        assertTrue(fooType instanceof GeneratedType);
+        final GeneratedType foo = (GeneratedType) fooType;
+
+        GeneratedTransferObject bar = null;
+        GeneratedTransferObject bar1 = null;
+        for (GeneratedType enc : foo.getEnclosedTypes()) {
+            switch (enc.getName()) {
+                case "Bar":
+                    assertTrue(enc instanceof GeneratedTransferObject);
+                    bar = (GeneratedTransferObject) enc;
+                    break;
+                case "Bar$1":
+                    assertTrue(enc instanceof GeneratedTransferObject);
+                    bar1 = (GeneratedTransferObject) enc;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected type " + enc);
+            }
+        }
+        assertNotNull(bar);
+        assertTrue(bar.isUnionType());
+        assertNotNull(bar1);
+        assertTrue(bar1.isUnionType());
+
+        final MethodSignature getBar = Iterables.getOnlyElement(foo.getMethodDefinitions());
+        final Type getBarType = getBar.getReturnType();
+        assertTrue(getBarType instanceof GeneratedTransferObject);
+        final GeneratedTransferObject getBarTO = (GeneratedTransferObject) getBarType;
+        assertTrue(getBarTO.isUnionType());
+        assertEquals(bar, getBarTO);
+
+        final GeneratedProperty bar1Prop = bar.getProperties().stream().filter(prop -> "bar$1".equals(prop.getName()))
+                .findFirst().get();
+        final Type bar1PropRet = bar1Prop.getReturnType();
+        assertEquals(bar1, bar1PropRet);
+    }
+}
