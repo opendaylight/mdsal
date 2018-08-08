816a468cceb5bc0e88b3f467919786d253fd2c3b
Robert Varga <robert.varga@pantheon.tech>
2018-08-09 00:00:27 +0200

    Migrate com.google.common.base.Supplier
    Java 8 provides and enquivalent, migrate to it.
    
    Change-Id: I93e23cd7548e2135aae9ea20a2734347282901d4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/context/base/NodeContextSupplier.java
+++ binding2/mdsal-binding2-dom-codec/src/main/java/org/opendaylight/mdsal/binding/javav2/dom/codec/impl/context/base/NodeContextSupplier.java
@@ -5,11 +5,10 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.mdsal.binding.javav2.dom.codec.impl.context.base;
 
 import com.google.common.annotations.Beta;
-import com.google.common.base.Supplier;
+import java.util.function.Supplier;
 import javax.annotation.Nonnull;
 
 /**
