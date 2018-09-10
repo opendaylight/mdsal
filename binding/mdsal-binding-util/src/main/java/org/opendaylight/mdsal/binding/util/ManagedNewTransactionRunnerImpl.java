8c7e53dd318d298c08d702a3ae032ae2d1a4494a
Stephen Kitt <skitt@redhat.com>
2018-09-10 18:31:07 +0200

    Implement managed transactions
    Managed transactions ensure that transactions are always closed in the
    appropriate way:
    * read-only transactions are closed when they are no longer needed;
    * read-write and write-only transactions are submitted if the code
      manipulating them didn’t throw an exception, cancelled otherwise.
    
    Managed transactions are datastore-specific and enforce either
    configuration or operational reads and writes.
    
    Two providers are included: a basic transaction manager which provides
    transactions, ensures they are closed, without any extras, and a
    retrying transaction manager which will retry if a retriable error
    occurs (i.e. an OptimisticLockFailedException when writing, or a
    ReadFailedException when reading).
    
    This is copied from Genius where it’s used extensively; it’s also used
    in NetVirt.
    
    Change-Id: I391ca4ca50d1b1177d827e33c0d11e98378e1fb4
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ binding/mdsal-binding-util/src/main/java/org/opendaylight/mdsal/binding/util/ManagedNewTransactionRunnerImpl.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.FluentFuture;
+import com.google.common.util.concurrent.Futures;
+import java.util.function.Function;
+import javax.annotation.CheckReturnValue;
+import javax.inject.Inject;
+import org.opendaylight.mdsal.binding.api.BindingTransactionChain;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.opendaylight.mdsal.common.api.AsyncTransaction;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.mdsal.common.api.TransactionChain;
+import org.opendaylight.mdsal.common.api.TransactionChainListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of {@link ManagedNewTransactionRunner}. This is based on {@link ManagedTransactionFactoryImpl} but
+ * re-implements operations based on read-write transactions to cancel transactions which don't end up making any
+ * changes to the datastore.
+ */
+@Beta
+// Do *NOT* mark this as @Singleton, because users choose their implementation
+public class ManagedNewTransactionRunnerImpl extends ManagedTransactionFactoryImpl
+        implements ManagedNewTransactionRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ManagedNewTransactionRunnerImpl.class);
+
+    private final DataBroker broker;
+
+    @Inject
+    public ManagedNewTransactionRunnerImpl(DataBroker broker) {
+        super(broker);
+        this.broker = broker;
+    }
+
+    // This is overridden to use this class’s commit method
+    @Override
+    @CheckReturnValue
+    public <D extends Datastore, E extends Exception, R> FluentFuture<R> applyWithNewReadWriteTransactionAndSubmit(
+        Class<D> datastoreType, InterruptibleCheckedFunction<TypedReadWriteTransaction<D>, R, E> txFunction) {
+        return super.applyWithNewTransactionAndSubmit(datastoreType, broker::newReadWriteTransaction,
+            WriteTrackingTypedReadWriteTransactionImpl::new, txFunction::apply, this::commit);
+    }
+
+    @Override
+    public <R> R applyWithNewTransactionChainAndClose(Function<ManagedTransactionChain, R> chainConsumer) {
+        try (BindingTransactionChain realTxChain = broker.createTransactionChain(new TransactionChainListener() {
+            @Override
+            public void onTransactionChainFailed(TransactionChain<?, ?> chain, AsyncTransaction<?, ?> transaction,
+                Throwable cause) {
+                LOG.error("Error handling a transaction chain", cause);
+            }
+
+            @Override
+            public void onTransactionChainSuccessful(TransactionChain<?, ?> chain) {
+                // Nothing to do
+            }
+        })) {
+            return chainConsumer.apply(new ManagedTransactionChainImpl(realTxChain));
+        }
+    }
+
+    // This is overridden to use this class’s commit method
+    @Override
+    @CheckReturnValue
+    public <D extends Datastore, E extends Exception> FluentFuture<Void>
+        callWithNewReadWriteTransactionAndSubmit(Class<D> datastoreType,
+            InterruptibleCheckedConsumer<TypedReadWriteTransaction<D>, E> txConsumer) {
+        return callWithNewTransactionAndSubmit(datastoreType, broker::newReadWriteTransaction,
+            WriteTrackingTypedReadWriteTransactionImpl::new, txConsumer::accept, this::commit);
+    }
+
+    // This is overridden to use this class’s commit method
+    @Override
+    @CheckReturnValue
+    public <D extends Datastore, E extends Exception> FluentFuture<Void> callWithNewWriteOnlyTransactionAndSubmit(
+        Class<D> datastoreType, InterruptibleCheckedConsumer<TypedWriteTransaction<D>, E> txConsumer) {
+        return super.callWithNewTransactionAndSubmit(datastoreType, broker::newWriteOnlyTransaction,
+            WriteTrackingTypedWriteTransactionImpl::new, txConsumer::accept, this::commit);
+    }
+
+    @CheckReturnValue
+    private FluentFuture<? extends CommitInfo> commit(WriteTransaction realTx, WriteTrackingTransaction wrappedTx) {
+        if (wrappedTx.isWritten()) {
+            // The transaction contains changes, commit it
+            return realTx.commit();
+        } else {
+            // The transaction only handled reads, cancel it
+            realTx.cancel();
+            return FluentFuture.from(Futures.immediateFuture(CommitInfo.empty()));
+        }
+    }
+}
---
+++ binding/mdsal-binding-util/src/main/java/org/opendaylight/mdsal/binding/util/ManagedTransactionFactoryImpl.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.util;
+
+import static com.google.common.util.concurrent.Futures.immediateFailedFuture;
+
+import com.google.common.util.concurrent.FluentFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+import javax.annotation.CheckReturnValue;
+import org.opendaylight.mdsal.binding.api.ReadTransaction;
+import org.opendaylight.mdsal.binding.api.TransactionFactory;
+import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Basic implementation of a {@link ManagedTransactionFactory}.
+ */
+class ManagedTransactionFactoryImpl implements ManagedTransactionFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(ManagedTransactionFactoryImpl.class);
+
+    private final TransactionFactory transactionFactory;
+
+    ManagedTransactionFactoryImpl(TransactionFactory transactionFactory) {
+        this.transactionFactory = transactionFactory;
+    }
+
+    @Override
+    public <D extends Datastore, E extends Exception, R> R applyWithNewReadOnlyTransactionAndClose(
+            Class<D> datastoreType, InterruptibleCheckedFunction<TypedReadTransaction<D>, R, E> txFunction)
+            throws E, InterruptedException {
+        try (ReadTransaction realTx = transactionFactory.newReadOnlyTransaction()) {
+            TypedReadTransaction<D>
+                wrappedTx = new TypedReadTransactionImpl<>(datastoreType, realTx);
+            return txFunction.apply(wrappedTx);
+        }
+    }
+
+    @Override
+    @CheckReturnValue
+    public <D extends Datastore, E extends Exception, R>
+        FluentFuture<R> applyWithNewReadWriteTransactionAndSubmit(Class<D> datastoreType,
+            InterruptibleCheckedFunction<TypedReadWriteTransaction<D>, R, E> txFunction) {
+        return applyWithNewTransactionAndSubmit(datastoreType, transactionFactory::newReadWriteTransaction,
+            TypedReadWriteTransactionImpl::new, txFunction, (realTx, wrappedTx) -> realTx.commit());
+    }
+
+    @Override
+    public <D extends Datastore, E extends Exception> void callWithNewReadOnlyTransactionAndClose(
+            Class<D> datastoreType, InterruptibleCheckedConsumer<TypedReadTransaction<D>, E> txConsumer)
+            throws E, InterruptedException {
+        try (ReadTransaction realTx = transactionFactory.newReadOnlyTransaction()) {
+            TypedReadTransaction<D>
+                wrappedTx = new TypedReadTransactionImpl<>(datastoreType, realTx);
+            txConsumer.accept(wrappedTx);
+        }
+    }
+
+    @Override
+    @CheckReturnValue
+    public <D extends Datastore, E extends Exception>
+        FluentFuture<Void> callWithNewReadWriteTransactionAndSubmit(Class<D> datastoreType,
+            InterruptibleCheckedConsumer<TypedReadWriteTransaction<D>, E> txConsumer) {
+        return callWithNewTransactionAndSubmit(datastoreType, transactionFactory::newReadWriteTransaction,
+            TypedReadWriteTransactionImpl::new, txConsumer, (realTx, wrappedTx) -> realTx.commit());
+    }
+
+    @Override
+    @CheckReturnValue
+    public <D extends Datastore, E extends Exception> FluentFuture<Void> callWithNewWriteOnlyTransactionAndSubmit(
+            Class<D> datastoreType, InterruptibleCheckedConsumer<TypedWriteTransaction<D>, E> txConsumer) {
+        return callWithNewTransactionAndSubmit(datastoreType, transactionFactory::newWriteOnlyTransaction,
+            TypedWriteTransactionImpl::new, txConsumer, (realTx, wrappedTx) -> realTx.commit());
+    }
+
+    @CheckReturnValue
+    protected <D extends Datastore, T extends WriteTransaction, W, E extends Exception> FluentFuture<Void>
+        callWithNewTransactionAndSubmit(
+            Class<D> datastoreType, Supplier<T> txSupplier, BiFunction<Class<D>, T, W> txWrapper,
+            InterruptibleCheckedConsumer<W, E> txConsumer, BiFunction<T, W, FluentFuture<?>> txSubmitter) {
+        return applyWithNewTransactionAndSubmit(datastoreType, txSupplier, txWrapper, tx -> {
+            txConsumer.accept(tx);
+            return null;
+        }, txSubmitter);
+    }
+
+    @CheckReturnValue
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    protected <D extends Datastore, T extends WriteTransaction, W, R, E extends Exception> FluentFuture<R>
+        applyWithNewTransactionAndSubmit(
+            Class<D> datastoreType, Supplier<T> txSupplier, BiFunction<Class<D>, T, W> txWrapper,
+            InterruptibleCheckedFunction<W, R, E> txFunction, BiFunction<T, W, FluentFuture<?>> txSubmitter) {
+        T realTx = txSupplier.get();
+        W wrappedTx = txWrapper.apply(datastoreType, realTx);
+        try {
+            // We must store the result before submitting the transaction; if we inline the next line in the
+            // transform lambda, that's not guaranteed
+            R result = txFunction.apply(wrappedTx);
+            return txSubmitter.apply(realTx, wrappedTx).transform(v -> result, MoreExecutors.directExecutor());
+        } catch (Exception e) {
+            // catch Exception for both the <E extends Exception> thrown by accept() as well as any RuntimeException
+            if (!realTx.cancel()) {
+                LOG.error("Transaction.cancel() return false - this should never happen (here)");
+            }
+            return FluentFuture.from(immediateFailedFuture(e));
+        }
+    }
+}
