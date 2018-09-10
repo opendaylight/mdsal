41527331872ecc6722aaee694e2792d1c3ca79d1
Stephen Kitt <skitt@redhat.com>
2018-09-10 18:31:07 +0200

    Implement managed transactions
    Managed transactions ensure that transactions are always closed in the
    appropriate way:
    * read-only transactions are closed when they are no longer needed;
    * read-write and write-only transactions are submitted if the code
      manipulating them didn’t throw an exception, cancelled otherwise.
    
    Managed transactions are datastore-specific and enforce either
    configuration or operational reads and writes.
    
    Two providers are included: a basic transaction manager which provides
    transactions, ensures they are closed, without any extras, and a
    retrying transaction manager which will retry if a retriable error
    occurs (i.e. an OptimisticLockFailedException when writing, or a
    ReadFailedException when reading).
    
    This is copied from Genius where it’s used extensively; it’s also used
    in NetVirt.
    
    Change-Id: I391ca4ca50d1b1177d827e33c0d11e98378e1fb4
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ binding/mdsal-binding-util/src/main/java/org/opendaylight/mdsal/binding/util/Datastore.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.util;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+
+/**
+ * Strongly-typed representation of a datastore (configuration or operational).
+ */
+@Beta
+// FIXME Base this on ietf-datastores.yang (RFC 8342)
+public abstract class Datastore {
+
+    /** Class representing the configuration datastore. */
+    public static final Class<Configuration> CONFIGURATION = Configuration.class;
+
+    /** Class representing the operational datastore. */
+    public static final Class<Operational> OPERATIONAL = Operational.class;
+
+    public static final class Configuration extends Datastore {}
+
+    public static final class Operational extends Datastore {}
+
+    /**
+     * Returns the logical datastore type corresponding to the given datastore class.
+     *
+     * @param datastoreClass The datastore class to convert.
+     * @return The corresponding logical datastore type.
+     * @throws IllegalArgumentException if the provided datastore class isn’t handled.
+     */
+    public static LogicalDatastoreType toType(Class<? extends Datastore> datastoreClass) {
+        if (datastoreClass.equals(Configuration.class)) {
+            return LogicalDatastoreType.CONFIGURATION;
+        } else if (Operational.class.equals(datastoreClass)) {
+            return LogicalDatastoreType.OPERATIONAL;
+        } else {
+            throw new IllegalArgumentException("Unknown datastore class " + datastoreClass);
+        }
+    }
+
+    /**
+     * Returns the datastore class corresponding to the given logical datastore type.
+     * @param datastoreType The logical datastore type to convert.
+     * @return The corresponding datastore class.
+     * @throws IllegalArgumentException if the provided logical datastore type isn’t handled.
+     */
+    public static Class<? extends Datastore> toClass(LogicalDatastoreType datastoreType) {
+        switch (datastoreType) {
+            case CONFIGURATION:
+                return CONFIGURATION;
+            case OPERATIONAL:
+                return OPERATIONAL;
+            default:
+                throw new IllegalArgumentException("Unknown datastore type " + datastoreType);
+        }
+    }
+
+    private Datastore() {}
+}
---
+++ binding/mdsal-binding-util/src/main/java/org/opendaylight/mdsal/binding/util/RetryingManagedNewTransactionRunner.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.concurrent.Executor;
+import javax.inject.Inject;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.opendaylight.mdsal.common.api.OptimisticLockFailedException;
+
+/**
+ * Implementation of {@link ManagedNewTransactionRunner} with automatic transparent retries.
+ *
+ * <h3>Details about the threading model used by this class</h3>
+ *
+ * <p>This class runs the first attempt to call the delegated {@link ManagedNewTransactionRunner},
+ * which typically is a {@link ManagedNewTransactionRunnerImpl} which safely invokes {@link WriteTransaction#commit()},
+ * in the using application's thread (like a {@link MoreExecutors#directExecutor()} would, if this were an
+ * {@link Executor}, which it's not).
+ *
+ * <p>Any retry attempts required, if that <code>submit()</code> (eventually) fails with an
+ * {@link OptimisticLockFailedException}, are run in the calling thread of that eventual future completion by a
+ * {@link MoreExecutors#directExecutor()} implicit in the constructor which does not require you to specify an
+ * explicit Executor argument.  Normally that will be an internal thread from the respective DataBroker implementation,
+ * not your application's thread anymore, because that meanwhile could well be off doing something else!  Normally,
+ * that is not a problem, because retries "should" be relatively uncommon, and (re)issuing some DataBroker
+ * <code>put()</code> or <code>delete()</code> and <code>re-submit()</code> <i>should</i> be fast.
+ *
+ * <p>If this default is not suitable (e.g. for particularly slow try/retry code), then you can specify
+ * another {@link Executor} to be used for the retries by using the alternative constructor.
+ *
+ * @author Michael Vorburger.ch &amp; Stephen Kitt
+ */
+@Beta
+// Do *NOT* mark this as @Singleton, because users choose Impl; and as long as this in API, because of https://wiki.opendaylight.org/view/BestPractices/DI_Guidelines#Nota_Bene
+public class RetryingManagedNewTransactionRunner extends RetryingManagedNewTransactionRunnerImpl {
+
+    /**
+     * Constructor.
+     * Please see the class level documentation above for more details about the threading model used.
+     * This uses the default of 3 retries, which is typically suitable.
+     *
+     * @param dataBroker the {@link DataBroker} from which transactions are obtained
+     * @throws NullPointerException if {@code dataBroker} is {@code null}.
+     */
+    @Inject
+    public RetryingManagedNewTransactionRunner(DataBroker dataBroker) {
+        super(new ManagedNewTransactionRunnerImpl(dataBroker));
+    }
+
+    /**
+     * Constructor.
+     * Please see the class level documentation above for more details about the threading model used.
+     *
+     * @param dataBroker the {@link DataBroker} from which transactions are obtained
+     * @param maxRetries the maximum number of retry attempts
+     */
+    public RetryingManagedNewTransactionRunner(DataBroker dataBroker, int maxRetries) {
+        super(new ManagedNewTransactionRunnerImpl(dataBroker), maxRetries);
+    }
+
+    /**
+     * Constructor.
+     * Please see the class level documentation above for more details about the threading model used.
+     *
+     * @param dataBroker the {@link DataBroker} from which transactions are obtained
+     * @param executor the {@link Executor} to asynchronously run any retry attempts in
+     * @param maxRetries the maximum number of retry attempts
+     */
+    public RetryingManagedNewTransactionRunner(DataBroker dataBroker, Executor executor, int maxRetries) {
+        super(new ManagedNewTransactionRunnerImpl(dataBroker), executor, maxRetries);
+    }
+}
