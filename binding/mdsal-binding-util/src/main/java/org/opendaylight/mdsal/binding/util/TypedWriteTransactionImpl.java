9d133778661dea48eba210a31f8ea6d8a251bbeb
Robert Varga <robert.varga@pantheon.tech>
2018-10-08 12:42:26 +0200

    Remove mdsal-binding-util javax.annotation.Nullable reference
    This removes the use of javax.annotation nullable annotations
    to remove import-package.
    
    JIRA: MDSAL-373
    Change-Id: Iace3fb91218fae8ccc8d65b5f295f865dc1ba94c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-util/src/main/java/org/opendaylight/mdsal/binding/util/TypedWriteTransactionImpl.java
+++ binding/mdsal-binding-util/src/main/java/org/opendaylight/mdsal/binding/util/TypedWriteTransactionImpl.java
@@ -7,7 +7,6 @@
  */
 package org.opendaylight.mdsal.binding.util;
 
-import javax.annotation.Nonnull;
 import org.opendaylight.mdsal.binding.api.WriteTransaction;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
@@ -22,38 +21,39 @@ class TypedWriteTransactionImpl<D extends Datastore> extends TypedTransaction<D>
     // Temporarily package protected for TransactionAdapter
     final WriteTransaction delegate;
 
-    TypedWriteTransactionImpl(Class<D> datastoreType, WriteTransaction realTx) {
+    TypedWriteTransactionImpl(final Class<D> datastoreType, final WriteTransaction realTx) {
         super(datastoreType);
         this.delegate = realTx;
     }
 
     @Override
-    public <T extends DataObject> void put(InstanceIdentifier<T> path, T data) {
+    public <T extends DataObject> void put(final InstanceIdentifier<T> path, final T data) {
         delegate.put(getDatastoreType(), path, data);
     }
 
     @Override
-    public <T extends DataObject> void put(InstanceIdentifier<T> path, T data, boolean createMissingParents) {
+    public <T extends DataObject> void put(final InstanceIdentifier<T> path, final T data,
+            final boolean createMissingParents) {
         delegate.put(getDatastoreType(), path, data, createMissingParents);
     }
 
     @Override
-    public <T extends DataObject> void merge(InstanceIdentifier<T> path, T data) {
+    public <T extends DataObject> void merge(final InstanceIdentifier<T> path, final T data) {
         delegate.merge(getDatastoreType(), path, data);
     }
 
     @Override
-    public <T extends DataObject> void merge(InstanceIdentifier<T> path, T data, boolean createMissingParents) {
+    public <T extends DataObject> void merge(final InstanceIdentifier<T> path, final T data,
+            final boolean createMissingParents) {
         delegate.merge(getDatastoreType(), path, data, createMissingParents);
     }
 
     @Override
-    public void delete(InstanceIdentifier<?> path) {
+    public void delete(final InstanceIdentifier<?> path) {
         delegate.delete(getDatastoreType(), path);
     }
 
     @Override
-    @Nonnull
     public Object getIdentifier() {
         return delegate.getIdentifier();
     }
