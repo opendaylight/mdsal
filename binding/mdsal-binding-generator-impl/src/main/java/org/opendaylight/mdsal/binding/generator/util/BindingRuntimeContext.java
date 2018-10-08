65ce350bb13fc50eb694ff2e2f35bc5c4553b112
Robert Varga <robert.varga@pantheon.tech>
2018-10-08 12:40:07 +0200

    Migrate mdsal-binding-generator-impl to JDT annotations
    This removes the use of javax.annotation nullable annotations
    to remove import-package.
    
    Change-Id: I51519c4587dfe6a06fb9f875c6e6e410192c0cc9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/util/BindingRuntimeContext.java
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/util/BindingRuntimeContext.java
@@ -30,7 +30,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
-import javax.annotation.Nullable;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.mdsal.binding.generator.api.BindingRuntimeTypes;
 import org.opendaylight.mdsal.binding.generator.api.ClassLoadingStrategy;
 import org.opendaylight.mdsal.binding.generator.impl.BindingGeneratorImpl;
