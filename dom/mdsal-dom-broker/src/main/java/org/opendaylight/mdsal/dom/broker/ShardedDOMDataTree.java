9fd17dc65d7203c36cb79eab5475de73bbc841a8
han <han.jie@zte.com.cn>
2018-10-12 11:16:19 +0800

    Fix check path contention when create tree producers
    - It's not allowed to create producers with their subtrees conflicts.
      Add a map to store producers aim to do this check more simply than
      to travel 'DOMDataTreePrefixTable'.
      It's assumed there're less producers in th map that we could accept
      to do 'for' operation.
    Change-Id: I105291402f21b530f54873307316f0f0c5640f6b
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/ShardedDOMDataTree.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/ShardedDOMDataTree.java
@@ -42,6 +42,7 @@ import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public final class ShardedDOMDataTree implements DOMDataTreeService, DOMDataTreeShardingService {
     private static final Logger LOG = LoggerFactory.getLogger(ShardedDOMDataTree.class);
 
@@ -49,6 +50,8 @@ public final class ShardedDOMDataTree implements DOMDataTreeService, DOMDataTree
     private final DOMDataTreePrefixTable<DOMDataTreeShardRegistration<?>> shards = DOMDataTreePrefixTable.create();
     @GuardedBy("this")
     private final DOMDataTreePrefixTable<DOMDataTreeProducer> producers = DOMDataTreePrefixTable.create();
+    @GuardedBy("this")
+    private final Map<DOMDataTreeIdentifier, DOMDataTreeProducer> producerMap = new HashMap<>();
 
     void removeShard(final DOMDataTreeShardRegistration<?> reg) {
         final DOMDataTreeIdentifier prefix = reg.getPrefix();
@@ -133,6 +136,7 @@ public final class ShardedDOMDataTree implements DOMDataTreeService, DOMDataTree
     synchronized void destroyProducer(final ShardedDOMDataTreeProducer producer) {
         for (final DOMDataTreeIdentifier s : producer.getSubtrees()) {
             producers.remove(s);
+            producerMap.remove(s);
         }
     }
 
@@ -148,6 +152,7 @@ public final class ShardedDOMDataTree implements DOMDataTreeService, DOMDataTree
         final DOMDataTreeProducer ret = ShardedDOMDataTreeProducer.create(this, subtrees, shardMap);
         for (final DOMDataTreeIdentifier subtree : subtrees) {
             producers.store(subtree, ret);
+            producerMap.put(subtree, ret);
         }
 
         return ret;
@@ -162,6 +167,12 @@ public final class ShardedDOMDataTree implements DOMDataTreeService, DOMDataTree
             // Attempting to create a disconnected producer -- all subtrees have to be unclaimed
             final DOMDataTreeProducer producer = findProducer(subtree);
             checkArgument(producer == null, "Subtree %s is attached to producer %s", subtree, producer);
+
+            for (DOMDataTreeIdentifier producerSubtree : producerMap.keySet()) {
+                checkArgument(!subtree.contains(producerSubtree),
+                    "Subtree {} contains subtree {} which is already attached to producer {}.",
+                    subtree, producerSubtree);
+            }
 
             final DOMDataTreePrefixTableEntry<DOMDataTreeShardRegistration<?>> possibleShardReg =
                     shards.lookup(subtree);
