55eb2d7bd75e45ae009f3a6cb338e3d120f729a0
Robert Varga <robert.varga@pantheon.tech>
2018-10-31 09:49:16 +0100

    Add @NonNull annotation to LazyDataTreeModification
    This fixes a couple of eclipse warnings and makes the code a bit
    more dense.
    
    Change-Id: Iab34963d54a6ee0f38a05e327c00c202ea9bf3b9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/LazyDataTreeModification.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/LazyDataTreeModification.java
@@ -7,12 +7,14 @@
  */
 package org.opendaylight.mdsal.binding.dom.adapter;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.base.MoreObjects;
-import com.google.common.base.Preconditions;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map.Entry;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.api.DataObjectModification;
 import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
 import org.opendaylight.mdsal.binding.api.DataTreeModification;
@@ -27,46 +29,44 @@ import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
  * Lazily translated {@link DataTreeModification} based on {@link DataTreeCandidate}.
  *
  * <p>
- * {@link DataTreeModification} represents Data tree change event,
- * but whole tree is not translated or resolved eagerly, but only child nodes
- * which are directly accessed by user of data object modification.
- *
+ * {@link DataTreeModification} represents Data tree change event, but whole tree is not translated or resolved eagerly,
+ * but only child nodes which are directly accessed by user of data object modification.
  */
-class LazyDataTreeModification<T extends DataObject> implements DataTreeModification<T> {
+final class LazyDataTreeModification<T extends DataObject> implements DataTreeModification<T> {
+    private final @NonNull DataTreeIdentifier<T> path;
+    private final @NonNull DataObjectModification<T> rootNode;
 
-    private final DataTreeIdentifier<T> path;
-    private final DataObjectModification<T> rootNode;
-
-    LazyDataTreeModification(final DataTreeIdentifier<T> path, final DataObjectModification<T> modification) {
-        this.path = Preconditions.checkNotNull(path);
-        this.rootNode = Preconditions.checkNotNull(modification);
+    private LazyDataTreeModification(final DataTreeIdentifier<T> path, final DataObjectModification<T> modification) {
+        this.path = requireNonNull(path);
+        this.rootNode = requireNonNull(modification);
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
     static <T extends DataObject> DataTreeModification<T> create(final BindingToNormalizedNodeCodec codec,
             final DataTreeCandidate domChange, final LogicalDatastoreType datastoreType) {
-        final Entry<InstanceIdentifier<?>, BindingCodecTreeNode<?>> codecCtx =
-                codec.getSubtreeCodec(domChange.getRootPath());
+        final Entry<InstanceIdentifier<?>, BindingCodecTreeNode<?>> codecCtx = codec.getSubtreeCodec(
+            domChange.getRootPath());
         final DataTreeIdentifier<?> path = DataTreeIdentifier.create(datastoreType, codecCtx.getKey());
-        final DataObjectModification<?> modification =
-                LazyDataObjectModification.create(codecCtx.getValue(), domChange.getRootNode());
+        final DataObjectModification<?> modification = LazyDataObjectModification.create(codecCtx.getValue(),
+            domChange.getRootNode());
         return new LazyDataTreeModification(path, modification);
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
     static <T extends DataObject> DataTreeModification<T> create(final BindingToNormalizedNodeCodec codec,
             final DOMDataTreeCandidate candidate) {
-        final Entry<InstanceIdentifier<?>, BindingCodecTreeNode<?>> codecCtx =
-                codec.getSubtreeCodec(candidate.getRootPath().getRootIdentifier());
-        final DataTreeIdentifier<?> path =
-                DataTreeIdentifier.create(candidate.getRootPath().getDatastoreType(), codecCtx.getKey());
-        final DataObjectModification<?> modification =
-                LazyDataObjectModification.create(codecCtx.getValue(), candidate.getRootNode());
+        final Entry<InstanceIdentifier<?>, BindingCodecTreeNode<?>> codecCtx = codec.getSubtreeCodec(
+            candidate.getRootPath().getRootIdentifier());
+        final DataTreeIdentifier<?> path = DataTreeIdentifier.create(candidate.getRootPath().getDatastoreType(),
+            codecCtx.getKey());
+        final DataObjectModification<?> modification = LazyDataObjectModification.create(codecCtx.getValue(),
+            candidate.getRootNode());
         return new LazyDataTreeModification(path, modification);
     }
 
-    static <T extends DataObject> Collection<DataTreeModification<T>> from(final BindingToNormalizedNodeCodec codec,
-            final Collection<DataTreeCandidate> domChanges, final LogicalDatastoreType datastoreType) {
+    static <T extends DataObject> @NonNull Collection<DataTreeModification<T>> from(
+            final BindingToNormalizedNodeCodec codec, final Collection<DataTreeCandidate> domChanges,
+            final LogicalDatastoreType datastoreType) {
         final List<DataTreeModification<T>> result = new ArrayList<>(domChanges.size());
         for (final DataTreeCandidate domChange : domChanges) {
             result.add(LazyDataTreeModification.create(codec, domChange, datastoreType));
