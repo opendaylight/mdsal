d1681e3a9a7cb9dddd83677dab476edae3062f37
Robert Varga <robert.varga@pantheon.tech>
2018-11-02 22:17:39 +0100

    Update WriteOperations/TypedTransaction API design
    Overloading merge/put methods with a boolean prevents a quick
    identification of users of costly operations. Moreover, the
    documentation (and contract) does not completely reflect behaviour
    as all purely-structural elements undergo automatic lifecycle:
    for example non-presence containers are created as needed and
    removed when they are no longer needed.
    
    Update the design by deprecating boolean-taking methods in favor
    of mergeParentStructureAnd{Merge,Put}, which are now implemented
    as defaults. This will allow us to migrate users first and then
    remove those methods in 4.0.0 timeframe.
    
    Also update documentation to discourage the use of these methods,
    as they have significant performance cost.
    
    JIRA: MDSAL-382
    Change-Id: I878863797dab01a1adfa2164a0ab0583b0641163
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/WriteOperations.java
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/WriteOperations.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.mdsal.binding.api;
 
+import com.google.common.annotations.Beta;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.yangtools.yang.binding.DataObject;
@@ -22,13 +23,8 @@ public interface WriteOperations {
      * whole subtree will be replaced by the specified data.
      *
      * <p>
-     * This method does not automatically create missing parent nodes. It is equivalent to invoking
-     * {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject, boolean)}
-     * with <code>createMissingParents</code> set to false.
-     *
-     * <p>
-     * If you need to make sure that a parent object exists but you do not want modify
-     * its pre-existing state by using put, consider using {@link #merge} instead.
+     * If you need to make sure that a parent object exists but you do not want modify its pre-existing state by using
+     * put, consider using {@link #merge} instead.
      *
      * @param store the logical data store which should be modified
      * @param path the data object path
@@ -47,32 +43,65 @@ public interface WriteOperations {
      * If you need to make sure that a parent object exists but you do not want modify its pre-existing state by using
      * put, consider using {@link #merge} instead.
      *
-     * <p>
-     * Note: Using <code>createMissingParents</code> with value true, may introduce garbage in data store, or recreate
-     * nodes, which were deleted by previous transaction.
+     * <b>WARNING:</b> Using this method may introduce garbage in data store, or recreate nodes, which were deleted by
+     *                 a previous transaction. It is not necessary in most scenarios and has a significantly higher cost
+     *                 than {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject)} and should only be used
+     *                 when absolutely necessary.
      *
      * @param store the logical data store which should be modified
      * @param path the data object path
      * @param data the data object to be written to the specified path
      * @param createMissingParents if {@link #CREATE_MISSING_PARENTS}, any missing parent nodes will be automatically
-     *                             created using a merge operation.
+     *                             created using a merge operation. <b>WARNING:</b> using this option is not needed
+     *                             in most scenarios and has a significant performance cost and should be avoided
+     *                             whenever possible.
+     * @throws IllegalStateException if the transaction has already been submitted
+     * @throws NullPointerException if any of the arguments is null
+     * @deprecated Use {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject)} or
+     *             {@link #mergeParentStructurePut(LogicalDatastoreType, InstanceIdentifier, DataObject)}
+     *             instead.
+     */
+    @Deprecated
+    <T extends DataObject> void put(@NonNull LogicalDatastoreType store, @NonNull InstanceIdentifier<T> path,
+            @NonNull T data, boolean createMissingParents);
+
+    /**
+     * Stores a piece of data at the specified path. This acts as an add / replace operation, which is to say that whole
+     * subtree will be replaced by the specified data. Unlike
+     * {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject)}, this method will attempt to create
+     * semantically-significant parent nodes, like list entries and presence containers, as indicated by {@code path}.
+     *
+     * <p>
+     * If you need to make sure that a parent object exists but you do not want modify its pre-existing state by using
+     * put, consider using {@link #merge} instead.
+     *
+     * <p>
+     * <b>WARNING:</b> Using this method may introduce garbage in data store, or recreate nodes, which were deleted by
+     *                 a previous transaction. It also has a significantly higher cost than
+     *                 {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject)} and should only be used when
+     *                 absolutely necessary.
+     *
+     * @param store the logical data store which should be modified
+     * @param path the data object path
+     * @param data the data object to be written to the specified path
      * @throws IllegalStateException if the transaction has already been submitted
      * @throws NullPointerException if any of the arguments is null
      */
-    <T extends DataObject> void put(@NonNull LogicalDatastoreType store, @NonNull InstanceIdentifier<T> path,
-            @NonNull T data, boolean createMissingParents);
+    // FIXME: 4.0.0: make this method non-default
+    // TODO: can we come up with a better name?
+    @Beta
+    default <T extends DataObject> void mergeParentStructurePut(@NonNull final LogicalDatastoreType store,
+            @NonNull final InstanceIdentifier<T> path, @NonNull final T data) {
+        put(store, path, data, CREATE_MISSING_PARENTS);
+    }
 
     /**
      * Merges a piece of data with the existing data at a specified path. Any pre-existing data which is not explicitly
      * overwritten will be preserved. This means that if you store a container, its child lists will be merged.
      *
      * <p>
-     * This method does not automatically create missing parent nodes. It is equivalent to invoking
-     * {@link #merge(LogicalDatastoreType, InstanceIdentifier, DataObject, boolean)}
-     * with <code>createMissingParents</code> set to false.
-     *
-     * <p>
      * If you require an explicit replace operation, use {@link #put} instead.
+     *
      * @param store the logical data store which should be modified
      * @param path the data object path
      * @param data the data object to be merged to the specified path
@@ -93,12 +122,47 @@ public interface WriteOperations {
      * @param path the data object path
      * @param data the data object to be merged to the specified path
      * @param createMissingParents if {@link #CREATE_MISSING_PARENTS}, any missing parent nodes will be automatically
-     *                             created using a merge operation.
+     *                             created using a merge operation. <b>WARNING:</b> using this option is not needed
+     *                             in most scenarios and has a significant performance cost and should be avoided
+     *                             whenever possible.
+     * @throws IllegalStateException if the transaction has already been submitted
+     * @throws NullPointerException if any of the arguments is null
+     * @deprecated Use {@link #merge(LogicalDatastoreType, InstanceIdentifier, DataObject)} or
+     *             {@link #mergeParentStructureMerge(LogicalDatastoreType, InstanceIdentifier, DataObject)}
+     *             instead.
+     */
+    @Deprecated
+    <T extends DataObject> void merge(@NonNull LogicalDatastoreType store, @NonNull InstanceIdentifier<T> path,
+            @NonNull T data, boolean createMissingParents);
+
+    /**
+     * Merges a piece of data with the existing data at a specified path. Any pre-existing data which is not explicitly
+     * overwritten will be preserved. This means that if you store a container, its child lists will be merged. Unlike
+     * {@link #merge(LogicalDatastoreType, InstanceIdentifier, DataObject)}, this method will attempt to create
+     * semantically-significant parent nodes, like list entries and presence containers, as indicated by {@code path}.
+     *
+     * <p>
+     * If you require an explicit replace operation, use {@link #put} instead.
+     *
+     * <p>
+     * <b>WARNING:</b> Using this method may introduce garbage in data store, or recreate nodes, which were deleted by
+     *                 a previous transaction. It is not necessary in most scenarios and has a significantly higher cost
+     *                 than {@link #merge(LogicalDatastoreType, InstanceIdentifier, DataObject)}. It should only be used
+     *                 when absolutely necessary.
+     *
+     * @param store the logical data store which should be modified
+     * @param path the data object path
+     * @param data the data object to be merged to the specified path
      * @throws IllegalStateException if the transaction has already been submitted
      * @throws NullPointerException if any of the arguments is null
      */
-    <T extends DataObject> void merge(@NonNull LogicalDatastoreType store, @NonNull InstanceIdentifier<T> path,
-            @NonNull T data, boolean createMissingParents);
+    // FIXME: 4.0.0: make this method non-default
+    // TODO: can we come up with a better name?
+    @Beta
+    default <T extends DataObject> void mergeParentStructureMerge(@NonNull final LogicalDatastoreType store,
+            @NonNull final InstanceIdentifier<T> path, @NonNull final T data) {
+        merge(store, path, data, CREATE_MISSING_PARENTS);
+    }
 
     /**
      * Removes a piece of data from specified path. This operation does not fail if the specified path does not exist.
@@ -111,11 +175,24 @@ public interface WriteOperations {
 
     /**
      * Flag value indicating that missing parents should be created.
+     *
+     * <p>
+     * <b>WARNING:</b> Using this flag may introduce garbage in data store, or recreate nodes, which were deleted by
+     *                 a previous transaction. It is not necessary in most scenarios and also has a significantly higher
+     *                 cost than {@link #FAIL_ON_MISSING_PARENTS} and should only be used when absolutely necessary.
+     *
+     * @deprecated To be removed with {@link #merge(LogicalDatastoreType, InstanceIdentifier, DataObject, boolean)}
+     *             and {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject, boolean)}.
      */
+    @Deprecated
     boolean CREATE_MISSING_PARENTS = true;
 
     /**
      * Flag value indicating that missing parents should cause an error.
+     *
+     * @deprecated To be removed with {@link #merge(LogicalDatastoreType, InstanceIdentifier, DataObject, boolean)}
+     *             and {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject, boolean)}.
      */
+    @Deprecated
     boolean FAIL_ON_MISSING_PARENTS = false;
 }
