935a87ca8cc3cf7947bf1bc1603d50b26aee8ea0
Robert Varga <robert.varga@pantheon.tech>
2018-11-08 00:31:14 +0100

    WIP: separate out DOMDataTree{Read,Write}Transactions
    DOMDataTreeReadWriteTransaction.close() does not make sense
    in presence of DOMDataTreeReadWriteTransaction.cancel(). Split
    out both operation sets, so ReadWriteTransaction can become
    a WriteTransaction which also has ReadOperations.
    
    Change-Id: Ib45e3e0d17ae2be591915fa618d5b468ec0c89e7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeWriteOperations.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.api;
+
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+public interface DOMDataTreeWriteOperations {
+    /**
+     * Stores a piece of data at the specified path. This acts as an add / replace operation, which is to say that whole
+     * subtree will be replaced by the specified data.
+     *
+     * <p>
+     * If you need to make sure that a parent object exists but you do not want modify its pre-existing state by using
+     * put, consider using {@link #merge} instead.
+     *
+     * @param store the logical data store which should be modified
+     * @param path the data object path
+     * @param data the data object to be written to the specified path
+     * @throws IllegalStateException if the transaction has already been submitted
+     */
+    void put(LogicalDatastoreType store, YangInstanceIdentifier path, NormalizedNode<?, ?> data);
+
+    /**
+     * Merges a piece of data with the existing data at a specified path. Any pre-existing data which is not explicitly
+     * overwritten will be preserved. This means that if you store a container, its child lists will be merged.
+     *
+     * <p>
+     * If you require an explicit replace operation, use {@link #put} instead.
+     *
+     * @param store the logical data store which should be modified
+     * @param path the data object path
+     * @param data the data object to be merged to the specified path
+     * @throws IllegalStateException if the transaction has already been submitted
+     */
+    void merge(LogicalDatastoreType store, YangInstanceIdentifier path, NormalizedNode<?, ?> data);
+
+    /**
+     * Removes a piece of data from specified path. This operation does not fail if the specified path does not exist.
+     *
+     * @param store Logical data store which should be modified
+     * @param path Data object path
+     * @throws IllegalStateException if the transaction was committed or canceled.
+     */
+    void delete(LogicalDatastoreType store, YangInstanceIdentifier path);
+}
