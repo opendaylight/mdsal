a762b2e0bf4266a26390f79bebe2d35a93ce7f61
Robert Varga <robert.varga@pantheon.tech>
2019-01-08 21:20:47 +0100

    WIP: Add mdsal-blueprint-common
    Original controller blueprint mixed binding-level concepts
    with common concepts. This patch separates out the common
    concepts and their handler, so they can be readily used.
    
    JIRA: MDSAL-413
    Change-Id: Id7a033e1c1e788069c2db3a981fbe8fca864da80
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- blueprint/mdsal-blueprint-binding/src/main/java/org/opendaylight/controller/blueprint/BlueprintBundleTracker.java
+++ blueprint/mdsal-blueprint-binding/src/main/java/org/opendaylight/controller/blueprint/BlueprintBundleTracker.java
@@ -22,7 +22,9 @@ import org.apache.aries.blueprint.services.BlueprintExtenderService;
 import org.apache.aries.quiesce.participant.QuiesceParticipant;
 import org.apache.aries.util.AriesFrameworkUtil;
 import org.opendaylight.controller.blueprint.ext.OpendaylightNamespaceHandler;
+import org.opendaylight.mdsal.blueprint.common.NamespaceHandlerUtils;
 import org.opendaylight.mdsal.blueprint.restart.api.BlueprintContainerRestartService;
+import org.opendaylight.mdsal.blueprint.restart.impl.BlueprintContainerRestartServiceImpl;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -80,7 +82,8 @@ public class BlueprintBundleTracker implements BundleActivator, BundleTrackerCus
 
         registerBlueprintEventHandler(context);
 
-        registerNamespaceHandler(context);
+        namespaceReg = NamespaceHandlerUtils.registerNamespaceHandler(context,
+            OpendaylightNamespaceHandler.NAMESPACE_1_0_0, new OpendaylightNamespaceHandler());
 
         bundleTracker = new BundleTracker<>(context, Bundle.ACTIVE, this);
 
@@ -156,7 +159,7 @@ public class BlueprintBundleTracker implements BundleActivator, BundleTrackerCus
         Dictionary<String, Object> props = new Hashtable<>();
         props.put("osgi.service.blueprint.namespace", OpendaylightNamespaceHandler.NAMESPACE_1_0_0);
         namespaceReg = context.registerService(NamespaceHandler.class.getName(),
-                new OpendaylightNamespaceHandler(), props);
+            new OpendaylightNamespaceHandler(), props);
     }
 
     private void registerBlueprintEventHandler(final BundleContext context) {
@@ -288,7 +291,7 @@ public class BlueprintBundleTracker implements BundleActivator, BundleTrackerCus
         LOG.info("Shutdown of blueprint containers complete");
     }
 
-    private List<Bundle> getBundlesToDestroy(final Collection<Bundle> containerBundles) {
+    private static List<Bundle> getBundlesToDestroy(final Collection<Bundle> containerBundles) {
         List<Bundle> bundlesToDestroy = new ArrayList<>();
 
         // Find all container bundles that either have no registered services or whose services are no
@@ -334,7 +337,7 @@ public class BlueprintBundleTracker implements BundleActivator, BundleTrackerCus
     }
 
     @Nullable
-    private Bundle findBundleWithHighestUsedServiceId(final Collection<Bundle> containerBundles) {
+    private static Bundle findBundleWithHighestUsedServiceId(final Collection<Bundle> containerBundles) {
         ServiceReference<?> highestServiceRef = null;
         for (Bundle bundle : containerBundles) {
             ServiceReference<?>[] references = bundle.getRegisteredServices();
