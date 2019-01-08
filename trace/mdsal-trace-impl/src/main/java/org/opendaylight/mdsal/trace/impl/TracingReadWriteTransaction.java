02b6592214e112d1b2d2aacfdc722ce6790370cd
Robert Varga <robert.varga@pantheon.tech>
2019-01-08 12:51:33 +0100

    Move mdsal-trace into its place
    This is code incoming from the controller, we need to integrate it
    into mdsal structure. This patch performs the preliminary move out
    of opendaylight/md-sal directory.
    
    While this code is being built, it is not part of the feature
    repository yet, so as not to interfere with controller's
    odl-mdsal-trace feature.
    
    JIRA: MDSAL-412
    Change-Id: I04cf3dcda5d990f5b1e45a3b3ecf15978a0db958
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ trace/mdsal-trace-impl/src/main/java/org/opendaylight/mdsal/trace/impl/TracingReadWriteTransaction.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.trace.impl;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.FluentFuture;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.common.api.CommitInfo;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeReadWriteTransaction;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+class TracingReadWriteTransaction
+    extends AbstractTracingWriteTransaction
+        implements DOMDataTreeReadWriteTransaction, CloseTracked<TracingReadWriteTransaction> {
+
+    private final CloseTrackedTrait<TracingReadWriteTransaction> closeTracker;
+    private final DOMDataTreeReadWriteTransaction delegate;
+
+    TracingReadWriteTransaction(DOMDataTreeReadWriteTransaction delegate, TracingBroker tracingBroker,
+            CloseTrackedRegistry<TracingReadWriteTransaction> readWriteTransactionsRegistry) {
+        super(delegate, tracingBroker);
+        this.closeTracker = new CloseTrackedTrait<>(readWriteTransactionsRegistry, this);
+        this.delegate = requireNonNull(delegate);
+    }
+
+    @Override
+    public FluentFuture<Optional<NormalizedNode<?, ?>>> read(LogicalDatastoreType store, YangInstanceIdentifier yiid) {
+        return delegate.read(store, yiid);
+    }
+
+    @Override
+    public FluentFuture<Boolean> exists(LogicalDatastoreType store, YangInstanceIdentifier yiid) {
+        return delegate.exists(store, yiid);
+    }
+
+    @Override
+    public FluentFuture<? extends CommitInfo> commit() {
+        closeTracker.removeFromTrackedRegistry();
+        return super.commit();
+    }
+
+    @Override
+    public boolean cancel() {
+        closeTracker.removeFromTrackedRegistry();
+        return super.cancel();
+    }
+
+    @Override
+    public @Nullable StackTraceElement[] getAllocationContextStackTrace() {
+        return closeTracker.getAllocationContextStackTrace();
+    }
+
+    @Override
+    public CloseTracked<TracingReadWriteTransaction> getRealCloseTracked() {
+        return this;
+    }
+}
