a0d83427ef8b02b4120be9f7bd895a2f568c6e8d
Robert Varga <robert.varga@pantheon.tech>
2019-01-09 01:34:32 +0100

    WIP: finish mdsal-blueprint-binding
    JIRA: MDSAL-413
    Change-Id: I2090faf8b7adbe2f53bf2a970a4009b76536ac64
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- blueprint/mdsal-blueprint-binding/src/main/java/org/opendaylight/mdsal/blueprint/binding/ConfigXMLReaderException.java
+++ blueprint/mdsal-blueprint-binding/src/main/java/org/opendaylight/mdsal/blueprint/binding/ConfigXMLReaderException.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.controller.blueprint.ext;
+package org.opendaylight.mdsal.blueprint.binding;
 
 /**
  * Exception thrown by {@link DataStoreAppConfigDefaultXMLReader}.
