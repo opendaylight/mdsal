d4708e9192105c2d349d50e3ff00b655d5dbe6ab
Michael Vorburger <vorburger@redhat.com>
2019-01-24 22:03:29 +0100

    add new wiring artifact, with test for wiring in standalone environments
    Test uses Guice, but the *Wiring classes themselves are not Guice
    specific, and could be used by any project wishing to use mdsal in
    standalone (non-OSGi) environments without Guice.
    
    This intentionally does not use infrautils.inject.guice (with its
    AutoWiringModule and GuiceClassPathBinder) but "simple" (regular,
    standard) Guice, only.
    
    This intentionally also does not even use
    infrautils.inject.guice.testutils with its JUnit @Rule GuiceRule, just
    to keep it real simple.
    
    JIRA: MDSAL-418
    Change-Id: Ie77ad6abb228cd22063abd71cc45f9f6a4eac284
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ wiring/src/main/java/org/opendaylight/mdsal/wiring/InMemoryDOMDataBrokerWiring.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.wiring;
+
+import static org.opendaylight.mdsal.common.api.LogicalDatastoreType.CONFIGURATION;
+import static org.opendaylight.mdsal.common.api.LogicalDatastoreType.OPERATIONAL;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.mdsal.dom.broker.SerializedDOMDataBroker;
+import org.opendaylight.mdsal.dom.spi.store.DOMStore;
+import org.opendaylight.mdsal.dom.store.inmemory.InMemoryDOMDataStore;
+
+/**
+ * Wiring for an in-memory DOMDataBroker, useful for dependency injection (DI) with your favourite (any) DI framework.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Singleton
+public class InMemoryDOMDataBrokerWiring {
+
+    private final DOMDataBroker domDataBroker;
+
+    @Inject
+    public InMemoryDOMDataBrokerWiring(DOMSchemaService schemaService) {
+        Map<LogicalDatastoreType, DOMStore> datastores = ImmutableMap.of(
+                OPERATIONAL, createDOMStore("OPER", schemaService),
+                CONFIGURATION, createDOMStore("CFG", schemaService));
+
+        this.domDataBroker = new SerializedDOMDataBroker(datastores, createCommitCoordinatorExecutor());
+    }
+
+    public DOMDataBroker getDOMDataBroker() {
+        return domDataBroker;
+    }
+
+    protected DOMStore createDOMStore(String name, DOMSchemaService schemaService) {
+        InMemoryDOMDataStore store = new InMemoryDOMDataStore(name, createDataTreeChangeListenerExecutor());
+        schemaService.registerSchemaContextListener(store);
+        return store;
+    }
+
+    protected ListeningExecutorService createDataTreeChangeListenerExecutor() {
+        return MoreExecutors.listeningDecorator(Executors.newCachedThreadPool());
+    }
+
+    protected ListeningExecutorService createCommitCoordinatorExecutor() {
+        return MoreExecutors.listeningDecorator(Executors.newSingleThreadExecutor());
+    }
+}
---
+++ wiring/src/test/java/org/opendaylight/mdsal/wiring/test/WiringTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.wiring.test;
+
+import static com.google.inject.Stage.PRODUCTION;
+import static org.junit.Assert.assertEquals;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Scopes;
+import javax.inject.Inject;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.wiring.guice.InMemoryDOMDataBrokerModule;
+import org.opendaylight.mdsal.wiring.guice.MdsalModule;
+
+/**
+ * Tests all the "Wiring classes in mdsal.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class WiringTest {
+
+    // Real world ODL SDN applications typically use the JUnit @Rule GuiceRule from infrautils.inject.guice.testutils,
+    // but for this test who's only purpose it is to test the modules, this "manual" simple Guice use is just fine.
+
+    @Inject DataBroker dataBroker;
+
+    @Test
+    public void testWiring() {
+        Injector injector = Guice.createInjector(PRODUCTION, new InMemoryDOMDataBrokerModule(), new MdsalModule());
+        injector.injectMembers(this);
+        assertEquals(true, Scopes.isSingleton(injector.getBinding(DataBroker.class)));
+    }
+}
