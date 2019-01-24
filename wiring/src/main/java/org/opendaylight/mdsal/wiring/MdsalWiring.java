722b6568c59ef63f7287970328eedb69a2616dff
Michael Vorburger <vorburger@redhat.com>
2019-01-24 22:03:29 +0100

    add new wiring artifact, with test for wiring in standalone environments
    Test uses Guice, but the *Wiring classes themselves are not Guice
    specific, and could be used by any project wishing to use mdsal in
    standalone (non-OSGi) environments without Guice.
    
    This intentionally does not use infrautils.inject.guice (with its
    AutoWiringModule and GuiceClassPathBinder) but "simple" (regular,
    standard) Guice, only.
    
    JIRA: MDSAL-418
    Change-Id: Ie77ad6abb228cd22063abd71cc45f9f6a4eac284
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ wiring/src/main/java/org/opendaylight/mdsal/wiring/MdsalWiring.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.wiring;
+
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.binding.generator.api.ClassLoadingStrategy;
+import org.opendaylight.mdsal.binding.generator.impl.GeneratedClassLoadingStrategy;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.mdsal.dom.broker.schema.ScanningSchemaServiceProvider;
+
+/**
+ * Wiring for DI of mdsal's base services in a standalone environment.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Singleton
+public class MdsalWiring {
+
+    private final ClassLoadingStrategy classLoadingStrategy =
+            GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy();
+
+    private final ScanningSchemaServiceProvider schemaService = new ScanningSchemaServiceProvider();
+
+    public ClassLoadingStrategy getClassLoadingStrategy() {
+        return classLoadingStrategy;
+    }
+
+    public DOMSchemaService getDOMSchemaService() {
+        return schemaService;
+    }
+}
---
+++ wiring/src/test/java/org/opendaylight/mdsal/wiring/test/WiringTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.wiring.test;
+
+import javax.inject.Inject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.opendaylight.infrautils.inject.guice.testutils.AnnotationsModule;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.wiring.guice.InMemoryModule;
+import org.opendaylight.mdsal.wiring.guice.MdsalModule;
+
+/**
+ * Tests all the "Wiring classes in mdsal.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class WiringTest {
+
+    public @Rule GuiceRule guice = new GuiceRule(InMemoryModule.class, MdsalModule.class, AnnotationsModule.class);
+
+    @Inject DataBroker dataBroker;
+
+    @Test
+    public void testWiring() {
+    }
+}
