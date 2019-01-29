79fab76782dfa4d226699c8426025f56b36a12d4
Michael Vorburger <vorburger@redhat.com>
2019-01-29 19:10:50 +0100

    significant clean up in mdsal-binding-dom-codec-osgi
    as per MDSAL-392, we remove BindingRuntimeContextListener and
    BindingRuntimeContextService, as they are not used anywhere, as wel as
    what used it but does not actually have to anymore.
    
    JIRA: MDSAL-392
    Change-Id: I8520a718ece407ee2eb7450e9455a2744e4a6fdc
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- binding/mdsal-binding-dom-codec-osgi/src/main/java/org/opendaylight/mdsal/binding/dom/codec/osgi/impl/Activator.java
+++ binding/mdsal-binding-dom-codec-osgi/src/main/java/org/opendaylight/mdsal/binding/dom/codec/osgi/impl/Activator.java
@@ -10,7 +10,6 @@ package org.opendaylight.mdsal.binding.dom.codec.osgi.impl;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import org.opendaylight.mdsal.binding.dom.codec.osgi.BindingRuntimeContextService;
 import org.opendaylight.mdsal.binding.generator.api.ClassLoadingStrategy;
 import org.opendaylight.mdsal.binding.generator.impl.ModuleInfoBackedContext;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
@@ -22,20 +21,16 @@ import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.BundleTracker;
 
 public final class Activator implements BundleActivator {
-    private final List<ServiceRegistration<?>> registrations = new ArrayList<>(2);
+    private final List<ServiceRegistration<?>> registrations = new ArrayList<>(1);
 
     private BundleTracker<Collection<ObjectRegistration<YangModuleInfo>>> moduleInfoResolvedBundleTracker = null;
-    private SimpleBindingRuntimeContextService service = null;
 
     @Override
     public void start(final BundleContext context) {
         // XXX: this will use thread-context class loader, which is probably appropriate
         final ModuleInfoBackedContext moduleInfoBackedContext = ModuleInfoBackedContext.create();
 
-        service = new SimpleBindingRuntimeContextService(context, moduleInfoBackedContext, moduleInfoBackedContext);
-
-        final OsgiModuleInfoRegistry registry = new OsgiModuleInfoRegistry(moduleInfoBackedContext,
-                moduleInfoBackedContext, service);
+        final OsgiModuleInfoRegistry registry = new OsgiModuleInfoRegistry(moduleInfoBackedContext);
 
         final ModuleInfoBundleTracker moduleInfoTracker = new ModuleInfoBundleTracker(registry);
         moduleInfoResolvedBundleTracker = new BundleTracker<>(context, Bundle.RESOLVED | Bundle.STARTING
@@ -43,15 +38,12 @@ public final class Activator implements BundleActivator {
         moduleInfoResolvedBundleTracker.open();
         moduleInfoTracker.finishStart();
 
-        service.open();
-        registrations.add(context.registerService(BindingRuntimeContextService.class, service, null));
         registrations.add(context.registerService(ClassLoadingStrategy.class, moduleInfoBackedContext, null));
     }
 
     @Override
     public void stop(final BundleContext context) {
         moduleInfoResolvedBundleTracker.close();
-        service.close();
         registrations.forEach(ServiceRegistration::unregister);
         registrations.clear();
     }
