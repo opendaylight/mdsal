912ab34a62c9e22213ea475695e27a95798b3e51
Michael Vorburger <vorburger@redhat.com>
2019-01-30 01:48:54 +0100

    provide Guice bindings for all mdsal binding service
    JIRA: MDSAL-418
    Change-Id: Ia6fdd6b58dd9f1b46f057bfd5a1c18546c0cd5d8
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- wiring/src/main/java/org/opendaylight/mdsal/wiring/guice/BindingModule.java
+++ wiring/src/main/java/org/opendaylight/mdsal/wiring/guice/BindingModule.java
@@ -11,7 +11,14 @@ import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Provides;
 import javax.inject.Singleton;
+import org.opendaylight.mdsal.binding.api.ActionProviderService;
+import org.opendaylight.mdsal.binding.api.ActionService;
 import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.MountPointService;
+import org.opendaylight.mdsal.binding.api.NotificationPublishService;
+import org.opendaylight.mdsal.binding.api.NotificationService;
+import org.opendaylight.mdsal.binding.api.RpcConsumerRegistry;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
 import org.opendaylight.mdsal.binding.dom.adapter.BindingWiring;
 import org.opendaylight.mdsal.binding.dom.adapter.spi.AdapterFactoryWiring;
 import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTreeFactory;
@@ -52,11 +59,6 @@ public class BindingModule implements Module {
     }
 
     @Provides
-    @Singleton DataBroker getDataBroker(AdapterFactoryWiring adapterFactoryWiring) {
-        return adapterFactoryWiring.getDataBroker();
-    }
-
-    @Provides
     @Singleton BindingNormalizedNodeSerializer getBindingNormalizedNodeSerializer(BindingWiring bindingWiring) {
         return bindingWiring.getBindingNormalizedNodeSerializer();
     }
@@ -64,5 +66,50 @@ public class BindingModule implements Module {
     @Provides
     @Singleton BindingCodecTreeFactory getBindingCodecTreeFactory(BindingWiring bindingWiring) {
         return bindingWiring.getBindingCodecTreeFactory();
+    }
+
+    @Provides
+    @Singleton DataBroker getDataBroker(AdapterFactoryWiring adapterFactoryWiring) {
+        return adapterFactoryWiring.getDataBroker();
+    }
+
+//  @Provides
+//  @Singleton DataTreeService getDataTreeService(AdapterFactoryWiring adapterFactoryWiring) {
+//      return adapterFactoryWiring.getDataTreeService();
+//  }
+
+    @Provides
+    @Singleton NotificationService getNotificationService(AdapterFactoryWiring adapterFactoryWiring) {
+        return adapterFactoryWiring.getNotificationService();
+    }
+
+    @Provides
+    @Singleton NotificationPublishService getNotificationPublishService(AdapterFactoryWiring adapterFactoryWiring) {
+        return adapterFactoryWiring.getNotificationPublishService();
+    }
+
+    @Provides
+    @Singleton MountPointService getMountPointService(AdapterFactoryWiring adapterFactoryWiring) {
+        return adapterFactoryWiring.getMountPointService();
+    }
+
+    @Provides
+    @Singleton RpcConsumerRegistry getRpcConsumerRegistry(AdapterFactoryWiring adapterFactoryWiring) {
+        return adapterFactoryWiring.getRpcConsumerRegistry();
+    }
+
+    @Provides
+    @Singleton RpcProviderService getRpcProviderService(AdapterFactoryWiring adapterFactoryWiring) {
+        return adapterFactoryWiring.getRpcProviderService();
+    }
+
+    @Provides
+    @Singleton ActionService getActionService(AdapterFactoryWiring adapterFactoryWiring) {
+        return adapterFactoryWiring.getActionService();
+    }
+
+    @Provides
+    @Singleton ActionProviderService getActionProviderService(AdapterFactoryWiring adapterFactoryWiring) {
+        return adapterFactoryWiring.getActionProviderService();
     }
 }
