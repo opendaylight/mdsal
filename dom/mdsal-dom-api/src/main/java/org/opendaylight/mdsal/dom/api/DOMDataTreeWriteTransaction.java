aa2a27ba6ff308eaaef396221c7c041443914c46
Robert Varga <robert.varga@pantheon.tech>
2019-03-15 18:51:21 +0100

    Bump odlparent to 5.0.0
    This bumps odlparent to 5.0.0.
    
    Change-Id: I90398f344b45c8f55bd92831ea2221c4af34ce5e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeWriteTransaction.java
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeWriteTransaction.java
@@ -8,7 +8,7 @@
 package org.opendaylight.mdsal.dom.api;
 
 import com.google.common.util.concurrent.FluentFuture;
-import javax.annotation.CheckReturnValue;
+import edu.umd.cs.findbugs.annotations.CheckReturnValue;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.common.api.DataValidationFailedException;
