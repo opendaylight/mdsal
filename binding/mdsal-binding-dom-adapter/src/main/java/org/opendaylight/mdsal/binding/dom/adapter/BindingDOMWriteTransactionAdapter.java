a479efdb8abf2500516f47b7c6c95cb5199dfb6f
Robert Varga <robert.varga@pantheon.tech>
2019-04-08 13:20:39 +0200

    Make WriteOperations.(put,merge) with boolean default methods
    This inverts the API, so that implementations are required to
    provide implementations of mergeParentStructure{Merge,Put} instead
    of the boolean-forking put()/merge() methods, addressing associated
    FIXMEs.
    
    Change-Id: I9750982fa1ec833fda17d583e2806d4350618c1c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMWriteTransactionAdapter.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMWriteTransactionAdapter.java
@@ -7,7 +7,10 @@
  */
 package org.opendaylight.mdsal.binding.dom.adapter;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.google.common.util.concurrent.FluentFuture;
+import java.util.Map.Entry;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.api.WriteTransaction;
 import org.opendaylight.mdsal.common.api.CommitInfo;
@@ -15,8 +18,10 @@ import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
-class BindingDOMWriteTransactionAdapter<T extends DOMDataTreeWriteTransaction> extends AbstractWriteTransaction<T>
+class BindingDOMWriteTransactionAdapter<T extends DOMDataTreeWriteTransaction> extends AbstractForwardedTransaction<T>
         implements WriteTransaction {
     BindingDOMWriteTransactionAdapter(final T delegateTx, final BindingToNormalizedNodeCodec codec) {
         super(delegateTx, codec);
@@ -25,27 +30,72 @@ class BindingDOMWriteTransactionAdapter<T extends DOMDataTreeWriteTransaction> e
     @Override
     public final <U extends DataObject> void put(final LogicalDatastoreType store, final InstanceIdentifier<U> path,
             final U data) {
-        put(store, path, data, false);
+        final Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> normalized = toNormalized("put", path, data);
+        getDelegate().put(store, normalized.getKey(), normalized.getValue());
+    }
+
+    @Override
+    public final <U extends DataObject> void mergeParentStructurePut(@NonNull LogicalDatastoreType store,
+            @NonNull InstanceIdentifier<U> path, @NonNull U data) {
+        final Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> normalized = toNormalized("put",
+            path, data);
+        ensureParentsByMerge(store, normalized.getKey(), path);
+        getDelegate().put(store, normalized.getKey(), normalized.getValue());
     }
 
     @Override
     public final <D extends DataObject> void merge(final LogicalDatastoreType store, final InstanceIdentifier<D> path,
             final D data) {
-        merge(store, path, data, false);
+        final Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> normalized = toNormalized("merge", path, data);
+        getDelegate().merge(store, normalized.getKey(), normalized.getValue());
+    }
+
+    @Override
+    public final <U extends DataObject> void mergeParentStructureMerge(@NonNull LogicalDatastoreType store,
+            @NonNull InstanceIdentifier<U> path, @NonNull U data) {
+        final Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> normalized = toNormalized("merge", path, data);
+        ensureParentsByMerge(store, normalized.getKey(), path);
+        getDelegate().merge(store, normalized.getKey(), normalized.getValue());
     }
 
     @Override
     public final void delete(final LogicalDatastoreType store, final InstanceIdentifier<?> path) {
-        doDelete(store, path);
+        checkArgument(!path.isWildcarded(), "Cannot delete wildcarded path %s", path);
+
+        final YangInstanceIdentifier normalized = getCodec().toYangInstanceIdentifierBlocking(path);
+        getDelegate().delete(store, normalized);
     }
 
     @Override
     public @NonNull FluentFuture<? extends @NonNull CommitInfo> commit() {
-        return doCommit();
+        return getDelegate().commit();
     }
 
     @Override
     public final boolean cancel() {
-        return doCancel();
+        return getDelegate().cancel();
+    }
+
+    /**
+     * Subclasses of this class are required to implement creation of parent nodes based on behaviour of their
+     * underlying transaction.
+     *
+     * @param store an instance of LogicalDatastoreType
+     * @param domPath an instance of YangInstanceIdentifier
+     * @param path an instance of InstanceIdentifier
+     */
+    private void ensureParentsByMerge(final LogicalDatastoreType store, final YangInstanceIdentifier domPath,
+            final InstanceIdentifier<?> path) {
+        final YangInstanceIdentifier parentPath = domPath.getParent();
+        if (parentPath != null) {
+            final NormalizedNode<?, ?> parentNode = getCodec().instanceIdentifierToNode(parentPath);
+            getDelegate().merge(store, YangInstanceIdentifier.create(parentNode.getIdentifier()), parentNode);
+        }
+    }
+
+    private <U extends DataObject> Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> toNormalized(
+            final String operation, final InstanceIdentifier<U> path, final U data) {
+        checkArgument(!path.isWildcarded(), "Cannot %s data into wildcarded path %s", operation, path);
+        return getCodec().toNormalizedNode(path, data);
     }
 }
