747123a6427100d14e25b0b3306b5ba694628954
Robert Varga <robert.varga@pantheon.tech>
2019-04-08 13:10:39 +0200

    Remove DOMDataTreeReadWriteTransaction.close()
    Inheriting from DOMDataTreeReadTransaction was a mistake, read-write
    transaction should not have a close() method. Fix that by inheriting
    DOMDataTreeReadOperations instead, addressing the associated FIXME.
    
    Change-Id: Ie26e19917bd78acca056c7c79de641b231fa9ea8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeReadWriteTransaction.java
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeReadWriteTransaction.java
@@ -11,20 +11,9 @@ package org.opendaylight.mdsal.dom.api;
  * A transaction that provides read/write access to a logical data store.
  *
  * <p>
- * For more information on usage and examples, please see the documentation in {@link DOMDataTreeReadTransaction}
- * and {@link DOMDataTreeWriteTransaction}.
+ * For more information on usage and examples, please see the documentation in {@link DOMDataTreeWriteTransaction}
+ * and {@link DOMDataTreeReadOperations}.
  */
-// FIXME: 4.0.0: extend DOMDataTreeReadOperations instead of DOMDataTreeReadTransaction
-public interface DOMDataTreeReadWriteTransaction extends DOMDataTreeWriteTransaction, DOMDataTreeReadTransaction {
-    /**
-     * This method is an API design mistake. Users should not use it and use {@link #cancel()} instead. Implementations
-     * should not override it and rely instead of its default implementation, which calls {@link #cancel()}.
-     *
-     * @deprecated Use {@link #cancel()} instead.
-     */
-    @Deprecated
-    @Override
-    default void close() {
-        cancel();
-    }
+public interface DOMDataTreeReadWriteTransaction extends DOMDataTreeWriteTransaction, DOMDataTreeReadOperations {
+
 }
