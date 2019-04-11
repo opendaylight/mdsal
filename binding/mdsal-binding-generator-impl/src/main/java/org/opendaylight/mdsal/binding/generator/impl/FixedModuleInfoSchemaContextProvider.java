b758c17fb7a3400f523772403e06b41f3f23876e
Michael Vorburger <mike@vorburger.ch>
2019-04-12 00:22:37 +0200

    intro. FixedModuleInfoSchemaContextProvider
    simply by factoring out existing code from AbstractSchemaAwareTest
    
    The code is mostly just moved as-is and not much changed; we do
    however let FixedModuleInfoSchemaContextProvider implement SchemaSourceProvider
    and for this need to extract the ModuleInfoBackedContext into a static final;
    that part originally used to be proposed as a separate follow-up change to make this
    easier to review, but was then requested to be squashed into 1 single change.
    
    JIRA: MDSAL-418
    Change-Id: Ic2bc4fbdd098c7f739135e21cd0b58524a9d0ba7
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/FixedModuleInfoSchemaContextProvider.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import com.google.common.annotations.Beta;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.ExecutionException;
+import org.opendaylight.mdsal.binding.spec.reflect.BindingReflections;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextProvider;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceProvider;
+
+/**
+ * {@link SchemaContextProvider} which internally uses (a cached version of the) {@link ModuleInfoBackedContext}.
+ *
+ * <p>This is useful for environments such as tests (which will frequently recreate
+ * this class, and caching is thus important) or "simple" standalone (non-OSGi)
+ * runtime environments with a fixed classpath and no dynamic (re)loading of
+ * {@link YangModuleInfo}s.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Beta
+public class FixedModuleInfoSchemaContextProvider
+        implements SchemaContextProvider, SchemaSourceProvider<YangTextSchemaSource> {
+
+    private static final LoadingCache<ImmutableSet<YangModuleInfo>, SchemaContext> SCHEMA_CONTEXT_CACHE =
+            CacheBuilder.newBuilder().weakValues().build(new CacheLoader<ImmutableSet<YangModuleInfo>, SchemaContext>() {
+                @Override
+                public SchemaContext load(final ImmutableSet<YangModuleInfo> key) {
+                    return ModuleInfoBackedContext.cacheThreadLocal(key).tryToCreateSchemaContext().get();
+                }
+            });
+
+    public ImmutableSet<YangModuleInfo> getModuleInfos() throws ExecutionException {
+        return BindingReflections.cacheModuleInfos(Thread.currentThread().getContextClassLoader());
+    }
+
+    @Override
+    public SchemaContext getSchemaContext() {
+        final ImmutableSet<YangModuleInfo> infos;
+        try {
+            infos = getModuleInfos();
+        } catch (ExecutionException e) {
+            throw new IllegalStateException("Failed to acquire YangModuleInfos", e);
+        }
+
+        return SCHEMA_CONTEXT_CACHE.getUnchecked(infos);
+    }
+
+    @Override
+    public ListenableFuture<? extends YangTextSchemaSource> getSource(final SourceIdentifier sourceIdentifier) {
+        return ModuleInfoBackedContext.cacheThreadLocal().getSource(sourceIdentifier);
+    }
+}
