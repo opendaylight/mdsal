cdb9743252f239cfa70c440805157aebf9c9f06d
Robert Varga <robert.varga@pantheon.tech>
2019-04-15 13:50:58 +0200

    WIP: add Abstract(Augmentable)DataObject
    These are abstract base classes for codec-derived implementations.
    
    // FIXME: augmentation access is borked a bit as AugmentationReader
              cannot be used due to "get" prefix method
    // FIXME: finish wiring up these classes by generating a template
    //        over them
    // FIXME: make sure the tests work
    // FIXME: test in multi-classloader environment
    
    Change-Id: I6acc34ab6b2cc62fd26dc34275259494ac4541fb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/AbstractAugmentableDataObject.java
@@ -0,0 +1,108 @@
+/**
+ * A base class for {@link DataObject}s which are also {@link Augmentable}, backed by {@link DataObjectCodecContext}.
+ * While this class is public, it not part of API surface and is an implementation detail. The only reason for it being
+ * public is that it needs to be accessible by code generated at runtime.
+ *
+ * @param <T> DataObject type
+ */
+package org.opendaylight.mdsal.binding.dom.codec.impl;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.collect.ImmutableClassToInstanceMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.binding.dom.codec.util.AugmentationReader;
+import org.opendaylight.mdsal.binding.spec.reflect.BindingReflections;
+import org.opendaylight.yangtools.yang.binding.Augmentable;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.OpaqueObject;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
+
+/**
+ * A base class for {@link OpaqueObject}s backed by {@link ForeignOpaqueData}. While this class is public, it not part
+ * of API surface and is an implementation detail. The only reason for it being public is that it needs to be accessible
+ * by code generated at runtime.
+ *
+ * @param <T> DataObject type
+ */
+public abstract class AbstractAugmentableDataObject<T extends DataObject & Augmentable<T>>
+        extends AbstractDataObject<T> implements Augmentable<T> {
+    @SuppressWarnings("rawtypes")
+    private static final AtomicReferenceFieldUpdater<AbstractDataObject, ImmutableClassToInstanceMap>
+            CACHED_AUGMENTATIONS_UPDATER = AtomicReferenceFieldUpdater.newUpdater(AbstractDataObject.class,
+                ImmutableClassToInstanceMap.class, "cachedAugmentations");
+    private volatile ImmutableClassToInstanceMap<Augmentation<T>> cachedAugmentations;
+
+    public AbstractAugmentableDataObject(final DataObjectCodecContext<T, ?> ctx,
+            final NormalizedNodeContainer<?, ?, ?> data) {
+        super(ctx, data);
+    }
+
+    @Override
+    public final <E$$ extends Augmentation<T>> @Nullable E$$ augmentation(final Class<E$$> augmentationType) {
+        requireNonNull(augmentationType, "Supplied augmentation must not be null.");
+
+        final ImmutableClassToInstanceMap<Augmentation<T>> aug = cachedAugmentations;
+        if (aug != null) {
+            return aug.getInstance(augmentationType);
+        }
+
+        @SuppressWarnings({"unchecked","rawtypes"})
+        final Optional<DataContainerCodecContext<?, ?>> optAugCtx = context.possibleStreamChild(
+            (Class) augmentationType);
+        if (optAugCtx.isPresent()) {
+            final DataContainerCodecContext<?, ?> augCtx = optAugCtx.get();
+            // Due to binding specification not representing grouping instantiations we can end up having the same
+            // augmentation applied to a grouping multiple times. While these augmentations have the same shape, they
+            // are still represented by distinct binding classes and therefore we need to make sure the result matches
+            // the augmentation the user is requesting -- otherwise a strict receiver would end up with a cryptic
+            // ClassCastException.
+            if (augmentationType.isAssignableFrom(augCtx.getBindingClass())) {
+                final Optional<NormalizedNode<?, ?>> augData = data.getChild(augCtx.getDomPathArgument());
+                if (augData.isPresent()) {
+                    return (E$$) augCtx.deserialize(augData.get());
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    final int codecAugmentedHashCode() {
+        return 31 * super.codecAugmentedHashCode() + augmentationsImpl().hashCode();
+    }
+
+    @Override
+    final boolean codecAugmentedEquals(final T other) {
+        return super.codecAugmentedEquals(other) && augmentationsImpl().equals(getAllAugmentations(other));
+    }
+
+    @Override
+    final ToStringHelper codecAugmentedFillToString(final ToStringHelper helper) {
+        return super.codecAugmentedFillToString(helper).add("augmentations", augmentationsImpl());
+    }
+
+    private Map<Class<? extends Augmentation<T>>, Augmentation<T>> augmentationsImpl() {
+        ImmutableClassToInstanceMap<Augmentation<T>> local = cachedAugmentations;
+        if (local != null) {
+            return local;
+        }
+
+        local = ImmutableClassToInstanceMap.copyOf(context.getAllAugmentationsFrom(data));
+        return CACHED_AUGMENTATIONS_UPDATER.compareAndSet(this, null, local) ? local : cachedAugmentations;
+    }
+
+    private static Map<Class<? extends Augmentation<?>>, Augmentation<?>> getAllAugmentations(
+            final Augmentable<?> dataObject) {
+        if (dataObject instanceof AugmentationReader) {
+            return ((AugmentationReader) dataObject).getAugmentations(dataObject);
+        }
+        return BindingReflections.getAugmentations(dataObject);
+    }
+}
