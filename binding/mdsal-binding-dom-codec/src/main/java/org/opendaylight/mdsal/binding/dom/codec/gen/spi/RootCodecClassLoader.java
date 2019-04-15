419626b362cdb4f27a0839e32c566d70ad8ae126
Robert Varga <robert.varga@pantheon.tech>
2019-04-15 10:02:03 +0200

    WIP: create a separate classloader for holding codec objects
    In order to make our code simpler and more performant, we can
    turn the use of java.lang.reflect.Proxy into generated classes,
    but in order to make things work correctly, we need to make sure
    we do not pollute the classpath.
    
    This patch introduces CodecClassLoader, which is a separate
    classloader, maintaining a connection between the codec class loader
    and classloaders of individual bindings. It also brings in
    Javassist, which we use to generate classes.
    
    As an example, ForeignOpaqueObject is converted from being a proxy
    to being an abstract base class, over which a concrete implementation
    is generated.
    
    This approach has a few advantages:
    - we do not generate a proxy class in public space, hence our mess
      will undergo normal GC rules
    - generated code is a normal final class, hence we can inherit
      method implementations from interfaces and subclasses
    - for each instance, we do not require a proxy and an invocation
      handler, so we instantiate only one object
    - there is no reflection in the invocation path, hence JIT has
      full (and direct) visibility and can perform all of its magic
    
    // FIXME: this needs to be tested in a multi-classloader environment
    
    Change-Id: I2be5bb88f9557375f517415d6bec9f68e803628c
    JIRA: MDSAL-442
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/gen/spi/RootCodecClassLoader.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.codec.gen.spi;
+
+import static com.google.common.base.Verify.verify;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import javassist.CtClass;
+import javassist.NotFoundException;
+import org.opendaylight.yangtools.yang.binding.DataContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class RootCodecClassLoader extends CodecClassLoader {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CodecClassLoader.class);
+
+    static {
+        verify(registerAsParallelCapable());
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static final AtomicReferenceFieldUpdater<RootCodecClassLoader, ImmutableMap> LOADERS_UPDATER =
+            AtomicReferenceFieldUpdater.newUpdater(RootCodecClassLoader.class, ImmutableMap.class, "loaders");
+
+    private volatile ImmutableMap<ClassLoader, CodecClassLoader> loaders = ImmutableMap.of();
+
+    RootCodecClassLoader() {
+        super(CodecClassLoader.class.getClassLoader());
+    }
+
+    @Override
+    CodecClassLoader findClassLoader(final Class<?> bindingClass) {
+        final ClassLoader target = bindingClass.getClassLoader();
+
+        // Cache for update
+        ImmutableMap<ClassLoader, CodecClassLoader> local = loaders;
+        final CodecClassLoader known = local.get(target);
+        if (known != null) {
+            return known;
+        }
+
+        // Alright, we need to determine if the class is accessible through our hierarchy (in which case we use
+        // ourselves) or we need to create a new Leaf.
+        final CodecClassLoader found;
+        if (!isOurClass(bindingClass)) {
+            verifyClassLoader(target);
+            found = new LeafCodecClassLoader(this, target);
+        } else {
+            found = this;
+        }
+
+        // Now make sure we cache this result
+        while (true) {
+            final Builder<ClassLoader, CodecClassLoader> builder = ImmutableMap.builderWithExpectedSize(
+                local.size() + 1);
+            builder.putAll(local);
+            builder.put(target, found);
+
+            if (LOADERS_UPDATER.compareAndSet(this, local, builder.build())) {
+                return found;
+            }
+
+            local = loaders;
+            final CodecClassLoader recheck = local.get(target);
+            if (recheck != null) {
+                return recheck;
+            }
+        }
+    }
+
+    @Override
+    CtClass getRootFrozen(final String name) throws NotFoundException {
+        return getLocalFrozen(name);
+    }
+
+    private boolean isOurClass(final Class<?> bindingClass) {
+        final Class<?> ourClass;
+        try {
+            ourClass = loadClass(bindingClass.getName(), false);
+        } catch (ClassNotFoundException e) {
+            LOG.debug("Failed to load class {}", e);
+            return false;
+        }
+        return bindingClass.equals(ourClass);
+    }
+
+    private static void verifyClassLoader(final ClassLoader target) {
+        // Sanity check: target has to resolve yang-binding contents to the same class, otherwise we are in a pickle
+        final Class<?> targetClazz;
+        try {
+            targetClazz = target.loadClass(DataContainer.class.getName());
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException("ClassLoader " + target + " cannot load " + DataContainer.class, e);
+        }
+        verify(DataContainer.class.equals(targetClazz),
+            "Class mismatch on DataContainer. Ours is from %s, target %s has %s from %s",
+            DataContainer.class.getClassLoader(), target, targetClazz, targetClazz.getClassLoader());
+    }
+}
