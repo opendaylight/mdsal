ed5716fa515d66d284d3f6a6f4b1866a74b79724
Robert Varga <robert.varga@pantheon.tech>
2019-07-08 11:24:06 +0200

    Remove unneeded null check
    We are performing a null and an instanceof check, where the latter
    can also serve as a a null check. Remove the explicit null check.
    
    Change-Id: Iac43aae00d8832cb7ade2f45041375451d5bedf4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/DOMActionRoutingTable.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/DOMActionRoutingTable.java
@@ -69,12 +69,10 @@ final class DOMActionRoutingTable extends AbstractDOMRoutingTable<DOMActionInsta
 
     private static ActionDefinition findActionDefinition(final SchemaContext context, final SchemaPath path) {
         final SchemaNode node = SchemaContextUtil.findDataSchemaNode(context, path.getParent());
-        if (node != null) {
-            if (node instanceof ActionNodeContainer) {
-                for (ActionDefinition action : ((ActionNodeContainer) node).getActions()) {
-                    if (action.getQName().equals(path.getLastComponent())) {
-                        return action;
-                    }
+        if (node instanceof ActionNodeContainer) {
+            for (ActionDefinition action : ((ActionNodeContainer) node).getActions()) {
+                if (action.getQName().equals(path.getLastComponent())) {
+                    return action;
                 }
             }
         }
