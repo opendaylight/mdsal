05865f5d08a39e58cfda49db54b9ad8213b90a47
Robert Varga <robert.varga@pantheon.tech>
2019-07-24 23:08:51 +0200

    Add a yanglib component
    This adds the basic bridging between YANG Library and yangtools,
    along with RFC7895 (non-NMDA) implementation. This allows schema
    mounts described through RFC7895 to be parsed, when the provided
    MountPointContextFactory implementation is fed into the parsers.
    
    JIRA: MDSAL-464
    Change-Id: I8092dd874648a4d115a5c16c3bbbbbc2b906419e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yanglib/mdsal-yanglib-api/src/main/java/org/opendaylight/mdsal/yanglib/api/SourceReference.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.yanglib.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.collect.ImmutableList;
+import java.net.URI;
+import java.util.Collection;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.concepts.AbstractIdentifiable;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+
+/**
+ * A reference to a YANG source, potentially containing a location hint.
+ */
+@Beta
+@NonNullByDefault
+public final class SourceReference extends AbstractIdentifiable<SourceIdentifier> implements Immutable {
+    // List vs. Set vs. Collection here is a class design decision based on use. We expect these objects to be used
+    // frequently, but for a short time. Locations may end up being ignored by SchemaContextResolver, hence it does not
+    // make sense to ensure a Set here just to de-duplicate URIs.
+    //
+    // This transports their iteration order, hence we accept Collection construction argument, but keep the immutable
+    // copy as a List
+    private final ImmutableList<URI> locations;
+
+    private SourceReference(final SourceIdentifier identifier, final ImmutableList<URI> locations) {
+        super(identifier);
+        this.locations = requireNonNull(locations);
+    }
+
+    public static SourceReference of(final SourceIdentifier identifier) {
+        return new SourceReference(identifier, ImmutableList.of());
+    }
+
+    public static SourceReference of(final SourceIdentifier identifier, final URI location) {
+        return new SourceReference(identifier, ImmutableList.of(location));
+    }
+
+    public static SourceReference of(final SourceIdentifier identifier, final Collection<URI> locations) {
+        return new SourceReference(identifier, ImmutableList.copyOf(locations));
+    }
+
+    public ImmutableList<URI> getLocations() {
+        return locations;
+    }
+
+    @Override
+    protected ToStringHelper addToStringAttributes(final ToStringHelper toStringHelper) {
+        super.addToStringAttributes(toStringHelper);
+        if (!locations.isEmpty()) {
+            toStringHelper.add("locations", locations);
+        }
+        return toStringHelper;
+    }
+}
