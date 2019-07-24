d10bb80d638e84b60c8cf35f1ffc5b5c9a2476ee
Robert Varga <robert.varga@pantheon.tech>
2019-07-24 23:08:51 +0200

    Add a yanglib component
    This adds the basic bridging between YANG Library and yangtools.
    
    JIRA: MDSAL-464
    Change-Id: I8092dd874648a4d115a5c16c3bbbbbc2b906419e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yanglib/mdsal-yanglib-rfc7895/src/main/java/org/opendaylight/mdsal/yanglib/rfc7895/YangModuleLibrarySupportFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.yanglib.rfc7895;
+
+import java.io.IOException;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.mdsal.yanglib.api.YangLibSupport;
+import org.opendaylight.mdsal.yanglib.api.YangLibSupportFactory;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParserException;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParserFactory;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+
+@MetaInfServices
+@NonNullByDefault
+public final class YangModuleLibrarySupportFactory implements YangLibSupportFactory {
+    @Override
+    public YangLibSupport createYangLibSupport(final YangParserFactory parserFactory)
+            throws YangSyntaxErrorException, YangParserException, IOException {
+        return new YangModuleLibrarySupport(parserFactory);
+
+    }
+}
