2fe82d9e78c22cb821e341ca38982d278bdf391a
Robert Varga <robert.varga@pantheon.tech>
2019-07-24 23:08:51 +0200

    Add a yanglib component
    This adds the basic bridging between YANG Library and yangtools.
    
    Change-Id: I8092dd874648a4d115a5c16c3bbbbbc2b906419e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yanglib/mdsal-yanglib-rfc7895/src/main/java/org/opendaylight/mdsal/yanglib/rfc7895/MountPointContextFactoryImpl.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.yanglib.rfc7895;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingDataObjectCodecTreeNode;
+import org.opendaylight.mdsal.yanglib.api.SchemaContextResolver;
+import org.opendaylight.mdsal.yanglib.api.SourceReference;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.ModulesState;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.RevisionUtils;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.module.list.CommonLeafs;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.module.list.Module;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.module.list.Module.ConformanceType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.module.list.module.Submodule;
+import org.opendaylight.yangtools.rcf8528.data.util.AbstractMountPointContextFactory;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointContextFactory;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointIdentifier;
+import org.opendaylight.yangtools.rfc8528.data.api.YangLibraryConstants.ContainerName;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParserException;
+import org.opendaylight.yangtools.yang.model.repo.api.RevisionSourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class MountPointContextFactoryImpl extends AbstractMountPointContextFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(MountPointContextFactoryImpl.class);
+
+    private final BindingDataObjectCodecTreeNode<ModulesState> moduleStateCodec;
+    private final EffectiveModelContext yangLibContext;
+    private final SchemaContextResolver resolver;
+
+    MountPointContextFactoryImpl(final MountPointIdentifier mountId, final SchemaContextResolver resolver,
+            final EffectiveModelContext yangLibContext,
+            final BindingDataObjectCodecTreeNode<ModulesState> moduleStateCodec) {
+        super(mountId);
+        this.resolver = requireNonNull(resolver);
+        this.yangLibContext = requireNonNull(yangLibContext);
+        this.moduleStateCodec = requireNonNull(moduleStateCodec);
+    }
+
+    @Override
+    protected MountPointContextFactory createContextFactory(final MountPointDefinition mountPoint) {
+        return new MountPointContextFactoryImpl(mountPoint.getIdentifier(), resolver, yangLibContext, moduleStateCodec);
+    }
+
+    @Override
+    protected Optional<SchemaContext> findSchemaForLibrary(final ContainerName containerName) {
+        switch (containerName) {
+            case RFC7895:
+                return Optional.of(yangLibContext);
+            default:
+                LOG.debug("Unhandled YANG library container {}", containerName);
+                return Optional.empty();
+        }
+    }
+
+    @Override
+    protected SchemaContext bindLibrary(final ContainerName containerName, final ContainerNode libData)
+            throws YangParserException {
+        checkArgument(containerName == ContainerName.RFC7895, "Unsupported container type %s", containerName);
+        checkArgument(ModulesState.QNAME.equals(libData.getNodeType()), "Unexpected container %s", libData);
+
+        // DOM-to-Binding magic
+        return bindLibrary(verifyNotNull(moduleStateCodec.deserialize(libData)));
+    }
+
+    private @NonNull SchemaContext bindLibrary(final @NonNull ModulesState modState) throws YangParserException {
+        final List<SourceReference> requiredSources = new ArrayList<>();
+        final List<SourceReference> librarySources = new ArrayList<>();
+
+        for (Module module : modState.nonnullModule()) {
+            final SourceReference modRef = sourceRefFor(module, module.getSchema());
+
+            // TODO: take deviations/features into account
+
+            if (ConformanceType.Import == module.getConformanceType()) {
+                librarySources.add(modRef);
+            } else {
+                requiredSources.add(modRef);
+            }
+
+            for (Submodule submodule : module.nonnullSubmodule()) {
+                // Submodules go to library, as they are pulled in as needed
+                librarySources.add(sourceRefFor(submodule, submodule.getSchema()));
+            }
+        }
+
+        return resolver.resolveSchemaContext(librarySources, requiredSources);
+    }
+
+    private static SourceReference sourceRefFor(final CommonLeafs obj, final Uri uri) {
+        final SourceIdentifier sourceId = RevisionSourceIdentifier.create(obj.getName().getValue(),
+            RevisionUtils.toYangCommon(obj.getRevision()));
+        return uri == null ? SourceReference.of(sourceId) : SourceReference.of(sourceId, URI.create(uri.getValue()));
+    }
+}
---
+++ yanglib/mdsal-yanglib-rfc7895/src/main/java/org/opendaylight/mdsal/yanglib/rfc7895/YangModuleLibrarySupport.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.yanglib.rfc7895;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.Collections2;
+import java.io.IOException;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTree;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingDataObjectCodecTreeNode;
+import org.opendaylight.mdsal.binding.dom.codec.impl.BindingNormalizedNodeCodecRegistry;
+import org.opendaylight.mdsal.binding.generator.util.BindingRuntimeContext;
+import org.opendaylight.mdsal.yanglib.api.SchemaContextResolver;
+import org.opendaylight.mdsal.yanglib.api.YangLibSupport;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.$YangModuleInfoImpl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.library.rev160621.ModulesState;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointContextFactory;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParserException;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParserFactory;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.model.repo.api.RevisionSourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+
+@Beta
+@NonNullByDefault
+public final class YangModuleLibrarySupport implements YangLibSupport {
+    private final BindingDataObjectCodecTreeNode<ModulesState> moduleStateCodec;
+    private final EffectiveModelContext context;
+
+    private YangModuleLibrarySupport(final EffectiveModelContext context,
+            final BindingDataObjectCodecTreeNode<ModulesState> moduleStateCodec) {
+        this.context = requireNonNull(context);
+        this.moduleStateCodec = requireNonNull(moduleStateCodec);
+    }
+
+    public static YangLibSupport create(final YangParserFactory parserFactory) throws YangSyntaxErrorException,
+            YangParserException, IOException {
+        final YangModuleInfo yangLibModule = $YangModuleInfoImpl.getInstance();
+        final EffectiveModelContext context = parserFactory.createParser()
+                .addLibSources(Collections2.transform(yangLibModule.getImportedModules(),
+                    YangModuleLibrarySupport::createSource))
+                .addSource(createSource(yangLibModule))
+                .buildEffectiveModel();
+        final BindingCodecTree codecTree = new BindingNormalizedNodeCodecRegistry(BindingRuntimeContext.create(
+            SimpleStrategy.INSTANCE, context)).getCodecContext();
+        return new YangModuleLibrarySupport(context,
+            verifyNotNull(codecTree.getSubtreeCodec(InstanceIdentifier.create(ModulesState.class))));
+    }
+
+    @Override
+    public MountPointContextFactory createMountPointContextFactory(final MountPointIdentifier mountId,
+            final SchemaContextResolver resolver) {
+        return new MountPointContextFactoryImpl(mountId, resolver, context, moduleStateCodec);
+    }
+
+    private static YangTextSchemaSource createSource(final YangModuleInfo info) {
+        return YangTextSchemaSource.delegateForByteSource(
+            RevisionSourceIdentifier.create(info.getName().getLocalName(), info.getName().getRevision()),
+            info.getYangTextByteSource());
+    }
+}
