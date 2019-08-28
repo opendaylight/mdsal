a111ac5e657b7aaabb7d3c67d837f6dde378f8d9
Robert Varga <robert.varga@pantheon.tech>
2019-08-28 17:34:08 +0200

    Expose service group name from ServiceGroupIdentifier
    AbstractIdentifier has removed public view of getValue(), hence
    we need to expose a domain-specific accessor. This patch does that
    and fixes up users.
    
    Change-Id: I1645e86e5417e0a8c13660f8820a9df8b4c7cad7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- singleton-service/mdsal-singleton-common-api/src/main/java/org/opendaylight/mdsal/singleton/common/api/ServiceGroupIdentifier.java
+++ singleton-service/mdsal-singleton-common-api/src/main/java/org/opendaylight/mdsal/singleton/common/api/ServiceGroupIdentifier.java
@@ -5,30 +5,32 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.mdsal.singleton.common.api;
 
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.util.AbstractStringIdentifier;
 
 /**
  * Identifier represents a service group competence. It's based on String.
  */
 public class ServiceGroupIdentifier extends AbstractStringIdentifier<ServiceGroupIdentifier> {
+    private static final long serialVersionUID = 6853612584804702662L;
+
+    protected ServiceGroupIdentifier(final @NonNull String string) {
+        super(string);
+    }
 
     /**
      * Method to create immutable instances of {@link ServiceGroupIdentifier}.
      *
-     * @param serviceGroupIdentifier the String identifier for the ServiceGroupIdentifier instance
+     * @param name the String identifier for the ServiceGroupIdentifier instance
      * @return {@link ServiceGroupIdentifier} new instance
      */
-    public static ServiceGroupIdentifier create(final String serviceGroupIdentifier) {
-        return new ServiceGroupIdentifier(serviceGroupIdentifier);
+    public static @NonNull ServiceGroupIdentifier create(final String name) {
+        return new ServiceGroupIdentifier(name);
     }
 
-    protected ServiceGroupIdentifier(final String string) {
-        super(string);
+    public final @NonNull String getName() {
+        return getValue();
     }
-
-    private static final long serialVersionUID = 6853612584804702662L;
-
 }
