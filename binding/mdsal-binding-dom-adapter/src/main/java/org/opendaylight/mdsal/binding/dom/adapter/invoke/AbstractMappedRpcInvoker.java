617b1705ebec65dec46a2641849223ad5e3141b6
Robert Varga <robert.varga@pantheon.tech>
2019-09-06 15:24:32 +0200

    WIP: expose RpcMethodInvoker
    As a first step to refactoring BindingDOMRpcImplementationAdapter,
    we need to expose RpcMethodInvoker, so it can be kept on the path.
    
    // FIXME: next steps:
    //        - expose the ability to look up the RPC codec
    //        - create a Map of bound codecs
    //        - register them independently
    
    JIRA: MDSAL-86
    Change-Id: I847952b1fa7496381567dba78cf966067a61cd9e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/invoke/AbstractMappedRpcInvoker.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/invoke/AbstractMappedRpcInvoker.java
@@ -7,21 +7,16 @@
  */
 package org.opendaylight.mdsal.binding.dom.adapter.invoke;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
-import com.google.common.util.concurrent.ListenableFuture;
 import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.Map.Entry;
 import org.opendaylight.mdsal.binding.spec.reflect.BindingReflections;
-import org.opendaylight.yangtools.yang.binding.DataObject;
-import org.opendaylight.yangtools.yang.binding.RpcService;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,12 +43,7 @@ abstract class AbstractMappedRpcInvoker<T> extends RpcServiceInvoker {
     protected abstract T qnameToKey(QName qname);
 
     @Override
-    public final ListenableFuture<RpcResult<?>> invokeRpc(final RpcService impl, final QName rpcName,
-            final DataObject input) {
-        requireNonNull(impl, "Implementation must be supplied");
-
-        RpcMethodInvoker invoker = map.get(qnameToKey(rpcName));
-        checkArgument(invoker != null, "Supplied RPC is not valid for implementation %s", impl);
-        return invoker.invokeOn(impl, input);
+    public final RpcMethodInvoker getMethodInvoker(final QName rpcName) {
+        return map.get(qnameToKey(requireNonNull(rpcName)));
     }
 }
