c79bcc82cadf2397fbc7c071043845d2661cb51c
Robert Varga <robert.varga@pantheon.tech>
2019-09-30 07:08:26 +0200

    Make DOMSchemaService operate of EffectiveModelContext
    We are stepping towards working on Declared/EffectiveStatements
    instead of SchemaNodes, this exposes EffectiveModelContext from
    the DOMSchemaService.
    
    JIRA: MDSAL-435
    Change-Id: I6b24f423e104683e008a97595647a7b9ce4d3a11
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-inmemory-datastore/src/test/java/org/opendaylight/controller/md/sal/dom/store/impl/TestModel.java
+++ dom/mdsal-dom-inmemory-datastore/src/test/java/org/opendaylight/controller/md/sal/dom/store/impl/TestModel.java
@@ -9,7 +9,7 @@ package org.opendaylight.controller.md.sal.dom.store.impl;
 
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 public final class TestModel {
@@ -41,7 +41,7 @@ public final class TestModel {
         throw new UnsupportedOperationException();
     }
 
-    public static SchemaContext createTestContext() {
+    public static EffectiveModelContext createTestContext() {
         return YangParserTestUtils.parseYangResource("/odl-datastore-test.yang");
     }
 }
