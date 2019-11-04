5ecca8e21a22bacef3dcb518799aa08458f7553a
Thanh Ha <zxiiro@gmail.com>
2019-11-04 11:11:50 -0500

    Parse docs version of pom.xml
    Issue: MDSAL-489
    Signed-off-by: Thanh Ha <zxiiro@gmail.com>
    Change-Id: Ie32ef32b3d89e98b9ea432bd8e2ac5ea91bf13b9

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,20 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
+import xml.etree.ElementTree as ET
+import subprocess
+
 from docs_conf.conf import *
+
+def format_version(version):
+    fmt = '{tag}.dev{commitcount}+{gitsha}'
+    parts = version.split('-')
+    assert len(parts) in (3, 4)
+    dirty = len(parts) == 4
+    tag, count, sha = parts[:3]
+    if count == '0' and not dirty:
+        return tag
+    return fmt.format(tag=tag, commitcount=count, gitsha=sha.lstrip('g'))
 
 extensions.append('sphinx.ext.extlinks')
 extensions.append('sphinxcontrib.plantuml')
@@ -18,3 +31,8 @@ extensions.append('sphinxcontrib.plantuml')
 extlinks = {
     'mdsal-apidoc': ('https://github.com/sphinx-doc/sphinx/issues/%s', 'api '),
 }
+
+data = ET.parse('pom.xml')
+mdsal_version = data.getroot().find('*//{http://maven.apache.org/POM/4.0.0}version').text
+version = mdsal_version
+release = mdsal_version
