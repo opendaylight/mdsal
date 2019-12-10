97e1a916ffcee961f160a20b3c89cd3f0ea3373d
Robert Varga <robert.varga@pantheon.tech>
2019-12-10 11:38:57 +0100

    WIP: instsance notification binding-api support
    Change-Id: If308adb3a26964cea90456f8fb020b7364c937db
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/NotificationService.java
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/NotificationService.java
@@ -7,8 +7,16 @@
  */
 package org.opendaylight.mdsal.binding.api;
 
+import java.util.EventListener;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.Identifiable;
+import org.opendaylight.yangtools.yang.binding.Identifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceNotification;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.Notification;
 import org.opendaylight.yangtools.yang.binding.NotificationListener;
 
 /**
@@ -94,4 +102,33 @@ public interface NotificationService extends BindingService {
      *         by invoking the {@link ListenerRegistration#close()} method when no longer needed.
      */
     <T extends NotificationListener> @NonNull ListenerRegistration<T> registerNotificationListener(@NonNull T listener);
+
+    <N extends Notification, T extends Listener<N>> @NonNull ListenerRegistration<T> registerListener(Class<N> type,
+            T listener);
+
+    <P extends DataObject, N extends InstanceNotification<N, P>, T extends InstanceListener<P, N>>
+        @NonNull ListenerRegistration<T> registerListener(Class<N> type, InstanceIdentifier<P> path, T listener);
+
+    <P extends DataObject & Identifiable<K>, N extends InstanceNotification<N, P>, K extends Identifier<P>,
+        T extends KeyedListListener<P, N, K>> @NonNull ListenerRegistration<T> registerListener(Class<N> type,
+                KeyedInstanceIdentifier<P, K> path, T listener);
+
+    @FunctionalInterface
+    interface Listener<N extends Notification> extends EventListener {
+
+        void onNotification(@NonNull N Notification);
+    }
+
+    @FunctionalInterface
+    interface InstanceListener<P extends DataObject, N extends InstanceNotification<N, P>> extends EventListener {
+
+        void onNotification(@NonNull InstanceIdentifier<P> path, @NonNull N notification);
+    }
+
+    @FunctionalInterface
+    interface KeyedListListener<P extends DataObject & Identifiable<K>, N extends InstanceNotification<N, P>,
+            K extends Identifier<P>> extends EventListener {
+
+        void onNotification(@NonNull KeyedInstanceIdentifier<P, K> path, @NonNull N notification);
+    }
 }
