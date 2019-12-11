3834c8ba1f0e9d195dc935a942d50630f88200fb
Robert Varga <robert.varga@pantheon.tech>
2019-12-11 01:21:18 +0100

    WIP: Add mdsal-{dom,binding}-testkit(-spi)
    Rather than providing abstract classes for subclassing by user
    tests, we introduce TestKit classes, which provide MD-SAL services
    with proper interfaces to control data flows.
    
    This allows downstreams to properly dispatch events, so that some
    of the uncertainities (mostly around DataTreeChangeListener) can
    be properly asserted.
    
    There are two individual testkits (dom, binding), where each is composed
    of two artifacts:
    
    1) odl-mdsal-{dom,binding}-testkit-spi, which holds the abstract
       definition of a testkit, along with reference testing entry points.
       IMDS is provided as the default DOMStore provider and the appropriate
       mdsal-binding-dom-codec instantiation.
    
    2) odl-mdsal-{dom,binding}-testkit, which provides concre
       of MD-SAL services, specializing the -spi contract to work on top
       of mdsal-binding-dom-adapter.
    
    Binding testkit is structured as an overlay on top of a DOM testkit,
    hence the setup mirrors a typical single-node stateless deployment.
    
    JIRA: MDSAL-393
    Change-Id: If6f979b0d1303f2efaaece4a954cc93d3da91400
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-testkit-spi/src/main/java/org/opendaylight/mdsal/dom/testkit/spi/ModelTestKit.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.testkit.spi;
+
+import com.google.common.annotations.Beta;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableSet;
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.generator.impl.GeneratedClassLoadingStrategy;
+import org.opendaylight.mdsal.binding.generator.impl.ModuleInfoBackedContext;
+import org.opendaylight.mdsal.binding.spec.reflect.BindingReflections;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+
+/**
+ * A simple testkit which maintains a lazily-populated {@link EffectiveModelContext} based either on an explicit list of
+ * {@link YangModuleInfo} instances or based on all instances discovered through Thread Context Class Loader at
+ * instantiation time.
+ */
+@Beta
+public class ModelTestKit {
+    private static final LoadingCache<Set<YangModuleInfo>, EffectiveModelContext> MODULE_CONTEXT_CACHE =
+            CacheBuilder.newBuilder().weakValues().build(new CacheLoader<Set<YangModuleInfo>, EffectiveModelContext>() {
+                @Override
+                public EffectiveModelContext load(final Set<YangModuleInfo> key) {
+                    return ModuleInfoBackedContext.cacheContext(
+                        GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy(), ImmutableSet.copyOf(key))
+                            .tryToCreateModelContext().get();
+                }
+            });
+
+    private final ImmutableSet<YangModuleInfo> moduleInfos;
+
+    private volatile EffectiveModelContext modelContext;
+
+    public ModelTestKit() {
+        this(BindingReflections.cacheModuleInfos(Thread.currentThread().getContextClassLoader()));
+    }
+
+    public ModelTestKit(final Set<YangModuleInfo> moduleInfos) {
+        this.moduleInfos = ImmutableSet.copyOf(moduleInfos);
+    }
+
+    public final @NonNull EffectiveModelContext effectiveModelContext() {
+        EffectiveModelContext local = modelContext;
+        if (local == null) {
+            synchronized (this) {
+                local = modelContext;
+                if (local == null) {
+                    modelContext = local = MODULE_CONTEXT_CACHE.getUnchecked(moduleInfos);
+                }
+            }
+        }
+        return local;
+    }
+}
