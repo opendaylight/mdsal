fe2486f3e5af2fcdb58fb9f4817727b2980381f1
Robert Varga <robert.varga@pantheon.tech>
2019-12-11 01:21:18 +0100

    WIP: Add mdsal-{dom,binding}-testkit(-spi)
    Rather than providing abstract classes for subclassing by user
    tests, we introduce TestKit classes, which provide MD-SAL services
    with proper interfaces to control data flows.
    
    This allows downstreams to properly dispatch events, so that some
    of the uncertainities (mostly around DataTreeChangeListener) can
    be properly asserted.
    
    There are two individual testkits (dom, binding), where each is composed
    of two artifacts:
    
    1) odl-mdsal-{dom,binding}-testkit-spi, which holds the abstract
       definition of a testkit, along with reference testing entry points.
       IMDS is provided as the default DOMStore provider and the appropriate
       mdsal-binding-dom-codec instantiation.
    
    2) odl-mdsal-{dom,binding}-testkit, which provides concre
       of MD-SAL services, specializing the -spi contract to work on top
       of mdsal-binding-dom-adapter.
    
    Binding testkit is structured as an overlay on top of a DOM testkit,
    hence the setup mirrors a typical single-node stateless deployment.
    
    JIRA: MDSAL-393
    Change-Id: If6f979b0d1303f2efaaece4a954cc93d3da91400
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-testkit-spi/src/main/java/org/opendaylight/mdsal/binding/testkit/spi/AbstractTestKit.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.testkit.spi;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataTreeChangeListener;
+import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.mdsal.binding.dom.codec.impl.BindingNormalizedNodeCodecRegistry;
+import org.opendaylight.mdsal.binding.generator.impl.GeneratedClassLoadingStrategy;
+import org.opendaylight.mdsal.binding.generator.util.BindingRuntimeContext;
+import org.opendaylight.mdsal.dom.testkit.DOMTestKit;
+import org.opendaylight.mdsal.dom.testkit.spi.AbstractDOMTestKit.ListenerPolicy;
+import org.opendaylight.mdsal.dom.testkit.spi.AbstractListenerEvent;
+
+@Beta
+public abstract class AbstractTestKit<D extends DataBroker> implements AutoCloseable {
+
+    @FunctionalInterface
+    @NonNullByDefault
+    public interface ListenerClassifier {
+
+        ListenerPolicy policyFor(DataTreeIdentifier<?> treeId, DataTreeChangeListener<?> listener);
+    }
+
+    @NonNullByDefault
+    public abstract static class ListenerEvent extends AbstractListenerEvent<DataTreeChangeListener<?>> {
+        ListenerEvent(final DataTreeChangeListener<?> listener, final Object marker) {
+            super(marker, listener);
+        }
+
+        abstract void fire();
+    }
+
+    private final ListenerClassifier classifier;
+    private final DOMTestKit domTestKit;
+
+    private volatile CapturingDataBrokerImpl<D> dataBroker;
+    private volatile BindingNormalizedNodeCodecRegistry codecRegistry;
+
+    protected AbstractTestKit() {
+        this(new DOMTestKit());
+    }
+
+    protected AbstractTestKit(final DOMTestKit domTestKit) {
+        this(domTestKit, (treeId, listener) -> ListenerPolicy.PASS);
+    }
+
+    protected AbstractTestKit(final DOMTestKit domTestKit,
+            final ListenerClassifier classifier) {
+        this.domTestKit = requireNonNull(domTestKit);
+        this.classifier = requireNonNull(classifier);
+    }
+
+    public final DOMTestKit domTestKit() {
+        return domTestKit;
+    }
+
+    public abstract BindingNormalizedNodeSerializer nodeSerializer();
+
+    public final BindingNormalizedNodeCodecRegistry codecRegistry() {
+        BindingNormalizedNodeCodecRegistry local = codecRegistry;
+        if (local == null) {
+            synchronized (this) {
+                local = codecRegistry;
+                if (local == null) {
+                    codecRegistry = local = new BindingNormalizedNodeCodecRegistry(
+                        BindingRuntimeContext.create(GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy(),
+                            domTestKit.effectiveModelContext()));
+                }
+            }
+        }
+        return local;
+    }
+
+    public final CapturingDataBroker dataBroker() {
+        CapturingDataBrokerImpl<D> local = dataBroker;
+        if (local == null) {
+            synchronized (this) {
+                local = dataBroker;
+                if (local == null) {
+                    dataBroker = local = new CapturingDataBrokerImpl<>(createDataBroker(), classifier);
+                }
+            }
+        }
+        return local;
+    }
+
+    @Override
+    public void close()  {
+        if (dataBroker != null) {
+            closeDataBroker(dataBroker.delegate());
+        }
+     }
+
+    protected abstract @NonNull D createDataBroker();
+
+    protected abstract void closeDataBroker(D dataBroker);
+}
