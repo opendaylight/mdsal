8fe1a3f52497707d9a2e920f3a7b0739674ed41e
Robert Varga <robert.varga@pantheon.tech>
2019-12-11 01:21:18 +0100

    WIP: Add mdsal-{dom,binding}-testkit(-spi)
    Rather than providing abstract classes for subclassing by user
    tests, we introduce TestKit classes, which provide MD-SAL services
    with proper interfaces to control data flows.
    
    This allows downstreams to properly dispatch events, so that some
    of the uncertainities (mostly around DataTreeChangeListener) can
    be properly asserted.
    
    There are two individual testkits (dom, binding), where each is composed
    of two artifacts:
    
    1) odl-mdsal-{dom,binding}-testkit-spi, which holds the abstract
       definition of a testkit, along with reference testing entry points.
       IMDS is provided as the default DOMStore provider and the appropriate
       mdsal-binding-dom-codec instantiation.
    
    2) odl-mdsal-{dom,binding}-testkit, which provides concre
       of MD-SAL services, specializing the -spi contract to work on top
       of mdsal-binding-dom-adapter.
    
    Binding testkit is structured as an overlay on top of a DOM testkit,
    hence the setup mirrors a typical single-node stateless deployment.
    
    JIRA: MDSAL-393
    Change-Id: If6f979b0d1303f2efaaece4a954cc93d3da91400
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-testkit-spi/src/main/java/org/opendaylight/mdsal/dom/testkit/spi/CapturingDOMDataBrokerImpl.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.testkit.spi;
+
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableClassToInstanceMap;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMDataBrokerExtension;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeChangeService;
+import org.opendaylight.mdsal.dom.spi.ForwardingDOMDataBroker;
+import org.opendaylight.mdsal.dom.testkit.spi.AbstractDOMTestKit.ListenerClassifier;
+
+final class CapturingDOMDataBrokerImpl<D extends DOMDataBroker> extends ForwardingDOMDataBroker
+        implements CapturingDOMDataBroker {
+    private final @NonNull D delegate;
+    private final @Nullable FilteringDOMDataTreeChangeService dtcs;
+
+    CapturingDOMDataBrokerImpl(final D delegate, final ListenerClassifier classifier) {
+        this.delegate = requireNonNull(delegate);
+        final DOMDataTreeChangeService delegateDtcs = delegate.getExtensions()
+                .getInstance(DOMDataTreeChangeService.class);
+        this.dtcs = delegateDtcs == null ? null : new FilteringDOMDataTreeChangeService(delegateDtcs, classifier);
+    }
+
+    @Override
+    public ImmutableClassToInstanceMap<DOMDataBrokerExtension> getExtensions() {
+        // Local temporary to keep Eclipse happy
+        final DOMDataTreeChangeService local = dtcs;
+        return local == null ? ImmutableClassToInstanceMap.of()
+                : ImmutableClassToInstanceMap.of(DOMDataTreeChangeService.class, local);
+    }
+
+    @Override
+    public void fireCapturedEvents(final EventPredicate predicate) {
+        checkState(dtcs != null, "Attempted to filter events without a DataTreeChangeService");
+        dtcs.fireCapturedEvents(predicate);
+    }
+
+    @Override
+    protected D delegate() {
+        return delegate;
+    }
+}
---
+++ dom/mdsal-dom-testkit/src/main/java/org/opendaylight/mdsal/dom/testkit/DOMTestKit.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.testkit;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.Map;
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMActionProviderService;
+import org.opendaylight.mdsal.dom.api.DOMActionService;
+import org.opendaylight.mdsal.dom.api.DOMRpcProviderService;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
+import org.opendaylight.mdsal.dom.api.DOMService;
+import org.opendaylight.mdsal.dom.broker.DOMRpcRouter;
+import org.opendaylight.mdsal.dom.broker.SerializedDOMDataBroker;
+import org.opendaylight.mdsal.dom.spi.store.DOMStore;
+import org.opendaylight.mdsal.dom.testkit.spi.AbstractDOMTestKit;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+
+/**
+ * A testkit which creates on-demand common {@link DOMService}s for use with tests which need fully operational
+ * instances.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public class DOMTestKit extends AbstractDOMTestKit<SerializedDOMDataBroker> {
+    private static final @NonNull ImmutableSet<LogicalDatastoreType> BOTH_DATASTORES = ImmutableSet.of(
+        LogicalDatastoreType.CONFIGURATION,
+        LogicalDatastoreType.OPERATIONAL);
+
+    private volatile DOMRpcRouter domRpcRouter;
+
+    public DOMTestKit() {
+
+    }
+
+    public DOMTestKit(final Set<LogicalDatastoreType> datastoreTypes) {
+        super(datastoreTypes);
+    }
+
+    public DOMTestKit(final Set<LogicalDatastoreType> datastoreTypes, final ListenerClassifier classifier) {
+        super(datastoreTypes, classifier);
+    }
+
+    public DOMTestKit(final Set<YangModuleInfo> moduleInfos, final Set<LogicalDatastoreType> datastoreTypes) {
+        super(moduleInfos, datastoreTypes);
+    }
+
+    public DOMTestKit(final Set<YangModuleInfo> moduleInfos, final Set<LogicalDatastoreType> datastoreTypes,
+            final ListenerClassifier classifier) {
+        super(moduleInfos, datastoreTypes, classifier);
+    }
+
+    @Override
+    public final DOMActionProviderService domActionProviderService() {
+        return rpcRouter().getActionProviderService();
+    }
+
+    @Override
+    public final DOMActionService domActionService() {
+        return rpcRouter().getActionService();
+    }
+
+    @Override
+    public final DOMRpcProviderService domRpcProviderService() {
+        return rpcRouter().getRpcProviderService();
+    }
+
+    @Override
+    public final DOMRpcService domRpcService() {
+        return rpcRouter().getRpcService();
+    }
+
+    @Override
+    protected final SerializedDOMDataBroker createDomDataBroker(final Map<LogicalDatastoreType, DOMStore> datastores) {
+        return new SerializedDOMDataBroker(datastores, MoreExecutors.newDirectExecutorService());
+    }
+
+    @Override
+    protected final void closeDomDataBroker(final SerializedDOMDataBroker dataBroker) {
+        dataBroker.close();
+    }
+
+    private DOMRpcRouter rpcRouter() {
+        DOMRpcRouter local = domRpcRouter;
+        if (local == null) {
+            synchronized (this) {
+                local = domRpcRouter;
+                if (local == null) {
+                    domRpcRouter = local = DOMRpcRouter.newInstance(domSchemaService());
+                }
+            }
+        }
+        return local;
+    }
+}
