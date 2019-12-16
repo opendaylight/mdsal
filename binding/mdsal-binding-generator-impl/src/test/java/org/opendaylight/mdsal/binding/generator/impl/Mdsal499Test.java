80bc648855e67a560bba6a14150f8fe8fa618c04
miroslav.kovac <miroslav.kovac@pantheon.tech>
2019-12-16 16:57:22 +0100

    Unit test testing prefix of imported module in submodule
    JIRA: MDSAL-499
    Change-Id: Ifa8a8958d7118d851d27fcc2c70c6d8382e3e3c0
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal499Test.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class Mdsal499Test {
+
+    @Test
+    public void testSubmoduleImport() {
+        SchemaContext context = YangParserTestUtils.parseYangResourceDirectory("/mdsal-499");
+
+        List<Type> generateTypes = new BindingGeneratorImpl().generateTypes(context);
+        assertNotNull(generateTypes);
+        assertEquals(5, generateTypes.size());
+    }
+}
