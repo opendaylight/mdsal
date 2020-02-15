b3d7f2e1cd94d1759bd62af3be6832ec2dabe53c
Robert Varga <robert.varga@pantheon.tech>
2020-02-15 20:53:55 +0100

    Add DOMRpcProviderService bulk registration
    binding-dom-adapter will need the ability to atomically register
    a number of implementations. This patch adds that capability.
    
    JIRA: MDSAL-86
    Change-Id: Ia948d449ad83b79ca833d5510a4885493b53e202
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ilya Igushev <illia.ihushev@pantheon.tech>

--- dom/mdsal-dom-spi/src/test/java/org/opendaylight/mdsal/dom/spi/AbstractDOMRpcProviderServiceTest.java
+++ dom/mdsal-dom-spi/src/test/java/org/opendaylight/mdsal/dom/spi/AbstractDOMRpcProviderServiceTest.java
@@ -11,12 +11,14 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import java.util.Map;
 import java.util.Set;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.opendaylight.mdsal.dom.api.DOMRpcIdentifier;
 import org.opendaylight.mdsal.dom.api.DOMRpcImplementation;
 import org.opendaylight.mdsal.dom.api.DOMRpcImplementationRegistration;
+import org.opendaylight.yangtools.concepts.Registration;
 
 public class AbstractDOMRpcProviderServiceTest extends AbstractDOMRpcProviderService {
 
@@ -34,5 +36,10 @@ public class AbstractDOMRpcProviderServiceTest extends AbstractDOMRpcProviderSer
     public <T extends DOMRpcImplementation> DOMRpcImplementationRegistration<T> registerRpcImplementation(
             final T implementation, final Set<DOMRpcIdentifier> rpcs) {
         return domRpcImplementationRegistration;
+    }
+
+    @Override
+    public Registration registerRpcImplementations(Map<DOMRpcIdentifier, DOMRpcImplementation> map) {
+        throw new UnsupportedOperationException();
     }
 }
