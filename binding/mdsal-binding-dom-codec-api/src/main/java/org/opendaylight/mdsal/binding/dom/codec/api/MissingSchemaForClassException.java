320b0d36b059e1010870c73ea935f47932cdafbd
Robert Varga <robert.varga@pantheon.tech>
2020-02-18 00:53:20 +0100

    Promote codec exceptions to binding.codec.api
    This exception should be part of documented API contract, promote
    it.
    
    JIRA: MDSAL-392
    Change-Id: Ia0985e0399468ee3f57a4a50f65b38085245edeb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-codec-api/src/main/java/org/opendaylight/mdsal/binding/dom/codec/api/MissingSchemaForClassException.java
+++ binding/mdsal-binding-dom-codec-api/src/main/java/org/opendaylight/mdsal/binding/dom/codec/api/MissingSchemaForClassException.java
@@ -5,13 +5,12 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.mdsal.binding.dom.codec.impl;
+package org.opendaylight.mdsal.binding.dom.codec.api;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.annotations.Beta;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import org.opendaylight.mdsal.binding.generator.util.BindingRuntimeContext;
-import org.opendaylight.yangtools.yang.binding.Augmentation;
 
 /**
  * Thrown when Java Binding class was used in data for which codec does not have schema.
@@ -19,35 +18,19 @@ import org.opendaylight.yangtools.yang.binding.Augmentation;
  * <p>
  * By serialization /  deserialization of this exception {@link #getBindingClass()} will return null.
  */
+@Beta
 public final class MissingSchemaForClassException extends MissingSchemaException {
-
     private static final long serialVersionUID = 1L;
 
-    @SuppressFBWarnings("SE_TRANSIENT_FIELD_NOT_RESTORED")
+    @SuppressFBWarnings(value = "SE_TRANSIENT_FIELD_NOT_RESTORED", justification = "Documented in API contract")
     private final transient Class<?> bindingClass;
 
-    private MissingSchemaForClassException(final Class<?> clz) {
+    public MissingSchemaForClassException(final Class<?> clz) {
         super(String.format("Schema is not available for %s", clz));
         this.bindingClass = requireNonNull(clz);
     }
 
-    static MissingSchemaForClassException forClass(final Class<?> clz) {
-        return new MissingSchemaForClassException(clz);
-    }
-
     public Class<?> getBindingClass() {
         return bindingClass;
-    }
-
-    public static void check(final BindingRuntimeContext runtimeContext, final Class<?> bindingClass) {
-        final Object schema;
-        if (Augmentation.class.isAssignableFrom(bindingClass)) {
-            schema = runtimeContext.getAugmentationDefinition(bindingClass);
-        } else {
-            schema = runtimeContext.getSchemaDefinition(bindingClass);
-        }
-        if (schema == null) {
-            throw MissingSchemaForClassException.forClass(bindingClass);
-        }
     }
 }
