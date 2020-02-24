7d05c81515ff129f44771eaf3aae453efcbf4dcf
Robert Varga <robert.varga@pantheon.tech>
2020-02-24 12:40:56 +0100

    Rework binding component instantiation
    This reshuffles how DOM and Binding interoperate where schema is
    concerned. Since Binding requires Classloader information available
    at scanning time, we refactor mdsal-dom-schema-service-osgi to
    actually perform Binding-aware scanning, so that this information
    is available in ModuleInfoSnapshot.
    
    These are published as OSGiModuleInfoSnapshot along with generation
    number for easier reference by a new component, OSGiModelRuntime.
    
    DOMSchemaService is realized on top of ServiceRegistry, where it
    listens for OSGiModelRuntime and for SchemaContextListeners, driving
    an OSGi whiteboard pattern.
    
    This renders mdsal-binding-dom-codec-osgi completely superfluous,
    as there is not enough magic going on to justify it.
    
    mdsal-binding-runtime-osgi is also hooked onto OSGiModuleInfoSnapshot,
    providing BindingRuntimeContexts as needed and retaining generation
    inforation.
    
    JIRA: MDSAL-392
    Change-Id: I083caf3abbc3d7e2212d1df14e45d2745096b5f9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-schema-osgi/src/test/java/org/opendaylight/mdsal/dom/schema/osgi/impl/OSGiModelRuntimeTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.schema.osgi.impl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParserFactory;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentFactory;
+
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class OSGiModelRuntimeTest {
+    @Mock
+    private YangParserFactory parserFactory;
+    @Mock
+    private ComponentFactory contextFactory;
+    @Mock
+    private BundleContext bundleContext;
+
+    private OSGiModelRuntime target;
+
+    @Before
+    public void before() {
+        target = new OSGiModelRuntime();
+        target.parserFactory = parserFactory;
+        target.contextFactory = contextFactory;
+    }
+
+    @After
+    public void after() {
+        verifyNoMoreInteractions(parserFactory);
+        verifyNoMoreInteractions(contextFactory);
+        verifyNoMoreInteractions(bundleContext);
+    }
+
+    @Test
+    public void testActivate() {
+        doReturn(new Bundle[0]).when(bundleContext).getBundles();
+        doNothing().when(bundleContext).addBundleListener(any());
+        target.activate(bundleContext);
+    }
+
+    @Test
+    public void testDeactivate() {
+        testActivate();
+        doNothing().when(bundleContext).removeBundleListener(any());
+        target.deactivate();
+    }
+}
