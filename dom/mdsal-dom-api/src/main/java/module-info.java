7c755c4e419027b6d2aa2ea5ec8d88eef1e799c3
Robert Varga <robert.varga@pantheon.tech>
2020-02-24 11:16:37 +0100

    Turn mdsal-dom-api into a JPMS module
    This is a simple API module with only a few dependencies, turn it
    into an explicit module.
    
    Change-Id: Iab42d97e6101392e2d752c16f1c775366f41c276
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-api/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.mdsal.dom.api {
+    exports org.opendaylight.mdsal.dom.api;
+
+    requires org.opendaylight.yangtools.util;
+    requires transitive org.opendaylight.yangtools.yang.data.api;
+    requires transitive org.opendaylight.yangtools.yang.model.api;
+    requires transitive org.opendaylight.mdsal.common.api;
+
+    requires static transitive org.eclipse.jdt.annotation;
+}
