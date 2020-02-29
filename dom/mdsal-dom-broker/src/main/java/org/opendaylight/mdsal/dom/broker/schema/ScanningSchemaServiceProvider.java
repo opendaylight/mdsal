a0f9eb7e4252f5d0b285a8ff25f496075247d4e6
Robert Varga <robert.varga@pantheon.tech>
2020-02-29 11:57:05 +0100

    Use ListenerRegistry.clear()
    We do not need to access the registrations, just instruct the registry
    to clear them.
    
    Change-Id: I710989a5fe473135d5051ab127e23dc33e4227c5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/schema/ScanningSchemaServiceProvider.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/schema/ScanningSchemaServiceProvider.java
@@ -105,7 +105,7 @@ public class ScanningSchemaServiceProvider extends AbstractDOMSchemaService.With
     @Override
     public void close() {
         synchronized (lock) {
-            listeners.getRegistrations().forEach(ListenerRegistration::close);
+            listeners.clear();
         }
     }
 }
