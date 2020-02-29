188ae1f063b889ce2a7028c60e285bc1f1626e21
Robert Varga <robert.varga@pantheon.tech>
2020-02-29 09:10:04 +0100

    WIP: uses/augment/notification does not work
    Targeting a notification statement via augment in uses does not
    work, as it fails to look up.
    
    Change-Id: Iade8134b7a6ca6170c97dc2c539014bdb8e5e179
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal517Test.java
+++ binding/mdsal-binding-generator/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal517Test.java
@@ -21,6 +21,6 @@ public class Mdsal517Test extends AbstractOpaqueTest {
         final List<GeneratedType> types = DefaultBindingGenerator.generateFor(
                 YangParserTestUtils.parseYangResourceDirectory("/mdsal-517"));
         assertNotNull(types);
-        assertEquals(12, types.size());
+        assertEquals(14, types.size());
     }
 }
