a11a355a203886b1bdcd520cfe657e6eccd133d8
Robert Varga <robert.varga@pantheon.tech>
2020-03-01 13:14:00 +0100

    WIP: Add simple query language
    Applications want to perform rudimentary queries without having to
    perform the filtering by themselves.
    
    On the Binding layer this means providing enough expressive power
    to formulate such queries in a type-safe manner and translating
    them to their DOM counterparts. This has the benefit of allowing
    filtering to occur in DOM world, side-stepping the need to instantiate
    Binding objects.
    
    On the DOM layer the expression can be transmitted, hence it gives
    the possibility to move the execution to storage backend, thus
    reducing app/backend data interchange volume.
    
    // FIXME: finish DOM builder/query
    // FIXME: finish Binding implementation
    // FIXME: add tests for translation
    
    Change-Id: I51c8ec7e34c2485f62aeb5bdbe35fe1507cabaa9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ query/mdsal-query-dom-api/src/main/java/org/opendaylight/mdsal/query/dom/api/DOMQuery.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.query.dom.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+@Beta
+public final class DOMQuery implements Immutable {
+    private final @NonNull YangInstanceIdentifier root;
+    // Note: relative to root
+    private final @NonNull YangInstanceIdentifier select;
+    private final @NonNull ImmutableList<DOMQueryPredicate> predicates;
+
+    public DOMQuery(final YangInstanceIdentifier root, final YangInstanceIdentifier select,
+            final List<DOMQueryPredicate> predicates) {
+        this.root = requireNonNull(root);
+        this.select = requireNonNull(select);
+        this.predicates = ImmutableList.copyOf(predicates);
+    }
+}
