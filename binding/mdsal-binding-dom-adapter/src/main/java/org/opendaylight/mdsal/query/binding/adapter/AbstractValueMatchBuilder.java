cbbd4d4ea953e4b9433834d89468962fe7fa9c26
Robert Varga <robert.varga@pantheon.tech>
2020-03-01 13:14:00 +0100

    Add Binding/DOM Query language adapter
    Provide implementation of binding language translation on top
    of its DOM counterpart.
    
    On the DOM layer the expression can be transmitted, hence it gives
    the possibility to move the execution to storage backend, thus
    reducing app/backend data interchange volume.
    
    Change-Id: I51c8ec7e34c2485f62aeb5bdbe35fe1507cabaa9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 342ef68448beb2dbf1e7705671074ce461050948)

---
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/query/binding/adapter/AbstractValueMatchBuilder.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.query.binding.adapter;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.api.query.ValueMatch;
+import org.opendaylight.mdsal.binding.api.query.ValueMatchBuilder;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryPredicate;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryPredicate.Exists;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryPredicate.ValueEquals;
+import org.opendaylight.mdsal.query.binding.adapter.QueryBuilderState.BoundMethod;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+
+abstract class AbstractValueMatchBuilder<T extends DataObject, V> implements ValueMatchBuilder<T, V> {
+    private final QueryBuilderState builder;
+    private final InstanceIdentifier<T> select;
+    private final BoundMethod method;
+
+    AbstractValueMatchBuilder(final QueryBuilderState builder, final InstanceIdentifier<T> select,
+            final BoundMethod method) {
+        this.builder = requireNonNull(builder);
+        this.select = requireNonNull(select);
+        this.method = requireNonNull(method);
+    }
+
+    @Override
+    public final ValueMatch<T> nonNull() {
+        return withPredicate(new Exists(relativePath()));
+    }
+
+    @Override
+    public final ValueMatch<T> valueEquals(final V value) {
+        return withPredicate(new ValueEquals<>(relativePath(), value));
+    }
+
+    final YangInstanceIdentifier relativePath() {
+        return method.parentPath.node(((DataSchemaNode) method.methodCodec.getSchema()).getQName());
+    }
+
+    final @NonNull ValueMatch<T> withPredicate(final DOMQueryPredicate predicate) {
+        // FIXME: this does not quite take value codec into account :(
+        builder.addPredicate(predicate);
+        return new DefaultValueMatch<>(builder, select);
+    }
+}
---
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/query/DOMQueryPredicate.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.api.query;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+@Beta
+public abstract class DOMQueryPredicate implements Immutable, Predicate<Object> {
+    abstract static class AbstractValueDOMQueryPredicate<T> extends DOMQueryPredicate {
+        private final @NonNull T value;
+
+        AbstractValueDOMQueryPredicate(final YangInstanceIdentifier relativePath, final T value) {
+            super(relativePath);
+            this.value = requireNonNull(value);
+        }
+
+        final @NonNull T value() {
+            return value;
+        }
+
+        @Override
+        ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+            return helper.add("value", value);
+        }
+    }
+
+    abstract static class AbstractComparableDOMQueryPredicate<T extends Comparable<T>>
+            extends AbstractValueDOMQueryPredicate<T> {
+        AbstractComparableDOMQueryPredicate(final YangInstanceIdentifier relativePath, final T value) {
+            super(relativePath, value);
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public final boolean test(final Object data) {
+            return data != null && test(value().compareTo((T) data));
+        }
+
+        abstract boolean test(int valueToData);
+    }
+
+    abstract static class AbstractStringDOMQueryPredicate extends AbstractValueDOMQueryPredicate<String> {
+        AbstractStringDOMQueryPredicate(final YangInstanceIdentifier relativePath, final String value) {
+            super(relativePath, value);
+        }
+
+        @Override
+        public final boolean test(final Object data) {
+            return data instanceof String && test((String) data);
+        }
+
+        abstract boolean test(@NonNull String str);
+    }
+
+    public static final class Exists extends DOMQueryPredicate {
+        public Exists(final YangInstanceIdentifier relativePath) {
+            super(relativePath);
+        }
+
+        @Override
+        public boolean test(final Object data) {
+            return data != null;
+        }
+    }
+
+    public static final class ValueEquals<T> extends AbstractValueDOMQueryPredicate<T> {
+        public ValueEquals(final YangInstanceIdentifier relativePath, final T value) {
+            super(relativePath, value);
+        }
+
+        @Override
+        public boolean test(final Object data) {
+            return value().equals(data);
+        }
+    }
+
+    public static final class GreaterThan<T extends Comparable<T>> extends AbstractComparableDOMQueryPredicate<T> {
+        public GreaterThan(final YangInstanceIdentifier relativePath, final T value) {
+            super(relativePath, value);
+        }
+
+        @Override
+        boolean test(final int valueToData) {
+            return valueToData <= 0;
+        }
+    }
+
+    public static final class GreaterThanOrEqual<T extends Comparable<T>>
+            extends AbstractComparableDOMQueryPredicate<T> {
+        public GreaterThanOrEqual(final YangInstanceIdentifier relativePath, final T value) {
+            super(relativePath, value);
+        }
+
+        @Override
+        boolean test(final int valueToData) {
+            return valueToData < 0;
+        }
+    }
+
+    public static final class LessThan<T extends Comparable<T>> extends AbstractComparableDOMQueryPredicate<T> {
+        public LessThan(final YangInstanceIdentifier relativePath, final T value) {
+            super(relativePath, value);
+        }
+
+        @Override
+        boolean test(final int valueToData) {
+            return valueToData >= 0;
+        }
+    }
+
+    public static final class LessThanOrEqual<T extends Comparable<T>> extends AbstractComparableDOMQueryPredicate<T> {
+        public LessThanOrEqual(final YangInstanceIdentifier relativePath, final T value) {
+            super(relativePath, value);
+        }
+
+        @Override
+        boolean test(final int valueToData) {
+            return valueToData > 0;
+        }
+    }
+
+    public static final class StartsWith extends AbstractStringDOMQueryPredicate {
+        public StartsWith(final YangInstanceIdentifier relativePath, final String str) {
+            super(relativePath, str);
+        }
+
+        @Override
+        boolean test(final String str) {
+            return str.startsWith(value());
+        }
+    }
+
+    public static final class EndsWith extends AbstractStringDOMQueryPredicate {
+        public EndsWith(final YangInstanceIdentifier relativePath, final String str) {
+            super(relativePath, str);
+        }
+
+        @Override
+        boolean test(final String str) {
+            return str.endsWith(value());
+        }
+    }
+
+    public static final class Contains extends AbstractStringDOMQueryPredicate {
+        public Contains(final YangInstanceIdentifier relativePath, final String str) {
+            super(relativePath, str);
+        }
+
+        @Override
+        boolean test(final String str) {
+            return str.contains(value());
+        }
+    }
+
+    public static final class MatchesPattern extends DOMQueryPredicate {
+        private final Pattern pattern;
+
+        public MatchesPattern(final YangInstanceIdentifier relativePath, final Pattern pattern) {
+            super(relativePath);
+            this.pattern = requireNonNull(pattern);
+        }
+
+        @Override
+        ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+            return helper.add("pattern", pattern);
+        }
+
+        @Override
+        public boolean test(final Object data) {
+            return data instanceof CharSequence && pattern.matcher((CharSequence) data).matches();
+        }
+    }
+
+    private final @NonNull YangInstanceIdentifier relativePath;
+
+    DOMQueryPredicate(final YangInstanceIdentifier relativePath) {
+        this.relativePath = requireNonNull(relativePath);
+    }
+
+    public final @NonNull YangInstanceIdentifier getPath() {
+        return relativePath;
+    }
+
+    @Override
+    public abstract boolean test(@Nullable Object data);
+
+    @Override
+    public String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this).add("path", relativePath)).toString();
+    }
+
+    ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return helper;
+    }
+}
