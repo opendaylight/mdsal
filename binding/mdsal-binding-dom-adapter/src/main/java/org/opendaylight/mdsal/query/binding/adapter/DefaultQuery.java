69645b2e80601a5a6cd37a5f98564d9bc90558be
Robert Varga <robert.varga@pantheon.tech>
2020-03-01 13:14:00 +0100

    Add Binding/DOM Query language adapter
    Provide implementation of binding language translation on top
    of its DOM counterpart.
    
    On the DOM layer the expression can be transmitted, hence it gives
    the possibility to move the execution to storage backend, thus
    reducing app/backend data interchange volume.
    
    Change-Id: I51c8ec7e34c2485f62aeb5bdbe35fe1507cabaa9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/query/binding/adapter/DefaultQuery.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.query.binding.adapter;
+
+import static java.util.Objects.requireNonNull;
+
+import org.opendaylight.mdsal.binding.api.query.QueryExpression;
+import org.opendaylight.mdsal.dom.api.query.DOMQuery;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+final class DefaultQuery<T extends DataObject> implements QueryExpression<T> {
+    private final DOMQuery domQuery;
+
+    DefaultQuery(final DOMQuery domQuery) {
+        this.domQuery = requireNonNull(domQuery);
+    }
+}
