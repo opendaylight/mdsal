206b9e8bc56c402a977c73fc9098aec83375aed3
Robert Varga <robert.varga@pantheon.tech>
2020-03-01 13:14:00 +0100

    WIP: Add simple query language
    Applications want to perform rudimentary queries without having to
    perform the filtering by themselves.
    
    On the Binding layer this means providing enough expressive power
    to formulate such queries in a type-safe manner and translating
    them to their DOM counterparts. This has the benefit of allowing
    filtering to occur in DOM world, side-stepping the need to instantiate
    Binding objects.
    
    On the DOM layer the expression can be transmitted, hence it gives
    the possibility to move the execution to storage backend, thus
    reducing app/backend data interchange volume.
    
    // FIXME: finish DOM builder/query
    // FIXME: finish Binding implementation
    // FIXME: add tests for translation
    
    Change-Id: I51c8ec7e34c2485f62aeb5bdbe35fe1507cabaa9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ query/mdsal-query-binding-adapter/src/test/java/org/opendaylight/mdsal/query/binding/adapter/QueryBuilderTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.query.binding.adapter;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.binding.runtime.spi.BindingRuntimeHelpers;
+import org.opendaylight.mdsal.binding.api.query.QueryExpression;
+import org.opendaylight.mdsal.binding.api.query.QueryFactory;
+import org.opendaylight.mdsal.binding.api.query.QueryStructureException;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTree;
+import org.opendaylight.mdsal.binding.dom.codec.impl.DefaultBindingCodecTreeFactory;
+import org.opendaylight.mdsal.binding.generator.impl.DefaultBindingRuntimeGenerator;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsal.test.binding.rev140701.Top;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsal.test.binding.rev140701.two.level.list.TopLevelList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsal.test.binding.rev140701.two.level.list.top.level.list.NestedList;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class QueryBuilderTest {
+    private static BindingCodecTree CODEC;
+
+    private final QueryFactory factory = new DefaultQueryFactory(CODEC);
+
+    @BeforeClass
+    public static void beforeClass() {
+        CODEC = new DefaultBindingCodecTreeFactory().create(BindingRuntimeHelpers.createRuntimeContext(
+            new DefaultBindingRuntimeGenerator()));
+    }
+
+    @Test
+    public void bar() throws QueryStructureException {
+        final QueryExpression<TopLevelList> query = factory.querySubtree(InstanceIdentifier.builder(Top.class).build())
+                .extractChild(TopLevelList.class)
+                .matching()
+                    .childObject(NestedList.class)
+                    .leaf(NestedList::getName).contains("foo")
+                    .and().leaf(TopLevelList::getName).valueEquals("bar")
+                .build();
+//
+//        // Execution start
+//        final Result<TopLevelList> result = query.getResult();
+//        // Execution fetch
+//        final TopLevelList value = result.getValue();
+    }
+}
