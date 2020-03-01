37e32bcead9c7e0dfc15eaedf7cc8db6be40e69a
Robert Varga <robert.varga@pantheon.tech>
2020-03-01 13:14:00 +0100

    Add Binding/DOM Query language adapter
    Provide implementation of binding language translation on top
    of its DOM counterpart.
    
    On the DOM layer the expression can be transmitted, hence it gives
    the possibility to move the execution to storage backend, thus
    reducing app/backend data interchange volume.
    
    Change-Id: I51c8ec7e34c2485f62aeb5bdbe35fe1507cabaa9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 342ef68448beb2dbf1e7705671074ce461050948)

---
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/query/binding/adapter/DefaultStringMatchBuilder.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.query.binding.adapter;
+
+import java.util.regex.Pattern;
+import org.opendaylight.mdsal.binding.api.query.StringMatchBuilder;
+import org.opendaylight.mdsal.binding.api.query.ValueMatch;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryPredicate.Contains;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryPredicate.EndsWith;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryPredicate.MatchesPattern;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryPredicate.StartsWith;
+import org.opendaylight.mdsal.query.binding.adapter.QueryBuilderState.BoundMethod;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+final class DefaultStringMatchBuilder<T extends DataObject> extends AbstractValueMatchBuilder<T, String>
+        implements StringMatchBuilder<T> {
+    DefaultStringMatchBuilder(final QueryBuilderState builder, final InstanceIdentifier<T> select,
+            final BoundMethod method) {
+        super(builder, select, method);
+    }
+
+    @Override
+    public ValueMatch<T> startsWith(final String str) {
+        return withPredicate(new StartsWith(relativePath(), str));
+    }
+
+    @Override
+    public ValueMatch<T> endsWith(final String str) {
+        return withPredicate(new EndsWith(relativePath(), str));
+    }
+
+    @Override
+    public ValueMatch<T> contains(final String str) {
+        return withPredicate(new Contains(relativePath(), str));
+    }
+
+    @Override
+    public ValueMatch<T> matchesPattern(final Pattern pattern) {
+        return withPredicate(new MatchesPattern(relativePath(), pattern));
+    }
+}
