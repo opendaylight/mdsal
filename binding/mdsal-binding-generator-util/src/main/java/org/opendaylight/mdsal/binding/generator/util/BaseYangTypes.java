c040b10f586385381e0509c9ba0627479f07a5bd
Robert Varga <robert.varga@pantheon.tech>
2020-04-20 13:18:09 +0200

    Split out BaseYangTypesProvider
    BaseYangTypes is related to binding.model.api, while the type provider
    is related to binding.generator.api. Split out
    BaseYangTypes.BASE_YANG_TYPES_PROVIDER as a BaseYangTypesProvider.INSTANCE,
    separating these two concerns.
    
    Change-Id: I61dc6ed7f39ba233017bb9938eaab151a7c41e3e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator-util/src/main/java/org/opendaylight/mdsal/binding/generator/util/BaseYangTypes.java
+++ binding/mdsal-binding-generator-util/src/main/java/org/opendaylight/mdsal/binding/generator/util/BaseYangTypes.java
@@ -9,20 +9,14 @@ package org.opendaylight.mdsal.binding.generator.util;
 
 import com.google.common.collect.ImmutableMap;
 import java.math.BigDecimal;
-import org.opendaylight.mdsal.binding.generator.spi.TypeProvider;
-import org.opendaylight.mdsal.binding.model.api.Restrictions;
 import org.opendaylight.mdsal.binding.model.api.Type;
 import org.opendaylight.mdsal.binding.model.util.Types;
-import org.opendaylight.mdsal.binding.spec.naming.BindingMapping;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.Uint16;
 import org.opendaylight.yangtools.yang.common.Uint32;
 import org.opendaylight.yangtools.yang.common.Uint64;
 import org.opendaylight.yangtools.yang.common.Uint8;
-import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaNode;
-import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 
 public final class BaseYangTypes {
     /**
@@ -121,7 +115,7 @@ public final class BaseYangTypes {
      * It is undesirable to create instance of this class.
      */
     private BaseYangTypes() {
-        throw new UnsupportedOperationException();
+
     }
 
     /**
@@ -135,75 +129,4 @@ public final class BaseYangTypes {
     public static Type javaTypeForYangType(final String type) {
         return TYPE_MAP.get(type);
     }
-
-    public static final TypeProvider BASE_YANG_TYPES_PROVIDER = new TypeProvider() {
-        /**
-         * Searches <code>Type</code> value to which is YANG <code>type</code>
-         * mapped.
-         *
-         * @param type
-         *            type definition representation of YANG type
-         * @return java <code>Type</code> representation of <code>type</code>.
-         *         If <code>type</code> isn't found then <code>null</code> is
-         *         returned.
-         */
-        @Override
-        public Type javaTypeForSchemaDefinitionType(final TypeDefinition<?> type, final SchemaNode parentNode,
-                final boolean lenientRelativeLeafrefs) {
-            if (type != null) {
-                return TYPE_MAP.get(type.getQName().getLocalName());
-            }
-
-            return null;
-        }
-
-        @Override
-        public Type javaTypeForSchemaDefinitionType(final TypeDefinition<?> type, final SchemaNode parentNode,
-                final Restrictions restrictions, final boolean lenientRelativeLeafrefs) {
-            String typeName = type.getQName().getLocalName();
-            switch (typeName) {
-                case "binary":
-                    return restrictions == null ? Types.BYTE_ARRAY : Types.typeForClass(byte[].class, restrictions);
-                case "decimal64":
-                    return Types.typeForClass(BigDecimal.class, restrictions);
-                case "enumeration":
-                    return Types.typeForClass(Enum.class, restrictions);
-                case "int8":
-                    return Types.typeForClass(Byte.class, restrictions);
-                case "int16":
-                    return Types.typeForClass(Short.class, restrictions);
-                case "int32":
-                    return Types.typeForClass(Integer.class, restrictions);
-                case "int64":
-                    return Types.typeForClass(Long.class, restrictions);
-                case "string":
-                    return Types.typeForClass(String.class, restrictions);
-                case "uint8":
-                    return Types.typeForClass(Uint8.class, restrictions);
-                case "uint16":
-                    return Types.typeForClass(Uint16.class, restrictions);
-                case "uint32":
-                    return Types.typeForClass(Uint32.class, restrictions);
-                case "uint64":
-                    return Types.typeForClass(Uint64.class, restrictions);
-                default:
-                    return javaTypeForSchemaDefinitionType(type, parentNode, lenientRelativeLeafrefs);
-            }
-        }
-
-        @Override
-        public String getTypeDefaultConstruction(final LeafSchemaNode node) {
-            return null;
-        }
-
-        @Override
-        public String getConstructorPropertyName(final SchemaNode node) {
-            return null;
-        }
-
-        @Override
-        public String getParamNameFromType(final TypeDefinition<?> type) {
-            return "_" + BindingMapping.getPropertyName(type.getQName().getLocalName());
-        }
-    };
 }
