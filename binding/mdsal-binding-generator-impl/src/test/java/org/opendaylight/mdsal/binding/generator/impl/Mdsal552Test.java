f9aecc3936ed7968207102ac4a633d4e6d2201bd
Peter Valka <Peter.Valka@pantheon.tech>
2020-05-06 09:54:18 +0200

    Fix leafref-to-enum encoding
    When we have a leafref pointing to an enum via an absolute path,
    in and RPC, we end up using a no-op codec, whereas we should be
    extracting the name (from enum constant, or SchemaContext).
    
    This ends up being a problem not in codec itself, as it is using
    reflection to acquire the return type, defaulting to no-op.
    
    The problem lies with AbstractTypeGenerator, which ends up not
    being able to look up the generated type in case of RPC. The problem
    here is the order in which types are generated, as the leaf being
    referenced is generated after the RPC itself -- hence we cannot
    find the definition.
    
    While this is again highlighting the problem of single-pass code
    generation, we can side-step the problem by moving RPC/notification
    generation after the root data generation.
    
    As a further complication, we need to record the enum/schema mapping,
    as the codec needs to be able to find them via leafref types.
    
    JIRA: MDSAL-552
    Change-Id: Ifd92807029cdb7ba92dcad5655bb34d3ff4cef9d
    Signed-off-by: Peter Valka <Peter.Valka@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit f9eceb5081877db3fc3f840c44e943605f3f4335)

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal552Test.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.JavaTypeName;
+import org.opendaylight.mdsal.binding.model.api.MethodSignature;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Mdsal552Test {
+    private static final JavaTypeName BAR_INPUT =
+            JavaTypeName.create("org.opendaylight.yang.gen.v1.mdsal552.norev", "BarInput");
+    private static final JavaTypeName BAZ =
+            JavaTypeName.create("org.opendaylight.yang.gen.v1.mdsal552.norev", "Baz");
+    private static final JavaTypeName ENUMERATION =
+            JavaTypeName.create("org.opendaylight.yang.gen.v1.mdsal552.norev", "Mdsal552Data").createEnclosed("Foo");
+
+    @Test
+    public void enumLeafrefTest() {
+        final List<Type> types = new BindingGeneratorImpl().generateTypes(
+                YangParserTestUtils.parseYangResource("/mdsal552.yang"));
+        assertNotNull(types);
+        assertEquals(5, types.size());
+
+        final Type baz = types.stream()
+                .filter(type -> BAZ.equals(type.getIdentifier()))
+                .findFirst().orElseThrow();
+        assertThat(baz, instanceOf(GeneratedType.class));
+        final MethodSignature bazGetRef = ((GeneratedType) baz).getMethodDefinitions().stream()
+                .filter(method -> method.getName().equals("getRef"))
+                .findFirst().orElseThrow();
+        assertEquals(ENUMERATION, bazGetRef.getReturnType().getIdentifier());
+
+        final Type input = types.stream()
+                .filter(type -> BAR_INPUT.equals(type.getIdentifier()))
+                .findFirst().orElseThrow();
+        assertThat(input, instanceOf(GeneratedType.class));
+        final MethodSignature inputGetRef = ((GeneratedType) input).getMethodDefinitions().stream()
+                .filter(method -> method.getName().equals("getRef"))
+                .findFirst().orElseThrow();
+        assertEquals(ENUMERATION, inputGetRef.getReturnType().getIdentifier());
+    }
+}
