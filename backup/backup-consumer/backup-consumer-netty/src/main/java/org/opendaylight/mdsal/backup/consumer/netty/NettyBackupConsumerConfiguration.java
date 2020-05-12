d2a928f5953277ca5ed8f792a2dd13ee062fcf96
Tibor Král <tibor.kral@pantheon.tech>
2020-05-13 00:48:19 +0200

    Add Incremental Backup Export/Import features to mdsal
    Local replication using Kafka provided by features:
     - odl-mdsal-kafka-backup-consumer
     - odl-mdsal-kafka-backup-producer
    WAN replication using Netty provided by features:
     - odl-mdsal-netty-backup-consumer
     - odl-mdsal-netty-backup-producer
    
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Change-Id: I70ed6aa80716a95feb634984517cfd266e2faf43

---
+++ backup/backup-consumer/backup-consumer-netty/src/main/java/org/opendaylight/mdsal/backup/consumer/netty/NettyBackupConsumerConfiguration.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.backup.consumer.netty;
+
+import com.google.common.base.Preconditions;
+
+public class NettyBackupConsumerConfiguration {
+
+    private final int listeningPort;
+    private final boolean enableServerAutoRestart;
+
+    public NettyBackupConsumerConfiguration(final Integer listeningPort, final boolean enableServerAutoRestart) {
+        this.listeningPort = Preconditions.checkNotNull(listeningPort,
+            "Listening port must be specified in the configuration");
+        this.enableServerAutoRestart = enableServerAutoRestart;
+    }
+
+    public int getListeningPort() {
+        return listeningPort;
+    }
+
+    public boolean isEnableServerAutoRestart() {
+        return enableServerAutoRestart;
+    }
+}
