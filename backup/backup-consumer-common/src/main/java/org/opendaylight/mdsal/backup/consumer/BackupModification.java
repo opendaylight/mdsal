ad8d2f6a09d0f5788787b11a3339a6b447151453
Tibor Král <tibor.kral@pantheon.tech>
2020-05-13 00:48:19 +0200

    Add Incremental Backup Export/Import features to mdsal
    Local replication using Kafka provided by features:
     - odl-mdsal-kafka-backup-consumer
     - odl-mdsal-kafka-backup-producer
    WAN replication using Netty provided by features:
     - odl-mdsal-netty-backup-consumer
     - odl-mdsal-netty-backup-producer
    
    Change-Id: I70ed6aa80716a95feb634984517cfd266e2faf43
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ backup/backup-consumer-common/src/main/java/org/opendaylight/mdsal/backup/consumer/BackupModification.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.backup.consumer;
+
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModificationCursor;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackupModification implements org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BackupModification.class);
+
+    private DOMDataTreeWriteTransaction writeTransaction;
+
+    public BackupModification(final DOMDataTreeWriteTransaction transaction) {
+        writeTransaction = transaction;
+    }
+
+    @Override
+    public void delete(YangInstanceIdentifier path) {
+        LOG.debug("BackupModification - DELETE - {}", path);
+        writeTransaction.delete(LogicalDatastoreType.CONFIGURATION, path);
+    }
+
+    @Override
+    public void merge(YangInstanceIdentifier path, NormalizedNode<?, ?> data) {
+
+    }
+
+    @Override
+    public void write(YangInstanceIdentifier path, NormalizedNode<?, ?> data) {
+        LOG.debug("BackupModification - WRITE - {} - DATA: {}", path, data);
+        writeTransaction.put(LogicalDatastoreType.CONFIGURATION, path, data);
+    }
+
+    @Override
+    public void ready() {
+
+    }
+
+    @Override
+    public void applyToCursor(@NonNull DataTreeModificationCursor cursor) {
+
+    }
+
+    @Override
+    public Optional<NormalizedNode<?, ?>> readNode(YangInstanceIdentifier path) {
+        return Optional.empty();
+    }
+
+    @Override
+    public @NonNull DataTreeModification newModification() {
+        return null;
+    }
+
+    @Override
+    public @NonNull SchemaContext getSchemaContext() {
+        return null;
+    }
+}
---
+++ backup/backup-producer-netty/src/main/java/org/opendaylight/mdsal/backup/producer/netty/NettyBackupProducer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.backup.producer.netty;
+
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceRegistration;
+
+/**
+ * Bean producing the {@link NettyBackupProducerService} and registering it as a ClusterSingleton.
+ */
+
+public class NettyBackupProducer {
+
+    private DOMDataBroker domDataBroker;
+    private ClusterSingletonServiceProvider clusterSingletonServiceProvider;
+    private ClusterSingletonService backupProducerService;
+    private ClusterSingletonServiceRegistration serviceRegistration;
+
+    public NettyBackupProducer(final DOMDataBroker dataBroker,
+            final ClusterSingletonServiceProvider singletonServiceProvider) {
+        this.domDataBroker = dataBroker;
+        this.clusterSingletonServiceProvider = singletonServiceProvider;
+        this.backupProducerService = new NettyBackupProducerService(domDataBroker);
+    }
+
+    public void setDomDataBroker(final DOMDataBroker dataBroker) {
+        this.domDataBroker = dataBroker;
+    }
+
+    public void setClusterSingletonServiceProvider(final ClusterSingletonServiceProvider singletonServiceProvider) {
+        this.clusterSingletonServiceProvider = singletonServiceProvider;
+    }
+
+    public void init() {
+        serviceRegistration = clusterSingletonServiceProvider.registerClusterSingletonService(backupProducerService);
+    }
+
+    public void close() {
+        if (serviceRegistration != null) {
+            serviceRegistration.close();
+        } else {
+            backupProducerService.closeServiceInstance();
+        }
+    }
+}
