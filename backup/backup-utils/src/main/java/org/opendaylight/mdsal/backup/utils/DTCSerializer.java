042d61bc10af0cf93bbbc94141145f385789f1cd
Tibor Král <tibor.kral@pantheon.tech>
2020-05-13 00:48:19 +0200

    Add Incremental Backup Export/Import features to mdsal
    Local replication using Kafka provided by features:
     - odl-mdsal-kafka-backup-consumer
     - odl-mdsal-kafka-backup-producer
    WAN replication using Netty provided by features:
     - odl-mdsal-netty-backup-consumer
     - odl-mdsal-netty-backup-producer
    
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Change-Id: I70ed6aa80716a95feb634984517cfd266e2faf43

---
+++ backup/backup-utils/src/main/java/org/opendaylight/mdsal/backup/utils/DTCSerializer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.backup.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import org.opendaylight.mdsal.backup.utils.exceptions.DTCSerializationException;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.codec.binfmt.DataTreeCandidateInputOutput;
+import org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeDataInput;
+import org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeDataOutput;
+import org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeStreamVersion;
+import org.opendaylight.yangtools.yang.data.impl.schema.ReusableImmutableNormalizedNodeStreamWriter;
+
+public final class DTCSerializer {
+
+    private DTCSerializer() {
+
+    }
+
+    public static byte[] serializeDTC(final DataTreeCandidate candidate) throws DTCSerializationException {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             NormalizedNodeDataOutput dataOutput = NormalizedNodeStreamVersion.current()
+                 .newDataOutput(new DataOutputStream(byteArrayOutputStream))) {
+            DataTreeCandidateInputOutput.writeDataTreeCandidate(dataOutput, candidate);
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            throw new DTCSerializationException("DataTreeCandidate serialization failed", e);
+        }
+    }
+
+    public static DataTreeCandidate deserializeDTC(final byte[] candidateData) throws DTCSerializationException {
+        try (DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(candidateData))) {
+            NormalizedNodeDataInput nodeDataInput = NormalizedNodeDataInput.newDataInput(dataInputStream);
+            DataTreeCandidate dataTreeCandidate =
+                DataTreeCandidateInputOutput.readDataTreeCandidate(nodeDataInput,
+                    ReusableImmutableNormalizedNodeStreamWriter.create());
+            return dataTreeCandidate;
+        } catch (IOException e) {
+            throw new DTCSerializationException("DataTreeCandidate deserialization failed", e);
+        }
+    }
+}
