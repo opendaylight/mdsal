e3a9673ab03da81db3f00fdd8c2006767dc63fee
Tibor Král <tibor.kral@pantheon.tech>
2020-05-13 00:48:19 +0200

    Add Incremental Backup Export/Import features to mdsal
    Local replication using Kafka provided by features:
     - odl-mdsal-kafka-backup-consumer
     - odl-mdsal-kafka-backup-producer
    WAN replication using Netty provided by features:
     - odl-mdsal-netty-backup-consumer
     - odl-mdsal-netty-backup-producer
    
    Change-Id: I70ed6aa80716a95feb634984517cfd266e2faf43
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ replicate/mdsal-replicate-netty/src/main/java/org/opendaylight/mdsal/replicate/netty/NettyReplication.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.replicate.netty;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.time.Duration;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.yangtools.concepts.AbstractRegistration;
+import org.opendaylight.yangtools.concepts.Registration;
+
+public final class NettyReplication {
+    private static final class Disabled extends AbstractRegistration {
+        @Override
+        protected void removeRegistration() {
+            // no-op
+        }
+    }
+
+    private NettyReplication() {
+        // Hidden on purpose
+    }
+
+    public static Registration createSink(final DOMDataBroker dataBroker,
+            final ClusterSingletonServiceProvider singletonService, final boolean enabled,
+            final InetAddress sourceAddress, final int sourcePort, final Duration reconnectDelay) {
+        return enabled ? singletonService.registerClusterSingletonService(new SinkSingletonService(dataBroker,
+            new InetSocketAddress(sourceAddress, sourcePort), reconnectDelay)) : new Disabled();
+    }
+}
