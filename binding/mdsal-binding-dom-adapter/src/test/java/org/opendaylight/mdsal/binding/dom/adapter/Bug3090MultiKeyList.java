a2d84883ad00cfef3d54af042e2e2ce59eac4175
Robert Varga <robert.varga@pantheon.tech>
2020-05-29 13:11:04 +0200

    Backport controller's binding-broker tests
    Pick up the class as it existed in controller as of
    822e39fedebaf35ea9826067ad223dbfa98042ee.
    
    JIRA: MDSAL-557
    Change-Id: Ieaaeb594e33f8c5a4ac515d93c70db960cedb81c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/Bug3090MultiKeyList.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter;
+
+import com.google.common.collect.ImmutableSet;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.api.DataObjectModification.ModificationType;
+import org.opendaylight.mdsal.binding.api.ReadWriteTransaction;
+import org.opendaylight.mdsal.binding.dom.adapter.test.AbstractDataTreeChangeListenerTest;
+import org.opendaylight.mdsal.binding.spec.reflect.BindingReflections;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.opendaylight.test.bug._3090.rev160101.Root;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.opendaylight.test.bug._3090.rev160101.RootBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.opendaylight.test.bug._3090.rev160101.root.ListInRoot;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.opendaylight.test.bug._3090.rev160101.root.ListInRootBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+
+public class Bug3090MultiKeyList extends AbstractDataTreeChangeListenerTest {
+    private static final InstanceIdentifier<Root> ROOT_PATH = InstanceIdentifier.create(Root.class);
+
+    @Override
+    protected Set<YangModuleInfo> getModuleInfos() throws Exception {
+        return ImmutableSet.of(BindingReflections.getModuleInfo(Root.class));
+    }
+
+    @Test
+    public void listWithMultiKeyTest() {
+        final List<ListInRoot> listInRoots = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            listInRoots.add(new ListInRootBuilder()
+                .setLeafA("leaf a" + i)
+                .setLeafC("leaf c" + i)
+                .setLeafB("leaf b" + i)
+                .build()
+            );
+        }
+
+        final Root root = new RootBuilder().setListInRoot(listInRoots).build();
+
+        final TestListener<Root> listener = createListener(LogicalDatastoreType.CONFIGURATION, ROOT_PATH,
+                match(ModificationType.WRITE, ROOT_PATH, Objects::isNull,
+                        (Function<Root, Boolean>) dataAfter -> checkData(root, dataAfter)));
+
+        final ReadWriteTransaction readWriteTransaction = getDataBroker().newReadWriteTransaction();
+        readWriteTransaction.put(LogicalDatastoreType.CONFIGURATION, ROOT_PATH, root);
+        assertCommit(readWriteTransaction.commit());
+
+        listener.verify();
+    }
+
+    private static boolean checkData(final Root expected, final Root actual) {
+        if (actual == null) {
+            return false;
+        }
+
+        Set<ListInRoot> expListInRoot = new HashSet<>(expected.getListInRoot().values());
+        Set<ListInRoot> actualListInRoot = actual.getListInRoot().values().stream()
+                .map(list -> new ListInRootBuilder(list).build()).collect(Collectors.toSet());
+        return expListInRoot.equals(actualListInRoot);
+    }
+}
