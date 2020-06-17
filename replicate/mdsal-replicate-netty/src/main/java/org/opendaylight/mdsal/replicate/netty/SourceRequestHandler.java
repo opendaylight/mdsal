cd61eab688c3bbd7d54d04b2c72021838ee3ca71
Robert Varga <robert.varga@pantheon.tech>
2020-06-17 12:29:12 +0200

    Add basic netty replication utility
    This adds a source/sink datastore replication component based on
    Netty TCP channels. The sink (consumer) connects to source (producer)
    via a channel, specifies which data tree it wants replicated and
    then listens for DataTreeCandidates to arrive from the source.
    
    Change-Id: Ib283baa9a186ae2fb4ccf909b257006d4645de37
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit d7b666857c54c07d9bf5c8e5e38671151d89fb4c)

---
+++ replicate/mdsal-replicate-netty/src/main/java/org/opendaylight/mdsal/replicate/netty/SourceRequestHandler.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.replicate.netty;
+
+import static com.google.common.base.Verify.verify;
+import static java.util.Objects.requireNonNull;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import java.io.IOException;
+import java.util.Collection;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.ClusteredDOMDataTreeChangeListener;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeChangeService;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeDataInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Final inbound handler on source side. Handles requests coming from sink and reacts to them.
+ */
+final class SourceRequestHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    private static final Logger LOG = LoggerFactory.getLogger(SourceRequestHandler.class);
+
+    private final DOMDataTreeChangeService dtcs;
+
+    private ListenerRegistration<?> reg;
+
+    SourceRequestHandler(final DOMDataTreeChangeService dtcs) {
+        this.dtcs = requireNonNull(dtcs);
+    }
+
+    @Override
+    public void channelInactive(final ChannelHandlerContext ctx) {
+        LOG.trace("Channel {} going inactive", ctx.channel());
+        if (reg != null) {
+            reg.close();
+            reg = null;
+        }
+        ctx.fireChannelInactive();
+    }
+
+    @Override
+    protected void channelRead0(final ChannelHandlerContext ctx, final ByteBuf msg) throws IOException {
+        verify(msg.isReadable(), "Empty message received");
+
+        final short msgType = msg.readUnsignedByte();
+        final Channel channel = ctx.channel();
+        LOG.trace("Channel {} received message type {}", channel, msgType);
+        switch (msgType) {
+            case Constants.MSG_SUBSCRIBE_REQ:
+                subscribe(channel, msg);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected message type " + msgType);
+        }
+    }
+
+    private void subscribe(final Channel channel, final ByteBuf msg) throws IOException {
+        verify(reg == null, "Unexpected subscription when already subscribed");
+
+        final DOMDataTreeIdentifier dataTree;
+        try (ByteBufInputStream input = new ByteBufInputStream(msg)) {
+            final NormalizedNodeDataInput normalizedInput = NormalizedNodeDataInput.newDataInput(input);
+
+            dataTree = new DOMDataTreeIdentifier(LogicalDatastoreType.readFrom(normalizedInput),
+                normalizedInput.readYangInstanceIdentifier());
+        }
+
+        LOG.info("Channel {} subscribing to {}", channel, dataTree);
+        reg = dtcs.registerDataTreeChangeListener(dataTree, new ClusteredDOMDataTreeChangeListener() {
+            @Override
+            public void onInitialData() {
+                channel.writeAndFlush(AbstractSourceMessage.empty());
+            }
+
+            @Override
+            public void onDataTreeChanged(final Collection<DataTreeCandidate> changes) {
+                channel.writeAndFlush(AbstractSourceMessage.of(changes));
+            }
+        });
+    }
+}
---
+++ replicate/mdsal-replicate-netty/src/main/java/org/opendaylight/mdsal/replicate/netty/SplittingOutputStream.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.replicate.netty;
+
+import static com.google.common.base.Verify.verify;
+import static java.util.Objects.requireNonNull;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * An OutputStream which makes sure to slice messages to a maximum size. This prevents array reallocations and
+ * GC thrashing on huge objects.
+ */
+final class SplittingOutputStream extends OutputStream {
+    private static final int INIT_BUF = 4096;
+
+    static {
+        verify(INIT_BUF <= Constants.LENGTH_FIELD_MAX);
+    }
+
+    private final List<Object> out;
+
+    private ByteBuf buf;
+
+    SplittingOutputStream(final List<Object> out) {
+        this.out = requireNonNull(out);
+        allocBuffer();
+    }
+
+    @Override
+    @SuppressWarnings("checkstyle:parameterName")
+    public void write(final int b) throws IOException {
+        buf.writeByte(b);
+        checkSend();
+    }
+
+    @Override
+    @SuppressWarnings("checkstyle:parameterName")
+    public void write(final byte[] b, final int off, final int len) throws IOException {
+        if (off < 0 || len < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int left = len;
+        int ptr = off;
+        while (left > 0) {
+            final int chunk = Math.min(Constants.LENGTH_FIELD_MAX - buf.writerIndex(), left);
+
+            buf.writeBytes(b, ptr, chunk);
+            ptr += chunk;
+            left -= chunk;
+            checkSend();
+        }
+    }
+
+    @Override
+    public void close() {
+        if (buf.writerIndex() != 0) {
+            out.add(buf);
+        }
+        buf = null;
+    }
+
+    private void allocBuffer() {
+        // FIXME: use buffer allocator?
+        buf = Unpooled.buffer(INIT_BUF, Constants.LENGTH_FIELD_MAX);
+        buf.writeByte(Constants.MSG_DTC_CHUNK);
+    }
+
+    private void checkSend() {
+        if (buf.writerIndex() == Constants.LENGTH_FIELD_MAX) {
+            out.add(buf);
+            allocBuffer();
+        }
+    }
+}
