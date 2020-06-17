3bcc66519666f2b167a4091eeb8b815c716bf4e3
Robert Varga <robert.varga@pantheon.tech>
2020-06-17 12:29:12 +0200

    Add basic netty replication utility
    This adds a source/sink datastore replication component based on
    Netty TCP channels. The sink (consumer) connects to source (producer)
    via a channel, specifies which data tree it wants replicated and
    then listens for DataTreeCandidates to arrive from the source.
    
    Change-Id: Ib283baa9a186ae2fb4ccf909b257006d4645de37
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ replicate/mdsal-replicate-common/src/main/java/org/opendaylight/mdsal/replicate/common/DataTreeCandidateUtils.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.replicate.common;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteOperations;
+import org.opendaylight.mdsal.dom.spi.store.DOMStoreWriteTransaction;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidates;
+
+@Beta
+public final class DataTreeCandidateUtils {
+    private DataTreeCandidateUtils() {
+        // Hidden on purpose
+    }
+
+    public static void applyToTransaction(final DOMDataTreeWriteOperations transaction,
+            final LogicalDatastoreType datastore, final DataTreeCandidate candidate) {
+        DataTreeCandidates.applyToModification(new DOMDataBrokerModification(transaction, datastore), candidate);
+    }
+
+    public static void applyToTransaction(final DOMStoreWriteTransaction transaction,
+            final DataTreeCandidate candidate) {
+        DataTreeCandidates.applyToModification(new DOMStoreModification(transaction), candidate);
+    }
+}
