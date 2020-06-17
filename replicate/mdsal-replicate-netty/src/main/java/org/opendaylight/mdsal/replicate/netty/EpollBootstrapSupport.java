09da0d2b4472762126102bd2d545fccda753889c
Robert Varga <robert.varga@pantheon.tech>
2020-06-17 12:29:12 +0200

    Add basic netty replication utility
    This adds a source/sink datastore replication component based on
    Netty TCP channels. The sink (consumer) connects to source (producer)
    via a channel, specifies which data tree it wants replicated and
    then listens for DataTreeCandidates to arrive from the source.
    
    Change-Id: Ib283baa9a186ae2fb4ccf909b257006d4645de37
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ replicate/mdsal-replicate-netty/src/main/java/org/opendaylight/mdsal/replicate/netty/EpollBootstrapSupport.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.replicate.netty;
+
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.epoll.EpollSocketChannel;
+
+final class EpollBootstrapSupport extends AbstractBootstrapSupport {
+    EpollBootstrapSupport() {
+        super(EpollSocketChannel.class, EpollServerSocketChannel.class, new EpollEventLoopGroup(),
+            new EpollEventLoopGroup());
+    }
+}
---
+++ replicate/mdsal-replicate-netty/src/main/java/org/opendaylight/mdsal/replicate/netty/MessageFrameEncoder.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.replicate.netty;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.handler.codec.LengthFieldPrepender;
+
+@Sharable
+final class MessageFrameEncoder extends LengthFieldPrepender {
+    private static final MessageFrameEncoder INSTANCE = new MessageFrameEncoder();
+
+    private MessageFrameEncoder() {
+        super(Constants.LENGTH_FIELD_LENGTH);
+    }
+
+    static MessageFrameEncoder instance() {
+        return INSTANCE;
+    }
+}
