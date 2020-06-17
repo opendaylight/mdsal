b22daf3ffbdea6e9495071489ca513e27d40a1ed
Robert Varga <robert.varga@pantheon.tech>
2020-06-17 12:29:12 +0200

    Add basic netty replication utility
    This adds a source/sink datastore replication component based on
    Netty TCP channels. The sink (consumer) connects to source (producer)
    via a channel, specifies which data tree it wants replicated and
    then listens for DataTreeCandidates to arrive from the source.
    
    Change-Id: Ib283baa9a186ae2fb4ccf909b257006d4645de37
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit d7b666857c54c07d9bf5c8e5e38671151d89fb4c)

---
+++ replicate/mdsal-replicate-netty/src/main/java/org/opendaylight/mdsal/replicate/netty/AbstractBootstrapSupport.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.replicate.netty;
+
+import static java.util.Objects.requireNonNull;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.socket.ServerSocketChannel;
+import java.util.concurrent.TimeUnit;
+import org.eclipse.jdt.annotation.NonNull;
+
+public abstract class AbstractBootstrapSupport implements AutoCloseable, BootstrapSupport {
+    private final Class<? extends Channel> channelClass;
+    private final Class<? extends ServerSocketChannel> serverChannelClass;
+    private final EventLoopGroup bossGroup;
+    private final EventLoopGroup workerGroup;
+
+    AbstractBootstrapSupport(final Class<? extends Channel> channelClass,
+            final Class<? extends ServerSocketChannel> serverChannelClass, final EventLoopGroup bossGroup,
+            final EventLoopGroup workerGroup) {
+        this.channelClass = requireNonNull(channelClass);
+        this.serverChannelClass = requireNonNull(serverChannelClass);
+        this.bossGroup = requireNonNull(bossGroup);
+        this.workerGroup = requireNonNull(workerGroup);
+    }
+
+    public static @NonNull BootstrapSupport create() {
+        if (Epoll.isAvailable()) {
+            return new EpollBootstrapSupport();
+        }
+        return new NioBootstrapSupport();
+    }
+
+    @Override
+    public final Bootstrap newBootstrap() {
+        return new Bootstrap().group(workerGroup).channel(channelClass);
+    }
+
+    @Override
+    public  final ServerBootstrap newServerBootstrap() {
+        return new ServerBootstrap().group(bossGroup, workerGroup).channel(serverChannelClass);
+    }
+
+    @Override
+    public final void close() throws InterruptedException {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+
+        bossGroup.awaitTermination(10, TimeUnit.SECONDS);
+        workerGroup.awaitTermination(10, TimeUnit.SECONDS);
+    }
+}
