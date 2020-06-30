e75e5bb01a42b06b3487a990ec2a9baeff3d0435
Tibor Král <tibor.kral@pantheon.tech>
2020-06-30 03:29:04 +0200

    Netty Replicator - improve the reconnection and keepalive mechanisms
    - In some cases during a network partition the disconnected
    channel got closed with delay after a new channel
    was already created. This started reconnection process
    which closed the new channel and created yet another one.
    - Also improved the keepalive mechanism since some types
    of network partitions left one side unaware of the issue.
    It is important that both the Sink and the Source are
    notified about any connection issue ASAP. PING-PONG
    messages are exchanged between the two sides when no
    deltas are sent for a period of time.
    - config options keepalive-interval-seconds and
    max-missed-keepalives added to both configurations.
    
    Change-Id: Iebde72963bddb748ab97617d07cfc77cd8614da4
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>

--- replicate/mdsal-replicate-netty/src/main/java/org/opendaylight/mdsal/replicate/netty/SinkKeepaliveHandler.java
+++ replicate/mdsal-replicate-netty/src/main/java/org/opendaylight/mdsal/replicate/netty/SinkKeepaliveHandler.java
@@ -7,27 +7,20 @@
  */
 package org.opendaylight.mdsal.replicate.netty;
 
-import static java.util.Objects.requireNonNull;
-
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.timeout.IdleStateEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class KeepaliveHandler extends ChannelDuplexHandler {
-    private static final Logger LOG = LoggerFactory.getLogger(KeepaliveHandler.class);
-
-    private final Runnable reconnect;
-
-    KeepaliveHandler(final Runnable reconnectCallback) {
-        this.reconnect = requireNonNull(reconnectCallback, "Reconnect callback should not be null");
-    }
+class SinkKeepaliveHandler extends ChannelDuplexHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(SinkKeepaliveHandler.class);
 
     @Override
     public void userEventTriggered(final ChannelHandlerContext ctx, final Object evt) {
         if (evt instanceof IdleStateEvent) {
-            ctx.writeAndFlush(Constants.PING);
+            LOG.debug("IdleStateEvent received. Closing channel");
+            ctx.close();
         } else {
             ctx.fireUserEventTriggered(evt);
         }
@@ -35,7 +28,7 @@ class KeepaliveHandler extends ChannelDuplexHandler {
 
     @Override
     public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
-        LOG.warn("There was an exception on the channel. Attempting to reconnect.", cause);
-        reconnect.run();
+        LOG.warn("Closing channel due to an exception", cause);
+        ctx.close();
     }
 }
