1c66609eaded653c8e93ea85729bfc7cbdf0a8e0
Ilya Igushev <illia.ihushev@pantheon.tech>
2020-07-03 08:07:22 +0000

    Generate bindingEquals() and use it in generated implementations
    As we are gearing towards generating default methods in interfaces,
    we need the ability to completely analyze all a target type from
    a template, not from a generator.
    
    This ability was previously available to Builder*Template, now it
    is part of JavaFileTemplate and hence can be picked up by
    InterfaceTemplate as needed.
    
    JIRA: MDSAL-474
    Change-Id: Ife60dadb9b66dc01df04ebeba11d82c8806f2236
    Signed-off-by: illia.ihushev <illia.ihushev@pantheon.tech>

--- binding/mdsal-binding-spec-util/src/main/java/org/opendaylight/mdsal/binding/spec/reflect/BindingReflections.java
+++ binding/mdsal-binding-spec-util/src/main/java/org/opendaylight/mdsal/binding/spec/reflect/BindingReflections.java
@@ -657,6 +657,9 @@ public final class BindingReflections {
             return false;
         }
         for (Method potentialMethod : potential.getMethods()) {
+            if (potentialMethod.getName().equals(BindingMapping.BINDING_EQUALS_NAME)) {
+                continue;
+            }
             try {
                 Method targetMethod = target.getMethod(potentialMethod.getName(), potentialMethod.getParameterTypes());
                 if (!potentialMethod.getReturnType().equals(targetMethod.getReturnType())) {
