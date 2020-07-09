9c79be6ace66505524a8c38988cc8a511aa2e496
Ilya Igushev <illia.ihushev@pantheon.tech>
2020-07-09 09:52:02 +0000

    Generate bindingToString() and use it in generated implementations
    As we are gearing towards generating default methods in interfaces,
    we need the ability to completely analyze all a target type from
    a template, not from a generator.
    
    This ability was previously available to Builder*Template, now it
    is part of JavaFileTemplate and hence can be picked up by
    InterfaceTemplate as needed.
    
    JIRA: MDSAL-479
    Change-Id: I1e939aca15f2f77b82a83ca454503522c96bcabe
    Signed-off-by: illia.ihushev <illia.ihushev@pantheon.tech>

--- binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/AbstractTypeGenerator.java
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/AbstractTypeGenerator.java
@@ -289,7 +289,7 @@ abstract class AbstractTypeGenerator {
             return null;
         }
         final GeneratedTypeBuilder genType = addDefaultInterfaceDefinition(context, node, baseInterface);
-        defaultImplementedInterace(genType);
+        addConcreteInterfaceMethods(genType);
         annotateDeprecatedIfNecessary(node, genType);
 
         final Module module = context.module();
@@ -569,7 +569,7 @@ abstract class AbstractTypeGenerator {
         addImplementedInterfaceFromUses(schema, outType);
         outType.addImplementsType(type);
         outType.addImplementsType(augmentable(outType));
-        defaultImplementedInterace(outType);
+        addConcreteInterfaceMethods(outType);
         annotateDeprecatedIfNecessary(rpc, outType);
         resolveDataSchemaNodes(context, outType, outType, schema.getChildNodes(), false);
         context.addChildNodeType(schema, outType);
@@ -609,7 +609,7 @@ abstract class AbstractTypeGenerator {
 
                 final GeneratedTypeBuilder notificationInterface = addDefaultInterfaceDefinition(
                     context.modulePackageName(), notification, DATA_OBJECT, context);
-                defaultImplementedInterace(notificationInterface);
+                addConcreteInterfaceMethods(notificationInterface);
                 annotateDeprecatedIfNecessary(notification, notificationInterface);
                 notificationInterface.addImplementsType(NOTIFICATION);
                 context.addChildNodeType(notification, notificationInterface);
@@ -650,7 +650,7 @@ abstract class AbstractTypeGenerator {
 
             final GeneratedTypeBuilder notifInterface = addDefaultInterfaceDefinition(
                 packageNameForGeneratedType(context.modulePackageName(), notif.getPath()), notif, DATA_OBJECT, context);
-            defaultImplementedInterace(notifInterface);
+            addConcreteInterfaceMethods(notifInterface);
             annotateDeprecatedIfNecessary(notif, notifInterface);
 
             notifInterface.addImplementsType(keyType != null ? keyedListNotification(notifInterface, parent, keyType)
@@ -1018,7 +1018,7 @@ abstract class AbstractTypeGenerator {
             JavaTypeName.create(augmentPackageName, augTypeName));
 
         augTypeBuilder.addImplementsType(augmentation(targetTypeRef));
-        defaultImplementedInterace(augTypeBuilder);
+        addConcreteInterfaceMethods(augTypeBuilder);
 
         annotateDeprecatedIfNecessary(augSchema, augTypeBuilder);
         addImplementedInterfaceFromUses(augSchema, augTypeBuilder);
@@ -1231,7 +1231,7 @@ abstract class AbstractTypeGenerator {
             if (caseNode != null && !caseNode.isAddedByUses() && !caseNode.isAugmenting()) {
                 final GeneratedTypeBuilder caseTypeBuilder = addDefaultInterfaceDefinition(context, caseNode);
                 caseTypeBuilder.addImplementsType(refChoiceType);
-                defaultImplementedInterace(caseTypeBuilder);
+                addConcreteInterfaceMethods(caseTypeBuilder);
                 annotateDeprecatedIfNecessary(caseNode, caseTypeBuilder);
                 context.addCaseType(caseNode.getPath(), caseTypeBuilder);
                 context.addChoiceToCaseMapping(refChoiceType, caseTypeBuilder, caseNode);
@@ -1311,7 +1311,7 @@ abstract class AbstractTypeGenerator {
             if (caseNode != null) {
                 final GeneratedTypeBuilder caseTypeBuilder = addDefaultInterfaceDefinition(context, caseNode);
                 caseTypeBuilder.addImplementsType(targetType);
-                defaultImplementedInterace(caseTypeBuilder);
+                addConcreteInterfaceMethods(caseTypeBuilder);
 
                 CaseSchemaNode node = null;
                 final String caseLocalName = caseNode.getQName().getLocalName();
@@ -2040,6 +2040,15 @@ abstract class AbstractTypeGenerator {
             default:
                 throw new IllegalStateException("Unhandled status in " + node);
         }
+    }
+
+    private static void addConcreteInterfaceMethods(final GeneratedTypeBuilder typeBuilder) {
+        defaultImplementedInterace(typeBuilder);
+
+        typeBuilder.addMethod(BindingMapping.BINDING_TO_STRING_NAME)
+            .setAccessModifier(AccessModifier.PUBLIC)
+            .setStatic(true)
+            .setReturnType(STRING).setDefault(true);
     }
 
     private static void narrowImplementedInterface(final GeneratedTypeBuilder typeBuilder) {
