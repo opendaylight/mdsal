085336d1304acd817f020f38632addc4a2c5ced3
Robert Varga <robert.varga@pantheon.tech>
2020-07-30 23:46:00 +0200

    Add binding adapter components for notification services
    Rather than operating on service registry directly, create binding
    adapters as dedicated components. This allows proper provides/requires
    validation for downstream component users.
    
    JIRA: MDSAL-585
    Change-Id: I8a3ec440d9e74cfaecdc310812da32b4b8b28241
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 0e769de7475abae5dd06fc2c93d69d0906c66bee)

---
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/osgi/OSGiNotificationPublishService.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter.osgi;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.opendaylight.mdsal.binding.api.NotificationPublishService;
+import org.opendaylight.yangtools.yang.binding.Notification;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+@Beta
+@Component(factory = OSGiNotificationPublishService.FACTORY_NAME)
+public final class OSGiNotificationPublishService extends AbstractAdaptedService<NotificationPublishService>
+        implements NotificationPublishService {
+    // OSGi DS Component Factory name
+    static final String FACTORY_NAME = "org.opendaylight.mdsal.binding.dom.adapter.osgi.OSGiNotificationPublishService";
+
+    public OSGiNotificationPublishService() {
+        super(NotificationPublishService.class);
+    }
+
+    @Override
+    public void putNotification(final Notification notification) throws InterruptedException {
+        delegate().putNotification(notification);
+    }
+
+    @Override
+    public ListenableFuture<? extends Object> offerNotification(final Notification notification) {
+        return delegate().offerNotification(notification);
+    }
+
+    @Override
+    public ListenableFuture<? extends Object> offerNotification(final Notification notification, final int timeout,
+            final TimeUnit unit) throws InterruptedException {
+        return delegate().offerNotification(notification, timeout, unit);
+    }
+
+    @Activate
+    void activate(final Map<String, ?> properties) {
+        start(properties);
+    }
+
+    @Deactivate
+    void deactivate() {
+        stop();
+    }
+}
