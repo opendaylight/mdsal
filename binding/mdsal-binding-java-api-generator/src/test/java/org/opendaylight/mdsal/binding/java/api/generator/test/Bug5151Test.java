1bb47a086c8f43d09f7b407e2cdc8b2881e7bd5c
illia.ihushev <illia.ihushev@pantheon.tech>
2020-08-11 14:47:27 +0300

    Move common util methods to dedicated class
    Move common file util methods to dedicated class, since they would be
    needed not only in Bug5151Test class.
    
    Change-Id: I4ad75ba20da12936311bdb81c7b2e8e7a7a284d0
    Signed-off-by: illia.ihushev <illia.ihushev@pantheon.tech>

--- binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/mdsal/binding/java/api/generator/test/Bug5151Test.java
+++ binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/mdsal/binding/java/api/generator/test/Bug5151Test.java
@@ -10,12 +10,11 @@ package org.opendaylight.mdsal.binding.java.api.generator.test;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.opendaylight.mdsal.binding.java.api.generator.test.FileSearchUtil.findInFile;
+import static org.opendaylight.mdsal.binding.java.api.generator.test.FileSearchUtil.getFiles;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import org.junit.Test;
 
 /**
@@ -50,33 +49,5 @@ public class Bug5151Test extends BaseCompilationTest {
             "FooContainer</code> <code>fooContainer</code>, or <code>null</code> if not present"));
 
         CompilationTestUtils.cleanUp(sourcesOutputDir, compiledOutputDir);
-    }
-
-    private static boolean findInFile(final File file, final String searchText) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                final String nextLine = scanner.nextLine();
-                if (nextLine.contains(searchText)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private static Map<String, File> getFiles(final File path) {
-        return getFiles(path, new HashMap<>());
-    }
-
-    private static Map<String, File> getFiles(final File path, final Map<String, File> files) {
-        final File [] dirFiles = path.listFiles();
-        for (File file : dirFiles) {
-            if (file.isDirectory()) {
-                return getFiles(file, files);
-            }
-
-            files.put(file.getName(), file);
-        }
-        return files;
     }
 }
