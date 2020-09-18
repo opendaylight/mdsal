a4a83d5d625fe48083aa328517065abfe03ef3b8
Illia <illia.ihushev@pantheon.tech>
2020-09-18 12:23:12 +0300

    Context benchmark
    Change-Id: I7f7b3dcf6aa3b2175cc60b60194737e3df491f81
    Signed-off-by: Illia <illia.ihushev@pantheon.tech>

---
+++ binding/mdsal-binding-dom-codec-merge/src/main/java/org/opendaylight/mdsal/binding/dom/codec/merge/loader/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * {@link java.lang.ClassLoader} support for Binding/DOM codec translation code generators. This package provides one
+ * core class, {@link org.opendaylight.mdsal.binding.dom.codec.merge.loader.CodecClassLoader}, which allows lookup of
+ * compile-time-generated Binding classes for the purpose of referencing them within code generators and which
+ * serves as the ClassLoader holding runtime-generated codecs.
+ *
+ * <p>
+ * While the interfaces and classes in this package may be publicly accessible, they are an implementation detail and
+ * may change incompatibly at any time.
+ */
+package org.opendaylight.mdsal.binding.dom.codec.merge.loader;
