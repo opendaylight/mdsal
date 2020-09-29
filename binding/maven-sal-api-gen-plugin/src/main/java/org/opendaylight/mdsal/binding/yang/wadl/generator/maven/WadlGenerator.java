f4a0fd336dc17f997035b5b3d526d5cab57364b8
Robert Varga <robert.varga@pantheon.tech>
2020-09-29 16:17:03 +0200

    Bump to odlparent-8.0.0/yangtools-8.0.0-SNAPSHOT
    This patch starts integration for MRI window.
    
    Change-Id: Ifa9780fbdfb411b4613df0ee18407d85e81bd517
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/maven-sal-api-gen-plugin/src/main/java/org/opendaylight/mdsal/binding/yang/wadl/generator/maven/WadlGenerator.java
+++ binding/maven-sal-api-gen-plugin/src/main/java/org/opendaylight/mdsal/binding/yang/wadl/generator/maven/WadlGenerator.java
@@ -14,14 +14,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
 import org.opendaylight.mdsal.binding.yang.wadl.generator.WadlRestconfGenerator;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang2sources.spi.BasicCodeGenerator;
 import org.opendaylight.yangtools.yang2sources.spi.BuildContextAware;
+import org.opendaylight.yangtools.yang2sources.spi.ModuleResourceResolver;
 import org.sonatype.plexus.build.incremental.BuildContext;
 
 public class WadlGenerator implements BasicCodeGenerator, BuildContextAware {
@@ -44,7 +43,7 @@ public class WadlGenerator implements BasicCodeGenerator, BuildContextAware {
 
     @Override
     public Collection<File> generateSources(final EffectiveModelContext context, final File outputBaseDir,
-            final Set<Module> currentModules, final Function<Module, Optional<String>> moduleResourcePathResolver)
+            final Set<Module> currentModules, final ModuleResourceResolver moduleResourcePathResolver)
             throws IOException {
         final File outputDir;
         if (outputBaseDir == null) {
