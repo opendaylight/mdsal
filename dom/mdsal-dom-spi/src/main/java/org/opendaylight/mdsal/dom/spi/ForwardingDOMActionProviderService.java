5c8abc02f4fe7972b60fc78c4b05b67a06bd9360
Robert Varga <robert.varga@pantheon.tech>
2020-10-08 00:31:01 +0200

    Clean up DOMExtensibleService
    Make sure we do not require implementations to deal with extensions,
    as the default implementation serves most use cases.
    
    Also introduce ForwardingDOMExtensibleService, so that the forwarding
    part is neatly shared by all forwarders.
    
    Change-Id: I10440111a89ad72061779bcb846cfd934d96b2bd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/ForwardingDOMActionProviderService.java
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/ForwardingDOMActionProviderService.java
@@ -8,8 +8,6 @@
 package org.opendaylight.mdsal.dom.spi;
 
 import com.google.common.annotations.Beta;
-import com.google.common.collect.ClassToInstanceMap;
-import com.google.common.collect.ForwardingObject;
 import java.util.Set;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.dom.api.DOMActionImplementation;
@@ -19,18 +17,12 @@ import org.opendaylight.mdsal.dom.api.DOMActionProviderServiceExtension;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
 
 @Beta
-public abstract class ForwardingDOMActionProviderService extends ForwardingObject implements DOMActionProviderService {
-    @Override
-    public ClassToInstanceMap<DOMActionProviderServiceExtension> getExtensions() {
-        return delegate().getExtensions();
-    }
-
+public abstract class ForwardingDOMActionProviderService
+        extends ForwardingDOMExtensibleService<DOMActionProviderService, DOMActionProviderServiceExtension>
+        implements DOMActionProviderService {
     @Override
     public <T extends @NonNull DOMActionImplementation> ObjectRegistration<T> registerActionImplementation(
             final T implementation, final Set<@NonNull DOMActionInstance> instances) {
         return delegate().registerActionImplementation(implementation, instances);
     }
-
-    @Override
-    protected abstract @NonNull DOMActionProviderService delegate();
 }
