b5eb017f8f1c3fd6f228e4000e7962837236012f
Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>
2020-10-22 15:51:40 +0300

    Propagate notification status to generated listener methods
    When we are generating listener interface for notifications:
    - deprecated notification methods are marked as @deprecated;
    - obsolete notification methods are marked as @deprecated and
      additionally are "default no-op" (so that users are not forced
      to implement them);
    
    JIRA: MDSAL-554
    Change-Id: I298b4dff44bd74715d9e1ebac277ac4d7918ffe6
    Signed-off-by: Vladyslav Marchenko <vladyslav.marchenko@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/test/java/org/opendaylight/mdsal/binding/generator/impl/Mdsal554Test.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.JavaTypeName;
+import org.opendaylight.mdsal.binding.model.api.MethodSignature;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Mdsal554Test {
+    @Test
+    public void builderTemplateGenerateListenerMethodsTest() {
+        final EffectiveModelContext context = YangParserTestUtils.parseYangResource("/mdsal554.yang");
+
+        final Set<Module> toGenModules = new HashSet<>();
+        for (final Module module : context.getModules()) {
+            toGenModules.add(module);
+        }
+
+        final List<Type> genTypes = DefaultBindingGenerator.generateFor(context, toGenModules);
+
+        final GeneratedType type = (GeneratedType) genTypes.get(3);
+
+        final List<MethodSignature> methods = type.getMethodDefinitions();
+
+        assertEquals(3, methods.size());
+        assertEquals(methods.get(0).getName(), "onDeprecatedNotification");
+        assertEquals(methods.get(0).isDefault(), false);
+        assertEquals(methods.get(0).getAnnotations().size(), 1);
+        assertEquals(methods.get(0).getAnnotations().get(0).getIdentifier(), JavaTypeName.create(Deprecated.class));
+        assertEquals(methods.get(0).getAnnotations().get(0).getParameters().size(), 0);
+
+        assertEquals(methods.get(1).getName(), "onObsoleteNotification");
+        assertEquals(methods.get(1).isDefault(), true);
+        assertEquals(methods.get(1).getAnnotations().size(), 1);
+        assertEquals(methods.get(1).getAnnotations().get(0).getIdentifier(), JavaTypeName.create(Deprecated.class));
+        assertEquals(methods.get(1).getAnnotations().get(0).getParameters().size(), 1);
+        assertEquals(methods.get(1).getAnnotations().get(0).getParameters().get(0).getName(), "forRemoval");
+        assertEquals(methods.get(1).getAnnotations().get(0).getParameters().get(0).getValue(), "true");
+
+        assertEquals(methods.get(2).getName(), "onTestNotification");
+        assertEquals(methods.get(2).isDefault(), false);
+        assertEquals(methods.get(2).getAnnotations().size(), 0);
+    }
+}
