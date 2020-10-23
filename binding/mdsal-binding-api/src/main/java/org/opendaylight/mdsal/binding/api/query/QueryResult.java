4e3407063b827b4c863974938da502f05fab0991
Robert Varga <robert.varga@pantheon.tech>
2020-10-23 21:19:20 +0200

    Update QueryResult design
    A query needs to result in a set of path/object tuples, as we really
    need to discern matches. Having a concrete path of matches helps
    getting identifiers of parent objects for subquent queries.
    
    DOMQueryResult already contains this capability, we just need to
    propagate it.
    
    JIRA: MDSAL-604
    Change-Id: Iec9ad34aac105eeb45c26a31a24611c9e0f2bb82
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/query/QueryResult.java
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/query/QueryResult.java
@@ -7,14 +7,18 @@
  */
 package org.opendaylight.mdsal.binding.api.query;
 
-import com.google.common.annotations.Beta;
+import static java.util.Objects.requireNonNull;
+
 import java.util.List;
 import java.util.Spliterator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
-import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
  * Result of executing a {@link QueryExpression}. It is composed of one or more result values, which can be accessed via
@@ -22,8 +26,50 @@ import org.opendaylight.yangtools.yang.binding.DataObject;
  *
  * @param <T> Result object type
  */
-@Beta
+@NonNullByDefault
 public interface QueryResult<T extends DataObject> {
+    /**
+     * A single item in the result set. It is identified by its path and the corresponding object..
+     *
+     * @param <T> Result object type
+     */
+    // FIXME: This is our DTO. It is value-based and hence should become a record.
+    final class Item<T extends DataObject> implements Immutable {
+        private final InstanceIdentifier<T> path;
+        private final T object;
+
+        private Item(final InstanceIdentifier<T> path, final T object) {
+            this.path = requireNonNull(path);
+            this.object = requireNonNull(object);
+        }
+
+        public static <T extends DataObject> Item<T> of(final InstanceIdentifier<T> path, final T obj) {
+            return new Item<>(path, obj);
+        }
+
+        public T object() {
+            return object;
+        }
+
+        public InstanceIdentifier<T> path() {
+            return path;
+        }
+
+        @Override
+        public int hashCode() {
+            return path.hashCode() * 31 + object.hashCode();
+        }
+
+        @Override
+        public boolean equals(final @Nullable Object obj) {
+            if (!(obj instanceof Item)) {
+                return false;
+            }
+            final Item<?> other = (Item<?>) obj;
+            return path.equals(other.path) && obj.equals(other.object);
+        }
+    }
+
     /**
      * Returns a spliterator over values of the result.
      *
@@ -31,14 +77,14 @@ public interface QueryResult<T extends DataObject> {
      */
     // TODO: @throws IllegalStateException if values have been already been consumed?
     // FIXME: we really may want to wrap each entry in a CheckedValue, so that we can communicate fetch problems
-    @NonNull Spliterator<? extends T> spliterator();
+    Spliterator<? extends Item<T>> spliterator();
 
     /**
      * Returns a sequential {@link Stream} of values from the result.
      *
      * @return A stream of non-null values.
      */
-    default @NonNull Stream<? extends T> stream() {
+    default Stream<? extends Item<T>> stream() {
         return StreamSupport.stream(spliterator(), false);
     }
 
@@ -47,11 +93,15 @@ public interface QueryResult<T extends DataObject> {
      *
      * @return A stream of non-null values.
      */
-    default @NonNull Stream<? extends T> parallelStream() {
+    default Stream<? extends Item<T>> parallelStream() {
         return StreamSupport.stream(spliterator(), true);
     }
 
-    default @NonNull List<? extends T> getValues() {
+    default List<? extends T> getValues() {
+        return stream().map(Item::object).collect(Collectors.toUnmodifiableList());
+    }
+
+    default List<? extends Item<T>> getItems() {
         return stream().collect(Collectors.toUnmodifiableList());
     }
 }
