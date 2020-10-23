180a4f319e9d85e3781c818c58646c1c489bb561
Robert Varga <robert.varga@pantheon.tech>
2020-10-23 22:46:07 +0200

    Add SimpleQueryExecutor
    This refactors the interace a bit and provides a basic evaluator.
    The idea is that given a root object it will evaluate the query.
    
    JIRA: MDSAL-605
    Change-Id: Ifcf8880d502eed93fa243a2c1e2f50a116306fe5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/query/SimpleQueryExecutor.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter.query;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.api.query.QueryExecutor;
+import org.opendaylight.mdsal.binding.api.query.QueryExpression;
+import org.opendaylight.mdsal.binding.api.query.QueryResult;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTree;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingDataObjectCodecTreeNode;
+import org.opendaylight.mdsal.dom.api.query.DOMQuery;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryLike;
+import org.opendaylight.mdsal.dom.spi.query.DOMQueryEvaluator;
+import org.opendaylight.yangtools.yang.binding.ChildOf;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.DataRoot;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+@Beta
+public final class SimpleQueryExecutor implements QueryExecutor {
+    private final BindingCodecTree codec;
+    private final NormalizedNode<?, ?> root;
+
+    public SimpleQueryExecutor(final BindingCodecTree codec, final NormalizedNode<?, ?> root) {
+        this.codec = requireNonNull(codec);
+        this.root = requireNonNull(root);
+    }
+
+    @Override
+    public <T extends @NonNull DataObject> QueryResult<T> executeQuery(final QueryExpression<T> query) {
+        checkArgument(query instanceof DOMQueryLike, "Unsupported expression %s", query);
+        final DOMQuery domQuery = ((DOMQueryLike) query).asDOMQuery();
+        return new DefaultQueryResult<>(codec, DOMQueryEvaluator.evaluate(domQuery, root));
+    }
+
+    public static @NonNull Builder builder(final BindingCodecTree codec) {
+        return new Builder(codec);
+    }
+
+    public static final class Builder implements org.opendaylight.yangtools.concepts.Builder<SimpleQueryExecutor> {
+        private final DataContainerNodeBuilder<NodeIdentifier, ContainerNode> rootBuilder = Builders.containerBuilder()
+            .withNodeIdentifier(new NodeIdentifier(SchemaContext.NAME));
+        private final BindingCodecTree codec;
+
+        Builder(final BindingCodecTree codec) {
+            this.codec = requireNonNull(codec);
+        }
+
+        public <T extends ChildOf<? extends DataRoot>> @NonNull Builder add(final @NonNull T data) {
+            @SuppressWarnings("unchecked")
+            final BindingDataObjectCodecTreeNode<T> dataCodec = (BindingDataObjectCodecTreeNode<T>)
+                codec.getSubtreeCodec(InstanceIdentifier.create(data.implementedInterface()));
+            rootBuilder.withChild((DataContainerChild<?, ?>) verifyNotNull(dataCodec).serialize(data));
+            return this;
+        }
+
+        @Override
+        public SimpleQueryExecutor build() {
+            return new SimpleQueryExecutor(codec, rootBuilder.build());
+        }
+    }
+}
