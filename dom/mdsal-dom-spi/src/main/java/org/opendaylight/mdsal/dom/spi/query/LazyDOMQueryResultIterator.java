2b5f41066c4576877b38cfb4f6bf84ae94c618e9
Robert Varga <robert.varga@pantheon.tech>
2020-10-26 15:17:55 +0100

    Add LazyDOMQueryResult
    Rehost the evaluation logic into a dedicated iterator class. This
    does not make iteration lazy, but sets the stage by giving the state
    being passed around a place to live.
    
    JIRA: MDSAL-610
    Change-Id: Ic23eb38d7e5a6bd1cf1b7637dec88eff552520d7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/query/LazyDOMQueryResultIterator.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.spi.query;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.mdsal.dom.api.query.DOMQuery;
+import org.opendaylight.mdsal.dom.api.query.DOMQueryPredicate;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
+
+@NonNullByDefault
+final class LazyDOMQueryResultIterator implements Iterator<Entry<YangInstanceIdentifier, NormalizedNode<?, ?>>> {
+    // Absolute path from root of current data item
+    private final ArrayDeque<PathArgument> currentPath;
+    // Steps remaining in the select part of the query
+    private final ArrayDeque<PathArgument> selectSteps;
+    private final DOMQuery query;
+
+    // FIXME: MDSAL-610: This needs to be a stack of sorts, it is passed recursively
+    private final NormalizedNode<?, ?> queryRoot;
+    // FIXME: MDSAL-610: this needs to be eliminated
+    private final Iterator<Entry<YangInstanceIdentifier, NormalizedNode<?, ?>>> iter;
+
+    LazyDOMQueryResultIterator(final DOMQuery query, final NormalizedNode<?, ?> queryRoot) {
+        currentPath = new ArrayDeque<>(query.getRoot().getPathArguments());
+        selectSteps = new ArrayDeque<>(query.getSelect().getPathArguments());
+        this.query = requireNonNull(query);
+
+        this.queryRoot = requireNonNull(queryRoot);
+
+        // FIXME: MDSAL-610: this is a recursive algo, filling provided list. Turn it around into a state mutator.
+        final List<Entry<YangInstanceIdentifier, NormalizedNode<?,?>>> result = new ArrayList<>();
+        evalPath(result, queryRoot);
+        this.iter = result.iterator();
+        currentPath.clear();
+        selectSteps.clear();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iter.hasNext();
+    }
+
+    @Override
+    public Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> next() {
+        return iter.next();
+    }
+
+    private void evalPath(final List<Entry<YangInstanceIdentifier, NormalizedNode<?,?>>> result,
+            final NormalizedNode<?, ?> data) {
+        final PathArgument next = selectSteps.poll();
+        if (next == null) {
+            if (matches(data, query)) {
+                result.add(new SimpleImmutableEntry<>(YangInstanceIdentifier.create(currentPath), data));
+            }
+            return;
+        }
+
+        if (data instanceof MapNode && next instanceof NodeIdentifier) {
+            checkArgument(data.getIdentifier().equals(next), "Unexpected step %s", next);
+            for (MapEntryNode child : ((MapNode) data).getValue()) {
+                evalChild(result, child);
+            }
+        } else {
+            NormalizedNodes.getDirectChild(data, next).ifPresent(child -> evalChild(result, child));
+        }
+        selectSteps.push(next);
+    }
+
+    private void evalChild(final List<Entry<YangInstanceIdentifier, NormalizedNode<?,?>>> result,
+            final NormalizedNode<?, ?> child) {
+        currentPath.addLast(child.getIdentifier());
+        evalPath(result, child);
+        currentPath.removeLast();
+    }
+
+    static boolean matches(final NormalizedNode<?, ?> data, final DOMQuery query) {
+        for (DOMQueryPredicate pred : query.getPredicates()) {
+            // Okay, now we need to deal with predicates, but do it in a smart fashion, so we do not end up iterating
+            // all over the place. Typically we will be matching just a leaf.
+            final YangInstanceIdentifier path = pred.getPath();
+            final Optional<NormalizedNode<?, ?>> node;
+            if (path.coerceParent().isEmpty()) {
+                node = NormalizedNodes.getDirectChild(data, path.getLastPathArgument());
+            } else {
+                node = NormalizedNodes.findNode(data, path);
+            }
+
+            if (!pred.test(node.orElse(null))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
