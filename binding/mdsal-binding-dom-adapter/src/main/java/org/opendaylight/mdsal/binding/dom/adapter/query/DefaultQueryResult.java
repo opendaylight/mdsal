dd3740d479d5e3af86a50a9f51d87ed1e80a5bbf
Robert Varga <robert.varga@pantheon.tech>
2020-10-26 02:51:43 +0100

    QueryResults should be Iterable, Immutable
    Each query result really is a holder of result items, in some
    iteration order. Expose this by implementing Iterable, so that
    we end up correctly implementing stream() et al.
    
    JIRA: MDSAL-605
    Change-Id: I4cf1cd29e74e8c640ee62126e4f1cb9efa956cce
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 74bf22cda1905fee5ab6d539a8a725a81c9ff826)

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/query/DefaultQueryResult.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/query/DefaultQueryResult.java
@@ -10,16 +10,19 @@ package org.opendaylight.mdsal.binding.dom.adapter.query;
 import static com.google.common.base.Verify.verifyNotNull;
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.base.Function;
 import com.google.common.base.VerifyException;
+import com.google.common.collect.Iterators;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
+import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Spliterator;
-import java.util.function.Function;
 import java.util.stream.Stream;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.gaul.modernizer_maven_annotations.SuppressModernizer;
 import org.opendaylight.mdsal.binding.api.query.QueryResult;
 import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTree;
 import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTreeNode;
@@ -31,6 +34,7 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
 @NonNullByDefault
+@SuppressModernizer
 final class DefaultQueryResult<T extends DataObject>
         implements QueryResult<T>, Function<Entry<YangInstanceIdentifier, NormalizedNode<?, ?>>, QueryResult.Item<T>> {
     private static final VarHandle ITEM_CODEC;
@@ -57,17 +61,22 @@ final class DefaultQueryResult<T extends DataObject>
     }
 
     @Override
-    public Spliterator<? extends Item<T>> spliterator() {
+    public Iterator<Item<T>> iterator() {
+        return Iterators.transform(domResult.iterator(), this);
+    }
+
+    @Override
+    public Spliterator<Item<T>> spliterator() {
         return new DefaultQueryResultSpliterator<>(this, domResult.spliterator());
     }
 
     @Override
-    public Stream<? extends Item<T>> stream() {
+    public Stream<Item<T>> stream() {
         return domResult.stream().map(this);
     }
 
     @Override
-    public Stream<? extends Item<T>> parallelStream() {
+    public Stream<Item<T>> parallelStream() {
         return domResult.parallelStream().map(this);
     }
 
