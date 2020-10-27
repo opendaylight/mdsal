57091a3c275818e4bdd1fe81d8824658199a6966
Robert Varga <robert.varga@pantheon.tech>
2020-10-27 23:55:47 +0100

    Rename LazyDOMQueryResultIterator
    This is an implementation-internal class, shorten its name to
    DOMQueryIterator. It is shorter and matches naming of others.
    
    JIRA: MDSAL-605
    Change-Id: I8413a20c4ba533c62bf6c5c3013b4d1bf8f841e2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 870ebfe9aef87e245f98bc377a8d8fb15f2b820f)

--- dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/query/DOMQueryIterator.java
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/query/DOMQueryIterator.java
@@ -33,7 +33,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
 
 @NonNullByDefault
-final class LazyDOMQueryResultIterator extends AbstractIterator<Entry<YangInstanceIdentifier, NormalizedNode<?, ?>>> {
+final class DOMQueryIterator extends AbstractIterator<Entry<YangInstanceIdentifier, NormalizedNode<?, ?>>> {
     private static class Frame {
         final NormalizedNode<?, ?> data;
         final @Nullable PathArgument select;
@@ -93,7 +93,7 @@ final class LazyDOMQueryResultIterator extends AbstractIterator<Entry<YangInstan
     // The predicates which need to be evaluated
     private final List<? extends DOMQueryPredicate> predicates;
 
-    LazyDOMQueryResultIterator(final DOMQuery query, final NormalizedNode<?, ?> queryRoot) {
+    DOMQueryIterator(final DOMQuery query, final NormalizedNode<?, ?> queryRoot) {
         // Note: DOMQueryEvaluator has taken care of the empty case, this is always non-empty
         remainingSelect.addAll(query.getSelect().getPathArguments());
         currentPath.addAll(query.getRoot().getPathArguments());
