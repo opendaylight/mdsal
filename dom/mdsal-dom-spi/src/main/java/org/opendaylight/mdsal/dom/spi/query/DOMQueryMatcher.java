bee072b3ccce7f8dc50902f035cc569799dcd86e
Robert Varga <robert.varga@pantheon.tech>
2020-10-27 23:55:47 +0100

    Rename LazyDOMQueryResultIterator
    This is an implementation-internal class, shorten its name to
    DOMQueryIterator. It is shorter and matches naming of others.
    
    Change-Id: I8413a20c4ba533c62bf6c5c3013b4d1bf8f841e2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/query/DOMQueryMatcher.java
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/query/DOMQueryMatcher.java
@@ -22,7 +22,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
 
 /**
- * Generalized utility for matching predicates. Split out of {@link LazyDOMQueryResultIterator} for simplicity.
+ * Generalized utility for matching predicates. Split out of {@link DOMQueryIterator} for simplicity.
  */
 final class DOMQueryMatcher {
     DOMQueryMatcher() {
