c167281ced5a3c6a088433cf5ad45de16eb79390
Robert Varga <robert.varga@pantheon.tech>
2020-11-08 10:55:26 +0100

    Convert mdsal-eos-dom-simple to a JPMS module
    This is a simple implementation of the DOMEntityOwnershipService
    contract. Turn it into a JPMS module, removing the internal package
    visibility and exposing DI class from a separate package.
    
    Since we are in the area, also clean up UT, eliminating unneeded
    mocking.
    
    JIRA: MDSAL-638
    Change-Id: Ifd0b0a6575e39b619868b251abfce6d2a2a340d5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ entityownership/mdsal-eos-dom-simple/src/main/java/module-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+import org.opendaylight.mdsal.eos.dom.api.DOMEntityOwnershipService;
+import org.opendaylight.mdsal.eos.dom.simple.SimpleDOMEntityOwnershipService;
+
+module org.opendaylight.mdsal.eos.dom.simple {
+    exports org.opendaylight.mdsal.eos.dom.simple.di;
+
+    requires org.opendaylight.yangtools.concepts;
+    requires org.opendaylight.mdsal.eos.dom.api;
+    requires org.slf4j;
+
+    provides DOMEntityOwnershipService with SimpleDOMEntityOwnershipService;
+
+    // Annotations
+    requires static com.github.spotbugs.annotations;
+    requires static org.eclipse.jdt.annotation;
+    requires static org.checkerframework.checker.qual;
+    requires static javax.inject;
+    requires static metainf.services;
+    requires static org.osgi.service.component.annotations;
+}
