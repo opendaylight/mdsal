b97577b2ad6cc763f317b2bf1560e594407a3313
Ilya Igushev <illia.ihushev@pantheon.tech>
2020-11-12 17:59:17 +0200

    Expose RPCMethodInvoker
    Change-Id: Ib1a19ca6a209d07480c4f8e263156b9d058f6e06
    Signed-off-by: Ilya Igushev <illia.ihushev@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMRpcProviderServiceAdapter.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMRpcProviderServiceAdapter.java
@@ -8,27 +8,43 @@
 package org.opendaylight.mdsal.binding.dom.adapter;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableSet;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import org.opendaylight.mdsal.binding.api.RpcProviderService;
 import org.opendaylight.mdsal.dom.api.DOMRpcIdentifier;
-import org.opendaylight.mdsal.dom.api.DOMRpcImplementationRegistration;
+import org.opendaylight.mdsal.dom.api.DOMRpcImplementation;
 import org.opendaylight.mdsal.dom.api.DOMRpcProviderService;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.RpcService;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
 
 @VisibleForTesting
 public class BindingDOMRpcProviderServiceAdapter extends AbstractBindingAdapter<DOMRpcProviderService>
         implements RpcProviderService {
     private static final ImmutableSet<YangInstanceIdentifier> GLOBAL = ImmutableSet.of(YangInstanceIdentifier.empty());
+    private LoadingCache<Class<? extends RpcService>, RpcServiceClassContext> rpcServiceClasses =
+            CacheBuilder.newBuilder().build(
+                    new CacheLoader<>() {
+                        @Override
+                        public RpcServiceClassContext load(final Class<? extends RpcService> key) {
+                            final Map<QName, Method> rpcs = currentSerializer().getRpcMethodToQName(key)
+                                    .inverse();
+                            return new RpcServiceClassContext(adapterContext(), key, rpcs);
+                        }
+                    });
 
     public BindingDOMRpcProviderServiceAdapter(final AdapterContext adapterContext,
             final DOMRpcProviderService domRpcRegistry) {
@@ -50,23 +66,35 @@ public class BindingDOMRpcProviderServiceAdapter extends AbstractBindingAdapter<
     private <S extends RpcService, T extends S> ObjectRegistration<T> register(final Class<S> type,
             final T implementation, final Collection<YangInstanceIdentifier> rpcContextPaths) {
         final Map<QName, Method> rpcs = currentSerializer().getRpcMethodToQName(type).inverse();
+        final Map<QName, Set<DOMRpcIdentifier>> domRpcs = createDomRpcIdentifiers(rpcs.keySet(), rpcContextPaths);
+        final RpcServiceClassContext context = rpcServiceClasses.getUnchecked(type);
 
-        final BindingDOMRpcImplementationAdapter adapter = new BindingDOMRpcImplementationAdapter(adapterContext(),
-            type, rpcs, implementation);
-        final Set<DOMRpcIdentifier> domRpcs = createDomRpcIdentifiers(rpcs.keySet(), rpcContextPaths);
-        final DOMRpcImplementationRegistration<?> domReg = getDelegate().registerRpcImplementation(adapter, domRpcs);
+        final Map<DOMRpcIdentifier, DOMRpcImplementation> implementations = new HashMap<>();
+        for (Map.Entry<QName, Set<DOMRpcIdentifier>> entry : domRpcs.entrySet()) {
+            final BindingDOMRpcImplementationAdapter domImpl =
+                    new BindingDOMRpcImplementationAdapter(implementation,
+                            context.getMethodContexts().get(SchemaNodeIdentifier.Absolute.of(entry.getKey())));
+
+            for (DOMRpcIdentifier domRpcIdentifier : entry.getValue()) {
+                implementations.put(domRpcIdentifier, domImpl);
+            }
+        }
+
+        final Registration domReg = getDelegate().registerRpcImplementations(implementations);
         return new BindingRpcAdapterRegistration<>(implementation, domReg);
     }
 
-    private static Set<DOMRpcIdentifier> createDomRpcIdentifiers(final Set<QName> rpcs,
-            final Collection<YangInstanceIdentifier> paths) {
-        final Set<DOMRpcIdentifier> ret = new HashSet<>();
-        for (final YangInstanceIdentifier path : paths) {
-            for (final QName rpc : rpcs) {
-                ret.add(DOMRpcIdentifier.create(rpc, path));
+    private static Map<QName,  Set<DOMRpcIdentifier>> createDomRpcIdentifiers(final Set<QName> rpcsPaths,
+            final Collection<YangInstanceIdentifier> contextsPaths) {
+        final Map<QName,  Set<DOMRpcIdentifier>> result = new HashMap<>();
+        for (final QName rpcPath : rpcsPaths) {
+            final Set<DOMRpcIdentifier> set = new HashSet<>();
+            for (final YangInstanceIdentifier contextPath : contextsPaths) {
+                set.add(DOMRpcIdentifier.create(rpcPath, contextPath));
             }
+            result.put(rpcPath, set);
         }
-        return ret;
+        return result;
     }
 
     private Collection<YangInstanceIdentifier> toYangInstanceIdentifiers(final Set<InstanceIdentifier<?>> identifiers) {
