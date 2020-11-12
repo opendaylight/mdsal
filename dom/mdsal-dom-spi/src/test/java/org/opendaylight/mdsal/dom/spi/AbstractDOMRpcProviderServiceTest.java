1dc744885da1af4725356b84b4d5a9ad06bee73b
Ilya Igushev <illia.ihushev@pantheon.tech>
2020-11-12 17:59:17 +0200

    Expose RPCMethodInvoker
    Change-Id: Ib1a19ca6a209d07480c4f8e263156b9d058f6e06
    Signed-off-by: Ilya Igushev <illia.ihushev@pantheon.tech>

--- dom/mdsal-dom-spi/src/test/java/org/opendaylight/mdsal/dom/spi/AbstractDOMRpcProviderServiceTest.java
+++ dom/mdsal-dom-spi/src/test/java/org/opendaylight/mdsal/dom/spi/AbstractDOMRpcProviderServiceTest.java
@@ -12,7 +12,6 @@ import static org.mockito.Mockito.mock;
 
 import java.util.Map;
 import java.util.Set;
-import org.eclipse.jdt.annotation.NonNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
