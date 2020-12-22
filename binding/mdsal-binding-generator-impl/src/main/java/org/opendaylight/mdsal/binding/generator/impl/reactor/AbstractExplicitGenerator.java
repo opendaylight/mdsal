377b47cdd69890e34a848e2bc90b41bb0354d812
Robert Varga <robert.varga@pantheon.tech>
2020-12-22 22:01:36 +0100

    WIP: Bump to odlparent-9.0.0/yangtools-7.0.0
    Aside from the usual mechanical updates, we need to also rewrite
    mdsal-binding-generator-impl, as its mechanics relied completely on
    TypeDefinition.getPath(), which is no longer available.
    
    The rewritten component is a wee bit more navigable, making a number of
    deficiencies clearly visible. These are marked as FIXMEs for future
    improvements.
    
    One such problem arises when we attempt to create a derived type from an
    enumeration: here we ended up generating an empty DTO rather than
    properly specializing the type. Attempting to retain compatibility would
    be fruitless, as it would violate a number of method signatures.
    
    Furthermore leafref resolution is made stricter, requiring that
    references actually point to a leaf, not to a random location -- which
    was true in abstract-topology@2013-02-08.yang test model.
    
    Another change revolves around nested union types. These previously
    ended up being flattened in the class hierarchy, i.e. siblings of the
    generated type and enclosed in the top-level type. Refactored code
    structure is showing this is inconsistent, as bits/enums end up not as
    siblings, but rather normal enclosed types.
    
    // generator-impl      1/1 out of 85
    // java-api-generator  2/0 out of 80
    
    // FIXME: return type of added-by-uses leaf unions
    // FIXME: augment-identifier now impacts assigned package (!)
    // FIXME: duplicate Index generator?
    // FIXME: SchemaPath reconstruction
    
    JIRA: MDSAL-503
    Change-Id: I40c445aa1d3f79fd55f6e8007db1259d9a09c8d8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/AbstractExplicitGenerator.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import static com.google.common.base.Verify.verify;
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.binding.generator.impl.reactor.CollisionDomain.Member;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.mdsal.binding.model.api.type.builder.AnnotableTypeBuilder;
+import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilderBase;
+import org.opendaylight.mdsal.binding.model.api.type.builder.MethodSignatureBuilder;
+import org.opendaylight.mdsal.binding.spec.naming.BindingMapping;
+import org.opendaylight.yangtools.yang.common.AbstractQName;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.AddedByUsesAware;
+import org.opendaylight.yangtools.yang.model.api.CopyableNode;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * An explicit {@link Generator}, associated with a particular {@link EffectiveStatement}.
+ */
+public abstract class AbstractExplicitGenerator<T extends EffectiveStatement<?, ?>> extends Generator
+        implements CopyableNode {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractExplicitGenerator.class);
+
+    private final @NonNull T statement;
+
+    // FIXME: this is usually a proxy to original. We probably want to use a dedicated class for this
+    private AbstractExplicitGenerator<?> prev;
+
+    AbstractExplicitGenerator(final T statement) {
+        this.statement = requireNonNull(statement);
+    }
+
+    AbstractExplicitGenerator(final T statement, final AbstractCompositeGenerator<?> parent) {
+        super(parent);
+        this.statement = requireNonNull(statement);
+    }
+
+    /**
+     * Return the {@link EffectiveStatement} associated with this generator.
+     *
+     * @return An EffectiveStatement
+     */
+    public final @NonNull T statement() {
+        return statement;
+    }
+
+    @Override
+    public final boolean isAddedByUses() {
+        return statement instanceof AddedByUsesAware && ((AddedByUsesAware) statement).isAddedByUses();
+    }
+
+    @Override
+    public final boolean isAugmenting() {
+        return statement instanceof CopyableNode && ((CopyableNode) statement).isAugmenting();
+    }
+
+    final void linkOriginalGenerator(final GeneratorContext context) {
+        if (isAddedByUses() || isAugmenting()) {
+            LOG.trace("Linking {}", this);
+            prev = getParent().getOriginalChild(getQName());
+            LOG.trace("Linked {} to {}", this, prev);
+        }
+    }
+
+    final @Nullable AbstractExplicitGenerator<?> previous() {
+        return prev;
+    }
+
+    @NonNull AbstractExplicitGenerator<?> getOriginal() {
+        return prev == null ? this : prev.getOriginal();
+    }
+
+    @Nullable AbstractExplicitGenerator<?> findSchemaTreeGenerator(final QName qname) {
+        for (Generator child : this) {
+            if (child instanceof AbstractExplicitGenerator) {
+                final AbstractExplicitGenerator<?> gen = (AbstractExplicitGenerator<?>) child;
+                final EffectiveStatement<?, ?> stmt = gen.statement();
+                if (stmt instanceof SchemaTreeEffectiveStatement && qname.equals(stmt.argument())) {
+                    return gen;
+                }
+            }
+        }
+        return null;
+    }
+
+    final @NonNull AbstractExplicitGenerator<?> resolveSchemaNode(final @NonNull SchemaNodeIdentifier path) {
+        AbstractExplicitGenerator<?> current = this;
+        // FIXME: does not quite work as we need to find the original target, i.e. we need to understand when we are
+        //        following into an augmentation/grouping so that we correctly adjust remaining QNames to match
+        //        the expected namespace
+        for (QName qname : path.getNodeIdentifiers()) {
+            current = verifyNotNull(current.findSchemaTreeGenerator(qname), "Failed to find %s in %s", qname, current);
+        }
+        return current;
+    }
+
+    final @NonNull QName getQName() {
+        final Object arg = statement.argument();
+        verify(arg instanceof QName, "Unexpected argument %s", arg);
+        return (QName) arg;
+    }
+
+    @NonNull AbstractQName localName() {
+        // FIXME: this should be done in a nicer way
+        final Object argument = statement.argument();
+        verify(argument instanceof AbstractQName, "Illegal argument %s", argument);
+        return (AbstractQName) argument;
+    }
+
+    @Override
+    ClassPlacement classPlacement() {
+        // We process nodes introduced through augment or uses separately
+        // FIXME: this is not quite right!
+        return isAddedByUses() || isAugmenting() ? ClassPlacement.NONE : ClassPlacement.TOP_LEVEL;
+    }
+
+    @Override
+    Member createMember(final CollisionDomain domain) {
+        return domain.addPrimary(new CamelCaseNamingStrategy(namespace(), localName()));
+    }
+
+    void addAsGetterMethod(final @NonNull GeneratedTypeBuilderBase<?> builder,
+            final @NonNull TypeBuilderFactory builderFactory) {
+        if (isAugmenting()) {
+            // Do not process augmented nodes: they will be taken care of in their home augmentation
+            return;
+        }
+        if (isAddedByUses()) {
+            // If this generator has been added by a uses node, it is already taken care of by the corresponding
+            // grouping. There is one exception to this rule: 'type leafref' can use a relative path to point
+            // outside of its home grouping. In this case we need to examine the instantiation until we succeed in
+            // resolving the reference.
+            addAsGetterMethodOverride(builder, builderFactory);
+            return;
+        }
+
+        constructGetter(builder, methodReturnType(builderFactory));
+    }
+
+    MethodSignatureBuilder constructGetter(final GeneratedTypeBuilderBase<?> builder, final Type returnType) {
+        final MethodSignatureBuilder getMethod = builder
+            .addMethod(BindingMapping.getGetterMethodName(localName().getLocalName()))
+            .setReturnType(returnType);
+
+        annotateDeprecatedIfNecessary(getMethod);
+//        addComment(getMethod, node);
+
+        return getMethod;
+    }
+
+    void addAsGetterMethodOverride(final @NonNull GeneratedTypeBuilderBase<?> builder,
+            final @NonNull TypeBuilderFactory builderFactory) {
+        // No-op for most cases
+    }
+
+    @NonNull Type methodReturnType(final @NonNull TypeBuilderFactory builderFactory) {
+        return getGeneratedType(builderFactory);
+    }
+
+    final void annotateDeprecatedIfNecessary(final AnnotableTypeBuilder builder) {
+        annotateDeprecatedIfNecessary(statement, builder);
+    }
+
+    @Override
+    ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        helper.add("argument", statement.argument());
+
+        if (isAddedByUses()) {
+            helper.addValue("addedByUses");
+        }
+        if (isAugmenting()) {
+            helper.addValue("augmenting");
+        }
+        return helper;
+    }
+}
