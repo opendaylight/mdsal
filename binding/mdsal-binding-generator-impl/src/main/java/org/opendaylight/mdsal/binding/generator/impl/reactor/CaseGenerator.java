aff9d58b5d8e7715ed2c06d0dccef125e9ab2ac4
Robert Varga <robert.varga@pantheon.tech>
2020-12-22 22:01:36 +0100

    WIP: Bump to odlparent-9.0.0/yangtools-7.0.0
    Aside from the usual mechanical updates, we need to also rewrite
    mdsal-binding-generator-impl, as its mechanics relied completely on
    TypeDefinition.getPath(), which is no longer available.
    
    The rewritten component is a wee bit more navigable, making a number of
    deficiencies clearly visible. These are marked as FIXMEs for future
    improvements.
    
    One such problem arises when we attempt to create a derived type from an
    enumeration: here we ended up generating an empty DTO rather than
    properly specializing the type. Attempting to retain compatibility would
    be fruitless, as it would violate a number of method signatures.
    
    Furthermore leafref resolution is made stricter, requiring that
    references actually point to a leaf, not to a random location -- which
    was true in abstract-topology@2013-02-08.yang test model.
    
    Another change revolves around nested union types. These previously
    ended up being flattened in the class hierarchy, i.e. siblings of the
    generated type and enclosed in the top-level type. Refactored code
    structure is showing this is inconsistent, as bits/enums end up not as
    siblings, but rather normal enclosed types.
    
    // generator-impl       1/1 out of 85
    // java-api-generator  19/7 out of 80
    
    // FIXME: deal with prev/original generators
              - test lookups
              - filter phases in a logical way
              - augmenting
    // FIXME: duplicate Index generator?
    // FIXME: SchemaPath reconstruction
    
    JIRA: MDSAL-503
    Change-Id: I40c445aa1d3f79fd55f6e8007db1259d9a09c8d8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/CaseGenerator.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import static com.google.common.base.Verify.verify;
+
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilder;
+import org.opendaylight.mdsal.binding.model.util.BindingTypes;
+import org.opendaylight.yangtools.yang.model.api.stmt.CaseEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+
+/**
+ * Generator corresponding to a {@code case} statement.
+ */
+final class CaseGenerator extends AbstractCompositeGenerator<CaseEffectiveStatement> {
+    CaseGenerator(final CaseEffectiveStatement statement, final AbstractCompositeGenerator<?> parent) {
+        super(statement, parent);
+    }
+
+    @Override
+    void pushToInference(final SchemaInferenceStack dataTree) {
+        // No-op
+    }
+
+    @Override
+    GeneratedType createTypeImpl(final TypeBuilderFactory builderFactory) {
+        final GeneratedTypeBuilder builder = builderFactory.newGeneratedTypeBuilder(typeName());
+        builder.addImplementsType(BindingTypes.DATA_OBJECT);
+
+        // We also are implementing target choice's type. This is tricky, as we need to cover two distinct cases:
+        // - being a child of a choice (i.e. normal definition)
+        // - being a child of an augment (i.e. augmented into a choice)
+        final AbstractCompositeGenerator<?> parent = getParent();
+        final ChoiceGenerator choice;
+        if (parent instanceof ChoiceGenerator) {
+            choice = (ChoiceGenerator) parent;
+        } else {
+            verify(parent instanceof AbstractAugmentGenerator, "Unexpected parent %s", parent);
+            choice = ((AbstractAugmentGenerator) parent).targetChoiceGenerator();
+        }
+        // Most generators have a parent->child dependency due to parent methods' return types and therefore children
+        // must not request parent's type. That is not true for choice->case relationship and hence we do not need to
+        // go through DefaultType here.
+        builder.addImplementsType(choice.getGeneratedType(builderFactory));
+
+        addUsesInterfaces(builder, builderFactory);
+        addConcreteInterfaceMethods(builder);
+        annotateDeprecatedIfNecessary(builder);
+
+        addGetterMethods(builder, builderFactory);
+
+        // FIXME: implement this
+        //        context.addCaseType(caseNode.getPath(), caseTypeBuilder);
+        //        context.addChoiceToCaseMapping(refChoiceType, caseTypeBuilder, caseNode);
+        //        final SchemaPath choiceNodeParentPath = choiceNode.getPath().getParent();
+        //
+        //        if (!Iterables.isEmpty(choiceNodeParentPath.getPathFromRoot())) {
+        //            SchemaNode parent = findDataSchemaNode(schemaContext, choiceNodeParentPath);
+        //
+        //            if (parent instanceof AugmentationSchemaNode) {
+        //                final AugmentationSchemaNode augSchema = (AugmentationSchemaNode) parent;
+        //                final SchemaNodeIdentifier targetPath = augSchema.getTargetPath();
+        //                // FIXME: can we use findDataSchemaNode?
+        //                SchemaNode targetSchemaNode = findNodeInSchemaContext(schemaContext,
+        //                    targetPath.getNodeIdentifiers());
+        //                if (targetSchemaNode instanceof DataSchemaNode
+        //                       && ((DataSchemaNode) targetSchemaNode).isAddedByUses()) {
+        //                    if (targetSchemaNode instanceof DerivableSchemaNode) {
+        //                        targetSchemaNode = ((DerivableSchemaNode) targetSchemaNode).getOriginal()
+        //                                .orElse(null);
+        //                    }
+        //                    if (targetSchemaNode == null) {
+        //                        throw new IllegalStateException(
+        //                                "Failed to find target node from grouping for augmentation " + augSchema
+        //                                        + " in module " + context.module().getName());
+        //                    }
+        //                }
+        //                parent = targetSchemaNode;
+        //            }
+        //
+        //            checkState(parent != null, "Could not find Choice node parent %s", choiceNodeParentPath);
+        //            Type childOfType = findChildNodeByPath(parent.getPath());
+        //            if (childOfType == null) {
+        //                childOfType = findGroupingByPath(parent.getPath());
+        //            }
+        //            resolveDataSchemaNodes(context, caseTypeBuilder, childOfType, caseChildNodes, inGrouping);
+        //        } else {
+        //            resolveDataSchemaNodes(context, caseTypeBuilder, moduleToDataType(context), caseChildNodes,
+        //                inGrouping);
+        //        }
+
+        return builder.build();
+    }
+}
