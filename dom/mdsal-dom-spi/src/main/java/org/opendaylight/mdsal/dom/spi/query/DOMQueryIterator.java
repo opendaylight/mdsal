bdfda9383c39ca7a2711eb95406a355361c0009c
Peter Suna <peter.suna@pantheon.tech>
2021-01-22 16:53:42 +0100

    Nested lists cannot be decoded
    DOMQueryIterator is a bit confused about the path it is returning,
    causing binding codec to fail.
    
    The first part is wrong use of a MapEntryNode identifier where we
    should be pushing MapNode identifier.
    
    JIRA: MDSAL-654
    Change-Id: Ibf7466f6f390f517f1186a725c265a88e7266934
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/query/DOMQueryIterator.java
+++ dom/mdsal-dom-spi/src/main/java/org/opendaylight/mdsal/dom/spi/query/DOMQueryIterator.java
@@ -109,8 +109,8 @@ final class DOMQueryIterator extends AbstractIterator<Entry<YangInstanceIdentifi
 
     @SuppressFBWarnings(value = "NP_NONNULL_RETURN_VIOLATION", justification = "Ungrokked @Nullable")
     private @Nullable Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> findNext() {
-        // We always start with non-empty frames, as we signal end of data when we reach the end. We know this never
-        // null, by Eclipse insists. We do not care (that much) and use a poll() here.
+        // We always start with non-empty frames, as we signal end of data when we reach the end. We know this is never
+        // null, but Eclipse insists. We do not care (that much) and use a poll() here.
         // TODO: this is a huge method which could be restructured with hard tailcalls, alas we do not have those (yet?)
         //       Any such refactor better have some benchmarks to show non-regression.
         Frame current = frames.poll();
@@ -193,10 +193,6 @@ final class DOMQueryIterator extends AbstractIterator<Entry<YangInstanceIdentifi
                 continue;
             }
 
-            // Push our state back, it's just a placeholder for 'currentSelect'. Current path points at us and so does
-            // the saved Frame.
-            currentPath.addLast(current.data.getIdentifier());
-
             // Now decide what sort of entry to push. For maps we want to start an iterator already, so it gets
             // picked up as a continuation.
             if (child instanceof MapNode) {
@@ -231,10 +227,12 @@ final class DOMQueryIterator extends AbstractIterator<Entry<YangInstanceIdentifi
 
                 // We have a wildcard, expand it
                 frames.push(current);
+                currentPath.addLast(next);
                 current = new MapFrame(child, next, map.getValue().iterator());
             } else {
                 // Next step in iteration, deal with it
                 frames.push(current);
+                currentPath.addLast(child.getIdentifier());
                 current = new Frame(child, next);
             }
         }
