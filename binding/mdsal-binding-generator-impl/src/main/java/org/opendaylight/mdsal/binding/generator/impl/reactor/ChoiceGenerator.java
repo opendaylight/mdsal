1edb3d9c65dd697405eec37f9669bb2bf414d2f1
Robert Varga <robert.varga@pantheon.tech>
2021-02-20 08:14:21 +0100

    WIP: Add GeneratorReactor
    // FIXME: finish this up
    
    JIRA: MDSAL-503
    Change-Id: I2866271cd5395e1ceb8fd6de2f68e4d4197863ec
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/ChoiceGenerator.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import org.opendaylight.yangtools.yang.model.api.stmt.ChoiceEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+
+/**
+ * Generator corresponding to a {@code choice} statement.
+ */
+public final class ChoiceGenerator extends AbstractCompositeGenerator<ChoiceEffectiveStatement> {
+    ChoiceGenerator(final ChoiceEffectiveStatement statement) {
+        super(statement);
+    }
+
+    @Override
+    void pushToInference(final SchemaInferenceStack dataTree) {
+        // No-op
+    }
+}
