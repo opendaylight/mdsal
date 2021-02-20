a6d74d2a5ee1b054889d8117a91f34d18fea8ec3
Robert Varga <robert.varga@pantheon.tech>
2021-02-20 08:14:21 +0100

    WIP: Add GeneratorReactor
    The updated code layout makes it clear we have been doing a very
    bad thing for derived enumerations, actively requiring proper fix.
    
    Furthermore leafref resolution is made stricter, requiring that
    references actually point to a leaf, not to a random location -- which
    was true in abstract-topology@2013-02-08.yang test model.
    
    // Tests: 33/6 out of 113
    
    // FIXME: uses/augment cross-module lookups
    // FIXME: union placement
    // FIXME: uses -> action -> input
    // FIXME: mdsal572: duplicate nested builder (?!)
    // FIXME: SchemaPath reconstruction
    
    JIRA: MDSAL-503
    Change-Id: I2866271cd5395e1ceb8fd6de2f68e4d4197863ec
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/TypedefGenerator.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.mdsal.binding.model.api.GeneratedTransferObject;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTOBuilder;
+import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilderBase;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypedefEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+
+/**
+ * Generator corresponding to a {@code typedef} statement.
+ */
+final class TypedefGenerator extends AbstractTypeObjectGenerator<TypedefEffectiveStatement> {
+    /**
+     * List of all generators for types directly derived from this typedef. We populate this list during initial type
+     * linking. It allows us to easily cascade inferences made by this typedef down the type derivation tree.
+     */
+    private List<AbstractTypeObjectGenerator<?>> derivedGenerators = null;
+
+    TypedefGenerator(final TypedefEffectiveStatement statement, final AbstractCompositeGenerator<?> parent) {
+        super(statement, parent);
+    }
+
+    @Override
+    StatementNamespace namespace() {
+        return StatementNamespace.TYPEDEF;
+    }
+
+    @Override
+    void pushToInference(final SchemaInferenceStack dataTree) {
+        // No-op
+    }
+
+    void addDerivedGenerator(final AbstractTypeObjectGenerator<?> derivedGenerator) {
+        if (derivedGenerators == null) {
+            derivedGenerators = new ArrayList<>(4);
+        }
+        derivedGenerators.add(requireNonNull(derivedGenerator));
+    }
+
+    @Override
+    void bindDerivedGenerators(final TypeReference reference) {
+        // Trigger any derived resolvers ...
+        if (derivedGenerators != null) {
+            for (AbstractTypeObjectGenerator<?> derived : derivedGenerators) {
+                derived.bindTypeDefinition(reference);
+            }
+        }
+        // ... and make sure nobody can come in late
+        derivedGenerators = List.of();
+    }
+
+    @Override
+    ClassPlacement classPlacementImpl(final TypedefGenerator baseGen) {
+        return ClassPlacement.TOP_LEVEL;
+    }
+
+    @Override
+    TypeDefinition<?> extractTypeDefinition() {
+        return statement().getTypeDefinition();
+    }
+
+    @Override
+    GeneratedTransferObject createDerivedType(final TypeBuilderFactory builderFactory,
+            final GeneratedTransferObject baseType) {
+        final GeneratedTOBuilder builder = builderFactory.newGeneratedTOBuilder(typeName());
+        builder.setTypedef(true);
+        builder.setExtendsType(baseType);
+
+        final TypeDefinition<?> typedef = statement().getTypeDefinition();
+        addStringRegExAsConstant(builder, resolveRegExpressions(typedef));
+
+        // FIXME: anything else?
+
+        return builder.build();
+    }
+
+    @Override
+    void addAsGetterMethod(final GeneratedTypeBuilderBase<?> builder, final TypeBuilderFactory builderFactory) {
+        // typedefs are a separate concept
+    }
+
+    @Override
+    Type methodReturnType(final TypeBuilderFactory builderFactory, final TypedefGenerator baseGen,
+            final TypeReference refType) {
+        // we should never reach here
+        throw new UnsupportedOperationException();
+    }
+}
