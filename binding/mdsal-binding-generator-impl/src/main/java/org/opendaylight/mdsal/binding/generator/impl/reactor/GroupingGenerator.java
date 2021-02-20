d3d3fb3588725d1b964c0f20e677f2eb62241e3c
Robert Varga <robert.varga@pantheon.tech>
2021-02-20 08:14:21 +0100

    WIP: Add GeneratorReactor
    // FIXME: finish this up
    
    Change-Id: I2866271cd5395e1ceb8fd6de2f68e4d4197863ec
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/GroupingGenerator.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import org.opendaylight.yangtools.yang.model.api.stmt.GroupingEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+
+/**
+ * Generator corresponding to a {@code grouping} statement.
+ */
+public final class GroupingGenerator extends AbstractCompositeGenerator<GroupingEffectiveStatement> {
+    GroupingGenerator(final GroupingEffectiveStatement statement) {
+        super(statement);
+    }
+
+    @Override
+    StatementNamespace namespace() {
+        return StatementNamespace.GROUPING;
+    }
+
+    @Override
+    boolean pushToDataTree(final SchemaInferenceStack dataTree) {
+        dataTree.enterGrouping(statement().argument());
+        return true;
+    }
+}
---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/IdentityGenerator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import org.opendaylight.yangtools.yang.model.api.stmt.IdentityEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+
+/**
+ * Generator corresponding to a {@code identity} statement.
+ */
+public final class IdentityGenerator extends Generator<IdentityEffectiveStatement> {
+    IdentityGenerator(final IdentityEffectiveStatement statement) {
+        super(statement);
+    }
+
+    @Override
+    StatementNamespace namespace() {
+        return StatementNamespace.IDENTITY;
+    }
+
+    @Override
+    boolean pushToDataTree(final SchemaInferenceStack dataTree) {
+        throw new UnsupportedOperationException("Cannot push " + statement() + " to data tree");
+    }
+}
