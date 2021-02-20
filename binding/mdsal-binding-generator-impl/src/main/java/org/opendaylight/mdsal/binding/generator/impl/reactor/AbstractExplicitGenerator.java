0f0380e86e05e48f7b653ba779b61effebd68f63
Robert Varga <robert.varga@pantheon.tech>
2021-02-20 08:14:21 +0100

    WIP: Add GeneratorReactor
    // FIXME: composite child methods
    // FIXME: union typedefs
    // FIXME: leaf types
    // FIXME: leaf union types
    // FIXME: RPC services
    // FIXME: Notification serives
    
    JIRA: MDSAL-503
    Change-Id: I2866271cd5395e1ceb8fd6de2f68e4d4197863ec
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/AbstractExplicitGenerator.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import static com.google.common.base.Verify.verify;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.binding.generator.impl.reactor.CollisionDomain.Member;
+import org.opendaylight.yangtools.yang.common.AbstractQName;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.AddedByUsesAware;
+import org.opendaylight.yangtools.yang.model.api.CopyableNode;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
+
+/**
+ * An explicit {@link Generator}, associated with a particular {@link EffectiveStatement}.
+ */
+abstract class AbstractExplicitGenerator<T extends EffectiveStatement<?, ?>> extends Generator implements CopyableNode {
+    private final @NonNull T statement;
+
+    private Optional<Member> member;
+
+    AbstractExplicitGenerator(final T statement) {
+        this.statement = requireNonNull(statement);
+    }
+
+    AbstractExplicitGenerator(final T statement, final AbstractCompositeGenerator<?> parent) {
+        super(parent);
+        this.statement = requireNonNull(statement);
+    }
+
+    /**
+     * Return the {@link EffectiveStatement} associated with this generator.
+     *
+     * @return An EffectiveStatement
+     */
+    public final @NonNull T statement() {
+        return statement;
+    }
+
+    @Override
+    public final boolean isAddedByUses() {
+        return statement instanceof AddedByUsesAware && ((AddedByUsesAware) statement).isAddedByUses();
+    }
+
+    @Override
+    public final boolean isAugmenting() {
+        return statement instanceof CopyableNode && ((CopyableNode) statement).isAugmenting();
+    }
+
+    @Nullable AbstractExplicitGenerator<?> findSchemaTreeGenerator(final QName qname) {
+        for (Generator child : this) {
+            if (child instanceof AbstractExplicitGenerator) {
+                final AbstractExplicitGenerator<?> gen = (AbstractExplicitGenerator<?>) child;
+                final EffectiveStatement<?, ?> stmt = gen.statement();
+                if (stmt instanceof SchemaTreeEffectiveStatement && qname.equals(stmt.argument())) {
+                    return gen;
+                }
+            }
+        }
+        return null;
+    }
+
+    @NonNull AbstractQName localName() {
+        // FIXME: this should be done in a nicer way
+        final Object argument = statement.argument();
+        verify(argument instanceof AbstractQName, "Illegal argument %s", argument);
+        return (AbstractQName) argument;
+    }
+
+    @Override
+    ClassPlacement classPlacement() {
+        // We process nodes introduced through augment or uses separately
+        return isAddedByUses() || isAugmenting() ? ClassPlacement.NONE : ClassPlacement.TOP_LEVEL;
+    }
+
+    @Override
+    final Member ensureMember() {
+        if (member == null) {
+            final ClassPlacement placement = classPlacement();
+            switch (placement) {
+                case NONE:
+                    member = Optional.empty();
+                    break;
+                case TOP_LEVEL:
+                case PHANTOM:
+                    member = Optional.of(createMember());
+                    break;
+                default:
+                    throw new IllegalStateException("Unhandled placement " + placement);
+            }
+        }
+        return member.orElse(null);
+    }
+
+    @NonNull Member createMember() {
+        return getParent().domain().addPrimary(new CamelCaseNamingStrategy(namespace(), localName()));
+    }
+
+    @Override
+    ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return helper.add("argument", statement.argument());
+    }
+}
---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/ListGenerator.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import static org.opendaylight.mdsal.binding.model.util.BindingTypes.identifiable;
+
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.JavaTypeName;
+import org.opendaylight.mdsal.binding.model.api.ParameterizedType;
+import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilder;
+import org.opendaylight.mdsal.binding.model.util.Types;
+import org.opendaylight.mdsal.binding.spec.naming.BindingMapping;
+import org.opendaylight.yangtools.yang.common.Ordering;
+import org.opendaylight.yangtools.yang.model.api.stmt.KeyEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ListEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.OrderedByEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+
+/**
+ * Generator corresponding to a {@code list} statement.
+ */
+public final class ListGenerator extends AbstractCompositeGenerator<ListEffectiveStatement> {
+    private static final JavaTypeName OVERRIDE_ANNOTATION = JavaTypeName.create(Override.class);
+
+    private KeyGenerator keyGen;
+
+    ListGenerator(final ListEffectiveStatement statement, final AbstractCompositeGenerator<?> parent) {
+        super(statement, parent);
+    }
+
+    @Override
+    void pushToInference(final SchemaInferenceStack dataTree) {
+        dataTree.enterDataTree(statement().getIdentifier());
+    }
+
+    @Override
+    void linkCompositeDependencies(final GeneratorContext context) {
+        super.linkCompositeDependencies(context);
+        if (producesType()) {
+            keyGen = statement().findFirstEffectiveSubstatement(KeyEffectiveStatement.class)
+                .map(stmt -> getParent().getGenerator(stmt))
+                .map(KeyGenerator.class::cast)
+                .orElse(null);
+        }
+    }
+
+    @Override
+    GeneratedType createTypeImpl(final TypeBuilderFactory builderFactory) {
+        final GeneratedTypeBuilder builder = builderFactory.newGeneratedTypeBuilder(typeName());
+        addImplementsChildOf(builder);
+        addUsesInterfaces(builder, builderFactory);
+        addConcreteInterfaceMethods(builder);
+        // annotateDeprecatedIfNecessary(node, genType);
+
+        //    final Module module = context.module();
+        //    builder.setModuleName(module.getName());
+        //    addCodegenInformation(builder, module, node);
+        //    builder.setSchemaPath(node.getPath());
+
+        final GeneratedType keyType = findKeyType(builderFactory);
+        if (keyType != null) {
+            // Add yang.binding.Identifiable and its key() method
+            builder.addImplementsType(identifiable(keyType));
+            builder.addMethod(BindingMapping.IDENTIFIABLE_KEY_NAME)
+                .setReturnType(keyType)
+                .addAnnotation(OVERRIDE_ANNOTATION);
+        }
+
+        return builder.build();
+    }
+
+    private void getters(final TypeBuilderFactory builderFactory) {
+        final GeneratedType myType = createType(builderFactory);
+
+        // Decide whether to generate a List or a Map
+        final GeneratedType keyType = findKeyType(builderFactory);
+        final ParameterizedType listType = keyType != null ? Types.mapTypeFor(keyType, myType)
+            : Types.listTypeFor(myType);
+
+        //      constructGetter(parent, listType, node).setMechanics(ValueMechanics.NULLIFY_EMPTY);
+        //      constructNonnull(parent, listType, node);
+        //
+        //      actionsToGenType(context, genType, node, keyTypeBuilder, inGrouping);
+        //      notificationsToGenType(context, genType, node, keyTypeBuilder, inGrouping);
+        //
+        //      for (final DataSchemaNode schemaNode : node.getChildNodes()) {
+        //          if (!schemaNode.isAugmenting()) {
+        //              addSchemaNodeToListBuilders(context, schemaNode, genType, keyTypeBuilder, listKeys, inGrouping);
+        //          }
+        //      }
+        //
+        //      typeBuildersToGenTypes(context, genType, keyTypeBuilder);
+    }
+
+    private @Nullable GeneratedType findKeyType(final TypeBuilderFactory builderFactory) {
+        if (keyGen != null) {
+            final Ordering ordering = statement()
+                .findFirstEffectiveSubstatementArgument(OrderedByEffectiveStatement.class)
+                .orElse(Ordering.SYSTEM);
+            if (ordering == Ordering.SYSTEM) {
+                return keyGen.createType(builderFactory);
+            }
+        }
+        return null;
+    }
+}
