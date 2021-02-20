db589cd7cb2e9ad6e22fcdc8d91c7fdbf32c472a
Robert Varga <robert.varga@pantheon.tech>
2021-02-20 08:14:21 +0100

    WIP: Add GeneratorReactor
    The updated code layout makes it clear we have been doing a very
    bad thing for derived enumerations, actively requiring proper fix.
    
    // Tests: 32/21 out of 113
    
    // FIXME: uses/augment
    // FIXME: unions
    // FIXME: routing-context
    // FIXME: SchemaPath reconstruction
    
    JIRA: MDSAL-503
    Change-Id: I2866271cd5395e1ceb8fd6de2f68e4d4197863ec
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/BindingRuntimeTypesFactory.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl;
+
+import static com.google.common.base.Verify.verify;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.generator.impl.reactor.AbstractExplicitGenerator;
+import org.opendaylight.mdsal.binding.generator.impl.reactor.Generator;
+import org.opendaylight.mdsal.binding.generator.impl.reactor.GeneratorReactor;
+import org.opendaylight.mdsal.binding.generator.impl.reactor.ModuleGenerator;
+import org.opendaylight.mdsal.binding.generator.impl.reactor.TypeBuilderFactory;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeTypes;
+import org.opendaylight.yangtools.concepts.Mutable;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DocumentedNode.WithStatus;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.IdentityEffectiveStatement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class BindingRuntimeTypesFactory implements Mutable {
+    private static final Logger LOG = LoggerFactory.getLogger(BindingRuntimeTypesFactory.class);
+
+    private final Map<Type, AugmentationSchemaNode> augmentationToSchema = new HashMap<>();
+    private final Multimap<Type, Type> choiceToCases = HashMultimap.create();
+    private final Map<Type, WithStatus> typeToSchema = new HashMap<>();
+    private final Map<QName, Type> identities = new HashMap<>();
+
+    // Note: we are keying through WithStatus, but these nodes compare on semantics, so equivalent schema nodes
+    //       can result in two distinct types. We certainly need to keep them separate.
+    private final Map<WithStatus, Type> schemaToType = new IdentityHashMap<>();
+
+    /*
+     * Fun parts are here. ModuleContext maps have Builders in them, we want plain types. We may encounter each
+     * builder multiple times, hence we keep a builder->instance cache.
+     */
+    private final Map<Type, Type> builderToType = new IdentityHashMap<>();
+
+    private BindingRuntimeTypesFactory() {
+        // Hidden on purpose
+    }
+
+    static @NonNull BindingRuntimeTypes createTypes(final @NonNull EffectiveModelContext context) {
+        final Collection<ModuleGenerator> moduleGens = new GeneratorReactor(context)
+            .execute(TypeBuilderFactory.runtime())
+            .values();
+
+        final Stopwatch sw = Stopwatch.createStarted();
+        final BindingRuntimeTypesFactory factory = new BindingRuntimeTypesFactory();
+        factory.indexTypes(moduleGens);
+        LOG.debug("Indexed {} generators in {}", moduleGens.size(), sw);
+
+        return new BindingRuntimeTypes(context, factory.augmentationToSchema, factory.typeToSchema,
+            factory.schemaToType, factory.choiceToCases, factory.identities);
+    }
+
+    private void indexTypes(final Iterable<? extends Generator> generators) {
+        for (Generator gen : generators) {
+            gen.generatedType().ifPresent(type -> indexType(gen, type));
+            indexTypes(gen);
+        }
+    }
+
+    private void indexType(final @NonNull Generator generator, final @NonNull GeneratedType type) {
+        if (generator instanceof AbstractExplicitGenerator) {
+            final EffectiveStatement<?, ?> stmt = ((AbstractExplicitGenerator<?>) generator).statement();
+            if (stmt instanceof IdentityEffectiveStatement) {
+                identities.put(((IdentityEffectiveStatement) stmt).argument(), type);
+            } else if (stmt instanceof AugmentEffectiveStatement) {
+                verify(stmt instanceof AugmentationSchemaNode, "Unexpected statement %s", stmt);
+                augmentationToSchema.put(type, (AugmentationSchemaNode) stmt);
+            }
+
+            if (stmt instanceof WithStatus) {
+                final WithStatus schema = (WithStatus) stmt;
+                typeToSchema.put(type, schema);
+                schemaToType.put(schema, type);
+            }
+
+//            for (Entry<Type, Type> e : ctx.getChoiceToCases().entries()) {
+//                choiceToCases.put(builtType(builderToType, e.getKey()), builtType(builderToType, e.getValue()));
+//            }
+        }
+    }
+}
---
+++ binding/mdsal-binding-generator-impl/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/LeafListGenerator.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.mdsal.binding.model.util.Types;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafListEffectiveStatement;
+
+/**
+ * Generator corresponding to a {@code leaf-list} statement.
+ */
+final class LeafListGenerator extends AbstractTypeAwareGenerator<LeafListEffectiveStatement> {
+    LeafListGenerator(final LeafListEffectiveStatement statement, final AbstractCompositeGenerator<?> parent) {
+        super(statement, parent);
+    }
+
+    @Override
+    Type methodReturnType(final TypeBuilderFactory builderFactory, final TypedefGenerator baseGen,
+            final TypeReference refType) {
+        return Types.listTypeFor(super.methodReturnType(builderFactory, baseGen, refType));
+    }
+}
