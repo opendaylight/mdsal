5d5ca963d16dbfec9d6652fe371bf392bdb3137b
Robert Varga <robert.varga@pantheon.tech>
2021-02-21 13:15:35 +0100

    Do not allow multi-datastore transactions
    Cross-datastore transtions are something that does not fit into
    interaction model of neither MD-SAL nor NETCONF WG. As we are evolving
    our APIs to make them impossible, make sure we report attempts to use
    transations across multiple datastore.
    
    JIRA: MDSAL-775
    Change-Id: If9a3bb619f4fd5703f786d14f42485d2073968ec
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/AbstractDOMForwardedCompositeTransaction.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/AbstractDOMForwardedCompositeTransaction.java
@@ -10,16 +10,19 @@ package org.opendaylight.mdsal.dom.broker;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.util.Collection;
 import java.util.Map;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.mdsal.common.api.TransactionDatastoreMismatchException;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeTransaction;
 import org.opendaylight.mdsal.dom.spi.store.DOMStoreTransaction;
 
 /**
  * Composite DOM Transaction backed by {@link DOMStoreTransaction}.
  *
- *<p>
+ * <p>
  * Abstract base for composite transaction, which provides access only to common
  * functionality as retrieval of subtransaction, close method and retrieval of
  * identifier.
@@ -28,8 +31,22 @@ import org.opendaylight.mdsal.dom.spi.store.DOMStoreTransaction;
  */
 abstract class AbstractDOMForwardedCompositeTransaction<T extends DOMStoreTransaction>
         implements DOMDataTreeTransaction {
+    private static final VarHandle DSTYPE;
+
+    static {
+        try {
+            DSTYPE = MethodHandles.lookup().findVarHandle(AbstractDOMForwardedCompositeTransaction.class, "dsType",
+                LogicalDatastoreType.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
     private final Map<LogicalDatastoreType, T> backingTxs;
     private final Object identifier;
+
+    @SuppressWarnings("unused")
+    private volatile LogicalDatastoreType dsType;
 
     /**
      * Creates new composite Transactions.
@@ -46,26 +63,33 @@ abstract class AbstractDOMForwardedCompositeTransaction<T extends DOMStoreTransa
     }
 
     /**
-     * Returns subtransaction associated with supplied key.
+     * Returns subtransaction associated with supplied datastore type.
      *
-     * @param key is used to retrieve subtransaction object
+     * <p>
+     * The method allows usage of single datastore type per transaction instance;
+     * eligible datastore type is defined by first method access.
+     *
+     * @param datastoreType is used to identify subtransaction object
      * @return the subtransaction object
-     * @throws NullPointerException
-     *             if key is null
-     * @throws IllegalArgumentException
-     *             if no subtransaction is associated with key.
+     * @throws NullPointerException     if datastoreType is null
+     * @throws IllegalArgumentException if no ubtransaction is associated with datastoreType.
+     * @throws TransactionDatastoreMismatchException if datastoreType mismatches the one used at first access
      */
-    protected final T getSubtransaction(final LogicalDatastoreType key) {
-        requireNonNull(key, "key must not be null.");
+    protected final T getSubtransaction(final LogicalDatastoreType datastoreType) {
+        final var ds = requireNonNull(datastoreType, "datastoreType must not be null.");
 
-        final T ret = backingTxs.get(key);
-        checkArgument(ret != null, "No subtransaction associated with %s", key);
+        final var witness = (LogicalDatastoreType) DSTYPE.compareAndExchange(this, null, ds);
+        if (witness != null && witness != ds) {
+            throw new TransactionDatastoreMismatchException(ds, witness);
+        }
+
+        final T ret = backingTxs.get(datastoreType);
+        checkArgument(ret != null, "No subtransaction associated with %s", datastoreType);
         return ret;
     }
 
     /**
      * Returns immutable Iterable of all subtransactions.
-     *
      */
     protected Collection<T> getSubtransactions() {
         return backingTxs.values();
