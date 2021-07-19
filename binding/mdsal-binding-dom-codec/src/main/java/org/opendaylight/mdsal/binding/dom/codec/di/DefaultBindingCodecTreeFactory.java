d8bf46b5b45622a801b53711f5a2a37b56c405a9
tadei.bilan <tadei.bilan@pantheon.tech>
2021-07-19 17:19:52 +0300

    Convert mdsal-binding-dom-codec to a JPMS module
    Convert mdsal-binding-dom-codec to a properly-injected JPMS module,
    available through @Singleton, @Component and @MetaInfServices.
    
    Also update NonCachingCodecTest to expose its mock fields and
    UnionValueOptionContextTest to use test-model's classes.
    
    JIRA: MDSAL-641
    Change-Id: I3cbe8da4fc4905a1246a5e94a7e208e1bccfa1d8
    Signed-off-by: tadei.bilan <tadei.bilan@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/di/DefaultBindingCodecTreeFactory.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.codec.di;
+
+import com.google.common.annotations.Beta;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.binding.dom.codec.impl.SimpleBindingDOMCodecFactory;
+
+@Beta
+@Singleton
+public final class DefaultBindingCodecTreeFactory extends SimpleBindingDOMCodecFactory {
+    @Inject
+    public DefaultBindingCodecTreeFactory() {
+        // Exposed for DI
+    }
+}
---
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/di/DefaultBindingDOMCodecServices.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.codec.di;
+
+import com.google.common.annotations.Beta;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.dom.codec.impl.BindingCodecContext;
+import org.opendaylight.mdsal.binding.dom.codec.spi.ForwardingBindingDOMCodecServices;
+import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeContext;
+
+@Beta
+@Singleton
+public final class DefaultBindingDOMCodecServices extends ForwardingBindingDOMCodecServices {
+    private final @NonNull BindingCodecContext delegate;
+
+    @Inject
+    public DefaultBindingDOMCodecServices(final BindingRuntimeContext context) {
+        delegate = new BindingCodecContext(context);
+    }
+
+    @Override
+    protected BindingCodecContext delegate() {
+        return delegate;
+    }
+}
