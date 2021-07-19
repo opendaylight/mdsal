62a8b8540d0fbb68148bc258671a9a7ca845d9d9
tadei.bilan <tadei.bilan@pantheon.tech>
2021-07-19 17:19:52 +0300

    Convert mdsal-binding-dom-codec to a JPMS module
    Convert mdsal-binding-dom-codec to a properly-injected JPMS module,
    available through @Singleton, @Component and @MetaInfServices.
    
    Also update NonCachingCodecTest to expose its mock fields and
    UnionValueOptionContextTest to use test-model's classes.
    
    This also necessitates relocating the loeader package, as otherwise
    maven-javadoc-plugin ends up being confused passes down invalid
    @packages to javadoc.
    
    JIRA: MDSAL-641
    Change-Id: I3cbe8da4fc4905a1246a5e94a7e208e1bccfa1d8
    Signed-off-by: tadei.bilan <tadei.bilan@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/loader/package-info.java
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/loader/package-info.java
@@ -7,7 +7,7 @@
  */
 /**
  * {@link java.lang.ClassLoader} support for Binding/DOM codec translation code generators. This package provides one
- * core class, {@link org.opendaylight.mdsal.binding.dom.codec.loader.CodecClassLoader}, which allows lookup of
+ * core class, {@link org.opendaylight.mdsal.binding.dom.codec.impl.loader.CodecClassLoader}, which allows lookup of
  * compile-time-generated Binding classes for the purpose of referencing them within code generators and which
  * serves as the ClassLoader holding runtime-generated codecs.
  *
@@ -15,4 +15,4 @@
  * While the interfaces and classes in this package may be publicly accessible, they are an implementation detail and
  * may change incompatibly at any time.
  */
-package org.opendaylight.mdsal.binding.dom.codec.loader;
+package org.opendaylight.mdsal.binding.dom.codec.impl.loader;
