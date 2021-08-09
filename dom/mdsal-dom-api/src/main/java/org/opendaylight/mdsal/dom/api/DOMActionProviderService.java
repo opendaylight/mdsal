ba6bba4d2c94aeb7c1e1cf864f3a454afaa6718f
Robert Varga <robert.varga@pantheon.tech>
2021-08-09 22:36:05 +0200

    WIP: Support DOMAction-level routing
    We have a distinct use case where we want to route actions across
    a system. In this scenario, a next hop provider registers itself as the
    local provider with the appropriate cost.
    
    This capability is also exposed as the simplest way of registering
    binding-level implementations -- where we want to register to all
    actions on the operational datastore.
    
    Unfortunately our current interface does not allow that, as we strictly
    require at least DOMActionInstance. This is a modeling accident, as we
    need a datastore type -- which we can easily provide.
    
    Update DOMActionProviderService with new methods to allow for this use
    case and add proper tests for action routing.
    
    // FIXME: finish implementation
    
    JIRA: MDSAL-681
    Change-Id: I6daf70b28347fab88c8dc467fce267d8ba71e57d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMActionProviderService.java
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMActionProviderService.java
@@ -11,35 +11,109 @@ import com.google.common.annotations.Beta;
 import com.google.common.collect.ImmutableSet;
 import java.util.Set;
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
 
 /**
  * A {@link DOMService} which allows registration of action implementations with a conceptual router. The client
  * counterpart of this service is {@link DOMActionService}.
+ *
+ * <p>
+ * Implementations can be registered to specific {@link DOMActionInstance}s, where the implementation will be invoked
+ * only for requests matching that instances. Alternatively an implementation can be registered to service all
+ * {@link DOMActionInstance}s on specified {@link LogicalDatastoreType}. Registrations to specific instances take
+ * precedence over registrations to all instances on a datastore.
  */
 @Beta
 @NonNullByDefault
 public interface DOMActionProviderService
         extends DOMExtensibleService<DOMActionProviderService, DOMActionProviderServiceExtension> {
     /**
-     * Register an {@link DOMActionImplementation} object with this service.
+     * Register an {@link DOMActionImplementation} object with this service, servicing specified action instances.
      *
      * @param implementation action implementation, must not be null
      * @param instances Set of supported operation identifiers. Must not be null, empty, or contain a null element.
      * @return A {@link ObjectRegistration} object, guaranteed to be non-null.
-     * @throws NullPointerException if implementation or types is null
+     * @throws NullPointerException if {@code implementation} or {@code instances} is null, or if {@code instances}
+     *                              contains a null element.
      * @throws IllegalArgumentException if {@code instances} is empty
      */
     <T extends DOMActionImplementation> ObjectRegistration<T> registerActionImplementation(T implementation,
-            Set<DOMActionInstance> instances);
+        Set<DOMActionInstance> instances);
 
+    /**
+     * Register an {@link DOMActionImplementation} object with this service, servicing specified action instance.
+     *
+     * @param implementation action implementation, must not be null
+     * @param instance supported operation identifier. Must not be null.
+     * @return A {@link ObjectRegistration} object, guaranteed to be non-null.
+     * @throws NullPointerException if any argument is null
+     */
     default <T extends DOMActionImplementation> ObjectRegistration<T> registerActionImplementation(
             final T implementation, final DOMActionInstance instance) {
         return registerActionImplementation(implementation, ImmutableSet.of(instance));
     }
 
+    /**
+     * Register an {@link DOMActionImplementation} object with this service, servicing specified action instances.
+     *
+     * @param implementation action implementation, must not be null
+     * @param instances Set of supported operation identifiers. Must not be null, empty, or contain a null element.
+     * @return A {@link ObjectRegistration} object, guaranteed to be non-null.
+     * @throws NullPointerException if {@code implementation} or {@code instances} is null, or if {@code instances}
+     *                              contains a null element.
+     * @throws IllegalArgumentException if {@code instances} is empty
+     */
     default <T extends DOMActionImplementation> ObjectRegistration<T> registerActionImplementation(
             final T implementation, final DOMActionInstance... instances) {
         return registerActionImplementation(implementation, ImmutableSet.copyOf(instances));
+    }
+
+    /**
+     * Register an {@link DOMActionImplementation} object with this service, servicing all action instances on specified
+     * datastores.
+     *
+     * @param implementation action implementation, must not be null
+     * @param type action type, must not be null
+     * @param datastores Set of supported datastores, must not be null, empty, or contain a null element.
+     * @return A {@link ObjectRegistration} object, guaranteed to be non-null.
+     * @throws NullPointerException if {@code implementation} or {@code datastores} is null, or if {@code datastores}
+     *                              contains a null element.
+     * @throws IllegalArgumentException if {@code instances} is empty
+     */
+    <T extends DOMActionImplementation> ObjectRegistration<T> registerActionImplementation(T implementation,
+        Absolute type, Set<LogicalDatastoreType> datastores);
+
+    /**
+     * Register an {@link DOMActionImplementation} object with this service, servicing all action instances on specified
+     * datastore.
+     *
+     * @param implementation action implementation, must not be null
+     * @param type action type, must not be null
+     * @param datastore datastore to service
+     * @return A {@link ObjectRegistration} object, guaranteed to be non-null.
+     * @throws NullPointerException if any argument is null
+     */
+    default <T extends DOMActionImplementation> ObjectRegistration<T> registerActionImplementation(
+            final T implementation, final Absolute type, final LogicalDatastoreType datastore) {
+        return registerActionImplementation(implementation, type, ImmutableSet.of(datastore));
+    }
+
+    /**
+     * Register an {@link DOMActionImplementation} object with this service, servicing all action instances on specified
+     * datastores.
+     *
+     * @param implementation action implementation, must not be null
+     * @param type action type, must not be null
+     * @param datastores Set of supported datastores, must not be null, empty, or contain a null element.
+     * @return A {@link ObjectRegistration} object, guaranteed to be non-null.
+     * @throws NullPointerException if {@code implementation} or {@code datastores} is null, or if {@code datastores}
+     *                              contains a null element.
+     * @throws IllegalArgumentException if {@code instances} is empty
+     */
+    default <T extends DOMActionImplementation> ObjectRegistration<T> registerActionImplementation(
+            final T implementation, final Absolute type, final LogicalDatastoreType... datastores) {
+        return registerActionImplementation(implementation, type, ImmutableSet.copyOf(datastores));
     }
 }
