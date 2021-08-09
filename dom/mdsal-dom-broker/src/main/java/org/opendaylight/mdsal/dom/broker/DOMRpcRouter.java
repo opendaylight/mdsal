42538414dd58febbb2cdf9fe82593a6d68aab58c
Robert Varga <robert.varga@pantheon.tech>
2021-08-09 22:36:05 +0200

    Support DOMAction-level routing
    We have a distinct use case where we want to route actions across
    a system. In this scenario, a next hop provider registers itself as the
    local provider with the appropriate cost.
    
    This capability is also exposed as the simplest way of registering
    binding-level implementations -- where we want to register to all
    actions on the operational datastore.
    
    Add explicit documentation around how how the arguments are interpreted
    and teach DOMRpcRouter about wildcards -- similar to how it deals with
    RPCs.
    
    JIRA: MDSAL-681
    Change-Id: I6daf70b28347fab88c8dc467fce267d8ba71e57d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/DOMRpcRouter.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/DOMRpcRouter.java
@@ -426,13 +426,13 @@ public final class DOMRpcRouter extends AbstractRegistration
         @Override
         public ListenableFuture<? extends DOMActionResult> invokeAction(final Absolute type,
                 final DOMDataTreeIdentifier path, final ContainerNode input) {
-            final DOMActionRoutingTableEntry entry = (DOMActionRoutingTableEntry) actionRoutingTable.getEntry(type);
-            if (entry == null) {
-                return Futures.immediateFailedFuture(
-                    new DOMActionNotAvailableException("No implementation of Action %s available", type));
-            }
+            final YangInstanceIdentifier pathRoot = path.getRootIdentifier();
+            checkArgument(!pathRoot.isEmpty(), "Action path must not be empty");
 
-            return OperationInvocation.invoke(entry, type, path, requireNonNull(input));
+            final DOMActionRoutingTableEntry entry = (DOMActionRoutingTableEntry) actionRoutingTable.getEntry(type);
+            return entry != null ? OperationInvocation.invoke(entry, type, path, requireNonNull(input))
+                : Futures.immediateFailedFuture(
+                    new DOMActionNotAvailableException("No implementation of Action %s available", type));
         }
     }
 
@@ -440,7 +440,7 @@ public final class DOMRpcRouter extends AbstractRegistration
     private final class ActionProviderServiceFacade implements DOMActionProviderService {
         @Override
         public <T extends DOMActionImplementation> ObjectRegistration<T> registerActionImplementation(
-            final T implementation, final Set<DOMActionInstance> instances) {
+                final T implementation, final Set<DOMActionInstance> instances) {
 
             synchronized (DOMRpcRouter.this) {
                 final DOMActionRoutingTable oldTable = actionRoutingTable;
@@ -539,10 +539,14 @@ public final class DOMRpcRouter extends AbstractRegistration
 
         static ListenableFuture<? extends DOMActionResult> invoke(final DOMActionRoutingTableEntry entry,
                 final Absolute type, final DOMDataTreeIdentifier path, final ContainerNode input) {
-            final List<DOMActionImplementation> impls = entry.getImplementations(path);
+            List<DOMActionImplementation> impls = entry.getImplementations(path);
             if (impls == null) {
-                return Futures.immediateFailedFuture(
-                    new DOMActionNotAvailableException("No implementation of Action %s available for %s", type, path));
+                impls = entry.getImplementations(
+                    new DOMDataTreeIdentifier(path.getDatastoreType(), YangInstanceIdentifier.empty()));
+                if (impls == null) {
+                    return Futures.immediateFailedFuture(new DOMActionNotAvailableException(
+                        "No implementation of Action %s available for %s", type, path));
+                }
             }
 
             return impls.get(0).invokeAction(type, path, input);
