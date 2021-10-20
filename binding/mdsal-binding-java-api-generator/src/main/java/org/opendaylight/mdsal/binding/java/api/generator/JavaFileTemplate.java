fd96874f538be886a885e88329eaa55dcdd58ff8
Robert Varga <robert.varga@pantheon.tech>
2021-10-20 11:01:34 +0200

    Bump odlparent/yangtools
    Adopt odlparent-8.1.4, yangtools-6.0.8.
    
    Change-Id: I6dcc28573e01b1f0df69fdf8980e328d04c5e26f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/JavaFileTemplate.java
+++ binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/JavaFileTemplate.java
@@ -394,12 +394,11 @@ class JavaFileTemplate {
      *
      * @param method method signature from which is the method name and return type obtained
      * @return generated property instance for the getter <code>method</code>
-     * @throws IllegalArgumentException <ul>
-     *  <li>if the <code>method</code> equals <code>null</code></li>
-     *  <li>if the name of the <code>method</code> equals <code>null</code></li>
-     *  <li>if the name of the <code>method</code> is empty</li>
-     *  <li>if the return type of the <code>method</code> equals <code>null</code></li>
-     * </ul>
+     * @throws IllegalArgumentException if:
+     *         - the <code>method</code> equals <code>null</code>, or
+     *         - the name of the <code>method</code> equals <code>null</code>, or
+     *         - the name of the <code>method</code> is empty, or
+     *         - the return type of the <code>method</code> equals <code>null</code>
      */
     private static BuilderGeneratedProperty propertyFromGetter(final MethodSignature method) {
         checkArgument(method != null);
