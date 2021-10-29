f01354b131d373a31d8c05e44bd09a632c2436d6
Robert Varga <robert.varga@pantheon.tech>
2021-10-29 18:06:01 +0200

    WIP: Rework BindingRuntimeTypes
    BindingRuntimeTypes serves to bring together GeneratedTypes and
    SchemaNodes. We do this in a a number of different ways, but a lot of
    the times this ends up looking a Type and then finding the SchemaNode
    which generated it -- and then perhaps inquire children by SchemaNode
    (which again is looked up in some way).
    
    Once we have done these cross-lookups, we resolve the Type to a Class
    through BindingRuntimeContext and instantiate a
    DataContainerCodecPrototype, which holds these (and other) bits
    together.
    
    Current code relies on DerivableSchemaNode and SchemaNode-based lookups
    to resolve these due to historical reasons. Our Generator infrastructure
    already has AbstractExplicitGenerator.getOriginal() to perform the
    equivalent operation.
    
    This patch defines a RuntimeType base interface which holds together an
    EffectiveStatement and a Generated type. We also define a
    RuntimeTypeContainer to expose lookup methods for inquiring child
    RuntimeTypes as appropriate.
    
    BindingRuntimeTypes serves as the root of a tree hierarchy, where
    RuntimeType is a leaf node and CompositeRuntimeType is an interior
    node.
    
    NodeCodecContext and its subclasses are modified to maintain a reference
    to their backing RuntimeType and issue appropriate localized lookups.
    
    // FIXME: augment substitution does not work
    // FIXME: codec tests: 0 failed / 2 error / 109 total
    
    JIRA: MDSAL-696
    Change-Id: I45850d320e01fce0b227512b9f5f7b19331df60e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/ListGenerator.java
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/ListGenerator.java
@@ -9,7 +9,10 @@ package org.opendaylight.mdsal.binding.generator.impl.reactor;
 
 import static org.opendaylight.mdsal.binding.model.ri.BindingTypes.identifiable;
 
+import java.util.List;
+import java.util.Map;
 import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.binding.generator.impl.rt.DefaultListRuntimeType;
 import org.opendaylight.mdsal.binding.model.api.GeneratedType;
 import org.opendaylight.mdsal.binding.model.api.MethodSignature.ValueMechanics;
 import org.opendaylight.mdsal.binding.model.api.Type;
@@ -17,8 +20,12 @@ import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilde
 import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilderBase;
 import org.opendaylight.mdsal.binding.model.api.type.builder.MethodSignatureBuilder;
 import org.opendaylight.mdsal.binding.model.ri.Types;
+import org.opendaylight.mdsal.binding.runtime.api.AugmentRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.ListRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.RuntimeType;
 import org.opendaylight.mdsal.binding.spec.naming.BindingMapping;
 import org.opendaylight.yangtools.yang.common.Ordering;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.KeyEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ListEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.OrderedByEffectiveStatement;
@@ -27,10 +34,10 @@ import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
 /**
  * Generator corresponding to a {@code list} statement.
  */
-final class ListGenerator extends CompositeSchemaTreeGenerator<ListEffectiveStatement, ListGenerator> {
+final class ListGenerator extends CompositeSchemaTreeGenerator<ListEffectiveStatement, ListRuntimeType, ListGenerator> {
     private final @Nullable KeyGenerator keyGen;
 
-    ListGenerator(final ListEffectiveStatement statement, final AbstractCompositeGenerator<?> parent) {
+    ListGenerator(final ListEffectiveStatement statement, final AbstractCompositeGenerator<?, ?> parent) {
         super(statement, parent);
         keyGen = statement.findFirstEffectiveSubstatement(KeyEffectiveStatement.class)
             .map(key -> new KeyGenerator(key, parent, this))
@@ -74,6 +81,12 @@ final class ListGenerator extends CompositeSchemaTreeGenerator<ListEffectiveStat
         //    builder.setSchemaPath(node.getPath());
 
         return builder.build();
+    }
+
+    @Override
+    ListRuntimeType createRuntimeType(final GeneratedType type, final ListEffectiveStatement statement,
+            final Map<RuntimeType, EffectiveStatement<?, ?>> children, final List<AugmentRuntimeType> augments) {
+        return new DefaultListRuntimeType(type, statement, children, augments);
     }
 
     @Override
