e48a28cbecbf100f2b97f8a7cb20a8c4165a4f53
Robert Varga <robert.varga@pantheon.tech>
2021-10-29 18:06:01 +0200

    WIP: Rework BindingRuntimeTypes
    BindingRuntimeTypes serves to bring together GeneratedTypes and
    SchemaNodes. We do this in a a number of different ways, but a lot of
    the times this ends up looking a Type and then finding the SchemaNode
    which generated it -- and then perhaps inquire children by SchemaNode
    (which again is looked up in some way).
    
    Once we have done these cross-lookups, we resolve the Type to a Class
    through BindingRuntimeContext and instantiate a
    DataContainerCodecPrototype, which holds these (and other) bits
    together.
    
    Current code relies on DerivableSchemaNode and SchemaNode-based lookups
    to resolve these due to historical reasons. Our Generator infrastructure
    already has AbstractExplicitGenerator.getOriginal() to perform the
    equivalent operation.
    
    This patch defines a RuntimeType base interface which holds together an
    EffectiveStatement and a Generated type. We also define a
    RuntimeTypeContainer to expose lookup methods for inquiring child
    RuntimeTypes as appropriate.
    
    BindingRuntimeTypes serves as the root of a tree hierarchy, where
    RuntimeType is a leaf node and CompositeRuntimeType is an interior
    node.
    
    NodeCodecContext and its subclasses are modified to maintain a reference
    to their backing RuntimeType and issue appropriate localized lookups.
    
    // FIXME: reduce the number of RuntimeTypes (?)
    // FIXME: examine binding-dom-codec-api
              - getActionDefinition() should be in RuntimeType, which in
                turn should be looked up via path + class
    // FIXME: finish indexing work
    
    JIRA: MDSAL-696
    Change-Id: I45850d320e01fce0b227512b9f5f7b19331df60e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/rt/AbstractCompositeRuntimeType.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.rt;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.JavaTypeName;
+import org.opendaylight.mdsal.binding.runtime.api.AugmentRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.CompositeRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.RuntimeType;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
+
+abstract class AbstractCompositeRuntimeType<S extends EffectiveStatement<?, ?>>
+        extends AbstractRuntimeType<S, GeneratedType> implements CompositeRuntimeType {
+    private final ImmutableMap<JavaTypeName, RuntimeType> byClass;
+    private final ImmutableMap<QName, RuntimeType> bySchemaTree;
+
+    AbstractCompositeRuntimeType(final GeneratedType bindingType, final S schema,
+            final Map<RuntimeType, EffectiveStatement<?, ?>> children) {
+        super(bindingType, schema);
+        byClass = Maps.uniqueIndex(children.keySet(), RuntimeType::getIdentifier);
+
+        // Note: this may be over-sized, but we typically deal with schema tree statements, hence it is kind of accurate
+        final var builder = ImmutableMap.<QName, RuntimeType>builderWithExpectedSize(children.size());
+        for (var entry : children.entrySet()) {
+            final var stmt = entry.getValue();
+            if (stmt instanceof SchemaTreeEffectiveStatement) {
+                builder.put(((SchemaTreeEffectiveStatement<?>)stmt).argument(), entry.getKey());
+            }
+        }
+        bySchemaTree = builder.build();
+    }
+
+    @Override
+    public final ImmutableMap<AugmentationIdentifier, AugmentRuntimeType> augments() {
+        // FIXME: implement this
+        return ImmutableMap.of();
+    }
+
+    @Override
+    public final RuntimeType schemaTreeChild(final QName qname) {
+        return bySchemaTree.get(requireNonNull(qname));
+    }
+
+    @Override
+    public final RuntimeType bindingChild(final JavaTypeName typeName) {
+        return byClass.get(requireNonNull(typeName));
+
+// FIXME: this part should live in CompositeGenerator
+//        @Override
+//        public final DataSchemaNode findChildSchemaDefinition(final DataNodeContainer parentSchema,
+//                final QNameModule parentNamespace, final Class<?> childClass) {
+//            final DataSchemaNode origDef = getSchemaDefinition(childClass);
+//            if (origDef == null) {
+//                // Weird, the child does not have an associated definition
+//                return null;
+//            }
+//
+//            // Direct instantiation or use in same module in which grouping was defined.
+//            final QName origName = origDef.getQName();
+//            final DataSchemaNode sameName = parentSchema.dataChildByName(origName);
+//            if (sameName != null) {
+//                // Check if it is:
+//                // - exactly same schema node, or
+//                // - instantiated node was added via uses statement and is instantiation of same grouping
+//                if (origDef.equals(sameName) || origDef.equals(getRootOriginalIfPossible(sameName))) {
+//                    return sameName;
+//                }
+//
+//                // Node has same name, but clearly is different
+//                return null;
+//            }
+//
+//            // We are looking for instantiation via uses in other module
+//            final DataSchemaNode potential = parentSchema.dataChildByName(origName.bindTo(parentNamespace));
+//            // We check if it is really instantiated from same definition as class was derived
+//            if (potential != null && origDef.equals(getRootOriginalIfPossible(potential))) {
+//                return potential;
+//            }
+//            return null;
+//        }
+    }
+
+//    private static @Nullable SchemaNode getRootOriginalIfPossible(final SchemaNode data) {
+//        SchemaNode previous = null;
+//        SchemaNode next = originalNodeOf(data);
+//        while (next != null) {
+//            previous = next;
+//            next = originalNodeOf(next);
+//        }
+//        return previous;
+//    }
+
+    @Override
+    public final Entry<AugmentationIdentifier, AugmentRuntimeType> resolveAugmentation(final AugmentRuntimeType type) {
+
+//        @Override
+//        public final Entry<AugmentationIdentifier, AugmentRuntimeType> getResolvedAugmentationSchema(
+//                final DataNodeContainer target, final Class<? extends Augmentation<?>> aug) {
+//            final AugmentRuntimeType origSchema = getAugmentationDefinition(aug);
+//            checkArgument(origSchema != null, "Augmentation %s is not known in current schema context", aug);
+//            /*
+//             * FIXME: Validate augmentation schema lookup
+//             *
+//             * Currently this algorithm, does not verify if instantiated child nodes
+//             * are real one derived from augmentation schema. The problem with
+//             * full validation is, if user used copy builders, he may use
+//             * augmentation which was generated for different place.
+//             *
+//             * If this augmentations have same definition, we emit same identifier
+//             * with data and it is up to underlying user to validate data.
+//             *
+//             */
+//            final Set<QName> childNames = new HashSet<>();
+//            final Set<DataSchemaNode> realChilds = new HashSet<>();
+//            for (final DataSchemaNode child : origSchema.getChildNodes()) {
+//                final DataSchemaNode dataChildQNname = target.dataChildByName(child.getQName());
+//                final String childLocalName = child.getQName().getLocalName();
+//                if (dataChildQNname == null) {
+//                    for (DataSchemaNode dataSchemaNode : target.getChildNodes()) {
+//                        if (childLocalName.equals(dataSchemaNode.getQName().getLocalName())) {
+//                            realChilds.add(dataSchemaNode);
+//                            childNames.add(dataSchemaNode.getQName());
+//                        }
+//                    }
+//                } else {
+//                    realChilds.add(dataChildQNname);
+//                    childNames.add(child.getQName());
+//                }
+//            }
+//
+//            final AugmentationIdentifier identifier = AugmentationIdentifier.create(childNames);
+//            final AugmentationSchemaNode proxy = new EffectiveAugmentationSchema(origSchema, realChilds);
+//            return new SimpleEntry<>(identifier, proxy);
+//        }
+        return null;
+    }
+}
