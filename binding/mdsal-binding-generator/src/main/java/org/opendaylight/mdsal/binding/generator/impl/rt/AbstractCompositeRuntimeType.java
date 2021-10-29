5ac44228013375ba0546c6769e8771e31fb012f7
Robert Varga <robert.varga@pantheon.tech>
2021-10-29 18:06:01 +0200

    WIP: Rework BindingRuntimeTypes
    BindingRuntimeTypes serves to bring together GeneratedTypes and
    SchemaNodes. We do this in a a number of different ways, but a lot of
    the times this ends up looking a Type and then finding the SchemaNode
    which generated it -- and then perhaps inquire children by SchemaNode
    (which again is looked up in some way).
    
    Once we have done these cross-lookups, we resolve the Type to a Class
    through BindingRuntimeContext and instantiate a
    DataContainerCodecPrototype, which holds these (and other) bits
    together.
    
    Current code relies on DerivableSchemaNode and SchemaNode-based lookups
    to resolve these due to historical reasons. Our Generator infrastructure
    already has AbstractExplicitGenerator.getOriginal() to perform the
    equivalent operation.
    
    This patch defines a RuntimeType base interface which holds together an
    EffectiveStatement and a Generated type. We also define a
    RuntimeTypeContainer to expose lookup methods for inquiring child
    RuntimeTypes as appropriate.
    
    BindingRuntimeTypes serves as the root of a tree hierarchy, where
    RuntimeType is a leaf node and CompositeRuntimeType is an interior
    node.
    
    NodeCodecContext and its subclasses are modified to maintain a reference
    to their backing RuntimeType and issue appropriate localized lookups.
    
    // FIXME: computeChildAddressabilitySummary() performs wrong checks
    // FIXME: augment substitution does not work
    // FIXME: codec tests: 0 failed / 4 error / 109 total
    // FIXME: adapter tests: 0 failed / 9 error / 139 total
    
    JIRA: MDSAL-696
    Change-Id: I45850d320e01fce0b227512b9f5f7b19331df60e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/rt/AbstractCompositeRuntimeType.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.rt;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import java.util.List;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.model.api.JavaTypeName;
+import org.opendaylight.mdsal.binding.runtime.api.AugmentRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.CompositeRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.GeneratedRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.RuntimeType;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
+
+abstract class AbstractCompositeRuntimeType<S extends EffectiveStatement<?, ?>>
+        extends AbstractRuntimeType<S, GeneratedType> implements CompositeRuntimeType {
+    private final ImmutableMap<JavaTypeName, GeneratedRuntimeType> byClass;
+    private final ImmutableMap<QName, RuntimeType> bySchemaTree;
+    private final @NonNull ImmutableList<AugmentRuntimeType> augments;
+
+    AbstractCompositeRuntimeType(final GeneratedType bindingType, final S statement,
+            final Map<RuntimeType, EffectiveStatement<?, ?>> children, final List<AugmentRuntimeType> augments) {
+        super(bindingType, statement);
+        this.augments = ImmutableList.copyOf(augments);
+
+        byClass = children.keySet().stream()
+            .filter(GeneratedRuntimeType.class::isInstance)
+            .map(GeneratedRuntimeType.class::cast)
+            .collect(ImmutableMap.toImmutableMap(GeneratedRuntimeType::getIdentifier, Functions.identity()));
+
+        // Note: this may be over-sized, but we typically deal with schema tree statements, hence it is kind of accurate
+        final var builder = ImmutableMap.<QName, RuntimeType>builderWithExpectedSize(children.size());
+        for (var entry : children.entrySet()) {
+            final var stmt = entry.getValue();
+            if (stmt instanceof SchemaTreeEffectiveStatement) {
+                builder.put(((SchemaTreeEffectiveStatement<?>)stmt).argument(), entry.getKey());
+            }
+        }
+        bySchemaTree = builder.build();
+    }
+
+    @Override
+    public final List<AugmentRuntimeType> augments() {
+        return augments;
+    }
+
+    @Override
+    public final List<AugmentRuntimeType> mismatchedAugments() {
+        // FIXME: implement this
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final RuntimeType schemaTreeChild(final QName qname) {
+        return bySchemaTree.get(requireNonNull(qname));
+    }
+
+    @Override
+    public final GeneratedRuntimeType bindingChild(final JavaTypeName typeName) {
+        return byClass.get(requireNonNull(typeName));
+    }
+}
