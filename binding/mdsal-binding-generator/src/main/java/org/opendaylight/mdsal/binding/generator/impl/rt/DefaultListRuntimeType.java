74324f158cf590e01223e304782c22144160f9d8
Robert Varga <robert.varga@pantheon.tech>
2021-10-29 18:06:01 +0200

    WIP: Rework BindingRuntimeTypes
    BindingRuntimeTypes serves to bring together GeneratedTypes and
    SchemaNodes. We do this in a a number of different ways, but a lot of
    the times this ends up looking a Type and then finding the SchemaNode
    which generated it -- and then perhaps inquire children by SchemaNode
    (which again is looked up in some way).
    
    Once we have done these cross-lookups, we resolve the Type to a Class
    through BindingRuntimeContext and instantiate a
    DataContainerCodecPrototype, which holds these (and other) bits
    together.
    
    Current code relies on DerivableSchemaNode and SchemaNode-based lookups
    to resolve these due to historical reasons. Our Generator infrastructure
    already has AbstractExplicitGenerator.getOriginal() to perform the
    equivalent operation.
    
    This patch defines a RuntimeType base interface which holds together an
    EffectiveStatement and a Generated type. We also define a
    RuntimeTypeContainer to expose lookup methods for inquiring child
    RuntimeTypes as appropriate.
    
    BindingRuntimeTypes serves as the root of a tree hierarchy, where
    RuntimeType is a leaf node and CompositeRuntimeType is an interior
    node.
    
    NodeCodecContext and its subclasses are modified to maintain a reference
    to their backing RuntimeType and issue appropriate localized lookups.
    
    // FIXME: reduce the number of RuntimeTypes
    // FIXME: examine binding-dom-codec-api
    // FIXME: finish indexing work
    
    JIRA: MDSAL-696
    Change-Id: I45850d320e01fce0b227512b9f5f7b19331df60e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/rt/DefaultListRuntimeType.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.rt;
+
+import com.google.common.annotations.Beta;
+import java.util.Map;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.runtime.api.KeyRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.ListRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.RuntimeType;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ListEffectiveStatement;
+
+@Beta
+public final class DefaultListRuntimeType extends AbstractCompositeRuntimeType<ListEffectiveStatement>
+        implements ListRuntimeType {
+    public DefaultListRuntimeType(final GeneratedType bindingType, final ListEffectiveStatement schema,
+            final Map<RuntimeType, EffectiveStatement<?, ?>> children) {
+        super(bindingType, schema, children);
+    }
+
+    @Override
+    public KeyRuntimeType keyType() {
+        throw new UnsupportedOperationException();
+    }
+}
