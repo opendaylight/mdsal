997154dd5fe97c5a109f50028e8cc3ce0697bdf4
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2021-11-04 15:10:26 +0100

    Add nonnullFoo() getters for non-semantic objects
    Add non-null getters for containers without presence
    statement. They return lazy-initialized containers'
    empty instances in case the container is null.
    
    Non-null getters are implemented in Builder's
    inner implementation of the container's parent.
    
    Container's empty instance is located in
    container's builder class and is hidden behind
    a static method. This way we ensure empty instance
    is initialized only once.
    
    In the binding codec part a nonnull method is generated
    with return statement as follows:
    return (FooType) codecMemberOrEmpty(getFoo(), FooType.class)
    
    JIRA: MDSAL-673
    Change-Id: I3a5a346b32b89e7c28cad31536593824dee3c232
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/ContainerNodeCodecContext.java
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/ContainerNodeCodecContext.java
@@ -7,14 +7,31 @@
  */
 package org.opendaylight.mdsal.binding.dom.codec.impl;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.runtime.api.ContainerLikeRuntimeType;
 import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 
 final class ContainerNodeCodecContext<D extends DataObject>
         extends DataObjectCodecContext<D, ContainerLikeRuntimeType<?, ?>>
         implements RpcInputCodec<D> {
+    private static final VarHandle EMPTY_OBJECT;
+
+    static {
+        try {
+            EMPTY_OBJECT = MethodHandles.lookup().findVarHandle(ContainerNodeCodecContext.class, "emptyObject",
+                DataObject.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    private volatile D emptyObject;
 
     ContainerNodeCodecContext(final DataContainerCodecPrototype<ContainerLikeRuntimeType<?, ?>> prototype) {
         super(prototype);
@@ -31,5 +48,21 @@ final class ContainerNodeCodecContext<D extends DataObject>
     @Override
     protected Object deserializeObject(final NormalizedNode normalizedNode) {
         return deserialize(normalizedNode);
+    }
+
+    @Override
+    D emptyObject() {
+        final D local;
+        return (local = emptyObject) != null ? local : loadEmptyObject();
+    }
+
+    private @NonNull D loadEmptyObject() {
+        final var domArg = getDomPathArgument();
+        if (!(domArg instanceof NodeIdentifier nodeId)) {
+            throw new IllegalStateException("Unexpected identifier " + domArg);
+        }
+        final var local = createBindingProxy(Builders.containerBuilder().withNodeIdentifier(nodeId).build());
+        final var witness = (D) EMPTY_OBJECT.compareAndExchange(this, null, local);
+        return witness != null ? witness : local;
     }
 }
