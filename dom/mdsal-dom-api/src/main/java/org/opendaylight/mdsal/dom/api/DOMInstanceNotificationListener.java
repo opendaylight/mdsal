d4f6416efd17c95b496d450529b062bbb7584d71
Robert Varga <robert.varga@pantheon.tech>
2021-11-15 09:03:27 +0100

    WIP: Add DOMInstanceNotificationService
    DOMNotificationService provides a means to subscribe to top-level
    notifications, as defined by YANG 1.0. RFC7950 (YANG 1.1) extended
    this construct with the notion that notifications can be bound to
    a datastore instance.
    
    Define a set of DOMInstanceNotification* interfaces which cover provide
    a conterpart to DOMNotification* interfaces and cover the extensions
    introduced in YANG 1.1.
    
    // FIXME: needs DOMNotificationRouter update
    
    JIRA: MDSAL-494
    Change-Id: I610f761bfcb9fb292709dd12618ec5bcb9b843c2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMInstanceNotificationListener.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.api;
+
+import com.google.common.annotations.Beta;
+import java.util.EventListener;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * An {@link EventListener} registered to receive instance (YANG 1.1) {@link DOMNotification}s from a
+ * {@link DOMInstanceNotificationService}.
+ */
+@Beta
+@FunctionalInterface
+@NonNullByDefault
+public interface DOMInstanceNotificationListener extends EventListener {
+    /**
+     * Invoked whenever a {@link DOMNotification} matching the subscription criteria is received.
+     *
+     * @param path Notification's parent path
+     * @param notification Received notification
+     */
+    void onNotification(DOMDataTreeIdentifier path, DOMNotification notification);
+}
---
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMInstanceNotificationService.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.dom.api;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.concurrent.Executor;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.common.QName;
+
+/**
+ * A {@link DOMService} providing access to subscription to YANG 1.1 instance notifications.
+ */
+@Beta
+@NonNullByDefault
+public interface DOMInstanceNotificationService extends DOMService {
+    /**
+     * Register a {@link DOMInstanceNotificationListener} for a particular {@code path} and notification {@code type}.
+     *
+     * @param path Instance notification's parent path, must not be empty, but may be a wildcard identifier.
+     * @param type Notification type, i.e. its schema node QName
+     * @param listener Listener to deliver notifications to
+     * @param executor Executor to use for invoking
+     * @return Listener's registration object
+     * @throws NullPointerException if any of the arguments is null
+     * @throws IllegalArgumentException if the {@code path} is empty or if the combination of {@code path} and
+     *                                  {@code type} does not identify an instance notification
+     */
+    Registration registerNotificationListener(DOMDataTreeIdentifier path, QName type,
+        DOMInstanceNotificationListener listener, Executor executor);
+
+    /**
+     * Register a {@link DOMInstanceNotificationListener} for a particular {@code path} and notification {@code type}.
+     * This method is a convenience equivalent to
+     * {@code registerNotificationListener(path, type, listener, MoreExecutors.directExecutor())}.
+     *
+     * @param path Instance notification's parent path, must not be empty, but may be a wildcard identifier.
+     * @param type Notification type, i.e. its schema node QName
+     * @param listener Listener to deliver notifications to
+     * @return Listener's registration object
+     * @throws NullPointerException if any of the arguments is null
+     * @throws IllegalArgumentException if the {@code path} is empty or if the combination of {@code path} and
+     *                                  {@code type} does not identify an instance notification
+     */
+    default Registration registerNotificationListener(final DOMDataTreeIdentifier path, final QName type,
+            final DOMInstanceNotificationListener listener) {
+        return registerNotificationListener(path, type, listener, MoreExecutors.directExecutor());
+    }
+}
