3b4964895599042088262301ea36173dc4b114b1
Robert Varga <robert.varga@pantheon.tech>
2021-12-21 14:22:03 +0100

    WIP: Refactor augment generator linkage
    The uses/augment and module/augment cases are quite different from
    the perspective how we map their argument reference to generators.
    
    The uses/augment case is quite simple, as it refers to grouping's nodes,
    not augmentations. The only variation we need to account for is the
    skip back to the defining grouping.
    
    The module/augment case, on the other hand, is more complex, as it can
    refer to augmentations -- hence it need to account for changes namespace
    changes along uses/grouping as well as back from grouping child to
    augmentations.
    
    // FIXME: add more tests
    
    JIRA: MDSAL-715
    Change-Id: I7425301afa6de566d985d19450ccfc64ea527119
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/UsesAugmentGenerator.java
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/UsesAugmentGenerator.java
@@ -7,51 +7,43 @@
  */
 package org.opendaylight.mdsal.binding.generator.impl.reactor;
 
-import static com.google.common.base.Verify.verify;
-import static com.google.common.base.Verify.verifyNotNull;
-import static java.util.Objects.requireNonNull;
-
 import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
-import org.opendaylight.yangtools.yang.model.api.stmt.UsesEffectiveStatement;
 
 /**
  * Generator corresponding to a {@code augment} statement used as a child of a {@code uses} statement.
  */
 final class UsesAugmentGenerator extends AbstractAugmentGenerator {
-    private final UsesEffectiveStatement uses;
-
-    private GroupingGenerator grouping;
-
-    UsesAugmentGenerator(final AugmentEffectiveStatement statement, final AbstractCompositeGenerator<?> parent,
-            final UsesEffectiveStatement uses) {
+    UsesAugmentGenerator(final AugmentEffectiveStatement statement, final AbstractCompositeGenerator<?> parent) {
         super(statement, parent);
-        this.uses = requireNonNull(uses);
-    }
-
-    void linkGroupingDependency(final UsesEffectiveStatement checkUses, final GroupingGenerator resolvedGrouping) {
-        if (uses.equals(checkUses)) {
-            verify(grouping == null, "Attempted to relink %s from %s to %s", this, grouping, resolvedGrouping);
-            this.grouping = requireNonNull(resolvedGrouping);
-        }
     }
 
     @Override
     void loadTargetGenerator() {
-        final GroupingGenerator grp = verifyNotNull(grouping, "No grouping linked in %s", this);
-        final SchemaNodeIdentifier path = statement().argument();
-
-        /*
-         *  Here we are going in the opposite direction of RFC7950, section 3.13:
-         *
-         *        The effect of a "uses" reference to a grouping is that the nodes
-         *        defined by the grouping are copied into the current schema tree and
-         *        are then updated according to the "refine" and "augment" statements.
-         *
-         *  Our argument is composed of QNames in the current schema tree's namespace, but the grouping may have been
-         *  defined in a different module -- and therefore it knows those children under its namespace. Adjust the path
-         *  we are searching if that happens to be the case.
-         */
-        setTargetGenerator(grp.resolveSchemaNode(path, grp.statement().argument().getModule()));
+//        final GroupingGenerator grp = verifyNotNull(grouping, "No grouping linked in %s", this);
+//
+//        /*
+//         *  Here we are going in the opposite direction of RFC7950, section 7.13:
+//         *
+//         *        The effect of a "uses" reference to a grouping is that the nodes
+//         *        defined by the grouping are copied into the current schema tree and
+//         *        are then updated according to the "refine" and "augment" statements.
+//         *
+//         *  Our argument is composed of QNames in the current schema tree's namespace, but the grouping may have been
+//         *  defined in a different module -- and therefore it knows those children under its namespace. Adjust the path
+//         *  we are searching if that happens to be the case.
+//         */
+//        QNameModule namespace = grp.statement().argument().getModule();
+//        AbstractExplicitGenerator<?> current = grp;
+//
+//        for (QName next : statement().argument().getNodeIdentifiers()) {
+//            final QName qname = next.bindTo(namespace);
+//            current = verifyNotNull(current.findSchemaTreeGenerator(qname),
+//                "Failed to find %s as %s in %s", next, qname, current);
+//
+//            // Returned generator's namespace might be different from we used as the lookup. This happens when the
+//            // generator is inherited from a grouping -- see AbstractCompositeGenerator.findInferredGenerator().
+//            namespace = current.getQName().getModule();
+//        }
+        setTargetGenerator(getParent().getSchemaTreeGenerator(statement().argument()));
     }
 }
