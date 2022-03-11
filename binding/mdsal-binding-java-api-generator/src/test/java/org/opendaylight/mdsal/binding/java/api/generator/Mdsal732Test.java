54a5b74b46f270581fe7e9095119fa4f10f79c85
Robert Varga <robert.varga@pantheon.tech>
2022-03-11 20:09:22 +0100

    WIP: Fix identityref wildcards
    Identityref use Class<? extends Target> for their return types, hence
    we mask unmask the ParameterizedType to Target's JavaTypeName.
    
    // FIXME: add proper CodeHelper methods to validate that values are
              Class.isAssignableFrom()
    // FIXME: generate callouts
    // FIXME: finish test suite
    
    JIRA: MDSAL-732
    Change-Id: I96fd0ffcffcd65aa7c658ab9d881a950ba41c6cb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/mdsal/binding/java/api/generator/Mdsal732Test.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.java.api.generator;
+
+import static org.opendaylight.mdsal.binding.java.api.generator.FileSearchUtil.getFiles;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+
+public class Mdsal732Test extends BaseCompilationTest {
+    private File sourcesOutputDir;
+    private File compiledOutputDir;
+    private List<GeneratedType> types;
+    private Map<String, File> files;
+
+    @Before
+    public void before() throws IOException, URISyntaxException {
+        sourcesOutputDir = CompilationTestUtils.generatorOutput("mdsal732");
+        compiledOutputDir = CompilationTestUtils.compilerOutput("mdsal732");
+        types = generateTestSources("/compilation/mdsal732", sourcesOutputDir);
+        CompilationTestUtils.testCompilation(sourcesOutputDir, compiledOutputDir);
+        files = getFiles(sourcesOutputDir);
+    }
+
+    @After
+    public void after() {
+        CompilationTestUtils.cleanUp(sourcesOutputDir, compiledOutputDir);
+    }
+
+    @Test
+    public void testIdentityrefLeafrefSpecialization() {
+        // assert code shape for setters and copy buuilder
+    }
+}
