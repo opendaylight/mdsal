917661be6df5e44ebb04bc76408b80bc00c555e3
Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>
2022-03-30 19:53:53 +0200

    Inject RPC input QName into LazySerializedContainerNode
    When ExecuteRpc message is sent with LazySerializedContainerNode
    as 'input' parameter, during serialization is invoked its
    getIdentifier() method that returns NodeIdentfier of rpc,
    not input. So OpsInvoker receives the message with incorrect 'input'
    parameter and  we get error downstream because of it.
    
    We need to inject rpc input Qname into LazySerializedContainerNode
    to get NodeIdentifier of rpc input from getIdentifier() method.
    
    JIRA: MDSAL-739
    Change-Id: I77c870fe91a758037bd27e54607e778255d611fc
    Signed-off-by: Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/Mdsal739.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.binding.dom.adapter.test.util.BindingBrokerTestFactory;
+import org.opendaylight.mdsal.binding.dom.adapter.test.util.BindingTestContext;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.bi.ba.rpcservice.rev140701.OpendaylightTestRpcServiceService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.bi.ba.rpcservice.rev140701.RockTheHouseInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.bi.ba.rpcservice.rev140701.RockTheHouseInputBuilder;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+@ExtendWith(MockitoExtension.class)
+public class Mdsal739 {
+    private ListeningExecutorService executorService;
+    private AdapterContext adapterContext;
+
+    @BeforeEach
+    public void before() {
+        executorService = MoreExecutors.newDirectExecutorService();
+
+        final BindingBrokerTestFactory bindingBrokerTestFactory = new BindingBrokerTestFactory();
+        bindingBrokerTestFactory.setExecutor(executorService);
+        final BindingTestContext bindingTestContext = bindingBrokerTestFactory.getTestContext();
+        bindingTestContext.start();
+
+        adapterContext = bindingTestContext.getCodec();
+    }
+
+    @AfterEach
+    public void after() {
+        executorService.shutdownNow();
+    }
+
+    @Test
+    public void testRpcInputName() {
+        final var rpcService = mock(DOMRpcService.class);
+
+        final var captor = ArgumentCaptor.forClass(NormalizedNode.class);
+        doReturn(Futures.immediateFailedFuture(new Throwable())).when(rpcService).invokeRpc(any(), captor.capture());
+        final var adapter = (OpendaylightTestRpcServiceService) new RpcServiceAdapter(
+            OpendaylightTestRpcServiceService.class, adapterContext, rpcService).getProxy();
+
+        final var result = adapter.rockTheHouse(new RockTheHouseInputBuilder().build());
+        assertThrows(ExecutionException.class, () -> Futures.getDone(result));
+        final var input = captor.getValue();
+        assertThat(input, instanceOf(ContainerNode.class));
+        assertEquals(new NodeIdentifier(RockTheHouseInput.QNAME), input.getIdentifier());
+    }
+}
