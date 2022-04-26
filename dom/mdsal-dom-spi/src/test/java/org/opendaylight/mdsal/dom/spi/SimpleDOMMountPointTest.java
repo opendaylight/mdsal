28bc6f36e96b19962588a05cbb82b585584452a0
Robert Varga <robert.varga@pantheon.tech>
2022-04-26 15:18:02 +0200

    Modernize SimpleDOMMountPointTest
    Do not use YangInstanceIdentifier mocking and clean up the structure
    with better asserts.
    
    Change-Id: Id8da5ad4c549990822628c50068b0ba5565408cc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-spi/src/test/java/org/opendaylight/mdsal/dom/spi/SimpleDOMMountPointTest.java
+++ dom/mdsal-dom-spi/src/test/java/org/opendaylight/mdsal/dom/spi/SimpleDOMMountPointTest.java
@@ -7,34 +7,28 @@
  */
 package org.opendaylight.mdsal.dom.spi;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
-import com.google.common.collect.ClassToInstanceMap;
 import com.google.common.collect.ImmutableClassToInstanceMap;
 import org.junit.Test;
 import org.opendaylight.mdsal.dom.api.DOMService;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
 public class SimpleDOMMountPointTest {
-
     @Test
     public void basicTest() throws Exception {
-        final YangInstanceIdentifier yangInstanceIdentifier = mock(YangInstanceIdentifier.class);
-        final DOMService domService = mock(DOMService.class);
-        final ClassToInstanceMap<DOMService> classToInstanceMap =
-                ImmutableClassToInstanceMap.<DOMService>builder().put(DOMService.class, domService).build();
+        final var domService = mock(DOMService.class);
+        final var classToInstanceMap = ImmutableClassToInstanceMap.of(DOMService.class, domService);
 
         final SimpleDOMMountPoint simpleDOMMountPoint =
-                SimpleDOMMountPoint.create(yangInstanceIdentifier, classToInstanceMap);
-
+                SimpleDOMMountPoint.create(YangInstanceIdentifier.empty(), classToInstanceMap);
         assertNotNull(simpleDOMMountPoint);
 
-        assertEquals(yangInstanceIdentifier, simpleDOMMountPoint.getIdentifier());
-
+        assertSame(YangInstanceIdentifier.empty(), simpleDOMMountPoint.getIdentifier());
         assertTrue(simpleDOMMountPoint.getService(DOMService.class).isPresent());
-        assertEquals(domService, simpleDOMMountPoint.getService(DOMService.class).get());
+        assertSame(domService, simpleDOMMountPoint.getService(DOMService.class).orElseThrow());
     }
 }
