e1edf284df830edf87757fcb83ed5c0f2f582427
Robert Varga <robert.varga@pantheon.tech>
2022-08-11 01:51:46 +0200

    WIP: Add QNameAware interface
    Rather than going through reflection, expose the static QName field via
    a marker interface.
    
    // FIXME: retrofit into Identity at least
    
    Change-Id: I2f2e2db7efb69737ff85da6696f00f21f55a63d2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/QNameAware.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.binding;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.QName;
+
+/**
+ * Marker interface for generated classes which expose{@link QName} of the underlying YANG statement which produced
+ * them.
+ */
+@NonNullByDefault
+public sealed interface QNameAware {
+    /**
+     * Return the {@link QName} associated with this construct.
+     *
+     * @return A QName
+     */
+    QName qname();
+}
