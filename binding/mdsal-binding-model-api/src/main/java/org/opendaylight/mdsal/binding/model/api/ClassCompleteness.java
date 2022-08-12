07d46c115d2df9689476147f63426f4f346f3566
Robert Varga <robert.varga@pantheon.tech>
2022-08-12 23:21:15 +0200

    Add ClassCompleteness
    It is useful to express how complete a proposed class is. Expose that as
    an enum.
    
    Change-Id: I45d7f93aa8fb2dae8306e070dca1f7c875416af0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-model-api/src/main/java/org/opendaylight/mdsal/binding/model/api/ClassCompleteness.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.model.api;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * Completeness of a Java class. A class can be abstract or final or non-final (default in-between). Note this is
+ * distinct use of where {@code abstract}, {@code final}.
+ */
+@Beta
+public enum ClassCompleteness {
+    /**
+     * Abstract class. Also applies to interface, but we do not model that right now.
+     */
+    ABSTRACT,
+    /**
+     * Non-final class. May apply to other constructs, but we do not model that here. This construct has not relevance
+     * to {@code default} methods.
+     */
+    DEFAULT,
+    /**
+     * Final class.
+     */
+    FINAL;
+}
