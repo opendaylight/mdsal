323d7cbbd3d40d96867d472b1c43a2df7d5343b3
Ivan Martiniak <ivan.martiniak@pantheon.tech>
2022-08-17 18:07:39 +0200

    Create and update unit tests for BitsTypeObject storage
    New storage changes the functionality of bits-type objects from a basis.
    Existing tests were updated and new tests were added.
    
    JIRA: MDSAL-746
    Change-Id: I0e382db32a162fb1f4f44d965991b6f40079155f
    Signed-off-by: Ivan Martiniak <ivan.martiniak@pantheon.tech>

--- binding/mdsal-binding-generator/src/test/java/org/opendaylight/mdsal/binding/generator/impl/GeneratedTypesBitsTest.java
+++ binding/mdsal-binding-generator/src/test/java/org/opendaylight/mdsal/binding/generator/impl/GeneratedTypesBitsTest.java
@@ -12,13 +12,17 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import com.google.common.collect.ImmutableMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.Test;
 import org.opendaylight.mdsal.binding.model.api.GeneratedProperty;
 import org.opendaylight.mdsal.binding.model.api.GeneratedTransferObject;
 import org.opendaylight.mdsal.binding.model.api.GeneratedType;
 import org.opendaylight.mdsal.binding.model.api.MethodSignature;
 import org.opendaylight.mdsal.binding.model.api.MethodSignature.Parameter;
+import org.opendaylight.mdsal.binding.model.ri.Types;
+import org.opendaylight.yangtools.yang.common.Uint32;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 public class GeneratedTypesBitsTest {
@@ -105,4 +109,74 @@ public class GeneratedTypesBitsTest {
         assertFalse(setByteLeafMethodFound);
         assertEquals(0, setByteLeafMethodParamNum);
     }
+
+    @Test
+    public void testGeneratedTypesRestrictedBitsTest() {
+        final List<GeneratedType> genTypes = DefaultBindingGenerator.generateFor(YangParserTestUtils.parseYangResource(
+                "/simple-restricted-bits-demo.yang"));
+        assertNotNull(genTypes);
+
+        boolean myBitsFound = false;
+        boolean myBitsRestrictedFound = false;
+        Map<Uint32, String> validMyBits = ImmutableMap.of(Uint32.valueOf(0), "bitZero",
+                Uint32.valueOf(1), "bitOne", Uint32.valueOf(2), "bitTwo");
+        Map<Uint32, String> validMyBitsRestricted = ImmutableMap.of(Uint32.valueOf(0), "bitZero",
+                Uint32.valueOf(1), "bitOne");
+
+        for (final GeneratedType type : genTypes) {
+            if (type instanceof final GeneratedTransferObject genTO) {
+                if (genTO.getName().equals("MyBits")) {
+                    myBitsFound = true;
+                    assertEquals(1, genTO.getProperties().size());
+                    assertEquals(validMyBits, genTO.getConstantDefinitions().get(0).getValue());
+                    assertEquals(1, genTO.getToStringIdentifiers().size());
+                    assertEquals(1, genTO.getEqualsIdentifiers().size());
+                    assertEquals(1, genTO.getHashCodeIdentifiers().size());
+                } else if (genTO.getName().equals("MyBitsRestricted")) {
+                    myBitsRestrictedFound = true;
+                    assertEquals("MyBits", genTO.getSuperType().getName());
+                    assertEquals(validMyBitsRestricted, genTO.getConstantDefinitions().get(0).getValue());
+                    assertEquals(0, genTO.getProperties().size());
+                    assertEquals(0, genTO.getToStringIdentifiers().size());
+                    assertEquals(0, genTO.getEqualsIdentifiers().size());
+                    assertEquals(0, genTO.getHashCodeIdentifiers().size());
+                }
+            }
+        }
+        assertTrue(myBitsFound);
+        assertTrue(myBitsRestrictedFound);
+    }
+
+    @Test
+    public void testBitsTypeObjectStorage() {
+        final List<GeneratedType> genTypes = DefaultBindingGenerator.generateFor(YangParserTestUtils.parseYangResource(
+                "/test-bits-type-object-storage.yang"));
+        assertNotNull(genTypes);
+
+        boolean bitsIntFound = false;
+        boolean bitsLongFound = false;
+        boolean bitsIntArrayFound = false;
+
+        for (final GeneratedType type : genTypes) {
+            if (type instanceof final GeneratedTransferObject genTO) {
+                assertTrue(genTO.getImplements().stream().anyMatch(impl -> impl.getName().equals("BitsTypeObject")));
+                if (genTO.getName().equals("BitsInt")) {
+                    bitsIntFound = true;
+                    assertEquals(1, genTO.getProperties().size());
+                    assertEquals(Types.primitiveIntType(), genTO.getProperties().get(0).getReturnType());
+                } else if (genTO.getName().equals("BitsLong")) {
+                    assertEquals(1, genTO.getProperties().size());
+                    assertEquals(Types.primitiveLongType(), genTO.getProperties().get(0).getReturnType());
+                    bitsLongFound = true;
+                } else if (genTO.getName().equals("BitsIntArray")) {
+                    bitsIntArrayFound = true;
+                    assertEquals(Types.intArrayType(), genTO.getProperties().get(0).getReturnType());
+                }
+            }
+        }
+        assertTrue(bitsIntFound);
+        assertTrue(bitsLongFound);
+        assertTrue(bitsIntArrayFound);
+    }
+
 }
