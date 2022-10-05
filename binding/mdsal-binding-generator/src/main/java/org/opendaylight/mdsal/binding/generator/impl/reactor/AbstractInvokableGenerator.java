63e67eaae11e38f0ef853726ee74c7b1f97afcc6
Robert Varga <robert.varga@pantheon.tech>
2022-10-05 14:43:57 +0200

    WIP: Generate Rpc services
    JIRA: MDSAL-773
    Change-Id: I5fec1ac5c81b908714965e71c3471b954b58d1f9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/AbstractInvokableGenerator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilderBase;
+import org.opendaylight.mdsal.binding.runtime.api.CompositeRuntimeType;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+
+abstract class AbstractInvokableGenerator<S extends SchemaTreeEffectiveStatement<?>, R extends CompositeRuntimeType>
+        extends CompositeSchemaTreeGenerator<S, R> {
+    AbstractInvokableGenerator(final S statement, final AbstractCompositeGenerator<?, ?> parent) {
+        super(statement, parent);
+    }
+
+    @Override
+    final void pushToInference(final SchemaInferenceStack dataTree) {
+        dataTree.enterSchemaTree(statement().argument());
+    }
+
+    @Override
+    final void addAsGetterMethod(final GeneratedTypeBuilderBase<?> builder, final TypeBuilderFactory builderFactory) {
+        // RPCs/Actions are a separate concept
+    }
+}
