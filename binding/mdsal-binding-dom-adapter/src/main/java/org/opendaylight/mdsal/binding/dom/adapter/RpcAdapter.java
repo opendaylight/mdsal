0ee0f7d8d22e8a8d451d2f7019e224847f66b7db
Robert Varga <robert.varga@pantheon.tech>
2022-10-06 13:47:52 +0200

    WIP: Add support for registering single-RPC services
    // FIXME: integrate invocation strategy
    // FIXME: tests
    
    JIRA: MDSAL-777
    Change-Id: I114bd3795ba353da57e7f96d5db9680986ecb5f6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/RpcAdapter.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter;
+
+import java.lang.reflect.Method;
+import org.opendaylight.mdsal.dom.api.DOMRpcService;
+import org.opendaylight.yangtools.yang.binding.Rpc;
+import org.opendaylight.yangtools.yang.binding.RpcInput;
+
+final class RpcAdapter<T extends Rpc<?, ?>> extends AbstractRpcAdapter {
+    private final RpcInvocationStrategy strategy;
+    private final Method invokeMethod;
+
+    RpcAdapter(final AdapterContext adapterContext, final DOMRpcService delegate, final Class<T> type) {
+        super(adapterContext, delegate, type);
+
+        final var desc = adapterContext.currentSerializer().getRpcInvokeDescription(type);
+        invokeMethod = desc.getKey();
+        strategy = RpcInvocationStrategy.of(this, invokeMethod, desc.getValue());
+    }
+
+    @Override
+    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+        return invokeMethod.equals(method) ? strategy.invoke((RpcInput) args[0]) : defaultInvoke(proxy, method, args);
+    }
+}
