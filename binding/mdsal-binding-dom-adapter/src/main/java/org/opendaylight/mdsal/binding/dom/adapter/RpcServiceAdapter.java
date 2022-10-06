531e71cfc354cc481ba670d4081dd158eac41acb
Robert Varga <robert.varga@pantheon.tech>
2022-10-06 13:47:52 +0200

    Add support for registering single-RPC services
    We are currently providing an aggregated RpcService generated class,
    which is binding the invocation model to that class -- without the
    ability to provide an implementation of a single RPC. It also
    complicates the invocation path from DOM, since we need to perform
    two lookups: one in DOM, where we route the invocation towards the
    RpcServiceAdapter and then a method-based lookup to dispatch it in
    the proxy.
    
    Furthermore this prevents the possibility of adapting the invocation
    contract, as the codegen is always involved.
    
    We are now generating interfaces representing individual RPCs, just like
    we do for actions. This provides a more granular access and notably
    allows mdsal-binding-api to provide invocation contract adapters -- thus
    disconnecting it from codegen.
    
    JIRA: MDSAL-777
    Change-Id: I114bd3795ba353da57e7f96d5db9680986ecb5f6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/RpcServiceAdapter.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/RpcServiceAdapter.java
@@ -10,35 +10,23 @@ package org.opendaylight.mdsal.binding.dom.adapter;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.collect.ImmutableMap;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.Map;
 import java.util.Map.Entry;
-import org.eclipse.jdt.annotation.NonNull;
-import org.opendaylight.mdsal.binding.dom.adapter.RpcInvocationStrategy.ContentRouted;
 import org.opendaylight.mdsal.binding.runtime.api.RpcRuntimeType;
 import org.opendaylight.mdsal.binding.spec.naming.BindingMapping;
 import org.opendaylight.mdsal.binding.spec.reflect.BindingReflections;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
-import org.opendaylight.mdsal.dom.spi.ContentRoutedRpcContext;
 import org.opendaylight.yangtools.yang.binding.RpcInput;
 import org.opendaylight.yangtools.yang.binding.RpcService;
 import org.opendaylight.yangtools.yang.model.api.stmt.RpcEffectiveStatement;
 
-class RpcServiceAdapter implements InvocationHandler {
+final class RpcServiceAdapter extends AbstractRpcAdapter {
     private final ImmutableMap<Method, RpcInvocationStrategy> rpcNames;
-    private final @NonNull Class<? extends RpcService> type;
-    private final @NonNull AdapterContext adapterContext;
-    private final @NonNull DOMRpcService delegate;
-    private final @NonNull RpcService facade;
 
     RpcServiceAdapter(final Class<? extends RpcService> type, final AdapterContext adapterContext,
             final DOMRpcService domService) {
-        this.type = requireNonNull(type);
-        this.adapterContext = requireNonNull(adapterContext);
-        delegate = requireNonNull(domService);
-        facade = (RpcService) Proxy.newProxyInstance(type.getClassLoader(), new Class[] {type}, this);
+        super(adapterContext, domService, type);
 
         // FIXME: This should be probably part of BindingRuntimeContext and RpcServices perhaps should have their own
         //        RuntimeType. At any rate, we are dancing around to reconstruct information RpcServiceRuntimeType would
@@ -69,61 +57,15 @@ class RpcServiceAdapter implements InvocationHandler {
                     throw new IllegalStateException("Unexpected run-time type " + runtimeType + " for " + rpcName);
                 }
 
-                final var contentContext = ContentRoutedRpcContext.forRpc(rpc);
-                final RpcInvocationStrategy strategy;
-                if (contentContext != null) {
-                    final var extractor = serializer.findExtractor(rpcType.input());
-                    strategy = extractor == null ? new RpcInvocationStrategy(this, rpcName)
-                        : new ContentRouted(this, rpcName, contentContext.leaf(), extractor);
-                } else {
-                    strategy = new RpcInvocationStrategy(this, rpcName);
-                }
-
-                return Map.entry(method, strategy);
+                return Map.entry(method, createStrategy(serializer, rpcType));
             })
             .collect(ImmutableMap.toImmutableMap(Entry::getKey, Entry::getValue));
     }
 
-    final @NonNull CurrentAdapterSerializer currentSerializer() {
-        return adapterContext.currentSerializer();
-    }
-
-    final @NonNull DOMRpcService delegate() {
-        return delegate;
-    }
-
-    final @NonNull RpcService facade() {
-        return facade;
-    }
-
     @Override
-    public Object invoke(final Object proxy, final Method method, final Object[] args) {
+    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
         final var strategy = rpcNames.get(method);
-        if (strategy != null) {
-            return strategy.invoke((RpcInput) requireNonNull(args[0]));
-        }
-
-        switch (method.getName()) {
-            case "toString":
-                if (method.getReturnType().equals(String.class) && method.getParameterCount() == 0) {
-                    return type.getName() + "$Adapter{delegate=" + delegate.toString() + "}";
-                }
-                break;
-            case "hashCode":
-                if (method.getReturnType().equals(int.class) && method.getParameterCount() == 0) {
-                    return System.identityHashCode(proxy);
-                }
-                break;
-            case "equals":
-                if (method.getReturnType().equals(boolean.class) && method.getParameterCount() == 1
-                        && method.getParameterTypes()[0] == Object.class) {
-                    return proxy == args[0];
-                }
-                break;
-            default:
-                break;
-        }
-
-        throw new UnsupportedOperationException("Method " + method.toString() + "is unsupported.");
+        return strategy != null ? strategy.invoke((RpcInput) requireNonNull(args[0]))
+            : defaultInvoke(proxy, method, args);
     }
 }
