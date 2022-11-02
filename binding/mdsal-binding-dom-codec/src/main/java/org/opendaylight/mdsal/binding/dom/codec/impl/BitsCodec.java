4054e6b06897d5239803da792f8ae00a4004b968
Ivan Martiniak <ivan.martiniak@pantheon.tech>
2022-11-02 20:19:26 +0100

    Adjust bits codec to recognize 3 storage classes
    Adjustment for BitsCodec to be able to recognize int, long, int[] bit types.
    Storage classes are based on the length of bits.
    
    Signed-off-by: Ivan Martiniak <ivan.martiniak@pantheon.tech>
    Change-Id: I1223c1ff43b52fb8961dee96d379d0566c53a164

--- binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/BitsCodec.java
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/BitsCodec.java
@@ -12,20 +12,16 @@ import static java.util.Objects.requireNonNull;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.spec.naming.BindingMapping;
@@ -35,7 +31,7 @@ import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition.Bit;
 // FIXME: 'SchemaUnawareCodec' is not correct: we use BitsTypeDefinition in construction
 // FIXME: require the base class to be a TypeObject
 // FIXME: MDSAL-743: require BitsTypeObject base class
-final class BitsCodec extends SchemaUnawareCodec {
+abstract class BitsCodec<N> extends SchemaUnawareCodec {
     /*
      * Use identity comparison for keys and allow classes to be GCd themselves.
      *
@@ -46,35 +42,39 @@ final class BitsCodec extends SchemaUnawareCodec {
      *     "Virtual machine implementations are, however, encouraged to bias against clearing recently-created or
      *      recently-used soft references."
      */
-    private static final Cache<Class<?>, @NonNull BitsCodec> CACHE = CacheBuilder.newBuilder().weakKeys().softValues()
-        .build();
+    private static final Cache<Class<?>, @NonNull BitsCodec<?>> CACHE = CacheBuilder.newBuilder()
+            .weakKeys().softValues().build();
     private static final MethodType CONSTRUCTOR_INVOKE_TYPE = MethodType.methodType(Object.class, Object.class);
-
-    // Ordered by position
-    private final ImmutableMap<String, Method> getters;
-    private final ImmutableMap<String, Method> setters;
     private final MethodHandle ctor;
-
+    private final ImmutableMap<String, Method> setters;
+    private final Method getter;
     private final Object builder;
 
-    private BitsCodec(final MethodHandle ctor, final Map<String, Method> getters, Map<String, Method> setters,
-                      Object builder) {
+    private BitsCodec(final MethodHandle ctor, final Map<String, Method> setters, final Method getter,
+                      final Object builder) {
         this.ctor = requireNonNull(ctor);
-        this.getters = ImmutableMap.copyOf(getters);
+        this.builder = requireNonNull(builder);
+        this.getter = requireNonNull(getter);
         this.setters = ImmutableMap.copyOf(setters);
-        this.builder = builder;
     }
 
-    static @NonNull BitsCodec of(final Class<?> returnType, final BitsTypeDefinition rootType)
+    static BitsCodec<?> ofType(final BitsTypeDefinition type, final MethodHandle ctor,
+                               final Map<String, Method> setters, final Method getter, final Object builder) {
+        final int size = type.getBits().size();
+        if (size < 32) {
+            return new BitsCodecInteger(ctor, setters, getter, builder);
+        } else if (size < 64) {
+            return new BitsCodecLong(ctor, setters, getter, builder);
+        }
+        return new BitsCodecIntArray(ctor, setters, getter, builder);
+    }
+
+    static @NonNull BitsCodec<?> of(final Class<?> returnType, final BitsTypeDefinition rootType)
             throws ExecutionException {
         return CACHE.get(returnType, () -> {
-            final Map<String, Method> getters = new LinkedHashMap<>();
-            final Map<String, Method> setters = new LinkedHashMap<>();
+            final var setters = new LinkedHashMap<String, Method>();
 
-            for (Bit bit : rootType.getBits()) {
-                final Method valueGetter = returnType.getMethod(BindingMapping.GETTER_PREFIX
-                    + BindingMapping.getClassName(bit.getName()));
-                getters.put(bit.getName(), valueGetter);
+            for (final Bit bit : rootType.getBits()) {
                 if (returnType.getClasses().length > 0) {
                     final Method valueSetter = returnType.getClasses()[0].getMethod(BindingMapping.SETTER_PREFIX
                             + BindingMapping.getClassName(bit.getName()), boolean.class);
@@ -82,66 +82,97 @@ final class BitsCodec extends SchemaUnawareCodec {
                 }
             }
             Constructor<?> constructor = null;
-            for (Constructor<?> cst : returnType.getConstructors()) {
-                Class<?>[] parameterTypes = cst.getParameterTypes();
-                // Checks the length in case of constructor with no parameters
+            for (final Constructor<?> cst : returnType.getConstructors()) {
+                final Class<?>[] parameterTypes = cst.getParameterTypes();
                 if (parameterTypes.length > 0) {
                     if (!parameterTypes[0].equals(returnType)) {
                         constructor = cst;
                     }
                 }
             }
-            Object builder = returnType.getMethod("builder").invoke(null);
-
-            final MethodHandle ctor = MethodHandles.publicLookup().unreflectConstructor(constructor)
+            final var builder = returnType.getMethod("builder").invoke(null);
+            final var getBits = returnType.getMethod("getBits");
+            final var ctor = MethodHandles.publicLookup().unreflectConstructor(constructor)
                     .asType(CONSTRUCTOR_INVOKE_TYPE);
-            return new BitsCodec(ctor, getters, setters, builder);
+            return BitsCodec.ofType(rootType, ctor, setters, getBits, builder);
         });
     }
 
     @Override
     @SuppressWarnings("checkstyle:illegalCatch")
-    protected Object deserializeImpl(final Object input) {
-        checkArgument(input instanceof Set);
-        @SuppressWarnings("unchecked")
-        final Set<String> casted = (Set<String>) input;
-
+    protected @NonNull Object deserializeImpl(final @NonNull Object input) {
+        checkArgument(input instanceof Integer || input instanceof Long || input instanceof int[]);
+        final N casted = (N) input;
         /*
          * We can do this walk based on field set sorted by name, since constructor arguments in Java Binding are
          * sorted by name.
          *
          * This means we will construct correct array for construction of bits object.
          */
-        for (Entry<String, Method> valueSet : setters.entrySet()) {
+        final var settersList = ImmutableList.copyOf(setters.entrySet());
+        for (final var valueSet : settersList) {
             try {
-                valueSet.getValue().invoke(builder, casted.contains(valueSet.getKey()));
-            } catch (IllegalAccessException | InvocationTargetException e) {
+                valueSet.getValue().invoke(builder, isBitSet(settersList.indexOf(valueSet), casted));
+            } catch (final IllegalAccessException | InvocationTargetException e) {
                 throw new IllegalArgumentException("Failed to set bit " + valueSet.getKey(), e);
             }
         }
-
         try {
             return ctor.invokeExact(builder);
-        } catch (Throwable e) {
+        } catch (final Throwable e) {
             throw new IllegalStateException("Failed to instantiate object for " + input, e);
         }
     }
 
     @Override
-    protected Set<String> serializeImpl(final Object input) {
-        final Collection<String> result = new ArrayList<>(getters.size());
-        for (Entry<String, Method> valueGet : getters.entrySet()) {
-            final boolean value;
-            try {
-                value = (boolean) valueGet.getValue().invoke(input);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new IllegalArgumentException("Failed to get bit " + valueGet.getKey(), e);
-            }
-
-            if (value) {
-                result.add(valueGet.getKey());
-            }
+    protected @NonNull N serializeImpl(final @NonNull Object input) {
+        try {
+            return (N) getter.invoke(input);
+        } catch (final IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalArgumentException("Failed to get bits in object " + input, e);
         }
-        return result.size() == getters.size() ? getters.keySet() : ImmutableSet.copyOf(result);
+    }
+
+    protected abstract boolean isBitSet(int position, N bits);
+
+    static final class BitsCodecInteger extends BitsCodec<Integer> {
+
+        private BitsCodecInteger(final MethodHandle ctor, final Map<String, Method> setters, final Method getter,
+                                 final Object builder) {
+            super(ctor, setters, getter, builder);
+        }
+
+        @Override
+        protected boolean isBitSet(final int position, final Integer bits) {
+            return (bits & (1 << position)) != 0;
+        }
+    }
+
+    static final class BitsCodecLong extends BitsCodec<Long> {
+
+        private BitsCodecLong(final MethodHandle ctor, final Map<String, Method> setters, final Method getBits,
+                              final Object builder) {
+            super(ctor, setters, getBits, builder);
+        }
+
+        @Override
+        protected boolean isBitSet(final int position, final Long bits) {
+            return (bits & (1L << position)) != 0;
+        }
+    }
+
+    static final class BitsCodecIntArray extends BitsCodec<int[]> {
+        private static final int WORD_SIZE = 32;
+
+        private BitsCodecIntArray(final MethodHandle ctor, final Map<String, Method> setters, final Method getBits,
+                                  final Object builder) {
+            super(ctor, setters, getBits, builder);
+        }
+
+        @Override
+        protected boolean isBitSet(final int position, final int[] bits) {
+            final int word = bits[position / WORD_SIZE];
+            return (word & 1 << (position % WORD_SIZE)) != 0;
+        }
     }
 }
