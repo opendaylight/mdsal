f994b61c0f2f149bb133d54e437795169ef6b31e
matus.matok <matus.matok@pantheon.tech>
2022-11-07 10:59:42 +0100

    Add utility for DTO to JSON conversion.
    Added utility which converts DataObject instances into JSON and vice versa.
    Utility implemented as a two step conversion, where:
    - BindingIndependentMappingService converts DataObject -> NormalizedNode and vice versa.
    - JSONCodec which uses BindingIndependentMappingService as a static field and converts NormalizedNode -> JSON and vice versa.
    
    JIRA: MDSAL-39
    Change-Id: Ibce3565264b0f9d122703d1e4d5c6c33584d24fd
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

---
+++ binding/mdsal-binding-json-codec/src/main/java/org/opendaylight/mdsal/binding/json/codec/BindingIndependentMappingService.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.json.codec;
+
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.mdsal.binding.dom.codec.impl.BindingCodecContext;
+import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeContext;
+import org.opendaylight.mdsal.binding.runtime.spi.BindingRuntimeHelpers;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+public class BindingIndependentMappingService {
+    private static final BindingRuntimeContext RUNTIME_CONTEXT = BindingRuntimeHelpers.createRuntimeContext();
+    private static final BindingNormalizedNodeSerializer BINDING_CODEC_CONTEXT = new BindingCodecContext(
+            RUNTIME_CONTEXT);
+
+    public static <T extends DataObject> NormalizedNode toDataDom(final InstanceIdentifier<T> path, final T data) {
+        return BINDING_CODEC_CONTEXT.toNormalizedNode(path, data).getValue();
+    }
+
+    public static DataObject fromDataDom(final QName nodeType, final NormalizedNode node) {
+        return BINDING_CODEC_CONTEXT.fromNormalizedNode(YangInstanceIdentifier.builder().node(nodeType).build(), node)
+                .getValue();
+    }
+
+}
