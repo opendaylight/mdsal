51654b924adc900c568504c8eaa3eaca1fd87301
Oleksandr Panasiuk <oleksandr.panasiuk@pantheon.tech>
2022-11-23 16:16:00 +0200

    Improve Decimal64 validity checks
    Created a separate check for Decimal64 scale which allowed to modify value check as it is done to Uint32/64
    
    JIRA: MDSAL-736
    Signed-off-by: Oleksandr Panasiuk <oleksandr.panasiuk@pantheon.tech>
    Change-Id: I098b909b0f952d7b427cda827397c0efdd69dd37

--- binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/Decimal64RangeGenerator.java
+++ binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/Decimal64RangeGenerator.java
@@ -8,8 +8,6 @@
 package org.opendaylight.mdsal.binding.java.api.generator;
 
 import com.google.common.collect.Range;
-import java.lang.reflect.Array;
-import java.util.Locale;
 import java.util.Set;
 import java.util.function.Function;
 import org.opendaylight.yangtools.yang.binding.CodeHelpers;
@@ -24,24 +22,9 @@ import org.opendaylight.yangtools.yang.model.api.type.RangeConstraint;
  * boundary values.
  */
 final class Decimal64RangeGenerator extends AbstractRangeGenerator<Decimal64> {
+
     Decimal64RangeGenerator() {
         super(Decimal64.class);
-    }
-
-    private static String range(final Function<Class<?>, String> classImporter) {
-        return classImporter.apply(Range.class);
-    }
-
-    private static String itemType(final Function<Class<?>, String> classImporter) {
-        return range(classImporter) + '<' + classImporter.apply(Decimal64.class) + '>';
-    }
-
-    private static String arrayType(final Function<Class<?>, String> classImporter) {
-        return itemType(classImporter) + "[]";
-    }
-
-    private static String format(final Function<Class<?>, String> classImporter, final Decimal64 value) {
-        return classImporter.apply(Decimal64.class) +  ".of(" + value.scale() + ", " + value.unscaledValue() + "L)";
     }
 
     @Override
@@ -59,44 +42,28 @@ final class Decimal64RangeGenerator extends AbstractRangeGenerator<Decimal64> {
 
     @Override
     protected String generateRangeCheckerImplementation(final String checkerName,
-            final RangeConstraint<?> constraint, final Function<Class<?>, String> classImporter) {
+        final RangeConstraint<?> constraint, final Function<Class<?>, String> classImporter) {
         final Set<? extends Range<? extends Number>> constraints = constraint.getAllowedRanges().asRanges();
-        final String fieldName = checkerName.toUpperCase(Locale.ENGLISH) + "_RANGES";
         final StringBuilder sb = new StringBuilder();
 
-        // Field to hold the Range objects in an array
-        sb.append("private static final ").append(arrayType(classImporter)).append(' ').append(fieldName).append(";\n");
+        Range<? extends Number> range = constraints.stream().findAny().orElse(null);
+        Decimal64 min = getValue(range.lowerEndpoint());
+        Decimal64 max = getValue(range.upperEndpoint());
 
-        // Static initializer block for the array
-        sb.append("static {\n");
-        sb.append("    @SuppressWarnings(\"unchecked\")\n");
-        sb.append("    final ").append(arrayType(classImporter)).append(" a = (").append(arrayType(classImporter))
-            .append(") ").append(classImporter.apply(Array.class)).append(".newInstance(").append(range(classImporter))
-            .append(".class, ").append(constraints.size()).append(");\n");
+        String sc = String.valueOf(getValue(range.upperEndpoint()).scale());
 
-        int offset = 0;
-        for (Range<? extends Number> r : constraints) {
-            final String min = format(classImporter, getValue(r.lowerEndpoint()));
-            final String max = format(classImporter, getValue(r.upperEndpoint()));
-
-            sb.append("    a[").append(offset++).append("] = ").append(range(classImporter)).append(".closed(")
-                .append(min).append(", ").append(max).append(");\n");
-        }
-
-        sb.append("    ").append(fieldName).append(" = a;\n");
-        sb.append("}\n");
-
-        // Static enforcement method
         sb.append("private static void ").append(checkerName).append("(final ").append(getTypeName())
             .append(" value) {\n");
-        sb.append("    for (").append(itemType(classImporter)).append(" r : ").append(fieldName).append(") {\n");
-        sb.append("        if (r.contains(value)) {\n");
+        sb.append("    if (value.scale() <= ").append(sc).append(") {\n");
+        sb.append("        if (value.unscaledValue() >= ").append(min.unscaledValue()).append("L && ")
+            .append("value.unscaledValue() <= ").append(max.unscaledValue()).append("L) {\n");
         sb.append("            return;\n");
         sb.append("        }\n");
+        sb.append("        ").append(classImporter.apply(CodeHelpers.class)).append(".throwInvalidRange(")
+            .append("\"[[").append(min).append("..").append(max).append("]]\", value);\n");
         sb.append("    }\n");
-
-        sb.append("    ").append(classImporter.apply(CodeHelpers.class)).append(".throwInvalidRange(").append(fieldName)
-            .append(", value);\n");
+        sb.append("    ").append(classImporter.apply(CodeHelpers.class)).append(".throwInvalidScale(").append(sc)
+            .append(" , value.scale());\n");
         sb.append("}\n");
 
         return sb.toString();
