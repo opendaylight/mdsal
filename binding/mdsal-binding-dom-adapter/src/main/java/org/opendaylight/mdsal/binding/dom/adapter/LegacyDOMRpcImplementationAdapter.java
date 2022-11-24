c8547676458919c2183964eb1c068ca39486aaed
Robert Varga <robert.varga@pantheon.tech>
2022-11-25 00:40:13 +0100

    Centralize AbstractDOMRpcImplementationAdapter.serialize()
    The invocation paths between the two specializations are exactly the
    same, hence we can peel arguments and just pass down the input.
    
    JIRA: MDSAL-86
    Change-Id: I2df5061b47c63cd2886efdacfe5178acc08239de
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/LegacyDOMRpcImplementationAdapter.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/LegacyDOMRpcImplementationAdapter.java
@@ -12,27 +12,25 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.lang.invoke.MethodHandle;
-import org.opendaylight.mdsal.dom.api.DOMRpcIdentifier;
+import org.opendaylight.yangtools.yang.binding.RpcInput;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.RpcResult;
-import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 
 @Deprecated(since = "11.0.0", forRemoval = true)
 final class LegacyDOMRpcImplementationAdapter extends AbstractDOMRpcImplementationAdapter {
     private final MethodHandle handle;
 
-    LegacyDOMRpcImplementationAdapter(final AdapterContext adapterContext, final QName inputName,
+    LegacyDOMRpcImplementationAdapter(final AdapterContext adapterContext, final QName rpcName,
             final MethodHandle handle) {
-        super(adapterContext, inputName);
+        super(adapterContext, rpcName);
         this.handle = requireNonNull(handle);
     }
 
     @Override
     @SuppressWarnings("checkstyle:illegalCatch")
-    ListenableFuture<RpcResult<?>> invokeRpc(final CurrentAdapterSerializer serializer, final DOMRpcIdentifier rpc,
-            final ContainerNode input) {
+    ListenableFuture<RpcResult<?>> invokeRpc(final RpcInput input) {
         try {
-            return (ListenableFuture<RpcResult<?>>) handle.invokeExact(deserialize(serializer, rpc.getType(), input));
+            return (ListenableFuture<RpcResult<?>>) handle.invokeExact(input);
         } catch (Throwable e) {
             Throwables.throwIfUnchecked(e);
             throw new IllegalStateException(e);
