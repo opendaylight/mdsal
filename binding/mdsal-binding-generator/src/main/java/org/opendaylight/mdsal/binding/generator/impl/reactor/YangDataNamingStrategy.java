88663e0164c89ef3b6f2744066cdf1c89293a86a
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2022-12-13 12:34:08 +0200

    RFC8040 'rc:yang-data' support for mdsal binding generator
    New naming strategy is introduced to generate artifact names out of
    yang-data argument: if name is yang identifier compliant the camel-case
    transformation used, otherwise non-compliant characters are encoded.
    
    JIRA: MDSAL-675
    Change-Id: I6644f550a378cd176e5f53a201ee6e70b32c6410
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/YangDataNamingStrategy.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding.generator.impl.reactor;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.mdsal.binding.spec.naming.BindingMapping;
+import org.opendaylight.yangtools.yang.common.AbstractQName;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName;
+
+@NonNullByDefault
+public class YangDataNamingStrategy extends CamelCaseNamingStrategy {
+
+    final String originalName;
+
+    public YangDataNamingStrategy(final StatementNamespace namespace, final String originalName, final String altName) {
+        super(namespace, buildNodeIdentifier(requireNonNull(originalName), requireNonNull(altName)));
+        this.originalName = originalName;
+    }
+
+    private static AbstractQName buildNodeIdentifier(final String originalName, final String altName) {
+        final AbstractQName originalNodeIdentier = UnresolvedQName.tryLocalName(originalName);
+        return originalNodeIdentier == null ? UnresolvedQName.tryLocalName(altName) : originalNodeIdentier;
+    }
+
+    @Override
+    String simpleClassName() {
+        // if name is yang identifier compliant then use camel case transformation,
+        // encode non-compliant characters otherwise
+        return originalName.equals(nodeIdentifier().getLocalName())
+                ? BindingMapping.getClassName(originalName) : BindingMapping.mapYangDataName(originalName);
+    }
+
+    @Override
+    ToStringHelper addToStringAttributes(@NonNull ToStringHelper helper) {
+        return helper.add("originalName", originalName);
+    }
+}
