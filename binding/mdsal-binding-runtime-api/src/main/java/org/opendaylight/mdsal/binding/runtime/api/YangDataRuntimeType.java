9dab4048b11db123c9bd235efea469e486292c6f
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2022-12-13 12:34:08 +0200

    RFC8040 'rc:yang-data' support for mdsal binding generator
    JIRA: MDSAL-675
    Change-Id: I6644f550a378cd176e5f53a201ee6e70b32c6410
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ binding/mdsal-binding-runtime-api/src/main/java/org/opendaylight/mdsal/binding/runtime/api/YangDataRuntimeType.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.mdsal.binding.runtime.api;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.rfc8040.model.api.YangDataEffectiveStatement;
+
+public interface YangDataRuntimeType extends CompositeRuntimeType {
+
+    @Override
+    @NonNull YangDataEffectiveStatement statement();
+
+}
