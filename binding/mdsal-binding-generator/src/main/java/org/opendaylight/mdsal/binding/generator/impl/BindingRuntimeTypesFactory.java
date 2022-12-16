d587b510507f711a4eb70409a86dc6ed4ab9987b
Šimon Ukuš <simon.ukus@pantheon.tech>
2022-12-16 12:04:14 +0100

    Precalculate RuntimeTypes mapping for substitution
    Rather than determining whether two particular cases or augmentations
    could be substituted using reflection, the same logic can be achieved
    using BindingRuntimeTypes.
    The mapping is precalculated, when creating BindingRuntimeTypes.
    
    JIRA: MDSAL-785
    Change-Id: I4f81c500b00035b41b58eb2f17468237408eb166
    Signed-off-by: Šimon Ukuš <simon.ukus@pantheon.tech>

--- binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/BindingRuntimeTypesFactory.java
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/BindingRuntimeTypesFactory.java
@@ -11,9 +11,16 @@ import static com.google.common.base.Verify.verify;
 
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.SetMultimap;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.generator.impl.reactor.AbstractExplicitGenerator;
 import org.opendaylight.mdsal.binding.generator.impl.reactor.Generator;
@@ -27,6 +34,7 @@ import org.opendaylight.mdsal.binding.generator.impl.reactor.TypeBuilderFactory;
 import org.opendaylight.mdsal.binding.generator.impl.rt.DefaultBindingRuntimeTypes;
 import org.opendaylight.mdsal.binding.model.api.GeneratedType;
 import org.opendaylight.mdsal.binding.model.api.JavaTypeName;
+import org.opendaylight.mdsal.binding.runtime.api.AugmentRuntimeType;
 import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeTypes;
 import org.opendaylight.mdsal.binding.runtime.api.CaseRuntimeType;
 import org.opendaylight.mdsal.binding.runtime.api.IdentityRuntimeType;
@@ -38,6 +46,8 @@ import org.opendaylight.yangtools.concepts.Mutable;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -56,6 +66,10 @@ final class BindingRuntimeTypesFactory implements Mutable {
     private final Map<QName, InputRuntimeType> rpcInputs = new HashMap<>();
     // All known 'choice's to their corresponding cases
     private final SetMultimap<JavaTypeName, CaseRuntimeType> choiceToCases = HashMultimap.create();
+    // All case to cases mapping, values are the cases that can substitute case that is the key
+    private final Multimap<GeneratedType, CaseRuntimeType> caseToSubstitutionCases = HashMultimap.create();
+    // All augment to augments mapping where values are augments that can substitute augment that is the key
+    private final Multimap<GeneratedType, AugmentRuntimeType> augmentToSubstitutionAugments = HashMultimap.create();
 
     private BindingRuntimeTypesFactory() {
         // Hidden on purpose
@@ -70,7 +84,8 @@ final class BindingRuntimeTypesFactory implements Mutable {
         LOG.debug("Indexed {} generators in {}", moduleGens.size(), sw);
 
         return new DefaultBindingRuntimeTypes(context, factory.modules, factory.allTypes, factory.identities,
-            factory.rpcInputs, factory.rpcOutputs, factory.choiceToCases);
+                factory.rpcInputs, factory.rpcOutputs, factory.choiceToCases, factory.caseToSubstitutionCases,
+                factory.augmentToSubstitutionAugments);
     }
 
     private void indexModules(final Map<QNameModule, ModuleGenerator> moduleGens) {
@@ -101,13 +116,25 @@ final class BindingRuntimeTypesFactory implements Mutable {
             }
         }
 
-        indexRuntimeTypes(moduleGens.values());
+        final Map<CaseRuntimeType, List<EffectiveStatement<?, ?>>> caseToChildren = new HashMap<>();
+        final Map<AugmentRuntimeType, List<EffectiveStatement<?, ?>>> augmentToChildren = new HashMap<>();
+        indexRuntimeTypes(moduleGens.values(), caseToChildren, augmentToChildren);
+
+        collectSubstsForCase(caseToChildren);
+        collectSubstsForAugment(augmentToChildren);
+
     }
 
-    private void indexRuntimeTypes(final Iterable<? extends Generator> generators) {
+    private void indexRuntimeTypes(final Iterable<? extends Generator> generators,
+            final Map<CaseRuntimeType, List<EffectiveStatement<?, ?>>> caseToChildren,
+            final Map<AugmentRuntimeType, List<EffectiveStatement<?, ?>>> augmentToChildren) {
+
         for (Generator gen : generators) {
             if (gen instanceof AbstractExplicitGenerator<?, ?> explicitGen && gen.generatedType().isPresent()) {
                 final var type = explicitGen.runtimeType().orElseThrow();
+                // calling iterator() on Generator returns Iterator<Generator> of its childGenerators
+                // (check AbstractCompositeGenerator.java)
+                Iterator<Generator> it = gen.iterator();
                 if (type.javaType() instanceof GeneratedType genType) {
                     final var name = genType.getIdentifier();
                     final var prev = allTypes.put(name, type);
@@ -118,15 +145,140 @@ final class BindingRuntimeTypesFactory implements Mutable {
                     // and make assumptions about its shape -- which works just fine without touching the
                     // ChoiceRuntimeType for cases.
                     if (type instanceof CaseRuntimeType caseType) {
+                        caseToChildren.put(caseType, generatorsToStatements(it));
                         final var ifaces = genType.getImplements();
                         // The appropriate choice and DataObject at the very least. The choice interface is the first
                         // one mentioned.
                         verify(ifaces.size() >= 2, "Unexpected implemented interfaces %s", ifaces);
                         choiceToCases.put(ifaces.get(0).getIdentifier(), caseType);
                     }
+
+                    if (type instanceof AugmentRuntimeType augmentType) {
+                        augmentToChildren.put(augmentType, generatorsToStatements(it));
+                    }
                 }
             }
-            indexRuntimeTypes(gen);
+            indexRuntimeTypes(gen, caseToChildren, augmentToChildren);
+        }
+    }
+
+    /**
+     * {@link Generator} represent a single node in a generator tree.
+     * {@link AbstractExplicitGenerator} is a Generator associated with a particular {@link EffectiveStatement}.
+     * This method maps {@link Iterator} of {@link Generator}s to {@link List} of {@link EffectiveStatement}s.
+     *
+     * @param iterator iterator over a collection of child generators
+     * @return list of statements corresponding to the generators
+     */
+    private static List<EffectiveStatement<?, ?>> generatorsToStatements(final Iterator<Generator> iterator) {
+        List<Generator> preResult = new ArrayList<>();
+        iterator.forEachRemaining(preResult::add);
+        return preResult.stream()
+                .map(gen -> ((AbstractExplicitGenerator<?, ?>) gen).statement())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * This method populates {@code caseToSubstitutionCases} that maps each case to its possible substitutions.
+     *
+     * @param caseToChildernStmts map of case to its corresponding children
+     */
+    private void collectSubstsForCase(final Map<CaseRuntimeType, List<EffectiveStatement<?, ?>>> caseToChildernStmts) {
+        final Multimap<GeneratedType, CaseRuntimeType> localToSubstitutions = HashMultimap.create();
+        for (Entry<JavaTypeName, CaseRuntimeType> entry : choiceToCases.entries()) {
+            JavaTypeName choice = entry.getKey();
+            // CaseRuntimeTypes associated with this choice
+            Collection<CaseRuntimeType> caseTypes = choiceToCases.get(choice);
+
+            for (CaseRuntimeType caseType : caseTypes) {
+                addSubstitutionalCases(localToSubstitutions, caseType, caseTypes, caseToChildernStmts);
+            }
+        }
+        caseToSubstitutionCases.putAll(localToSubstitutions);
+    }
+
+
+    /**
+     * Put to map:
+     * <ul>
+     *  <li>key - GeneratedType - the CaseRuntimeType.javaType() we want to substitute</li>
+     *  <li>value - CaseRuntimeType - the type that can be used as a substitution</li>
+     * </ul>
+     *
+     * <p>
+     * Two cases could be substituted only if and if:
+     * <ul>
+     *   <li>Both implements same interfaces</li>
+     *   <li>Both have same children</li>
+     *   <li>Both are from same choice. Choice class was generated for data node in grouping.</li>
+     * </ul>
+     *
+     * <p>
+     * The last condition can be ignored in this case, since the {@code candidates} are extracted
+     * from {@code choiceToCases}, which already provides only cases from one particular choice (containing also
+     * all the augmented cases belonging to that choice).
+     *
+     * @param localToSubstitutions mapping from case to all cases that could be used as a substitution
+     * @param local                current CaseType for which substitutions we are looking for
+     * @param candidates           available cases from one particular choice
+     */
+    private static void addSubstitutionalCases(final Multimap<GeneratedType, CaseRuntimeType> localToSubstitutions,
+            final CaseRuntimeType local,
+            final Collection<CaseRuntimeType> candidates,
+            final Map<CaseRuntimeType, List<EffectiveStatement<?, ?>>> caseToChildernStmts) {
+
+        for (CaseRuntimeType candidate : candidates) {
+            if (candidate.equals(local)) {
+                continue;
+            }
+            boolean substitutional;
+            // check if both have same interfaces
+            substitutional = candidate.javaType().getImplements().equals(local.javaType().getImplements());
+            // check if both have same children
+            substitutional &= caseToChildernStmts.get(candidate).equals(caseToChildernStmts.get(local));
+
+            // no need to check if they are from same choice
+            if (substitutional) {
+                localToSubstitutions.put(local.javaType(), candidate);
+            }
+        }
+    }
+
+    private void collectSubstsForAugment(
+            final Map<AugmentRuntimeType, List<EffectiveStatement<?, ?>>> augToChildrenStmts) {
+        // let's use allStmts for now
+        for (var runtimeType : allTypes.values()) {
+            // skip types that are not interesting
+            if (!(runtimeType instanceof AugmentRuntimeType augmentType)) {
+                continue;
+            }
+            for (var entryAugToChildren : augToChildrenStmts.entrySet()) {
+                // do not compare the same instance to itself
+                if (augmentType.equals(entryAugToChildren.getKey())) {
+                    continue;
+                }
+                final var substitution = entryAugToChildren.getKey();
+                final var substitChildren = entryAugToChildren.getValue();
+                final var substitIfaces = entryAugToChildren.getKey().javaType().getImplements();
+
+                final var runtimeTypeChildren = augToChildrenStmts.get(runtimeType);
+                final var runtimeTypeIfaces = augmentType.javaType().getImplements();
+
+                boolean substitutional;
+                // check if both have same interfaces
+                substitutional = substitIfaces.equals(runtimeTypeIfaces);
+                // check if both have same children
+                substitutional &= substitChildren.equals(runtimeTypeChildren);
+                // the last check needs to verify if they have the same target class
+                AugmentEffectiveStatement typeTarget = augmentType.statement();
+                AugmentEffectiveStatement substitTarget = substitution.statement();
+                substitutional &= typeTarget.equals(substitTarget);
+
+                // if the 'augmentType' can be substituted for 'substitution', add it to the map
+                if (substitutional) {
+                    augmentToSubstitutionAugments.put(augmentType.javaType(), substitution);
+                }
+            }
         }
     }
 
