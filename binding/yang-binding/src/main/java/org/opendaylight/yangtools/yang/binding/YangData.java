de10f083f5bb08801eedbaf5aefe5d74682c5536
Robert Varga <robert.varga@pantheon.tech>
2023-01-17 21:52:18 +0100

    WIP: Add YangData base interface
    RFC8040 defines a yang-data extension, which defines a piece of data,
    which is yang-modeled, but does not tie to any of the RFC7950
    constructs.
    
    Add a baseline binding interface which captures its basic contract and
    codegen generates interfaces on top of it.
    
    JIRA: MDSAL-675
    Change-Id: I6f93733ce15ad47294c7c04bc7de2d7aa22ac9cd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/YangData.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.binding;
+
+/**
+ * A piece of YANG-modeled data, as defined by <a href="https://www.rfc-editor.org/rfc/rfc8040>RESTCONF</a>
+ * {@code yang-data extension}.
+ *
+ * <p>
+ * This is quite similar to a {@link DataObject}, but it cannot be directly stored in a data store. Its data subtree may
+ * actually have a relationship to a data store subtree -- but that part is dependent on the context in which this
+ * object is used.
+ *
+ * <p>
+ * As per the extension definition, the structure of children is such that there is a single {@code container} which
+ * represents the content. This is variadic and may contain, for example, a modeling indirection through a number of
+ * {@code choice} and {@code case} statements. Unlike a {@link DataObject}, though, this construct is not subject to
+ * {@code augment} statements nor any sort of extensibility.
+ *
+ * <p>
+ * Furthermore {@code rc:yang-data} can only appear as a top-level module contract and therefore the code generation is
+ * limited to a single interface, which only provides the default implementation of {@link #implementedInterface()}
+ * bound to itself.
+ *
+ * @param <T> Generated interface type
+ */
+// TODO: when we add support for RFC8791 YANG Data Structure Extensions, these should be built on top of this
+//       construct, which is to say the hypothetical YangStructure interface needs to extend this interface and bring
+//       Augmentable into the picture -- augment-structure maps directly to 'augment' semantics, I think.
+//       On the other hand, users of YangData may assume the lack of augmentations, which would warrant creating
+//       a completely separate YangStructure hierarchy of BindingObject, DataContainer and Augmentable
+public non-sealed interface YangData<T extends YangData<T>> extends BindingObject, DataContainer {
+    @Override
+    Class<T> implementedInterface();
+}
