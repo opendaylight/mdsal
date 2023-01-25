d1ac7d72ec10e96e650dda0210e7d194a02ec1d2
matus.matok <matus.matok@pantheon.tech>
2023-01-25 16:37:27 +0100

    WIP: Make InstanceIdentifierBuilder variadic to KeyedInstanceIdentifier
    1) provide a new implementation of InstanceIdentifierBuilder - similar to InstanceIdentifierBuilderImpl, but one which builds KeyedInstanceIdentifiers instead.
    2) make child() methods with key values return this new KeyedInstanceIdentifier.Builder
    3) add tests to cover this new change and possibly fix older tests which might be failing because of it.
    
    JIRA: MDSAL-798
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>
    Change-Id: I404df6aa1f3c79f259ea705adedd58a9d466cb78

---
+++ binding/mdsal-binding-test-model/src/test/java/org/opendaylight/mdsal/binding/test/model/Mdsal798Test.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.test.model;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.test.model.mock.List12KeyMock;
+import org.opendaylight.mdsal.binding.test.model.mock.List12Mock;
+import org.opendaylight.mdsal.binding.test.model.mock.List1KeyMock;
+import org.opendaylight.mdsal.binding.test.model.mock.List1Mock;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifierBuilder;
+import org.opendaylight.yangtools.yang.binding.WildcardedInstanceIdentifierBuilder;
+
+public class Mdsal798Test {
+
+    @Test
+    public void test() {
+        assertThrows(IllegalArgumentException.class, () -> InstanceIdentifier.builder(List1Mock.class).child(
+                List12Mock.class, new List12KeyMock(
+                Integer.valueOf("78"))));
+        final var keyedBuilder = InstanceIdentifier.builder(List1Mock.class, new List1KeyMock("placeholder"));
+        assertTrue(keyedBuilder instanceof KeyedInstanceIdentifierBuilder<List1Mock>);
+        assertThrows(IllegalArgumentException.class, () -> keyedBuilder.child(List12Mock.class));
+        assertTrue(keyedBuilder.wildcardChild(List12Mock.class) instanceof
+                WildcardedInstanceIdentifierBuilder<List12Mock>);
+        assertTrue(keyedBuilder.child(List12Mock.class, new List12KeyMock(55)) instanceof
+                KeyedInstanceIdentifierBuilder<List12Mock>);
+    }
+}
---
+++ binding/mdsal-binding-test-model/src/test/java/org/opendaylight/mdsal/binding/test/model/mock/List12Mock.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.test.model.mock;
+
+import com.google.common.base.MoreObjects;
+import java.lang.Class;
+import java.lang.Integer;
+import java.lang.NullPointerException;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import javax.annotation.processing.Generated;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.extension.yang.ext.rev130709.$YangModuleInfoImpl;
+import org.opendaylight.yangtools.yang.binding.Augmentable;
+import org.opendaylight.yangtools.yang.binding.ChildOf;
+import org.opendaylight.yangtools.yang.binding.CodeHelpers;
+import org.opendaylight.yangtools.yang.binding.Identifiable;
+import org.opendaylight.yangtools.yang.common.QName;
+
+/**
+ *
+ * <p>
+ * This class represents the following YANG schema fragment defined in module <b>opendaylight-of-migration-test-model</b>
+ * <pre>
+ * list list1-2 {
+ *   key attr-int;
+ *   leaf attr-int {
+ *     type int32;
+ *   }
+ *   leaf attr-str {
+ *     type string;
+ *   }
+ * }
+ * </pre>
+ * <p>To create instances of this class use {@link List12Builder}.
+ * @see List12Builder
+ * @see List12Key
+ *
+ */
+@Generated("mdsal-binding-generator")
+public interface List12Mock
+        extends
+        ChildOf<List1Mock>,
+        Augmentable<List12Mock>,
+        Identifiable<List12KeyMock>
+{
+
+
+
+    /**
+     * YANG identifier of the statement represented by this class.
+     */
+    public static final @NonNull QName QNAME = $YangModuleInfoImpl.qnameOf("list1-2");
+
+    @Override
+    default Class<List12Mock> implementedInterface() {
+        return List12Mock.class;
+    }
+
+    /**
+     * Default implementation of {@link Object#hashCode()} contract for this interface.
+     * Implementations of this interface are encouraged to defer to this method to get consistent hashing
+     * results across all implementations.
+     *
+     * @param obj Object for which to generate hashCode() result.
+     * @return Hash code value of data modeled by this interface.
+     * @throws NullPointerException if {@code obj} is null
+     */
+    static int bindingHashCode(final @NonNull List12Mock obj) {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Objects.hashCode(obj.getAttrInt());
+        result = prime * result + Objects.hashCode(obj.getAttrStr());
+        for (var augmentation : obj.augmentations().values()) {
+            result += augmentation.hashCode();
+        }
+        return result;
+    }
+
+    /**
+     * Default implementation of {@link Object#equals(Object)} contract for this interface.
+     * Implementations of this interface are encouraged to defer to this method to get consistent equality
+     * results across all implementations.
+     *
+     * @param thisObj Object acting as the receiver of equals invocation
+     * @param obj Object acting as argument to equals invocation
+     * @return True if thisObj and obj are considered equal
+     * @throws NullPointerException if {@code thisObj} is null
+     */
+    static boolean bindingEquals(final @NonNull List12Mock thisObj, final Object obj) {
+        if (thisObj == obj) {
+            return true;
+        }
+        final List12Mock other = CodeHelpers.checkCast(List12Mock.class, obj);
+        if (other == null) {
+            return false;
+        }
+        if (!Objects.equals(thisObj.getAttrInt(), other.getAttrInt())) {
+            return false;
+        }
+        if (!Objects.equals(thisObj.getAttrStr(), other.getAttrStr())) {
+            return false;
+        }
+        return thisObj.augmentations().equals(other.augmentations());
+    }
+
+    /**
+     * Default implementation of {@link Object#toString()} contract for this interface.
+     * Implementations of this interface are encouraged to defer to this method to get consistent string
+     * representations across all implementations.
+     *
+     * @param obj Object for which to generate toString() result.
+     * @return {@link String} value of data modeled by this interface.
+     * @throws NullPointerException if {@code obj} is null
+     */
+    static String bindingToString(final @NonNull List12Mock obj) {
+        final var helper = MoreObjects.toStringHelper("List12");
+        CodeHelpers.appendValue(helper, "attrInt", obj.getAttrInt());
+        CodeHelpers.appendValue(helper, "attrStr", obj.getAttrStr());
+        CodeHelpers.appendAugmentations(helper, "augmentation", obj);
+        return helper.toString();
+    }
+
+    @Override
+    List12KeyMock key();
+
+    /**
+     * Return attrInt, or {@code null} if it is not present.
+     *
+     * @return {@code Integer} attrInt, or {@code null} if it is not present.
+     *
+     */
+    Integer getAttrInt();
+
+    /**
+     * Return attrInt, guaranteed to be non-null.
+     *
+     * @return {@code Integer} attrInt, guaranteed to be non-null.
+     * @throws NoSuchElementException if attrInt is not present
+     *
+     */
+    default @NonNull Integer requireAttrInt() {
+        return CodeHelpers.require(getAttrInt(), "attrint");
+    }
+
+    /**
+     * Return attrStr, or {@code null} if it is not present.
+     *
+     * @return {@code String} attrStr, or {@code null} if it is not present.
+     *
+     */
+    String getAttrStr();
+
+    /**
+     * Return attrStr, guaranteed to be non-null.
+     *
+     * @return {@code String} attrStr, guaranteed to be non-null.
+     * @throws NoSuchElementException if attrStr is not present
+     *
+     */
+    default @NonNull String requireAttrStr() {
+        return CodeHelpers.require(getAttrStr(), "attrstr");
+    }
+
+}
+
+
