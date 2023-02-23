473b07792dbe0bf88f60d7640068fb7d018bb99d
matus.matok <matus.matok@pantheon.tech>
2023-02-23 13:34:42 +0100

    WIP: Make InstanceIdentifierBuilder variadic to KeyedInstanceIdentifier
    Added new interface KeyedInstanceIdentifierBuilder nested in KeyedInstanceIdentifier identical to InstanceIdentifierBuilder in all regards but generic constraints.
    Added KeyedInstanceIdentifierBuilderImpl which implements KeyedInstanceIdentifierBuilder.
    
    JIRA: MDSAL-798
    Change-Id: I404df6aa1f3c79f259ea705adedd58a9d466cb78
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/InstanceIdentifierBuilderImpl.java
+++ binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/InstanceIdentifierBuilderImpl.java
@@ -16,26 +16,38 @@ import org.opendaylight.yangtools.util.HashCodeBuilder;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.IdentifiableItem;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.InstanceIdentifierBuilder;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.Item;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.KeyedBuilder;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.PathArgument;
 
-final class InstanceIdentifierBuilderImpl<T extends DataObject> implements InstanceIdentifierBuilder<T> {
-    private final ImmutableList.Builder<PathArgument> pathBuilder = ImmutableList.builder();
-    private final HashCodeBuilder<PathArgument> hashBuilder;
-    private final Iterable<? extends PathArgument> basePath;
-    private boolean wildcard = false;
-    private PathArgument arg = null;
+class InstanceIdentifierBuilderImpl<T extends DataObject> implements InstanceIdentifierBuilder<T> {
+    protected final ImmutableList.Builder<PathArgument> pathBuilder;
+    protected final HashCodeBuilder<PathArgument> hashBuilder;
+    protected final Iterable<? extends PathArgument> basePath;
+    protected boolean wildcard = false;
+    protected PathArgument arg = null;
 
     InstanceIdentifierBuilderImpl() {
-        this.hashBuilder = new HashCodeBuilder<>();
-        this.basePath = null;
+        hashBuilder = new HashCodeBuilder<>();
+        pathBuilder = ImmutableList.builder();
+        basePath = null;
     }
 
     InstanceIdentifierBuilderImpl(final PathArgument item, final Iterable<? extends PathArgument> pathArguments,
             final int hash, final boolean wildcard) {
-        this.hashBuilder = new HashCodeBuilder<>(hash);
-        this.basePath = pathArguments;
+        hashBuilder = new HashCodeBuilder<>(hash);
+        basePath = pathArguments;
+        pathBuilder = ImmutableList.builder();
         this.wildcard = wildcard;
-        this.arg = item;
+        arg = item;
+    }
+
+    InstanceIdentifierBuilderImpl(final PathArgument item, final Iterable<? extends PathArgument> pathArguments,
+            final ImmutableList.Builder<PathArgument> pathBuilder, final int hash, final boolean wildcard) {
+        hashBuilder = new HashCodeBuilder<>(hash);
+        basePath = pathArguments;
+        this.pathBuilder = pathBuilder;
+        this.wildcard = wildcard;
+        arg = item;
     }
 
     @Override
@@ -58,27 +70,37 @@ final class InstanceIdentifierBuilderImpl<T extends DataObject> implements Insta
         return false;
     }
 
+    @SuppressWarnings("unchecked")
+    protected <N extends DataObject> InstanceIdentifierBuilderImpl<N> getInstanceIdentifierBuilder() {
+        return (InstanceIdentifierBuilderImpl<N>) this;
+    }
+
+    protected <N extends DataObject & Identifiable<K>, K extends Identifier<N>>
+        KeyedInstanceIdentifierBuilderImpl<N, K> getKeyedInstanceIdentifierBuilder(final K key) {
+        return new KeyedInstanceIdentifierBuilderImpl<>(arg, basePath, pathBuilder, hashCode(), wildcard);
+    }
+
     @Override
-    public <N extends ChildOf<? super T>> InstanceIdentifierBuilderImpl<N> child(final Class<N> container) {
+    public <N extends ChildOf<? super T>> @NonNull InstanceIdentifierBuilder<N> child(final Class<N> container) {
         return addNode(container);
     }
 
     @Override
-    public <C extends ChoiceIn<? super T> & DataObject, N extends ChildOf<? super C>> InstanceIdentifierBuilder<N>
-            child(final Class<C> caze, final Class<N> container) {
+    public <C extends ChoiceIn<? super T> & DataObject, N extends ChildOf<? super C>> @NonNull
+            InstanceIdentifierBuilder<N> child(final Class<C> caze, final Class<N> container) {
         return addWildNode(Item.of(caze, container));
     }
 
     @Override
-    public <N extends Identifiable<K> & ChildOf<? super T>, K extends Identifier<N>> InstanceIdentifierBuilderImpl<N>
-            child(final Class<@NonNull N> listItem, final K listKey) {
+    public <N extends Identifiable<K> & ChildOf<? super T>, K extends Identifier<N>> @NonNull
+            KeyedBuilder<N,K> child(final Class<@NonNull N> listItem, final K listKey) {
         return addNode(IdentifiableItem.of(listItem, listKey));
     }
 
     @Override
     public <C extends ChoiceIn<? super T> & DataObject, K extends Identifier<N>,
-        N extends Identifiable<K> & ChildOf<? super C>> InstanceIdentifierBuilder<N> child(final Class<C> caze,
-                final Class<N> listItem, final K listKey) {
+        N extends Identifiable<K> & ChildOf<? super C>> @NonNull KeyedBuilder<N,K>
+        child(final Class<C> caze, final Class<N> listItem, final K listKey) {
         return addNode(IdentifiableItem.of(caze, listItem, listKey));
     }
 
@@ -91,13 +113,44 @@ final class InstanceIdentifierBuilderImpl<T extends DataObject> implements Insta
      * @return This builder
      */
     @Override
-    public <N extends DataObject & Augmentation<? super T>> InstanceIdentifierBuilderImpl<N> augmentation(
+    public <N extends DataObject & Augmentation<? super T>> @NonNull InstanceIdentifierBuilder<N> augmentation(
             final Class<N> container) {
         return addNode(container);
     }
 
+
+    <N extends DataObject> @NonNull InstanceIdentifierBuilderImpl<N> addWildNode(final Item<?> newArg) {
+        return addNode(newArg);
+    }
+
+    @SuppressWarnings("unchecked")
+    <N extends DataObject> @NonNull InstanceIdentifierBuilderImpl<N> addNode(final Item<?> newArg) {
+        if (Identifiable.class.isAssignableFrom(newArg.getType())) {
+            wildcard = true;
+        }
+        addNodeInternal(newArg);
+        return getInstanceIdentifierBuilder();
+    }
+
+    @SuppressWarnings("unchecked")
+    <N extends DataObject & Identifiable<K>, K extends Identifier<N>>
+            @NonNull KeyedBuilder<N, K> addNode(final IdentifiableItem<?,?> newArg) {
+        addNodeInternal(newArg);
+        return getKeyedInstanceIdentifierBuilder((K)newArg.getKey());
+    }
+
+    protected  <N extends DataObject> @NonNull InstanceIdentifierBuilderImpl<N> addNode(final Class<N> container) {
+        return addWildNode(Item.of(container));
+    }
+
+    protected void addNodeInternal(final PathArgument newArg) {
+        arg = newArg;
+        hashBuilder.addArgument(newArg);
+        pathBuilder.add(newArg);
+    }
+
     @Override
-    public InstanceIdentifier<T> build() {
+    public @NonNull InstanceIdentifier<T> build() {
         Preconditions.checkState(arg != null, "No path arguments present");
 
         final Iterable<PathArgument> pathArguments;
@@ -108,24 +161,5 @@ final class InstanceIdentifierBuilderImpl<T extends DataObject> implements Insta
         }
 
         return InstanceIdentifier.trustedCreate(arg, pathArguments, hashBuilder.build(), wildcard);
-    }
-
-    <N extends DataObject> @NonNull InstanceIdentifierBuilderImpl<N> addWildNode(final PathArgument newArg) {
-        if (Identifiable.class.isAssignableFrom(newArg.getType())) {
-            wildcard = true;
-        }
-        return addNode(newArg);
-    }
-
-    @SuppressWarnings("unchecked")
-    <N extends DataObject> @NonNull InstanceIdentifierBuilderImpl<N> addNode(final PathArgument newArg) {
-        arg = newArg;
-        hashBuilder.addArgument(newArg);
-        pathBuilder.add(newArg);
-        return (InstanceIdentifierBuilderImpl<N>) this;
-    }
-
-    private <N extends DataObject> @NonNull InstanceIdentifierBuilderImpl<N> addNode(final Class<N> container) {
-        return addWildNode(Item.of(container));
     }
 }
