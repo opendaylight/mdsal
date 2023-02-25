d442b934fdbb317694fdf351d85d682b5b86ecff
Robert Varga <robert.varga@pantheon.tech>
2023-02-25 11:50:36 +0100

    WIP: Bump yangtools to 11.0.0-SNAPSHOT
    This is a test for integration. Also migrates to use YangDataName.
    
    Change-Id: I7d331e28ace9c8b512f960f4452da1fa8d178d05
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-spec-util/src/test/java/org/opendaylight/mdsal/binding/spec/naming/BindingMappingTest.java
+++ binding/mdsal-binding-spec-util/src/test/java/org/opendaylight/mdsal/binding/spec/naming/BindingMappingTest.java
@@ -11,12 +11,12 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.common.YangDataName;
 
 public class BindingMappingTest {
     @Test
@@ -107,7 +107,7 @@ public class BindingMappingTest {
 
     private static void assertEqualMapping(final List<String> mapped, final List<String> yang) {
         assertEquals(mapped.size(), yang.size());
-        final Map<String, String> expected = new HashMap<>();
+        final var expected = new HashMap<String, String>();
         for (int i = 0; i < mapped.size(); ++i) {
             expected.put(yang.get(i), mapped.get(i));
         }
@@ -117,13 +117,15 @@ public class BindingMappingTest {
 
     @Test
     public void yangDataMapping() {
+        final var ns = QNameModule.create(XMLNamespace.of("unused"));
+
         // single ascii compliant non-conflicting word - remain as is
-        assertEquals("single", BindingMapping.mapYangDataName("single"));
+        assertEquals("single", BindingMapping.mapYangDataName(new YangDataName(ns, "single")));
         // ascii compliant - non-compliany chars only encoded
-        assertEquals("$abc$20$cde", BindingMapping.mapYangDataName("abc cde"));
+        assertEquals("$abc$20$cde", BindingMapping.mapYangDataName(new YangDataName(ns, "abc cde")));
         // latin1 compliant -> latin chars normalized, non-compliant chars are encoded
-        assertEquals("$ľaľaho$20$papľuhu", BindingMapping.mapYangDataName("ľaľaho papľuhu"));
+        assertEquals("$ľaľaho$20$papľuhu", BindingMapping.mapYangDataName(new YangDataName(ns, "ľaľaho papľuhu")));
         // latin1 non-compliant - all non-compliant characters encoded
-        assertEquals("$привет$20$papľuhu", BindingMapping.mapYangDataName("привет papľuhu"));
+        assertEquals("$привет$20$papľuhu", BindingMapping.mapYangDataName(new YangDataName(ns, "привет papľuhu")));
     }
 }
