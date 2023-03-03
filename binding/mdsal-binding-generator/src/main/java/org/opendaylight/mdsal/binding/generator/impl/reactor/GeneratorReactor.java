96c7a06e1827b423ffd5c1f61f2526eb8a7c0ed2
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-03-03 08:42:50 +0100

    Retain grouping/uses instantiation vectors
    We need the ability fo find all instantiations of a grouping for
    closed-world analysis of BindingRuntimeTypes.
    
    This analysis is need to determine, for example:
    - possible types of 'type leafref's pointing outside a grouping,
      to determine which Binding/DOM codecs are applicable
    - YANG/Binding overload mapping, i.e. whether a 'container'
      defined in a grouping is instantiated only once or multiple
      types, to use a strongly-bound CodecDataObject it the former case
    
    This patch exposes a GroupingRuntimeType.instantiations(), which exposes
    exactly this information.
    
    DefaultGroupingRuntimeType stores this information in the form of
    a set of vectors, each pointing either to another grouping or to a
    concrete instantiation.
    
    GeneratorReactor collects this information sufficiently early so that it
    can be also used to perform partial closed-world analysis during
    compile-time.
    
    JIRA: MDSAL-669
    Change-Id: I2e21a6b93ce30d9bd1022be5747d44663b6198fc
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/GeneratorReactor.java
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/GeneratorReactor.java
@@ -17,8 +17,10 @@ import com.google.common.collect.Maps;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.eclipse.jdt.annotation.NonNull;
@@ -182,15 +184,20 @@ public final class GeneratorReactor extends GeneratorContext implements Mutable 
          */
         linkDependencies(children);
 
-        // Step five: resolve all 'type leafref' and 'type identityref' statements, so they point to their
-        //            corresponding Java type representation.
+        // Step 5: resolve grouping usage, so that each GroupingGenerator has links to their instantiation sites and
+        //         any unused
+        resolveGroupingUsers();
+        freezeGroupingUsers(children);
+
+        // Step 6: resolve all 'type leafref' and 'type identityref' statements, so they point to their corresponding
+        //         Java type representation.
         bindTypeDefinition(children);
 
-        // Step six: walk all composite generators and link ChildOf/ChoiceIn relationships with parents. We have taken
-        //           care of this step during tree construction, hence this now a no-op.
+        // Step 7: walk all composite generators and link ChildOf/ChoiceIn relationships with parents. We have taken
+        //         care of this step during tree construction, hence this now a no-op.
 
         /*
-         * Step seven: assign java packages and JavaTypeNames
+         * Step 8: assign java packages and JavaTypeNames
          *
          * This is a really tricky part, as we have large number of factors to consider:
          * - we are mapping grouping, typedef, identity and schema tree namespaces into Fully Qualified Class Names,
@@ -222,11 +229,13 @@ public final class GeneratorReactor extends GeneratorContext implements Mutable 
             }
         } while (haveUnresolved);
 
-        // Step eight: generate actual Types
-        //
-        // We have now properly cross-linked all generators and have assigned their naming roots, so from this point
-        // it looks as though we are performing a simple recursive execution. In reality, though, the actual path taken
-        // through generators is dictated by us as well as generator linkage.
+        /*
+         * Step 9: generate actual Types
+         *
+         * We have now properly cross-linked all generators and have assigned their naming roots, so from this point
+         * it looks as though we are performing a simple recursive execution. In reality, though, the actual path taken
+         * through generators is dictated by us as well as generator linkage.
+         */
         for (var module : children) {
             module.ensureType(builderFactory);
         }
@@ -416,6 +425,73 @@ public final class GeneratorReactor extends GeneratorContext implements Mutable 
                 bindTypeDefinition(child);
             }
             stack.pop();
+        }
+    }
+
+    private void resolveGroupingUsers() {
+        // Primary pass on modules, collecting all groupings which were left unprocessed
+        final var remaining = new HashSet<GroupingGenerator>();
+        linkGroupingUsers(remaining, children);
+        LOG.debug("Grouping pass 1 found {} groupings", remaining.size());
+
+        // Secondary passes: if any unprocessed groupings have been marked as used, process their children, potentially
+        //                   adding more work
+        int passes = 2;
+        int processed;
+        do {
+            // Do not process groupings again unless we make some progress
+            processed = 0;
+
+            final var found = new HashSet<GroupingGenerator>();
+            final var it = remaining.iterator();
+            while (it.hasNext()) {
+                final var next = it.next();
+                if (next.hasUser()) {
+                    // Process this grouping and remember we need to iterate again, as groupings we have already visited
+                    // may become used as a side-effect.
+                    it.remove();
+                    linkGroupingUsers(found, next);
+                    processed++;
+                }
+            }
+
+            final var foundSize = found.size();
+            LOG.debug("Grouping pass {} processed {} and found {} grouping(s)", passes, processed, foundSize);
+            if (foundSize != 0) {
+                // we have some more groupings to process, shove them into the next iteration
+                remaining.addAll(found);
+            }
+
+            passes++;
+        } while (processed != 0);
+
+        LOG.debug("Grouping usage completed after {} pass(es) with unused {} grouping(s)", passes, remaining.size());
+    }
+
+    // Iterate over a some generators recursively, linking them to the GroupingGenerators they use. GroupingGenerators
+    // are skipped and added to unprocessedGroupings for later processing.
+    private static void linkGroupingUsers(final Set<GroupingGenerator> groupings,
+            final Iterable<? extends Generator> parent) {
+        for (var child : parent) {
+            if (child instanceof GroupingGenerator grouping) {
+                groupings.add(grouping);
+            } else if (child instanceof AbstractCompositeGenerator<?, ?> composite) {
+                for (var grouping : composite.groupings()) {
+                    grouping.addUser(composite);
+                }
+                linkGroupingUsers(groupings, composite);
+            }
+        }
+    }
+
+    private static void freezeGroupingUsers(final Iterable<? extends Generator> parent) {
+        for (var child : parent) {
+            if (child instanceof AbstractCompositeGenerator<?, ?> composite) {
+                if (composite instanceof GroupingGenerator grouping) {
+                    grouping.freezeUsers();
+                }
+                freezeGroupingUsers(composite);
+            }
         }
     }
 }
