6262168fdbde8ccd645ec1abf4f53602d551e5bc
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-03-03 08:42:50 +0100

    WIP: Retain grouping/uses instantiation vectors
    For each encountered grouping we now retain information
    about their corresponding instantiations. These can be
    acquired trough BindingRuntimeTypes. Back-references
    stored here points to instantiation roots.
    
    // FIXME: test augmentations
    // FIXME: test uses/augment/uses
    
    JIRA: MDSAL-669
    Change-Id: I2e21a6b93ce30d9bd1022be5747d44663b6198fc
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-runtime-api/src/main/java/org/opendaylight/mdsal/binding/runtime/api/GroupingRuntimeType.java
+++ binding/mdsal-binding-runtime-api/src/main/java/org/opendaylight/mdsal/binding/runtime/api/GroupingRuntimeType.java
@@ -7,6 +7,8 @@
  */
 package org.opendaylight.mdsal.binding.runtime.api;
 
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.model.api.stmt.GroupingEffectiveStatement;
 
 /**
@@ -15,4 +17,16 @@ import org.opendaylight.yangtools.yang.model.api.stmt.GroupingEffectiveStatement
 public interface GroupingRuntimeType extends CompositeRuntimeType {
     @Override
     GroupingEffectiveStatement statement();
+
+    /**
+     * Return vectors towards concrete instantiations of this type -- i.e. the manifestation in the effective data tree.
+     * Each item in this list represents either:
+     * <ul>
+     *   <li>a concrete instantiation, or<li>
+     *   <li>another {@link GroupingRuntimeType}</li>
+     * </ul>
+     *
+     * @return A list of vectors through which this grouping is instantiated.
+     */
+    @NonNull List<CompositeRuntimeType> instantiationVectors();
 }
