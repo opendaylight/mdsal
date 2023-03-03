300d5724a15699bc2c7255f17ceaafc119b4f9d6
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-03-03 08:42:50 +0100

    Retain grouping/uses instantiation vectors
    We need the ability fo find all instantiations of a grouping for
    closed-world analysis of BindingRuntimeTypes.
    
    This analysis is need to determine, for example:
    - possible types of 'type leafref's pointing outside a grouping,
      to determine which Binding/DOM codecs are applicable
    - YANG/Binding overload mapping, i.e. whether a 'container'
      defined in a grouping is instantiated only once or multiple
      types, to use a strongly-bound CodecDataObject it the former case
    
    This patch exposes a GroupingRuntimeType.instantiations(), which exposes
    exactly this information.
    
    DefaultGroupingRuntimeType stores this information in the form of
    a set of vectors, each pointing either to another grouping or to a
    concrete instantiation.
    
    GeneratorReactor collects this information sufficiently early so that it
    can be also used to perform partial closed-world analysis during
    compile-time.
    
    JIRA: MDSAL-669
    Change-Id: I2e21a6b93ce30d9bd1022be5747d44663b6198fc
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/rt/DefaultGroupingRuntimeType.java
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/rt/DefaultGroupingRuntimeType.java
@@ -7,16 +7,80 @@
  */
 package org.opendaylight.mdsal.binding.generator.impl.rt;
 
+import com.google.common.base.VerifyException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.mdsal.binding.model.api.GeneratedType;
+import org.opendaylight.mdsal.binding.runtime.api.CompositeRuntimeType;
 import org.opendaylight.mdsal.binding.runtime.api.GroupingRuntimeType;
 import org.opendaylight.mdsal.binding.runtime.api.RuntimeType;
 import org.opendaylight.yangtools.yang.model.api.stmt.GroupingEffectiveStatement;
 
 public final class DefaultGroupingRuntimeType extends AbstractCompositeRuntimeType<GroupingEffectiveStatement>
         implements GroupingRuntimeType {
+    /**
+     * These are vectors towards concrete instantiations of this type -- i.e. the manifestation in the effective data
+     * tree. Each item in this list represents either:
+     * <ul>
+     *   <li>a concrete instantiation, or<li>
+     *   <li>another {@link GroupingRuntimeType}</li>
+     * </ul>
+     * We use these vectors to create {@link #instantiations()}.
+     */
+    private final @Nullable Object instantiationVectors;
+
     public DefaultGroupingRuntimeType(final GeneratedType bindingType, final GroupingEffectiveStatement statement,
-            final List<RuntimeType> children) {
+            final List<RuntimeType> children, final List<? extends CompositeRuntimeType> instantiationVectors) {
         super(bindingType, statement, children);
+        this.instantiationVectors = switch (instantiationVectors.size()) {
+            case 0 -> null;
+            case 1 -> Objects.requireNonNull(instantiationVectors.get(0));
+            default -> instantiationVectors.stream().map(Objects::requireNonNull).toArray(CompositeRuntimeType[]::new);
+        };
+    }
+
+    @Override
+    public Set<CompositeRuntimeType> instantiations() {
+        final var local = instantiationVectors;
+        if (local == null) {
+            return Set.of();
+        }
+
+        final var ret = new HashSet<CompositeRuntimeType>();
+        resolveVectors(ret, local);
+        return ret;
+    }
+
+    private void resolveVectors(final Set<CompositeRuntimeType> set) {
+        final var local = instantiationVectors;
+        if (local != null) {
+            resolveVectors(set, local);
+        }
+    }
+
+    private static void resolveVectors(final Set<CompositeRuntimeType> set, final @NonNull Object vector) {
+        if (vector instanceof CompositeRuntimeType composite) {
+            resolveVector(set, composite);
+        } else if (vector instanceof CompositeRuntimeType[] composites) {
+            for (var composite : composites) {
+                resolveVector(set, composite);
+            }
+        } else {
+            throw new VerifyException("Unhandled vectors " + vector.getClass());
+        }
+    }
+
+    private static void resolveVector(final Set<CompositeRuntimeType> set, final CompositeRuntimeType vector) {
+        if (vector instanceof DefaultGroupingRuntimeType thisImpl) {
+            thisImpl.resolveVectors(set);
+        } else if (vector instanceof GroupingRuntimeType grouping) {
+            set.addAll(grouping.instantiations());
+        } else {
+            set.add(vector);
+        }
     }
 }
