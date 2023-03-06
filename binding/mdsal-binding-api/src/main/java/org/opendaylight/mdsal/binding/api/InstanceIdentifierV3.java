7287753a5e9b938f5d7a62a93a4aa665aef202fa
matus.matok <matus.matok@pantheon.tech>
2023-03-06 12:54:10 +0100

    Add binding.api.(Keyed)Instance{Identifier,Wildcard}
    Moved WIP implementation of (Keyed)InstanceIdentifier and its respective builder from yang.binding into mdsal.binding.api
    Removed field `boolean wildcarded` from (Keyed)InstanceIdentifier(BuilderImpl).
    Added (Keyed)InstanceWildcard
    Added (Keyed)WildcardBuilder(Impl)
    Improved implementation of (Keyed)InstanceIdentifier(Builder). Keyed{InstanceIdentifier|Wildcard}BuilderImpl now extends {InstanceIdentifier|Wildcard}BuilderImpl respectively.
    Ported InstanceIdentifier related tests from yang.binding to mdsal.binding.api and fixed them to work with the new local implementation.
    Added static ofLegacy() and toLegacy() methods for a conversion between mdsal.binding.api.(Keyed)Instance{Identifier|Wildcard} and yang.binding.(Keyed)Instance{Identifier|Wildcard}. KeyedInstanceWildcard has no such methods as the KeyedInstanceWildcard is represented as a yang.binding.InstanceIdentifier whose last argument is IdentifiableItem and is wildcarded. Hence, such methods would not make sense. Instead InstanceWildcard.{to|of}Legacy() methods are available that of course are accessible through KeyedInstanceWildcard.
    
    JIRA: MDSAL-815
    Change-Id: I6c55246d642dacc9ab946893e87d02971d3e2c71
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

---
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/InstanceIdentifierV3.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.api;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.ObjectStreamException;
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.binding.api.InstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+class InstanceIdentifierV3<T extends DataObject> implements Externalizable {
+    @Serial
+    private static final long serialVersionUID = 3L;
+
+    private @Nullable Iterable<PathArgument> pathArguments;
+    private @Nullable Class<T> targetType;
+    private int hash;
+
+    @SuppressWarnings("redundantModifier")
+    public InstanceIdentifierV3() {
+        // For Externalizable
+    }
+
+    InstanceIdentifierV3(final InstanceIdentifier<T> source) {
+        pathArguments = source.pathArguments;
+        targetType = source.getTargetType();
+        hash = source.hashCode();
+    }
+
+    final int getHash() {
+        return hash;
+    }
+
+    final Iterable<PathArgument> getPathArguments() {
+        return pathArguments;
+    }
+
+    final Class<T> getTargetType() {
+        return targetType;
+    }
+
+    @Override
+    public void writeExternal(final ObjectOutput out) throws IOException {
+        out.writeObject(targetType);
+        out.writeInt(hash);
+        out.writeInt(Iterables.size(pathArguments));
+        for (Object o : pathArguments) {
+            out.writeObject(o);
+        }
+    }
+
+    @Override
+    public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+        targetType = (Class<T>) in.readObject();
+        hash = in.readInt();
+
+        final int size = in.readInt();
+        final List<PathArgument> args = new ArrayList<>();
+        for (int i = 0; i < size; ++i) {
+            args.add((PathArgument) in.readObject());
+        }
+        pathArguments = ImmutableList.copyOf(args);
+    }
+
+    @Serial
+    Object readResolve() throws ObjectStreamException {
+        return new InstanceIdentifier<>(targetType, pathArguments, hash);
+    }
+}
