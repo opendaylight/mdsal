710fa3ce542d98a0275eba6d5c7bb3baca5deb37
matus.matok <matus.matok@pantheon.tech>
2023-03-17 15:22:52 +0100

    Duplicate {Read|Write}Operations, DataTreeChangeService methods
    Duplicated methods in interfaces ReadOperations, WriteOperations,
    DataTreeChangeService parametrized by mdsal.binding.api.InstanceIdentifier
    instead of yang.binding.InstanceIdentifier. Implemented the methods in all the
    inheritors. For that purpose a duplication of
    BindingCodecContext.toYangInstanceIdentifier() method for
    mdsal.binding.api.Instance{Identifier|Wildcard}, with all methods that are
    called in the implementation.
    Added test, to ensure correct behavior of these new methods.
    
    JIRA: MDSAL-817
    Change-Id: Ibad30e0ae1dfdd66181d6df8b3e62ab7ebc95a99
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

--- binding/mdsal-binding-dom-codec/src/test/java/org/opendaylight/mdsal/binding/dom/codec/impl/InstanceIdentifierSerializeDeserializeTest.java
+++ binding/mdsal-binding-dom-codec/src/test/java/org/opendaylight/mdsal/binding/dom/codec/impl/InstanceIdentifierSerializeDeserializeTest.java
@@ -94,11 +94,16 @@ public class InstanceIdentifierSerializeDeserializeTest extends AbstractBindingC
 
     @Test
     public void testBindingAwareIIToYangIContainer() {
-        final YangInstanceIdentifier yangInstanceIdentifier = codecContext.toYangInstanceIdentifier(
+        final YangInstanceIdentifier yangInstanceIdentifier1 = codecContext.toYangInstanceIdentifier(
                 InstanceIdentifier.create(Top.class).child(TopLevelList.class));
-        final PathArgument lastPathArgument = yangInstanceIdentifier.getLastPathArgument();
-        assertTrue(lastPathArgument instanceof NodeIdentifier);
-        assertEquals(TopLevelList.QNAME, lastPathArgument.getNodeType());
+        final YangInstanceIdentifier yangInstanceIdentifier2 = codecContext.toYangInstanceIdentifier(
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier.create(Top.class)
+                        .wildcardChild(TopLevelList.class));
+        final PathArgument lastPathArgument1 = yangInstanceIdentifier1.getLastPathArgument();
+        final PathArgument lastPathArgument2 = yangInstanceIdentifier2.getLastPathArgument();
+        assertTrue(lastPathArgument1 instanceof NodeIdentifier && lastPathArgument2 instanceof NodeIdentifier);
+        assertEquals(TopLevelList.QNAME, lastPathArgument1.getNodeType());
+        assertEquals(TopLevelList.QNAME, lastPathArgument2.getNodeType());
     }
 
     @Test
@@ -112,57 +117,99 @@ public class InstanceIdentifierSerializeDeserializeTest extends AbstractBindingC
 
     @Test
     public void testBindingAwareIIToYangIIListWithKey() {
-        final YangInstanceIdentifier yangInstanceIdentifier = codecContext.toYangInstanceIdentifier(
+        final YangInstanceIdentifier yangInstanceIdentifier1 = codecContext.toYangInstanceIdentifier(
                 InstanceIdentifier.create(Top.class).child(TopLevelList.class, TOP_FOO_KEY));
-        final PathArgument lastPathArgument = yangInstanceIdentifier.getLastPathArgument();
-        assertTrue(lastPathArgument instanceof NodeIdentifierWithPredicates);
-        assertTrue(((NodeIdentifierWithPredicates) lastPathArgument).values().contains(TOP_LEVEL_LIST_KEY_VALUE));
-        assertEquals(TopLevelList.QNAME, lastPathArgument.getNodeType());
+        final YangInstanceIdentifier yangInstanceIdentifier2 = codecContext.toYangInstanceIdentifier(
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier.create(Top.class)
+                        .child(TopLevelList.class, TOP_FOO_KEY));
+        final PathArgument lastPathArgument1 = yangInstanceIdentifier1.getLastPathArgument();
+        final PathArgument lastPathArgument2 = yangInstanceIdentifier2.getLastPathArgument();
+        assertTrue(lastPathArgument1 instanceof NodeIdentifierWithPredicates);
+        assertTrue(lastPathArgument2 instanceof NodeIdentifierWithPredicates);
+        assertTrue(((NodeIdentifierWithPredicates) lastPathArgument1).values().contains(TOP_LEVEL_LIST_KEY_VALUE));
+        assertTrue(((NodeIdentifierWithPredicates) lastPathArgument2).values().contains(TOP_LEVEL_LIST_KEY_VALUE));
+        assertEquals(TopLevelList.QNAME, lastPathArgument1.getNodeType());
+        assertEquals(TopLevelList.QNAME, lastPathArgument2.getNodeType());
     }
 
     @Test
     public void testBindingAwareIIToYangIIAugmentation() {
-        final PathArgument lastArg = codecContext.toYangInstanceIdentifier(BA_TREE_COMPLEX_USES).getLastPathArgument();
-        assertTrue(lastArg instanceof AugmentationIdentifier);
+        final PathArgument lastArg1 = codecContext.toYangInstanceIdentifier(BA_TREE_COMPLEX_USES).getLastPathArgument();
+        final PathArgument lastArg2 = codecContext.toYangInstanceIdentifier(
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier.ofLegacy(BA_TREE_COMPLEX_USES))
+                .getLastPathArgument();
+        assertTrue(lastArg1 instanceof AugmentationIdentifier);
+        assertTrue(lastArg2 instanceof AugmentationIdentifier);
     }
 
     @Test
     public void testBindingAwareIIToYangIILeafOnlyAugmentation() {
-        final PathArgument leafOnlyLastArg = codecContext.toYangInstanceIdentifier(BA_TREE_LEAF_ONLY)
+        final PathArgument leafOnlyLastArg1 = codecContext.toYangInstanceIdentifier(BA_TREE_LEAF_ONLY)
                 .getLastPathArgument();
-        assertTrue(leafOnlyLastArg instanceof AugmentationIdentifier);
-        assertTrue(((AugmentationIdentifier) leafOnlyLastArg).getPossibleChildNames().contains(SIMPLE_VALUE_QNAME));
+        final PathArgument leafOnlyLastArg2 = codecContext.toYangInstanceIdentifier(
+                        org.opendaylight.mdsal.binding.api.InstanceIdentifier.ofLegacy(BA_TREE_LEAF_ONLY))
+                .getLastPathArgument();
+        assertTrue(leafOnlyLastArg1 instanceof AugmentationIdentifier);
+        assertTrue(leafOnlyLastArg2 instanceof AugmentationIdentifier);
+        assertTrue(((AugmentationIdentifier) leafOnlyLastArg1).getPossibleChildNames().contains(SIMPLE_VALUE_QNAME));
+        assertTrue(((AugmentationIdentifier) leafOnlyLastArg2).getPossibleChildNames().contains(SIMPLE_VALUE_QNAME));
     }
 
     @Test
     public void testChoiceCaseGroupingFromBinding() {
+        final InstanceIdentifier<GrpCont> legacyId1 = InstanceIdentifier.builder(Cont.class)
+                .child(ContBase.class, GrpCont.class)
+                .build();
         final YangInstanceIdentifier contBase = codecContext.toYangInstanceIdentifier(
-            InstanceIdentifier.builder(Cont.class).child(ContBase.class, GrpCont.class).build());
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier.ofLegacy(legacyId1).toLegacy());
         assertEquals(YangInstanceIdentifier.create(NodeIdentifier.create(Cont.QNAME),
             NodeIdentifier.create(ContChoice.QNAME), NodeIdentifier.create(GrpCont.QNAME)), contBase);
 
-        final YangInstanceIdentifier contAug = codecContext.toYangInstanceIdentifier(
+
+        final org.opendaylight.mdsal.binding.api.InstanceIdentifier<GrpCont> id1 =
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier
+                .builder(Cont.class).child(ContBase.class, GrpCont.class).build();
+        final YangInstanceIdentifier contBase2 = codecContext.toYangInstanceIdentifier(id1);
+        assertEquals(YangInstanceIdentifier.create(NodeIdentifier.create(Cont.QNAME),
+                NodeIdentifier.create(ContChoice.QNAME), NodeIdentifier.create(GrpCont.QNAME)), contBase2);
+
+        final YangInstanceIdentifier contAugFromLegacy = codecContext.toYangInstanceIdentifier(
             InstanceIdentifier.builder(Cont.class).child(ContAug.class, GrpCont.class).build());
+        final YangInstanceIdentifier contAug = codecContext.toYangInstanceIdentifier(
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier.builder(Cont.class).child(ContAug.class,
+                        GrpCont.class).build()
+        );
         assertEquals(YangInstanceIdentifier.create(NodeIdentifier.create(Cont.QNAME),
             NodeIdentifier.create(ContChoice.QNAME),
-            NodeIdentifier.create(GrpCont.QNAME.bindTo(ContAug.QNAME.getModule()))), contAug);
+            NodeIdentifier.create(GrpCont.QNAME.bindTo(ContAug.QNAME.getModule()))), contAugFromLegacy);
+        assertEquals(contAugFromLegacy, contAug);
 
         // Legacy: downcast the child to Class, losing type safety but still working. Faced with ambiguity, it will
         //         select the lexically-lower class
         assertEquals(1, ContBase.class.getCanonicalName().compareTo(ContAug.class.getCanonicalName()));
         final YangInstanceIdentifier contAugLegacy = codecContext.toYangInstanceIdentifier(
             InstanceIdentifier.builder(Cont.class).child((Class) GrpCont.class).build());
-        assertEquals(contAug, contAugLegacy);
+        final YangInstanceIdentifier contAug2 = codecContext.toYangInstanceIdentifier(
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier.builder(Cont.class).child((Class)GrpCont.class)
+                        .build());
+        assertEquals(contAugFromLegacy, contAugLegacy);
+        assertEquals(contAug, contAug2);
 
-        final YangInstanceIdentifier rootBase = codecContext.toYangInstanceIdentifier(
+        final YangInstanceIdentifier rootBaseFromLegacy = codecContext.toYangInstanceIdentifier(
             InstanceIdentifier.builder(RootBase.class, GrpCont.class).build());
+        final YangInstanceIdentifier rootBase = codecContext.toYangInstanceIdentifier(
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier.builder(RootBase.class, GrpCont.class).build());
         assertEquals(YangInstanceIdentifier.create(NodeIdentifier.create(Root.QNAME),
-            NodeIdentifier.create(GrpCont.QNAME)), rootBase);
+            NodeIdentifier.create(GrpCont.QNAME)), rootBaseFromLegacy);
+        assertEquals(rootBaseFromLegacy, rootBase);
 
-        final YangInstanceIdentifier rootAug = codecContext.toYangInstanceIdentifier(
+        final YangInstanceIdentifier rootAugFromLegacy = codecContext.toYangInstanceIdentifier(
             InstanceIdentifier.builder(RootAug.class, GrpCont.class).build());
+        final YangInstanceIdentifier rootAug = codecContext.toYangInstanceIdentifier(
+                org.opendaylight.mdsal.binding.api.InstanceIdentifier.builder(RootAug.class, GrpCont.class).build());
         assertEquals(YangInstanceIdentifier.create(NodeIdentifier.create(Root.QNAME),
-            NodeIdentifier.create(GrpCont.QNAME.bindTo(RootAug.QNAME.getModule()))), rootAug);
+            NodeIdentifier.create(GrpCont.QNAME.bindTo(RootAug.QNAME.getModule()))), rootAugFromLegacy);
+        assertEquals(rootAugFromLegacy, rootAug);
     }
 
     @Test
