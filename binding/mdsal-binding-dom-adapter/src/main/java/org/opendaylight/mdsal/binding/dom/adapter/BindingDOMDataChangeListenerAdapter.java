9caba1f025515a74e4f2120ad441619ddf0b4050
Oleksandr Panasiuk <oleksandr.panasiuk@pantheon.tech>
2023-03-28 12:36:14 +0300

    Add simple data listeners
    DataTreeChangeListener is a rather complex contract, where users
    sometimes want to receive only the latest state or the delta of the
    state.
    
    Add DataListener, which reports the current value and
    DataChangeListener, which reports changes.
    
    JIRA: MDSAL-813
    Change-Id: I7db024f76709b9d4afcc0db5cbca3f1d35218e3f
    Signed-off-by: Oleksandr Panasiuk <oleksandr.panasiuk@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMDataChangeListenerAdapter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.mdsal.binding.api.DataChangeListener;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingAugmentationCodecTreeNode;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingDataObjectCodecTreeNode;
+import org.opendaylight.mdsal.binding.dom.codec.api.CommonDataObjectCodecTreeNode;
+import org.opendaylight.mdsal.dom.api.ClusteredDOMDataTreeChangeListener;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.tree.api.DataTreeCandidate;
+
+final class BindingDOMDataChangeListenerAdapter<T extends DataObject> implements ClusteredDOMDataTreeChangeListener {
+    private final AdapterContext adapterContext;
+    private final DataChangeListener<T> listener;
+
+    BindingDOMDataChangeListenerAdapter(final AdapterContext adapterContext, final DataChangeListener<T> listener) {
+        this.adapterContext = requireNonNull(adapterContext);
+        this.listener = requireNonNull(listener);
+    }
+
+    @Override
+    public void onDataTreeChanged(final List<DataTreeCandidate> changes) {
+        final var first = changes.get(0);
+        final var serializer = adapterContext.currentSerializer();
+        final var codec = serializer.getSubtreeCodec(serializer.coerceInstanceIdentifier(first.getRootPath()));
+
+        listener.dataChanged(deserialize(codec, first.getRootNode().dataBefore()),
+            deserialize(codec, changes.get(changes.size() - 1).getRootNode().dataAfter()));
+    }
+
+    @Override
+    public void onInitialData() {
+        listener.dataChanged(null, null);
+    }
+
+    @SuppressWarnings("unchecked")
+    private @Nullable T deserialize(final CommonDataObjectCodecTreeNode<?> codec, final NormalizedNode data) {
+        if (data == null) {
+            return null;
+        } else if (codec instanceof BindingDataObjectCodecTreeNode<?> dataObject) {
+            return (T) dataObject.deserialize(data);
+        } else if (codec instanceof BindingAugmentationCodecTreeNode<?> augmentation) {
+            return (T) augmentation.filterFrom(data);
+        } else {
+            throw new IllegalStateException("Unhandled codec " + codec);
+        }
+    }
+}
