f79c4bbbf7d771217c724b70f7d58ba3c82e9e93
Oleksandr Panasiuk <oleksandr.panasiuk@pantheon.tech>
2023-03-28 12:36:14 +0300

    Add DataBroker.registerListener
    Add DataBroker.registerListener(DataTreeIdentifier,DataListener)
    Implemented functional interface DataListener and implemented interface DataObjectListenerAdapter
    
    JIRA: MDSAL-813
    Change-Id: I7db024f76709b9d4afcc0db5cbca3f1d35218e3f
    Signed-off-by: Oleksandr Panasiuk <oleksandr.panasiuk@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/DataTreeChangeService.java
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/DataTreeChangeService.java
@@ -9,6 +9,7 @@ package org.opendaylight.mdsal.binding.api;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 
 /**
@@ -55,4 +56,81 @@ public interface DataTreeChangeService extends BindingService {
      */
     <T extends DataObject, L extends DataTreeChangeListener<T>> @NonNull ListenerRegistration<L>
             registerDataTreeChangeListener(@NonNull DataTreeIdentifier<T> treeId, @NonNull L listener);
+
+    /**
+     * Registers a {@link DataTreeChangeListener} to receive
+     * notifications when data changes under a given path in the conceptual data tree.
+     *
+     * <p>
+     * You are able to register for notifications  for any node or subtree
+     * which can be represented using {@link DataTreeIdentifier}.
+     *
+     * <p>
+     * This method returns a {@link Registration} object. To
+     * "unregister" your listener for changes call the {@link Registration#close()}
+     * method on the returned object.
+     *
+     * <p>
+     * You MUST explicitly unregister your listener when you no longer want to receive
+     * notifications. This is especially true in OSGi environments, where failure to
+     * do so during bundle shutdown can lead to stale listeners being still registered.
+     * @implSpec This method provides {@link DataListenerAdapter} as listener during
+     *      the registration of {@link DataTreeChangeListener}. This would allow users
+     *      to know the last state of data instead of getting details about what changed
+     *      in the entire tree.
+     * @param treeId Data tree identifier of the subtree which should be watched for
+     *            changes.
+     * @param listener Listener instance which is being registered
+     * @return Listener registration object, which may be used to unregister
+     *         your listener using {@link Registration#close()} to stop
+     *         delivery of change events.
+     */
+    default <T extends DataObject> @NonNull Registration registerDataListener(
+            final @NonNull DataTreeIdentifier<T> treeId, final @NonNull DataListener<T> listener) {
+        return registerDataTreeChangeListener(checkNotWildcard(treeId), new DataListenerAdapter<>(listener));
+    }
+
+    /**
+     * Registers a {@link DataTreeChangeListener} to receive
+     * notifications about the last data state when it changes under a given path in the conceptual data
+     * tree.
+     *
+     * <p>
+     * You are able to register for notifications  for any node or subtree
+     * which can be represented using {@link DataTreeIdentifier}.
+     *
+     * <p>
+     * This method returns a {@link Registration} object. To
+     * "unregister" your listener for changes call the {@link Registration#close()}
+     * method on the returned object.
+     *
+     * <p>
+     * You MUST explicitly unregister your listener when you no longer want to receive
+     * notifications. This is especially true in OSGi environments, where failure to
+     * do so during bundle shutdown can lead to stale listeners being still registered.
+     *
+     * @implSpec This method provides {@link DataChangeListenerAdapter} as listener during
+     *      the registration of {@link DataTreeChangeListener}, which provides a comparison
+     *      of before-value and after-value.
+     *
+     * @param treeId Data tree identifier of the subtree which should be watched for
+     *            changes.
+     * @param listener Listener instance which is being registered
+     * @return Listener registration object, which may be used to unregister
+     *         your listener using {@link Registration#close()} to stop
+     *         delivery of change events.
+     */
+    default <T extends DataObject> @NonNull Registration registerDataChangeListener(
+            final @NonNull DataTreeIdentifier<T> treeId, final @NonNull DataChangeListener<T> listener) {
+        return registerDataTreeChangeListener(checkNotWildcard(treeId), new DataChangeListenerAdapter<>(listener));
+    }
+
+    private static <T extends DataObject> @NonNull DataTreeIdentifier<T> checkNotWildcard(
+            final DataTreeIdentifier<T> treeId) {
+        final var instanceIdentifier = treeId.getRootIdentifier();
+        if (instanceIdentifier.isWildcarded()) {
+            throw new IllegalArgumentException("Cannot register listener for wildcard " + instanceIdentifier);
+        }
+        return treeId;
+    }
 }
---
+++ binding/mdsal-binding-api/src/test/java/org/opendaylight/mdsal/binding/api/DataTreeChangeServiceWildcarded.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.api;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.mdsal813.norev.RegisterListenerTest;
+import org.opendaylight.yang.gen.v1.mdsal813.norev.register.listener.test.Item;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class DataTreeChangeServiceWildcarded {
+
+    DataBroker dataBroker;
+
+    DataListener<Item> listener;
+
+    DataChangeListener<Item> changeListener;
+
+    @Before
+    public void setUp() {
+        dataBroker = mock(DataBroker.class);
+        listener = mock(DataListener.class);
+        changeListener = mock(DataChangeListener.class);
+    }
+
+    @Test
+    public void testThrowExceptionOnRegister() {
+        final InstanceIdentifier<Item> instanceIdentifier = InstanceIdentifier.builder(RegisterListenerTest.class)
+            .child(Item.class).build();
+        final DataTreeIdentifier<Item> itemsDataTreeIdentifier = DataTreeIdentifier.create(
+            LogicalDatastoreType.OPERATIONAL, instanceIdentifier);
+
+        doCallRealMethod().when(dataBroker).registerDataListener(any(), any());
+        final var dataListenerException = assertThrows(IllegalArgumentException.class,
+            () -> dataBroker.registerDataListener(itemsDataTreeIdentifier, listener));
+        assertTrue(dataListenerException.getMessage().contains("Cannot register listener for wildcard"));
+
+        doCallRealMethod().when(dataBroker).registerDataChangeListener(any(), any());
+        final var dataListenerChangeException = assertThrows(IllegalArgumentException.class,
+            () -> dataBroker.registerDataChangeListener(itemsDataTreeIdentifier, changeListener));
+        assertTrue(dataListenerChangeException.getMessage().contains("Cannot register listener for wildcard"));
+    }
+
+}
