0429d2ae711a48e9551336f3db4813e2acd9609e
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-04-28 17:59:06 +0200

    Convert mdsal-dom-broker to a JPMS module
    Convert to a JPMS to prevent reflective access.
    For compatibility, we still need to export the implementation details.
    
    JIRA: MDSAL-749
    Change-Id: I85baf0f41c415d02fe2d7b02a70e2b57b502ad9f
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ dom/mdsal-dom-broker/src/main/java/module-info.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+import org.opendaylight.mdsal.dom.api.DOMMountPointService;
+import org.opendaylight.mdsal.dom.broker.DOMMountPointServiceImpl;
+
+module org.opendaylight.mdsal.dom.broker {
+    exports org.opendaylight.mdsal.dom.broker;
+
+    provides DOMMountPointService with DOMMountPointServiceImpl;
+
+    requires transitive org.opendaylight.mdsal.common.api;
+    requires transitive org.opendaylight.mdsal.dom.api;
+    requires transitive org.opendaylight.mdsal.dom.spi;
+    requires com.google.common;
+    requires org.slf4j;
+
+    // Annotations
+    requires static transitive java.annotation;
+    requires static transitive javax.inject;
+    requires static transitive org.kohsuke.metainf_services;
+    requires static transitive org.osgi.service.component.annotations;
+    requires static org.osgi.service.metatype.annotations;
+}
