7934dcdb387b2008241bdeb1afc5e2cd733572a0
Samuel Schneider <samuel.schneider@pantheon.tech>
2023-05-03 17:51:06 +0200

    Convert mdsal-dom-schema-osgi to a JPMS module
    mdsal-binding-dom-codec-osgi is a rather simple artifact.
    Convert it to a JPMS module.
    
    JIRA: MDSAL-748
    Change-Id: I00e2e11047f4580ef7d443c4baefc036a1d0a58e
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ dom/mdsal-dom-schema-osgi/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.mdsal.dom.schema.osgi {
+    exports org.opendaylight.mdsal.dom.schema.osgi;
+
+    requires transitive com.google.common;
+    requires transitive org.opendaylight.mdsal.binding.runtime.api;
+    requires org.opendaylight.mdsal.binding.runtime.spi;
+    requires org.opendaylight.mdsal.dom.spi;
+    requires org.opendaylight.yangtools.yang.binding;
+    requires org.opendaylight.yangtools.yang.model.api;
+    requires org.opendaylight.yangtools.yang.parser.api;
+    requires org.osgi.framework;
+    requires org.osgi.service.component;
+    requires org.osgi.util.tracker;
+    requires org.slf4j;
+
+    // Annotations
+    requires static org.apache.karaf.features.core;
+    requires static org.osgi.service.component.annotations;
+}
