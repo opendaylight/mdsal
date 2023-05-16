0868c31a0d42eb184561dc9a55c1a4d0cd4ca4e5
Robert Varga <robert.varga@pantheon.tech>
2023-05-16 04:12:37 +0200

    Convert rfc6991-ietf-inet-types to explicit JPMS
    Promote the artifact to a full module.
    
    JIRA: MDSAL-826
    Change-Id: I86dca7e40c9403c1771753943bde665d856dbb34
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/ietf/rfc6991-ietf-inet-types/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Binding classes generated for <a href="https://www.rfc-editor.org/rfc/rfc6991#page-15">RFC6991 ietf-inet-types</a>.
+ */
+open module org.opendaylight.yang.gen.ietf.inet.types.rfc6991 {
+    requires transitive com.google.common;
+    requires transitive org.opendaylight.yangtools.yang.binding;
+    requires transitive org.opendaylight.yangtools.yang.common;
+    requires org.opendaylight.mdsal.binding.spec.util;
+    requires org.opendaylight.mdsal.model.ietf.type.util;
+
+    // Annotations
+    requires static transitive java.compiler;
+    requires static transitive java.management;
+}
