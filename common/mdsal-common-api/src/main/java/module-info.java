21b31de0a1c69c2a86e8707e0fbe22371ffb2d05
Robert Varga <robert.varga@pantheon.tech>
2023-06-20 14:42:53 +0200

    Clean up dependencies
    We have a ton of warnings, reign some of them in.
    
    Change-Id: I4aa0b3871f9416c09fb17fe5f9325015231ac40a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/mdsal-common-api/src/main/java/module-info.java
+++ common/mdsal-common-api/src/main/java/module-info.java
@@ -13,5 +13,4 @@ module org.opendaylight.mdsal.common.api {
 
     // Annotations
     requires static transitive org.eclipse.jdt.annotation;
-    requires static com.github.spotbugs.annotations;
 }
