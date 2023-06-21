64a8f0cab9efc1ec22ae66aabce7633e6840451c
Robert Varga <robert.varga@pantheon.tech>
2023-06-21 14:16:30 +0200

    Move BindingReflections.cacheModuleInfos()
    This caching is only used in AbstractSchemaAwareTest, move them there.
    
    JIRA: MDSAL-781
    Change-Id: Id0f9d16f5ec046e7b545486aabbfca7e706b9f2e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/test/AbstractSchemaAwareTest.java
+++ binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/test/AbstractSchemaAwareTest.java
@@ -10,11 +10,14 @@ package org.opendaylight.mdsal.binding.dom.adapter.test;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableSet;
+import java.util.ServiceLoader;
 import java.util.Set;
+import org.eclipse.jdt.annotation.NonNull;
 import org.junit.Before;
 import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeContext;
 import org.opendaylight.mdsal.binding.runtime.spi.BindingRuntimeHelpers;
-import org.opendaylight.mdsal.binding.spec.reflect.BindingReflections;
+import org.opendaylight.yangtools.yang.binding.YangModelBindingProvider;
 import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
@@ -27,6 +30,15 @@ public abstract class AbstractSchemaAwareTest {
                         return BindingRuntimeHelpers.createRuntimeContext(key);
                     }
                 });
+    private static final LoadingCache<ClassLoader, ImmutableSet<YangModuleInfo>> MODULE_INFO_CACHE =
+        CacheBuilder.newBuilder().weakKeys().weakValues().build(
+            new CacheLoader<ClassLoader, ImmutableSet<YangModuleInfo>>() {
+                @Override
+                public ImmutableSet<YangModuleInfo> load(final ClassLoader key) {
+                    return BindingRuntimeHelpers.loadModuleInfos(key);
+                }
+            });
+
 
     @Before
     public final void setup() throws Exception {
@@ -34,7 +46,7 @@ public abstract class AbstractSchemaAwareTest {
     }
 
     protected Set<YangModuleInfo> getModuleInfos() throws Exception {
-        return BindingReflections.cacheModuleInfos(Thread.currentThread().getContextClassLoader());
+        return cacheModuleInfos(Thread.currentThread().getContextClassLoader());
     }
 
     protected BindingRuntimeContext getRuntimeContext() throws Exception {
@@ -56,5 +68,25 @@ public abstract class AbstractSchemaAwareTest {
      */
     protected void setupWithSchema(final EffectiveModelContext context) {
         // No-op
+    }
+
+    /**
+     * Loads {@link YangModuleInfo} instances available on supplied {@link ClassLoader}, assuming the set of available
+     * information does not change. Subsequent accesses may return cached values.
+     *
+     * <p>
+     * {@link YangModuleInfo} are discovered using {@link ServiceLoader} for {@link YangModelBindingProvider}.
+     * {@link YangModelBindingProvider} are simple classes which holds only pointers to actual instance
+     * {@link YangModuleInfo}.
+     *
+     * <p>
+     * When {@link YangModuleInfo} is available, all dependencies are recursively collected into returning set by
+     * collecting results of {@link YangModuleInfo#getImportedModules()}.
+     *
+     * @param loader Class loader for which {@link YangModuleInfo} should be retrieved.
+     * @return Set of {@link YangModuleInfo} available for supplied classloader.
+     */
+    protected final static @NonNull ImmutableSet<YangModuleInfo> cacheModuleInfos(final ClassLoader loader) {
+        return MODULE_INFO_CACHE.getUnchecked(loader);
     }
 }
