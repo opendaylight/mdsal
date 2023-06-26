dd83af740d1f0b306c9dbb0b36bb410fa2ef8866
Robert Varga <robert.varga@pantheon.tech>
2023-06-26 22:42:22 +0200

    WIP: Add codec support for (Normalized)YangData
    // FIXME: codec implementation
    // FIXME: tests
    
    Change-Id: Ibd9109fc7a4a15d05687ae911d2b4890838ea88d
    JIRA: MDSAL-805
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-codec-api/src/main/java/org/opendaylight/mdsal/binding/dom/codec/api/CommonDataObjectCodecTreeNode.java
+++ binding/mdsal-binding-dom-codec-api/src/main/java/org/opendaylight/mdsal/binding/dom/codec/api/CommonDataObjectCodecTreeNode.java
@@ -11,10 +11,8 @@ import com.google.common.annotations.Beta;
 import java.util.List;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
-import org.opendaylight.yangtools.yang.binding.Augmentation;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
 /**
@@ -23,58 +21,7 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
  *
  * @param <T> DataObject type
  */
-public interface CommonDataObjectCodecTreeNode<T extends DataObject>
-        extends BindingDataObjectCodecTreeParent<Empty>, BindingObjectCodecTreeNode<T> {
-    /**
-     * Returns binding class of interface which represents API of current schema node. The result is same as invoking
-     * {@link DataObject#implementedInterface()} on instance of data.
-     *
-     * @return interface which defines API of binding representation of data.
-     */
-    @Override
-    @NonNull Class<T> getBindingClass();
-
-    /**
-     * Returns child context as if it was walked by {@link BindingStreamEventWriter}. This means that to enter case,
-     * one must issue getChild(ChoiceClass).getChild(CaseClass).
-     *
-     * <p>
-     * This method differs from {@link #getStreamChild(Class)}, that is less strict for interfaces representing
-     * augmentation and cases, that may return {@link BindingCodecTreeNode} even if augmentation interface containing
-     * same data was supplied and does not represent augmentation of this node.
-     *
-     * @param childClass Child class by Binding Stream navigation
-     * @return Context of child or {@code null} is supplied class is not applicable in context.
-     * @throws NullPointerException if {@code childClass} is {@code null}
-     */
-    <E extends DataObject> @Nullable CommonDataObjectCodecTreeNode<E> streamChild(@NonNull Class<E> childClass);
-
-    default <A extends Augmentation<?>> @Nullable BindingAugmentationCodecTreeNode<A> streamAugmentation(
-            final @NonNull Class<A> childClass) {
-        final var result = streamChild(childClass);
-        if (result instanceof BindingAugmentationCodecTreeNode) {
-            return (BindingAugmentationCodecTreeNode<A>) result;
-        } else if (result == null) {
-            return null;
-        } else {
-            throw new IllegalArgumentException(
-                "Child " + childClass.getName() + " results in non-Augmentation " + result);
-        }
-    }
-
-    default <E extends DataObject> @Nullable BindingDataObjectCodecTreeNode<E> streamDataObject(
-            final @NonNull Class<E> childClass) {
-        final var result = streamChild(childClass);
-        if (result instanceof BindingDataObjectCodecTreeNode) {
-            return (BindingDataObjectCodecTreeNode<E>) result;
-        } else if (result == null) {
-            return null;
-        } else {
-            throw new IllegalArgumentException(
-                "Child " + childClass.getName() + " results in non-DataObject " + result);
-        }
-    }
-
+public interface CommonDataObjectCodecTreeNode<T extends DataObject> extends BindingDataContainerCodecTreeNode<T> {
     /**
      * Returns nested node context using supplied YANG Instance Identifier.
      *
@@ -126,33 +73,4 @@ public interface CommonDataObjectCodecTreeNode<T extends DataObject>
     @Beta
     InstanceIdentifier.@Nullable PathArgument deserializePathArgument(
             YangInstanceIdentifier.@Nullable PathArgument arg);
-
-    /**
-     * Return a summary of addressability of potential children. Binding specification does not allow all DOM tree
-     * elements to be directly addressed, which means some recursive tree operations, like data tree changes do not
-     * have a one-to-one mapping from DOM to binding in all cases. This method provides an optimization hint to guide
-     * translation of data structures, allowing for fast paths when all children are known to either be addressable
-     * or non-addressable.
-     *
-     * @return Summary children addressability.
-     */
-    @NonNull ChildAddressabilitySummary getChildAddressabilitySummary();
-
-    /**
-     * Enumeration of possible addressability attribute of all children.
-     */
-    enum ChildAddressabilitySummary {
-        /**
-         * All children are addressable.
-         */
-        ADDRESSABLE,
-        /**
-         * All children are non-addressable, including the case when this node does not have any children.
-         */
-        UNADDRESSABLE,
-        /**
-         * Mixed children, some are addressable and some are not.
-         */
-        MIXED
-    }
 }
