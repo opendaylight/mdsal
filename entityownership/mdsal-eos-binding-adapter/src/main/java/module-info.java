0d9e5d72c7c1aba43f608e22ee4e44854adcb2c6
Robert Varga <robert.varga@pantheon.tech>
2023-06-26 13:48:45 +0200

    Do not require spotbugs transitively
    Clean up dependencies, eliminating unused declared dependencies on
    spotbugs-annotations.
    
    Change-Id: Iaa6681c3e53ca8b9bd9e63abecf199b316fa9b4d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- entityownership/mdsal-eos-binding-adapter/src/main/java/module-info.java
+++ entityownership/mdsal-eos-binding-adapter/src/main/java/module-info.java
@@ -15,6 +15,7 @@ module org.opendaylight.mdsal.eos.binding.dom.adapter {
     requires org.slf4j;
 
     // Annotations
+    requires static com.github.spotbugs.annotations;
     requires static javax.inject;
     requires static org.osgi.service.component.annotations;
 }
