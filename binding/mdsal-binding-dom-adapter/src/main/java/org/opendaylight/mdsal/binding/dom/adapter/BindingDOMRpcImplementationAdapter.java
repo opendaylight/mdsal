7175ad4613f0a6dc0f0dbffe4d8004e537dbfa15
Robert Varga <robert.varga@pantheon.tech>
2023-10-27 00:35:19 +0200

    WIP: Remove yang.binding.RpcService
    RpcService is a base marker for generated interfaces representing
    the collection of 'rpc' statements in a module.
    
    This interface and its uses have been deprecated, as their design
    assumptions are based on Java 7, that is do not consider lambdas.
    
    Since we have yang.binding.Rpc and related interfaces to cleanly
    represent RPCs in lambda-cognizant world, remove RpcService and all its
    related codegen/runtime support.
    
    // FIXME: mdsal-binding-dom-adapter tests
    // FIXME: audit second order removals/flattening opportunities
    
    JIRA: MDSAL-772
    Change-Id: I41b3c6513869a43b6373ad588336ebed2dc873d4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMRpcImplementationAdapter.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMRpcImplementationAdapter.java
@@ -7,28 +7,64 @@
  */
 package org.opendaylight.mdsal.binding.dom.adapter;
 
+import static com.google.common.base.Verify.verifyNotNull;
 import static java.util.Objects.requireNonNull;
+import static org.opendaylight.mdsal.binding.dom.adapter.StaticConfiguration.ENABLE_CODEC_SHORTCUT;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingLazyContainerNode;
+import org.opendaylight.mdsal.dom.api.DOMRpcIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMRpcImplementation;
+import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.yangtools.yang.binding.Rpc;
 import org.opendaylight.yangtools.yang.binding.RpcInput;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
 
-final class BindingDOMRpcImplementationAdapter extends AbstractDOMRpcImplementationAdapter {
+final class BindingDOMRpcImplementationAdapter implements DOMRpcImplementation {
+    private final @NonNull AdapterContext adapterContext;
+    private final @NonNull QName rpcName;
     @SuppressWarnings("rawtypes")
     private final @NonNull Rpc delegate;
 
     BindingDOMRpcImplementationAdapter(final AdapterContext adapterContext, final QName rpcName,
             final Rpc<?, ?> delegate) {
-        super(adapterContext, rpcName);
+        this.adapterContext = requireNonNull(adapterContext);
+        this.rpcName = requireNonNull(rpcName);
         this.delegate = requireNonNull(delegate);
     }
 
     @Override
-    @SuppressWarnings("unchecked")
-    ListenableFuture<RpcResult<?>> invokeRpc(final RpcInput input) {
-        return delegate.invoke(input);
+    public long invocationCost() {
+        // Default implementations are 0, we need to perform some translation, hence we have a slightly higher cost
+        return 1;
+    }
+
+    @Override
+    public ListenableFuture<DOMRpcResult> invokeRpc(final DOMRpcIdentifier rpc, final ContainerNode input) {
+        final var serializer = adapterContext.currentSerializer();
+        @SuppressWarnings("unchecked")
+        final var future = delegate.invoke(deserialize(serializer, input));
+        return LazyDOMRpcResultFuture.create(serializer, future);
+    }
+
+    private @NonNull RpcInput deserialize(final @NonNull CurrentAdapterSerializer serializer,
+            final @NonNull ContainerNode input) {
+        if (ENABLE_CODEC_SHORTCUT && input instanceof BindingLazyContainerNode<?> lazy) {
+            return (RpcInput) lazy.getDataObject();
+        }
+
+        final var inputName = input.name().getNodeType();
+        if (!"input".equals(inputName.getLocalName()) || !rpcName.getModule().equals(inputName.getModule())) {
+            throw new IllegalArgumentException("Unexpected RPC " + rpcName + " input " + input.prettyTree());
+        }
+
+        // TODO: this is a bit inefficient: typically we get the same CurrentAdapterSerializer and the path is also
+        //       constant, hence we should be able to cache this lookup and just have the appropriate
+        //       BindingDataObjectCodecTreeNode and reuse it directly
+        // FIXME: should be a guaranteed return, as input is @NonNull
+        return verifyNotNull((RpcInput) serializer.fromNormalizedNodeRpcData(Absolute.of(rpcName, inputName), input));
     }
 }
