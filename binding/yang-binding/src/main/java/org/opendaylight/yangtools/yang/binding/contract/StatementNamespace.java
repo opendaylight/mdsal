de4be4e737ebc444723f9fa255c1e39acd112a07
Robert Varga <robert.varga@pantheon.tech>
2023-11-14 15:17:12 +0100

    Move StatementNamespace to yang.binding.contract
    This enumeration forms a part of the binding naming contract, move it to
    yang-binding.
    
    Change-Id: I8fe170d5791ff36c5c1728b0ea7112b178e3d808
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/contract/StatementNamespace.java
+++ binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/contract/StatementNamespace.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.mdsal.binding.generator.impl.reactor;
+package org.opendaylight.yangtools.yang.binding.contract;
 
 import static com.google.common.base.Verify.verify;
 import static java.util.Objects.requireNonNull;
@@ -15,8 +15,7 @@ import org.eclipse.jdt.annotation.NonNull;
 /**
  * <a href="https://www.rfc-editor.org/rfc/rfc6020#section-6.2.1">YANG statement namespaces</a> which we process.
  */
-// FIXME: move this to 'BindingNamespace' in binding-spec-util
-enum StatementNamespace {
+public enum StatementNamespace {
     /**
      * The namespace of all {@code feature} statements, bullet 3.
      */
@@ -101,11 +100,11 @@ enum StatementNamespace {
         this.resistant = resistant;
     }
 
-    @NonNull String suffix() {
+    public @NonNull String suffix() {
         return suffix;
     }
 
-    boolean resistant() {
+    public boolean resistant() {
         return resistant;
     }
 }
