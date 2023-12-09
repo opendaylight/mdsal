ac89f61e39c8883ca0c586f52ca482aaaac1e7d9
Robert Varga <robert.varga@pantheon.tech>
2023-12-09 06:49:20 +0100

    Remove GenericEntityOwnershipService
    GenericEntityOwnershipService acts as a straight jacket for
    EntityOwnershipService and DOMEntityOwnershipService, which end up
    overriding its methods.
    
    Opt to duplicate the API contracts rather than having this generic
    weirdness.
    
    JIRA: MDSAL-843
    Change-Id: I867162f8da643b0163fb20bd4de3f8e790e0d4f9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- entityownership/mdsal-eos-binding-api/src/main/java/org/opendaylight/mdsal/eos/binding/api/EntityOwnershipService.java
+++ entityownership/mdsal-eos-binding-api/src/main/java/org/opendaylight/mdsal/eos/binding/api/EntityOwnershipService.java
@@ -9,28 +9,65 @@ package org.opendaylight.mdsal.eos.binding.api;
 
 import com.google.common.annotations.Beta;
 import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.mdsal.eos.common.api.CandidateAlreadyRegisteredException;
 import org.opendaylight.mdsal.eos.common.api.EntityOwnershipState;
-import org.opendaylight.mdsal.eos.common.api.GenericEntityOwnershipService;
 import org.opendaylight.yangtools.concepts.Registration;
 
 /**
- * Binding version of {@link GenericEntityOwnershipService}.
+ * An interface that provides the means for a component/application to request ownership for a given {@link Entity} on
+ * the current cluster member. Entity ownership is always tied to a process and two components on the same process
+ * cannot register a candidate for a given Entity.
+ *
+ * <p>
+ * A component/application may also register interest in the ownership status of an Entity. The listener would be
+ * notified whenever the ownership status changes.
  *
  * @author Thomas Pantelis
  */
 @Beta
-public interface EntityOwnershipService extends GenericEntityOwnershipService<Entity, EntityOwnershipListener> {
-
-    @Override
+@NonNullByDefault
+public interface EntityOwnershipService {
+    /**
+     * Registers a candidate for ownership of the given entity. Only one such request can be made per entity per
+     * process. If multiple requests for registering a candidate for a given entity are received in the current process
+     * a CandidateAlreadyRegisteredException will be thrown.
+     *
+     * <p>
+     * The registration is performed asynchronously and any registered entity ownership listener is notified of
+     * ownership status changes for the entity.
+     *
+     * @param entity the entity which the Candidate wants to own
+     * @return a registration object that can be used to unregister the Candidate
+     * @throws CandidateAlreadyRegisteredException if the candidate was already registered
+     */
     Registration registerCandidate(Entity entity) throws CandidateAlreadyRegisteredException;
 
-    @Override
+    /**
+     * Registers a listener that is interested in ownership changes for entities of the given entity type. The listener
+     * is notified whenever its process instance is granted ownership of the entity and also whenever it loses
+     * ownership. On registration the listener will be notified of all entities its process instance currently owns at
+     * the time of registration.
+     *
+     * @param entityType the type of entities whose ownership status the Listener is interested in
+     * @param listener the listener that is interested in the entities
+     * @return a registration object that can be used to unregister the Listener
+     */
     Registration registerListener(String entityType, EntityOwnershipListener listener);
 
-    @Override
+    /**
+     * Gets the current ownership state information for an entity.
+     *
+     * @param forEntity the entity to query.
+     * @return an Optional EntityOwnershipState whose instance is present if the entity is found
+     */
     Optional<EntityOwnershipState> getOwnershipState(Entity forEntity);
 
-    @Override
+    /**
+     * Checks if a local candidate is registered for the given entity.
+     *
+     * @param forEntity the entity to query.
+     * @return true if a candidate is registered locally, false otherwise
+     */
     boolean isCandidateRegistered(Entity forEntity);
 }
