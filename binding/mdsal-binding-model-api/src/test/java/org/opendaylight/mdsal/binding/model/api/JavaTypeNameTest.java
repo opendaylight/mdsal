dc96f0e0c1e10ac8ffa7247476f4107e7de944a6
Robert Varga <robert.varga@pantheon.tech>
2023-12-17 18:40:52 +0100

    Make JavaTypeName
    The components of JavaTypeName are quite Comparable. Define a total
    order based on the components.
    
    Change-Id: I17681b49319b4a32ff66ee33f71a1b1d84fa8516
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-model-api/src/test/java/org/opendaylight/mdsal/binding/model/api/JavaTypeNameTest.java
+++ binding/mdsal-binding-model-api/src/test/java/org/opendaylight/mdsal/binding/model/api/JavaTypeNameTest.java
@@ -7,42 +7,42 @@
  */
 package org.opendaylight.mdsal.binding.model.api;
 
-import static com.google.common.collect.ImmutableList.of;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static java.util.List.of;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Optional;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class JavaTypeNameTest {
-
+class JavaTypeNameTest {
     @Test
-    public void testOperations() {
-        final JavaTypeName byteName = JavaTypeName.create(byte.class);
+    void testOperations() {
+        final var byteName = JavaTypeName.create(byte.class);
         assertEquals("", byteName.packageName());
         assertEquals("byte", byteName.simpleName());
         assertEquals("byte", byteName.toString());
-        assertEquals(Optional.empty(), byteName.immediatelyEnclosingClass());
+        assertNull(byteName.immediatelyEnclosingClass());
         assertSame(byteName, byteName.topLevelClass());
         assertEquals(of("byte"), byteName.localNameComponents());
         assertEquals("byte", byteName.localName());
 
-        final JavaTypeName charName = byteName.createSibling("char");
+        final var charName = byteName.createSibling("char");
         assertEquals("", charName.packageName());
         assertEquals("char", charName.simpleName());
         assertEquals("char", charName.toString());
-        assertEquals(Optional.empty(), charName.immediatelyEnclosingClass());
+        assertNull(charName.immediatelyEnclosingClass());
         assertSame(charName, charName.topLevelClass());
         assertEquals(of("char"), charName.localNameComponents());
         assertEquals("char", charName.localName());
 
-        final JavaTypeName threadName = JavaTypeName.create(Thread.class);
+        final var threadName = JavaTypeName.create(Thread.class);
         assertEquals("java.lang", threadName.packageName());
         assertEquals("Thread", threadName.simpleName());
         assertEquals("java.lang.Thread", threadName.toString());
-        assertEquals(Optional.empty(), threadName.immediatelyEnclosingClass());
+        assertNull(threadName.immediatelyEnclosingClass());
         assertTrue(threadName.canCreateEnclosed("Foo"));
         assertFalse(threadName.canCreateEnclosed("Thread"));
         assertEquals(threadName, JavaTypeName.create("java.lang", "Thread"));
@@ -50,36 +50,36 @@ public class JavaTypeNameTest {
         assertEquals(of("Thread"), threadName.localNameComponents());
         assertEquals("Thread", threadName.localName());
 
-        final JavaTypeName stringName = threadName.createSibling("String");
+        final var stringName = threadName.createSibling("String");
         assertEquals("java.lang", stringName.packageName());
         assertEquals("String", stringName.simpleName());
         assertEquals("java.lang.String", stringName.toString());
-        assertEquals(Optional.empty(), stringName.immediatelyEnclosingClass());
+        assertNull(stringName.immediatelyEnclosingClass());
         assertEquals(stringName, JavaTypeName.create("java.lang", "String"));
 
-        final JavaTypeName enclosedName = threadName.createEnclosed("Foo");
+        final var enclosedName = threadName.createEnclosed("Foo");
         assertEquals("java.lang", enclosedName.packageName());
         assertEquals("Foo", enclosedName.simpleName());
         assertEquals("java.lang.Thread.Foo", enclosedName.toString());
-        assertEquals(Optional.of(threadName), enclosedName.immediatelyEnclosingClass());
+        assertEquals(threadName, enclosedName.immediatelyEnclosingClass());
         assertSame(threadName, enclosedName.topLevelClass());
         assertEquals(of("Thread", "Foo"), enclosedName.localNameComponents());
         assertEquals("Thread.Foo", enclosedName.localName());
 
-        final JavaTypeName uehName = JavaTypeName.create(Thread.UncaughtExceptionHandler.class);
+        final var uehName = JavaTypeName.create(Thread.UncaughtExceptionHandler.class);
         assertEquals("java.lang", uehName.packageName());
         assertEquals("UncaughtExceptionHandler", uehName.simpleName());
         assertEquals("java.lang.Thread.UncaughtExceptionHandler", uehName.toString());
-        assertEquals(Optional.of(threadName), uehName.immediatelyEnclosingClass());
+        assertEquals(threadName, uehName.immediatelyEnclosingClass());
         assertTrue(uehName.canCreateEnclosed("Foo"));
         assertFalse(uehName.canCreateEnclosed("Thread"));
         assertFalse(uehName.canCreateEnclosed("UncaughtExceptionHandler"));
 
-        final JavaTypeName siblingName = uehName.createSibling("Foo");
+        final var siblingName = uehName.createSibling("Foo");
         assertEquals("java.lang", siblingName.packageName());
         assertEquals("Foo", siblingName.simpleName());
         assertEquals("java.lang.Thread.Foo", siblingName.toString());
-        assertEquals(Optional.of(threadName), siblingName.immediatelyEnclosingClass());
+        assertEquals(threadName, siblingName.immediatelyEnclosingClass());
         assertTrue(siblingName.canCreateEnclosed("UncaughtExceptionHandler"));
         assertFalse(siblingName.canCreateEnclosed("Thread"));
         assertFalse(siblingName.canCreateEnclosed("Foo"));
@@ -90,23 +90,125 @@ public class JavaTypeNameTest {
         assertFalse(threadName.equals("foo"));
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testCreateEmptyPackage() {
-        JavaTypeName.create("", "Foo");
+    @Test
+    void testCreateEmptyPackage() {
+        assertThrows(IllegalArgumentException.class, () -> JavaTypeName.create("", "Foo"));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testCreateEmptyName() {
-        JavaTypeName.create("foo", "");
+        assertThrows(IllegalArgumentException.class, () -> JavaTypeName.create("foo", ""));
     }
 
-    @Test(expected = UnsupportedOperationException.class)
-    public void testCanCreateEnclosedPrimitive() {
-        JavaTypeName.create(byte.class).canCreateEnclosed("foo");
+    @Test
+    void testCanCreateEnclosedPrimitive() {
+        assertThrows(UnsupportedOperationException.class,
+            () -> JavaTypeName.create(byte.class).canCreateEnclosed("foo"));
     }
 
-    @Test(expected = UnsupportedOperationException.class)
-    public void testCreateEnclosedPrimitive() {
-        JavaTypeName.create(byte.class).createEnclosed("foo");
+    @Test
+    void testCreateEnclosedPrimitive() {
+        assertThrows(UnsupportedOperationException.class,
+            () -> JavaTypeName.create(byte.class).createEnclosed("foo"));
+    }
+
+    @Test
+    void testCompareTo() {
+        final var byteName = JavaTypeName.create(byte.class);
+        final var charName = byteName.createSibling("char");
+        final var threadName = JavaTypeName.create(Thread.class);
+        final var stringName = threadName.createSibling("String");
+        final var enclosedName = threadName.createEnclosed("Foo");
+        final var uehName = JavaTypeName.create(Thread.UncaughtExceptionHandler.class);
+        final var siblingName = uehName.createSibling("Bar");
+        final var otherStringName = JavaTypeName.create("java.util", "String");
+
+        assertEqualTo(byteName, byteName);
+        assertLessThan(byteName, charName);
+        assertGreaterThan(byteName, threadName);
+        assertGreaterThan(byteName, stringName);
+        assertGreaterThan(byteName, enclosedName);
+        assertGreaterThan(byteName, uehName);
+        assertGreaterThan(byteName, siblingName);
+        assertGreaterThan(byteName, otherStringName);
+
+        assertGreaterThan(charName, byteName);
+        assertEqualTo(charName, charName);
+        assertGreaterThan(charName, threadName);
+        assertGreaterThan(charName, stringName);
+        assertGreaterThan(charName, enclosedName);
+        assertGreaterThan(charName, uehName);
+        assertGreaterThan(charName, siblingName);
+        assertGreaterThan(charName, otherStringName);
+
+        assertLessThan(threadName, byteName);
+        assertLessThan(threadName, charName);
+        assertEqualTo(threadName, threadName);
+        assertGreaterThan(threadName, stringName);
+        assertGreaterThan(threadName, enclosedName);
+        assertLessThan(threadName, uehName);
+        assertGreaterThan(threadName, siblingName);
+        assertGreaterThan(threadName, otherStringName);
+
+        assertLessThan(stringName, byteName);
+        assertLessThan(stringName, charName);
+        assertLessThan(stringName, threadName);
+        assertEqualTo(stringName, stringName);
+        assertGreaterThan(stringName, enclosedName);
+        assertLessThan(stringName, uehName);
+        assertGreaterThan(stringName, siblingName);
+        assertLessThan(stringName, otherStringName);
+
+        assertLessThan(enclosedName, byteName);
+        assertLessThan(enclosedName, charName);
+        assertLessThan(enclosedName, threadName);
+        assertLessThan(enclosedName, stringName);
+        assertEqualTo(enclosedName, enclosedName);
+        assertLessThan(enclosedName, uehName);
+        assertGreaterThan(enclosedName, siblingName);
+        assertLessThan(enclosedName, otherStringName);
+
+        assertLessThan(uehName, byteName);
+        assertLessThan(uehName, charName);
+        assertGreaterThan(uehName, threadName);
+        assertGreaterThan(uehName, stringName);
+        assertGreaterThan(uehName, enclosedName);
+        assertEqualTo(uehName, JavaTypeName.create(Thread.UncaughtExceptionHandler.class));
+        assertGreaterThan(uehName, siblingName);
+        assertGreaterThan(uehName, otherStringName);
+
+        assertLessThan(siblingName, byteName);
+        assertLessThan(siblingName, charName);
+        assertLessThan(siblingName, threadName);
+        assertLessThan(siblingName, stringName);
+        assertLessThan(siblingName, enclosedName);
+        assertLessThan(siblingName, uehName);
+        assertEqualTo(siblingName, siblingName);
+        assertLessThan(siblingName, otherStringName);
+
+        assertLessThan(otherStringName, byteName);
+        assertLessThan(otherStringName, charName);
+        assertLessThan(otherStringName, threadName);
+        assertGreaterThan(otherStringName, stringName);
+        assertGreaterThan(otherStringName, enclosedName);
+        assertLessThan(otherStringName, uehName);
+        assertGreaterThan(otherStringName, siblingName);
+        assertEqualTo(otherStringName, otherStringName);
+    }
+
+    private static void assertLessThan(final JavaTypeName receiver, final JavaTypeName other) {
+        assertTrue(assertCompareTo(receiver, other) < 0);
+    }
+
+    private static void assertGreaterThan(final JavaTypeName receiver, final JavaTypeName other) {
+        assertTrue(assertCompareTo(receiver, other) > 0);
+    }
+
+    private static void assertEqualTo(final JavaTypeName receiver, final JavaTypeName other) {
+        assertEquals(0, assertCompareTo(receiver, other));
+    }
+
+    private static int assertCompareTo(final JavaTypeName receiver, final JavaTypeName other) {
+        return receiver.compareTo(other);
     }
 }
