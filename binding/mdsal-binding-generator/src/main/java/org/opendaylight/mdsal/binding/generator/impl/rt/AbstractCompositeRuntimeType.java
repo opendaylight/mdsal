ee905ec0e2a2307903ef0609bb8feda931e7c89d
Robert Varga <robert.varga@pantheon.tech>
2023-12-17 17:40:01 +0100

    WIP: Add ArrayIndex
    Change-Id: Ifb954ea61a7b73736dfa31ef39fc8b5905529a23
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/rt/AbstractCompositeRuntimeType.java
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/rt/AbstractCompositeRuntimeType.java
@@ -8,13 +8,10 @@
 package org.opendaylight.mdsal.binding.generator.impl.rt;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.Objects.requireNonNullElse;
 
 import com.google.common.base.Functions;
 import com.google.common.base.VerifyException;
 import com.google.common.collect.ImmutableMap;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.model.api.GeneratedType;
@@ -28,10 +25,40 @@ import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStateme
 
 abstract class AbstractCompositeRuntimeType<S extends EffectiveStatement<?, ?>>
         extends AbstractRuntimeType<S, GeneratedType> implements CompositeRuntimeType {
-    private static final RuntimeType[] EMPTY = new RuntimeType[0];
+    private static final ArrayIndex<QName, RuntimeType> SCHEMA_TREE_INDEX =
+        new ArrayIndex<>(RuntimeType.class, RuntimeType[].class) {
+            private static final RuntimeType[] EMPTY = new RuntimeType[0];
+
+            @Override
+            RuntimeType[] emptyArray() {
+                return EMPTY;
+            }
+
+            @Override
+            RuntimeType[] newArray(final int length) {
+                return new RuntimeType[length];
+            }
+
+            int compareValue(final RuntimeType obj, final QName key) {
+                return key.compareTo(extractQName(obj));
+            }
+
+            @Override
+            int compareValues(final RuntimeType o1, final RuntimeType o2) {
+                return compareValue(o1, extractQName(o2));
+            }
+
+            private static @NonNull QName extractQName(final RuntimeType type) {
+                final var stmt = type.statement();
+                if (stmt instanceof SchemaTreeEffectiveStatement<?> schemaTreeStmt) {
+                    return schemaTreeStmt.argument();
+                }
+                throw new VerifyException("Unexpected statement " + stmt + " in " + type);
+            }
+        };
 
     private final ImmutableMap<JavaTypeName, GeneratedRuntimeType> byClass;
-    private final Object bySchemaTree;
+    private final @NonNull Object bySchemaTree;
 
     AbstractCompositeRuntimeType(final GeneratedType bindingType, final S statement, final List<RuntimeType> children) {
         super(bindingType, statement);
@@ -41,40 +68,13 @@ abstract class AbstractCompositeRuntimeType<S extends EffectiveStatement<?, ?>>
             .map(GeneratedRuntimeType.class::cast)
             .collect(ImmutableMap.toImmutableMap(GeneratedRuntimeType::getIdentifier, Functions.identity()));
 
-        final var tmp = children.stream()
-            .filter(child -> child.statement() instanceof SchemaTreeEffectiveStatement)
-            .toArray(RuntimeType[]::new);
-        bySchemaTree = switch (tmp.length) {
-            case 0 -> EMPTY;
-            case 1 -> tmp[0];
-            default -> {
-                Arrays.sort(tmp, (o1, o2) -> {
-                    final int cmp = extractQName(o1).compareTo(extractQName(o2));
-                    if (cmp == 0) {
-                        throw new VerifyException("Type " + o1 + " conflicts with " + o2 + " on schema tree");
-                    }
-                    return cmp;
-                });
-                yield tmp;
-            }
-        };
+        bySchemaTree = SCHEMA_TREE_INDEX.index(children.stream()
+            .filter(child -> child.statement() instanceof SchemaTreeEffectiveStatement));
     }
 
     @Override
     public final RuntimeType schemaTreeChild(final QName qname) {
-        if (bySchemaTree instanceof RuntimeType tmp) {
-            return qname.equals(tmp.statement().argument()) ? tmp : null;
-        }
-
-        final var tmp = (RuntimeType[]) bySchemaTree;
-        // Here we are assuming that Arrays.binarySearch() accepts a null object, so as to help CHA by not introducing
-        // a fake RuntimeType class and the corresponding instanceof checks to side-step the statement lookup (which
-        // would need more faking).
-        // We make a slight assumption that o2 is the what we specify as a key, but can recover if it is the other way
-        // around.
-        final int offset = Arrays.binarySearch(tmp, null,
-            (o1, o2) -> extractQName(requireNonNullElse(o1, o2)).compareTo(qname));
-        return offset < 0 ? null : tmp[offset];
+        return SCHEMA_TREE_INDEX.lookup(bySchemaTree, qname);
     }
 
     @Override
@@ -82,27 +82,7 @@ abstract class AbstractCompositeRuntimeType<S extends EffectiveStatement<?, ?>>
         return byClass.get(requireNonNull(typeName));
     }
 
-    // Makes an assertion of all types being of specified type
-    @SuppressWarnings("unchecked")
     final <T extends RuntimeType> @NonNull List<T> schemaTree(final Class<T> expectedType) {
-        if (expectedType.isInstance(bySchemaTree)) {
-            return List.of(expectedType.cast(bySchemaTree));
-        }
-
-        final var tmp = (RuntimeType[]) bySchemaTree;
-        for (var item : tmp) {
-            if (!expectedType.isInstance(item)) {
-                throw new VerifyException("Unexpected schema tree child " + item);
-            }
-        }
-        return (List<T>) Collections.unmodifiableList(Arrays.asList(tmp));
-    }
-
-    private static @NonNull QName extractQName(final RuntimeType type) {
-        final var stmt = type.statement();
-        if (stmt instanceof SchemaTreeEffectiveStatement<?> schemaTreeStmt) {
-            return schemaTreeStmt.argument();
-        }
-        throw new VerifyException("Unexpected statement " + stmt + " in " + type);
+        return SCHEMA_TREE_INDEX.toList(expectedType, bySchemaTree);
     }
 }
---
+++ binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/rt/ArrayIndex.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.generator.impl.rt;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.Objects.requireNonNullElse;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Support for indexing a set of non-null objects and efficiently look them up. This essentially boils down to:
+ * <ul>
+ *   <li>{@link #index(Stream)}ing, which results in an opaque non-null state</li>
+ *   <li>{@link #lookup(Object, Comparable)}ing up an object in provided state</li>
+ *   <li>turning provided state {@link #toList(Class, Object)} of objects of expected type</li>
+ * </ul>
+ */
+abstract class ArrayIndex<K extends Comparable<K>, V> {
+    private final Class<V[]> arrayType;
+    private final Class<V> valueType;
+
+    ArrayIndex(final Class<V> valueType, final Class<V[]> arrayType) {
+        this.valueType = requireNonNull(valueType);
+        this.arrayType = requireNonNull(arrayType);
+    }
+
+    final @NonNull Object index(final Stream<? extends V> values) {
+        final var tmp = values
+            .map(Objects::requireNonNull)
+            .toArray(length -> length == 0 ? emptyArray() : newArray(length));
+        return switch (tmp.length) {
+            case 0 -> tmp;
+            case 1 -> requireNonNull(tmp[0]);
+            default -> {
+                Arrays.sort(tmp, (o1, o2) -> {
+                    final int cmp = compareValues(o1, o2);
+                    if (cmp == 0) {
+                        throw new IllegalArgumentException("Type " + o1 + " conflicts with " + o2);
+                    }
+                    return cmp;
+                });
+
+                yield tmp;
+            }
+        };
+    }
+
+    final @Nullable V lookup(final @NonNull Object values, final K key) {
+        final var k = requireNonNull(key);
+        if (arrayType.isInstance(values)) {
+            return lookup(arrayType.cast(values), k);
+        }
+        final var value = requireNonNull(valueType.cast(values));
+        return compareValue(value, k) == 0 ? value : null;
+    }
+
+    private @Nullable V lookup(final V[] array, final K key) {
+        final var k = requireNonNull(key);
+        if (array.length == 0) {
+            return null;
+        }
+        // Here we are assuming that Arrays.binarySearch() accepts a null object, so as to help CHA by not introducing
+        // a fake RuntimeType class and the corresponding instanceof checks to side-step the statement lookup (which
+        // would need more faking).
+        // We make a slight assumption that o2 is the what we specify as a key, but can recover if it is the other way
+        // around.
+        final var offset = Arrays.binarySearch(array, null, (o1, o2) -> compareValue(requireNonNullElse(o1, o2), k));
+        return offset < 0 ? null : array[offset];
+    }
+
+    final <T extends V> @NonNull List<T> toList(final Class<T> type, final @NonNull Object values) {
+        return arrayType.isInstance(values) ? toList(type, arrayType.cast(values))
+            : List.of(type.cast(values));
+    }
+
+    // Unchecked cast is a false positive: we check all items
+    @SuppressWarnings("unchecked")
+    private <T extends V> @NonNull List<T> toList(final Class<T> type, final V[] array) {
+        if (array.length == 0) {
+            return List.of();
+        }
+        for (var item : array) {
+            type.cast(requireNonNull(item));
+        }
+        return (List<T>) Collections.unmodifiableList(Arrays.asList(array));
+    }
+
+    abstract V[] emptyArray();
+
+    abstract V[] newArray(int length);
+
+    abstract int compareValue(@NonNull V obj, @NonNull K key);
+
+    abstract int compareValues(@NonNull V o1, @NonNull V o2y);
+}
