730b24538dfa113ea18ab874d932d5726aef96ce
Robert Varga <robert.varga@pantheon.tech>
2024-01-13 20:29:25 +0100

    CommitInfo forward compatibility
    This patch refactors EmptyCommitInfo to implement the future CommitInfo
    interaface -- carrying uuid/time information and being Serializable.
    
    JIRA: YANGTOOLS-1554
    Change-Id: Ibe5070b17fe50fc8aa7f75a7483aa9f0e9ef5139
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/mdsal-common-api/src/main/java/org/opendaylight/mdsal/common/api/CI.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.common.api;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.util.concurrent.FluentFuture;
+import com.google.common.util.concurrent.Futures;
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * Default {@link CommitInfo} implementation.
+ */
+record CI(UUID uuid, Instant instant) implements CommitInfo, Serializable {
+    @java.io.Serial
+    private static final long serialVersionUID = 0L;
+
+    static final @NonNull CI EMPTY = new CI(null, null);
+    static final @NonNull FluentFuture<@NonNull CommitInfo> EMPTY_FUTURE =
+        FluentFuture.from(Futures.immediateFuture(EMPTY));
+
+    static @NonNull CI of(final UUID uuid, final Instant time) {
+        return time != null || uuid != null ? new CI(uuid, time) : EMPTY;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(CommitInfo.class).omitNullValues()
+            .add("uuid", uuid)
+            .add("instant", instant)
+            .toString();
+    }
+
+    @java.io.Serial
+    private Object readResolve() throws ObjectStreamException {
+        return instant != null || uuid != null ? this : EMPTY;
+    }
+}
