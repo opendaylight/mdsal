97e142cd4bbd6f44ea5857afb8ca044bebcdcab7
Robert Varga <robert.varga@pantheon.tech>
2024-05-28 20:47:49 +0200

    Eliminate @SuppressFBWarnings in mdsal-dom-broker
    We are getting warnings about an unchecked cast due to erasure. Provide
    an explicit override method in ransactionCommitFailedExceptionMapper to
    carry this specialization.
    
    Change-Id: I7e17e3c3c3892e8c374b6a5ef9bab98526aea75c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-broker/src/main/java/module-info.java
+++ dom/mdsal-dom-broker/src/main/java/module-info.java
@@ -22,7 +22,6 @@ module org.opendaylight.mdsal.dom.broker {
     // Annotations
     requires static transitive java.annotation;
     requires static transitive javax.inject;
-    requires static com.github.spotbugs.annotations;
     requires static org.eclipse.jdt.annotation;
     requires static org.kohsuke.metainf_services;
     requires static org.osgi.service.component.annotations;
