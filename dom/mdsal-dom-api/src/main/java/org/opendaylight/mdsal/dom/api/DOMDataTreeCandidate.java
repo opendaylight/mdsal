96b28518d0cdced86fc7ba8098d18c69a0cc07db
Robert Varga <robert.varga@pantheon.tech>
2024-07-07 17:01:56 +0200

    WIP: Expose CandidateNode in DOMDataTreeCandidate
    Provide compatibility via exposed tree node.
    
    // FIXME: Migrate callers to use root() instead
    // FIXME: JIRA issue
    
    Change-Id: I29b2a484a6341875a252a0a4f6270262dddcb46d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeCandidate.java
+++ dom/mdsal-dom-api/src/main/java/org/opendaylight/mdsal/dom/api/DOMDataTreeCandidate.java
@@ -7,15 +7,24 @@
  */
 package org.opendaylight.mdsal.dom.api;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.data.tree.api.DataTreeCandidate.CandidateNode;
 import org.opendaylight.yangtools.yang.data.tree.api.DataTreeCandidateNode;
 
 /**
- * An encapsulation of a data tree modification. This candidate is ready for atomic commit to the
- * data broker. It allows access to before- and after-state as it will be seen in to subsequent
- * commit. This capture can be accessed for reference, but cannot be modified and the content is
- * limited to nodes which were affected by the modification from which this instance originated.
+ * An encapsulation of a data tree modification. This candidate is ready for atomic commit to the data broker. It allows
+ * access to before- and after-state as it will be seen in to subsequent commit. This capture can be accessed for
+ * reference, but cannot be modified and the content is limited to nodes which were affected by the modification from
+ * which this instance originated.
  */
+@NonNullByDefault
 public interface DOMDataTreeCandidate {
+    /**
+     * Get the root {@link CandidateNode}.
+     *
+     * @return the root {@link CandidateNode}
+     */
+    CandidateNode root();
 
     /**
      * Get the candidate tree root path. This is the path of the root node relative to the root of
@@ -29,6 +38,11 @@ public interface DOMDataTreeCandidate {
      * Get the candidate tree root node.
      *
      * @return Candidate tree root node
+     * @deprecated Use {@link #root()} with
+     *             <a href="https://openjdk.org/jeps/441">JEP 441: Pattern Matching for switch</a> instead.
      */
-    DataTreeCandidateNode getRootNode();
+    @Deprecated(since = "14.0.0")
+    default DataTreeCandidateNode getRootNode() {
+        return root().toLegacy();
+    }
 }
