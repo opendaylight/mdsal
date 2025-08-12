8fb9718f1451417982c5365cf0c36e3f34c7ddb9
Robert Varga <robert.varga@pantheon.tech>
2025-08-12 09:08:24 +0200

    Add NotificationPublishService.DemandMonitor
    The DOMNotificationPublishService's DemandListener extension is not
    exposed via binding-api.
    
    This patch fixes that omission by exposint
    NotificationPublishService.DemandMonitor along with register method.
    
    JIRA: MDSAL-881
    Change-Id: Ib66f463dc78f8440fadab98371dc8666ad990fa8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMNotificationPublishServiceAdapter.java
+++ binding/mdsal-binding-dom-adapter/src/main/java/org/opendaylight/mdsal/binding/dom/adapter/BindingDOMNotificationPublishServiceAdapter.java
@@ -7,6 +7,8 @@
  */
 package org.opendaylight.mdsal.binding.dom.adapter;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ClassToInstanceMap;
 import com.google.common.collect.ImmutableSet;
@@ -14,21 +16,36 @@ import com.google.common.util.concurrent.ListenableFuture;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 import org.opendaylight.mdsal.binding.dom.adapter.BindingDOMAdapterBuilder.Factory;
 import org.opendaylight.mdsal.dom.api.DOMNotification;
+import org.opendaylight.mdsal.dom.api.DOMNotificationPublishDemandExtension;
+import org.opendaylight.mdsal.dom.api.DOMNotificationPublishDemandExtension.DemandListener;
 import org.opendaylight.mdsal.dom.api.DOMNotificationPublishService;
 import org.opendaylight.mdsal.dom.api.DOMService;
+import org.opendaylight.yangtools.binding.DataObject;
 import org.opendaylight.yangtools.binding.Notification;
+import org.opendaylight.yangtools.binding.reflect.BindingReflections;
+import org.opendaylight.yangtools.concepts.AbstractRegistration;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
 
 @VisibleForTesting
 public final class BindingDOMNotificationPublishServiceAdapter
         extends AbstractBindingAdapter<DOMNotificationPublishService> implements NotificationPublishService {
     static final Factory<NotificationPublishService> BUILDER_FACTORY = Builder::new;
 
+    private final OnDemandSupport onDemandSupport;
+
     public BindingDOMNotificationPublishServiceAdapter(final AdapterContext adapterContext,
             final DOMNotificationPublishService domPublishService) {
         super(adapterContext, domPublishService);
+
+        final var demandExt = domPublishService.extension(DOMNotificationPublishDemandExtension.class);
+        onDemandSupport = demandExt == null ? null : new OnDemandSupport(demandExt);
+
     }
 
     @Override
@@ -46,6 +63,15 @@ public final class BindingDOMNotificationPublishServiceAdapter
             final TimeUnit unit) throws InterruptedException {
         return toBindingResult(getDelegate().offerNotification(toDomNotification(notification), timeout, unit));
     }
+
+    @Override
+    @NonNullByDefault
+    public <N extends Notification<N> & DataObject> Registration registerDemandMonitor(final Class<N> type,
+            final DemandMonitor monitor) {
+        return onDemandSupport == null ? NotificationPublishService.super.registerDemandMonitor(type, monitor)
+            : onDemandSupport.registerDemandMonitor(requireNonNull(type), requireNonNull(monitor));
+    }
+
 
     private @NonNull DOMNotification toDomNotification(final Notification<?> notification) {
         return new LazySerializedNotification(currentSerializer(), notification);
@@ -71,6 +97,73 @@ public final class BindingDOMNotificationPublishServiceAdapter
         protected NotificationPublishService createInstance(final ClassToInstanceMap<DOMService<?, ?>> delegates) {
             return new BindingDOMNotificationPublishServiceAdapter(adapterContext(),
                 delegates.getInstance(DOMNotificationPublishService.class));
+        }
+    }
+
+    @NonNullByDefault
+    private static final class OnDemandSupport {
+        private final DOMNotificationPublishDemandExtension demandExt;
+
+        OnDemandSupport(final DOMNotificationPublishDemandExtension demandExt) {
+            this.demandExt = requireNonNull(demandExt);
+        }
+
+        <N extends Notification<N> & DataObject> Registration registerDemandMonitor(final Class<N> type,
+                final DemandMonitor monitor) {
+            // FIXME: do not use BindingReflections here
+            final var qname = BindingReflections.findQName(type);
+            if (qname == null) {
+                throw new IllegalArgumentException("Cannot find QName of " + type);
+            }
+            return new MonitorDemandListener(qname, monitor, demandExt);
+        }
+    }
+
+    private static final class MonitorDemandListener extends AbstractRegistration implements DemandListener {
+        private final Absolute type;
+        private final DemandMonitor monitor;
+        private final Registration domReg;
+
+        private Registration monitorReg;
+
+        @NonNullByDefault
+        MonitorDemandListener(final QName type, final DemandMonitor monitor,
+                final DOMNotificationPublishDemandExtension demandExt) {
+            this.type = Absolute.of(type);
+            this.monitor = requireNonNull(monitor);
+            domReg = demandExt.registerDemandListener(this);
+        }
+
+        @Override
+        public synchronized void onDemandUpdated(final ImmutableSet<Absolute> neededTypes) {
+            if (neededTypes.contains(type)) {
+                becomeActive();
+            } else {
+                becomeInactive();
+            }
+        }
+
+        @Override
+        protected synchronized void removeRegistration() {
+            try {
+                becomeInactive();
+            } finally {
+                domReg.close();
+            }
+        }
+
+        private void becomeActive() {
+            if (monitorReg == null) {
+                monitorReg = monitor.demandEncountered();
+            }
+        }
+
+        private void becomeInactive() {
+            final var local = monitorReg;
+            if (local != null) {
+                monitorReg = null;
+                local.close();
+            }
         }
     }
 }
---
+++ binding/mdsal-binding-dom-adapter/src/test/java/org/opendaylight/mdsal/binding/dom/adapter/Mdsal881Test.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.adapter;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.mdsal.binding.api.NotificationPublishService;
+import org.opendaylight.mdsal.binding.api.NotificationPublishService.DemandMonitor;
+import org.opendaylight.mdsal.binding.api.NotificationService;
+import org.opendaylight.mdsal.binding.api.NotificationService.Listener;
+import org.opendaylight.mdsal.dom.broker.DOMNotificationRouter;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.bi.ba.notification.rev150205.OutOfPixieDustNotification;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsal.test.binding.rev140701.TwoLevelListChanged;
+import org.opendaylight.yangtools.binding.data.codec.impl.BindingCodecContext;
+import org.opendaylight.yangtools.binding.runtime.spi.BindingRuntimeHelpers;
+import org.opendaylight.yangtools.concepts.Registration;
+
+@ExtendWith(MockitoExtension.class)
+class Mdsal881Test {
+    private static CurrentAdapterSerializer SERIALIZER;
+
+    @Mock
+    private DemandMonitor monitor;
+    @Mock
+    private Registration registration;
+    @Mock
+    private Listener<TwoLevelListChanged> tllListener;
+    @Mock
+    private Listener<OutOfPixieDustNotification> pdListener;
+
+    private DOMNotificationRouter router;
+    private NotificationService notificationService;
+    private NotificationPublishService publishService;
+
+    @BeforeAll
+    static void beforeAll() {
+        SERIALIZER = new CurrentAdapterSerializer(new BindingCodecContext(
+            BindingRuntimeHelpers.createRuntimeContext()));
+    }
+
+    @AfterAll
+    static void afterAll() {
+        SERIALIZER = null;
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        router = new DOMNotificationRouter(100);
+        notificationService = new BindingDOMNotificationServiceAdapter(() -> SERIALIZER, router.notificationService());
+        publishService = new BindingDOMNotificationPublishServiceAdapter(() -> SERIALIZER,
+            router.notificationPublishService());
+    }
+
+    @AfterEach
+    void afterEach() {
+        router.close();
+    }
+
+    @Test
+    void subscriberTriggersDemand() {
+        doReturn(registration).when(monitor).demandEncountered();
+        doNothing().when(registration).close();
+
+        try (var monitorReg = publishService.registerDemandMonitor(TwoLevelListChanged.class, monitor)) {
+            assertNotNull(monitorReg);
+            verifyNoInteractions(monitor);
+
+            try (var listenerReg = notificationService.registerListener(TwoLevelListChanged.class, tllListener)) {
+                assertNotNull(listenerReg);
+                verify(monitor, timeout(100)).demandEncountered();
+                verifyNoInteractions(registration);
+            }
+
+            verify(registration, timeout(100)).close();
+        }
+
+        verifyNoInteractions(tllListener);
+    }
+
+    @Test
+    void differentSubscriberDoesNotTriggerDemand() {
+        try (var monitorReg = publishService.registerDemandMonitor(TwoLevelListChanged.class, monitor)) {
+            assertNotNull(monitorReg);
+
+            try (var listenerReg = notificationService.registerListener(OutOfPixieDustNotification.class, pdListener)) {
+                assertNotNull(listenerReg);
+            }
+        }
+
+        verifyNoInteractions(monitor);
+        verifyNoInteractions(registration);
+        verifyNoInteractions(pdListener);
+    }
+}
