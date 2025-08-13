7ceee60b64f3d11a08aeca6c835524eb974c411b
Robert Varga <robert.varga@pantheon.tech>
2025-08-13 11:47:57 +0200

    Remove ActionServiceFacade
    Our OSGi/Guice wiring is always using two implementations of
    DOMActionProviderService -- ActionServiceFacade and
    RouterDOMActionervice.
    
    This patch factors out the individual services provided into
    RouterDOMAction{Service,AvailabilityExtension}, eliminating the need
    for ForwardingDOMActionService.
    
    Change-Id: I9dda5c602e804ad485a1f9583730f6f8895fc5bb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/RouterDOMActionService.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/RouterDOMActionService.java
@@ -7,28 +7,44 @@
  */
 package org.opendaylight.mdsal.dom.broker;
 
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.mdsal.dom.api.DOMActionService;
-import org.opendaylight.mdsal.dom.spi.ForwardingDOMActionService;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMRpcResult;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 @Singleton
-@Component(service = DOMActionService.class)
-public final class RouterDOMActionService extends ForwardingDOMActionService {
-    private final @NonNull DOMActionService delegate;
+@Component
+@NonNullByDefault
+public final class RouterDOMActionService implements DOMActionService {
+    private final List<Extension> supportedExtensions;
+    private final DOMRpcRouter router;
 
     @Inject
     @Activate
     public RouterDOMActionService(@Reference final DOMRpcRouter router) {
-        delegate = router.actionService();
+        this.router = requireNonNull(router);
+        supportedExtensions = List.of(new RouterDOMActionAvailabilityExtension(router));
     }
 
     @Override
-    protected DOMActionService delegate() {
-        return delegate;
+    public List<Extension> supportedExtensions() {
+        return supportedExtensions;
+    }
+
+    @Override
+    public ListenableFuture<? extends DOMRpcResult> invokeAction(final Absolute type, final DOMDataTreeIdentifier path,
+            final ContainerNode input) {
+        return router.invokeAction(type, path, input);
     }
 }
