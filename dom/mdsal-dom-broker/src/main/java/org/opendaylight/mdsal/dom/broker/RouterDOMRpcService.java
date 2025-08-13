ade5d49ed22c7437d19f49b80ab9cc28980a6f33
Robert Varga <robert.varga@pantheon.tech>
2025-08-13 12:09:06 +0200

    Remove RpcServiceFacade
    Our OSGi/Guice wiring is always using two implementations of
    DOMActionProviderService -- RpcServiceFacade and RouterDOMRpcService.
    
    This patch relocates the single method into DOMRpcRouter, eliminating
    the need for ForwardingDOMRpcService.
    
    Change-Id: I8ad311251604783c945d806c8bac2e0ec7c477be
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/RouterDOMRpcService.java
+++ dom/mdsal-dom-broker/src/main/java/org/opendaylight/mdsal/dom/broker/RouterDOMRpcService.java
@@ -7,28 +7,41 @@
  */
 package org.opendaylight.mdsal.dom.broker;
 
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.util.concurrent.ListenableFuture;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.mdsal.dom.api.DOMRpcAvailabilityListener;
+import org.opendaylight.mdsal.dom.api.DOMRpcResult;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
-import org.opendaylight.mdsal.dom.spi.ForwardingDOMRpcService;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 @Singleton
-@Component(service = DOMRpcService.class)
-public final class RouterDOMRpcService extends ForwardingDOMRpcService {
-    private final @NonNull DOMRpcService delegate;
+@Component
+@NonNullByDefault
+public final class RouterDOMRpcService implements DOMRpcService {
+    private final DOMRpcRouter router;
 
     @Inject
     @Activate
     public RouterDOMRpcService(@Reference final DOMRpcRouter router) {
-        delegate = router.rpcService();
+        this.router = requireNonNull(router);
     }
 
     @Override
-    protected DOMRpcService delegate() {
-        return delegate;
+    public ListenableFuture<? extends DOMRpcResult> invokeRpc(final QName type, final ContainerNode input) {
+        return router.invokeRpc(type, input);
+    }
+
+    @Override
+    public Registration registerRpcListener(final DOMRpcAvailabilityListener listener) {
+        return router.registerRpcListener(listener);
     }
 }
