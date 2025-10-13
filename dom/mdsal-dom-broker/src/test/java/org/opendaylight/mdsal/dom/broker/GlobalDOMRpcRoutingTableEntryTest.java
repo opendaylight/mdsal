b3f0007c7d15e69416ffa657a3032a1c67f159c7
Robert Varga <robert.varga@pantheon.tech>
2025-10-13 17:09:53 +0200

    Migrate GlobalDOMRpcRoutingTableEntryTest
    Use JUnit5, hiding the class and eliminating the use of Hamcrest.
    
    Change-Id: I0ef3ad7cd2db5f613235db17e15e924cbbacb80f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/GlobalDOMRpcRoutingTableEntryTest.java
+++ dom/mdsal-dom-broker/src/test/java/org/opendaylight/mdsal/dom/broker/GlobalDOMRpcRoutingTableEntryTest.java
@@ -7,37 +7,34 @@
  */
 package org.opendaylight.mdsal.dom.broker;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.opendaylight.mdsal.dom.broker.TestUtils.EXCEPTION_TEXT;
 import static org.opendaylight.mdsal.dom.broker.TestUtils.TEST_CONTAINER;
 import static org.opendaylight.mdsal.dom.broker.TestUtils.getTestRpcImplementation;
 
 import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.opendaylight.mdsal.dom.api.DOMRpcImplementation;
 import org.opendaylight.mdsal.dom.api.DOMRpcImplementationNotAvailableException;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
-public class GlobalDOMRpcRoutingTableEntryTest {
+class GlobalDOMRpcRoutingTableEntryTest {
     @Test
-    public void basicTest() {
-        final Map<YangInstanceIdentifier, List<DOMRpcImplementation>> rpcImplementations = new HashMap<>();
-        final List<DOMRpcImplementation> rpcImplementation = new ArrayList<>();
-        final YangInstanceIdentifier yangInstanceIdentifier = YangInstanceIdentifier.builder().build();
+    void basicTest() {
+        final var rpcImplementations = new HashMap<YangInstanceIdentifier, List<DOMRpcImplementation>>();
+        final var rpcImplementation = new ArrayList<DOMRpcImplementation>();
+        final var yangInstanceIdentifier = YangInstanceIdentifier.builder().build();
 
-        final GlobalDOMRpcRoutingTableEntry globalDOMRpcRoutingTableEntry = new GlobalDOMRpcRoutingTableEntry(
+        final var globalDOMRpcRoutingTableEntry = new GlobalDOMRpcRoutingTableEntry(
             TestModel.TEST2_QNAME, new HashMap<>());
         rpcImplementation.add(getTestRpcImplementation());
         rpcImplementations.put(yangInstanceIdentifier, rpcImplementation);
@@ -48,11 +45,11 @@ public class GlobalDOMRpcRoutingTableEntryTest {
         assertTrue(globalDOMRpcRoutingTableEntry.newInstance(rpcImplementations).getImplementations().containsValue(
                 rpcImplementation));
 
-        final ListenableFuture<?> future = OperationInvocation.invoke(
+        final var future = OperationInvocation.invoke(
             globalDOMRpcRoutingTableEntry.newInstance(rpcImplementations), TEST_CONTAINER);
 
-        final var cause = assertThrows(ExecutionException.class, () -> Futures.getDone(future)).getCause();
-        assertThat(cause, instanceOf(DOMRpcImplementationNotAvailableException.class));
-        assertThat(cause.getMessage(), containsString(EXCEPTION_TEXT));
+        final var ee = assertThrows(ExecutionException.class, () -> Futures.getDone(future));
+        final var cause = assertInstanceOf(DOMRpcImplementationNotAvailableException.class, ee.getCause());
+        assertThat(cause.getMessage()).contains(EXCEPTION_TEXT);
     }
 }
