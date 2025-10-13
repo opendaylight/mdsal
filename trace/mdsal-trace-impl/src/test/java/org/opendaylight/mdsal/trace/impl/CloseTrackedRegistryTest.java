74a019ca466ba655c02c3c515b88a8897782c5fc
Robert Varga <robert.varga@pantheon.tech>
2025-10-13 16:55:50 +0200

    Modernize mdsal-trace-impl tests
    Migrate to using JUnit5 + AssertJ.
    
    Change-Id: Ia3afd48cc1c4b8f013e5eca2abeb3fef34ead209
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- trace/mdsal-trace-impl/src/test/java/org/opendaylight/mdsal/trace/impl/CloseTrackedRegistryTest.java
+++ trace/mdsal-trace-impl/src/test/java/org/opendaylight/mdsal/trace/impl/CloseTrackedRegistryTest.java
@@ -7,20 +7,14 @@
  */
 package org.opendaylight.mdsal.trace.impl;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Set;
-import java.util.function.Predicate;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class CloseTrackedRegistryTest {
-
+class CloseTrackedRegistryTest {
     private static class SomethingClosable extends AbstractCloseTracked<SomethingClosable> implements AutoCloseable {
         SomethingClosable(final CloseTrackedRegistry<SomethingClosable> transactionChainRegistry) {
             super(transactionChainRegistry);
@@ -33,38 +27,31 @@ public class CloseTrackedRegistryTest {
     }
 
     @Test
-    public void testDuplicateAllocationContexts() {
-        final CloseTrackedRegistry<SomethingClosable> registry =
-                new CloseTrackedRegistry<>(this, "testDuplicateAllocationContexts", true);
+    void testDuplicateAllocationContexts() {
+        final var registry = new CloseTrackedRegistry<SomethingClosable>(this, "testDuplicateAllocationContexts", true);
 
         for (int i = 0; i < 100; i++) {
-            SomethingClosable isClosedManyTimes = new SomethingClosable(registry);
+            final var isClosedManyTimes = new SomethingClosable(registry);
             isClosedManyTimes.close();
             someOtherMethodWhichDoesNotClose(registry);
         }
         @SuppressWarnings({ "resource", "unused" })
-        SomethingClosable forgotToCloseOnce = new SomethingClosable(registry);
+        final var forgotToCloseOnce = new SomethingClosable(registry);
 
-        Set<CloseTrackedRegistryReportEntry<SomethingClosable>> uniqueNonClosed = registry.getAllUnique();
-        assertThat(uniqueNonClosed, hasSize(2));
-        assertContains(uniqueNonClosed, entry ->
+        final var uniqueNonClosed = registry.getAllUnique();
+        assertThat(uniqueNonClosed).hasSize(2).anyMatch(entry ->
             entry.getNumberAddedNotRemoved() == 100 || entry.getNumberAddedNotRemoved() == 1);
         uniqueNonClosed.forEach(entry -> {
             if (entry.getNumberAddedNotRemoved() == 100) {
-                assertContains(entry.getStackTraceElements(),
+                assertThat(entry.getStackTraceElements()).anyMatch(
                     element -> element.getMethodName().equals("someOtherMethodWhichDoesNotClose"));
             } else if (entry.getNumberAddedNotRemoved() == 1) {
-                assertContains(entry.getStackTraceElements(),
+                assertThat(entry.getStackTraceElements()).anyMatch(
                     element -> element.getMethodName().equals("testDuplicateAllocationContexts"));
             } else {
                 fail("Unexpected number of added, not removed: " + entry.getNumberAddedNotRemoved());
             }
         });
-    }
-
-    // FIXME: use a Matcher
-    private static <T> void assertContains(final Collection<T> collection, final Predicate<T> predicate) {
-        assertTrue("Iterable did not contain any element matching predicate", collection.stream().anyMatch(predicate));
     }
 
     @SuppressWarnings({ "resource", "unused" })
@@ -73,19 +60,17 @@ public class CloseTrackedRegistryTest {
     }
 
     @Test
-    @SuppressWarnings({ "unused", "resource" })
-    public void testDebugContextDisabled() {
-        final CloseTrackedRegistry<SomethingClosable> debugContextDisabledRegistry =
-                new CloseTrackedRegistry<>(this, "testDebugContextDisabled", false);
+    void testDebugContextDisabled() {
+        final var debugContextDisabledRegistry = new CloseTrackedRegistry<SomethingClosable>(this,
+            "testDebugContextDisabled", false);
 
-        SomethingClosable forgotToCloseOnce = new SomethingClosable(debugContextDisabledRegistry);
+        @SuppressWarnings({ "unused", "resource" })
+        final var forgotToCloseOnce = new SomethingClosable(debugContextDisabledRegistry);
 
-        Set<CloseTrackedRegistryReportEntry<SomethingClosable>>
-            closeRegistryReport = debugContextDisabledRegistry.getAllUnique();
-        assertThat(closeRegistryReport, hasSize(1));
+        final var closeRegistryReport = debugContextDisabledRegistry.getAllUnique();
+        assertThat(closeRegistryReport).hasSize(1);
 
-        CloseTrackedRegistryReportEntry<SomethingClosable>
-            closeRegistryReportEntry1 = closeRegistryReport.iterator().next();
+        final var closeRegistryReportEntry1 = closeRegistryReport.iterator().next();
         assertEquals(1, closeRegistryReportEntry1.getNumberAddedNotRemoved());
         assertEquals(List.of(), closeRegistryReportEntry1.getStackTraceElements());
     }
--- trace/mdsal-trace-impl/src/test/java/org/opendaylight/mdsal/trace/test/TracingBrokerTest.java
+++ trace/mdsal-trace-impl/src/test/java/org/opendaylight/mdsal/trace/test/TracingBrokerTest.java
@@ -7,17 +7,18 @@
  */
 package org.opendaylight.mdsal.trace.test;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import org.junit.Test;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeReadWriteTransaction;
 import org.opendaylight.mdsal.dom.api.DOMTransactionChain;
@@ -32,11 +33,14 @@ import org.opendaylight.yangtools.binding.data.codec.api.BindingCodecTree;
  *
  * @author Michael Vorburger.ch
  */
-public class TracingBrokerTest {
+@ExtendWith(MockitoExtension.class)
+class TracingBrokerTest {
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private DOMDataBroker domDataBroker;
+
     @Test
     @SuppressWarnings({ "resource", "unused" }) // Finding resource leaks is the point of this test
-    public void testPrintOpenTransactions() {
-        DOMDataBroker domDataBroker = mock(DOMDataBroker.class, RETURNS_DEEP_STUBS);
+    void testPrintOpenTransactions() {
         Config config = new ConfigBuilder().setTransactionDebugContextEnabled(true).build();
         BindingCodecTree codec = mock(BindingCodecTree.class);
         TracingBroker tracingBroker = new TracingBroker(domDataBroker, config, codec);
@@ -52,23 +56,23 @@ public class TracingBrokerTest {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(baos);
         boolean printReturnValue = tracingBroker.printOpenTransactions(ps, 1);
-        String output = new String(baos.toByteArray(), UTF_8);
+        String output = new String(baos.toByteArray(), StandardCharsets.UTF_8);
 
         assertTrue(printReturnValue);
         // Assert expectations about stack trace
-        assertThat(output, containsString("testPrintOpenTransactions(TracingBrokerTest.java"));
-        assertThat(output, not(containsString(TracingBroker.class.getName())));
+        assertThat(output).contains("testPrintOpenTransactions(TracingBrokerTest.java")
+            .doesNotContain(TracingBroker.class.getName());
 
         String previousLine = "";
         for (String line : output.split("\n")) {
             if (line.contains("(...")) {
-                assertThat(previousLine, not(containsString("(...)")));
+                assertThat(previousLine).doesNotContain("(...)");
             }
             previousLine = line;
         }
 
         // assert that the sorting works - the x3 is shown before the x1
-        assertThat(output, containsString("  DataBroker : newReadWriteTransaction()\n    3x"));
+        assertThat(output).contains("  DataBroker : newReadWriteTransaction()\n    3x");
 
         // We don't do any verify/times on the mocks,
         // because the main point of the test is just to verify that
