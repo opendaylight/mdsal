b63daa78c1d1485d59a89b8d9d71ed7270e6ca9e
Jimmi Wimmersjö <jimmi.wimmersjo@smartoptics.com>
2026-06-10 06:52:51 +0000

    Replicate operational datastore
    Added functionality to also replicate the operational datastore
    if desired.
    Fixed the integration tests.
    
    Jira: MDSAL-896
    Change-Id: I9ce018d28db3d8ff2f2d75dea51d08d1abb6eaf7
    Signed-off-by: Jimmi Wimmersjö <jimmi.wimmersjo@smartoptics.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- replicate/mdsal-replicate-netty/src/test/java/org/opendaylight/mdsal/replicate/netty/IntegrationTest.java
+++ replicate/mdsal-replicate-netty/src/test/java/org/opendaylight/mdsal/replicate/netty/IntegrationTest.java
@@ -8,6 +8,8 @@
 package org.opendaylight.mdsal.replicate.netty;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doCallRealMethod;
@@ -17,7 +19,10 @@ import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
 import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import org.junit.After;
 import org.junit.Before;
@@ -31,6 +36,7 @@ import org.opendaylight.mdsal.binding.dom.adapter.test.AbstractDataBrokerTest;
 import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
 import org.opendaylight.mdsal.dom.api.DOMTransactionChain;
 import org.opendaylight.mdsal.eos.dom.simple.SimpleDOMEntityOwnershipService;
@@ -75,7 +81,7 @@ public class IntegrationTest extends AbstractDataBrokerTest {
      * changes to the Source's datastore and verify that they were all replicated to the Sink
      */
     @Test
-    public void testSourceToSink() throws InterruptedException, ExecutionException {
+    public void testSourceToSink() throws InterruptedException, ExecutionException, UnknownHostException {
         // Make sure to start source...
         try (var source = NettyReplicationSource.createSource(support, getDomBroker(), css, true, TEST_PORT,
             Duration.ZERO, 5)) {
@@ -90,16 +96,33 @@ public class IntegrationTest extends AbstractDataBrokerTest {
             doReturn(sinkTx).when(sinkChain).newWriteOnlyTransaction();
             final DOMDataBroker sinkBroker = mock(DOMDataBroker.class);
             doReturn(sinkChain).when(sinkBroker).createMergingTransactionChain();
+            //Creating the tree for config
+            DOMDataTreeIdentifier confTree = DOMDataTreeIdentifier.of(LogicalDatastoreType.CONFIGURATION,
+                    YangInstanceIdentifier.of());
+
+            //Creating the tree for operational
+            DOMDataTreeIdentifier operTree = DOMDataTreeIdentifier.of(LogicalDatastoreType.OPERATIONAL,
+                    YangInstanceIdentifier.of());
+
+            NettyReplicationConfig config = new NettyReplicationConfig(support,
+                    sinkBroker,
+                    new InetSocketAddress(Inet4Address.getLoopbackAddress(), TEST_PORT),
+                    Duration.ZERO,
+                    Duration.ZERO,
+                    3,
+                    List.of(confTree, operTree));
 
             // Kick of the sink ...
-            try (var sink = NettyReplicationSink.createSink(support, sinkBroker, css, true,
-                Inet4Address.getLoopbackAddress(), TEST_PORT, Duration.ZERO, Duration.ZERO, 3)) {
+            try (var sink = NettyReplicationSink.createSinkReg(css, config)) {
                 // ... and sync on it starting up
 
                 // verify the connection was established and MSG_EMPTY_DATA was transferred
-                verify(sinkBroker, timeout(1000)).createMergingTransactionChain();
+                // now two times because we are doing for both configuration and operational data trees.
+                verify(sinkBroker, timeout(1000).times(2)).createMergingTransactionChain();
                 verify(sinkTx, timeout(1000)).put(eq(LogicalDatastoreType.CONFIGURATION),
                     eq(YangInstanceIdentifier.of()), any(ContainerNode.class));
+                verify(sinkTx, timeout(1000)).put(eq(LogicalDatastoreType.OPERATIONAL),
+                        eq(YangInstanceIdentifier.of()), any(ContainerNode.class));
 
                 // generate some deltas
                 final int deltaCount = 5;
@@ -107,15 +130,16 @@ public class IntegrationTest extends AbstractDataBrokerTest {
 
                 // verify that all the deltas were transferred and committed + 1 invocation from receiving
                 // MSG_EMPTY_DATA
-                verify(sinkChain, timeout(2000).times(deltaCount + 1)).newWriteOnlyTransaction();
-                verify(sinkTx, timeout(2000).times(deltaCount + 1)).commit(any(), any());
+                // now delta+1 times two times because we are doing for both configuration and operational data trees.
+                verify(sinkChain, timeout(2000).times(2 * (deltaCount + 1))).newWriteOnlyTransaction();
+                verify(sinkTx, timeout(2000).times(2 * (deltaCount + 1))).commit(any(), any());
             }
         }
     }
 
     /**
-     * Add some data first and then start replication. Only 1 change is expected to return from the Source and it
-     * should contain the initial state of the Source's datastore.
+     * Add some data first and then start replication. One initial change is expected per subscribed
+     * datastore tree, and each tree should contain the expected initial state from the source.
      */
     @Test
     public void testReplicateInitialState() throws InterruptedException, ExecutionException {
@@ -123,9 +147,13 @@ public class IntegrationTest extends AbstractDataBrokerTest {
         final int deltaCount = 5;
         generateModification(getDataBroker(), deltaCount);
 
+        // expected initial state per tree
+        final NormalizedNode expectedConfigState = generateConfiguationNormalizedNodeForEntities(deltaCount);
+        final NormalizedNode expectedOperationalState = generateOperationalNormalizedNodeForEntities(deltaCount);
+
         // Make sure to start source...
-        try (var source = NettyReplicationSource.createSource(support, getDomBroker(), css, true, TEST_PORT,
-            Duration.ZERO, 5)) {
+        try (var source = NettyReplicationSource.createSource(
+                support, getDomBroker(), css, true, TEST_PORT, Duration.ZERO, 5)) {
             // ... and give it some time start up and open up the port
             Thread.sleep(1000);
 
@@ -135,38 +163,91 @@ public class IntegrationTest extends AbstractDataBrokerTest {
             doReturn(CommitInfo.emptyFluentFuture()).when(sinkTx).commit();
             doCallRealMethod().when(sinkTx).commit(any(), any());
             doReturn(sinkTx).when(sinkChain).newWriteOnlyTransaction();
+
             final DOMDataBroker sinkBroker = mock(DOMDataBroker.class);
             doReturn(sinkChain).when(sinkBroker).createMergingTransactionChain();
 
-            // Kick of the sink ...
-            try (var sink = NettyReplicationSink.createSink(support, sinkBroker, css, true,
-                Inet4Address.getLoopbackAddress(), TEST_PORT, Duration.ZERO, Duration.ZERO, 3)) {
-                // ... and sync on it starting up
+            //Creating the tree for config
+            DOMDataTreeIdentifier confTree = DOMDataTreeIdentifier.of(LogicalDatastoreType.CONFIGURATION,
+                    YangInstanceIdentifier.of());
 
-                // verify the connection was established and MSG_EMPTY_DATA was transferred
-                verify(sinkBroker, timeout(1000)).createMergingTransactionChain();
-                verify(sinkChain, timeout(2000).times(1)).newWriteOnlyTransaction();
+            //Creating the tree for operational
+            DOMDataTreeIdentifier operTree = DOMDataTreeIdentifier.of(LogicalDatastoreType.OPERATIONAL,
+                    YangInstanceIdentifier.of());
 
-                // verify that the initial data invoked onDataTreeChanged() and was transferred to sink
+            NettyReplicationConfig config = new NettyReplicationConfig(support,
+                    sinkBroker,
+                    new InetSocketAddress(Inet4Address.getLoopbackAddress(), TEST_PORT),
+                    Duration.ZERO,
+                    Duration.ZERO,
+                    3,
+                    List.of(confTree, operTree));
+
+            // Kick off the sink ...
+            try (var sink = NettyReplicationSink.createSinkReg(css, config)) {
+                // verify the connection was established for both trees
+                verify(sinkBroker, timeout(1000).times(2)).createMergingTransactionChain();
+                verify(sinkChain, timeout(2000).times(2)).newWriteOnlyTransaction();
+
+                final var storeCaptor = ArgumentCaptor.forClass(LogicalDatastoreType.class);
+                final var pathCaptor = ArgumentCaptor.forClass(YangInstanceIdentifier.class);
                 final var dataCaptor = ArgumentCaptor.forClass(NormalizedNode.class);
-                verify(sinkTx, timeout(2000).times(1)).put(any(), any(), dataCaptor.capture());
-                // verify that the initial state contains everything
-                NormalizedNode capturedInitialState = dataCaptor.getAllValues().iterator().next();
-                NormalizedNode expectedEntityState = generateNormalizedNodeForEntities(deltaCount);
-                assertEquals(expectedEntityState, capturedInitialState);
 
-                verify(sinkTx, timeout(2000).times(1)).commit(any(), any());
+                verify(sinkTx, timeout(2000).times(2)).put(
+                        storeCaptor.capture(),
+                        pathCaptor.capture(),
+                        dataCaptor.capture());
+
+                NormalizedNode capturedConfigState = null;
+                NormalizedNode capturedOperationalState = null;
+
+                for (int i = 0; i < storeCaptor.getAllValues().size(); ++i) {
+                    final LogicalDatastoreType store = storeCaptor.getAllValues().get(i);
+                    final YangInstanceIdentifier path = pathCaptor.getAllValues().get(i);
+                    final NormalizedNode node = dataCaptor.getAllValues().get(i);
+
+                    assertEquals(YangInstanceIdentifier.of(), path);
+
+                    switch (store) {
+                        case CONFIGURATION -> capturedConfigState = node;
+                        case OPERATIONAL -> capturedOperationalState = node;
+                        default -> fail("Unexpected datastore type " + store);
+                    }
+                }
+
+                assertNotNull("CONFIGURATION initial state was not replicated", capturedConfigState);
+                assertNotNull("OPERATIONAL initial state was not replicated", capturedOperationalState);
+
+                assertEquals(expectedConfigState, capturedConfigState);
+                assertEquals(expectedOperationalState, capturedOperationalState);
+
+                verify(sinkTx, timeout(2000).times(2)).commit(any(), any());
             }
         }
     }
 
-    private static ContainerNode generateNormalizedNodeForEntities(final int amount) {
+    private static ContainerNode generateConfiguationNormalizedNodeForEntities(final int amount) {
         final var builder = ImmutableNodes.newSystemMapBuilder().withNodeIdentifier(new NodeIdentifier(ENTITY_QNAME));
         for (int i = 0; i < amount; i++) {
             final var name = "testEntity" + i;
             builder.withChild(ImmutableNodes.newMapEntryBuilder()
                 .withNodeIdentifier(NodeIdentifierWithPredicates.of(ENTITY_QNAME, ENTITY_NAME_QNAME, name))
                 .build());
+        }
+
+        return ImmutableNodes.newContainerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(SchemaContext.NAME))
+                .withChild(builder.build())
+                .build();
+    }
+
+    private static ContainerNode generateOperationalNormalizedNodeForEntities(final int amount) {
+        final var builder = ImmutableNodes.newSystemMapBuilder().withNodeIdentifier(new NodeIdentifier(ENTITY_QNAME));
+        for (int i = 0; i < amount; i++) {
+            final var name = "testEntityOper" + i;
+            builder.withChild(ImmutableNodes.newMapEntryBuilder()
+                    .withNodeIdentifier(NodeIdentifierWithPredicates.of(ENTITY_QNAME, ENTITY_NAME_QNAME, name))
+                    .build());
         }
 
         return ImmutableNodes.newContainerBuilder()
@@ -184,6 +265,14 @@ public class IntegrationTest extends AbstractDataBrokerTest {
             writeTransaction.put(LogicalDatastoreType.CONFIGURATION,
                 DataObjectIdentifier.builder(Entity.class, key).build(), new EntityBuilder().withKey(key).build());
             writeTransaction.commit().get();
+
+            //Quick "fix" of the test, insert the same mod into both stores.
+            final WriteTransaction writeTransactionOper = broker.newWriteOnlyTransaction();
+            final EntityKey keyOper = new EntityKey("testEntityOper" + i);
+            writeTransactionOper.put(LogicalDatastoreType.OPERATIONAL,
+                    DataObjectIdentifier.builder(Entity.class, keyOper).build(),
+                    new EntityBuilder().withKey(keyOper).build());
+            writeTransactionOper.commit().get();
         }
     }
 }
