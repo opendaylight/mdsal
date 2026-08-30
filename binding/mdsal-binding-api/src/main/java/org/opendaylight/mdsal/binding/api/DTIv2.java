1a83fccc8b43a246719c28c4dae3a81b1db08e63
Robert Varga <robert.varga@pantheon.tech>
2026-08-30 10:30:24 +0200

    Add binding.api.DTIv2
    This is a new serialization proxy, guaranteeing serialization does not
    carry InstanceIdentifier.
    
    JIRA: MDSAL-901
    Change-Id: Idae9b0ad522ff63a0f2137f185b33b90c3c8882b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit d75d81dc1aacd5af2a6bb8f0bb25d2c36716a420)

---
+++ binding/mdsal-binding-api/src/main/java/org/opendaylight/mdsal/binding/api/DTIv2.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.api;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yangtools.binding.DataObjectReference;
+
+/**
+ * A serialization proxy for {@link DataTreeIdentifier}.
+ */
+final class DTIv2 implements Externalizable {
+    @java.io.Serial
+    private static final long serialVersionUID = 1L;
+
+    private DataTreeIdentifier<?> id;
+
+    @SuppressWarnings("redundantModifier")
+    public DTIv2() {
+        // For Externalizable
+    }
+
+    DTIv2(final DataTreeIdentifier<?> id) {
+        this.id = requireNonNull(id);
+    }
+
+    @Override
+    public void writeExternal(final ObjectOutput out) throws IOException {
+        id.datastore().writeTo(out);
+        out.writeObject(id.path().toReference());
+    }
+
+    @Override
+    public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+        id = DataTreeIdentifier.of(LogicalDatastoreType.readFrom(in), (DataObjectReference<?>) in.readObject());
+    }
+
+    @java.io.Serial
+    Object readResolve() {
+        return verifyNotNull(id);
+    }
+}
