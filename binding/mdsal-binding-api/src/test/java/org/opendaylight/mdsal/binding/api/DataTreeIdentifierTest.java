5fa3109ea1a230f8670d9ddd37bbc1c8ed79506c
Robert Varga <robert.varga@pantheon.tech>
2026-08-30 09:59:53 +0200

    WIP: Disconnect {DataTree,Instance}Identifier
    Ditch InstanceIdentifier use in signature, which also means that
    LogicalDatastorePath no longer guarantees the underlying path is a
    HierarchicalIdentifier.
    
    // FIXME: contains
    // FIXME: split out DTIv2
    
    Change-Id: Ied201f000fb2a297916bfd65e4be64b25048f607
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-api/src/test/java/org/opendaylight/mdsal/binding/api/DataTreeIdentifierTest.java
+++ binding/mdsal-binding-api/src/test/java/org/opendaylight/mdsal/binding/api/DataTreeIdentifierTest.java
@@ -23,7 +23,6 @@ import org.opendaylight.yangtools.binding.ChildOf;
 import org.opendaylight.yangtools.binding.DataObject;
 import org.opendaylight.yangtools.binding.DataObjectIdentifier;
 import org.opendaylight.yangtools.binding.DataRoot;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 class DataTreeIdentifierTest {
     private static final DataTreeIdentifier<TestDataObject1> TEST_IDENTIFIER1 = DataTreeIdentifier.of(
@@ -34,32 +33,30 @@ class DataTreeIdentifierTest {
     @Test
     void basicTest() {
         assertEquals(LogicalDatastoreType.OPERATIONAL, TEST_IDENTIFIER1.datastore());
-        assertEquals(InstanceIdentifier.create(TestDataObject1.class), TEST_IDENTIFIER1.path());
+        assertEquals(DataObjectIdentifier.builder(TestDataObject1.class).build(), TEST_IDENTIFIER1.path());
     }
 
     @Test
     void containsTest() {
-        assertTrue(TEST_IDENTIFIER1.contains(
-            DataTreeIdentifier.of(LogicalDatastoreType.OPERATIONAL, InstanceIdentifier.create(TestDataObject1.class))));
+        assertTrue(TEST_IDENTIFIER1.contains(DataTreeIdentifier.of(LogicalDatastoreType.OPERATIONAL,
+            DataObjectIdentifier.builder(TestDataObject1.class).build())));
         assertFalse(TEST_IDENTIFIER1.contains(TEST_IDENTIFIER2));
     }
 
     @Test
     void hashCodeTest() {
-        assertEquals(TEST_IDENTIFIER1.hashCode(),
-            DataTreeIdentifier.of(LogicalDatastoreType.OPERATIONAL, InstanceIdentifier.create(TestDataObject1.class))
-                .hashCode());
+        assertEquals(TEST_IDENTIFIER1.hashCode(), DataTreeIdentifier.of(LogicalDatastoreType.OPERATIONAL,
+            DataObjectIdentifier.builder(TestDataObject1.class).build()).hashCode());
         assertNotEquals(TEST_IDENTIFIER1.hashCode(), TEST_IDENTIFIER2.hashCode());
     }
 
     @Test
     void equalsTest() {
         assertEquals(TEST_IDENTIFIER1, TEST_IDENTIFIER1);
-        assertEquals(TEST_IDENTIFIER1,
-            DataTreeIdentifier.of(LogicalDatastoreType.OPERATIONAL,InstanceIdentifier.create(TestDataObject1.class)));
-        assertNotEquals(TEST_IDENTIFIER1,
-            DataTreeIdentifier.of(LogicalDatastoreType.CONFIGURATION,
-                InstanceIdentifier.create(TestDataObject1.class)));
+        assertEquals(TEST_IDENTIFIER1, DataTreeIdentifier.of(LogicalDatastoreType.OPERATIONAL,
+            DataObjectIdentifier.builder(TestDataObject1.class).build()));
+        assertNotEquals(TEST_IDENTIFIER1, DataTreeIdentifier.of(LogicalDatastoreType.CONFIGURATION,
+            DataObjectIdentifier.builder(TestDataObject1.class).build()));
         assertNotEquals(TEST_IDENTIFIER1, TEST_IDENTIFIER2);
         assertNotEquals(TEST_IDENTIFIER1, null);
         assertNotEquals(TEST_IDENTIFIER1, new Object());
@@ -75,8 +72,8 @@ class DataTreeIdentifierTest {
         final var bytes = bos.toByteArray();
         assertEquals("""
             aced0005737200286f72672e6f70656e6461796c696768742e6d6473616c2e62696e64696e672e6170692e445449763100000000000\
-            000010c000078707701017372002c6f72672e6f70656e6461796c696768742e79616e67746f6f6c732e79616e672e62696e64696e67\
-            2e4949763500000000000000010c000078707704000000017372002b6f72672e6f70656e6461796c696768742e79616e67746f6f6c7\
+            000010c000078707701017372002c6f72672e6f70656e6461796c696768742e79616e67746f6f6c732e62696e64696e672e696d706c\
+            2e4f52763100000000000000010c000078707704000000017372002b6f72672e6f70656e6461796c696768742e79616e67746f6f6c7\
             32e62696e64696e672e4e6f64655374657000000000000000000200024c000863617365547970657400114c6a6176612f6c616e672f\
             436c6173733b4c00047479706571007e0005787070767200496f72672e6f70656e6461796c696768742e6d6473616c2e62696e64696\
             e672e6170692e44617461547265654964656e746966696572546573742454657374446174614f626a65637431000000000000000000\
