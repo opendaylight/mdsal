a1bd80d3f34a73434c8a79f5ed831652d56bee92
Robert Varga <robert.varga@pantheon.tech>
2026-08-31 18:07:41 +0200

    Convert mdsal-dom-inmemory-datastore to a module
    Perform a straightforward conversion, switching to bnd-parent.
    
    JIRA: MDSAL-902
    Change-Id: I0453238fa2c188641804be9f0b8b8b6ed597398e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ dom/mdsal-dom-inmemory-datastore/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.mdsal.dom.store.inmemory {
+    exports org.opendaylight.mdsal.dom.store.inmemory;
+
+    requires transitive org.opendaylight.mdsal.dom.spi;
+    requires org.opendaylight.yangtools.yang.data.spi;
+    requires org.opendaylight.yangtools.yang.data.tree;
+    requires org.opendaylight.yangtools.yang.data.tree.spi;
+    requires org.slf4j;
+
+    // Annotations
+    requires static transitive org.eclipse.jdt.annotation;
+//    requires static com.github.spotbugs.annotations;
+//    requires static com.google.errorprone.annotations;
+    requires static org.osgi.annotation.bundle;
+}
